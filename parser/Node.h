5c1baf22f99dca9104434030138be6a9136a932e
Dmitry Petrashko <dmitry@stripe.com>
2017-10-10 16:12:40 -0700

    Generate Ruby AST that can print itself.

--- parser/Node.h
+++ parser/Node.h
@@ -2,6 +2,7 @@
 #include "common/common.h"
 
 #include <memory>
+#include <string>
 #include <vector>
 
 namespace ruby_typer {
@@ -16,7 +17,7 @@ struct Loc {
     u4 begin_pos, end_pos;
 
     static Loc none() {
-        return Loc{(u4)-1, (u4) -1};
+        return Loc{(u4)-1, (u4)-1};
     }
 
     bool is_none() {
@@ -28,8 +29,12 @@ class Node {
 public:
     Node(Loc loc) : loc(loc) {}
     virtual ~Node() = default;
-
+    virtual std::string toString(ast::ContextBase &ctx, int tabs = 0) = 0;
     Loc loc;
+
+protected:
+    void printTabs(std::stringstream &to, int count);
+    void printNode(std::stringstream &to, unique_ptr<Node> &node, ast::ContextBase &ctx, int tabs);
 };
 
 #include "parser/Node_gen.h"
