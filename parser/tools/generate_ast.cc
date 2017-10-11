05f6f40eca22bdec887dea7817e7218623f1101a
Dmitry Petrashko <dmitry@stripe.com>
2017-10-11 10:56:04 -0700

    Document many kinds of ruby AST.

--- parser/tools/generate_ast.cc
+++ parser/tools/generate_ast.cc
@@ -23,93 +23,158 @@ struct NodeDef {
 };
 
 NodeDef nodes[] = {
-
+    // alias bar foo
     {"Alias", vector<FieldDef>({{"from", Node}, {"to", Node}})},
+    // logical and
     {"And", vector<FieldDef>({{"left", Node}, {"right", Node}})},
+    // &&=
     {"AndAsgn", vector<FieldDef>({{"left", Node}, {"right", Node}})},
+    // Wraps every single definition of argument for methods and blocks
     {"Arg", vector<FieldDef>({{"name", Name}})},
+    // Wraps block arg, method arg, and send arg
     {"Args", vector<FieldDef>({{"args", NodeVec}})},
+    // inline array with elements
     {"Array", vector<FieldDef>({{"elts", NodeVec}})},
+    //  ??? not used in gerald.rb
     {"Backref", vector<FieldDef>({{"name", Name}})},
+    //  top level, bodies of classes, modules and methods
     {"Begin", vector<FieldDef>({{"stmts", NodeVec}})},
+    //  Node is always a send, which is previous call, args is arguments of body
     {"Block", vector<FieldDef>({{"send", Node}, {"args", Node}, {"body", Node}})},
+    //  ??? not used in gerald.rb
     {"Blockarg", vector<FieldDef>({{"name", Name}})},
+    //  e.g. map(&:token)
     {"BlockPass", vector<FieldDef>({{"block", Node}})},
     {"Break", vector<FieldDef>({{"exprs", NodeVec}})},
+    // not used in gerald.rb
     {"Case", vector<FieldDef>({{"condition", Node}, {"whens", NodeVec}, {"else_", Node}})},
+    // not used in gerald.rb
     {"Cbase", vector<FieldDef>()},
+    // superclass is Null if emtpy superclass, body is Begin
     {"Class", vector<FieldDef>({{"name", Node}, {"superclass", Node}, {"body", Node}})},
     {"Complex", vector<FieldDef>({{"value", String}})},
+    // Used as path to Select, scope is Null for end of specified list
     {"Const", vector<FieldDef>({{"scope", Node}, {"name", Name}})},
+    // ???, not used in gerald.rb
     {"ConstLhs", vector<FieldDef>({{"scope", Node}, {"name", Name}})},
+    // ???, not used in gerald.rb
     {"ConstAsgn", vector<FieldDef>({{"scope", Node}, {"name", Name}, {"expr", Node}})},
+    // ???, not used in gerald.rb
     {"CSend", vector<FieldDef>({{"receiver", Node}, {"method", Name}, {"args", NodeVec}})},
+    // ???, not used in gerald.rb
     {"CVar", vector<FieldDef>({{"name", Name}})},
+    // ???, not used in gerald.rb
     {"CVarLhs", vector<FieldDef>({{"name", Name}})},
+    // ???, not used in gerald.rb
     {"CVarAsgn", vector<FieldDef>({{"name", Name}, {"expr", Node}})},
+    // args may be NULL, body does not have to be a block.
     {"DefMethod", vector<FieldDef>({{"name", Name}, {"args", Node}, {"body", Node}})},
+    // ???, not used in gerald.rb
     {"Defined", vector<FieldDef>({{"value", Node}})},
+    // ???, not used in gerald.rb
     {"DefS", vector<FieldDef>({{"name", Name}, {"singleton", Node}, {"args", Node}, {"body", Node}})},
+    // string interpolation, all nodes are concatenated in a single string
     {"DString", vector<FieldDef>({{"nodes", NodeVec}})},
+    // ???, not used in gerald.rb
     {"DSymbol", vector<FieldDef>({{"nodes", NodeVec}})},
+    // ???, not used in gerald.rb
     {"EFlipflop", vector<FieldDef>({{"left", Node}, {"right", Node}})},
     {"EncodingLiteral", vector<FieldDef>()},
     {"Ensure", vector<FieldDef>({{"body", Node}, {"ensure", Node}})},
     {"ERange", vector<FieldDef>({{"from", Node}, {"to", Node}})},
     {"False", vector<FieldDef>()},
+    // __FILE__
     {"FileLiteral", vector<FieldDef>()},
+    // ???, not used in gerald.rb
     {"For", vector<FieldDef>({{"vars", Node}, {"expr", Node}, {"body", Node}})},
     {"Float", vector<FieldDef>({{"val", String}})},
+    // ???, not used in gerald.rb
     {"GVar", vector<FieldDef>({{"name", Name}})},
+    // ???, not used in gerald.rb
     {"GVarAsgn", vector<FieldDef>({{"name", Name}, {"expr", Node}})},
+    // ???, not used in gerald.rb
     {"GVarLhs", vector<FieldDef>({{"name", Name}})},
+    // entries are `Pair`s,
     {"Hash", vector<FieldDef>({{"pairs", NodeVec}})},
     {"Ident", vector<FieldDef>({{"name", Name}})},
     {"If", vector<FieldDef>({{"condition", Node}, {"then_", Node}, {"else_", Node}})},
+    // ???, not used in gerald.rb
     {"IFlipflop", vector<FieldDef>({{"left", Node}, {"right", Node}})},
+    // inclusive range. Could the subnodes be non-integers?
     {"IRange", vector<FieldDef>({{"from", Node}, {"to", Node}})},
     {"Integer", vector<FieldDef>({{"val", String}})},
+    // instance variable reference
     {"IVar", vector<FieldDef>({{"name", Name}})},
+    // ivar assign
     {"IVarAsgn", vector<FieldDef>({{"name", Name}, {"expr", Node}})},
+    // @rules in `@rules, invalid_rules = ...`
     {"IVarLhs", vector<FieldDef>({{"name", Name}})},
+    // ???, not used in gerald.rb
     {"Kwarg", vector<FieldDef>({{"name", Name}})},
+    // ???, not used in gerald.rb
     {"Kwbegin", vector<FieldDef>({{"vars", NodeVec}})},
+    // optional arg with default value provided
     {"Kwoptarg", vector<FieldDef>({{"name", Name}, {"default_", Node}})},
     {"Kwrestarg", vector<FieldDef>({{"name", Name}})},
     {"Kwsplat", vector<FieldDef>({{"expr", Node}})},
     {"Lambda", vector<FieldDef>()},
     {"LineLiteral", vector<FieldDef>()},
+    // local variable referense
     {"LVar", vector<FieldDef>({{"name", Name}})},
     {"LVarAsgn", vector<FieldDef>({{"name", Name}, {"expr", Node}})},
+    // invalid_rules in `@rules, invalid_rules = ...`
     {"LVarLhs", vector<FieldDef>({{"name", Name}})},
+    // not used in gerald.rb
     {"MatchAsgn", vector<FieldDef>({{"regex", Node}, {"expr", Node}})},
     {"MatchCurLine", vector<FieldDef>({{"cond", Node}})},
+    // multiple left hand sides: `@rules, invalid_rules = ...`
     {"Masgn", vector<FieldDef>({{"lhs", Node}, {"rhs", Node}})},
+    // multiple left hand sides: `@rules, invalid_rules = ...`
     {"Mlhs", vector<FieldDef>({{"exprs", NodeVec}})},
     {"Module", vector<FieldDef>({{"name", Node}, {"body", Node}})},
+    // next. ??? arguments ???
     {"Next", vector<FieldDef>({{"exprs", NodeVec}})},
     {"Nil", vector<FieldDef>()},
+    // not used in gerald.rb
     {"NthRef", vector<FieldDef>({{"ref", Uint}})},
+    // not used in gerald.rb, seems to be  ${op}=
     {"OpAsgn", vector<FieldDef>({{"left", Node}, {"op", Name}, {"right", Node}})},
+    // not used in gerald.rb, seems to be ||
     {"Or", vector<FieldDef>({{"left", Node}, {"right", Node}})},
+    // not used in gerald.rb, seems to be ||=
     {"OrAsgn", vector<FieldDef>({{"left", Node}, {"right", Node}})},
+    // not used in gerald.rb
     {"Optarg", vector<FieldDef>({{"name", Name}, {"default_", Node}})},
+    // entries of Hash
     {"Pair", vector<FieldDef>({{"key", Node}, {"value", Node}})},
+    // not used in gerald.rb
     {"Postexe", vector<FieldDef>({{"body", Node}})},
+    // not used in gerald.rb
     {"Preexe", vector<FieldDef>({{"body", Node}})},
+    // not used in gerald.rb
     {"Procarg0", vector<FieldDef>({{"arg", Node}})},
+    // not used in gerald.rb
     {"Rational", vector<FieldDef>({{"val", String}})},
+    // not used in gerald.rb
     {"Redo", vector<FieldDef>()},
+    // regular expression. ??? why multiple ???
     {"Regexp", vector<FieldDef>({{"regex", NodeVec}, {"opts", Node}})},
+    // opts of regexp
     {"Regopt", vector<FieldDef>({{"opts", String}})},
+    // not used in gerald.rb
     {"Resbody", vector<FieldDef>({{"exception", Node}, {"var", Node}, {"body", Node}})},
+    // not used in gerald.rb
     {"Rescue", vector<FieldDef>({{"body", Node}, {"rescue", NodeVec}, {"else_", Node}})},
+    // not used in gerald.rb, likely repeated arg
     {"Restarg", vector<FieldDef>({{"name", Name}})},
     {"Retry", vector<FieldDef>()},
     {"Return", vector<FieldDef>({{"exprs", NodeVec}})},
+    // not used in gerald.rb, likely self class
     {"SClass", vector<FieldDef>({{"expr", Node}, {"body", Node}})},
     {"Self", vector<FieldDef>()},
+    // invocation
     {"Send", vector<FieldDef>({{"receiver", Node}, {"method", Name}, {"args", NodeVec}})},
+    // not used in gerald.rb ???
     {"ShadowArg", vector<FieldDef>({{"name", Name}})},
     {"Splat", vector<FieldDef>({{"var", Node}})},
     {"String", vector<FieldDef>({{"val", String}})},
@@ -121,6 +186,7 @@ NodeDef nodes[] = {
     {"UntilPost", vector<FieldDef>({{"cond", Node}, {"body", Node}})},
     {"When", vector<FieldDef>({{"cond", Node}, {"body", Node}})},
     {"While", vector<FieldDef>({{"cond", Node}, {"body", Node}})},
+    // is there a non-syntactic difference in behaviour between post and non-post while?
     {"WhilePost", vector<FieldDef>({{"cond", Node}, {"body", Node}})},
     {"Yield", vector<FieldDef>({{"exprs", NodeVec}})},
     {"ZSuper", vector<FieldDef>()},
