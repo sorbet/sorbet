98946cdac0219c6d7d123305d4c4878d88c292f1
Dmitry Petrashko <dmitry@stripe.com>
2017-10-19 13:46:46 -0700

    Un-std CFG.

--- cfg/CFG.cc
+++ cfg/CFG.cc
@@ -6,17 +6,19 @@
 // helps debugging
 template class std::unique_ptr<ruby_typer::cfg::CFG>;
 
+using namespace std;
+
 namespace ruby_typer {
 namespace cfg {
 
-std::unique_ptr<CFG> CFG::buildFor(ast::Context ctx, ast::MethodDef &md) {
-    std::unique_ptr<CFG> res(new CFG); // private constructor
+unique_ptr<CFG> CFG::buildFor(ast::Context ctx, ast::MethodDef &md) {
+    unique_ptr<CFG> res(new CFG); // private constructor
     res->symbol = md.symbol;
     auto retSym = ctx.state.newTemporary(ast::UniqueNameKind::CFG, ast::Names::returnMethodTemp(), md.symbol);
     auto cont = res->walk(ctx, md.rhs.get(), res->entry(), *res.get(), retSym);
     auto retSym1 = ctx.state.newTemporary(ast::UniqueNameKind::CFG, ast::Names::returnMethodTemp(), md.symbol);
 
-    cont->exprs.emplace_back(retSym1, std::make_unique<Return>(retSym)); // dead assign.
+    cont->exprs.emplace_back(retSym1, make_unique<Return>(retSym)); // dead assign.
     cont->bexit.cond = ast::ContextBase::defn_cfg_never();
     cont->bexit.thenb = res->deadBlock();
     cont->bexit.elseb = res->deadBlock();
@@ -77,13 +79,13 @@ BasicBlock *CFG::walk(ast::Context ctx, ast::Statement *what, BasicBlock *curren
                 body->bexit.elseb = headerBlock;
                 body->bexit.thenb = headerBlock;
             }
-            continueBlock->exprs.emplace_back(target, std::make_unique<Nil>());
+            continueBlock->exprs.emplace_back(target, make_unique<Nil>());
             ret = continueBlock;
         },
         [&](ast::Return *a) {
             auto retSym = ctx.state.newTemporary(ast::UniqueNameKind::CFG, ast::Names::returnTemp(), inWhat.symbol);
             auto cont = walk(ctx, a->expr.get(), current, inWhat, retSym);
-            cont->exprs.emplace_back(target, std::make_unique<Return>(retSym)); // dead assign.
+            cont->exprs.emplace_back(target, make_unique<Return>(retSym)); // dead assign.
             if (cont != deadBlock()) {
                 Error::check(!cont->bexit.cond.exists());
                 cont->bexit.cond = ast::ContextBase::defn_cfg_never();
@@ -126,38 +128,38 @@ BasicBlock *CFG::walk(ast::Context ctx, ast::Statement *what, BasicBlock *curren
             }
         },
         [&](ast::IntLit *a) {
-            current->exprs.emplace_back(target, std::make_unique<IntLit>(a->value));
+            current->exprs.emplace_back(target, make_unique<IntLit>(a->value));
             ret = current;
         },
         [&](ast::FloatLit *a) {
-            current->exprs.emplace_back(target, std::make_unique<FloatLit>(a->value));
+            current->exprs.emplace_back(target, make_unique<FloatLit>(a->value));
             ret = current;
         },
         [&](ast::StringLit *a) {
-            current->exprs.emplace_back(target, std::make_unique<StringLit>(a->value));
+            current->exprs.emplace_back(target, make_unique<StringLit>(a->value));
             ret = current;
         },
         [&](ast::BoolLit *a) {
-            current->exprs.emplace_back(target, std::make_unique<BoolLit>(a->value));
+            current->exprs.emplace_back(target, make_unique<BoolLit>(a->value));
             ret = current;
         },
         [&](ast::ConstantLit *a) {
-            current->exprs.emplace_back(target, std::make_unique<ConstantLit>(a->cnst));
+            current->exprs.emplace_back(target, make_unique<ConstantLit>(a->cnst));
             ret = current;
         },
         [&](ast::Ident *a) {
-            current->exprs.emplace_back(target, std::make_unique<Ident>(a->symbol));
+            current->exprs.emplace_back(target, make_unique<Ident>(a->symbol));
             ret = current;
         },
         [&](ast::Self *a) {
-            current->exprs.emplace_back(target, std::make_unique<Self>(a->claz));
+            current->exprs.emplace_back(target, make_unique<Self>(a->claz));
             ret = current;
         },
         [&](ast::Assign *a) {
             auto lhsIdent = dynamic_cast<ast::Ident *>(a->lhs.get());
             Error::check(lhsIdent != nullptr);
             auto rhsCont = walk(ctx, a->rhs.get(), current, inWhat, lhsIdent->symbol);
-            rhsCont->exprs.emplace_back(target, std::make_unique<Ident>(lhsIdent->symbol));
+            rhsCont->exprs.emplace_back(target, make_unique<Ident>(lhsIdent->symbol));
             ret = rhsCont;
         },
         [&](ast::InsSeq *a) {
@@ -168,7 +170,7 @@ BasicBlock *CFG::walk(ast::Context ctx, ast::Statement *what, BasicBlock *curren
             ret = walk(ctx, a->expr.get(), current, inWhat, target);
         },
         [&](ast::Statement *n) {
-            current->exprs.emplace_back(target, std::make_unique<NotSupported>(""));
+            current->exprs.emplace_back(target, make_unique<NotSupported>(""));
             ret = current;
         });
     /*[&](ast::Break *a) {}, [&](ast::Next *a) {}, [&](ast::Block *a) {},*/
@@ -180,30 +182,30 @@ BasicBlock *CFG::walk(ast::Context ctx, ast::Statement *what, BasicBlock *curren
     return ret;
 }
 
-std::string CFG::toString(ast::Context ctx) {
-    std::stringstream buf;
-    buf << "digraph " << this->symbol.info(ctx).name.name(ctx).toString(ctx) << " {" << std::endl;
-    buf << "    bb0 [shape = invhouse];" << std::endl;
-    buf << "    bb1 [shape = parallelogram];" << std::endl << std::endl;
+string CFG::toString(ast::Context ctx) {
+    stringstream buf;
+    buf << "digraph " << this->symbol.info(ctx).name.name(ctx).toString(ctx) << " {" << endl;
+    buf << "    bb0 [shape = invhouse];" << endl;
+    buf << "    bb1 [shape = parallelogram];" << endl << endl;
     for (int i = 0; i < this->basicBlocks.size(); i++) {
         auto text = this->basicBlocks[i]->toString(ctx);
-        buf << "    bb" << i << " [label = \"" << text << "\"];" << std::endl;
-        auto thenI = std::find_if(this->basicBlocks.begin(), this->basicBlocks.end(),
+        buf << "    bb" << i << " [label = \"" << text << "\"];" << endl;
+        auto thenI = find_if(this->basicBlocks.begin(), this->basicBlocks.end(),
                                   [&](auto &a) { return a.get() == this->basicBlocks[i]->bexit.thenb; });
-        auto elseI = std::find_if(this->basicBlocks.begin(), this->basicBlocks.end(),
+        auto elseI = find_if(this->basicBlocks.begin(), this->basicBlocks.end(),
                                   [&](auto &a) { return a.get() == this->basicBlocks[i]->bexit.elseb; });
-        buf << "    bb" << i << " -> bb" << thenI - this->basicBlocks.begin() << ";" << std::endl;
+        buf << "    bb" << i << " -> bb" << thenI - this->basicBlocks.begin() << ";" << endl;
         if (this->basicBlocks[i]->bexit.cond != ctx.state.defn_cfg_always() &&
             this->basicBlocks[i]->bexit.cond != ctx.state.defn_cfg_never()) {
-            buf << "    bb" << i << " -> bb" << elseI - this->basicBlocks.begin() << ";" << std::endl << std::endl;
+            buf << "    bb" << i << " -> bb" << elseI - this->basicBlocks.begin() << ";" << endl << endl;
         }
     }
     buf << "}";
     return buf.str();
 }
 
-std::string BasicBlock::toString(ast::Context ctx) {
-    std::stringstream buf;
+string BasicBlock::toString(ast::Context ctx) {
+    stringstream buf;
     for (auto &exp : this->exprs) {
         buf << exp.bind.info(ctx).name.name(ctx).toString(ctx) << " = " << exp.value->toString(ctx);
         buf << "\\n"; // intentional! graphviz will do interpolation.
@@ -212,19 +214,19 @@ std::string BasicBlock::toString(ast::Context ctx) {
     return buf.str();
 }
 
-Binding::Binding(const ast::SymbolRef &bind, std::unique_ptr<Instruction> value)
-    : bind(bind), value(std::move(value)) {}
+Binding::Binding(const ast::SymbolRef &bind, unique_ptr<Instruction> value)
+    : bind(bind), value(move(value)) {}
 
 Return::Return(const ast::SymbolRef &what) : what(what) {}
 
-std::string Return::toString(ast::Context ctx) {
+string Return::toString(ast::Context ctx) {
     return "return " + this->what.info(ctx).name.name(ctx).toString(ctx);
 }
 
-New::New(const ast::SymbolRef &claz, std::vector<ast::SymbolRef> &args) : claz(claz), args(std::move(args)) {}
+New::New(const ast::SymbolRef &claz, vector<ast::SymbolRef> &args) : claz(claz), args(move(args)) {}
 
-std::string New::toString(ast::Context ctx) {
-    std::stringstream buf;
+string New::toString(ast::Context ctx) {
+    stringstream buf;
     buf << "new " << this->claz.info(ctx).name.name(ctx).toString(ctx) << "(";
     bool isFirst = true;
     for (auto arg : this->args) {
@@ -238,10 +240,10 @@ std::string New::toString(ast::Context ctx) {
     return buf.str();
 }
 
-Super::Super(std::vector<ast::SymbolRef> &args) : args(std::move(args)) {}
+Super::Super(vector<ast::SymbolRef> &args) : args(move(args)) {}
 
-std::string Super::toString(ast::Context ctx) {
-    std::stringstream buf;
+string Super::toString(ast::Context ctx) {
+    stringstream buf;
     buf << "super(";
     bool isFirst = true;
     for (auto arg : this->args) {
@@ -257,24 +259,24 @@ std::string Super::toString(ast::Context ctx) {
 
 FloatLit::FloatLit(float value) : value(value) {}
 
-std::string FloatLit::toString(ast::Context ctx) {
-    return std::to_string(this->value);
+string FloatLit::toString(ast::Context ctx) {
+    return to_string(this->value);
 }
 
 IntLit::IntLit(int value) : value(value) {}
 
-std::string IntLit::toString(ast::Context ctx) {
-    return std::to_string(this->value);
+string IntLit::toString(ast::Context ctx) {
+    return to_string(this->value);
 }
 
 Ident::Ident(const ast::SymbolRef &what) : what(what) {}
 
-std::string Ident::toString(ast::Context ctx) {
+string Ident::toString(ast::Context ctx) {
     return this->what.info(ctx).name.name(ctx).toString(ctx);
 }
 
-std::string Send::toString(ast::Context ctx) {
-    std::stringstream buf;
+string Send::toString(ast::Context ctx) {
+    stringstream buf;
     buf << this->recv.info(ctx).name.name(ctx).toString(ctx) << "." << this->fun.name(ctx).toString(ctx) << "(";
     bool isFirst = true;
     for (auto arg : this->args) {
@@ -288,11 +290,11 @@ std::string Send::toString(ast::Context ctx) {
     return buf.str();
 }
 
-std::string StringLit::toString(ast::Context ctx) {
+string StringLit::toString(ast::Context ctx) {
     return this->value.name(ctx).toString(ctx);
 }
 
-std::string BoolLit::toString(ast::Context ctx) {
+string BoolLit::toString(ast::Context ctx) {
     if (value) {
         return "true";
     } else {
@@ -300,19 +302,19 @@ std::string BoolLit::toString(ast::Context ctx) {
     }
 }
 
-std::string ConstantLit::toString(ast::Context ctx) {
+string ConstantLit::toString(ast::Context ctx) {
     return this->cnst.name(ctx).toString(ctx);
 }
 
-std::string Nil::toString(ast::Context ctx) {
+string Nil::toString(ast::Context ctx) {
     return "nil";
 }
 
-std::string Self::toString(ast::Context ctx) {
+string Self::toString(ast::Context ctx) {
     return "self";
 }
 
-std::string NotSupported::toString(ast::Context ctx) {
+string NotSupported::toString(ast::Context ctx) {
     return "NotSupported(" + why + ")";
 }
 } // namespace cfg
