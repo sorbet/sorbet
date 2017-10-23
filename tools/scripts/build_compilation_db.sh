8b2cb06b80d45fcb24b7f1d5ffa42d918abeb362
Nelson Elhage <nelhage@nelhage.com>
2017-10-23 10:38:28 -0700

    Refactor compilation DB some
    Stop requiring visibility:public everywhere
    
    Also, `compilation_database` follows dependencies, so we only need to
    name entrypoints, not libraries.

--- tools/scripts/build_compilation_db.sh
+++ tools/scripts/build_compilation_db.sh
@@ -4,5 +4,5 @@ set -e
 
 cd "$(dirname $0)/../.."
 
-bazel build //:compdb
-sed "s,__EXEC_ROOT__,$(bazel info execution_root)," bazel-bin/compile_commands.json > compile_commands.json
+bazel build //tools:compdb
+sed "s,__EXEC_ROOT__,$(bazel info execution_root)," bazel-bin/tools/compile_commands.json > compile_commands.json
