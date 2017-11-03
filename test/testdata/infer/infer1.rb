b09aba443f4de9361ce5d1fd3d76b9910717b947
Dmitry Petrashko <dmitry@stripe.com>
2017-11-02 17:18:52 -0700

    More inference tests.

--- test/testdata/infer/infer1.rb
+++ test/testdata/infer/infer1.rb
@@ -20,8 +20,53 @@ class Integer
   end
 end
 
-def baz
+def baz1
   a = "foo"
-  b = a.getbyte(1)
-  2 + b
+  b = a.getbyte(a) # wrong argument type
 end
+
+def baz2
+  a = "foo"
+  b = a.getbyte("foo") # wrong argument type
+end
+
+def baz3
+  b = "foo".getbyte("foo") # wrong argument type
+end
+
+def baz4
+  b = a.getbyte("foo") # wrong reciever type
+end
+
+def baz5
+ if (true)
+   b = 1
+ else
+   b = "foo"
+ end
+ b = b.getbyte(b) # wrong reciever type
+end
+
+def baz6
+ if (true)
+   b = 1
+ else
+   b = "foo"
+ end
+ b = "foo".getbyte(b) # wrong argument type
+end
+
+def baz7
+ if (true)
+   b = 1
+ end
+ b = "foo".getbyte(b) # wrong argument type type
+end
+
+
+def baz8
+ while (true)
+   b = 1 # cant widen type
+ end
+end
+
