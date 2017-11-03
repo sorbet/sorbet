951a592070f49f2de7fc5f06d5849dfad4825889
Dmitry Petrashko <dmitry@stripe.com>
2017-11-03 14:58:18 -0700

    Update infer test.

--- test/testdata/infer/infer1.rb
+++ test/testdata/infer/infer1.rb
@@ -22,20 +22,20 @@ end
 
 def baz1
   a = "foo"
-  b = a.getbyte(a) # error: wrong argument type
+  b = a.getbyte(a) # error: does not match expected type
 end
 
 def baz2
   a = "foo"
-  b = a.getbyte("foo") # error: wrong argument type
+  b = a.getbyte("foo") # error: does not match expected type
 end
 
 def baz3
-  b = "foo".getbyte("foo") # error: wrong argument type
+  b = "foo".getbyte("foo") # error: does not match expected type
 end
 
 def baz4
-  b = a.getbyte("foo") # error: wrong reciever type
+  b = a.getbyte("foo") # error: does not exist
 end
 
 def baz5
@@ -44,7 +44,7 @@ def baz5
  else
    b = "foo"
  end
- b = b.getbyte(b) # error: wrong reciever type
+ b = b.getbyte(1) # error: does not exist
 end
 
 def baz6
@@ -53,20 +53,20 @@ def baz6
  else
    b = "foo"
  end
- b = "foo".getbyte(b) # error: wrong argument type
+ b = "foo".getbyte(b) # error: does not match expected type
 end
 
 def baz7
  if (true)
    b = 1
  end
- b = "foo".getbyte(b) # error: wrong argument type type
+ b = "foo".getbyte(b) # error: does not match expected type
 end
 
 
 def baz8
  while (true)
-   b = 1 # error: cant widen type
+   b = 1
  end
 end
 
