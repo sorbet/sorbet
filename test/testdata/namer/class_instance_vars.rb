ddcb071e57b81db8b4360bec61a0bb1071bbe8e3
Nelson Elhage <nelhage@nelhage.com>
2017-11-08 12:41:39 -0800

    update tests

--- test/testdata/namer/class_instance_vars.rb
+++ test/testdata/namer/class_instance_vars.rb
@@ -1,4 +1,4 @@
-class A
+class Parent
   declare_variables(
     :@ivar => Integer,
     :@ivar2 => Integer,
@@ -9,5 +9,27 @@ class A
   def hi
     @ivar = 1
     @ivar2
+  end
+end
+
+module Mixin
+  declare_variables(
+    :@@mixin_cvar => Integer,
+  )
+  @@mixin_cvar
+end
+
+class Child < Parent
+  include Mixin
+
+  @@class_var
+  @@mixin_cvar
+  @@undefined_cvar # error: Use of undeclared variable
+
+  def child_method
+    @@class_var
+    @@mixin_cvar
+    @ivar2
+    @undefinedivar # error: Use of undeclared variable
   end
 end
