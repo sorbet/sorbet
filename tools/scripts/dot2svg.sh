150f0a51f29e9928e7d802d6891c6da4b6eb1052
Nelson Elhage <nelhage@nelhage.com>
2017-11-08 15:57:29 -0800

    Only regenerate .svg files if the input changes.
    Even with the exact same version of `graphviz` as @pt, I get different
    output. Let's stabilize the .svg churn by only regenerating them if
    the input changes.

--- tools/scripts/dot2svg.sh
+++ tools/scripts/dot2svg.sh
@@ -7,5 +7,19 @@ rb_src=(
 
 for src in "${rb_src[@]}"; do
     out="$src.svg"
-    dot -Tsvg "$src"  > "$out"
+    if test -f "$out"; then
+        orig_sha=$(tail -n 1 "$out" | grep -Eo 'ruby-types:sha256: [a-f0-9]+' | cut -f2 -d' ')
+    else
+        orig_sha=""
+    fi
+
+    cur_sha=$(shasum -a 256 "$src" | cut -f1 -d ' ')
+    if [ "$orig_sha" = "$cur_sha" ]; then
+        echo "Not regenerating $out..."
+        continue
+    fi
+    (
+        dot -Tsvg "$src"
+        echo "<!-- ruby-types:sha256: $cur_sha -->"
+    ) > "$out"
 done
