5bd8da8319f2076b0af5e31bceb10212b62e83ff
Dmitry Petrashko <dmitry@stripe.com>
2017-11-08 14:30:59 -0800

    Annotate current errors.

--- test/testdata/cfg/side_effects.rb
+++ test/testdata/cfg/side_effects.rb
@@ -1,6 +1,6 @@
 class Side
   def foo
     a = 1
-    a.foo(a, if true; a = true; else a = 2; end);
+    a.foo(a, if true; a = true; else a = 2; end); # error: Method foo does not exist
   end
 end
--- test/testdata/namer/locals.rb
+++ test/testdata/namer/locals.rb
@@ -3,17 +3,17 @@ class TestLocals
     a = 1
     c = 4
 
-    proc do
+    proc do # error: Method proc does not exist on TestLocals
       b = 2
-      a + b
-      proc do
-        b + c
+      a + b # error: Method + does not exist on Integer
+      proc do # error: Method proc does not exist on TestLocals
+        b + c # error: Method + does not exist on Integer
       end
     end
 
     b = 3
 
-    proc do |a; b|
+    proc do |a; b| # error: Method proc does not exist on TestLocals
       a + b
     end
   end
