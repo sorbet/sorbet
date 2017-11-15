35467dc5888e9b0d7cbf3f99e3b998f2a5164cc9
Dmitry Petrashko <dmitry@stripe.com>
2017-11-15 15:43:29 -0800

    Fast-path for casts from T to T

--- common/common.h
+++ common/common.h
@@ -86,6 +86,8 @@ template <class From, class To> To *fast_cast(From *what) {
 #else
     static_assert(false);
 #endif
+    if (std::is_same<From, To>::value)
+        return static_cast<To *>(what);
     if (what == nullptr) {
         return nullptr;
     }
