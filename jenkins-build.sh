bba7a70a4254e53d95d8498a913a1874101e72b8
Nelson Elhage <nelhage@nelhage.com>
2017-11-15 10:22:43 -0800

    Write junit output

--- jenkins-build.sh
+++ jenkins-build.sh
@@ -14,4 +14,10 @@ cp bazelrc-jenkins .bazelrc
 # Disable leak sanatizer. Does not work in docker
 # https://github.com/google/sanitizers/issues/764
 
-bazel test //... --test_output=errors --test_env="ASAN_OPTIONS=detect_leaks=0" --test_env="LSAN_OPTIONS=verbosity=1:log_threads=1"
+err=0
+bazel test //... --test_output=errors --test_env="ASAN_OPTIONS=detect_leaks=0" --test_env="LSAN_OPTIONS=verbosity=1:log_threads=1" || err=$?
+
+mkdir -p /log/junit
+cp -a bazel-testlogs/. /log/junit
+
+exit "$err"
