6d07bf7f1e12f14427bd95fe8d571519b5e07083
Nelson Elhage <nelhage@nelhage.com>
2017-11-15 15:25:36 -0800

    Use the LLVM sanitizer's symbolizer on Linux

--- common/os/linux.cc
+++ common/os/linux.cc
@@ -3,21 +3,31 @@
 #include <cstdio>
 #include <limits.h>
 #include <signal.h>
+#include <sstream>
 #include <string>
 #include <sys/stat.h>
 #include <unistd.h>
 
 using namespace std;
 
+extern "C" {
+// If we are linked against the LLVM sanitizers, this symbol will be
+// replaced with the definition from the sanitizer runtime
+void __attribute__((weak)) __sanitizer_symbolize_pc(void *pc, const char *fmt, char *out_buf, size_t out_buf_size) {
+    snprintf(out_buf, out_buf_size, "%p", pc);
+}
+}
+
 string exec(string cmd);
 
 string addr2line(const string program_name, void const *const *addr, int count) {
-    auto addr2line_cmd = strprintf("atos -o %.256s", program_name.c_str());
+    stringstream os;
     for (int i = 3; i < count; ++i) {
-        addr2line_cmd = strprintf("%s %p", addr2line_cmd.c_str(), addr[i]);
+        char buf[4096];
+        __sanitizer_symbolize_pc(const_cast<void *>(addr[i]), "%p in %f %s:%l:%c", buf, sizeof(buf));
+        os << "  #" << i << " " << buf << "\n";
     }
-
-    return exec(addr2line_cmd);
+    return os.str();
 }
 string getProgramName() {
     char dest[512] = {}; // explicitly zero out
