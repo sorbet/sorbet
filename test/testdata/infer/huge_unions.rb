2b7da3a31de47f3163e9132990ea9f64150fcdca
Dmitry Petrashko <dmitry@stripe.com>
2017-11-17 15:43:02 -0800

    Add a minimized testcase of pay-server failure.

---
+++ test/testdata/infer/huge_unions.rb
@@ -0,0 +1,78 @@
+class C1; end;
+class C2; end;
+class C3; end;
+class C4; end;
+class C5; end;
+class C6; end;
+class C7; end;
+class C8; end;
+class C9; end;
+class C10; end;
+class C11; end;
+class C12; end;
+class C13; end;
+class C13; end;
+class C13; end;
+class C13; end;
+class C13; end;
+class C13; end;
+class C13; end;
+class C14; end;
+class C15; end;
+class C16; end;
+class C17; end;
+class C18; end;
+class C19; end;
+class C20; end;
+
+
+class A
+  def self.send_beta_invitation(invite:)
+    case invite
+      when 1
+        r = C1
+      when 2
+        r = C2
+      when 3
+        r = C3
+      when 4
+        r = C4
+      when 5
+        r = C5
+      when 6
+        r = C6
+      when 7
+        r = C7
+      when 8
+        r = C8
+      when 9
+        r = C9
+      when 10
+        r = C10
+      when 11
+        r = C11
+      when 12
+        r = C12
+      when 13
+        r = C13
+      when 14
+        r = C14
+      when 15
+        r = C15
+      when 16
+        r = C16
+      when 17
+        r = C17
+      when 18
+        r = C18
+      when 19
+        r = C19
+      when 20
+        r = C20
+      else
+        raise "Bla bla bla"
+    end
+    s = r
+    return s
+  end
+end
