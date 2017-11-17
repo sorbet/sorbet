6cbdb054959361a24e51cffdd55bbff35eabebff
Dmitry Petrashko <dmitry@stripe.com>
2017-11-17 13:42:26 -0800

    Revert changes to CI script.

--- ci/stripe-internal-ruby-typer-pay-server.sh
+++ ci/stripe-internal-ruby-typer-pay-server.sh
@@ -12,8 +12,8 @@ cd $DIR
 git checkout "$(cat ci/stripe-internal-ruby-typer-pay-server-sha)"
 cd -
 
-bazel build main:ruby-typer -c opt --config=dbg
+bazel build main:ruby-typer -c opt
 
 # Disable leak sanatizer. Does not work in docker
 # https://github.com/google/sanitizers/issues/764
-find $DIR -name *.rb | sort | ASAN_OPTIONS=detect_leaks=0 LSAN_OPTIONS=verbosity=1:log_threads=1 xargs ./bazel-bin/main/ruby-typer --quiet --trace
+find $DIR -name *.rb | sort | ASAN_OPTIONS=detect_leaks=0 LSAN_OPTIONS=verbosity=1:log_threads=1 xargs ./bazel-bin/main/ruby-typer --quiet
