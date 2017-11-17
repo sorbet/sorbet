ffe651518d8dc6f7673c3b873162003577254c9f
Dmitry Petrashko <dmitry@stripe.com>
2017-11-17 13:41:55 -0800

    Test it.

--- test/testdata/namer/redefines_object.rb
+++ test/testdata/namer/redefines_object.rb
@@ -4,6 +4,11 @@ end
 
 class Trigger
   def trigger
-    @__fake_logger
+    @__fake_logger # error: undeclared variable
   end
+end
+
+class Foo < Bar
+end
+class Bar < Foo # error: Circular dependency
 end
