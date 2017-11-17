224ac371d2a41a3c6c7aae6bf09c60ceb6cdc276
Nelson Elhage <nelhage@nelhage.com>
2017-11-17 12:24:15 -0800

    Initial improvements to argument-matching
    - Optional arguments
    - Initial keyword argument handling

--- test/testdata/infer/arg_matching.rb
+++ test/testdata/infer/arg_matching.rb
@@ -3,9 +3,9 @@ class TestArgs
   end
 
   def call_required
-    required(1) # error: not enough arguments
+    required(1) # error: Not enough arguments
     required(1, 2)
-    required(1, 2, 3) # error: too many arguments
+    required(1, 2, 3) # error: Too many arguments
   end
 
   def optional(a, b=1)
@@ -14,21 +14,21 @@ class TestArgs
   def call_optional
     optional(1)
     optional(1, 2)
-    optional(1, 2, 3) # error: too many arguments
+    optional(1, 2, 3) # error: Too many arguments
   end
 
   standard_method(
     {
       a: Integer,
       b: Integer,
-    }, returns: NilType)
+    }, returns: NilClass)
   def kwarg(a, b:)
   end
 
   def call_kwarg
-    kwarg(1, 2) # error: too many arguments
+    kwarg(1, 2) # error: Too many arguments
     kwarg(1, b: 2)
-    kwarg(1, {}) # error: missing argument b
-    kwarg(1, b: "hi") # error:  Argument `b' does not match expected type.
+    kwarg(1, {}) # error: Missing argument b
+    kwarg(1, b: "hi") # error: Argument `b' does not match expected type.
   end
 end
