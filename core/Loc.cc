b26a8341ffada076550240d529353cd36de5a919
Nelson Elhage <nelhage@nelhage.com>
2017-11-21 09:50:22 -0800

    Loc::offset2Pos: Use lower_bound
    The newline should be considered part of the previous line, which is
    the behavior this version now implements.

--- core/Loc.cc
+++ core/Loc.cc
@@ -14,7 +14,7 @@ Loc::Detail Loc::offset2Pos(core::FileRef source, u4 off, core::GlobalState &gs)
 
     core::File &file = source.file(gs);
     Error::check(off <= file.source().to);
-    auto it = std::upper_bound(file.line_breaks.begin(), file.line_breaks.end(), off);
+    auto it = std::lower_bound(file.line_breaks.begin(), file.line_breaks.end(), off);
     if (it == file.line_breaks.begin()) {
         pos.line = 1;
         pos.column = off + 1;
