db09b665b72c8e506de9cb60f64ac21570f0a192
Dmitry Petrashko <dmitry@stripe.com>
2017-11-21 17:37:38 -0800

    Generate offsetted exp files.

--- tools/scripts/update_exp_files.sh
+++ tools/scripts/update_exp_files.sh
@@ -3,7 +3,7 @@ set -e
 
 passes=(cfg parse-tree ast ast-raw name-table name-tree name-tree-raw)
 
-bazel build //main:ruby-typer
+bazel build //main:ruby-typer --config=unsafe -c opt
 
 rb_src=("$@")
 
@@ -17,8 +17,8 @@ for src in "${rb_src[@]}"; do
     for pass in "${passes[@]}" ; do
         candidate="$src.$pass.exp"
         if [ -e "$candidate" ]; then
-            echo "bazel-bin/main/ruby-typer --no-stdlib --print $pass $src > $candidate"
-            bazel-bin/main/ruby-typer --no-stdlib --print "$pass" "$src" > "$candidate"
+            echo "bazel-bin/main/ruby-typer --print $pass --set-freshNameId=10000 $src > $candidate"
+            bazel-bin/main/ruby-typer --print "$pass" --set-freshNameId=10000 "$src" > "$candidate"
         fi
     done
 done
