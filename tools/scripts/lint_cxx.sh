7d61e1b5798ba7615e9fef774e5879480276db27
Nelson Elhage <nelhage@nelhage.com>
2017-11-27 10:45:07 -0500

    Make `tools/scripts/lint_cxx.sh -fix` work
    (as well as other `clang-tidy` arguments)

--- tools/scripts/lint_cxx.sh
+++ tools/scripts/lint_cxx.sh
@@ -7,8 +7,15 @@ cd $(dirname $0)/../..
 bazel build //tools:clang-tidy
 ./tools/scripts/build_compilation_db.sh
 
-if [ "$#" -gt 0 ]; then
-    cxx_src=("$@")
+got_cc=
+for arg in "$@"; do
+    if [ "${arg%.cc}" != "$arg" ]; then
+        got_cc=1
+        break
+    fi
+done
+if [ "$got_cc" ]; then
+    cxx_src=()
 else
     cxx_src=(
         $(find . -path ./third_party -prune -false -o -name ".?*" -prune -false -o -name '*.cxx' -o -name '*.cc' )
@@ -18,4 +25,4 @@ fi
 # Explicitly pass our configuration. Somehow clang-tidy gets confused
 # and doesn't respect it even though `-list-checks` and
 # `-explain-config` appear to.
-exec bazel-bin/tools/clang-tidy -config "$(cat .clang-tidy)" -quiet "${cxx_src[@]}"
+exec bazel-bin/tools/clang-tidy -config "$(cat .clang-tidy)" -quiet "$@" "${cxx_src[@]}"
