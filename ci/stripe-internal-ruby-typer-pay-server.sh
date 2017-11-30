09771d5644c689f223718df462620b5d8dea2733
ptarjan <github@paulisageek.com>
2017-11-30 14:22:45 -0800

    use scripts/ruby-types/typecheck

--- ci/stripe-internal-ruby-typer-pay-server.sh
+++ ci/stripe-internal-ruby-typer-pay-server.sh
@@ -3,40 +3,38 @@ set -eux
 
 DIR=./pay-server
 
+# This is what we'll ship to our users
+bazel build main:ruby-typer --config=unsafe -c opt
+export PATH=$PATH:"$(pwd)/bazel-bin/main/"
+
 if [ ! -d $DIR ]; then
     echo "$DIR doesn't exist"
     exit 1
 fi
-
 cd $DIR
+
 if [ ! -f "../ci/stripe-internal-ruby-typer-pay-server-sha" ]; then
     echo "ci/stripe-internal-ruby-typer-pay-server-sha doesn't exist"
     exit 1
 fi
 git checkout "$(cat ../ci/stripe-internal-ruby-typer-pay-server-sha)"
-cd -
-
-# This is what we'll ship to our users
-bazel build main:ruby-typer --config=unsafe -c opt
-
-ALL_FILES="$(mktemp)"
-find $DIR -name *.rb | sort > "$ALL_FILES"
 
 # Make sure these specific files are typed
-for f in $(cat ci/stripe-internal-ruby-typer-pay-server-typechecked); do
+for f in $(cat ../ci/stripe-internal-ruby-typer-pay-server-typechecked); do
     echo "# @typed" >> "$f"
 done
 
 OUT=$(mktemp)
-./bazel-bin/main/ruby-typer @"$ALL_FILES" > "$OUT" 2>&1
+./scripts/ruby-types/typecheck 2>&1 | tee "$OUT"
 if [ -s "$OUT" ]; then
-    cat "$OUT"
     exit 1
 fi
 
+cd -
 # Make sure we don't crash on all of pay-server with ASAN on
 bazel build main:ruby-typer -c opt
+cd -
 
 # Disable leak sanatizer. Does not work in docker
 # https://github.com/google/sanitizers/issues/764
-ASAN_OPTIONS=detect_leaks=0 LSAN_OPTIONS=verbosity=1:log_threads=1 /usr/bin/time -v ./bazel-bin/main/ruby-typer --quiet --error-stats --typed=always @"$ALL_FILES"
+ASAN_OPTIONS=detect_leaks=0 LSAN_OPTIONS=verbosity=1:log_threads=1 /usr/bin/time -v ./scripts/ruby-types/typecheck --quiet --error-stats --typed=always
