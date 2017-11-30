7083e7316de2a00b9fc7cf89fae107d948f8a51e
ptarjan <github@paulisageek.com>
2017-11-30 11:55:40 -0800

    add test to make sure all of pay-server is clean

--- ci/stripe-internal-ruby-typer-pay-server.sh
+++ ci/stripe-internal-ruby-typer-pay-server.sh
@@ -16,21 +16,27 @@ fi
 git checkout "$(cat ../ci/stripe-internal-ruby-typer-pay-server-sha)"
 cd -
 
+# This is what we'll ship to our users
+bazel build main:ruby-typer --config=unsafe -c opt
+
+ALL_FILES="$(mktemp)"
+find $DIR -name *.rb | sort > "$ALL_FILES"
+
+# Make sure these specific files are typed
+for f in $(cat ci/stripe-internal-ruby-typer-pay-server-typechecked); do
+    echo "# @typed" >> "$f"
+done
+
+OUT=$(mktemp)
+./bazel-bin/main/ruby-typer @"$ALL_FILES" > "$OUT" 2>&1
+if [ -s "$OUT" ]; then
+    cat "$OUT"
+    exit 1
+fi
+
+# Make sure we don't crash on all of pay-server with ASAN on
 bazel build main:ruby-typer -c opt
 
 # Disable leak sanatizer. Does not work in docker
 # https://github.com/google/sanitizers/issues/764
-export ASAN_OPTIONS=detect_leaks=0
-
-# Make sure these files have 0 type errors
-TMP=$(mktemp)
-./bazel-bin/main/ruby-typer @ci/stripe-internal-ruby-typer-pay-server-typechecked > "$TMP" 2>&1
-if [ -s "$TMP" ]; then
-    cat "$TMP"
-    exit 1
-fi
-
-# Make sure we don't crash on all of pay-server
-TMP="$(mktemp)"
-find $DIR -name *.rb | sort > "$TMP"
-LSAN_OPTIONS=verbosity=1:log_threads=1 /usr/bin/time -v ./bazel-bin/main/ruby-typer --quiet --error-stats --typed=always @"$TMP"
+ASAN_OPTIONS=detect_leaks=0 LSAN_OPTIONS=verbosity=1:log_threads=1 /usr/bin/time -v ./bazel-bin/main/ruby-typer --quiet --error-stats --typed=always @"$ALL_FILES"
