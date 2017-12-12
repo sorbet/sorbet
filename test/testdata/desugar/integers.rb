3dc8992f82761fac541337fe08cfd9f41712e818
Paul Tarjan <pt@stripe.com>
2017-12-12 22:53:04 +0000

    handle other number bases in the lexer
    (Squashed by Merge Bot)

--- test/testdata/desugar/integers.rb
+++ test/testdata/desugar/integers.rb
@@ -4,6 +4,6 @@ def test_large_int
     9223372036854775807,
     -9223372036854775808,
     18446744073709551616,   # error: Unsupported large integer
-    0xcafe, # error: Unsupported integer
+    0xcafe,
   ]
 end
