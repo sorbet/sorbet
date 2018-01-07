ea1d9882d4b57da8b5541aef24293d5847bde5f7
Paul Tarjan <pt@stripe.com>
2018-01-07 20:33:57 +0000

    stop using -r flag
    (Squashed by Merge Bot)

--- test/end-to-end-test.sh
+++ test/end-to-end-test.sh
@@ -3,4 +3,4 @@ set -eux
 
 # re-record with tools/scripts/update_exp_files.sh
 
-diff -u test/end-to-end-test-output <(main/ruby-typer test/end-to-end-test-input.rb 2>&1 | sed -r -e 's,(rbi/stdlib.rbi:)[0-9]+,\1__LINE__,')
+diff -u test/end-to-end-test-output <(main/ruby-typer test/end-to-end-test-input.rb 2>&1 | sed -e 's,\(rbi/stdlib.rbi:\)[0-9]*,\1__LINE__,')
