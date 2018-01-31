900bca2af3fdecae746e6ec8e2d72d1d57928394
Nelson Elhage <nelhage@stripe.com>
2018-01-31 22:47:43 +0000

    build_compilation_db: use the bazel symlink
    (Squashed by Merge Bot)

--- tools/scripts/build_compilation_db.sh
+++ tools/scripts/build_compilation_db.sh
@@ -5,4 +5,4 @@ set -e
 cd "$(dirname "$0")/../.."
 
 bazel build //tools:compdb
-sed "s,__EXEC_ROOT__,$(bazel info execution_root)," bazel-bin/tools/compile_commands.json > compile_commands.json
+sed "s,__EXEC_ROOT__,$(pwd)/bazel-ruby-typer," bazel-bin/tools/compile_commands.json > compile_commands.json
