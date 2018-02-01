33694922208a706049254822c9410c2bd9c446b0
Dmitry Petrashko <dmitry@stripe.com>
2018-02-01 00:33:06 +0000

    Fix isSubtype, improve lower upper bound.
    (Squashed by Merge Bot)

---
+++ test/testdata/infer/or_and_and_or.rb
@@ -0,0 +1,24 @@
+# @typed
+module Fake
+end
+class Test
+
+  sig.returns(T.untyped)
+  def untyped
+  end
+
+  sig.returns(T.all(T.any(Integer, Float), Fake))
+  def and_or
+    untyped
+  end
+
+  sig.returns(T.any(T.all(Integer, Fake), T.all(Float, Fake)))
+  def or_and
+    untyped
+  end
+
+  def test
+    T.assert_type!(or_and, T.all(T.any(Integer, Float), Fake))
+    T.assert_type!(and_or, T.any(T.all(Integer, Fake), T.all(Float, Fake)))
+  end
+end
