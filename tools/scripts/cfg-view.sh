2b76e597f5a530f52cebd198bce73f14f35acc65
Paul Tarjan <pt@stripe.com>
2018-03-01 00:41:29 +0000

    keep going even if there are errors
    (Squashed by Merge Bot)

--- tools/scripts/cfg-view.sh
+++ tools/scripts/cfg-view.sh
@@ -15,6 +15,6 @@ if [ ! -f "$src" ]; then
 fi
 
 dir="$( dirname "${BASH_SOURCE[0]}" )"
-"$dir"/../../bazel-bin/main/ruby-typer --print cfg "$src" > "$dot"
+"$dir"/../../bazel-bin/main/ruby-typer --suppress-non-critical --print cfg "$src" > "$dot"
 dot -Tsvg "$dot" > "$svg"
 open -a "Google Chrome" "$svg"
