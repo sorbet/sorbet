def9001b1ee466bf634f0200413b35f726d913d7
Paul Tarjan <pt@stripe.com>
2018-05-03 22:36:27 +0000

    Merge pull request #958 from stripe-internal/pt-delete-some-phase
    stop recording things that change a lot
    
    (Squashed by Merge Bot)

--- test/cli/phases/phases.sh
+++ test/cli/phases/phases.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 set -eu
 
-# Makes sure all these options don't crash us
-for p in parse-tree parse-tree-json ast ast-raw dsl-tree dsl-tree-raw name-table name-table-json name-table-full name-tree name-tree-raw cfg cfg-raw typed-source; do
+# Make sure these options don't change output
+for p in parse-tree parse-tree-json ast ast-raw dsl-tree dsl-tree-raw name-table name-tree name-tree-raw cfg cfg-raw typed-source; do
     echo "--- $p start ---"
     main/ruby-typer -p "$p" -e '1'
     echo "--- $p end ---"
+done
+
+# Makes sure all these options don't crash us
+for p in name-table-json name-table-full; do
+    main/ruby-typer -p "$p" -e '1' > /dev/null
 done
 
 for p in init parser desugarer dsl namer cfg inferencer; do
