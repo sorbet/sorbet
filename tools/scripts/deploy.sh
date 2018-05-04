ba6c0815d1fee9ca4f5777415c1d6febffa3fa59
Nelson Elhage <nelhage@stripe.com>
2018-05-04 22:20:11 +0000

    Merge pull request #968 from stripe-internal/nelhage-deploy
    Remove the henson service + deploy script
    
    (Squashed by Merge Bot)

--- tools/scripts/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# push the pay-server tag
-git push origin origin/master:pay-server
-
-# send the binary to dev servers
-henson deploy --qa ruby-typer -r origin/pay-server --quit-after-success
