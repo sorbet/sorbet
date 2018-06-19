b229928899927ffad5f348ce4119ac83f1bb1ed6
Nelson Elhage <nelhage@stripe.com>
2018-06-19 20:04:51 +0000

    Merge pull request #1112 from stripe-internal/nelhage-fix-master
    Delete a broken test
    
    (Squashed by Merge Bot)

--- test/testdata/dsl/coerce_in_prop.rb
+++
@@ -1,6 +0,0 @@
-  class Inputs
-    PAYMENT_METHODS_HASH = {a: Integer}
-
-    optional :supported_payment_methods1, T.coerce(PAYMENT_METHODS_HASH)
-    optional :supported_payment_methods2, T.coerce({a: Integer})
-  end
