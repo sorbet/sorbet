7f0e257ca9352330ddeb98d8688801a4bf37d869
Paul Tarjan <pt@stripe.com>
2018-06-22 00:55:45 +0000

    Merge pull request #1121 from stripe-internal/pt-SUBCLASSES
    suggest autogen.rb
    
    (Squashed by Merge Bot)

--- core/Symbols.h
+++ core/Symbols.h
@@ -814,6 +814,10 @@ public:
         return SymbolRef(nullptr, 52);
     }
 
+    static SymbolRef Subclasses() {
+        return SymbolRef(nullptr, 53);
+    }
+
     static constexpr int MAX_PROC_ARITY = 10;
     static SymbolRef Proc0() {
         return SymbolRef(nullptr, MAX_SYNTHETIC_SYMBOLS - MAX_PROC_ARITY * 2 - 2);
