452d2064216ebc6344ace5f047cdde0ff8583929
Alexander Thiemann <alexander@stripe.com>
2018-07-09 07:57:55 +0000

    Merge pull request #1175 from stripe-internal/alexander/present-flow
    Implement flow support for present?
    
    (Squashed by Merge Bot)

---
+++ test/testdata/infer/control_flow/present_p.rb
@@ -0,0 +1,41 @@
+# typed: strict
+
+class NilClass
+  sig.returns(FalseClass)
+  def present?
+    false
+  end
+end
+
+class Object
+  def present?
+    !self
+  end
+end
+
+class A
+  sig(s: T.nilable(String)).returns(NilClass)
+  def test_return(s)
+    if s.present?
+      s.length
+      nil
+    end
+    nil
+  end
+
+  sig(s: String).returns(NilClass)
+  def test_unchanged(s)
+    if s.present?
+      s.length
+    else
+      s.length
+    end
+    nil
+  end
+
+  def unreachable()
+    if nil.present?
+      "foo" # error: This code is unreachable
+    end
+  end
+end
