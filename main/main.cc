991b7a37f34ede1b5e2f819d5cd2a3b58a19522c
Dmitry Petrashko <dmitry@stripe.com>
2018-08-20 10:26:07 -0700

    install abseil singal hanlder (#1350)
    install abseil singal hanlder (#1350)
    
    (Squashed by Merge Bot)

--- main/main.cc
+++ main/main.cc
@@ -1,7 +1,13 @@
+#include "absl/debugging/failure_signal_handler.h"
+#include "absl/debugging/symbolize.h"
 #include "common/common.h"
 #include "main/realmain.h"
 int main(int argc, char *argv[]) {
     try {
+        // Initialize the symbolizer to get a human-readable stack trace
+        absl::InitializeSymbolizer(argv[0]);
+        absl::FailureSignalHandlerOptions options;
+        absl::InstallFailureSignalHandler(options);
         return sorbet::realmain::realmain(argc, argv);
     } catch (sorbet::realmain::options::EarlyReturnWithCode &c) {
         return c.returnCode;
