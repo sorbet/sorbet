f42391c1fc78865f5748e534bf2feef1f8421921
Paul Tarjan <pt@stripe.com>
2018-11-15 10:22:30 -0800

    tell you to update sha.html too (#1668)
    tell you to update sha.html too (#1668)
    
    (Squashed by Merge Bot)

--- tools/scripts/update-sorbet.run.sh
+++ tools/scripts/update-sorbet.run.sh
@@ -10,3 +10,4 @@ echo
 echo "./bazel-bin/emscripten:sorbet-wasm.tar should contain \".wasm\" and \".js\" file"
 echo "Please copy them to your clone of sorbet.run repo and make a commit"
 echo "     tar -C ../sorbet.run/docs/ -xvf ./bazel-bin/emscripten/sorbet-wasm.tar sorbet-wasm.wasm sorbet-wasm.js"
+echo "     git rev-parse HEAD > ../sorbet.run/docs/sha.html"
