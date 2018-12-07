52ad0593794ae8dc5a15f522a59770cca826b12a
Dmitry Petrashko <dmitry@stripe.com>
2018-12-06 16:34:14 -0800

    Fix a non-intuitive error (#1709)
    Fix a non-intuitive error (#1709)
    
    (Squashed by Merge Bot)

--- tools/scripts/build_compilation_db.sh
+++ tools/scripts/build_compilation_db.sh
@@ -4,5 +4,5 @@ set -e
 
 cd "$(dirname "$0")/../.."
 
-bazel build //tools:compdb
+bazel build //tools:compdb --config=dbg
 sed "s,__EXEC_ROOT__,$(pwd)/bazel-sorbet," bazel-bin/tools/compile_commands.json > compile_commands.json
