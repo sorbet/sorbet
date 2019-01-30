0986a89845d99067bce0df6deed9591185561fde
Dmitry Petrashko <dmitry@stripe.com>
2019-01-30 14:53:51 -0800

    Comment out old unused namer error codes (#1857)
    Comment out old unused namer error codes (#1857)
    
    * Comment out old unused namer error codes
    
    * Format
    
    (Squashed by Merge Bot)

--- core/errors/namer.h
+++ core/errors/namer.h
@@ -6,8 +6,8 @@ namespace sorbet::core::errors::Namer {
 constexpr ErrorClass IncludeMutipleParam{4001, StrictLevel::Stripe};
 constexpr ErrorClass IncludeNotConstant{4002, StrictLevel::Stripe};
 constexpr ErrorClass IncludePassedBlock{4003, StrictLevel::Stripe};
-constexpr ErrorClass DynamicConstantDefinition{4004, StrictLevel::Typed};
-constexpr ErrorClass DynamicMethodDefinition{4005, StrictLevel::Typed};
+// constexpr ErrorClass DynamicConstantDefinition{4004, StrictLevel::Typed};
+// constexpr ErrorClass DynamicMethodDefinition{4005, StrictLevel::Typed};
 constexpr ErrorClass SelfOutsideClass{4006, StrictLevel::Typed};
 constexpr ErrorClass DynamicDSLInvocation{4007, StrictLevel::Typed};
 constexpr ErrorClass MethodNotFound{4008, StrictLevel::Typed};
