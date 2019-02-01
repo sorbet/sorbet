28c6d72336b7b01ffb7d6f3ea27caf67be850a1d
Jacob Zimmerman <jez@stripe.com>
2019-01-31 21:45:26 -0800

    Another type alias test (#1870)
    Another type alias test (#1870)
    
    (Squashed by Merge Bot)

--- test/testdata/resolver/type_alias.rb
+++ test/testdata/resolver/type_alias.rb
@@ -15,13 +15,42 @@ module Main
 end
 
 module A
-  StringAlias
+  puts StringAlias
 end
 StringAlias = T.type_alias(String)
 
 
 IntegerAlias = T.type_alias(Integer)
 module B
-  IntegerAlias
+  puts IntegerAlias
 end
 
+# Stress-testing nesting / scope resolution of type aliases
+
+class Outside
+  class Inside
+    def inside
+      puts RootAlias
+      puts OutsideAlias
+      puts InsideAlias
+    end
+
+    InsideAlias = T.type_alias(Symbol)
+  end
+
+  def outside
+    puts RootAlias
+    puts OutsideAlias
+    puts InsideAlias # error: Unable to resolve constant `InsideAlias`
+  end
+
+  OutsideAlias = T.type_alias(String)
+end
+
+def main
+  puts RootAlias
+  puts OutsideAlias # error: Unable to resolve constant `OutsideAlias`
+  puts InsideAlias # error: Unable to resolve constant `InsideAlias`
+end
+
+RootAlias = T.type_alias(Integer)
