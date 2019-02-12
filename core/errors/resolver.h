71808642be947a34f3ae5d167458df93187e861b
Paul Tarjan <pt@stripe.com>
2019-02-12 12:12:46 -0800

    make commenting consistent (#1921)
    make commenting consistent (#1921)
    
    (Squashed by Merge Bot)

--- core/errors/resolver.h
+++ core/errors/resolver.h
@@ -9,9 +9,9 @@ constexpr ErrorClass InvalidMethodSignature{5003, StrictLevel::Stripe};
 constexpr ErrorClass InvalidTypeDeclaration{5004, StrictLevel::Stripe};
 constexpr ErrorClass InvalidDeclareVariables{5005, StrictLevel::Typed};
 constexpr ErrorClass DuplicateVariableDeclaration{5006, StrictLevel::Typed};
-/* constexpr ErrorClass UndeclaredVariable{5007, StrictLevel::Strict}; */
+// constexpr ErrorClass UndeclaredVariable{5007, StrictLevel::Strict};
 constexpr ErrorClass DynamicSuperclass{5008, StrictLevel::Typed};
-/* constexpr ErrorClass InvalidAttr{5009, StrictLevel::Typed}; */
+// constexpr ErrorClass InvalidAttr{5009, StrictLevel::Typed};
 constexpr ErrorClass InvalidCast{5010, StrictLevel::Stripe};
 constexpr ErrorClass CircularDependency{5011, StrictLevel::Stripe};
 constexpr ErrorClass RedefinitionOfParents{5012, StrictLevel::Stripe};
