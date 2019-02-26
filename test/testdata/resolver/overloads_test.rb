40bf83cba7b9f62575aa7632f1f06cb0c166fe2a
Jacob Zimmerman <jez@stripe.com>
2019-02-26 00:50:36 -0800

    Update overloads_test.rb (#2014)
    Update overloads_test.rb (#2014)
    
    I think this can help prevent against us regressing this behavior in the
    future (I almost regressed on this in another branch of me).
    
    (Squashed by Merge Bot)

--- test/testdata/resolver/overloads_test.rb
+++ test/testdata/resolver/overloads_test.rb
@@ -1,4 +1,7 @@
-# typed: false
+# typed: true
+
+extend T::Sig
+
 sig {params(s: Integer).void}
 sig {params(s: String).void}
 def f(s); end
@@ -6,3 +9,8 @@ def f(s); end
 sig {params(s: Symbol).void}
 sig {params(s: Float).void}
 def f(s); end
+
+f(0) # error: `Integer(0)` doesn't match `Symbol` for argument `s`
+f('') # error: `String("")` doesn't match `Symbol` for argument `s`
+f(:s)
+f(0.0)
