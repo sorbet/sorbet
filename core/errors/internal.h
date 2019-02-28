d5d1665e6be958c910293770d5708676ac5471db
Paul Tarjan <pt@stripe.com>
2019-02-27 20:37:39 -0800

    make this consistent with other files (#2049)
    make this consistent with other files (#2049)
    
    (Squashed by Merge Bot)

--- core/errors/internal.h
+++ core/errors/internal.h
@@ -3,9 +3,9 @@
 #include "core/Error.h"
 
 namespace sorbet::core::errors::Internal {
-constexpr ErrorClass InternalError = {1001, StrictLevel::Internal};
-constexpr ErrorClass WrongSigil = {1002, StrictLevel::Internal};
-constexpr ErrorClass CyclicReferenceError = {1003, StrictLevel::Internal};
+constexpr ErrorClass InternalError{1001, StrictLevel::Internal};
+constexpr ErrorClass WrongSigil{1002, StrictLevel::Internal};
+constexpr ErrorClass CyclicReferenceError{1003, StrictLevel::Internal};
 } // namespace sorbet::core::errors::Internal
 
 #endif
