5d767f46d4c4994ae6dfbe60d144613fa54059e5
Mark Hudnall <mark.hudnall@coinbase.com>
2019-03-21 15:47:18 -0700

    Split RbiGenerator into separate classes

--- rbi-generation/lib/gem-generator-tracepoint.rb
+++ rbi-generation/lib/gem-generator-tracepoint.rb
@@ -13,30 +13,33 @@ require 'set'
 alias DelegateClass_without_rbi_generator DelegateClass
 def DelegateClass(superclass)
   result = DelegateClass_without_rbi_generator(superclass)
-  SorbetRBIGeneration::RbiGenerator.register_delegate_class(superclass, result)
+  SorbetRBIGeneration::Tracer.register_delegate_class(superclass, result)
   result
 end
 
-# TODO switch the Struct handling to:
-#
-# class Subclass < Struct(:key1, :key2)
-# end
-#
-# generating:
-#
-# TemporaryStruct = Struct(:key1, :key2)
-# class Subclass < TemporaryStruct
-# end
-#
-# instead of manually defining every getter/setter
+class SorbetRBIGeneration::Module
+  @real_is_a = Module.instance_method(:is_a?)
+  @real_name = Module.instance_method(:name)
+  @real_object_id = Module.instance_method(:object_id)
 
-class SorbetRBIGeneration::RbiGenerator
-  SPECIAL_METHOD_NAMES = %w[! ~ +@ ** -@ * / % + - << >> & | ^ < <= => > >= == === != =~ !~ <=> [] []= `]
+  def self.real_is_a?(klass, type)
+    @real_is_a.bind(klass).call(type)
+  end
 
+  def self.real_name(klass)
+    @real_name.bind(klass).call
+  end
+
+  def self.real_object_id(klass)
+    @real_object_id.bind(klass).call
+  end
+end
+
+class SorbetRBIGeneration::Tracer
   module ModuleOverride
     def include(mod, *smth)
       result = super
-      SorbetRBIGeneration::RbiGenerator.module_included(mod, self)
+      SorbetRBIGeneration::Tracer.module_included(mod, self)
       result
     end
   end
@@ -45,7 +48,7 @@ class SorbetRBIGeneration::RbiGenerator
   module ObjectOverride
     def extend(mod, *args)
       result = super
-      SorbetRBIGeneration::RbiGenerator.module_extended(mod, self)
+      SorbetRBIGeneration::Tracer.module_extended(mod, self)
       result
     end
   end
@@ -54,14 +57,14 @@ class SorbetRBIGeneration::RbiGenerator
   module ClassOverride
     def new(*)
       result = super
-      SorbetRBIGeneration::RbiGenerator.module_created(result)
+      SorbetRBIGeneration::Tracer.module_created(result)
       result
     end
   end
   Class.prepend(ClassOverride)
 
   def self.register_delegate_class(klass, delegate)
-    @delegate_classes[real_object_id(delegate)] = klass
+    @delegate_classes[SorbetRBIGeneration::Module.real_object_id(delegate)] = klass
   end
 
   def self.module_created(mod)
@@ -80,41 +83,41 @@ class SorbetRBIGeneration::RbiGenerator
     add_to_context(type: :method, module: mod, method: method, singleton: singleton)
   end
 
-  def self.main
+  def self.trace
+    start
+    yield
+    finish
+    trace_results
+  end
+
+  def self.start
     pre_cache_module_methods
     install_tracepoints
+  end
 
-    SorbetRBIGeneration::RequireEverything.require_everything
-
-    output_dir = './rbi/gems/'
-    FileUtils.rm_r(output_dir) if Dir.exist?(output_dir)
+  def self.finish
     disable_tracepoints
-    generate_rbis(output_dir)
+  end
+
+  def self.trace_results
+    {
+      files: @files,
+      delegate_classes: @delegate_classes
+    }
   end
 
   private
 
-  # These methods don't match the signatures of their parents, so if we let
-  # them monkeypatch, they won't be subtypes anymore. Just don't support the
-  # bad monkeypatches.
-  BAD_METHODS = [
-    ['activesupport', 'Time', :to_s],
-    ['activesupport', 'Time', :initialize],
-  ]
-
   @modules = {}
   @context_stack = [[]]
-  @prev_anonymous_id = 0
-  @anonymous_map = {}
   @files = {}
   @delegate_classes = {}
 
-  @real_is_a = Module.instance_method(:is_a?)
-  @real_name = Module.instance_method(:name)
-  @real_object_id = Module.instance_method(:object_id)
-
-  def self.anonymous_id
-    @prev_anonymous_id += 1
+  def self.pre_cache_module_methods
+    ObjectSpace.each_object(Module) do |mod_|
+      mod = T.cast(mod_, Module)
+      @modules[SorbetRBIGeneration::Module.real_object_id(mod)] = (mod.instance_methods(false) + mod.private_instance_methods(false)).to_set
+    end
   end
 
   def self.add_to_context(item)
@@ -123,13 +126,6 @@ class SorbetRBIGeneration::RbiGenerator
     # the :c_return is traced.
     @context_stack << [] if @context_stack.empty?
     @context_stack.last << item
-  end
-
-  def self.pre_cache_module_methods
-    ObjectSpace.each_object(Module) do |mod_|
-      mod = T.cast(mod_, Module)
-      @modules[real_object_id(mod)] = (mod.instance_methods(false) + mod.private_instance_methods(false)).to_set
-    end
   end
 
   def self.install_tracepoints
@@ -169,7 +165,7 @@ class SorbetRBIGeneration::RbiGenerator
           singleton = tp.method_id == :singleton_method_added
           receiver = singleton ? tp.self.singleton_class : tp.self
           methods = receiver.instance_methods(false) + receiver.private_instance_methods(false)
-          set = @modules[real_object_id(receiver)] ||= Set.new
+          set = @modules[SorbetRBIGeneration::Module.real_object_id(receiver)] ||= Set.new
           added = methods.find { |m| !set.include?(m) }
           if added.nil?
             # warn("Warning: could not find method added to #{tp.self} at #{tp.path}:#{tp.lineno}")
@@ -194,73 +190,29 @@ class SorbetRBIGeneration::RbiGenerator
     @c_return_tracepoint.disable
   end
 
-  def self.gem_from_location(location)
-    match =
-      location&.match(/^.*\/(ruby)\/([\d.]+)\//) || # ruby stdlib
-      location&.match(/^.*\/(site_ruby)\/([\d.]+)\//) || # rubygems
-      location&.match(/^.*\/gems\/[\d.]+(?:\/bundler)?\/gems\/([^\/]+)-([^-\/]+)\//i) # gem
-    if match.nil?
-      # uncomment to generate files for methods outside of gems
-      # {
-      #   path: location,
-      #   gem: location.gsub(/[\/\.]/, '_'),
-      #   version: '1.0.0',
-      # }
-      nil
-    else
-      {
-        path: match[0],
-        gem: match[1],
-        version: match[2],
-      }
-    end
+end
+
+class SorbetRBIGeneration::RbiSerializer
+  SPECIAL_METHOD_NAMES = %w[! ~ +@ ** -@ * / % + - << >> & | ^ < <= => > >= == === != =~ !~ <=> [] []= `]
+
+  # These methods don't match the signatures of their parents, so if we let
+  # them monkeypatch, they won't be subtypes anymore. Just don't support the
+  # bad monkeypatches.
+  BAD_METHODS = [
+    ['activesupport', 'Time', :to_s],
+    ['activesupport', 'Time', :initialize],
+  ]
+
+  def initialize(trace_results)
+    @files = trace_results[:files]
+    @delegate_classes = trace_results[:delegate_classes]
+
+    @anonymous_map = {}
+    @prev_anonymous_id = 0
+
   end
 
-  def self.real_is_a?(klass, type)
-    @real_is_a.bind(klass).call(type)
-  end
-
-  def self.real_name(klass)
-    @real_name.bind(klass).call
-  end
-
-  def self.real_object_id(klass)
-    @real_object_id.bind(klass).call
-  end
-
-  def self.class_name(klass)
-    klass = @delegate_classes[real_object_id(klass)] || klass
-    name = real_name(klass) if real_is_a?(klass, Module)
-
-    # class/module has no name; it must be anonymous
-    if name.nil? || name == ""
-      middle = real_is_a?(klass, Class) ? klass.superclass : klass.class
-      id = @anonymous_map[real_object_id(klass)] ||= anonymous_id
-      return "Anonymous_#{class_name(middle).gsub('::', '_')}_#{id}"
-    end
-
-    # if the name doesn't only contain word characters and ':', or any part doesn't start with a capital, Sorbet doesn't support it
-    if name !~ /^[\w:]+$/ || !name.split('::').all? { |part| part =~ /^[A-Z]/ }
-      warn("Invalid class name: #{name}")
-      id = @anonymous_map[real_object_id(klass)] ||= anonymous_id
-      return "InvalidName_#{name.gsub(/[^\w]/, '_').gsub(/0x([0-9a-f]+)/, '0x00')}_#{id}"
-    end
-
-    name
-  end
-
-  def self.used?(klass, used)
-    used_by = used[klass] || []
-    used_by.any? { |user| user == true || used?(user, used) }
-  end
-
-  def self.valid_method_name?(symbol)
-    string = symbol.to_s
-    return true if SPECIAL_METHOD_NAMES.include?(string)
-    string =~ /^[[:word:]]+[?!=]?$/
-  end
-
-  def self.generate_rbis(output_dir)
+  def serialize(output_dir)
     grouped = {}
     used = {} # subclassed, included, or extended
     @files.each do |path, defined|
@@ -274,13 +226,13 @@ class SorbetRBIGeneration::RbiGenerator
       grouped[gem] ||= {}
       defined.each do |item|
         klass = item[:module]
-        klass_id = real_object_id(klass)
+        klass_id = SorbetRBIGeneration::Module.real_object_id(klass)
         values = grouped[gem][klass_id] ||= []
         values << item unless item[:type] == :module
 
         # only add an anon module if it's used as a superclass of a non-anon module, or is included/extended by a non-anon module
-        used_value = real_is_a?(klass, Module) && !real_name(klass).nil? ? true : real_object_id(klass) # if non-anon, set it to true, otherwise link to next anon class
-        (used[real_object_id(klass.superclass)] ||= Set.new) << used_value if real_is_a?(klass, Class)
+        used_value = SorbetRBIGeneration::Module.real_is_a?(klass, Module) && !SorbetRBIGeneration::Module.real_name(klass).nil? ? true : SorbetRBIGeneration::Module.real_object_id(klass) # if non-anon, set it to true, otherwise link to next anon class
+        (used[SorbetRBIGeneration::Module.real_object_id(klass.superclass)] ||= Set.new) << used_value if SorbetRBIGeneration::Module.real_is_a?(klass, Class)
         (used[item[item[:type]].object_id] ||= Set.new) << used_value if [:extend, :include].include?(item[:type])
       end
     end
@@ -297,13 +249,13 @@ class SorbetRBIGeneration::RbiGenerator
         klass_ids.each do |klass_id, defined|
           klass = ObjectSpace._id2ref(klass_id)
 
-          next if !((real_is_a?(klass, Module) && !real_name(klass).nil?) || used?(klass_id, used))
-          next if real_is_a?(klass, Class) && klass.superclass == Delegator && !klass.name
+          next if !((SorbetRBIGeneration::Module.real_is_a?(klass, Module) && !SorbetRBIGeneration::Module.real_name(klass).nil?) || used?(klass_id, used))
+          next if SorbetRBIGeneration::Module.real_is_a?(klass, Class) && klass.superclass == Delegator && !klass.name
 
           # next if [Object, BasicObject, Hash].include?(klass) # TODO should this be here?
 
-          f.write("#{real_is_a?(klass, Class) ? 'class' : 'module'} #{class_name(klass)}")
-          f.write(" < #{class_name(klass.superclass)}") if real_is_a?(klass, Class) && ![Object, nil].include?(klass.superclass)
+          f.write("#{SorbetRBIGeneration::Module.real_is_a?(klass, Class) ? 'class' : 'module'} #{class_name(klass)}")
+          f.write(" < #{class_name(klass.superclass)}") if SorbetRBIGeneration::Module.real_is_a?(klass, Class) && ![Object, nil].include?(klass.superclass)
           f.write("\n")
 
           rows = defined.map do |item|
@@ -335,7 +287,9 @@ class SorbetRBIGeneration::RbiGenerator
     end
   end
 
-  def self.generate_method(method, instance, spaces = 2)
+  private
+
+  def generate_method(method, instance, spaces = 2)
     # method.parameters is an array of:
     # a      [:req, :a]
     # b = 1  [:opt, :b]
@@ -369,6 +323,88 @@ class SorbetRBIGeneration::RbiGenerator
     parameters = parameters.join(', ')
     parameters = "(#{parameters})" unless parameters.empty?
     "#{prefix}def #{instance ? '' : 'self.'}#{method.name}#{parameters}; end"
+  end
+
+  def anonymous_id
+    @prev_anonymous_id += 1
+  end
+
+  def gem_from_location(location)
+    match =
+      location&.match(/^.*\/(ruby)\/([\d.]+)\//) || # ruby stdlib
+      location&.match(/^.*\/(site_ruby)\/([\d.]+)\//) || # rubygems
+      location&.match(/^.*\/gems\/[\d.]+(?:\/bundler)?\/gems\/([^\/]+)-([^-\/]+)\//i) # gem
+    if match.nil?
+      # uncomment to generate files for methods outside of gems
+      # {
+      #   path: location,
+      #   gem: location.gsub(/[\/\.]/, '_'),
+      #   version: '1.0.0',
+      # }
+      nil
+    else
+      {
+        path: match[0],
+        gem: match[1],
+        version: match[2],
+      }
+    end
+  end
+
+  def class_name(klass)
+    klass = @delegate_classes[SorbetRBIGeneration::Module.real_object_id(klass)] || klass
+    name = SorbetRBIGeneration::Module.real_name(klass) if SorbetRBIGeneration::Module.real_is_a?(klass, Module)
+
+    # class/module has no name; it must be anonymous
+    if name.nil? || name == ""
+      middle = SorbetRBIGeneration::Module.real_is_a?(klass, Class) ? klass.superclass : klass.class
+      id = @anonymous_map[SorbetRBIGeneration::Module.real_object_id(klass)] ||= anonymous_id
+      return "Anonymous_#{class_name(middle).gsub('::', '_')}_#{id}"
+    end
+
+    # if the name doesn't only contain word characters and ':', or any part doesn't start with a capital, Sorbet doesn't support it
+    if name !~ /^[\w:]+$/ || !name.split('::').all? { |part| part =~ /^[A-Z]/ }
+      warn("Invalid class name: #{name}")
+      id = @anonymous_map[SorbetRBIGeneration::Module.real_object_id(klass)] ||= anonymous_id
+      return "InvalidName_#{name.gsub(/[^\w]/, '_').gsub(/0x([0-9a-f]+)/, '0x00')}_#{id}"
+    end
+
+    name
+  end
+
+  def used?(klass, used)
+    used_by = used[klass] || []
+    used_by.any? { |user| user == true || used?(user, used) }
+  end
+
+  def valid_method_name?(symbol)
+    string = symbol.to_s
+    return true if SPECIAL_METHOD_NAMES.include?(string)
+    string =~ /^[[:word:]]+[?!=]?$/
+  end
+end
+
+# TODO switch the Struct handling to:
+#
+# class Subclass < Struct(:key1, :key2)
+# end
+#
+# generating:
+#
+# TemporaryStruct = Struct(:key1, :key2)
+# class Subclass < TemporaryStruct
+# end
+#
+# instead of manually defining every getter/setter
+
+class SorbetRBIGeneration::RbiGenerator
+  def self.main(output_dir = './rbi/gems/')
+    trace_results = SorbetRBIGeneration::Tracer.trace do
+      SorbetRBIGeneration::RequireEverything.require_everything
+    end
+
+    FileUtils.rm_r(output_dir) if Dir.exist?(output_dir)
+    SorbetRBIGeneration::RbiSerializer.new(trace_results).serialize(output_dir)
   end
 end
 
