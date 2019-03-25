778cff3006f14549abed4e4a7c09b45e1723ee72
Paul Tarjan <pt@stripe.com>
2019-03-25 11:11:38 -0700

    stop raising on requiring this gem (#2235)
    stop raising on requiring this gem (#2235)
    
    (Squashed by Merge Bot)

--- rbi-generation/lib/sorbet-rbi-generation.rb
+++ rbi-generation/lib/sorbet-rbi-generation.rb
@@ -1,1 +1,3 @@
-raise "This gem is only for binaries"
+# This file is intentionally left blank.
+# This gem doesn't provide anything when required.
+# Instead it gives you commands which are in the bin/ directory.
