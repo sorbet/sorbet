9289c88070e611cf74c4157414418d079cde0bf9
Dmitry Petrashko <dmitry@stripe.com>
2019-03-26 10:33:37 -0700

    Remove redundant wait

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -5,7 +5,7 @@ set -exuo pipefail
 whitelisted=0
 
 if [[ "${whitelisted}" -ne 1 ]] ; then
-   (echo -e "steps:\n  - wait: ~\n  - block: \":key: Needs contributor approval!\"\n  - wait: ~\n"; 
+   (echo -e "steps:\n  - block: \":key: Needs contributor approval!\"\n  - wait: ~\n";
    cat .buildkite/pipeline.yaml | grep -v "steps:") | buildkite-agent pipeline upload
 else
   buildkite-agent pipeline upload
