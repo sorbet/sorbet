f12eb0640b9f4fc63b910c99254e172ce2ac879a
Dmitry Petrashko <dmitry@stripe.com>
2019-03-26 16:06:43 -0700

    rbenv version of ruby

--- .buildkite/test-and-build-ruby-runtime.sh
+++ .buildkite/test-and-build-ruby-runtime.sh
@@ -3,8 +3,16 @@
 set -euo pipefail
 
 pushd runtime
+
+echo "--- setup :ruby:"
+eval "$(rbenv init -)"
+rbenv shell 2.4.3
 bundle install --path vendor/bundle
+
+echo "+++ tests"
 bundle exec rake test
+
+echo "--- build"
 gem build sorbet-runtime.gemspec
 popd
 
