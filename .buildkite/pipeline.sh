6bba4928d3583a42fa594735049d4518c07bc42b
Dmitry Petrashko <dmitry@stripe.com>
2019-03-26 01:24:34 -0700

    concat steps

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -6,7 +6,7 @@ whitelisted=0
 
 if [[ "${whitelisted}" -ne 1 ]] ; then
    (echo -e "steps:\n  - wait: ~\n  - block: \":key: Needs contributor approval!\"\n  - wait: ~\n"; 
-   cat .buildkite/pipeline.yaml) | buildkite-agent pipeline upload
+   cat .buildkite/pipeline.yaml | grep -v "steps:") | buildkite-agent pipeline upload
 else
   buildkite-agent pipeline upload
 fi
