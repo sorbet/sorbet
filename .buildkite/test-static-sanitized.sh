f5f22ea65fa128d32a756deb8c6bbe50b89239a1
Dmitry Petrashko <dmitry@stripe.com>
2019-03-25 17:46:56 -0700

    more config

--- .buildkite/test-static-sanitized.sh
+++ .buildkite/test-static-sanitized.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
 echo "--- Pre-setup"
+
 unameOut="$(uname -s)"
 case "${unameOut}" in
-    Linux*)     CONFIG_OPTS="--config=buildfarm-sanitized-linux";;
-    Darwin*)    CONFIG_OPTS="--config=buildfarm-sanitized-mac";;
+    Linux*)     platform="linux";;
+    Darwin*)    platform="mac";;
     *)          exit 1
 esac
+
+if [[ "linux" == $platform ]]; then
+  apt-get install pkg-config zip g++ zlib1g-dev unzip python
+  CONFIG_OPTS="--buildfarm-sanitized-linux"
+elif [[ "mac" == $platform ]]; then
+  CONFIG_OPTS="--buildfarm-sanitized-mac"
+fi
+
+echo will run with $CONFIG_OPTS
 ./bazel version
+
 
 echo "--- compilation"
 ./bazel build //... $CONFIG_OPTS
