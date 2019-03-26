1e24a5eab92ba9989838429748f2363ffd6b51dd
Dmitry Petrashko <dmitry@stripe.com>
2019-03-25 17:51:15 -0700

    Do mac builds

---
+++ .buildkite/build-emscripten.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "--- Pre-setup"
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     platform="linux";;
+    Darwin*)    platform="mac";;
+    *)          exit 1
+esac
+
+if [[ "linux" == $platform ]]; then
+  echo "only mac builds are supported"
+  exit 1
+elif [[ "mac" == $platform ]]; then
+fi
+
+echo will run with $CONFIG_OPTS
+./bazel version
+
+
+echo "--- compilation"
+
+tools/scripts/update-sorbet.run.sh
