a61ce8c66e47ed86bb004baac5499672e59832ed
Dmitry Petrashko <dmitry@stripe.com>
2019-03-25 20:59:21 -0700

    Add release config

---
+++ .buildkite/release-static.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -euo pipefail
+
+echo "--- Pre-setup :bazel:"
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     platform="linux";;
+    Darwin*)    platform="mac";;
+    *)          exit 1
+esac
+
+if [[ "linux" == $platform ]]; then
+  apt-get update -yy
+  apt-get install -yy pkg-config zip g++ zlib1g-dev unzip python ruby
+  CONFIG_OPTS="--config=release-linux"
+elif [[ "mac" == $platform ]]; then
+  CONFIG_OPTS="--config=release-mac"
+fi
+
+echo will run with $CONFIG_OPTS
+
+git checkout .bazelrc
+rm -f bazel-*
+mkdir -p /usr/local/var/bazelcache/output-bases/release /usr/local/var/bazelcache/build /usr/local/var/bazelcache/repos
+echo 'common --curses=no --color=yes' >> .bazelrc
+echo 'startup --output_base=/usr/local/var/bazelcache/output-bases/release' >> .bazelrc
+echo 'build  --disk_cache=/usr/local/var/bazelcache/build --repository_cache=/usr/local/var/bazelcache/repos' >> .bazelrc
+echo 'test   --disk_cache=/usr/local/var/bazelcache/build --repository_cache=/usr/local/var/bazelcache/repos' >> .bazelrc
+
+./bazel version
+
+echo "--- compilation"
+./bazel build //main:sorbet $CONFIG_OPTS
+
+rm -rf _out_
+mkdir _out_/${platform}
+cp bazel-bin/main/sorbet _out_/${platform}
