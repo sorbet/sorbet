84bbad689cbe77c717e866407350566f2dcbd1ca
Dmitry Petrashko <dmitry@stripe.com>
2019-03-26 01:15:13 -0700

    Self-checking pipeline.sh

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -2,4 +2,10 @@
 
 set -exuo pipefail
 
-buildkite-agent pipeline upload
+whitelisted=0
+
+if [[ "${whitelisted} -ne 1 ]]; then
+  (echo -e "steps:\n  - wait: ~\n  - block: \":key: Needs contributor approval!\"\n  - wait: ~\n"; cat .buildkite/pipeline.yaml) | buildkite-agent pipeline upload
+else
+  buildkite-agent pipeline upload
+fi
