4c0402d67573a5072997bc41b270e45688a4318f
Dmitry Petrashko <dmitry@stripe.com>
2019-03-26 01:20:48 -0700

    fix

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -4,8 +4,9 @@ set -exuo pipefail
 
 whitelisted=0
 
-if [[ "${whitelisted} -ne 1 ]]; then
-  (echo -e "steps:\n  - wait: ~\n  - block: \":key: Needs contributor approval!\"\n  - wait: ~\n"; cat .buildkite/pipeline.yaml) | buildkite-agent pipeline upload
+if [[ "${whitelisted}" -ne 1 ]] ; then
+   (echo -e "steps:\n  - wait: ~\n  - block: \":key: Needs contributor approval!\"\n  - wait: ~\n"; 
+   cat .buildkite/pipeline.yaml) | buildkite-agent pipeline upload
 else
   buildkite-agent pipeline upload
 fi
