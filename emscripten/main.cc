6534f738455e123642279bf41af2e03330b406f3
Paul Tarjan <pt@stripe.com>
2019-03-26 20:00:39 -0700

    Expose lsp to wasm (#2248)
    Expose lsp to wasm (#2248)
    
    * Expose a simple message passing interface on the WebAssembly module
    
    * Add deployment instructions
    
    * Move Sourcegraph docs to docs/sourcegraph.md
    
    * Add comments
    
    * Remove args used for debugging
    
    * get it to compile
    
    * don't need this
    
    * don't want a Makefile
    
    * add ASSERTIONS=1
    
    * try without these
    
    * stop using namespace
    
    * fix allocation
    
    * Remove debugging
    
    * remove docs, we aren't ready yet
    
    * format
    
    (Squashed by Merge Bot)

--- emscripten/main.cc
+++ emscripten/main.cc
@@ -1,3 +1,4 @@
+#include "main/lsp/wrapper.h"
 #include "main/realmain.h"
 
 #ifdef __EMSCRIPTEN__
@@ -8,12 +9,30 @@
 #endif
 
 using namespace std;
-using namespace sorbet;
+
+typedef struct LSP {
+    void (*respond)(const char *);
+    sorbet::realmain::lsp::LSPWrapper *wrapper;
+} LSP;
 
 extern "C" {
 void EMSCRIPTEN_KEEPALIVE typecheck(const char *rubySrc) {
     const char *argv[] = {"sorbet", "--color=always", "--silence-dev-message", "-e", rubySrc};
     sorbet::realmain::realmain(size(argv), const_cast<char **>(argv));
+}
+
+LSP *EMSCRIPTEN_KEEPALIVE lsp_initialize(void (*respond)(const char *)) {
+    LSP *lsp = new LSP();
+    lsp->respond = respond;
+    lsp->wrapper = new sorbet::realmain::lsp::LSPWrapper();
+    return lsp;
+}
+
+void EMSCRIPTEN_KEEPALIVE lsp_send(LSP *lsp, char *message) {
+    auto responses = lsp->wrapper->getLSPResponsesFor(message);
+    for (auto &response : responses) {
+        lsp->respond(response->toJSON().c_str());
+    }
 }
 
 int main(int argc, char **argv) {}
