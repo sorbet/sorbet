aa32c5cc836875f96efc7ccb8f266937eb1346e2
Jacob Zimmerman <jez@stripe.com>
2019-03-29 14:47:53 -0700

    Move config out of shell script into file (#2280)
    Move config out of shell script into file (#2280)
    
    (Squashed by Merge Bot)

--- test/cli/config-file-recursive/config-file-recursive.sh
+++ test/cli/config-file-recursive/config-file-recursive.sh
@@ -4,11 +4,7 @@ cwd="$(pwd)"
 
 cd test/cli/config-file-recursive || exit 1
 
-# If we made this a file in git, it wouldn't get copied into the bazel sandbox.
-echo '.' > other-config
-trap 'rm -f other-config' EXIT
-
-# Look ma, no args! (the args are in sorbet/config & ./other-config)
+# Look ma, no args! (the args are in sorbet/config & sorbet/other-config)
 "$cwd/main/sorbet" 2>&1
 
 echo ------------------------------
