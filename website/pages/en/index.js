7f194d5790b0ba0bfa0af0785de33614f7301139
Jacob Zimmerman <jez@stripe.com>
2019-04-05 16:44:43 -0700

    Add embed to landing page (#2383)
    * Add embed to landing page
    
    This will not stay here forever, I just wanted to make sure it works and
    get something in. We'll probably remove this / edit it to get it to fit
    in with the layout later.
    
    * Update example
    
    * Remove class Main
    
    * Shorten

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -13,6 +13,8 @@ const MarkdownBlock = CompLibrary.MarkdownBlock; /* Used to read markdown */
 const Container = CompLibrary.Container;
 const GridBlock = CompLibrary.GridBlock;
 
+const sorbetRunExample = "https://sorbet.run/#%23%20typed%3A%20true%0A%0A%23%20This%20code%20is%20editable!%0A%23%20You%20can%20also%20hover%20to%20see%20error%20messages%20and%20types!%0A%0Aextend%20T%3A%3ASig%0A%0Asig%20%7Bparams(name%3A%20String).returns(Integer)%7D%0Adef%20main(name)%0A%20%20puts%20%22Hello%2C%20%23%7Bname%7D!%22%0A%20%20name.length%0Aend%0A%0Amain(%22Sorbet%22)%20%23%20ok!%0Amain()%20%20%20%23%20error%3A%20Not%20enough%20arguments%20provided%20for%20method%20%60main%60%0Aman(%22%22)%20%20%23%20error%3A%20Method%20%60man%60%20does%20not%20exist";
+
 class HomeSplash extends React.Component {
   render() {
     const {siteConfig, language = ''} = this.props;
@@ -81,6 +83,9 @@ class Index extends React.Component {
       <div>
         <HomeSplash siteConfig={siteConfig} language={language} />
         <div className="mainContainer">
+        <Container align="center">
+          <iframe style={{width: '100%', height: '576px'}} src={sorbetRunExample} />
+        </Container>
         </div>
       </div>
     );
