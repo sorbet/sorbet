9a607f37a9f799a5563eba5d877579f391486c8f
Dmitry Petrashko <dmitry@stripe.com>
2019-04-12 14:01:49 -0700

    Dealias Stripe usernames

--- .buildkite/assign-reviewers.sh
+++ .buildkite/assign-reviewers.sh
@@ -3,7 +3,20 @@ set -exuo pipefail
 
 pr_id="$BUILDKITE_PULL_REQUEST"
 
+ALIASES=( "pt-stripe:ptarjan"
+        "dmitry-stripe:DarkDimius"
+        "jez-stripe:jez")
+
 pr_author=$(curl --silent --fail --netrc --location "https://api.github.com/repos/stripe/sorbet/pulls/${pr_id}" | jq '.["user"]["login"]'| tr -d '"')
+
+
+for pair in "${ALIASES[@]}" ; do
+    KEY="${pair%%:*}"
+    VALUE="${pair##*:}"
+    if [ "$KEY" == "$pr_author" ]; then
+      pr_author="$VALUE"
+    fi
+done
 
 potential_reviewers=$(comm -23 <(sort .buildkite/reviewer-list.txt) <(echo "$pr_author"))
 chosen_reviewer=$(echo "$potential_reviewers"|shuf -n 1)
