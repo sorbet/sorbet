469453e29e6695493ce2e759382766d0bcb97620
Dmitry Petrashko <dmitry@stripe.com>
2019-04-12 00:04:06 -0700

    Leave comment on why is the other script also checksummed

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -8,7 +8,7 @@ if [[ "$BUILDKITE_PULL_REQUEST" == "false" ]]; then
   # whitelist commits that are triggered in branch builds of github.com/stripe/sorbet
   whitelisted=1
 else
-  .buildkite/assign-reviewers.sh
+  .buildkite/assign-reviewers.sh # this means that this script will have to also be checksum-ed
 fi
 
 if [[ "$BUILDKITE_PULL_REQUEST_REPO" == "git://github.com/stripe/sorbet.git" ]]; then
