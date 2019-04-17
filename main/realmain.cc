6e6033086723f985ac52bee657a806a53acf2d0d
John Vilk <jvilk@stripe.com>
2019-04-17 13:09:34 -0700

    Change to 1GB log files.

--- main/realmain.cc
+++ main/realmain.cc
@@ -167,11 +167,11 @@ int realmain(int argc, char *argv[]) {
         startHUPMonitor();
     }
     if (!opts.debugLogFile.empty()) {
-        // LSP could run for a long time. Rotate log files, and trim at 5 GiB. Keep around 3 log files.
+        // LSP could run for a long time. Rotate log files, and trim at 1 GiB. Keep around 3 log files.
         // Cast first number to size_t to prevent integer multiplication.
         // TODO(jvilk): Reduce size once LSP logging is less chunderous.
         auto fileSink =
-            make_shared<spdlog::sinks::rotating_file_sink_mt>(opts.debugLogFile, ((size_t)5) * 1024 * 1024 * 1024, 3);
+            make_shared<spdlog::sinks::rotating_file_sink_mt>(opts.debugLogFile, ((size_t)1) * 1024 * 1024 * 1024, 3);
         fileSink->set_level(spd::level::debug);
         { // replace console & fatal loggers
             vector<spd::sink_ptr> sinks{stderrColorSink, fileSink};
