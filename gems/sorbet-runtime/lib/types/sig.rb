68d674175dfd5d6f6e1ba9d747d9a98f529ce333
Paul Tarjan <github@paulisageek.com>
2019-04-19 05:11:56 +0900

    add sig for `sig` (#311)
    * add sig for this
    
    * replace "T::Private::Methods::Builder" "Sorbet::Private::Builder"

--- gems/sorbet-runtime/lib/types/sig.rb
+++ gems/sorbet-runtime/lib/types/sig.rb
@@ -2,12 +2,26 @@
 # typed: true
 # rubocop:disable PrisonGuard/NoTopLevelDeclarations, PrisonGuard/PackageMatchesDirectory
 
+class Sorbet
+  # At runtime, does nothing, but statically it is treated exactly the same
+  # as T::Sig#sig. Only use it in cases where you can't use T::Sig#sig.
+  def self.sig(&blk)
+  end
+
+  # At runtime, does nothing, but statically it is treated exactly the same
+  # as T::Sig#sig. Only use it in cases where you can't use T::Sig#sig.
+  Sorbet.sig {params(blk: T.proc.bind(Sorbet::Private::Builder).void).void}
+  def self.sig(&blk)
+  end
+end
+
 # Monkeypatches standard objects so that you can call `sig` anywhere.
 # Docs at http://go/types
 module T::Sig
   # Declares a method with type signatures and/or
   # abstract/override/... helpers. See the documentation URL on
   # {T::Helpers}
+  Sorbet.sig {params(blk: T.proc.bind(Sorbet::Private::Builder).void).void}
   def sig(&blk) # rubocop:disable PrisonGuard/BanBuiltinMethodOverride
     T::Private::Methods.declare_sig(self, &blk)
   end
