fa20c1c6973f04192b1e566c87d68be9989073fc
Douglas Eichelberger <dug@stripe.com>
2019-04-23 12:46:32 -0700

    Add blank_p

--- infer/environment.cc
+++ infer/environment.cc
@@ -402,15 +402,28 @@ void Environment::updateKnowledge(core::Context ctx, core::LocalVariable local, 
         whoKnows.truthy.mutate().yesTypeTests.emplace_back(send->recv.variable, core::Types::nilClass());
         whoKnows.falsy.mutate().noTypeTests.emplace_back(send->recv.variable, core::Types::nilClass());
         whoKnows.sanityCheck();
+    } else if (send->fun == core::Names::blank_p()) {
+        if (!knowledgeFilter.isNeeded(local)) {
+            return;
+        }
+        // Note that this assumes that .blank? is a rails-compatible monkey patch.
+        // In other cases this flow analysis might make incorrect assumptions.
+        auto &originalType = send->recv.type;
+        auto knowledgeTypeWithoutFalsy = core::Types::approximateSubtract(ctx, originalType, core::Types::falsyTypes());
+
+        if (!core::Types::equiv(ctx, knowledgeTypeWithoutFalsy, originalType)) {
+            auto &whoKnows = getKnowledge(local);
+            whoKnows.falsy.mutate().yesTypeTests.emplace_back(send->recv.variable, knowledgeTypeWithoutFalsy);
+            whoKnows.sanityCheck();
+        }
     } else if (send->fun == core::Names::present_p()) {
         if (!knowledgeFilter.isNeeded(local)) {
             return;
         }
-        // Note that this assumes that .present? is a rails compatible monkey patch on both NilClass
-        // and Object. In all other cases this flow analysis might produce incorrect assumptions.
+        // Note that this assumes that .present? is a rails-compatible monkey patch.
+        // In other cases this flow analysis might make incorrect assumptions.
         auto &originalType = send->recv.type;
-        auto knowledgeTypeWithoutFalsy =
-            core::Types::approximateSubtract(ctx, originalType, core::Types::falsyTypes());
+        auto knowledgeTypeWithoutFalsy = core::Types::approximateSubtract(ctx, originalType, core::Types::falsyTypes());
 
         if (!core::Types::equiv(ctx, knowledgeTypeWithoutFalsy, originalType)) {
             auto &whoKnows = getKnowledge(local);
