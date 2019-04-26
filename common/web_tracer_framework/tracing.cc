6db8a58cfe6afbb4b1c8467b89d3b79f095fd839
Dmitry Petrashko <dmitry@stripe.com>
2019-04-25 19:41:06 -0700

    Various improvements to tracing (#348)
    * Fix a bug in counters
    
    * Include process name and version in trace
    
    * generate_compdb_targets
    
    * Add test
    
    * Add line breaks

--- common/web_tracer_framework/tracing.cc
+++ common/web_tracer_framework/tracing.cc
@@ -4,6 +4,7 @@
 #include "absl/strings/str_replace.h"
 #include "common/Counters_impl.h"
 #include "common/web_tracer_framework/tracing.h"
+#include "version/version.h"
 #include <chrono>
 #include <string>
 #include <unistd.h>
@@ -13,31 +14,32 @@ bool Tracing::storeTraces(const CounterState &counters, string_view fileName) {
     fmt::memory_buffer result;
 
     if (!FileOps::exists(fileName)) {
-        fmt::format_to(result, "[");
+        fmt::format_to(result, "[\n");
     }
     auto now = chrono::duration_cast<chrono::microseconds>(chrono::steady_clock::now().time_since_epoch()).count();
 
     auto pid = getpid();
+    fmt::format_to(result,
+                   "{{\"name\":\"process_name\",\"ph\":\"M\",\"pid\":{},\"args\":{{\"name\":\"Sorbet v{}\"}}}},\n", pid,
+                   Version::full_version_string);
     counters.counters->canonicalize();
     for (auto &cat : counters.counters->countersByCategory) {
-        fmt::format_to(
-            result, "{{\"name\": \"{}\", \"ph\": \"C\", \"ts\": {}, \"pid\": {}, \"cat\": \"byCategory\", \"args\": {{",
-            cat.first, now, pid);
+        fmt::format_to(result, "{{\"name\":\"{}\",\"ph\":\"C\",\"ts\":{},\"pid\":{},\"cat\":\"CC\",\"args\":{{",
+                       cat.first, now, pid);
         bool seenFirst = false;
         for (auto &e : cat.second) {
             if (seenFirst) {
                 fmt::format_to(result, ",");
             }
-            fmt::format_to(result, "\"{}\": {}", e.first, e.second);
+            fmt::format_to(result, "\"{}\":{}", e.first, e.second);
             seenFirst = true;
         }
         fmt::format_to(result, "}}}},\n");
     }
 
     for (auto &hist : counters.counters->histograms) {
-        fmt::format_to(
-            result, "{{\"name\": \"{}\", \"ph\": \"C\", \"ts\": {}, \"pid\": {}, \"cat\": \"histogram\", \"args\": {{",
-            hist.first, now, pid);
+        fmt::format_to(result, "{{\"name\":\"{}\",\"ph\":\"C\",\"ts\":{},\"pid\":{},\"cat\":\"H\",\"args\":{{",
+                       hist.first, now, pid);
 
         bool seenFirst = false;
         for (auto &e : hist.second) {
@@ -45,7 +47,7 @@ bool Tracing::storeTraces(const CounterState &counters, string_view fileName) {
                 fmt::format_to(result, ",");
             }
 
-            fmt::format_to(result, "\"{}\": {}", e.first, e.second);
+            fmt::format_to(result, "\"{}\":{}", e.first, e.second);
             seenFirst = true;
         }
         fmt::format_to(result, "}}}},\n");
@@ -53,35 +55,35 @@ bool Tracing::storeTraces(const CounterState &counters, string_view fileName) {
 
     for (auto &e : counters.counters->counters) {
         fmt::format_to(result,
-                       "{{\"name\": \"{}\", \"ph\": \"C\", \"ts\": {}, \"pid\": {}, \"cat\": \"counter\", \" args\": "
-                       "{{ \"value\": {} }}}},\n",
+                       "{{\"name\":\"{}\",\"ph\":\"C\",\"ts\":{},\"pid\":{},\"cat\":\"C\",\"args\":"
+                       "{{\"value\":{}}}}},\n",
                        e.first, now, pid, e.second);
     }
 
     for (const auto &e : counters.counters->timings) {
         if (e.kind == CounterImpl::Timing::Duration) {
             fmt::format_to(result,
-                           "{{\"name\": \"{}\", \"cat\": \"timing,duration\", \"ph\": \"X\", \"ts\": {}, \"dur\": "
-                           "{}, \"pid\": {}, \"tid\": {}}},\n",
+                           "{{\"name\":\"{}\",\"cat\":\"T,D\",\"ph\":\"X\",\"ts\":{},\"dur\":"
+                           "{},\"pid\":{},\"tid\":{}}},\n",
                            e.namePrefix, e.ts, e.duration.value(), pid, e.threadId.value());
         } else if (e.kind == CounterImpl::Timing::Async) {
             fmt::format_to(result,
-                           "{{\"name\": \"{}\", \"cat\": \"timing,async\", \"ph\": \"b\", \"ts\": {}, \"pid\": {}, "
-                           "\"id\": {}}},\n",
+                           "{{\"name\":\"{}\",\"cat\":\"T,A\",\"ph\":\"b\",\"ts\":{},\"pid\":{},"
+                           "\"id\":{}}},\n",
                            e.namePrefix, e.ts, pid, e.id);
             fmt::format_to(result,
-                           "{{\"name\": \"{}\", \"cat\": \"timing,async\", \"ph\": \"e\", \"ts\": {}, \"pid\": {}, "
-                           "\"id\": {}}},\n",
+                           "{{\"name\":\"{}\",\"cat\":\"T,A\",\"ph\":\"e\",\"ts\":{},\"pid\":{}, "
+                           "\"id\":{}}},\n",
                            e.namePrefix, e.ts + e.duration.value(), pid, e.id);
         } else if (e.kind == CounterImpl::Timing::FlowStart) {
             fmt::format_to(result,
-                           "{{\"name\": \"{}\", \"cat\": \"timing,flow\", \"ph\": \"s\", \"ts\": {}, \"pid\": {}, "
-                           "\"tid\": {}, \"id\": {}}},\n",
+                           "{{\"name\":\"{}\",\"cat\":\"T,F\",\"ph\":\"s\",\"ts\":{},\"pid\":{},"
+                           "\"tid\":{},\"id\":{}}},\n",
                            e.namePrefix, e.ts, pid, e.threadId.value(), e.id);
         } else if (e.kind == CounterImpl::Timing::FlowEnd) {
             fmt::format_to(result,
-                           "{{\"name\": \"{}\", \"cat\": \"timing,flow\", \"ph\": \"f\", \"bp\": \"e\", \"ts\": "
-                           "{}, \"pid\": {}, \"tid\": {}, \"id\": {}}},\n",
+                           "{{\"name\":\"{}\",\"cat\":\"T,F\",\"ph\":\"f\",\"bp\":\"e\",\"ts\":"
+                           "{},\"pid\":{},\"tid\":{},\"id\":{}}},\n",
                            e.namePrefix, e.ts, pid, e.threadId.value(), e.id);
         }
     }
