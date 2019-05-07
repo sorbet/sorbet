5b8db5f5a2c31da9ad0258439c2c5cee750ad086
Paul Tarjan <pt@stripe.com>
2019-05-06 17:32:16 -0700

    make this better

--- test/cli/symbol-table-json-alias/symbol-table-json-alias.sh
+++ test/cli/symbol-table-json-alias/symbol-table-json-alias.sh
@@ -1,1 +1,1 @@
-main/sorbet test/cli/symbol-table-json-alias/symbol-table-json-alias.rb -p symbol-table-json | grep "aliasTo"
+main/sorbet test/cli/symbol-table-json-alias/symbol-table-json-alias.rb -p symbol-table-json | grep aliasTo -B 5 | head -n 5
