0f0bbf6540c729808a916ad1ccefa35a952b5ac6
Getty Ritter <gdritter@stripe.com>
2019-05-09 16:49:39 -0700

    Add explanatory comment to test file

--- test/testdata/resolver/redefinition_of_subclass_type_member.rb
+++ test/testdata/resolver/redefinition_of_subclass_type_member.rb
@@ -1,6 +1,16 @@
 # typed: true
 # disable-fast-path: true
 
+# This was a fuzzer-generated test file. The specific edge case that
+# it ended up uncovering was that `Foo` here has two type members, and
+# `Bar` is attempting to redefine one of them as a constant value in a
+# way that also makes use of both type members. This rather specific
+# situation caused an internal invariant to be broken, where the
+# representation of Bar's class contained a vector of one type
+# parameter (the inherited V) while resolving the type of `foo` was
+# expecting at least two type parameters (the ones corresponding to
+# `K` and `V`), resulting in an out-of-bounds memory access.
+
 class Foo
   extend T::Generic
   extend T::Sig
