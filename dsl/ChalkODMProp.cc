809e2fbf66c1dac6c6d73b891e5365e9811643f9
benbuckman <ben@benbuckman.net>
2019-05-10 11:06:03 -0700

    back to overwriting getter - default getter's return type is meaningless anyway

--- dsl/ChalkODMProp.cc
+++ dsl/ChalkODMProp.cc
@@ -184,23 +184,18 @@ vector<unique_ptr<ast::Expression>> ChalkODMProp::replaceDSL(core::MutableContex
 
     stats.emplace_back(ast::MK::Sig(loc, ast::MK::Hash0(loc), ASTUtil::dupType(getType.get())));
 
-    ast::InsSeq::STATS_store getterBodyStats;
-
     if (computedByMethodName.exists()) {
         // Given `const :foo, type, computed_by: <name>`, where <name> is a Symbol pointing to a class method,
         // assert that the method takes 1 argument (of any type), and returns the same type as the prop,
-        // by adding `T.assert_type!(self.class.compute_foo(T.unsafe(nil)), type)` to the prop getter.
+        // via `T.assert_type!(self.class.compute_foo(T.unsafe(nil)), type)` in the getter.
         auto selfSendClass = ast::MK::Send0(computedByMethodNameLoc, ast::MK::Self(loc), core::Names::class_());
         auto unsafeNil = ast::MK::Unsafe(computedByMethodNameLoc, ast::MK::Nil(computedByMethodNameLoc));
         auto sendComputedMethod = ast::MK::Send1(computedByMethodNameLoc, std::move(selfSendClass), computedByMethodName, std::move(unsafeNil));
         auto assertTypeMatches = ast::MK::AssertType(computedByMethodNameLoc, std::move(sendComputedMethod), ASTUtil::dupType(getType.get()));
-        getterBodyStats.emplace_back(std::move(assertTypeMatches));
+        stats.emplace_back(mkGet(loc, name, std::move(assertTypeMatches)));
+    } else {
+        stats.emplace_back(mkGet(loc, name, ast::MK::Cast(loc, std::move(getType))));
     }
-
-    // Prop getter should return same type as prop
-    auto getterReturnCast = ast::MK::Cast(loc, std::move(getType));
-    auto getterBody = mkGet(loc, name, ast::MK::InsSeq(loc, std::move(getterBodyStats), std::move(getterReturnCast)));
-    stats.emplace_back(std::move(getterBody));
 
     core::NameRef setName = name.addEq(ctx);
 
