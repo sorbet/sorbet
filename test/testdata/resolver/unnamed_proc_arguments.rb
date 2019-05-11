a4a8d1bee475b6d041b24f29f53fb3b51b553bf7
Trevor Elliott <trevor@stripe.com>
2019-05-11 10:08:15 -0700

    Update tests

--- test/testdata/resolver/unnamed_proc_arguments.rb
+++ test/testdata/resolver/unnamed_proc_arguments.rb
@@ -4,4 +4,6 @@
 T.let(nil, T.nilable(T.proc.params(x: Integer).void))
 
 # this one fails because the proc doesn't have named params
-T.let(nil, T.nilable(T.proc.params(Integer).void)) # error: `params` expects keyword arguments
+T.let(nil, T.nilable(T.proc.params(Integer).void))
+                   # ^^^^^^^^^^^^^^^^^^^^^^ error: `params` expects keyword arguments
+                   # ^^^^^^^^^^^^^^^^^^^^^^ error: `T.class_of(Integer)` doesn't match `T::Hash[T.untyped, T.untyped]`
