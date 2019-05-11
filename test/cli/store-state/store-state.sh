894c2d4d0efb56d399776fbd5bbff7c76dd95095
Dmitry Petrashko <dmitry@stripe.com>
2019-05-11 01:19:21 -0700

    Make --store-state reproducible.
    It used to iterate over an unordered hash and thus had unstable order.

--- test/cli/store-state/store-state.sh
+++ test/cli/store-state/store-state.sh
@@ -5,3 +5,5 @@ if [ ! -f state ]; then
   echo "state file wasn't created"
   exit 1
 fi
+main/sorbet --silence-dev-message -e 'class Foo; end' --store-state state1
+diff state state1 # there should be no difference
