71e9a883237579b71144e06f6e2293f49718ef0d
Nathaniel Roman <nroman@stripe.com>
2019-05-19 11:02:55 -0700

    Add test diffing stdout vs fileout

--- test/cli/phases/phases.sh
+++ test/cli/phases/phases.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 set -eu
 
+rm -rf out fileout
+mkdir -p out fileout
+
 # Make sure these options don't change output
 for p in parse-tree parse-tree-json ast ast-raw dsl-tree dsl-tree-raw symbol-table symbol-table-raw name-tree name-tree-raw resolve-tree resolve-tree-raw file-table-json cfg; do
     echo "--- $p start ---"
-    main/sorbet --silence-dev-message -p "$p" -e '1'
+    main/sorbet --silence-dev-message -p "$p" -e '1' | tee "out/$p"
+    main/sorbet --silence-dev-message -p "$p:fileout/$p" -e '1' > /dev/null
     echo "--- $p end ---"
 done
+
+# Verify that output printed to stdout matches file output
+echo "--- checking diff ---"
+diff -r out fileout | sort
 
 # Makes sure all these options don't crash us
 for p in symbol-table-json symbol-table-full symbol-table-full-raw; do
