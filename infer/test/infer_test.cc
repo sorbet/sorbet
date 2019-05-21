c3156a4ed049669b1752c32dddcb6d614af442ed
Trevor Elliott <awesomelyawesome@gmail.com>
2019-05-21 16:23:15 -0700

    Update //infer:infer_test

--- infer/test/infer_test.cc
+++ infer/test/infer_test.cc
@@ -8,6 +8,7 @@
 #include "core/Unfreeze.h"
 #include "dsl/dsl.h"
 #include "infer/infer.h"
+#include "local_vars/local_vars.h"
 #include "namer/namer.h"
 #include "resolver/resolver.h"
 #include "spdlog/spdlog.h"
@@ -47,6 +48,7 @@ void processSource(core::GlobalState &cb, string str) {
     auto fileId = ast->loc.file();
     auto tree = ast::ParsedFile{ast::desugar::node2Tree(ctx, move(ast)), fileId};
     tree.tree = dsl::DSL::run(ctx, move(tree.tree));
+    tree.tree = local_vars::LocalVars::run(ctx, move(tree.tree));
     tree = namer::Namer::run(ctx, move(tree));
     vector<ast::ParsedFile> trees;
     trees.emplace_back(move(tree));
