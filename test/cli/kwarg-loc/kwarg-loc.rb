f483dfd6aba5d86f212ccca805114a0b88d089a3
Paul Tarjan <pt@stripe.com>
2019-05-22 16:37:18 -0700

    current state

---
+++ test/cli/kwarg-loc/kwarg-loc.rb
@@ -0,0 +1,23 @@
+# typed: true
+extend T::Sig
+sig do
+  params(
+    a: Integer,
+    b: Integer,
+    c: Integer,
+    d: Integer,
+    e: Integer,
+    f: Integer,
+  )
+    .void
+end
+def foo(a:, b:, c:, d:, e:, f:); end
+
+foo(
+  a: 1,
+  b: 1,
+  c: 1,
+  d: "1",
+  e: 1,
+  f: 1,
+)
