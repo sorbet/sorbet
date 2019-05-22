1d38c2ab8058f4fa85d83bf1361e8f6ca357ca16
Paul Tarjan <pt@stripe.com>
2019-05-22 13:23:47 -0700

    don't write back to the file

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -14,17 +14,20 @@ if [[ "$BUILDKITE_PULL_REQUEST_REPO" == "git://github.com/stripe/sorbet.git" ]];
   whitelisted=1
 fi
 
+pipeline=$(mktemp)
+cp .buildkite/pipeline.yaml "$pipeline"
+
 if [[ -n "${CLEAN_BUILD-}" ]]; then
     tmpfile=$(mktemp)
-    sed -e '/Run optional code coverage/{N;d;}' .buildkite/pipeline.yaml > "$tmpfile"
-    mv "$tmpfile" .buildkite/pipeline.yaml
+    sed -e '/Run optional code coverage/{N;d;}' "$pipeline" > "$tmpfile"
+    mv "$tmpfile" "$pipeline"
 fi
 
 if [[ "${whitelisted}" -ne 1 ]] ; then
     tmpfile=$(mktemp)
    (echo -e "steps:\\n  - block: \":key: Needs contributor approval!\"\\n  - wait: ~\\n";
-    grep -v "steps:" .buildkite/pipeline.yaml ) > "$tmpfile"
-    mv "$tmpfile" .buildkite/pipeline.yaml
+    grep -v "steps:" "$pipeline" ) > "$tmpfile"
+    mv "$tmpfile" "$pipeline"
 fi
 
-buildkite-agent pipeline upload
+buildkite-agent pipeline upload < "$pipeline"
