85aeac3c8ee38f2b83dcea2bcf05acc44f18ec8a
Nathaniel Roman <nroman@stripe.com>
2019-05-28 13:42:16 -0700

    more test cases

--- test/cli/autogen-classlist/a.rb
+++ test/cli/autogen-classlist/a.rb
@@ -12,8 +12,19 @@ module A
 
   class Parent; end
   class Child < Parent; end
+
+  sig {params(x: NonExistentClass).returns(Integer)}
+  def foo(x); 1; end
+
+  # None of the following should appear in the output:
+  TypeAlias = T.type_alias(Integer)
+  ClassAlias = Integer
+  Anon = Class.new
 end
 
 module A
   class Dup; end # Only appears once in output
 end
+
+# Parent is not resolved but class is still included in output
+class C::D < NonExistentParent; end
