0337b607f94644792593a0d309f0b61fc88e007c
Paul Tarjan <pt@stripe.com>
2019-05-29 15:48:32 -0700

    more explicit

--- gems/sorbet/test/snapshot/partial/rspec-lots/src/src.rb
+++ gems/sorbet/test/snapshot/partial/rspec-lots/src/src.rb
@@ -1,8 +1,8 @@
 require 'rspec'
 
-1000.times do
-  describe 'thing' do
-    it "does something" do
+1000.times do |i|
+  describe 'thing #{i}' do
+    it "does something #{i}" do
     end
   end
 end
