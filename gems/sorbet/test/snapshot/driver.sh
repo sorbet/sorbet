7b3f1a8956327c9e43dc88310efdafe8869e27fd
Jake Zimmerman <jez@stripe.com>
2019-05-31 10:53:32 -0700

    Add --record flag

--- gems/sorbet/test/snapshot/driver.sh
+++ gems/sorbet/test/snapshot/driver.sh
@@ -19,18 +19,29 @@ usage() {
   echo "Options:"
   echo "  --verbose    Be more verbose (more than just test summary)."
   echo "  --update     If a test fails, overwrite the expected with the actual"
+  echo "  --record     Treat partial tests as total tests for the purpose of creating"
+  echo "               new tests. Requires --update"
 }
 
 VERBOSE=
 UPDATE=
+RECORD=
+FLAGS=()
 while [[ $# -gt 0 ]]; do
   case $1 in
     --verbose)
       VERBOSE="--verbose"
+      FLAGS+=("$VERBOSE")
       shift
       ;;
     --update)
       UPDATE="--update"
+      FLAGS+=("$UPDATE")
+      shift
+      ;;
+    --record)
+      RECORD="--record"
+      FLAGS+=("$RECORD")
       shift
       ;;
     -*)
@@ -50,7 +61,7 @@ done
 
 # ----- Discover and run all the tests -----
 
-info "Running suite: $0 $VERBOSE $UPDATE"
+info "Running suite: $0 ${FLAGS[*]}"
 
 if [ -z "$VERBOSE" ]; then
   info "(re-run with --verbose for more information)"
@@ -67,10 +78,10 @@ for test_dir in "$root_dir/test/snapshot"/{partial,total}/*; do
   relative_test_exe="$(realpath --relative-to="$PWD" "$test_exe")"
   relative_test_dir="$(realpath --relative-to="$PWD" "$test_dir")"
 
-  if "$test_exe" "$test_dir" $VERBOSE $UPDATE; then
-    passing_tests+=("$relative_test_exe $relative_test_dir $VERBOSE $UPDATE")
+  if "$test_exe" "$test_dir" "${FLAGS[@]}"; then
+    passing_tests+=("$relative_test_exe $relative_test_dir ${FLAGS[*]}")
   else
-    failing_tests+=("$relative_test_exe $relative_test_dir $VERBOSE $UPDATE")
+    failing_tests+=("$relative_test_exe $relative_test_dir ${FLAGS[*]}")
   fi
 done
 
