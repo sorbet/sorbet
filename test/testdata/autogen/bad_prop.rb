e8a673f43d5c8dc148ca3aededa3518bca0fd4f2
Getty Ritter <gdritter@stripe.com>
2019-05-31 00:51:17 -0700

    Change bad_prop test to stop before resolving optional

--- test/testdata/autogen/bad_prop.rb
+++ test/testdata/autogen/bad_prop.rb
@@ -1,7 +1,4 @@
-# typed: true
-
-# the error below happens because this is an autogen test, which means
-# we don't run the DSL pass that would remove `optional`
+# typed: autogen
 class A
-  optional :plan, String # error: Method `optional` does not exist
+  optional :plan, String
 end
