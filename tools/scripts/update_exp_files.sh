069a4a7b1e8a7f0d5991226308e2ca54965051e5
Getty Ritter <gdritter@stripe.com>
2019-05-31 17:36:04 -0700

    Move resolve-tree-raw tests to flattened-tree-raw tests

--- tools/scripts/update_exp_files.sh
+++ tools/scripts/update_exp_files.sh
@@ -7,7 +7,7 @@ trap 'rm -f "$COMMAND_FILE"' EXIT
 # TODO(JEZ) Make this faster and better so we can uncomment this
 # gems/sorbet/test/snapshot/driver.sh --update
 
-passes=(parse-tree parse-tree-json ast ast-raw dsl-tree dsl-tree-raw symbol-table symbol-table-raw name-tree name-tree-raw resolve-tree resolve-tree-raw cfg cfg-json flattened flattened-tree autogen)
+passes=(parse-tree parse-tree-json ast ast-raw dsl-tree dsl-tree-raw symbol-table symbol-table-raw name-tree name-tree-raw resolve-tree resolve-tree-raw cfg cfg-json flattened-tree flattened-tree-raw autogen)
 
 bazel build test/cli:update test/lsp:update //main:sorbet-light -c opt "$@"
 
