2d207936e9ea8b2885572cc7c51ee0fd47ae94a7
Alan Wu <XrXr@users.noreply.github.com>
2019-06-04 16:49:38 -0400

    Runtime: Make sig work in class << self
    Previously, in order to use `sig` in `class << self`, one had to
    call `T::Hooks.install(self)` before `class << self`. Failing to
    do so can result in confusing errors like
    
    > You called sig twice without declaring a method inbetween
    
    This commit eliminate the need for calling `T::Hooks.install`
    making `extend T::Sig` the only entry requirement.
    
    Notes on implementation:
    
    `install_hook` is called with the receiver of `sig` and for
    `sig` to work, we need to hook the method definition that immediately
    follows. Sorbet accepts both `def foo` and `def self.foo` and we to
    handle both cases.
    
    The following shows what hooks Ruby call:
    ```
    class A
      class << self
        def foo; end # calls A.singleton_method_added
        def self.foo; end # calls A.singleton_class.singleton_method_added
      end
    end
    ```
    
    We catch `A.singleton_method_added` by defining it on
    `A.singleton_class` and catch `A.singleton_class.singleton_method_added`
    by installing it on `A.singleton_class.singleton_class`.
    
    The code for handling when sig is not a singleton class should be
    effectively untouched.

--- gems/sorbet-runtime/test/types/edge_cases.rb
+++ gems/sorbet-runtime/test/types/edge_cases.rb
@@ -153,6 +153,94 @@ class Opus::Types::Test::EdgeCasesTest < Critic::Unit::UnitTest
     assert_equal(:foo, klass.foo)
   end
 
+  it 'handles class scope change when hooked from class << self' do
+    klass = Class.new do
+      class << self
+        extend T::Sig
+
+        sig {returns(Symbol)}
+        def foo
+          :foo
+        end
+      end
+
+      extend T::Sig
+      sig {returns(Symbol)}
+      def bar
+        :bar
+      end
+    end
+    assert_equal(:foo, klass.foo)
+    assert_equal(:bar, klass.new.bar)
+  end
+
+  it 'checks for type error in class << self' do
+    klass = Class.new do
+      class << self
+        extend T::Sig
+
+        sig {returns(Symbol)}
+        def bad_return
+          1
+        end
+      end
+    end
+
+    assert_raises(TypeError) do
+      klass.bad_return
+    end
+  end
+
+  it 'checks for type error for self.foo in class << self' do
+    klass = Class.new do
+      class << self
+        extend T::Sig
+
+        sig {returns(Symbol)}
+        def self.bad_return
+          1
+        end
+
+        def sanity
+        end
+      end
+    end
+
+    klass.sanity
+    assert_raises(TypeError) do
+      klass.singleton_class.bad_return
+    end
+  end
+
+  it 'calls original singleton_method_added when registering hooks' do
+    klass = Class.new do
+      class << self
+        def singleton_method_added(name)
+          @called ||= []
+          @called << name
+        end
+      end
+
+      extend T::Sig
+      sig {returns(Symbol)}
+      def foo; end
+
+      def self.post_hook; end
+    end
+
+    assert_equal(
+      [
+        :singleton_method_added,
+        :method_added,
+        # hook registration overwrites this method but it should
+        # still call the original defined above
+        :singleton_method_added,
+        :post_hook
+      ],
+      klass.instance_variable_get(:@called)
+    )
+  end
+
   it 'keeps raising for bad sigs' do
     klass = Class.new do
       extend T::Sig
