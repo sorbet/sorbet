8403e0fe58aef65e859e5444e340646653a67e06
Getty Ritter <gdritter@stripe.com>
2019-06-05 16:05:29 -0700

    Remove merge noise left in cache-keeps-print-options test

--- test/cli/cache-keeps-print-options/cache-keeps-print-options.sh
+++ test/cli/cache-keeps-print-options/cache-keeps-print-options.sh
@@ -10,14 +10,9 @@ main/sorbet --silence-dev-message -p ast --cache-dir="$CACHE" test/cli/cache-kee
 main/sorbet --silence-dev-message -p ast --cache-dir="$CACHE" test/cli/cache-keeps-print-options/cache-keeps-print-options.rb 2>/dev/null
 rm -rf "$CACHE"/*.mdb
 
-<<<<<<< HEAD
-main/sorbet --silence-dev-message -p dsl-tree --cache-dir="$CACHE" test/cli/cache-keeps-print-options/cache-keeps-print-options.rb 2>&1
-main/sorbet --silence-dev-message -p dsl-tree --cache-dir="$CACHE" test/cli/cache-keeps-print-options/cache-keeps-print-options.rb 2>&1
+main/sorbet --silence-dev-message -p dsl-tree --cache-dir="$CACHE" test/cli/cache-keeps-print-options/cache-keeps-print-options.rb 2>/dev/null
+main/sorbet --silence-dev-message -p dsl-tree --cache-dir="$CACHE" test/cli/cache-keeps-print-options/cache-keeps-print-options.rb 2>/dev/null
 rm -rf "$CACHE"/*.mdb
 
-main/sorbet --silence-dev-message -p index-tree --cache-dir="$CACHE" test/cli/cache-keeps-print-options/cache-keeps-print-options.rb 2>&1
-main/sorbet --silence-dev-message -p index-tree --cache-dir="$CACHE" test/cli/cache-keeps-print-options/cache-keeps-print-options.rb 2>&1
-=======
-main/sorbet --silence-dev-message -p dsl-tree --cache-dir="$CACHE" test/cli/cache-keeps-print-options/cache-keeps-print-options.rb 2>/dev/null
-main/sorbet --silence-dev-message -p dsl-tree --cache-dir="$CACHE" test/cli/cache-keeps-print-options/cache-keeps-print-options.rb 2>/dev/null
->>>>>>> Remove flushing and fix tests instead
+main/sorbet --silence-dev-message -p index-tree --cache-dir="$CACHE" test/cli/cache-keeps-print-options/cache-keeps-print-options.rb 2>/dev/null
+main/sorbet --silence-dev-message -p index-tree --cache-dir="$CACHE" test/cli/cache-keeps-print-options/cache-keeps-print-options.rb 2>/dev/null
