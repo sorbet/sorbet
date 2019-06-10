335904ce77b4bb5b03a7a34baae7e1d8d685c6cf
Ariel Davis <azdavis@stripe.com>
2019-06-10 13:58:51 -0700

    Add more tests

--- test/testdata/case_exhaustive_union_type.rb
+++ test/testdata/case_exhaustive_union_type.rb
@@ -41,9 +41,47 @@ def exhaustive_unreachable_else(x)
 end
 
 sig {params(x: T.any(Integer, String)).returns(Integer)}
+def exhaustive_unreachable_when_before(x)
+  ret = case x
+  when Symbol
+    3 # error: This code is unreachable
+  when String
+    x.to_i
+  when Integer
+    x
+  end
+  T.reveal_type(ret) # error: Revealed type: `Integer`
+  ret
+end
+
+sig {params(x: T.any(Integer, String)).returns(Integer)}
+def exhaustive_unreachable_when_after(x)
+  ret = case x
+  when String
+    x.to_i
+  when Integer
+    x
+  when Symbol # error: This code is unreachable
+    3 # error: This code is unreachable
+  end
+  T.reveal_type(ret) # error: Revealed type: `Integer`
+  ret
+end
+
+sig {params(x: T.any(Integer, String)).returns(Integer)}
 def non_exhaustive_some_cases(x) # error: Returning value that does not conform to method result type
   case x
   when Integer
     x
+  end
+end
+
+sig {params(x: T.any(Integer, String)).returns(Integer)}
+def non_exhaustive_some_cases_unreachable_when(x) # error: Returning value that does not conform to method result type
+  case x
+  when Integer
+    x
+  when Symbol
+    3 # error: This code is unreachable
   end
 end
