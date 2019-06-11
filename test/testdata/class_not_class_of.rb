6e89746decbe497373f518fc4bfe30b7b784eba9
Ariel Davis <azdavis@stripe.com>
2019-06-11 09:55:43 -0700

    Weaken a test

--- test/testdata/class_not_class_of.rb
+++ test/testdata/class_not_class_of.rb
@@ -5,9 +5,4 @@ extend T::Sig
 sig {params(c: Class).void}
 def take_class(c)
   T.reveal_type(c) # error: Revealed type: `Class`
-  if c === c
-    puts
-  else
-    puts # error: This code is unreachable
-  end
 end
