68d5c9f1db6e4f916cd78088b454289c37af8e22
Jake Zimmerman <zimmerman.jake@gmail.com>
2019-06-11 10:16:16 -0700

    Add failing test (#794)
    * Add failing test
    
    * Fix test

---
+++ test/testdata/infer/case_when_any.rb
@@ -0,0 +1,18 @@
+# typed: true
+
+class A; end
+class B; end
+class C; end
+
+extend T::Sig
+
+# TODO(azdavis) This should not be an error after exhaustiveness checks
+sig {params(x: T.any(A, B, C)).returns(T.any(A, B, C))}
+def foo(x) # error: Returning value that does not conform to method result type
+  case x
+  when A, B
+    T.reveal_type(x) # error: Revealed type: `T.any(B, A)`
+  when C
+    T.reveal_type(x) # error: Revealed type: `C`
+  end
+end
