ffba8cef643df47b0771690581135e79ae804468
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2019-06-12 17:11:25 -0400

    Add tests for `--no-config` option
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

--- test/cli/config-file/config-file.sh
+++ test/cli/config-file/config-file.sh
@@ -11,3 +11,18 @@ echo ------------------------------
 
 # CLI args override config file args
 "$cwd/main/sorbet" --typed=false 2>&1
+
+echo ------------------------------
+
+# Option `--no-config` avoid loading config file
+"$cwd/main/sorbet" --silence-dev-message --no-config -e "1 + 1" 2>&1
+
+echo ------------------------------
+
+# Option `--no-config` cannot be used inside a config file
+"$cwd/main/sorbet" "@sorbet/bad_no_config" 2>&1
+
+echo ------------------------------
+
+# Option `--no-config` allows another config file
+"$cwd/main/sorbet" --no-config "@sorbet/other_config" 2>&1
