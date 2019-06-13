b7d821097bdbbf831a1c24fc13212e2b78464287
Sorbet build farm <sorbet+bot@stripe.com>
2019-06-13 17:55:21 +0000

    Updated site - 2019-06-13T17:55:21+0000

---
+++ js/error-reference.js
@@ -0,0 +1,17 @@
+(() => {
+  // Validate the string because we're about to inject it into the page.
+  const errorCode = window.location.hash.substring(1);
+  if (errorCode == null || errorCode.match(/\d\d\d\d/) == null) {
+    return;
+  }
+
+  if (document.getElementById(errorCode) != null) {
+    return;
+  }
+
+  const msg = `error code <strong>${errorCode}</strong>`;
+  document.getElementById('missing-error-code').innerHTML = msg;
+
+  const box = document.getElementById('missing-doc-for-error-code-box');
+  box.classList.toggle('is-hidden');
+})();
