892430c489e6d9a3a87ffd5093b2823d62d6f588
Ariel Davis <azdavis@stripe.com>
2019-06-12 17:58:54 -0700

    Re-extract is_final?

--- gems/sorbet-runtime/lib/types/private/final.rb
+++ gems/sorbet-runtime/lib/types/private/final.rb
@@ -6,12 +6,16 @@ module T::Private::Final
     if !klass.is_a?(Class)
       raise "#{klass.name} is not a class (it is a #{klass.class}), but was declared final"
     end
-    if klass.instance_variable_get(:@sorbet_is_final) # rubocop:disable PrisonGuard/NoLurkyInstanceVariableAccess
+    if self.is_final?(klass)
       raise "#{klass.name} was already declared as final"
     end
     klass.define_singleton_method(:inherited) do |sub|
       raise "#{self.name} was declared final and cannot be subclassed"
     end
     klass.instance_variable_set(:@sorbet_is_final, true) # rubocop:disable PrisonGuard/NoLurkyInstanceVariableAccess
+  end
+
+  def self.is_final?(klass)
+    klass.instance_variable_get(:@sorbet_is_final) # rubocop:disable PrisonGuard/NoLurkyInstanceVariableAccess
   end
 end
