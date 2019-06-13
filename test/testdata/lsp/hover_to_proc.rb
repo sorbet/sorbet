681a3a5b2190ddfd66176379932895c1b2a4db13
John Vilk <43145439+jvilk-stripe@users.noreply.github.com>
2019-06-13 15:31:24 -0700

    Fix Hover for &:symbol (#842)
    * Fix Hover for &:symbol
    
    Now shows type of :symbol.

---
+++ test/testdata/lsp/hover_to_proc.rb
@@ -0,0 +1,18 @@
+# typed: true
+
+class Dog
+  extend T::Sig
+
+  sig {returns(String)}
+  attr_reader :breed
+end
+
+def main
+  dogs = T.let([], T::Array[Dog])
+  # Check hover of breeds / map / : / breed
+  breeds = dogs.map(&:breed)
+# ^ hover: T::Array[String]
+              # ^ hover: sig {params(blk: T.proc.params(arg0: Dog).returns(String)).returns(T::Array[String])}
+                   # ^ hover: sig {returns(String)}
+                    # ^ hover: sig {returns(String)}
+end
