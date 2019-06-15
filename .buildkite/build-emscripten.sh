0998f4fa982defaae393427f307bdcedb14a6ba4
Dmitry Petrashko <dmitry@stripe.com>
2019-06-15 03:01:05 -0700

    Move emscripten build to linux

--- .buildkite/build-emscripten.sh
+++ .buildkite/build-emscripten.sh
@@ -4,28 +4,16 @@ set -euo pipefail
 
 echo "--- Pre-setup"
 
-unameOut="$(uname -s)"
-case "${unameOut}" in
-    Linux*)     platform="linux";;
-    Darwin*)    platform="mac";;
-    *)          exit 1
-esac
-
-if [[ "linux" == "$platform" ]]; then
-  echo "only mac builds are supported"
-  exit 1
-elif [[ "mac" == "$platform" ]]; then
-  echo "mac builds are supported"
-  command -v node >/dev/null 2>&1 || brew install node
-  command -v realpath >/dev/null 2>&1 || brew install realpath
-fi
+command -v node
+command -v realpath
 
 export JOB_NAME=build-emscripten
 source .buildkite/tools/setup-bazel.sh
 
 PATH=$PATH:$(pwd)
 export PATH
-tools/scripts/update-sorbet.run.sh
+
+bazel build //emscripten:sorbet-wasm.tar --config=webasm-linux --strip=always
 
 rm -rf _out_
 mkdir -p _out_/webasm
