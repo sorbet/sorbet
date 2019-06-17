467fba2a8652ac83ffcd0652a0de365324181c09
Ariel Davis <azdavis@stripe.com>
2019-06-17 12:22:30 -0700

    Runtime-test '< T::Struct'

---
+++ gems/sorbet-runtime/test/types/struct.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+require_relative '../test_helper'
+
+class Opus::Types::Test::StructValidationTest < Critic::Unit::UnitTest
+  it "forbids subclassing a Struct" do
+    c = Class.new(T::Struct)
+    err = assert_raises(RuntimeError) do
+      Class.new(c)
+    end
+    assert_includes(err.message, "is a subclass of T::Struct and cannot be subclassed")
+  end
+end
