997cc11305cd050742c4574f9d7989f80947db01
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2019-06-20 10:50:00 -0400

    Refactor `dedup-input-files` test
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

--- test/cli/dedup-input-files/dedup-input-files.sh
+++ test/cli/dedup-input-files/dedup-input-files.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-main/sorbet --silence-dev-message dummy dummy dummy dummy dummy dummy 2>&1
+cwd="$(pwd)"
+cd test/cli/dedup-input-files || exit 1
+
+# Dedup dummy files
+"$cwd/main/sorbet" --silence-dev-message dummy dummy dummy dummy dummy dummy 2>&1
