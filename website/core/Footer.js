7ab2564aaca51c990aec7b3958b61455c1567283
Jake Zimmerman <zimmerman.jake@gmail.com>
2019-06-20 12:41:30 -0700

    Fix broken links (#926)
    Fixes #924.

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -2,21 +2,25 @@ const React = require('react');
 
 class Footer extends React.Component {
   render() {
+    const {config: siteConfig} = this.props;
+    const {baseUrl, docsUrl} = siteConfig;
+    const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
+    const docUrl = (doc) => `${baseUrl}${docsPart}${doc}`;
     // TODO(jez) Consider putting the sitemap / GitHub buttons back.
     return (
       <footer className="nav-footer" id="footer">
         <div className="wrapper">
           <p className="footer">
             © 2019 Stripe{' · '}
-            <a href="docs/adopting">Get started</a>
+            <a href={docUrl('adopting')}>Get started</a>
             {' · '}
-            <a href="docs/">Docs</a>
+            <a href={docUrl('overview')}>Docs</a>
             {' · '}
             <a href="https://sorbet.run">Try</a>
             {' · '}
-            <a href="en/community">Community</a>
+            <a href={`${baseUrl}en/community`}>Community</a>
             {' · '}
-            <a href="blog/">Blog</a>
+            <a href={`${baseUrl}blog`}>Blog</a>
             {' · '}
             <a href="https://twitter.com/sorbet_ruby">Twitter</a>
           </p>
