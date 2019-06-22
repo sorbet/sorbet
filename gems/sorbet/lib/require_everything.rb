da2c7192c2a22cfe954d05d1f8fdc4223e361d3b
Edwin Mak <edwinthinks@gmail.com>
2019-06-22 12:28:20 -0500

    Add conditional in require_everything to support arity change in Rails 6+

--- gems/sorbet/lib/require_everything.rb
+++ gems/sorbet/lib/require_everything.rb
@@ -139,9 +139,7 @@ class Sorbet::Private::RequireEverything
     excluded_paths = Set.new
 
     # Exclude files that have already been loaded by rails
-    rails = Object.const_get(:Rails)
-    load_paths = rails.application.send(:_all_load_paths)
-    load_paths.each do |path|
+    rails_load_paths.each do |path|
       excluded_paths += Dir.glob("#{path}/**/*.rb")
     end
 
@@ -149,6 +147,21 @@ class Sorbet::Private::RequireEverything
     # can contain side-effects like monkey-patching that should
     # only be run once.
     excluded_paths += Dir.glob("#{Dir.pwd}/config/initializers/**/*.rb")
+  end
+
+  def rails_load_paths
+    rails = Object.const_get(:Rails)
+    _all_load_paths_method = rails.application.method(:_all_load_paths)
+
+    # As per changes made to change the arity of this method:
+    # https://github.com/rails/rails/commit/b6e17b6a4b67ccc9fac5fe16741c3db720f00959
+    # This sets the `add_autoload_paths_to_load_path` parameter to `true` which will
+    # provide parity with older versions of Rails prior to the mentioned commit.
+    if _all_load_paths_method.parameters == [:req, :add_autoload_paths_to_load_path]
+      _all_load_paths_method.call(true)
+    else
+      _all_load_paths_method.call
+    end
   end
 
   def self.rails?
