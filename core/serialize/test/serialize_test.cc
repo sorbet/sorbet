d5a7e4c47d5fc753d6037a037259c49b0f83e661
Dmitry Petrashko <dmitry@stripe.com>
2019-06-24 22:37:59 -0700

    Fix test compilation

--- core/serialize/test/serialize_test.cc
+++ core/serialize/test/serialize_test.cc
@@ -2,17 +2,22 @@
 // has to go first as it violates are requirements
 #include "core/serialize/pickler.h"
 #include "core/serialize/serialize.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+#include "spdlog/spdlog.h"
 
 using namespace std;
 
+namespace spd = spdlog;
 namespace sorbet::core::serialize {
+
+auto logger = spd::stderr_color_mt("serialize_test");
 
 TEST(SerializeTest, U4) { // NOLINT
     Pickler p;
     p.putU4(0);
     p.putU4(1);
     p.putU4(4294967295);
-    UnPickler u(p.result(Serializer::GLOBAL_STATE_COMPRESSION_DEGREE).data());
+    UnPickler u(p.result(Serializer::GLOBAL_STATE_COMPRESSION_DEGREE).data(), *logger);
     EXPECT_EQ(u.getU4(), 0);
     EXPECT_EQ(u.getU4(), 1);
     EXPECT_EQ(u.getU4(), 4294967295);
@@ -29,7 +34,7 @@ TEST(SerializeTest, U4U1) { // NOLINT
     p.putU4(1);
     p.putU1(0);
     p.putU4(4294967295);
-    UnPickler u(p.result(Serializer::GLOBAL_STATE_COMPRESSION_DEGREE).data());
+    UnPickler u(p.result(Serializer::GLOBAL_STATE_COMPRESSION_DEGREE).data(), *logger);
     EXPECT_EQ(u.getU4(), 0);
     EXPECT_EQ(u.getU4(), 0);
     EXPECT_EQ(u.getStr(), "aaaaa");
@@ -47,7 +52,7 @@ TEST(SerializeTest, U8) { // NOLINT
     p.putS8(1);
     p.putS8(-1);
     p.putS8(9223372036854775807);
-    UnPickler u(p.result(Serializer::GLOBAL_STATE_COMPRESSION_DEGREE).data());
+    UnPickler u(p.result(Serializer::GLOBAL_STATE_COMPRESSION_DEGREE).data(), *logger);
     EXPECT_EQ(u.getS8(), 0);
     EXPECT_EQ(u.getS8(), 1);
     EXPECT_EQ(u.getS8(), -1);
@@ -63,7 +68,7 @@ TEST(SerializeTest, Strings) { // NOLINT
     p.putStr("Z");
     p.putStr("НЯ");
     p.putStr("\0\0\0\t\n\f\rНЯЯЯЯЯ");
-    UnPickler u(p.result(Serializer::GLOBAL_STATE_COMPRESSION_DEGREE).data());
+    UnPickler u(p.result(Serializer::GLOBAL_STATE_COMPRESSION_DEGREE).data(), *logger);
     EXPECT_EQ(u.getStr(), "");
     EXPECT_EQ(u.getStr(), "a");
     EXPECT_EQ(u.getStr(), "aaaaa");
