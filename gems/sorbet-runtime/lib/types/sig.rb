3a2a15cfefbefff884b00b509ac488c3c6f64956
Jake Zimmerman <zimmerman.jake@gmail.com>
2019-06-26 16:37:44 -0700

    rubocop disable deleted from paul

--- gems/sorbet-runtime/lib/types/sig.rb
+++ gems/sorbet-runtime/lib/types/sig.rb
@@ -11,7 +11,7 @@ module T::Sig
 
     # At runtime, does nothing, but statically it is treated exactly the same
     # as T::Sig#sig. Only use it in cases where you can't use T::Sig#sig.
-    T::Sig::WithoutRuntime.sig {params(blk: T.proc.bind(T::Private::Methods::DeclBuilder).void).void} # rubocop:disable PrisonGuard/PrivateModule
+    T::Sig::WithoutRuntime.sig {params(blk: T.proc.bind(T::Private::Methods::DeclBuilder).void).void}
     def self.sig(&blk); end # rubocop:disable PrisonGuard/BanBuiltinMethodOverride, Lint/DuplicateMethods
   end
 
