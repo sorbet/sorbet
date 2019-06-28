3afae0bc50f64fc8af82d8098ea43e2993fff70e
Dmitry Petrashko <dmitry@stripe.com>
2019-06-27 22:46:05 -0700

    make linter happy

--- tools/scripts/fuzz.sh
+++ tools/scripts/fuzz.sh
@@ -4,8 +4,8 @@ set -exuo pipefail
 
 # build the actual fuzz target
 bazel build //test/fuzz:fuzz_dash_e --config=fuzz -c opt
-
-export PATH=$PATH:$(pwd)/bazel-sorbet/external/llvm_toolchain/bin/
+PATH=$PATH:$(pwd)/bazel-sorbet/external/llvm_toolchain/bin/
+export PATH
 export ASAN_OPTIONS=dedup_token_length=10 # use top 10 frames to tell different errors appart
 
 
@@ -18,4 +18,4 @@ mkdir -p fuzz_crashers/original
 command -v llvm_symbolizer >/dev/null 2>&1 || { echo 'will need llvm_symbolizer' ; exit 1; }
 
 # fuzz
-nice ./bazel-bin/test/fuzz/fuzz_dash_e -use_value_profile=1 -only_ascii=1 -dict=test/fuzz/ruby.dict -artifact_prefix=fuzz_crashers/original/ fuzz_corpus/ -jobs=100 --stress-incremental-resolver
+nice ./bazel-bin/test/fuzz/fuzz_dash_e -use_value_profile=1 -only_ascii=1 -dict=test/fuzz/ruby.dict -artifact_prefix=fuzz_crashers/original/ fuzz_corpus/ --stress-incremental-resolver
