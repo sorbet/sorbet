811f72dab8358332f779f17a7897714884cac5f4
Jake Zimmerman <zimmerman.jake@gmail.com>
2019-06-28 17:05:26 -0700

    Add ability to set default checked level (#1135)
    * Add ability to set default checked level
    
    * Add tests for default_checked_level=
    
    * Add scarier comment

--- gems/sorbet-runtime/lib/types/configuration.rb
+++ gems/sorbet-runtime/lib/types/configuration.rb
@@ -17,6 +17,19 @@ module T::Configuration
     T::Private::RuntimeLevels.enable_checking_in_tests
   end
 
+  # Configure the default checked level for a sig with no explicit `.checked`
+  # builder. When unset, the default checked level is `:always`.
+  #
+  # Note: setting this option is potentially dangerous! Sorbet can't check all
+  # code statically. The runtime checks complement the checks that Sorbet does
+  # statically, so that methods don't have to guard themselves from being
+  # called incorrectly by untyped code.
+  #
+  # @param [:never, :tests, :always] default_checked_level
+  def self.default_checked_level=(default_checked_level)
+    T::Private::RuntimeLevels.default_checked_level = default_checked_level
+  end
+
   # Set a handler to handle `TypeError`s raised by any in-line type assertions,
   # including `T.must`, `T.let`, `T.cast`, and `T.assert_type!`.
   #
