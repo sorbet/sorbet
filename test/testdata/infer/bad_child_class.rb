a65c4b3a586eaa9aeaee884a598f3b5db00050d4
Getty Ritter <gdritter@stripe.com>
2019-07-01 18:07:05 -0700

    Add fuzzed test case

---
+++ test/testdata/infer/bad_child_class.rb
@@ -0,0 +1,15 @@
+# typed: true
+
+# found by fuzzer: https://github.com/sorbet/sorbet/issues/1080
+
+class PreChild <Parent # error: Type `K` declared by parent `Parent` must be re-declared in `PreChild`
+ ::V = type_member
+end
+
+class Parent
+  extend T::Sig
+  K = type_member
+  sig {returns(K)}
+  def foo; T.unsafe(nil); end
+  puts PreChild.new.foo() # this line previously caused a failed ENFORCE in LambdaParam::_instantiate
+end
