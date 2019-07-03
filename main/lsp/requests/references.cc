022af1ce97d6be52500ae2476008a9258deebe4d
John Vilk <jvilk@stripe.com>
2019-07-02 17:54:15 -0700

    Fix go-to-def and find-references for sends and method defs.
    Also deletes older-style tests for go-to-def and find-ref. They are simpler than the new test cases.

--- main/lsp/requests/references.cc
+++ main/lsp/requests/references.cc
@@ -23,39 +23,44 @@ LSPResult LSPLoop::handleTextDocumentReferences(unique_ptr<core::GlobalState> gs
                                      LSPMethod::TextDocumentCompletion, false);
     if (auto run1 = get_if<TypecheckRun>(&result)) {
         gs = move(run1->gs);
+        // An explicit null indicates that we don't support this request (or that nothing was at the location).
+        // Note: Need to correctly type variant here so it goes into right 'slot' of result variant.
+        response->result = variant<JSONNullObject, vector<unique_ptr<Location>>>(JSONNullObject());
         auto &queryResponses = run1->responses;
         if (!queryResponses.empty()) {
             auto resp = move(queryResponses[0]);
-
+            // N.B.: Ignores literals.
             if (auto constResp = resp->isConstant()) {
                 if (constResp->symbol.exists()) {
-                    auto symRef = constResp->symbol;
-                    auto run2 = setupLSPQueryBySymbol(move(gs), symRef);
+                    auto run2 = setupLSPQueryBySymbol(move(gs), constResp->symbol);
                     gs = move(run2.gs);
-                    vector<unique_ptr<Location>> result;
-                    auto &queryResponses = run2.responses;
-                    for (auto &q : queryResponses) {
-                        result.push_back(loc2Location(*gs, q->getLoc()));
-                    }
-                    response->result = move(result);
+                    response->result = extractLocations(*gs, run2.responses);
+                }
+            } else if (auto defResp = resp->isDefinition()) {
+                if (defResp->symbol.exists()) {
+                    auto run2 = setupLSPQueryBySymbol(move(gs), defResp->symbol);
+                    gs = move(run2.gs);
+                    response->result = extractLocations(*gs, run2.responses);
                 }
             } else if (auto identResp = resp->isIdent()) {
                 std::vector<std::shared_ptr<core::File>> files;
                 auto run2 = runLSPQuery(
                     move(gs), core::lsp::Query::createVarQuery(identResp->owner, identResp->variable), files, true);
                 gs = move(run2.gs);
-                vector<unique_ptr<Location>> result;
-                auto &queryResponses = run2.responses;
-                for (auto &q : queryResponses) {
-                    result.push_back(loc2Location(*gs, q->getLoc()));
+                response->result = extractLocations(*gs, run2.responses);
+            } else if (auto sendResp = resp->isSend()) {
+                auto start = sendResp->dispatchResult.get();
+                vector<unique_ptr<Location>> locations;
+                while (start != nullptr) {
+                    if (start->main.method.exists() && !start->main.receiver->isUntyped()) {
+                        auto run2 = setupLSPQueryBySymbol(move(gs), start->main.method);
+                        gs = move(run2.gs);
+                        locations = extractLocations(*gs, run2.responses, move(locations));
+                    }
+                    start = start->secondary.get();
                 }
-                response->result = move(result);
+                response->result = move(locations);
             }
-        }
-        if (!response->result) {
-            // An explicit null indicates that we don't support this request (or that nothing was at the location).
-            // Note: Need to correctly type variant here so it goes into right 'slot' of result variant.
-            response->result = variant<JSONNullObject, vector<unique_ptr<Location>>>(JSONNullObject());
         }
     } else if (auto error = get_if<pair<unique_ptr<ResponseError>, unique_ptr<core::GlobalState>>>(&result)) {
         // An error happened while setting up the query.
