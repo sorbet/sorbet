91cd761f467e5656a5b17e2cf1219e7d13ab132b
Jake Zimmerman <zimmerman.jake@gmail.com>
2019-07-03 17:18:42 -0700

    sed out the IDs from the symbol-table-json test (#1193)
    * sed out the IDs from the symbol-table-json test
    
    This test usually needs to be updated for any RBI change, which is
    annoying.
    
    * fixup

--- test/cli/symbol-table-json/symbol-table-json.sh
+++ test/cli/symbol-table-json/symbol-table-json.sh
@@ -1,1 +1,5 @@
-main/sorbet test/cli/symbol-table-json/symbol-table-json.rb -p symbol-table-json
+main/sorbet \
+  --silence-dev-message \
+  -p symbol-table-json \
+  test/cli/symbol-table-json/symbol-table-json.rb 2>&1 | \
+    sed -e 's/^\( *\)"id": [0-9]*,$/\1"id": <redacted>,/'
