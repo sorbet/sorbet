7b4f7ab8e3f253e25563c51b43f277d99d963a5b
Trevor Elliott <awesomelyawesome@gmail.com>
2019-07-05 14:15:03 -0700

    Run sorbet gem testing during test-static-sanitized

--- .buildkite/test-static-sanitized.sh
+++ .buildkite/test-static-sanitized.sh
@@ -23,8 +23,9 @@ echo will run with $CONFIG_OPTS
 
 err=0
 
-  # NOTE: running ruby/gem testing without the sanitized flags
-./bazel test @ruby_2_4_3//... @gems//... --config=buildfarm --test_summary=terse || err=$?
+# NOTE: running ruby/gem/srb testing without the sanitized flags
+./bazel test @ruby_2_4_3//... @gems//... //gems/sorbet/test/snapshot \
+  --config=buildfarm --test_summary=terse || err=$?
 
 ./bazel test //... $CONFIG_OPTS --test_summary=terse || err=$?
 
