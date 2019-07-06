8f3e794f1bccdae097fda36511c1b71c4039a065
Trevor Elliott <awesomelyawesome@gmail.com>
2019-07-05 19:23:02 -0700

    Different build config?

--- .buildkite/test-static-sanitized.sh
+++ .buildkite/test-static-sanitized.sh
@@ -28,7 +28,8 @@ err=0
 ./bazel test @ruby_2_4_3//... @gems//... //gems/sorbet/test/snapshot \
   --config=buildfarm-ruby --test_summary=terse || err=$?
 
-./bazel test //... $CONFIG_OPTS --test_summary=terse || err=$?
+# ./bazel test //... $CONFIG_OPTS --test_summary=terse || err=$?
+./bazel test //... --curses=no --test_summary=terse || err=$?
 
 echo "--- uploading test results"
 
