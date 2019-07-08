51e0e05d54570aa69bb810e2d0272fb76ff4b90d
Trevor Elliott <awesomelyawesome@gmail.com>
2019-07-08 16:32:21 -0700

    Rename Key/Val to In/Out to make the tests easier to read

--- test/testdata/infer/generics/disabled/variance_methods.rb
+++ test/testdata/infer/generics/disabled/variance_methods.rb
@@ -7,39 +7,39 @@ class A
 
   abstract!
 
-  Key = type_member(:in) # error: Classes can only have invariant type members
-  Val = type_member(:out)
+  In = type_member(:in) # error: Classes can only have invariant type members
+  Out = type_member(:out)
 
   # should pass: the parameters are used correctly
-  sig {abstract.params(key: Key).returns(Val)}
+  sig {abstract.params(key: In).returns(Out)}
   def lookup(key); end
 
   # should pass: the parameters are used correctly, and the block argument swaps
   # the polarity of the type members
-  sig {abstract.params(key: Key, blk: T.proc.params(arg: Val).returns(Key)).returns(Val)}
+  sig {abstract.params(key: In, blk: T.proc.params(arg: Out).returns(In)).returns(Out)}
   def lookup(key, &blk); end
 
   # should fail: the contravariant type key is returned
-  sig {abstract.returns(Key)}
+  sig {abstract.returns(In)}
   def returns_contravariant; end
 
   # should fail: the contravariant type key is returned
-  sig {abstract.returns(T.nilable(Key))}
+  sig {abstract.returns(T.nilable(In))}
   def returns_nilable_contravariant; end
 
   # should fail: the contravariant type key is given to the block as an argument
-  sig {abstract.params(blk: T.proc.params(arg: Key).void).void}
+  sig {abstract.params(blk: T.proc.params(arg: In).void).void}
   def block_arg_contravariant(&blk); end
 
   # should fail: the covariant type key is taken as an argument
-  sig {abstract.params(val: Val).void}
+  sig {abstract.params(val: Out).void}
   def accepts_covariant(val); end
 
   # should fail: the covariant type key is taken as an argument
-  sig {abstract.params(val: T.nilable(Val)).void}
+  sig {abstract.params(val: T.nilable(Out)).void}
   def accepts_nilable_covariant(val); end
 
-  # should fail: the covariant type Val is returned from the block
-  sig {abstract.params(blk: T.proc.returns(Val)).void}
+  # should fail: the covariant type Out is returned from the block
+  sig {abstract.params(blk: T.proc.returns(Out)).void}
   def block_returns_covariant(&blk); end
 end
