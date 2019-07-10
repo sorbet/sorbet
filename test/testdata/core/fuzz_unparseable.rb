37a118c85c4e9213eb24801fec9e44a5143002a7
John Vilk <jvilk@stripe.com>
2019-07-10 10:37:14 -0700

    Fix up new test.

--- test/testdata/core/fuzz_unparseable.rb
+++ test/testdata/core/fuzz_unparseable.rb
@@ -1,4 +1,4 @@
 # typed: strict
 r *[1]g#typed:true
     # ^ error: Parse Error: unexpected token: syntax error
-m do # error: Parse Error: unexpected token: syntax error
+m do # error-with-dupes: Parse Error: unexpected token: syntax error
