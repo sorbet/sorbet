f378c038c4284f21e1632d371964bd8cb50920eb
Getty Ritter <gdritter@stripe.com>
2019-07-15 13:11:01 -0700

    Ensure that overloadable methods are always explicitly overloaded

--- definition_validator/validator.cc
+++ definition_validator/validator.cc
@@ -182,6 +182,12 @@ void validateOverriding(const core::GlobalState &gs, core::SymbolRef method) {
             }
         }
         auto isRBI = absl::c_any_of(method.data(gs)->locs(), [&](auto &loc) { return loc.file().data(gs).isRBI(); });
+        if (!method.data(gs)->isOverride() && method.data(gs)->hasSig() && overridenMethod.data(gs)->isOverridable() && overridenMethod.data(gs)->hasSig() && !isRBI) {
+            if (auto e = gs.beginError(method.data(gs)->loc(), core::errors::Resolver::UndeclaredOverride)) {
+                e.setHeader("Method `{}` overrides `{}` but is not declared with `{}`", method.data(gs)->show(gs), overridenMethod.data(gs)->show(gs), ".override");
+                e.addErrorLine(overridenMethod.data(gs)->loc(), "defined here");
+            }
+        }
         if ((overridenMethod.data(gs)->isAbstract() || overridenMethod.data(gs)->isOverridable()) &&
             !method.data(gs)->isIncompatibleOverride() && !isRBI) {
             validateCompatibleOverride(gs, overridenMethod, method);
