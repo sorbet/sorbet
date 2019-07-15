0cfaa07aadfbae16c4be763ae55de4a2b0dd8290
Getty Ritter <gdritter@stripe.com>
2019-07-15 16:34:00 -0700

    Don't bother checking if we're in an RBI

--- definition_validator/validator.cc
+++ definition_validator/validator.cc
@@ -181,15 +181,15 @@ void validateOverriding(const core::GlobalState &gs, core::SymbolRef method) {
                 e.addErrorLine(overridenMethod.data(gs)->loc(), "original method defined here");
             }
         }
-        auto isRBI = absl::c_any_of(method.data(gs)->locs(), [&](auto &loc) { return loc.file().data(gs).isRBI(); });
         if (!method.data(gs)->isOverride() && method.data(gs)->hasSig() && overridenMethod.data(gs)->isOverridable() &&
-            overridenMethod.data(gs)->hasSig() && !isRBI) {
+            overridenMethod.data(gs)->hasSig()) {
             if (auto e = gs.beginError(method.data(gs)->loc(), core::errors::Resolver::UndeclaredOverride)) {
                 e.setHeader("Method `{}` overrides `{}` but is not declared with `{}`", method.data(gs)->show(gs),
                             overridenMethod.data(gs)->show(gs), ".override");
                 e.addErrorLine(overridenMethod.data(gs)->loc(), "defined here");
             }
         }
+        auto isRBI = absl::c_any_of(method.data(gs)->locs(), [&](auto &loc) { return loc.file().data(gs).isRBI(); });
         if ((overridenMethod.data(gs)->isAbstract() || overridenMethod.data(gs)->isOverridable()) &&
             !method.data(gs)->isIncompatibleOverride() && !isRBI) {
             validateCompatibleOverride(gs, overridenMethod, method);
