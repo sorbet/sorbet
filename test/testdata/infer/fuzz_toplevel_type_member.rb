a3905b9a78dc8685151978b0e0d8d0fb27cd0275
Getty Ritter <gdritter@stripe.com>
2019-07-15 18:03:52 -0700

    Add comment about test case being fuzzer-derived

--- test/testdata/infer/fuzz_toplevel_type_member.rb
+++ test/testdata/infer/fuzz_toplevel_type_member.rb
@@ -1,4 +1,5 @@
 # typed: true
+# Adapted from fuzzed test case: https://github.com/sorbet/sorbet/issues/1087
 extend T::Sig
 Elem = type_member
 sig {params(block: T.proc.params(ar0: Elem).void).void}
