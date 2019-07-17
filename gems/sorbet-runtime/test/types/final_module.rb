a385bcf935f1cbd8ab1bfc38377946d189ded9b2
Ariel Davis <ariel.z.davis@icloud.com>
2019-07-17 10:17:41 -0700

    Update tests

--- gems/sorbet-runtime/test/types/final_module.rb
+++ gems/sorbet-runtime/test/types/final_module.rb
@@ -25,7 +25,7 @@ class Opus::Types::Test::FinalModuleTest < Critic::Unit::UnitTest
     end
   end
 
-  it "forbids inheriting from a final class" do
+  it "forbids inheriting a final class" do
     c = Class.new do
       extend T::Helpers
       final!
@@ -33,35 +33,30 @@ class Opus::Types::Test::FinalModuleTest < Critic::Unit::UnitTest
     err = assert_raises(RuntimeError) do
       Class.new(c)
     end
-    assert_includes(err.message, "was declared as final and cannot be inherited from")
+    assert_includes(err.message, "was declared as final and cannot be inherited")
   end
 
-  it "forbids declaring a class as final and its instance method as final" do
-    err = assert_raises(RuntimeError) do
-      Class.new do
-        extend T::Helpers
-        final!
-        extend T::Sig
-        sig(:final) {void}
-        def foo; end
-      end
+  it "allows declaring a module as final and its method as final" do
+    Module.new do
+      extend T::Helpers
+      final!
+      extend T::Sig
+      sig(:final) {void}
+      def self.foo; end
     end
-    assert_includes(err.message, "was declared as final and its method")
-    assert_includes(err.message, "cannot also be explicitly declared as final (it is implicitly final)")
   end
 
-  it "forbids declaring a class as final and its instance method as final" do
+  it "forbids declaring a module as final but not its method as final" do
     err = assert_raises(RuntimeError) do
-      Class.new do
+      Module.new do
         extend T::Helpers
         final!
         extend T::Sig
-        sig(:final) {void}
         def self.foo; end
       end
     end
-    assert_includes(err.message, "was declared as final and its method")
-    assert_includes(err.message, "cannot also be explicitly declared as final (it is implicitly final)")
+    assert_includes(err.message, "was declared as final but its method")
+    assert_includes(err.message, "was not declared as final")
   end
 
   it "forbids declaring a class as final and then abstract" do
@@ -105,32 +100,5 @@ class Opus::Types::Test::FinalModuleTest < Critic::Unit::UnitTest
       end
     end
     assert_includes(err.message, "is not a module or method and cannot be declared as final")
-  end
-
-  it "marks the methods of a final module as final" do
-    mod = Module.new do
-      extend T::Helpers
-      final!
-      def self.foo; end
-    end
-    err = assert_raises(RuntimeError) do
-      def mod.foo; end
-    end
-    assert_includes(err.message, "was declared as final and cannot be redefined")
-  end
-
-  it "works with include" do
-    m1 = Module.new do
-      extend T::Helpers
-      final!
-      def foo; end
-    end
-    err = assert_raises(RuntimeError) do
-      Module.new do
-        include m1
-        def foo; end
-      end
-    end
-    assert_includes(err.message, "was declared as final and cannot be overridden")
   end
 end
