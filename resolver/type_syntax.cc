83bd6bc253ddfa402d0f6b15f8a8510d94de7df5
Ufuk Kayserilioglu <ufuk.kayserilioglu@shopify.com>
2019-07-19 00:25:24 +0300

    Use `T.untyped` as the default type argument for all built-in enumerable types (#1309)
    * Silence generic errors on `Range`, `Enumerable` and `Enumerator` too
    
    * Add test for untyped generics behaving properly

--- resolver/type_syntax.cc
+++ resolver/type_syntax.cc
@@ -557,9 +557,11 @@ TypeSyntax::ResultType TypeSyntax::getResultTypeAndBind(core::MutableContext ctx
                 result.type = maybeAliased.data(ctx)->resultType;
                 return;
             }
-            bool silenceGenericError = maybeAliased == core::Symbols::Hash() ||
-                                       maybeAliased == core::Symbols::Array() || maybeAliased == core::Symbols::Set() ||
-                                       maybeAliased == core::Symbols::Struct() || maybeAliased == core::Symbols::File();
+            bool silenceGenericError =
+                maybeAliased == core::Symbols::Hash() || maybeAliased == core::Symbols::Array() ||
+                maybeAliased == core::Symbols::Set() || maybeAliased == core::Symbols::Range() ||
+                maybeAliased == core::Symbols::Enumerable() || maybeAliased == core::Symbols::Enumerator() ||
+                maybeAliased == core::Symbols::Struct() || maybeAliased == core::Symbols::File();
             // TODO: reduce this^^^ set.
             auto sym = maybeAliased.data(ctx)->dealias(ctx);
             if (sym.data(ctx)->isClass()) {
