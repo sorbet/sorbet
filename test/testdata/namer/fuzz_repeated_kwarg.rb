b7b93bc1eee8a18d29718ad81b1fdfa14f3016d3
Getty Ritter <gdritter@stripe.com>
2019-07-18 16:20:30 -0700

    Update repeated kwarg test to include lots of variations

--- test/testdata/namer/fuzz_repeated_kwarg.rb
+++ test/testdata/namer/fuzz_repeated_kwarg.rb
@@ -1,3 +1,17 @@
-# typed: true
+# typed: false
 # from fuzzer: https://github.com/sorbet/sorbet/issues/1133
-def f(x:, x:); end
+def f1(x, x); end # error: Duplicated argument name `x`
+def f2(x, x=0); end # error: Duplicated argument name `x`
+def f3(x:, x:); end # error: Duplicated argument name `x`
+def f4(x:, x: nil); end # error: Duplicated argument name `x`
+def f5(x, x:); end # error: Duplicated argument name `x`
+def f6(x, x: nil); end # error: Duplicated argument name `x`
+def f7(x=0, x: nil); end # error: Duplicated argument name `x`
+
+def f8(
+      this,
+      this: # error: Duplicated argument name `this`
+    )
+end
+
+l1 = lambda { |y, y| } # error: Duplicated argument name `y`
