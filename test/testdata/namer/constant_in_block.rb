4fcbb4a0d0958b95d1e1cf210f62e3adfe3b885f
Trevor Elliott <awesomelyawesome@gmail.com>
2019-07-18 16:06:40 -0700

    Update tests

--- test/testdata/namer/constant_in_block.rb
+++ test/testdata/namer/constant_in_block.rb
@@ -1,15 +1,17 @@
 # typed: true
 class A
-  
+
   def self.foo
     foo do
     C = 1 # this is parse error in Ruby, but not in Sorbet
+  # ^^^^^ error: Dynamic constant assignment
     C # error: Unable to resolve constant
     end
   end
-  
+
   [].each do
     B = 1
+  # ^^^^^ error: Dynamic constant assignment
     B
   end
 end
