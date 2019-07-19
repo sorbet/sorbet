9f138427cd10752baf3b9b79bb336d2a9be5112e
Getty Ritter <gdritter@stripe.com>
2019-07-18 19:09:52 -0700

    Skip LSP tests for alias-through-type-alias test

--- test/testdata/resolver/fuzz_alias_to_type_alias.rb
+++ test/testdata/resolver/fuzz_alias_to_type_alias.rb
@@ -1,4 +1,5 @@
 # typed: true
+# disable-fast-path: true
 # from fuzzer: https://github.com/sorbet/sorbet/issues/1126
 A = T.type_alias(A) # error: Unable to resolve right hand side of type alias `A`
 C = A::B # error: Resolving constants through type aliases is not supported
