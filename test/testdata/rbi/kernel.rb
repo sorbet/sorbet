37ed439e41ce7860bf8218a9d2c532ad440e7f96
Neil Parikh <n3parikh@csclub.uwaterloo.ca>
2019-07-18 22:44:34 -0400

    update sig for loop

--- test/testdata/rbi/kernel.rb
+++ test/testdata/rbi/kernel.rb
@@ -8,13 +8,15 @@ T.assert_type!(catch {1}, BasicObject)
 T.assert_type!(caller, T::Array[String])
 T.assert_type!(caller(10), T.nilable(T::Array[String]))
 
-# make sure we don't regress and mark `loop` as returning `nil`
 x = loop {break 1}
+T.assert_type!(x, Integer)
 if x
   puts x + 1
-else
-  puts x
 end
 
 define_singleton_method(:foo) { puts '' }
 define_singleton_method('foo') { puts '' }
+
+y = loop do
+end
+puts y # error: This code is unreachable
