25b9a2b7df9a15d4beb86ecb307f9d0db40e5863
Ariel Davis <ariel.z.davis@icloud.com>
2019-07-22 17:24:12 -0700

    Test for some allowed void things

--- test/testdata/cfg/return_expr_void.rb
+++ test/testdata/cfg/return_expr_void.rb
@@ -1,8 +1,19 @@
 # typed: true
 class C
   extend T::Sig
+
   sig {void}
-  def foo
-    return 3 # error: `C#foo` has return type `void` but explicitly returns an expression
+  def explicit_return_expr
+    return 3 # error: `C#explicit_return_expr` has return type `void` but explicitly returns an expression
+  end
+
+  sig {void}
+  def implicit_return_at_end
+    3
+  end
+
+  sig {void}
+  def explicit_return_nothing
+    return
   end
 end
