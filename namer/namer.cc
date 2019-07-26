fe22ea84552bece2f4ca6710230f6263e405b781
Getty Ritter <gdritter@stripe.com>
2019-07-26 13:36:39 -0700

    Add a new fresh name type for keyword args and use it when we overlap keyword args

--- namer/namer.cc
+++ namer/namer.cc
@@ -97,7 +97,8 @@ class NameInserter {
         // identical). We know that we need to match the arity of the function as written, so if we don't have as many
         // arguments as we expect, clone the one we got back from enterMethodArgumentSymbol in the position we expect
         if (ctx.owner.dataAllowingNone(ctx)->arguments().size() == pos) {
-            ctx.owner.dataAllowingNone(ctx)->arguments().emplace_back(argInfo.deepCopy());
+            auto &infoCopy = ctx.owner.dataAllowingNone(ctx)->arguments().emplace_back(argInfo.deepCopy());
+            infoCopy.name = ctx.state.freshNameUnique(core::UniqueNameKind::KeywordArg, argInfo.name, pos + 1);
             auto localExpr = make_unique<ast::Local>(parsedArg.loc, parsedArg.local);
             return move(localExpr);
         }
