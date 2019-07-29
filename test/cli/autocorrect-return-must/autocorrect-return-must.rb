28d281615ad7ce97cb013f5297cc668cef8fedd2
Jake Zimmerman <zimmerman.jake@gmail.com>
2019-07-29 16:34:20 -0700

    Add more tests for T.must autocorrect (#1432)
    I'd like to land these into git history first so we have a record of
    them in the revert that I'll make next

--- test/cli/autocorrect-return-must/autocorrect-return-must.rb
+++ test/cli/autocorrect-return-must/autocorrect-return-must.rb
@@ -6,3 +6,23 @@ sig {returns(String)}
 def direct_return
   T.let("", T.nilable(String))
 end
+
+sig {params(x: T.nilable(String)).returns(String)}
+def return_arg(x)
+  x
+end
+
+sig {params(x: T.nilable(String)).returns(String)}
+def return_uninitialized(x)
+  if T.unsafe(nil)
+    a = ""
+  end
+  a
+end
+
+sig {params(x: T.nilable(String)).returns(String)}
+def implicit_return(x)
+  if T.unsafe(nil)
+    ""
+  end
+end
