b394abdd435560efb95d02bc89e2e2631676fed1
Ariel Davis <ariel.z.davis@icloud.com>
2019-07-29 11:23:51 -0700

    Get empty fuzz_doc_symbols to compile

---
+++ test/fuzz/fuzz_doc_symbols.cc
@@ -0,0 +1,10 @@
+#include <cstddef>
+#include <cstdint>
+
+extern "C" int LLVMFuzzerInitialize(const int *argc, const char ***argv) {
+    return 0;
+}
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, const std::size_t size) {
+    return 0;
+}
