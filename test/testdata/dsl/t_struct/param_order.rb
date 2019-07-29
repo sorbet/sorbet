0b08d6bad71990400ea9b3ab60e8710cc2a655e7
Ariel Davis <ariel.z.davis@icloud.com>
2019-07-29 13:28:20 -0700

    Add a comment

--- test/testdata/dsl/t_struct/param_order.rb
+++ test/testdata/dsl/t_struct/param_order.rb
@@ -1,5 +1,8 @@
 # typed: true
 
+# We run checks to ensure that all optional arguments to a method appear after all required arguments. This means that
+# when we synthesize the initalize method for this struct, the optional foo parameter must by synthesized to come after
+# the required bar parameter, even though the order we wrote in the code was the opposite.
 class ParamOrder < T::Struct
   prop :foo, Integer, default: 3
   prop :bar, Integer
