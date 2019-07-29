541ef2abd4148c5547890ce5704e529b18cabdde
Ariel Davis <ariel.z.davis@icloud.com>
2019-07-29 15:38:08 -0700

    Clean up a script

--- tools/scripts/fuzz.sh
+++ tools/scripts/fuzz.sh
@@ -2,20 +2,23 @@
 
 set -exuo pipefail
 
-# build the actual fuzz target
+if ! command -v llvm-symbolizer >/dev/null; then
+  echo "fatal: command not found: llvm-symbolizer"
+  exit 1
+fi
+
 bazel build //test/fuzz:fuzz_dash_e --config=fuzz -c opt
-PATH=$PATH:$(pwd)/bazel-sorbet/external/llvm_toolchain/bin/
-export PATH
-export ASAN_OPTIONS=dedup_token_length=10 # use top 10 frames to tell different errors appart
+export PATH="$PATH:$(pwd)/bazel-sorbet/external/llvm_toolchain/bin"
+export ASAN_OPTIONS='dedup_token_length=10'
 
-
-# setup corpus
 mkdir -p fuzz_corpus
-find ./test/testdata/ -iname "*.rb"|grep -v disable|xargs -n 1 -I % cp % fuzz_corpus
+find test/testdata -iname "*.rb" | grep -v disable | xargs -n 1 -I % cp % fuzz_corpus
 
 mkdir -p fuzz_crashers/original
 
-command -v llvm-symbolizer >/dev/null 2>&1 || { echo 'will need llvm-symbolizer' ; exit 1; }
-
-# fuzz
-nice ./bazel-bin/test/fuzz/fuzz_dash_e -use_value_profile=1 -only_ascii=1 -dict=test/fuzz/ruby.dict -artifact_prefix=fuzz_crashers/original/ fuzz_corpus/ --stress-incremental-resolver
+nice ./bazel-bin/test/fuzz/fuzz_dash_e \
+  -use_value_profile=1 \
+  -only_ascii=1 \
+  -dict=test/fuzz/ruby.dict \
+  -artifact_prefix=fuzz_crashers/original/ \
+  fuzz_corpus
