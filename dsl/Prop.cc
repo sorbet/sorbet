a17dfb674af1dde439b4b7c92bf735542e1c98a5
Ariel Davis <ariel.z.davis@icloud.com>
2019-07-29 13:10:05 -0700

    Add required props then optional props

--- dsl/Prop.cc
+++ dsl/Prop.cc
@@ -391,12 +391,25 @@ void Prop::patchDSL(core::MutableContext ctx, ast::ClassDef *klass) {
         args.reserve(props.size());
         sigKeys.reserve(props.size());
         sigVals.reserve(props.size());
+        // add all the required props first.
         for (auto &prop : props) {
+            if (prop.default_.has_value()) {
+                continue;
+            }
             auto loc = prop.loc;
             auto arg = ast::MK::KeywordArg(loc, ast::MK::Local(loc, prop.name));
-            if (prop.default_.has_value()) {
-                arg = ast::MK::OptionalArg(loc, std::move(arg), std::move(*(prop.default_)));
+            args.emplace_back(std::move(arg));
+            sigKeys.emplace_back(ast::MK::Symbol(loc, prop.name));
+            sigVals.emplace_back(std::move(prop.type));
+        }
+        // then, add all the optional props.
+        for (auto &prop : props) {
+            if (!prop.default_.has_value()) {
+                continue;
             }
+            auto loc = prop.loc;
+            auto arg = ast::MK::OptionalArg(loc, ast::MK::KeywordArg(loc, ast::MK::Local(loc, prop.name)),
+                                            std::move(*(prop.default_)));
             args.emplace_back(std::move(arg));
             sigKeys.emplace_back(ast::MK::Symbol(loc, prop.name));
             sigVals.emplace_back(std::move(prop.type));
