40dc688eee3e09be9ef53bb27fb208e3ec9c96bb
Ariel Davis <ariel.z.davis@icloud.com>
2019-07-29 18:05:01 -0700

    Let fuzz take an argument

--- tools/scripts/fuzz.sh
+++ tools/scripts/fuzz.sh
@@ -2,12 +2,22 @@
 
 set -exuo pipefail
 
+what=""
+if [ "$#" -eq 0 ]; then
+  what="fuzz_dash_e"
+elif [ "$#" -eq 1 ]; then
+  what="$1"
+else
+  echo "usage: $0 <fuzz_target>"
+  exit 1
+fi
+
 if ! command -v llvm-symbolizer >/dev/null; then
   echo "fatal: command not found: llvm-symbolizer"
   exit 1
 fi
 
-bazel build //test/fuzz:fuzz_dash_e --config=fuzz -c opt
+bazel build "//test/fuzz:$what" --config=fuzz -c opt
 export PATH="$PATH:$(pwd)/bazel-sorbet/external/llvm_toolchain/bin"
 export ASAN_OPTIONS='dedup_token_length=10'
 
@@ -16,7 +26,7 @@ find test/testdata -iname "*.rb" | grep -v disable | xargs -n 1 -I % cp % fuzz_c
 
 mkdir -p fuzz_crashers/original
 
-nice ./bazel-bin/test/fuzz/fuzz_dash_e \
+nice "./bazel-bin/test/fuzz/$what" \
   -use_value_profile=1 \
   -only_ascii=1 \
   -dict=test/fuzz/ruby.dict \
