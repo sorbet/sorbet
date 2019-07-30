eaf2d26cb626085134666593e7a94de4af33fed8
Ariel Davis <ariel.z.davis@icloud.com>
2019-07-29 17:59:28 -0700

    Send msgs for {did open, get symbols}

--- test/fuzz/fuzz_doc_symbols.cc
+++ test/fuzz/fuzz_doc_symbols.cc
@@ -5,6 +5,7 @@
 #include "spdlog/sinks/stdout_sinks.h"
 #include "test/helpers/MockFileSystem.h"
 #include "test/helpers/lsp.h"
+#include "test/helpers/position_assertions.h"
 #include <cstddef>
 #include <cstdint>
 #include <memory>
@@ -46,8 +47,21 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, const std::size_t siz
     auto lspWrapper = sorbet::realmain::lsp::LSPWrapper(std::move(gs), mkOpts(), console, false);
 
     int nextId = 0;
+    std::string contents((const char *)data, size);
+
     // TODO ignore responses?
+
     sorbet::test::initializeLSP(rootPath, rootUri, lspWrapper, nextId);
+
+    lspWrapper.getLSPResponsesFor(sorbet::test::LSPMessage(std::make_unique<sorbet::test::NotificationMessage>(
+        "2.0", sorbet::test::LSPMethod::TextDocumentDidOpen,
+        std::make_unique<sorbet::test::DidOpenTextDocumentParams>(std::make_unique<sorbet::test::TextDocumentItem>(
+            sorbet::test::filePathToUri(rootUri, "file.rb"), "ruby", 1, contents)))));
+
+    lspWrapper.getLSPResponsesFor(sorbet::test::LSPMessage(std::make_unique<sorbet::test::RequestMessage>(
+        "2.0", nextId++, sorbet::test::LSPMethod::TextDocumentDocumentSymbol,
+        std::make_unique<sorbet::test::DocumentSymbolParams>(
+            std::make_unique<sorbet::test::TextDocumentIdentifier>("file.rb")))));
 
     return 0;
 }
