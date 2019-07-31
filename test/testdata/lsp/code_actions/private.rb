42a893bf22a6fd1ee69056e9ea098a5552405051
Sushain Cherivirala <sushain@stripe.com>
2019-07-31 13:43:44 -0700

    Fix tests

--- test/testdata/lsp/code_actions/private.rb
+++ test/testdata/lsp/code_actions/private.rb
@@ -11,12 +11,12 @@ end
 
 class C
   private_class_method def foo; end
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ error-with-dupes: Use `private` to define private instance methods
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ error: Use `private` to define private instance methods
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ apply-code-action: [A] Use `private` to define private instance methods
 end
 
 class D
   private def self.foo; end
-# ^^^^^^^^^^^^^^^^^^^^^^^^^ error-with-dupes: Use `private_class_method` to define private class methods
+# ^^^^^^^^^^^^^^^^^^^^^^^^^ error: Use `private_class_method` to define private class methods
 # ^^^^^^^^^^^^^^^^^^^^^^^^^ apply-code-action: [B] Use `private_class_method` to define private class methods
 end
