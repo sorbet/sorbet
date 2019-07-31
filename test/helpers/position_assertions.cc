b48d173ed97456e63505e9bfecebfdd428555946
Sushain Cherivirala <sushain@stripe.com>
2019-07-31 11:28:20 -0700

    Fix sort order

--- test/helpers/position_assertions.cc
+++ test/helpers/position_assertions.cc
@@ -925,7 +925,7 @@ void ApplyCodeActionAssertion::check(const UnorderedMap<std::string, std::shared
             << fmt::format("Requested code action for {}, but received edits in {}", fileUri, c->textDocument->uri);
 
         // First, sort the edits by increasing starting location and verify that none overlap.
-        fast_sort(c->edits, [](const auto &l, const auto &r) -> bool { return cmpRanges(*l->range, *r->range) > 0; });
+        fast_sort(c->edits, [](const auto &l, const auto &r) -> bool { return cmpRanges(*l->range, *r->range) < 0; });
         for (u4 i = 1; i < c->edits.size(); i++) {
             ASSERT_LT(cmpPositions(*c->edits[i - 1]->range->end, *c->edits[i]->range->start), 0)
                 << fmt::format("Received quick fix edit\n{}\nthat overlaps edit\n{}\nThe test runner does not support "
