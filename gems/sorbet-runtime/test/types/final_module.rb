3f73d09b9b923160ab322ae0a1e0ff21742f5ce5
Ariel Davis <ariel.z.davis@icloud.com>
2019-07-31 16:03:12 -0700

    Show the error message in tests

--- gems/sorbet-runtime/test/types/final_module.rb
+++ gems/sorbet-runtime/test/types/final_module.rb
@@ -33,7 +33,7 @@ class Opus::Types::Test::FinalModuleTest < Critic::Unit::UnitTest
     err = assert_raises(RuntimeError) do
       Class.new(c)
     end
-    assert_includes(err.message, "was declared as final and cannot be inherited")
+    assert_match(/^#<Class:0x[0-9a-f]+> was declared as final and cannot be inherited$/, err.message)
   end
 
   it "forbids including a final module" do
@@ -46,7 +46,7 @@ class Opus::Types::Test::FinalModuleTest < Critic::Unit::UnitTest
         include m
       end
     end
-    assert_includes(err.message, "was declared as final and cannot be included")
+    assert_match(/^#<Module:0x[0-9a-f]+> was declared as final and cannot be included$/, err.message)
   end
 
   it "forbids extending a final module" do
@@ -59,7 +59,7 @@ class Opus::Types::Test::FinalModuleTest < Critic::Unit::UnitTest
         extend m
       end
     end
-    assert_includes(err.message, "was declared as final and cannot be extended")
+    assert_match(/^#<Module:0x[0-9a-f]+> was declared as final and cannot be extended$/, err.message)
   end
 
   it "allows declaring a module as final and its instance method as final" do
@@ -91,8 +91,7 @@ class Opus::Types::Test::FinalModuleTest < Critic::Unit::UnitTest
         def foo; end
       end
     end
-    assert_includes(err.message, "was declared as final but its method")
-    assert_includes(err.message, "was not declared as final")
+    assert_match(/^#<Module:0x[0-9a-f]+> was declared as final but its method `foo` was not declared as final$/, err.message)
   end
 
   it "forbids declaring a module as final but not its class method as final" do
@@ -104,8 +103,7 @@ class Opus::Types::Test::FinalModuleTest < Critic::Unit::UnitTest
         def self.foo; end
       end
     end
-    assert_includes(err.message, "was declared as final but its method")
-    assert_includes(err.message, "was not declared as final")
+    assert_match(/^#<Class:#<Module:0x[0-9a-f]+>> was declared as final but its method `foo` was not declared as final$/, err.message)
   end
 
   it "forbids declaring a class as final and then abstract" do
@@ -116,7 +114,7 @@ class Opus::Types::Test::FinalModuleTest < Critic::Unit::UnitTest
         abstract!
       end
     end
-    assert_includes(err.message, "was already declared as final and cannot be declared as abstract")
+    assert_match(/^#<Class:0x[0-9a-f]+> was already declared as final and cannot be declared as abstract$/, err.message)
   end
 
   it "forbids declaring a class as abstract and then final" do
@@ -127,7 +125,7 @@ class Opus::Types::Test::FinalModuleTest < Critic::Unit::UnitTest
         final!
       end
     end
-    assert_includes(err.message, "was already declared as abstract and cannot be declared as final")
+    assert_match(/^#<Class:0x[0-9a-f]+> was already declared as abstract and cannot be declared as final$/, err.message)
   end
 
   it "forbids re-declaring a class as final" do
@@ -138,7 +136,7 @@ class Opus::Types::Test::FinalModuleTest < Critic::Unit::UnitTest
         final!
       end
     end
-    assert_includes(err.message, "was already declared as final and cannot be re-declared as final")
+    assert_match(/^#<Class:0x[0-9a-f]+> was already declared as final and cannot be re-declared as final$/, err.message)
   end
 
   it "forbids declaring a non-module, non-method as final" do
@@ -148,7 +146,7 @@ class Opus::Types::Test::FinalModuleTest < Critic::Unit::UnitTest
         final!
       end
     end
-    assert_includes(err.message, "is not a class or module and cannot be declared as final with `final!`")
+    assert_match(/^#<Opus::Types::Test::FinalModuleTest:0x[0-9a-f]+> is not a class or module and cannot be declared as final with `final!`$/, err.message)
   end
 
   it "allows a non-final class in a final class" do
@@ -160,7 +158,7 @@ class Opus::Types::Test::FinalModuleTest < Critic::Unit::UnitTest
     end
     Class.new(inner)
     err = assert_raises(RuntimeError) { Class.new(outer) }
-    assert_includes(err.message, "was declared as final")
+    assert_match(/^#<Class:0x[0-9a-f]+> was declared as final and cannot be inherited$/, err.message)
   end
 
   it "allows a non-final class in a final module" do
@@ -172,7 +170,7 @@ class Opus::Types::Test::FinalModuleTest < Critic::Unit::UnitTest
     end
     Class.new(inner)
     err = assert_raises(RuntimeError) { Class.new.include(outer) }
-    assert_includes(err.message, "was declared as final")
+    assert_match(/^#<Module:0x[0-9a-f]+> was declared as final and cannot be included$/, err.message)
   end
 
   it "allows a non-final module in a final class" do
@@ -184,7 +182,7 @@ class Opus::Types::Test::FinalModuleTest < Critic::Unit::UnitTest
     end
     Class.new.include(inner)
     err = assert_raises(RuntimeError) { Class.new(outer) }
-    assert_includes(err.message, "was declared as final")
+    assert_match(/^#<Class:0x[0-9a-f]+> was declared as final and cannot be inherited$/, err.message)
   end
 
   it "allows a non-final module in a final module" do
@@ -196,6 +194,6 @@ class Opus::Types::Test::FinalModuleTest < Critic::Unit::UnitTest
     end
     Class.new.include(inner)
     err = assert_raises(RuntimeError) { Class.new.include(outer) }
-    assert_includes(err.message, "was declared as final")
+    assert_match(/^#<Module:0x[0-9a-f]+> was declared as final and cannot be included$/, err.message)
   end
 end
