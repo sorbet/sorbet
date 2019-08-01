e88de46b47e77cdd84f1076ebc8b4b38003d9cbe
Dmitry Petrashko <dmitry@stripe.com>
2019-07-31 21:11:39 -0700

    Minimize dependencies of sorbet-orig even more (#1411)

---
+++ payload/text/nopopulate.cc
@@ -0,0 +1,9 @@
+#include "payload/text/text.h"
+using namespace std;
+
+namespace sorbet::rbi {
+void polulateRBIsInto(unique_ptr<core::GlobalState> &gs) {
+    Exception::raise("Should never happen");
+}
+
+} // namespace sorbet::rbi
