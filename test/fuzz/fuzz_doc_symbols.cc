90759df1cddaeace8161769b970fa32975e1509b
Ariel Davis <ariel.z.davis@icloud.com>
2019-08-01 14:42:53 -0700

    Use a static lspWrapper

--- test/fuzz/fuzz_doc_symbols.cc
+++ test/fuzz/fuzz_doc_symbols.cc
@@ -48,13 +48,13 @@ sorbet::realmain::lsp::LSPWrapper mkLSPWrapper() {
     return lspWrapper;
 }
 
+static auto lspWrapper = mkLSPWrapper();
+
 extern "C" int LLVMFuzzerInitialize(const int *argc, const char ***argv) {
     return 0;
 }
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, const std::size_t size) {
-    auto lspWrapper = mkLSPWrapper();
-
     // TODO slow
     int nextId = 0;
     sorbet::test::initializeLSP(rootPath, rootUri, lspWrapper, nextId);
