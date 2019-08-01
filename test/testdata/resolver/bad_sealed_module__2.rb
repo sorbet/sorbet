fda8e41b2d65d016cc6ae037024b4fd49743ce62
Jake Zimmerman <zimmerman.jake@gmail.com>
2019-07-31 17:14:49 -0700

    Remove test

--- test/testdata/resolver/bad_sealed_module__2.rb
+++
@@ -1,6 +0,0 @@
-# typed: false
-
-class ChildBad3
-  include Parent # error: `Parent` is sealed and cannot be included in `ChildBad3`
-end
-class ChildBad4; include Parent; end # error-with-dupes: `Parent` is sealed and cannot be included in `ChildBad4`
