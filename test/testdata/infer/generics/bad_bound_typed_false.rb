b22bd96d378467510a15e2789f5b96a46c27db05
Trevor Elliott <awesomelyawesome@gmail.com>
2019-08-01 16:46:00 -0700

    Fix comment in test

--- test/testdata/infer/generics/bad_bound_typed_false.rb
+++ test/testdata/infer/generics/bad_bound_typed_false.rb
@@ -11,7 +11,7 @@ end
 class Test
   extend T::Sig
 
-  # NOTE: there are two errors raised here, one for each bound not satisfied.
+  # NOTE, there are two errors raised here, one for each bound not satisfied.
   sig {params(x: A[String]).void}
                  # ^^^^^^ error-with-dupes: `String` cannot be used for type member
   def foo(x); end
