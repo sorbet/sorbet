cad901f7560944560469056591451e9d0d2ea21a
Jake Zimmerman <zimmerman.jake@gmail.com>
2019-08-02 13:18:27 -0700

    Fix the test

--- test/testdata/dsl/opus_enum_type_syntax.rb
+++ test/testdata/dsl/opus_enum_type_syntax.rb
@@ -19,7 +19,7 @@ class MyEnum < Opus::Enum
   D = new
 end
 
-sig {params(x: T.any(MyEnum::A, String)).void}
+sig {params(x: T.any(MyEnum::A, MyEnum::B)).void}
 def takes_a_or_b(x); end
 
 sig {params(x: MyEnum::C).void}
@@ -28,12 +28,10 @@ def takes_c(x); end
 sig {params(x: MyEnum).void}
 def some_common_cases(x)
   case x
-  when MyEnum::A
+  when MyEnum::A, MyEnum::B
     takes_a_or_b(x)
-  when MyEnum::B
   when MyEnum::C
     takes_c(x)
-  when MyEnum::D
   else
     T.absurd(x) # error: Control flow could reach `T.absurd` because the type `MyEnum::D` wasn't handled
   end
