66bab22ad5829f8c4998a418c5c2e686bae03516
Jake Zimmerman <zimmerman.jake@gmail.com>
2019-08-02 15:21:34 -0700

    Test limitations

---
+++ test/testdata/dsl/opus_enum_alias.rb
@@ -0,0 +1,31 @@
+# typed: true
+# disable-fast-path: true
+extend T::Sig
+
+module Opus
+  class Enum
+    def initialize(x = nil)
+    end
+  end
+end
+
+class MyEnum < Opus::Enum
+  X = new
+end
+
+# We probably never want to allow this
+AliasX = MyEnum::X
+
+# We want to allow this eventually, but Sorbet isn't architected correctly for
+# us to do it.
+#
+# Type alias parsing happens in ResolveConstantsWalk, which is before
+# ResolveSignaturesWalk populates the resultType of static fields declared with
+# T.let.
+TypeAliasX = T.type_alias(MyEnum::X)
+
+sig {params(x: AliasX).void}
+def with_class_alias(x); end
+
+sig {params(x: TypeAliasX).void}
+def with_type_alias(x); end
