cf8deb01684619b8fac62f8a80a1744c9397a34d
Ariel Davis <ariel.z.davis@icloud.com>
2019-08-02 11:56:17 -0700

    Start document symbol fuzzer (#1435)
    * Get empty fuzz_doc_symbols to compile
    
    * Clean up a script
    
    * Use cc_test
    
    * Make a MockFileSystem
    
    * Add some loggers
    
    * Make a global state
    
    * Make opts
    
    * Add a kvstore
    
    * Use createInitialGlobalState
    
    * Put the fs in the opts
    
    * Move to a fn
    
    * Rename
    
    * Use mkOpts
    
    * Pass in kvstore
    
    * Use commonGs
    
    * Add static
    
    * Run index, resolve, typecheck
    
    * Ask a question
    
    * Add ws, swap
    
    * Try to make a lspWrapper
    
    * Rm some
    
    * Add more questions
    
    * Add some const
    
    * Move up
    
    * Call initializeLSP
    
    * Rm unused?
    
    * Send msgs for {did open, get symbols}
    
    * Let fuzz take an argument
    
    * Rm -x
    
    * Swap, say more
    
    * Rm a comment
    
    * Use lspDocumentSymbolEnabled
    
    * Mark an arg as optional
    
    * Use auto
    
    * Modify rawInputDirNames
    
    * Use ignoreFlushes
    
    * Use fileUri
    
    * Use PWD
    
    * Use ""
    
    * Format
    
    * Add to compdb
    
    * Use lspWrapper.enableAllExperimentalFeatures
    
    * Add another TODO
    
    * Swap, explain
    
    * Say a bit less
    
    * Swap
    
    * Use mkLSPWrapper
    
    * Use a static lspWrapper
    
    * Move initializeLSP into mkLSPWrapper
    
    I'm a bit unsure as to whether it's safe to have a static variable that
    we then mutate.
    
    * Use noStdlib and less payload
    
    * Put initializeLSP back into LLVMFuzzerTestOneInput
    
    * Rm const
    
    * Be quiet
    
    * Shorten a comment
    
    * Add back a comment
    
    * Rm TODO
    
    * Rm some include
    
    * Rm a pipeline dep
    
    * Avoid a var
    
    * Rm ws

---
+++ test/fuzz/fuzz_doc_symbols.cc
@@ -0,0 +1,63 @@
+#include "main/lsp/wrapper.h"
+#include "payload/payload.h"
+#include "spdlog/sinks/stdout_sinks.h"
+#include "test/helpers/MockFileSystem.h"
+#include "test/helpers/lsp.h"
+#include "test/helpers/position_assertions.h"
+#include <cstddef>
+#include <cstdint>
+#include <memory>
+
+const auto console = spdlog::stdout_logger_mt("console");
+const auto typeErrors = spdlog::stdout_logger_mt("typeErrors");
+const auto rootPath = "/tmp";
+const auto rootUri = fmt::format("file://{}", rootPath);
+
+sorbet::realmain::options::Options mkOpts() {
+    sorbet::realmain::options::Options opts;
+    opts.fs = std::make_shared<sorbet::test::MockFileSystem>(rootPath);
+    opts.noStdlib = true;
+    opts.lspDocumentSymbolEnabled = true;
+    opts.rawInputDirNames.emplace_back(rootPath);
+    return opts;
+}
+
+std::unique_ptr<sorbet::core::GlobalState> mkGlobalState(const sorbet::realmain::options::Options &opts,
+                                                         std::unique_ptr<sorbet::KeyValueStore> &kvstore) {
+    auto gs = std::make_unique<sorbet::core::GlobalState>(
+        (std::make_shared<sorbet::core::ErrorQueue>(*typeErrors, *console)));
+    sorbet::payload::createInitialGlobalState(gs, opts, kvstore);
+    gs->errorQueue->ignoreFlushes = true;
+    return gs;
+}
+
+sorbet::realmain::lsp::LSPWrapper mkLSPWrapper() {
+    std::unique_ptr<sorbet::KeyValueStore> kvstore;
+    auto opts = mkOpts();
+    static const auto commonGs = mkGlobalState(opts, kvstore);
+    // TODO how to use opts and avoid another mkOpts()?
+    auto lspWrapper = sorbet::realmain::lsp::LSPWrapper(commonGs->deepCopy(true), mkOpts(), console, false);
+    lspWrapper.enableAllExperimentalFeatures();
+    return lspWrapper;
+}
+
+extern "C" int LLVMFuzzerInitialize(const int *argc, const char ***argv) {
+    return 0;
+}
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, const std::size_t size) {
+    auto lspWrapper = mkLSPWrapper();
+    int nextId = 0;
+    sorbet::test::initializeLSP(rootPath, rootUri, lspWrapper, nextId);
+    std::string contents((const char *)data, size);
+    auto fileUri = sorbet::test::filePathToUri(rootUri, "file.rb");
+    lspWrapper.getLSPResponsesFor(sorbet::test::LSPMessage(std::make_unique<sorbet::test::NotificationMessage>(
+        "2.0", sorbet::test::LSPMethod::TextDocumentDidOpen,
+        std::make_unique<sorbet::test::DidOpenTextDocumentParams>(
+            std::make_unique<sorbet::test::TextDocumentItem>(fileUri, "ruby", 1, contents)))));
+    lspWrapper.getLSPResponsesFor(sorbet::test::LSPMessage(std::make_unique<sorbet::test::RequestMessage>(
+        "2.0", nextId++, sorbet::test::LSPMethod::TextDocumentDocumentSymbol,
+        std::make_unique<sorbet::test::DocumentSymbolParams>(
+            std::make_unique<sorbet::test::TextDocumentIdentifier>(fileUri)))));
+    return 0;
+}
--- tools/scripts/fuzz.sh
+++ tools/scripts/fuzz.sh
@@ -1,21 +1,38 @@
 #!/bin/bash
 
-set -exuo pipefail
+set -euo pipefail
 
-# build the actual fuzz target
-bazel build //test/fuzz:fuzz_dash_e --config=fuzz -c opt
-PATH=$PATH:$(pwd)/bazel-sorbet/external/llvm_toolchain/bin/
-export PATH
-export ASAN_OPTIONS=dedup_token_length=10 # use top 10 frames to tell different errors appart
+what=""
+if [ "$#" -eq 0 ]; then
+  what="fuzz_dash_e"
+elif [ "$#" -eq 1 ]; then
+  what="$1"
+else
+  echo "usage: $0 [<fuzz_target>]"
+  exit 1
+fi
 
+echo "building $what"
+bazel build "//test/fuzz:$what" --config=fuzz -c opt
 
-# setup corpus
+# we want the bazel build command to run before this check so that bazel can download itself.
+export PATH="$PATH:$PWD/bazel-sorbet/external/llvm_toolchain/bin"
+if ! command -v llvm-symbolizer >/dev/null; then
+  echo "fatal: command not found: llvm-symbolizer"
+  exit 1
+fi
+
+echo "setting up files"
 mkdir -p fuzz_corpus
-find ./test/testdata/ -iname "*.rb"|grep -v disable|xargs -n 1 -I % cp % fuzz_corpus
-
+find test/testdata -iname "*.rb" | grep -v disable | xargs -n 1 -I % cp % fuzz_corpus
 mkdir -p fuzz_crashers/original
 
-command -v llvm-symbolizer >/dev/null 2>&1 || { echo 'will need llvm-symbolizer' ; exit 1; }
-
-# fuzz
-nice ./bazel-bin/test/fuzz/fuzz_dash_e -use_value_profile=1 -only_ascii=1 -dict=test/fuzz/ruby.dict -artifact_prefix=fuzz_crashers/original/ fuzz_corpus/ --stress-incremental-resolver
+echo "running"
+# use top 10 frames to tell different errors apart
+export ASAN_OPTIONS="dedup_token_length=10"
+nice "./bazel-bin/test/fuzz/$what" \
+  -use_value_profile=1 \
+  -only_ascii=1 \
+  -dict=test/fuzz/ruby.dict \
+  -artifact_prefix=fuzz_crashers/original/ \
+  fuzz_corpus
