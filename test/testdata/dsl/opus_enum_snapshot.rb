9cdb463144bc8ef1372e7bc0de0bd1ecfefaf6d4
Jake Zimmerman <zimmerman.jake@gmail.com>
2019-08-02 13:17:16 -0700

    Add a snapshot test

---
+++ test/testdata/dsl/opus_enum_snapshot.rb
@@ -0,0 +1,18 @@
+# typed: true
+module Opus
+  class Enum
+    def initialize(x = nil)
+    end
+  end
+end
+
+class MyEnum < Opus::Enum
+  X = new
+  Y = new('y')
+  Z = T.let(new, self)
+end
+
+class NotAnEnum
+  X = new
+  Y = T.let(new, self)
+end
