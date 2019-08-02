dd99a5d06371ebd597589a723a698707ef40a502
Jake Zimmerman <zimmerman.jake@gmail.com>
2019-08-02 16:19:14 -0700

    whoops

--- test/testdata/dsl/opus_enum_alias.rb
+++ test/testdata/dsl/opus_enum_alias.rb
@@ -22,9 +22,9 @@ AliasX = MyEnum::X
 # Type alias parsing happens in ResolveConstantsWalk, which is before
 # ResolveSignaturesWalk populates the resultType of static fields declared with
 # T.let.
-TypeAliasX = T.type_alias(MyEnum::X)
+TypeAliasX = T.type_alias(MyEnum::X) # error: Constant `MyEnum::X` is not a class or type alias
 
-sig {params(x: AliasX).void}
+sig {params(x: AliasX).void} # error: Constant `AliasX` is not a class or type alias
 def with_class_alias(x); end
 
 sig {params(x: TypeAliasX).void}
