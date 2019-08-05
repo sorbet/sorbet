32bc35b5974c4fe12f4324606266eb04acf856e7
Jake Zimmerman <zimmerman.jake@gmail.com>
2019-08-05 10:51:05 -0700

    Exhaustiveness checks for Opus::Enum (#1480)
    * Allow Opus::Enum in type position
    
    * DSL pass to allow exhaustiveness with Opus::Enum
    
    This PR allows for:
    
    - exhaustiveness over the cases within an Opus::Enum
    - Opus::Enum values to be used in type positions
    
    * Recording expected changes to existing test
    
    * Add a test
    
    * Add tests of the type syntax bits
    
    * Fix some bugs
    
    * pr feedback
    
    * Add a snapshot test
    
    * Fix the test
    
    * Add requested comments
    
    * Remove useless extend T::Helpers
    
    * update exp files
    
    * Simplify this condition
    
    * Test limitations
    
    * whoops
    
    * Repeat Elem in child class
    
    * Add test to make sure we handle conditions similar to pay-server

--- core/SymbolRef.h
+++ core/SymbolRef.h
@@ -388,6 +388,10 @@ public:
         return SymbolRef(nullptr, 78);
     }
 
+    static SymbolRef OpusEnum() {
+        return SymbolRef(nullptr, 79);
+    }
+
     static constexpr int MAX_PROC_ARITY = 10;
     static SymbolRef Proc0() {
         return SymbolRef(nullptr, MAX_SYNTHETIC_SYMBOLS - MAX_PROC_ARITY * 2 - 2);
