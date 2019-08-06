e9b32b989bdddafedda5872fcabb2a7b33a46127
Ariel Davis <ariel.z.davis@icloud.com>
2019-08-05 18:14:36 -0700

    Explain

--- test/testdata/lsp/struct_fuzz.rb
+++ test/testdata/lsp/struct_fuzz.rb
@@ -5,3 +5,5 @@
 # ^^^^^^^^^^^^^^^^ error: Method `type_member` does not exist on `T.class_of(B)`
 # ^^^^^^^^^^^^^^^^ error: Method `sig` does not exist on `T.class_of(T::Sig::WithoutRuntime)`
 # ^^^^^^^^^^^^^^^^ error: Method `params` does not exist on `T.class_of(B)`
+# these errors aren't actually what we're checking for, we just want to make sure sorbet doesn't crash on this input
+# when no-stdlib is true.
