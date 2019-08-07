de85c6db4dcbbe60e614b71b1de6a52c40bee10e
Vignesh Shankar <vigneshshankar@stripe.com>
2019-08-07 16:26:22 +0800

    Remove redundant line.size()

--- main/lsp/requests/completion.cc
+++ main/lsp/requests/completion.cc
@@ -169,7 +169,7 @@ optional<string> findDocumentation(string_view sourceCode, int beginIndex) {
             // #abc -> "abc"
             int skip_after_hash = absl::StartsWith(line, "# ") ? 2 : 1;
 
-            documentation_lines.push_back(line.substr(line.find('#') + skip_after_hash, line.size()));
+            documentation_lines.push_back(line.substr(line.find('#') + skip_after_hash));
         }
 
         // No other cases applied to this line, so stop looking.
