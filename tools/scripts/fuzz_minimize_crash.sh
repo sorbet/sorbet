8f213751fa7c116e234d1d849449b9cc92d9695a
Ariel Davis <ariel.z.davis@icloud.com>
2019-08-08 13:14:17 -0700

    Cd up front

--- tools/scripts/fuzz_minimize_crash.sh
+++ tools/scripts/fuzz_minimize_crash.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 set -exuo pipefail
+cd "$(dirname "$0")"
+cd "../.."
+# we're now at the root of the repo.
 
 if (( $# != 1 )); then
     echo "Illegal number of parameters. Need a single file to minimize"
@@ -8,18 +11,16 @@ fi
 
 FUZZ_ARG="--stress-incremental-resolver" # to run incremental resolver
 
-dir="$( dirname "${BASH_SOURCE[0]}" )"
-
-mkdir -p "${dir}/../../fuzz_crashers/fixed/min/" "${dir}/../../fuzz_crashers/fixed/original/"
+mkdir -p "fuzz_crashers/fixed/min/" "fuzz_crashers/fixed/original/"
 
 file_arg="$(basename "$1")"
 crash_full_path="$(realpath "$1")"
-output_file="${dir}/../../fuzz_crashers/min/${file_arg}"
+output_file="fuzz_crashers/min/${file_arg}"
 if [ -f "${output_file}.done" ]; then
     echo "already minimized"
-    if "${dir}/../../bazel-bin/test/fuzz/fuzz_dash_e" "${output_file}.done"; then
+    if "bazel-bin/test/fuzz/fuzz_dash_e" "${output_file}.done"; then
       echo "already fixed"
-      mv "${output_file}.done" "${dir}/../../fuzz_crashers/fixed/min/${file_arg}"
+      mv "${output_file}.done" "fuzz_crashers/fixed/min/${file_arg}"
     fi
     exit 0
 fi
@@ -29,9 +30,9 @@ if [ -f "$output_file" ]; then
     cp "$output_file" "$crash_full_path"
 fi
 
-if "${dir}/../../bazel-bin/test/fuzz/fuzz_dash_e" "${crash_full_path}" ${FUZZ_ARG}; then
+if "bazel-bin/test/fuzz/fuzz_dash_e" "${crash_full_path}" ${FUZZ_ARG}; then
   echo "already fixed"
-  mv "${crash_full_path}" "${dir}/../../fuzz_crashers/fixed/original/${file_arg}"
+  mv "${crash_full_path}" "fuzz_crashers/fixed/original/${file_arg}"
   exit 0
 fi
 
@@ -52,14 +53,13 @@ handler_term()
 trap handler_int SIGINT
 trap handler_term SIGTERM
 
-mkdir -p "${dir}/../../fuzz_crashers/min/"
+mkdir -p "fuzz_crashers/min/"
 PATH=$PATH:$(pwd)/bazel-sorbet/external/llvm_toolchain/bin/
 export PATH
 
 command -v llvm-symbolizer >/dev/null 2>&1 || { echo 'will need llvm-symbolizer' ; exit 1; }
 
 (
-    cd "$dir"/../..
     ASAN_OPTIONS=dedup_token_length=10 ./bazel-bin/test/fuzz/fuzz_dash_e -use_value_profile=1 -dict=test/fuzz/ruby.dict -minimize_crash=1 "$crash_full_path" -exact_artifact_path=fuzz_crashers/min/"$file_arg" ${FUZZ_ARG}
 ) & # start a subshell that we'll monitor
 
