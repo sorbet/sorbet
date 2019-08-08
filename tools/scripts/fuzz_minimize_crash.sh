0ad4dc21146c99ed2630a07dfed23692207f1c84
Ariel Davis <ariel.z.davis@icloud.com>
2019-08-08 13:17:35 -0700

    Outdent

--- tools/scripts/fuzz_minimize_crash.sh
+++ tools/scripts/fuzz_minimize_crash.sh
@@ -23,17 +23,17 @@ file_arg="$(basename "$crasher")"
 crash_full_path="$(realpath "$crasher")"
 output_file="fuzz_crashers/min/$file_arg"
 if [ -f "$output_file.done" ]; then
-    echo "already minimized"
-    if "bazel-bin/test/fuzz/fuzz_dash_e" "$output_file.done"; then
-      echo "already fixed"
-      mv "$output_file.done" "fuzz_crashers/fixed/min/$file_arg"
-    fi
-    exit 0
+  echo "already minimized"
+  if "bazel-bin/test/fuzz/fuzz_dash_e" "$output_file.done"; then
+    echo "already fixed"
+    mv "$output_file.done" "fuzz_crashers/fixed/min/$file_arg"
+  fi
+  exit 0
 fi
 if [ -f "$output_file" ]; then
-    echo "Reusing previous minimized state"
-    crash_full_path=$(mktemp)
-    cp "$output_file" "$crash_full_path"
+  echo "Reusing previous minimized state"
+  crash_full_path=$(mktemp)
+  cp "$output_file" "$crash_full_path"
 fi
 
 if "bazel-bin/test/fuzz/fuzz_dash_e" "${crash_full_path}" ${FUZZ_ARG}; then
@@ -46,14 +46,14 @@ interrupted=
 
 handler_int()
 {
-    kill -INT "$child" 2>/dev/null
-    interrupted=1
+  kill -INT "$child" 2>/dev/null
+  interrupted=1
 }
 
 handler_term()
 {
-    kill -TERM "$child" 2>/dev/null
-    interrupted=1
+  kill -TERM "$child" 2>/dev/null
+  interrupted=1
 }
 
 trap handler_int SIGINT
@@ -66,7 +66,7 @@ export PATH
 command -v llvm-symbolizer >/dev/null 2>&1 || { echo 'will need llvm-symbolizer' ; exit 1; }
 
 (
-    ASAN_OPTIONS=dedup_token_length=10 ./bazel-bin/test/fuzz/fuzz_dash_e -use_value_profile=1 -dict=test/fuzz/ruby.dict -minimize_crash=1 "$crash_full_path" -exact_artifact_path=fuzz_crashers/min/"$file_arg" ${FUZZ_ARG}
+  ASAN_OPTIONS=dedup_token_length=10 ./bazel-bin/test/fuzz/fuzz_dash_e -use_value_profile=1 -dict=test/fuzz/ruby.dict -minimize_crash=1 "$crash_full_path" -exact_artifact_path=fuzz_crashers/min/"$file_arg" ${FUZZ_ARG}
 ) & # start a subshell that we'll monitor
 
 child=$!
