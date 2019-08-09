6aaa2a4acf88b043a90053205a03b89c1edf07f6
Ariel Davis <ariel.z.davis@icloud.com>
2019-08-08 17:23:31 -0700

    Give num responses

--- test/fuzz/fuzz_doc_symbols.cc
+++ test/fuzz/fuzz_doc_symbols.cc
@@ -54,9 +54,11 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, const std::size_t siz
     auto lspWrapper = mkLSPWrapper(contents);
     int nextId = 0;
     sorbet::test::initializeLSP(rootPath, rootUri, lspWrapper, nextId);
-    lspWrapper.getLSPResponsesFor(sorbet::test::LSPMessage(std::make_unique<sorbet::test::RequestMessage>(
-        "2.0", nextId++, sorbet::test::LSPMethod::TextDocumentDocumentSymbol,
-        std::make_unique<sorbet::test::DocumentSymbolParams>(
-            std::make_unique<sorbet::test::TextDocumentIdentifier>(fileUri)))));
+    const auto responses =
+        lspWrapper.getLSPResponsesFor(sorbet::test::LSPMessage(std::make_unique<sorbet::test::RequestMessage>(
+            "2.0", nextId++, sorbet::test::LSPMethod::TextDocumentDocumentSymbol,
+            std::make_unique<sorbet::test::DocumentSymbolParams>(
+                std::make_unique<sorbet::test::TextDocumentIdentifier>(fileUri)))));
+    sorbet::fatalLogger->info("num responses: {}", responses.size());
     return 0;
 }
