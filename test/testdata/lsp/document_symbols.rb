923b565f6f046e7607291218ee39e6ce615d5e91
Ariel Davis <ariel.z.davis@icloud.com>
2019-08-13 13:14:14 -0700

    Update a test

--- test/testdata/lsp/document_symbols.rb
+++ test/testdata/lsp/document_symbols.rb
@@ -1,12 +1,42 @@
 # typed: true
 
 class A
-  def foo
-    1 + 3
+  extend T::Sig
+
+  sig {void}
+  def initialize
+    @ivar = T.let(0, Integer)
   end
 
+  sig {returns(Integer)}
+  def next
+    ret = @ivar
+    @ivar += 1
+    ret
+  end
+
+  # no sig
   def self.bar
-    1 + 2
+    puts "hey there"
+  end
+end
+
+module M
+  extend T::Sig
+
+	# should not appear, since no let
+	@@nope = "doesn't work"
+	# should appear, since yes let
+  @@greeting = T.let("hello", String)
+
+  sig {params(x: String).returns(String)}
+  def hi(x)
+    "#{@@greeting}, #{x}"
+  end
+
+  sig(:final) {params(x: String).returns(String)}
+  def bye(x)
+    "goodbye, #{x}"
   end
 end
 
