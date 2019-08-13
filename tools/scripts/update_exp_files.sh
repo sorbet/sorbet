01828124b2d69d0e545c85371cb1cceae8c9a75a
Ariel Davis <ariel.z.davis@icloud.com>
2019-08-13 13:24:37 -0700

    Outdent

--- tools/scripts/update_exp_files.sh
+++ tools/scripts/update_exp_files.sh
@@ -33,38 +33,38 @@ bazel build \
 
 # shellcheck disable=SC2207
 rb_src=(
-    $(find test/testdata -name '*.rb' | sort)
+  $(find test/testdata -name '*.rb' | sort)
 )
 
 basename=
 srcs=()
 
 for this_src in "${rb_src[@]}" DUMMY; do
-    this_base=${this_src%__*}
-    if [ "$this_base" = "$basename" ]; then
-        srcs=("${srcs[@]}" "$this_src")
-        continue
-    fi
+  this_base=${this_src%__*}
+  if [ "$this_base" = "$basename" ]; then
+    srcs=("${srcs[@]}" "$this_src")
+    continue
+  fi
 
-    if [ "$basename" ]; then
-        for pass in "${passes[@]}" ; do
-            candidate="$basename.$pass.exp"
-            args=()
-            if [ "$pass" = "autogen" ]; then
-                args=("--stop-after=namer --skip-dsl-passes")
-            fi
-            if [ -e "$candidate" ]; then
-                if [ "$pass" = "document-symbols" ]; then
-                    echo bazel-bin/test/print_document_symbols "${srcs[@]}" \> "$candidate" 2\>/dev/null >> "$COMMAND_FILE"
-                else
-                    echo bazel-bin/main/sorbet --silence-dev-message  --suppress-non-critical --censor-for-snapshot-tests --print "$pass" --max-threads 0 "${args[@]}" "${srcs[@]}" \> "$candidate" 2\>/dev/null >> "$COMMAND_FILE"
-                fi
-            fi
-        done
-    fi
+  if [ "$basename" ]; then
+    for pass in "${passes[@]}" ; do
+      candidate="$basename.$pass.exp"
+      args=()
+      if [ "$pass" = "autogen" ]; then
+        args=("--stop-after=namer --skip-dsl-passes")
+      fi
+      if [ -e "$candidate" ]; then
+        if [ "$pass" = "document-symbols" ]; then
+          echo bazel-bin/test/print_document_symbols "${srcs[@]}" \> "$candidate" 2\>/dev/null >> "$COMMAND_FILE"
+        else
+          echo bazel-bin/main/sorbet --silence-dev-message  --suppress-non-critical --censor-for-snapshot-tests --print "$pass" --max-threads 0 "${args[@]}" "${srcs[@]}" \> "$candidate" 2\>/dev/null >> "$COMMAND_FILE"
+        fi
+      fi
+    done
+  fi
 
-    basename="$this_base"
-    srcs=("$this_src")
+  basename="$this_base"
+  srcs=("$this_src")
 done
 
 parallel --joblog - < "$COMMAND_FILE"
