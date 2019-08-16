811d6dc0c4229cb3f2041bc96a21a276b477d4c9
Getty Ritter <gdritter@stripe.com>
2019-08-16 14:49:33 -0700

    Temporary hack to get it working---should figure out why suggestions do not persist

--- test/testdata/lsp/fast_path/string_literal_change.rb
+++ test/testdata/lsp/fast_path/string_literal_change.rb
@@ -1,3 +1,6 @@
 # typed: strict
-A = ["bar"] # error: Constants must have type annotations with `T.let` when specifying `# typed: strict`
+# disable-fast-path: true
+
+A = ["bar"]
+  # ^^^^^^^ error: Constants must have type annotations with `T.let` when specifying `# typed: strict`
   # ^^^^^^^ error: Suggested type for constant without type annotation: `T::Array[String]`
