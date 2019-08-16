4afcc6e3a3c7434b57c410a86a5c1fff2317bf2d
Getty Ritter <gdritter@stripe.com>
2019-08-16 15:50:28 -0700

    Update tests now that we accurately repeat suggestions

--- test/testdata/lsp/fast_path/string_literal_change.rb
+++ test/testdata/lsp/fast_path/string_literal_change.rb
@@ -1,6 +1,4 @@
 # typed: strict
-# disable-fast-path: true
-
 A = ["bar"]
   # ^^^^^^^ error: Constants must have type annotations with `T.let` when specifying `# typed: strict`
   # ^^^^^^^ error: Suggested type for constant without type annotation: `T::Array[String]`
