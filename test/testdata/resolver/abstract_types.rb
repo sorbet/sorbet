4a90d9f73676ad7f0c8eb04c747452399c6a68e2
Getty Ritter <gdritter@stripe.com>
2019-08-27 11:53:32 -0700

    Add abstract and overridable tests for type compatibility

--- test/testdata/resolver/abstract_types.rb
+++ test/testdata/resolver/abstract_types.rb
@@ -41,7 +41,7 @@ module NonMatchingTests
   end
 end
 
-module VarianceTests
+module OverrideVarianceTests
   # C <: B <: A
   class A; end
   class B < A; end
@@ -52,8 +52,8 @@ module VarianceTests
     extend T::Helpers
     abstract!
 
-    sig { abstract.params(arg: B).returns(B) }
-    def bar(arg); end
+    sig { overridable.params(arg: B).returns(B) }
+    def bar(arg); B.new; end
   end
 
   class GoodOverride < Abstract
@@ -71,7 +71,7 @@ module VarianceTests
   # but an implementation cannot reasonably accept a subtype
   class ArgNarrowing < Abstract
     sig { override.params(arg: C).returns(B) }
-    def bar(arg); B.new; end # error: Parameter `arg` of type `VarianceTests::C` not compatible with type of abstract method `VarianceTests::Abstract#bar`
+    def bar(arg); B.new; end # error: Parameter `arg` of type `OverrideVarianceTests::C` not compatible with type of overridable method `OverrideVarianceTests::Abstract#bar`
   end
 
   # methods are covariant in their return types, so an implementation
@@ -84,7 +84,71 @@ module VarianceTests
   # but an implementation cannot reasonably return a supertype
   class RetWidening < Abstract
     sig { override.params(arg: B).returns(A) }
-    def bar(arg); A.new; end # error: Return type `VarianceTests::A` does not match return type of abstract method `VarianceTests::Abstract#bar`
+    def bar(arg); A.new; end # error: Return type `OverrideVarianceTests::A` does not match return type of overridable method `OverrideVarianceTests::Abstract#bar`
+  end
+
+  # if we have an invalid type but allow_incompatible, then this should work
+  class RetWideningallowed < Abstract
+    sig { override(allow_incompatible: true).params(arg: B).returns(A) }
+    def bar(arg); A.new; end
+  end
+
+end
+
+module AbstractVarianceTests
+  # C <: B <: A
+  class A; end
+  class B < A; end
+  class C < B; end
+
+  module IFace
+    extend T::Sig
+    extend T::Helpers
+    interface!
+
+    sig { abstract.params(arg: B).returns(B) }
+    def bar(arg); end
+  end
+
+  class GoodOverride
+    include IFace
+    extend T::Sig
+    sig { implementation.params(arg: B).returns(B) }
+    def bar(arg); B.new; end
+  end
+
+  # methods are contravariant in their arguments, so an implementation
+  # could reasonably accept a supertype of the declared interface type
+  class ArgWidening
+    include IFace
+    extend T::Sig
+    sig { implementation.params(arg: A).returns(B) }
+    def bar(arg); B.new; end
+  end
+
+  # but an implementation cannot reasonably accept a subtype
+  class ArgNarrowing
+    include IFace
+    extend T::Sig
+    sig { implementation.params(arg: C).returns(B) }
+    def bar(arg); B.new; end # error: Parameter `arg` of type `AbstractVarianceTests::C` not compatible with type of abstract method `AbstractVarianceTests::IFace#bar`
+  end
+
+  # methods are covariant in their return types, so an implementation
+  # could reasonably return a subtype of the declared interface type
+  class RetNarrowing
+    include IFace
+    extend T::Sig
+    sig { implementation.params(arg: B).returns(C) }
+    def bar(arg); C.new; end
+  end
+
+  # but an implementation cannot reasonably return a supertype
+  class RetWidening
+    include IFace
+    extend T::Sig
+    sig { implementation.params(arg: B).returns(A) }
+    def bar(arg); A.new; end # error: Return type `AbstractVarianceTests::A` does not match return type of abstract method `AbstractVarianceTests::IFace#bar`
   end
 
 end
