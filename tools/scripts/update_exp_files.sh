7a175e99955eba7a9af34561d9ca55c0fbef348f
Jake Zimmerman <zimmerman.jake@gmail.com>
2019-09-09 11:27:11 -0700

    Add tools/scripts/update_testdata_exp.sh
    I got tired of waiting for ruby_2_4_3 to build when all I care about is
    seeing whether the test/testdata exp files change.

--- tools/scripts/update_exp_files.sh
+++ tools/scripts/update_exp_files.sh
@@ -6,90 +6,14 @@ cd "$(dirname "$0")"
 cd ../..
 # we are now at the repo root.
 
-if ! command -v parallel &> /dev/null; then
-  echo "This script requires GNU parallel to be installed"
-  exit 1
-fi
-
-COMMAND_FILE="$(mktemp)"
-trap 'rm -f "$COMMAND_FILE"' EXIT
-
-passes=(
-  parse-tree
-  parse-tree-json
-  ast
-  ast-raw
-  dsl-tree
-  dsl-tree-raw
-  symbol-table
-  symbol-table-raw
-  name-tree
-  name-tree-raw
-  resolve-tree
-  resolve-tree-raw
-  cfg
-  cfg-raw
-  cfg-json
-  flattened-tree
-  flattened-tree-raw
-  autogen
-  document-symbols
-)
-
 bazel build \
   //gems/sorbet/test/snapshot:update \
-  //test/cli:update //test/lsp:update //main:sorbet \
-  //test:print_document_symbols -c opt "$@"
+  //test/cli:update //test/lsp:update \
+  -c opt "$@"
+
+tools/scripts/update_testdata_exp.sh
 
 # shellcheck disable=SC2207
-rb_src=(
-  $(find test/testdata -name '*.rb*' | sort)
-)
-
-basename=
-srcs=()
-
-for this_src in "${rb_src[@]}" DUMMY; do
-  this_base="${this_src%__*}"
-  if [ "$this_base" = "$basename" ]; then
-    srcs=("${srcs[@]}" "$this_src")
-    continue
-  fi
-
-  if [ -n "$basename" ]; then
-    for pass in "${passes[@]}"; do
-      candidate="$basename.$pass.exp"
-      args=()
-      if [ "$pass" = "autogen" ]; then
-        args=("--stop-after=namer --skip-dsl-passes")
-      fi
-      if ! [ -e "$candidate" ]; then
-        continue
-      fi
-      if [ "$pass" = "document-symbols" ]; then
-        echo bazel-bin/test/print_document_symbols \
-          "${srcs[@]}" \
-          \> "$candidate" \
-          2\>/dev/null \
-          >>"$COMMAND_FILE"
-      else
-        echo bazel-bin/main/sorbet \
-          --silence-dev-message --suppress-non-critical --censor-for-snapshot-tests \
-          --print "$pass" --max-threads 0 \
-          "${args[@]}" "${srcs[@]}" \
-          \> "$candidate" \
-          2\>/dev/null \
-          >>"$COMMAND_FILE"
-      fi
-    done
-  fi
-
-  basename="$this_base"
-  srcs=("$this_src")
-done
-
-parallel --joblog - < "$COMMAND_FILE"
-
 bazel test \
   //gems/sorbet/test/snapshot:update \
   //test/cli:update //test/lsp:update -c opt "$@"
