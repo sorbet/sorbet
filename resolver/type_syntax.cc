168dc516e93aa57dfe7a0693cd6391c575e6d125
Getty Ritter <gdritter@stripe.com>
2019-09-12 10:10:17 -0700

    Allow implementation to stand in for override first

--- resolver/type_syntax.cc
+++ resolver/type_syntax.cc
@@ -280,9 +280,7 @@ ParsedSig TypeSyntax::parseSig(core::MutableContext ctx, ast::Send *sigSend, con
                     break;
                 }
                 case core::Names::implementation()._id:
-                    if (auto e = ctx.state.beginError(send->loc, core::errors::Resolver::ImplementationDeprecated)) {
-                        e.setHeader("Use of `{}` is deprecated; please use `{}` instead", "implementation", "override");
-                    }
+                    sig.seen.overridable = true;
                     break;
                 case core::Names::overridable()._id:
                     sig.seen.overridable = true;
