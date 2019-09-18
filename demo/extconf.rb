b4b961ac28e37aa4f1e3dde828b0657e8db3213e
Dmitry Petrashko <dmitry@stripe.com>
2019-09-18 15:54:35 -0700

    Better setup - no longer uses mutable makefile

--- demo/extconf.rb
+++ demo/extconf.rb
@@ -1,3 +1,23 @@
 require 'mkmf'
+
+RbConfig::MAKEFILE_CONFIG['cleanobjs'] = '*.ll' # nuke ll files in clean
+
+
+# force usage of clang
+ENV['CC'] ||= find_executable('clang')
+ENV['CXX'] ||= find_executable('clang++')
+RbConfig::MAKEFILE_CONFIG["CC"] = ENV["CC"] if ENV["CC"]
+RbConfig::MAKEFILE_CONFIG["CXX"] = ENV["CXX"] if ENV["CXX"]
+
 create_header
 create_makefile 'foobar'
+
+File.open('Makefile', 'at') do |mk|
+    mk.print <<EOF
+
+### CUSTOM MODIFICATIONS
+%.ll : %.c
+	$(ECHO) building LLVM bitcode for $(<) into $@
+	$(Q) $(CC) -S -emit-llvm $(INCFLAGS) $(CPPFLAGS) $(CFLAGS) $(COUTFLAG)$@ $(CSRCFLAG)$<
+EOF
+end
