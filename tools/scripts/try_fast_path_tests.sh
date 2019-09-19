0cdbedceb77ca2bc2426917e9030a5003ddabea9
Getty Ritter <gdritter@stripe.com>
2019-09-18 17:44:53 -0700

    Address bash style lints

--- tools/scripts/try_fast_path_tests.sh
+++ tools/scripts/try_fast_path_tests.sh
@@ -5,16 +5,16 @@ set -e
 cd "$(dirname "$0")"
 cd ../..
 
-for FILE in $(grep -l -r test -e '# disable-fast-path: true'); do
+grep -l -r test -e '# disable-fast-path: true' | while read -r FILE; do
     if [[ "$FILE" == *.rb && ! "$FILE" =~ .*__[0-9].rb ]]; then
         BASENAME=${FILE%.rb}
         TESTNAME="//test:test_LSPTests/${BASENAME#test/}"
-        sed -i .backup -e '/^# disable-fast-path: true$/d' $FILE
-        if bazel test $TESTNAME; then
+        sed -i .backup -e '/^# disable-fast-path: true$/d' "$FILE"
+        if bazel test "$TESTNAME"; then
             echo "$TESTNAME succeeds now; leaving as-is"
-            rm $FILE.backup
+            rm "$FILE.backup"
         else
-            mv $FILE.backup $FILE
+            mv "$FILE.backup" "$FILE"
         fi
     fi
 done
