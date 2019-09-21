77369b9cfc98f53ce38973df353dc14e5312591c
Trevor Elliott <awesomelyawesome@gmail.com>
2019-09-20 20:54:48 -0700

    Add a comment on `unwrapType`

--- core/types/calls.cc
+++ core/types/calls.cc
@@ -304,6 +304,22 @@ SymbolRef guessOverload(Context ctx, SymbolRef inClass, SymbolRef primary,
     return fallback;
 } // namespace sorbet::core
 
+/**
+ * unwrapType is used to take an expression that's parsed at the value-level,
+ * and turn it into a type. For example, consider the following two expressions:
+ *
+ * > Integer.sqrt 10
+ * > T::Array[Integer].new
+ *
+ * In both lines, `Integer` is initially resolved as the singleton class of
+ * `Integer`. This is because it's not immediately clear if we want to refer
+ * to the type `Integer` or if we want the singleton class of Integer for
+ * calling singleton methods. In the first line this was the correct choice, as
+ * we're just invoking the singleton method `sqrt`. In the second case we need
+ * to fix up the `Integer` sub-expression, and turn it back into the type of
+ * integer values. This is what `unwrapType` does, it turns the value-level
+ * expression back into a type-level one.
+ */
 TypePtr unwrapType(Context ctx, Loc loc, const TypePtr &tp) {
     if (auto *metaType = cast_type<MetaType>(tp.get())) {
         return metaType->wrapped;
