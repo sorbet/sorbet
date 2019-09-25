ee1ab368f03a18b65723438cb205059a567f0943
Trevor Elliott <awesomelyawesome@gmail.com>
2019-09-25 16:29:35 -0700

    Add a test to show the runtime limitations with AttachedClass

--- gems/sorbet-runtime/test/types/attached_class.rb
+++ gems/sorbet-runtime/test/types/attached_class.rb
@@ -19,5 +19,19 @@ module Opus::Types::Test
       assert_equal(Base.make.class, Base)
       assert_equal(A.make.class, A)
     end
+
+    it 'does not throw when the returned value is bad' do
+
+      class Base
+        extend T::Sig
+
+        sig {returns(AttachedClass)}
+        def self.make
+          10
+        end
+      end
+
+      assert_equal(Base.make.class, Integer)
+    end
   end
 end
