8f9346792d7423e0753f5c11d5ab63507459b037
Trevor Elliott <awesomelyawesome@gmail.com>
2019-09-25 16:45:05 -0700

    Comment the definition of AttachedClass in the runtime

--- gems/sorbet-runtime/lib/types/types/attached_class.rb
+++ gems/sorbet-runtime/lib/types/types/attached_class.rb
@@ -32,4 +32,7 @@ module T::Types
   end
 end
 
+# Type-checking the runtime complains that AttachedClass is being re-defined at
+# a different type without it, as it's expected to be a type member with an
+# upper bound of <root>.
 AttachedClass = T.unsafe(T::Types::AttachedClassType.new)
