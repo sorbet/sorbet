1dae68b7a6dafd59677ec2b77668abc23c323dd5
Jake Zimmerman <zimmerman.jake@gmail.com>
2019-09-25 16:50:50 -0700

    Censor loc information in errors.rb
    I made a change to kernel.rbi and this test failed. That shouldn't
    happen.

--- test/cli/errors/errors.sh
+++ test/cli/errors/errors.sh
@@ -4,5 +4,10 @@ main/sorbet --silence-dev-message test/cli/errors/errors.rb 2>&1
 
 CACHE=$(mktemp -d)
 
-main/sorbet --silence-dev-message --cache-dir="$CACHE" test/cli/errors/errors.rb 2>&1
-main/sorbet --silence-dev-message --cache-dir="$CACHE" test/cli/errors/errors.rb 2>&1
+censor_payload_locs() {
+  sed -e 's/https:\/\/github.com\/sorbet\/sorbet.*: /...: /' | \
+    sed -e 's/^\( *\)[0-9]* |/\1.. |/'
+}
+
+main/sorbet --silence-dev-message --cache-dir="$CACHE" test/cli/errors/errors.rb 2>&1 | censor_payload_locs
+main/sorbet --silence-dev-message --cache-dir="$CACHE" test/cli/errors/errors.rb 2>&1 | censor_payload_locs
