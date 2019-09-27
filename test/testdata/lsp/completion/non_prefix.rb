65a031e13f9706ca7eef8136fd7586bfbb47559c
Jake Zimmerman <zimmerman.jake@gmail.com>
2019-09-26 18:38:21 -0700

    Add failing test

---
+++ test/testdata/lsp/completion/non_prefix.rb
@@ -0,0 +1,8 @@
+# typed: true
+extend T::Sig
+
+sig {params(x: T::Array[Integer]).void}
+def foo(x)
+  x.me # error: does not exist
+  #   ^ completion: member?, method, methods, __method__, define_singleton_method, ...
+end
