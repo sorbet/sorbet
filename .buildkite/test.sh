1dac0b7327b2cdecb97dc36e591a46c84fa4d251
Paul Tarjan <pt@stripe.com>
2019-09-27 15:01:07 -0700

    add some buildkite files

---
+++ .buildkite/test.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+set -euo pipefail
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     platform="linux";;
+    Darwin*)    platform="mac";;
+    *)          exit 1
+esac
+
+export JOB_NAME=test
+source .buildkite/tools/setup-bazel.sh
+
+err=0
+
+./bazel test //... --test_summary=terse || err=$?
+
+echo "--- uploading test results"
+
+rm -rf _tmp_
+mkdir -p _tmp_/log/junit/
+
+# TODO: does this query omit the snapshot tests?
+./bazel query 'tests(//...) except attr("tags", "manual", //...)' | while read -r line; do
+    path="${line/://}"
+    path="${path#//}"
+    cp "bazel-testlogs/$path/test.xml" _tmp_/log/junit/"${path//\//_}-${BUILDKITE_JOB_ID}.xml"
+done
+
+annotation_dir="$(mktemp -d "junit-annotate-plugin-annotation-tmp.XXXXXXXXXX")"
+annotation_path="${annotation_dir}/annotation.md"
+
+function cleanup {
+  rm -rf "${annotation_dir}"
+}
+
+trap cleanup EXIT
+
+
+.buildkite/tools/annotate.rb _tmp_/log/junit > "$annotation_path"
+
+cat "$annotation_path"
+
+if grep -q "<details>" "$annotation_path"; then
+  echo "--- :buildkite: Creating annotation"
+  # shellcheck disable=SC2002
+  cat "$annotation_path" | buildkite-agent annotate --context junit-${platform} --style error --append
+fi
+
+if [ "$err" -ne 0 ]; then
+    exit "$err"
+fi
