174c6e871fabc8388d0fdefc536d7bef586282f1
Getty Ritter <gdritter@stripe.com>
2019-09-27 14:37:39 -0700

    Only skip serialization of repeated static fields

--- gems/sorbet/lib/hidden-definition-finder.rb
+++ gems/sorbet/lib/hidden-definition-finder.rb
@@ -191,8 +191,8 @@ class Sorbet::Private::HiddenMethodFinder
     ret = []
 
     rbi.each do |rbi_entry|
-      # skip synthetic constants
-      next if rbi_entry["name"]["kind"] == "UNIQUE"
+      # skip duplicated constant fields
+      next if rbi_entry["name"]["kind"] == "UNIQUE" and rbi_entry["kind"] == "STATIC_FIELD"
 
       source_entry = source_by_name[rbi_entry["name"]["name"]]
 
