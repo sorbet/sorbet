4d1f8cdeb024842c3506b4a0f1f28d5654504faf
John Vilk <jvilk@stripe.com>
2019-09-26 18:41:35 -0700

    Make cancelable slow path part of enableAllExperimentalFeatures.

--- main/lsp/wrapper.cc
+++ main/lsp/wrapper.cc
@@ -108,6 +108,7 @@ void LSPWrapper::enableAllExperimentalFeatures() {
     enableExperimentalFeature(LSPExperimentalFeature::DocumentSymbol);
     enableExperimentalFeature(LSPExperimentalFeature::SignatureHelp);
     enableExperimentalFeature(LSPExperimentalFeature::QuickFix);
+    enableExperimentalFeature(LSPExperimentalFeature::CancelableSlowPath);
 }
 
 void LSPWrapper::enableExperimentalFeature(LSPExperimentalFeature feature) {
@@ -126,6 +127,9 @@ void LSPWrapper::enableExperimentalFeature(LSPExperimentalFeature feature) {
             break;
         case LSPExperimentalFeature::SignatureHelp:
             opts.lspSignatureHelpEnabled = true;
+            break;
+        case LSPExperimentalFeature::CancelableSlowPath:
+            opts.lspCancelableSlowPathEnabled = true;
             break;
     }
 }
--- main/lsp/wrapper.h
+++ main/lsp/wrapper.h
@@ -45,6 +45,7 @@ public:
         DocumentSymbol = 6,
         SignatureHelp = 7,
         QuickFix = 8,
+        CancelableSlowPath = 9,
     };
 
     // N.B.: Sorbet assumes we 'own' this object; keep it alive to avoid memory errors.
