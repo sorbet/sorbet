8cfd5b8ac4377d48486f9f8fa5ad07de63123dd2
Paul Tarjan <pt@stripe.com>
2019-10-01 14:02:21 -0700

    call some LLVM stuff

--- llvm/linker/linker.cc
+++ llvm/linker/linker.cc
@@ -14,6 +14,12 @@ void init() {
 }
 
 void run(std::shared_ptr<spdlog::logger> logger) {
+    std::vector<::llvm::Type *> NoArgs(0, ::llvm::Type::getVoidTy(TheContext));
+    auto FT = ::llvm::FunctionType::get(::llvm::Type::getVoidTy(TheContext), NoArgs, false);
+    auto TheFunction = ::llvm::Function::Create(FT, ::llvm::Function::ExternalLinkage, "main", TheModule);
+    auto BB = ::llvm::BasicBlock::Create(TheContext, "entry", TheFunction);
+    Builder.SetInsertPoint(BB);
+    Builder.CreateRet(::llvm::ConstantInt::getTrue(TheContext));
     TheModule.print(::llvm::errs(), nullptr);
 }
 
--- test/testdata/llvm/hello.rb
+++ test/testdata/llvm/hello.rb
@@ -1,6 +1,11 @@
 puts(<<EOS)
 ; ModuleID = 'sorbet'
 source_filename = "sorbet"
+
+define void @main() {
+entry:
+  ret i1 true
+}
 EOS
 
 # puts("hello world")
