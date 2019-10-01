94a423c5c9bb81bca1abf7ea04d93fbc144bd3e8
John Vilk <jvilk@stripe.com>
2019-10-01 13:27:28 -0700

    Add requested comment.

--- main/lsp/LSPPreprocessor.cc
+++ main/lsp/LSPPreprocessor.cc
@@ -100,6 +100,7 @@ void LSPPreprocessor::mergeFileChanges(absl::Mutex &mtx, QueueState &state) {
                 if (msg->isNotification() && msg->method() == LSPMethod::SorbetWorkspaceEdit) {
                     Timer timeit(logger, "tryCancelSlowPath");
                     auto &params = get<unique_ptr<SorbetWorkspaceEditParams>>(msg->asNotification().params);
+                    // Note: This line calls indexer to re-create the index trees, so it's somewhat expensive.
                     auto combinedUpdates = ttgs.getCombinedUpdates(committed + 1, params->updates.versionEnd);
                     if (combinedUpdates.canTakeFastPath && gs.tryCancelSlowPath(params->updates.versionEnd)) {
                         logger->debug("[Preprocessor] Canceling typechecking, as edits {} thru {} can take fast path.",
