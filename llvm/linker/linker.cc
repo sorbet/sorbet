f5f7743c2d864573bc6293108caabaf7025bf9b7
Paul Tarjan <pt@stripe.com>
2019-10-01 17:18:51 -0700

    format

--- llvm/linker/linker.cc
+++ llvm/linker/linker.cc
@@ -4,7 +4,6 @@
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/LegacyPassManager.h"
 #include "llvm/IR/Module.h"
-#include "llvm/payload/payload.h"
 #include "llvm/Support/FileSystem.h"
 #include "llvm/Support/Host.h"
 #include "llvm/Support/TargetRegistry.h"
@@ -12,6 +11,7 @@
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Target/TargetMachine.h"
 #include "llvm/Target/TargetOptions.h"
+#include "llvm/payload/payload.h"
 
 using namespace std;
 namespace sorbet::llvm::linker {
