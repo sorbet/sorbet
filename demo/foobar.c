24157427b6c0a7aac680cc789d94f3d3a028af44
Dmitry Petrashko <dmitry@stripe.com>
2019-10-03 16:43:15 -0700

    Demo calling convention

--- demo/foobar.c
+++ demo/foobar.c
@@ -5,6 +5,11 @@ VALUE rb_return_nil() {
   return sorbet_rubyNil();
 }
 
+/* calling converntion that we'll use */
+VALUE my_method(int argc, VALUE* argv, VALUE self){
+  return self;
+}
+
 void Init_foobar() {
   VALUE mod = rb_define_module("DemoModule");
   rb_define_singleton_method(mod, "return_nil", rb_return_nil, 0);
