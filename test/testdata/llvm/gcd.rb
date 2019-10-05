4e6165901c63b4f809737783d33fc163c925d228
Dmitry Petrashko <dmitry@stripe.com>
2019-10-05 00:43:18 -0700

    Fix test

--- test/testdata/llvm/gcd.rb
+++ test/testdata/llvm/gcd.rb
@@ -1,9 +1,7 @@
 #typed: true
 module GCD
- extend T::Helpers
 
- sig { params(a: Integer, b:Integer).returns(Integer) }
- def self.gcd_typed(a, b)
+ def self.gcd(a, b)
    while (a != b)
      if a > b
        a -= b
@@ -13,15 +11,5 @@ module GCD
    end
    a
    end
- 
- def self.gcd_untyped(a, b)
-   while (a != b)
-     if a > b
-       a -= b
-     else
-       b -= a
-     end
-   end
-   a
  end
-end
+
