e455fca916270aa0e42172fb2e2443caa3f812b4
Trevor Elliott <awesomelyawesome@gmail.com>
2019-10-07 15:08:15 -0700

    Add tests for the new resolver behavior

---
+++ test/testdata/resolver/type_member_cycle.rb
@@ -0,0 +1,24 @@
+# typed: true
+
+class A
+  extend T::Sig
+  extend T::Generic
+
+  X = type_member(fixed: B)
+# ^^^^^^^^^^^^^^^^^^^^^^^^^ error: Type member `A::X` is involved in a cycle
+end
+
+class B
+  extend T::Sig
+  extend T::Generic
+
+  X = type_member(fixed: A)
+# ^^^^^^^^^^^^^^^^^^^^^^^^^ error: Type member `B::X` is involved in a cycle
+
+  sig {returns(X)}
+  def test
+    10
+  end
+end
+
+T.reveal_type(B.new.test) # error: Revealed type: `T.untyped`
---
+++ test/testdata/resolver/type_member_out_of_order.rb
@@ -0,0 +1,23 @@
+# typed: true
+
+Alias1 = T.type_alias(A)
+Alias2 = T.type_alias(B)
+
+class A
+  extend T::Generic
+  X = type_member(fixed: B)
+end
+
+class B
+  extend T::Sig
+  extend T::Generic
+
+  X = type_member(fixed: Integer)
+
+  sig {returns(X)}
+  def test
+    10
+  end
+end
+
+T.reveal_type(B.new.test) # error: Revealed type: `Integer`
