24cdfc85c082d6a806db3b3e148f1bda12818f37
Paul Tarjan <pt@stripe.com>
2019-10-08 10:02:24 -0700

    download it everytime I guess

--- .buildkite/linters.sh
+++ .buildkite/linters.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 export JOB_NAME=linters
 source .buildkite/tools/setup-bazel.sh
 
-go get github.com/buildkite/terminal-to-html/cmd/terminal-to-html
+wget -O - https://github.com/buildkite/terminal-to-html/releases/download/v3.2.0/terminal-to-html-3.2.0-linux-amd64.gz | gunzip -c > ./terminal-to-html
 
 set -x
 err=0
@@ -12,7 +12,7 @@ globalErr=0
 
 ./tools/scripts/format_build_files.sh -t &> buildifier || err=$?
 if [ "$err" -ne 0 ]; then
-    (echo '<pre class="term"><code>'; cat buildifier; echo '</code></pre>') > buildifier_wrapped
+    (echo '<pre class="term"><code>'; ./terminal-to-html < buildifier; echo '</code></pre>') > buildifier_wrapped
     buildkite-agent annotate --context tools/scripts/format_build_files.sh --style error --append < buildifier_wrapped
     globalErr=$err
 fi
@@ -20,7 +20,7 @@ fi
 err=0
 ./tools/scripts/format_cxx.sh -t &> format_cxx || err=$?
 if [ "$err" -ne 0 ]; then
-    (echo '<pre class="term"><code>'; cat format_cxx; echo '</code></pre>') > format_cxx_wrapped
+    (echo '<pre class="term"><code>'; ./terminal-to-html < format_cxx; echo '</code></pre>') > format_cxx_wrapped
     buildkite-agent annotate --context tools/scripts/format_cxx.sh --style error --append < format_cxx_wrapped
     globalErr=$err
 fi
@@ -28,7 +28,7 @@ fi
 err=0
 ./tools/scripts/build_compilation_db.sh &> compdb || err=$?
 if [ "$err" -ne 0 ]; then
-    (echo '<pre class="term"><code>'; cat compdb; echo '</code></pre>') > compdb_wrapped
+    (echo '<pre class="term"><code>'; ./terminal-to-html < compdb; echo '</code></pre>') > compdb_wrapped
     buildkite-agent annotate --context tools/scripts/build_compilation_db.sh --style error --append < compdb_wrapped
     globalErr=$err
 fi
@@ -36,7 +36,7 @@ fi
 err=0
 ./tools/scripts/generate_compdb_targets.sh -t &> compdb-targets || err=$?
 if [ "$err" -ne 0 ]; then
-    (echo '<pre class="term"><code>'; cat compdb-targets; echo '</code></pre>') > compdb-targets_wrapped
+    (echo '<pre class="term"><code>'; ./terminal-to-html < compdb-targets; echo '</code></pre>') > compdb-targets_wrapped
     buildkite-agent annotate --context tools/scripts/generate_compdb_targets.sh --style error --append < compdb-targets_wrapped
     globalErr=$err
 fi
@@ -44,7 +44,7 @@ fi
 err=0
 ./tools/scripts/check_using_namespace_std.sh &> std_check || err=$?
 if [ "$err" -ne 0 ]; then
-    (echo '<pre class="term"><code>'; cat std_check; echo '</code></pre>') > std_check_wrapped
+    (echo '<pre class="term"><code>'; ./terminal-to-html < std_check; echo '</code></pre>') > std_check_wrapped
     buildkite-agent annotate --context tools/scripts/check_using_namespace_std.sh --style error --append < std_check_wrapped
     globalErr=$err
 fi
@@ -52,7 +52,7 @@ fi
 err=0
 ./tools/scripts/lint_sh.sh -t &> lint_sh || err=$?
 if [ "$err" -ne 0 ]; then
-    (echo '<pre class="term"><code>'; cat lint_sh; echo '</code></pre>') > lint_sh_wrapped
+    (echo '<pre class="term"><code>'; ./terminal-to-html < lint_sh; echo '</code></pre>') > lint_sh_wrapped
     buildkite-agent annotate --context tools/scripts/lint_sh.sh --style error --append < lint_sh_wrapped
     globalErr=$err
 fi
