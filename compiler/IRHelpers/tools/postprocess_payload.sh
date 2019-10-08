ce6f363774eee3bb6fac8b11123ccb14decb8c0b
Dmitry Petrashko <dmitry@stripe.com>
2019-10-08 02:38:42 -0700

    Fix shellcheck warnings in new file

--- compiler/IRHelpers/tools/postprocess_payload.sh
+++ compiler/IRHelpers/tools/postprocess_payload.sh
@@ -2,8 +2,8 @@
 set -euo pipefail
 
 payload="$1"
-sed -i '.bak' 's/define double @sorbet_/define internal double @sorbet_/g' "$1"
-sed -i '.bak' 's/define i64 @sorbet_/define internal i64 @sorbet_/g' "$1"
-sed -i '.bak' 's/define i8\* @sorbet_/define internal i8\* @sorbet_/g' "$1"
-sed -i '.bak' 's/define void @sorbet_/define internal void @sorbet_/g' "$1"
-sed -i '.bak' 's/define zeroext i1 @sorbet_/define internal zeroext i1 @sorbet_/g' "$1"
+sed -i '.bak' 's/define double @sorbet_/define internal double @sorbet_/g' "$payload"
+sed -i '.bak' 's/define i64 @sorbet_/define internal i64 @sorbet_/g' "$payload"
+sed -i '.bak' 's/define i8\* @sorbet_/define internal i8\* @sorbet_/g' "$payload"
+sed -i '.bak' 's/define void @sorbet_/define internal void @sorbet_/g' "$payload"
+sed -i '.bak' 's/define zeroext i1 @sorbet_/define internal zeroext i1 @sorbet_/g' "$payload"
