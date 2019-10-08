dc71ffaf6d3bbcb2d07b72b6411a7d6566dc8f2b
Paul Tarjan <pt@stripe.com>
2019-10-08 09:12:49 -0700

    hard code triple

--- compiler/ObjectFileEmitter/ObjectFileEmitter.cc
+++ compiler/ObjectFileEmitter/ObjectFileEmitter.cc
@@ -34,7 +34,11 @@ void outputLLVM(llvm::legacy::PassManager &pm, string_view dir, string_view file
 
 void outputObjectFile(llvm::legacy::PassManager &pm, string_view dir, string_view fileNameWithoutExtension,
                       unique_ptr<llvm::Module> module) {
-    auto targetTriple = llvm::sys::getDefaultTargetTriple();
+    // We encode this value in our `.exp` files right now so we have to hard
+    // code it to something that doesn't chance accross sytems.
+    // TODO stop putting it in our .exp files and then unhardcode this
+    auto targetTriple = "x86_64-apple-darwin18.2.0";
+    // auto targetTriple = llvm::sys::getDefaultTargetTriple();
     module->setTargetTriple(targetTriple);
 
     std::string error;
