618d0307b754e7676cc44d75976473f8944fb0af
Paul Tarjan <pt@stripe.com>
2019-10-08 09:53:42 -0700

    wrap everything in pre to help formatting

--- .buildkite/linters.sh
+++ .buildkite/linters.sh
@@ -10,42 +10,48 @@ globalErr=0
 
 ./tools/scripts/format_build_files.sh -t &> buildifier || err=$?
 if [ "$err" -ne 0 ]; then
-    buildkite-agent annotate --context tools/scripts/format_build_files.sh --style error --append < buildifier
+    (echo '<pre class="term"><code>'; cat buildifier; echo '</code></pre>') > buildifier_wrapped
+    buildkite-agent annotate --context tools/scripts/format_build_files.sh --style error --append < buildifier_wrapped
     globalErr=$err
 fi
 
 err=0
 ./tools/scripts/format_cxx.sh -t &> format_cxx || err=$?
 if [ "$err" -ne 0 ]; then
-    buildkite-agent annotate --context tools/scripts/format_cxx.sh --style error --append < format_cxx
+    (echo '<pre class="term"><code>'; cat format_cxx; echo '</code></pre>') > format_cxx_wrapped
+    buildkite-agent annotate --context tools/scripts/format_cxx.sh --style error --append < format_cxx_wrapped
     globalErr=$err
 fi
 
 err=0
 ./tools/scripts/build_compilation_db.sh &> compdb || err=$?
 if [ "$err" -ne 0 ]; then
-    buildkite-agent annotate --context tools/scripts/build_compilation_db.sh --style error --append < compdb
+    (echo '<pre class="term"><code>'; cat compdb; echo '</code></pre>') > compdb_wrapped
+    buildkite-agent annotate --context tools/scripts/build_compilation_db.sh --style error --append < compdb_wrapped
     globalErr=$err
 fi
 
 err=0
 ./tools/scripts/generate_compdb_targets.sh -t &> compdb-targets || err=$?
 if [ "$err" -ne 0 ]; then
-    buildkite-agent annotate --context tools/scripts/generate_compdb_targets.sh --style error --append < compdb-targets
+    (echo '<pre class="term"><code>'; cat compdb-targets; echo '</code></pre>') > compdb-targets_wrapped
+    buildkite-agent annotate --context tools/scripts/generate_compdb_targets.sh --style error --append < compdb-targets_wrapped
     globalErr=$err
 fi
 
 err=0
 ./tools/scripts/check_using_namespace_std.sh &> std_check || err=$?
 if [ "$err" -ne 0 ]; then
-    buildkite-agent annotate --context tools/scripts/check_using_namespace_std.sh --style error --append < std_check
+    (echo '<pre class="term"><code>'; cat std_check; echo '</code></pre>') > std_check_wrapped
+    buildkite-agent annotate --context tools/scripts/check_using_namespace_std.sh --style error --append < std_check_wrapped
     globalErr=$err
 fi
 
 err=0
 ./tools/scripts/lint_sh.sh -t &> lint_sh || err=$?
 if [ "$err" -ne 0 ]; then
-    buildkite-agent annotate --context tools/scripts/lint_sh.sh --style error --append < lint_sh
+    (echo '<pre class="term"><code>'; cat lint_sh; echo '</code></pre>') > lint_sh_wrapped
+    buildkite-agent annotate --context tools/scripts/lint_sh.sh --style error --append < lint_sh_wrapped
     globalErr=$err
 fi
 
