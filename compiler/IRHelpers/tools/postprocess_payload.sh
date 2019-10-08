eef0f568c862f4ca5ca29528a15f11dd55edb7ed
Dmitry Petrashko <dmitry@stripe.com>
2019-10-08 02:48:01 -0700

    remove more noise

--- compiler/IRHelpers/tools/postprocess_payload.sh
+++ compiler/IRHelpers/tools/postprocess_payload.sh
@@ -2,8 +2,17 @@
 set -euo pipefail
 
 payload="$1"
+
+# mark all our unternal functions as, well, internal :-)
 sed -i '.bak' 's/define double @sorbet_/define internal double @sorbet_/g' "$payload"
 sed -i '.bak' 's/define i64 @sorbet_/define internal i64 @sorbet_/g' "$payload"
 sed -i '.bak' 's/define i8\* @sorbet_/define internal i8\* @sorbet_/g' "$payload"
-sed -i '.bak' 's/define void @sorbet_/define internal void @sorbet_/g' "$payload"
 sed -i '.bak' 's/define zeroext i1 @sorbet_/define internal zeroext i1 @sorbet_/g' "$payload"
+sed -i '.bak' 's/define void @sorbet_/define internal void @sorbet_/g' "$payload"
+
+
+# remove noise
+
+sed -i '.bak' '/llvm\.module\.flags/d' "$payload"
+sed -i '.bak' '/llvm\.ident/d' "$payload"
+sed -i '.bak' '/^\^/d' "$payload"
