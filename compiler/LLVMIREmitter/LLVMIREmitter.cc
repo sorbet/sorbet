c90d6c09c883a9561e38b3369c09a548cfdccd7a
Paul Tarjan <pt@stripe.com>
2019-10-14 15:56:28 -0700

    prefix for collisions

--- compiler/LLVMIREmitter/LLVMIREmitter.cc
+++ compiler/LLVMIREmitter/LLVMIREmitter.cc
@@ -39,7 +39,7 @@ llvm::FunctionType *getRubyFunctionTypeForSymbol(llvm::LLVMContext &lctx, const 
 llvm::CallInst *resolveSymbol(const core::GlobalState &gs, core::SymbolRef sym, llvm::IRBuilder<> &builder,
                               llvm::Module *module) {
     auto str = sym.data(gs)->name.show(gs);
-    auto rawCString = builder.CreateGlobalStringPtr(str, str);
+    auto rawCString = builder.CreateGlobalStringPtr(str, "rubyID_" + str);
     auto id = builder.CreateCall(module->getFunction("sorbet_IDIntern"), {rawCString}, "rubyID");
     return builder.CreateCall(module->getFunction("sorbet_getConstant"),
                               {builder.CreateCall(module->getFunction("sorbet_rb_cObject")), id});
