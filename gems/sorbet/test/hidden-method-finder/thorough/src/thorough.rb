5bd4cd2dfabd6f6bac459baebcf0828126511ce4
Getty Ritter <gdritter@infinitenegativeutility.com>
2019-10-14 17:43:29 -0700

    Update thorough test with stubbed-out stdlib stuff

--- gems/sorbet/test/hidden-method-finder/thorough/src/thorough.rb
+++ gems/sorbet/test/hidden-method-finder/thorough/src/thorough.rb
@@ -10,3 +10,45 @@ end
 
 class C3; end
 C3.const_set('X', 5)
+
+
+# the following are included to avoid including these in
+# hidden-definitions, as they differ between OS X and Linux
+
+class BasicSocket < IO
+  def read_nonblock(*_); end
+end
+
+Errno::EAUTH = Errno::NOERROR
+Errno::EBADEXEC = Errno::NOERROR
+Errno::EBADARCH = Errno::NOERROR
+Errno::EBADMACHO = Errno::NOERROR
+Errno::EBADRPC = Errno::NOERROR
+Errno::EDEADLOCK = Errno::NOERROR
+Errno::EDEVERR = Errno::NOERROR
+Errno::EFTYPE = Errno::NOERROR
+Errno::ELAST = Errno::NOERROR
+Errno::ENEEDAUTH = Errno::NOERROR
+Errno::ENOATTR = Errno::NOERROR
+Errno::ENOPOLICY = Errno::NOERROR
+Errno::EPROCLIM = Errno::NOERROR
+Errno::EPROCUNAVAIL = Errno::NOERROR
+Errno::EPROGMISMATCH = Errno::NOERROR
+Errno::EPROGUNAVAIL = Errno::NOERROR
+Errno::EPWROFF = Errno::NOERROR
+Errno::EQFULL = Errno::NOERROR
+Errno::ERPCMISMATCH = Errno::NOERROR
+Errno::EPWROFF = Errno::NOERROR
+Errno::EPROGUNAVAIL = Errno::NOERROR
+Errno::EQFULL = Errno::ELAST
+Errno::ESHLIBVERS = Errno::NOERROR
+
+class IPSocket < BasicSocket
+  def self.getaddress_orig(*_); end
+end
+
+module Process
+  CLOCK_MONOTONIC_RAW_APPROX = ::T.let(nil, ::T.untyped)
+  CLOCK_UPTIME_RAW = ::T.let(nil, ::T.untyped)
+  CLOCK_UPTIME_RAW_APPROX = ::T.let(nil, ::T.untyped)
+end
