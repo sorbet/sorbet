ef14db4c7d1233384fa82132a24f68bb3842a3e0
Getty Ritter <gdritter@infinitenegativeutility.com>
2019-10-14 17:44:47 -0700

    Add bash script to update hidden methods exp tests

---
+++ gems/sorbet/test/hidden-method-finder/update_bash.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+cd "$(dirname "$0")"
+base_dir="$(pwd)"
+cd ../../../..
+# we are now at the repo root
+
+versions=("ruby_2_4_3" "ruby_2_6_3")
+
+if ! bazel test //gems/sorbet/test/hidden-method-finder; then
+    for test_dir in bazel-genfiles/gems/sorbet/test/hidden-method-finder/*; do
+        suite="$(basename "${test_dir}")"
+        for version in ${versions[@]}; do
+            updated="${test_dir}/actual_${version}.rbi"
+            previous="${base_dir}/${suite}/${version}_hidden.rbi.exp"
+            if ! diff "$updated" "$previous" >/dev/null ; then
+                echo "$suite with $version differs; updating"
+                cp "$updated" "$previous"
+            fi
+        done
+    done
+else
+    echo "Hidden methods tests are up-to-date"
+fi
