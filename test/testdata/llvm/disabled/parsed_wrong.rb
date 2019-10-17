69f2d426d79c55599d320f7a73746b72b9ce6ba4
Dmitry Petrashko <dmitry@stripe.com>
2019-10-16 19:28:47 -0700

    Add test that failes due to us misparsing it.
    Force it to run with bazel test //test:test_PosTests/testdata/llvm/disabled/parsed_wrong

---
+++ test/testdata/llvm/disabled/parsed_wrong.rb
@@ -0,0 +1,22 @@
+a = <<EOD
+part 1 of heredoc #{ "not a heredoc" + <<EOM }
+eom part
+EOM
+part 2 of heredoc
+EOD
+
+b = <<-EOD
+oweqijfoiwjefqwoefij
+EOD
+
+
+def foo
+  c = <<~EOD
+    oqweijfoqwiejf
+  EOD
+  puts c
+end
+
+puts a
+puts b
+foo
