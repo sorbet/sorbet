f11c90187d1419583719c6a45a13be580c03b670
Paul Tarjan <pt@stripe.com>
2019-10-18 12:53:40 -0700

    add test from discussion about root weirdness

---
+++ test/testdata/llvm/disabled/root_weirdness.rb
@@ -0,0 +1,37 @@
+p self
+
+def on_object
+  puts 'on_object'
+end
+
+def self.on_class_of_object
+  puts 'self.on_class_of_object'
+end
+
+class << self
+  def on_root_singleton
+    puts 'on_root_singleton'
+  end
+end
+
+on_object
+on_class_of_object
+on_root_singleton
+
+begin
+  1.on_object
+rescue => e
+  p e
+end
+
+begin
+  1.on_class_of_object
+rescue => e
+  p e
+end
+
+begin
+  1.on_root_singleton
+rescue => e
+  p e
+end
