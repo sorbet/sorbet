7b139aa2b3214041ee0fa9bc8e99b96ec3ad753e
Paul Tarjan <github@paulisageek.com>
2019-10-21 16:12:36 -0700

    stop copying in the default_ tree and instead move it

--- resolver/resolver.cc
+++ resolver/resolver.cc
@@ -1462,7 +1462,7 @@ private:
             if (auto *optArgExp = ast::cast_tree<ast::OptionalArg>(argExp.get())) {
                 // Using optArgExp's loc will make errors point to the arg list, even though the T.let is in the
                 // body.
-                auto let = make_unique<ast::Cast>(optArgExp->loc, argType, optArgExp->default_->deepCopy(),
+                auto let = make_unique<ast::Cast>(optArgExp->loc, argType, move(optArgExp->default_),
                                                   core::Names::let());
                 lets.emplace_back(std::move(let));
             }
