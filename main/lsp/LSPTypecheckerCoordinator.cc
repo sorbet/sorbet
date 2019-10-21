b6f0accaf9a040fd3cf740f5828625f35ed097ae
John Vilk <jvilk@stripe.com>
2019-10-21 16:07:02 +0100

    Report metrics gathered on coordinator thread.

--- main/lsp/LSPTypecheckerCoordinator.cc
+++ main/lsp/LSPTypecheckerCoordinator.cc
@@ -23,12 +23,23 @@ void LSPTypecheckerCoordinator::asyncRun(function<void(LSPTypechecker &)> &&lamb
 
 void LSPTypecheckerCoordinator::syncRun(function<void(LSPTypechecker &)> &&lambda) {
     absl::Notification notification;
+    CounterState typecheckerCounters;
+    // If typechecker is running on a dedicated thread, then we need to merge its metrics w/ coordinator thread's so we
+    // report them.
+    const bool isMultithreaded = this_thread::get_id() == typecheckerThreadId;
     // Note: Capturing notification by reference is safe here, we we wait for the notification to happen prior to
     // returning.
-    asyncRunInternal([&typechecker = this->typechecker, lambda, &notification]() -> void {
-        lambda(typechecker);
-        notification.Notify();
-    });
+    asyncRunInternal(
+        [&typechecker = this->typechecker, lambda, &notification, &typecheckerCounters, isMultithreaded]() -> void {
+            lambda(typechecker);
+            if (isMultithreaded) {
+                typecheckerCounters = getAndClearThreadCounters();
+            }
+            notification.Notify();
+        });
+    if (isMultithreaded) {
+        counterConsume(move(typecheckerCounters));
+    }
     notification.WaitForNotification();
 }
 
