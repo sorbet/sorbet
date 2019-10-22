043ae95f05257dd29e19480c69c8b8ed157082f0
Getty Ritter <gdritter@infinitenegativeutility.com>
2019-10-22 13:26:31 -0700

    Do not flatten classes, just methods

--- dsl/flatten.cc
+++ dsl/flatten.cc
@@ -1,7 +1,7 @@
+#include "dsl/flatten.h"
 #include "ast/Helpers.h"
 #include "ast/ast.h"
 #include "ast/treemap/treemap.h"
-#include "dsl/flatten.h"
 #include "common/concurrency/WorkerPool.h"
 #include "core/core.h"
 
@@ -19,35 +19,49 @@ public:
     }
     ~FlattenWalk() {
         ENFORCE(methodScopes.empty());
-        ENFORCE(classes.empty());
         ENFORCE(classStack.empty());
     }
 
     unique_ptr<ast::ClassDef> preTransformClassDef(core::Context ctx, unique_ptr<ast::ClassDef> classDef) {
         newMethodSet();
-        classStack.emplace_back(classes.size());
-        classes.emplace_back();
+        int size = classStack.size();
+        classStack.emplace_back(ClassScope{size, classDef->name.get()});
         return classDef;
     }
 
     unique_ptr<ast::MethodDef> preTransformMethodDef(core::Context ctx, unique_ptr<ast::MethodDef> methodDef) {
+        if (skipMethods.contains(methodDef.get())) {
+            return methodDef;
+        }
         auto &methods = curMethodSet();
         methods.stack.emplace_back(methods.methods.size());
         methods.methods.emplace_back();
         return methodDef;
     }
 
+    /// Returns `true` if the method is one of the modifier names in Ruby (e.g. 'private' or 'protected' or
+    /// similar). This does not need to know about `module_function` because we have already re-written it in a previous
+    /// DSL pass.
+    bool isMethodModifier(core::NameRef fun) {
+        return fun == core::Names::private_() || fun == core::Names::protected_() || fun == core::Names::public_() ||
+               fun == core::Names::privateClassMethod();
+    }
+
     unique_ptr<ast::Send> preTransformSend(core::Context ctx, unique_ptr<ast::Send> send) {
-        if (send->fun == core::Names::sig()) {
+        if (send->fun == core::Names::sig() || isMethodModifier(send->fun)) {
             auto &methods = curMethodSet();
             methods.stack.emplace_back(methods.methods.size());
             methods.methods.emplace_back();
+
+            if (isMethodModifier(send->fun) && send->args.size() >= 1) {
+                skipMethods.insert(send->args[0].get());
+            }
         }
         return send;
     }
 
     unique_ptr<ast::Expression> postTransformSend(core::Context ctx, unique_ptr<ast::Send> send) {
-        if (send->fun == core::Names::sig()) {
+        if (send->fun == core::Names::sig() || isMethodModifier(send->fun)) {
             auto &methods = curMethodSet();
             ENFORCE(!methods.stack.empty());
             ENFORCE(methods.methods.size() > methods.stack.back());
@@ -55,6 +69,7 @@ public:
 
             methods.methods[methods.stack.back()] = std::move(send);
             methods.stack.pop_back();
+
             return make_unique<ast::EmptyTree>();
         } else {
             return send;
@@ -63,16 +78,17 @@ public:
 
     unique_ptr<ast::Expression> postTransformClassDef(core::Context ctx, unique_ptr<ast::ClassDef> classDef) {
         ENFORCE(!classStack.empty());
-        ENFORCE(classes.size() > classStack.back());
-        ENFORCE(classes[classStack.back()] == nullptr);
-
         classDef->rhs = addMethods(ctx, std::move(classDef->rhs));
-        classes[classStack.back()] = std::move(classDef);
         classStack.pop_back();
-        return make_unique<ast::EmptyTree>();
+        return classDef;
     };
 
     unique_ptr<ast::Expression> postTransformMethodDef(core::Context ctx, unique_ptr<ast::MethodDef> methodDef) {
+        // if this method is contained in a send like `private` or `protected`, then we should not move it, because
+        // moving the send will do that for us
+        if (skipMethods.contains(methodDef.get())) {
+            return methodDef;
+        }
         auto &methods = curMethodSet();
         ENFORCE(!methods.stack.empty());
         ENFORCE(methods.methods.size() > methods.stack.back());
@@ -82,31 +98,6 @@ public:
         methods.stack.pop_back();
         return make_unique<ast::EmptyTree>();
     };
-
-    unique_ptr<ast::Expression> addClasses(core::Context ctx, unique_ptr<ast::Expression> tree) {
-        if (classes.empty()) {
-            ENFORCE(sortedClasses().empty());
-            return tree;
-        }
-        if (classes.size() == 1 && (ast::cast_tree<ast::EmptyTree>(tree.get()) != nullptr)) {
-            // It was only 1 class to begin with, put it back
-            return std::move(sortedClasses()[0]);
-        }
-
-        auto insSeq = ast::cast_tree<ast::InsSeq>(tree.get());
-        if (insSeq == nullptr) {
-            ast::InsSeq::STATS_store stats;
-            auto sorted = sortedClasses();
-            stats.insert(stats.begin(), make_move_iterator(sorted.begin()), make_move_iterator(sorted.end()));
-            return ast::MK::InsSeq(tree->loc, std::move(stats), std::move(tree));
-        }
-
-        for (auto &clas : sortedClasses()) {
-            ENFORCE(!!clas);
-            insSeq->stats.emplace_back(std::move(clas));
-        }
-        return tree;
-    }
 
     unique_ptr<ast::Expression> addMethods(core::Context ctx, unique_ptr<ast::Expression> tree) {
         auto &methods = curMethodSet().methods;
@@ -135,13 +126,6 @@ public:
     }
 
 private:
-    vector<unique_ptr<ast::ClassDef>> sortedClasses() {
-        ENFORCE(classStack.empty());
-        auto ret = std::move(classes);
-        classes.clear();
-        return ret;
-    }
-
     ast::ClassDef::RHS_store addMethods(core::Context ctx, ast::ClassDef::RHS_store rhs) {
         if (curMethodSet().methods.size() == 1 && rhs.size() == 1 &&
             (ast::cast_tree<ast::EmptyTree>(rhs[0].get()) != nullptr)) {
@@ -181,6 +165,12 @@ private:
         methodScopes.pop_back();
     }
 
+    struct ClassScope {
+        int index;
+        ast::Expression *name;
+        ClassScope(int index, ast::Expression *name) : index(index), name(name){};
+    };
+
     // We flatten nested classes and methods into a flat list. We want to sort
     // them by their starts, so that `class A; class B; end; end` --> `class A;
     // end; class B; end`.
@@ -193,14 +183,13 @@ private:
     // pop from in the `postTransform` hook.
 
     vector<Methods> methodScopes;
-    vector<unique_ptr<ast::ClassDef>> classes;
-    vector<int> classStack;
+    vector<ClassScope> classStack;
+    UnorderedSet<ast::Expression *> skipMethods;
 };
 
 unique_ptr<ast::Expression> Flatten::patchFile(core::Context ctx, unique_ptr<ast::Expression> tree) {
     FlattenWalk flatten;
     tree = ast::TreeMap::apply(ctx, flatten, std::move(tree));
-    tree = flatten.addClasses(ctx, std::move(tree));
     tree = flatten.addMethods(ctx, std::move(tree));
 
     return tree;
