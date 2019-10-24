9092181544705cd769a39ae986e0a28d23eaf330
Paul Tarjan <github@paulisageek.com>
2019-10-24 10:38:07 -0700

    "fix" test

--- test/testdata/lsp/struct_fuzz.rb
+++ test/testdata/lsp/struct_fuzz.rb
@@ -2,6 +2,7 @@
 # no-stdlib: true
   ::B=Struct.new:x
 # ^^^^^^^^^^^^^^^^ error: Method `unsafe` does not exist on `T.class_of(T)`
+# ^^^^^^^^^^^^^^^^ error: Method `unsafe` does not exist on `T.class_of(T)`
 # ^^^^^^^^^^^^^^^^ error: Method `type_member` does not exist on `T.class_of(B)`
 # ^^^^^^^^^^^^^^^^ error: Method `sig` does not exist on `T.class_of(T::Sig::WithoutRuntime)`
 # ^^^^^^^^^^^^^^^^ error: Method `params` does not exist on `T.class_of(B)`
