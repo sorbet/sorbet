99af71701032fb072ab1b6a20ddecf43de67eab6
Paul Tarjan <pt@stripe.com>
2019-10-29 10:42:28 -0700

    more complicated test

---
+++ test/testdata/llvm/default_complex.rb
@@ -0,0 +1,19 @@
+def wrong_order(first="1", second)
+  first + " " + second
+end
+
+def only_defaults(first="1")
+  first
+end
+
+def fall_through(first="1", second="2")
+  first + " " + second
+end
+
+puts wrong_order("2")
+puts wrong_order("2", "3")
+
+puts only_defaults
+puts only_defaults("2")
+
+puts fall_through
