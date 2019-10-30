f7e2f280c4c282343ed3b2c0e8613f639bc97c6b
Dmitry Petrashko <dmitry@stripe.com>
2019-10-29 17:52:34 -0700

    Test

---
+++ test/testdata/llvm/classfields.rb
@@ -0,0 +1,26 @@
+# typed: true
+
+class B
+
+  def write(a)
+    @@f = a
+  end
+
+  # class << self
+  #   def ss
+  #     @@f
+  #   end
+  # end
+  def read
+    @@f
+  end
+  def self.read
+    @@f
+  end
+end
+
+# TODO: we'll need more tests when we implement subclasses and fix self in static scope
+B.new.write(1)
+
+puts B.read
+puts B.new.read
