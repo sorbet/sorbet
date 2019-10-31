a8ef19229bb70d32de5e51d3b8e458c7dc4ccbcb
Dmitry Petrashko <dmitry@stripe.com>
2019-10-30 18:37:08 -0700

    For some reason previous version didn't work

--- compiler/IRHelpers/IRHelpers.cc
+++ compiler/IRHelpers/IRHelpers.cc
@@ -51,8 +51,9 @@ llvm::FunctionType *CompilerState::getRubyFFIType() {
 }
 
 void CompilerState::setExpectedBool(llvm::IRBuilderBase &builder, llvm::Value *value, bool expected) {
-    builderCast(builder).CreateIntrinsic(llvm::Intrinsic::ID::expect, {llvm::Type::getInt1Ty(lctx)},
-                                         {value, builder.getInt1(expected)});
+    builderCast(builder).CreateCall(
+        llvm::Intrinsic::getDeclaration(module, llvm::Intrinsic::ID::expect, {llvm::Type::getInt1Ty(lctx)}),
+        {value, builder.getInt1(expected)});
 }
 
 void CompilerState::boxRawValue(llvm::IRBuilderBase &builder, llvm::AllocaInst *target, llvm::Value *rawData) {
