c7ebfb44a4f7aab41fc899c475adfe28ea518a65
Getty Ritter <gdritter@infinitenegativeutility.com>
2019-10-31 13:15:36 -0700

    Allow sigless methods

--- core/GlobalState.cc
+++ core/GlobalState.cc
@@ -1443,7 +1443,7 @@ bool GlobalState::shouldReportErrorOn(Loc loc, ErrorClass what) const {
             }
         } else if (level == StrictLevel::Stdlib) {
             level = StrictLevel::Strict;
-            if (what == errors::Resolver::OverloadNotAllowed || what == errors::Resolver::VariantTypeMemberInClass) {
+            if (what == errors::Resolver::OverloadNotAllowed || what == errors::Resolver::VariantTypeMemberInClass || what == errors::Infer::UntypedMethod) {
                 return false;
             }
         }
