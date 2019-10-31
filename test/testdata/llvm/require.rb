46b3659e41694dc84771db756ad49bae04b9e76d
Paul Tarjan <pt@stripe.com>
2019-10-31 15:28:18 -0700

    write test in __1 notation

--- test/testdata/llvm/require.rb
+++
@@ -1,7 +0,0 @@
-require 'tempfile'
-file = Tempfile.new(['foo', '.rb'])
-file.write("puts 'hello world'")
-file.close
-path = T.unsafe(file.path)
-puts File.read(path)
-require path
---
+++ test/testdata/llvm/require__1.rb
@@ -0,0 +1,1 @@
+require_relative './require__2'
