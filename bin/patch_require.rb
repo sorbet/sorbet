afc842ebfb537ace69fe677c86e568a66631f333
Paul Tarjan <pt@stripe.com>
2019-10-30 22:45:10 -0700

    refactor it to start patching require

--- bin/patch_require.rb
+++ bin/patch_require.rb
@@ -1,1 +1,10 @@
-1
+module Kernel
+  alias sorbet_old_require require
+  def require(name)
+    if File.exists?(name)
+      puts name
+      IO.popen([__dir__ + '/compile', name])
+    end
+    sorbet_old_require(name)
+  end
+end
