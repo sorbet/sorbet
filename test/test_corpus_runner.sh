0d07327cfa7a7b64fcb63f3a0475dbf2a6e2e6f6
Paul Tarjan <pt@stripe.com>
2019-11-01 09:22:07 -0700

    create stderr checking

--- test/test_corpus_runner.sh
+++ test/test_corpus_runner.sh
@@ -8,9 +8,10 @@ rbrunfile=$(mktemp)
 srbrunfile=$(mktemp)
 rbout=$(mktemp)
 srbout=$(mktemp)
+srberr=$(mktemp)
 
 cleanup() {
-    rm -r "$llvmir" "$rbrunfile" "$srbrunfile" "$rbout" "$srbout"
+    rm -r "$llvmir" "$rbrunfile" "$srbrunfile" "$rbout" "$srbout" "$srberr"
 }
 
 # trap cleanup EXIT
@@ -24,7 +25,14 @@ echo "Run Sorbet: run/ruby $rb"
 $ruby "$rbrunfile" 2>&1 | tee "$rbout"
 
 echo "Temp Dir: $llvmir"
-llvmir=$llvmir runfile=$srbrunfile run/ruby "$rb" | tee "$srbout"
+set +e
+llvmir=$llvmir runfile=$srbrunfile run/ruby "$rb" 2> "$srberr" | tee "$srbout"
+code=$?
+set -e
+
+if [ $code -ne 0 ]; then
+    diff "${rb%.rb}.stderr.exp" "$srberr"
+fi
 
 echo "Run LLDB: lldb bazel-bin/$ruby -- $srbrunfile"
 for i in "$llvmir"/*.llo; do
