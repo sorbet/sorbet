908eaaf51cb9e5034fab2e7d2b32a16647bc74be
Dmitry Petrashko <dmitry@stripe.com>
2019-11-05 14:42:27 -0800

    Extract this too

--- compiler/LLVMIREmitter/LLVMIREmitter.cc
+++ compiler/LLVMIREmitter/LLVMIREmitter.cc
@@ -19,59 +19,11 @@
 
 using namespace std;
 namespace sorbet::compiler {
-struct BasicBlockMap {
-    core::SymbolRef forMethod;
-    vector<llvm::BasicBlock *> functionInitializersByFunction;
-    vector<llvm::BasicBlock *> argumentSetupBlocksByFunction;
-    vector<llvm::BasicBlock *> userEntryBlockByFunction;
-    vector<llvm::BasicBlock *> llvmBlocksBySorbetBlocks;
-    vector<int> basicBlockJumpOverrides;
-    vector<llvm::AllocaInst *> sendArgArrayByBlock;
-    vector<llvm::Value *> escapedClosure;
-    UnorderedMap<core::LocalVariable, int> escapedVariableIndeces;
-    llvm::BasicBlock *sigVerificationBlock;
-    vector<shared_ptr<core::SendAndBlockLink>> blockLinks;
-    vector<vector<core::LocalVariable>> rubyBlockArgs;
-    UnorderedMap<core::LocalVariable, llvm::AllocaInst *> llvmVariables;
-};
 
 // https://docs.ruby-lang.org/en/2.6.0/extension_rdoc.html
 // and https://silverhammermba.github.io/emberb/c/ are your friends
 // use the `demo` module for experiments
 namespace {
-
-struct Alias {
-    enum class AliasKind { Constant, InstanceField, ClassField, GlobalField };
-    AliasKind kind;
-    core::SymbolRef constantSym;
-    core::NameRef instanceField;
-    core::NameRef classField;
-    core::SymbolRef globalField;
-    static Alias forConstant(core::SymbolRef sym) {
-        Alias ret;
-        ret.kind = AliasKind::Constant;
-        ret.constantSym = sym;
-        return ret;
-    }
-    static Alias forClassField(core::NameRef name) {
-        Alias ret;
-        ret.kind = AliasKind::ClassField;
-        ret.classField = name;
-        return ret;
-    }
-    static Alias forInstanceField(core::NameRef name) {
-        Alias ret;
-        ret.kind = AliasKind::InstanceField;
-        ret.instanceField = name;
-        return ret;
-    }
-    static Alias forGlobalField(core::SymbolRef sym) {
-        Alias ret;
-        ret.kind = AliasKind::GlobalField;
-        ret.globalField = sym;
-        return ret;
-    }
-};
 
 bool isStaticInit(CompilerState &cs, core::SymbolRef sym) {
     auto name = sym.data(cs)->name;
@@ -429,54 +381,6 @@ llvm::Function *getInitFunction(CompilerState &cs, std::string baseName,
     return getOrCreateFunction(cs, "Init_" + baseName, ft, linkageType);
 }
 
-// Create local allocas for local variables, initialize them all to `nil`.
-// load arguments, check their count
-// load self
-UnorderedMap<core::LocalVariable, llvm::AllocaInst *>
-setupLocalVariables(CompilerState &cs, cfg::CFG &cfg, vector<llvm::Function *> &rubyBlocks2Functions,
-                    const UnorderedMap<core::LocalVariable, optional<int>> &variablesPrivateToBlocks,
-                    const BasicBlockMap &blockMap, UnorderedMap<core::LocalVariable, Alias> &aliases) {
-    UnorderedMap<core::LocalVariable, llvm::AllocaInst *> llvmVariables;
-    llvm::IRBuilder<> builder(cs);
-    {
-        // nill out block local variables.
-        auto valueType = cs.getValueType();
-        vector<pair<core::LocalVariable, optional<int>>> variablesPrivateToBlocksSorted;
-
-        for (const auto &entry : variablesPrivateToBlocks) {
-            variablesPrivateToBlocksSorted.emplace_back(entry);
-        }
-        fast_sort(variablesPrivateToBlocksSorted,
-                  [](const auto &left, const auto &right) -> bool { return left.first < right.first; });
-        for (const auto &entry : variablesPrivateToBlocksSorted) {
-            auto var = entry.first;
-            if (entry.second == std::nullopt) {
-                continue;
-            }
-            auto svName = var._name.data(cs)->shortName(cs);
-            builder.SetInsertPoint(blockMap.functionInitializersByFunction[entry.second.value()]);
-            auto alloca = llvmVariables[var] =
-                builder.CreateAlloca(valueType, nullptr, llvm::StringRef(svName.data(), svName.length()));
-            auto nilValueRaw = cs.getRubyNilRaw(builder);
-            cs.boxRawValue(builder, alloca, nilValueRaw);
-        }
-    }
-
-    {
-        // nill out closure variables
-
-        builder.SetInsertPoint(blockMap.functionInitializersByFunction[0]);
-        auto escapedVariablesCount = blockMap.escapedVariableIndeces.size();
-        for (auto i = 0; i < escapedVariablesCount; i++) {
-            auto store =
-                builder.CreateCall(cs.module->getFunction("sorbet_getClosureElem"),
-                                   {blockMap.escapedClosure[0], llvm::ConstantInt::get(cs, llvm::APInt(32, i))});
-            builder.CreateStore(cs.getRubyNilRaw(builder), store);
-        }
-    }
-    return llvmVariables;
-}
-
 vector<core::ArgInfo::ArgFlags> getArgFlagsForBlockId(CompilerState &cs, int blockId, core::SymbolRef method,
                                                       const BasicBlockMap &blockMap) {
     if (blockId != 0) {
@@ -675,127 +579,6 @@ void setupArguments(CompilerState &cs, cfg::CFG &cfg, unique_ptr<ast::MethodDef>
             builder.CreateBr(blockMap.userEntryBlockByFunction[funcId]);
         }
     }
-}
-
-BasicBlockMap
-getSorbetBlocks2LLVMBlockMapping(CompilerState &cs, cfg::CFG &cfg, unique_ptr<ast::MethodDef> &md,
-                                 vector<llvm::Function *> rubyBlocks2Functions,
-                                 UnorderedMap<core::LocalVariable, int> &&escapedVariableIndices, int maxSendArgCount,
-                                 const UnorderedMap<core::LocalVariable, optional<int>> &variablesPrivateToBlocks,
-                                 UnorderedMap<core::LocalVariable, Alias> &aliases) {
-    vector<llvm::BasicBlock *> functionInitializersByFunction;
-    vector<llvm::BasicBlock *> argumentSetupBlocksByFunction;
-    vector<llvm::BasicBlock *> userEntryBlockByFunction(rubyBlocks2Functions.size());
-    vector<llvm::AllocaInst *> sendArgArrays;
-    vector<llvm::Value *> escapedClosure;
-    vector<int> basicBlockJumpOverrides(cfg.maxBasicBlockId);
-    llvm::IRBuilder<> builder(cs);
-    {
-        for (int i = 0; i < cfg.maxBasicBlockId; i++) {
-            basicBlockJumpOverrides[i] = i;
-        }
-    }
-    int i = 0;
-    for (auto &fun : rubyBlocks2Functions) {
-        auto inits = functionInitializersByFunction.emplace_back(llvm::BasicBlock::Create(
-            cs, "functionEntryInitializers",
-            fun)); // we will build a link for this block later, after we finish building expressions into it
-        builder.SetInsertPoint(inits);
-        auto sendArgArray = builder.CreateAlloca(llvm::ArrayType::get(llvm::Type::getInt64Ty(cs), maxSendArgCount),
-                                                 nullptr, "callArgs");
-        llvm::Value *localClosure = nullptr;
-        if (i == 0) {
-            if (!escapedVariableIndices.empty())
-                localClosure =
-                    builder.CreateCall(cs.module->getFunction("sorbet_allocClosureAsValue"),
-                                       {llvm::ConstantInt::get(cs, llvm::APInt(32, escapedVariableIndices.size()))});
-            else {
-                localClosure = cs.getRubyNilRaw(builder);
-            }
-        } else {
-            localClosure = fun->arg_begin() + 1;
-        }
-        escapedClosure.emplace_back(localClosure);
-        sendArgArrays.emplace_back(sendArgArray);
-        argumentSetupBlocksByFunction.emplace_back(llvm::BasicBlock::Create(cs, "argumentSetup", fun));
-        i++;
-    }
-
-    llvm::BasicBlock *sigVerificationBlock = llvm::BasicBlock::Create(cs, "checkSig", rubyBlocks2Functions[0]);
-
-    vector<llvm::BasicBlock *> llvmBlocks(cfg.maxBasicBlockId);
-    for (auto &b : cfg.basicBlocks) {
-        if (b.get() == cfg.entry()) {
-            llvmBlocks[b->id] = userEntryBlockByFunction[0] =
-                llvm::BasicBlock::Create(cs, "userEntry", rubyBlocks2Functions[0]);
-        } else {
-            llvmBlocks[b->id] = llvm::BasicBlock::Create(cs, llvm::Twine("BB") + llvm::Twine(b->id),
-                                                         rubyBlocks2Functions[b->rubyBlockId]);
-        }
-    }
-    vector<shared_ptr<core::SendAndBlockLink>> blockLinks(rubyBlocks2Functions.size());
-    vector<vector<core::LocalVariable>> rubyBlockArgs(rubyBlocks2Functions.size());
-
-    {
-        // fill in data about args for main function
-        for (auto &treeArg : md->args) {
-            auto *a = ast::MK::arg2Local(treeArg.get());
-            rubyBlockArgs[0].emplace_back(a->localVariable);
-        }
-    }
-
-    for (auto &b : cfg.basicBlocks) {
-        if (b->bexit.cond.variable == core::LocalVariable::blockCall()) {
-            userEntryBlockByFunction[b->rubyBlockId] = llvmBlocks[b->bexit.thenb->id];
-            basicBlockJumpOverrides[b->id] = b->bexit.elseb->id;
-            auto backId = -1;
-            for (auto bid = 0; bid < b->backEdges.size(); bid++) {
-                if (b->backEdges[bid]->rubyBlockId < b->rubyBlockId) {
-                    backId = bid;
-                    break;
-                };
-            }
-            ENFORCE(backId >= 0);
-            auto &expectedSendBind = b->backEdges[backId]->exprs[b->backEdges[backId]->exprs.size() - 2];
-            auto expectedSend = cfg::cast_instruction<cfg::Send>(expectedSendBind.value.get());
-            ENFORCE(expectedSend);
-            ENFORCE(expectedSend->link);
-            blockLinks[b->rubyBlockId] = expectedSend->link;
-
-            rubyBlockArgs[b->rubyBlockId].resize(expectedSend->link->argFlags.size(),
-                                                 core::LocalVariable::noVariable());
-            for (auto &maybeCallOnLoadYieldArg : b->bexit.thenb->exprs) {
-                auto maybeCast = cfg::cast_instruction<cfg::Send>(maybeCallOnLoadYieldArg.value.get());
-                if (maybeCast == nullptr || maybeCast->recv.variable._name != core::Names::blkArg() ||
-                    maybeCast->fun != core::Names::squareBrackets() || maybeCast->args.size() != 1) {
-                    continue;
-                }
-                auto litType = core::cast_type<core::LiteralType>(maybeCast->args[0].type.get());
-                if (litType == nullptr) {
-                    continue;
-                }
-                rubyBlockArgs[b->rubyBlockId][litType->value] = maybeCallOnLoadYieldArg.bind.variable;
-            }
-        }
-    }
-
-    auto approximation = BasicBlockMap{cfg.symbol,
-                                       functionInitializersByFunction,
-                                       argumentSetupBlocksByFunction,
-                                       userEntryBlockByFunction,
-                                       llvmBlocks,
-                                       move(basicBlockJumpOverrides),
-                                       move(sendArgArrays),
-                                       escapedClosure,
-                                       std::move(escapedVariableIndices),
-                                       sigVerificationBlock,
-                                       move(blockLinks),
-                                       move(rubyBlockArgs),
-                                       {}};
-    approximation.llvmVariables =
-        setupLocalVariables(cs, cfg, rubyBlocks2Functions, variablesPrivateToBlocks, approximation, aliases);
-
-    return approximation;
 }
 
 void defineMethod(CompilerState &cs, cfg::Send *i, bool isSelf, llvm::IRBuilder<> &builder) {
@@ -1228,9 +1011,9 @@ void LLVMIREmitter::run(CompilerState &cs, cfg::CFG &cfg, unique_ptr<ast::Method
 
     auto [variablesPrivateToBlocks, escapedVariableIndexes] = findCaptures(cs, md, cfg);
 
-    const BasicBlockMap blockMap =
-        getSorbetBlocks2LLVMBlockMapping(cs, cfg, md, rubyBlocks2Functions, std::move(escapedVariableIndexes),
-                                         maxSendArgCount, variablesPrivateToBlocks, aliases);
+    const BasicBlockMap blockMap = LLVMIREmitterHelpers::getSorbetBlocks2LLVMBlockMapping(
+        cs, cfg, md, rubyBlocks2Functions, std::move(escapedVariableIndexes), maxSendArgCount, variablesPrivateToBlocks,
+        aliases);
 
     ENFORCE(cs.functionEntryInitializers == nullptr, "modules shouldn't be reused");
 
--- compiler/LLVMIREmitter/LLVMIREmitterHelpers.h
+++ compiler/LLVMIREmitter/LLVMIREmitterHelpers.h
@@ -5,12 +5,70 @@
 
 namespace llvm {
 class Function;
-};
+class BasicBlock;
+class AllocaInst;
+}; // namespace llvm
 namespace sorbet::compiler {
+struct BasicBlockMap {
+    core::SymbolRef forMethod;
+    std::vector<llvm::BasicBlock *> functionInitializersByFunction;
+    std::vector<llvm::BasicBlock *> argumentSetupBlocksByFunction;
+    std::vector<llvm::BasicBlock *> userEntryBlockByFunction;
+    std::vector<llvm::BasicBlock *> llvmBlocksBySorbetBlocks;
+    std::vector<int> basicBlockJumpOverrides;
+    std::vector<llvm::AllocaInst *> sendArgArrayByBlock;
+    std::vector<llvm::Value *> escapedClosure;
+    UnorderedMap<core::LocalVariable, int> escapedVariableIndeces;
+    llvm::BasicBlock *sigVerificationBlock;
+    std::vector<std::shared_ptr<core::SendAndBlockLink>> blockLinks;
+    std::vector<std::vector<core::LocalVariable>> rubyBlockArgs;
+    UnorderedMap<core::LocalVariable, llvm::AllocaInst *> llvmVariables;
+};
+
+struct Alias {
+    enum class AliasKind { Constant, InstanceField, ClassField, GlobalField };
+    AliasKind kind;
+    core::SymbolRef constantSym;
+    core::NameRef instanceField;
+    core::NameRef classField;
+    core::SymbolRef globalField;
+    static Alias forConstant(core::SymbolRef sym) {
+        Alias ret;
+        ret.kind = AliasKind::Constant;
+        ret.constantSym = sym;
+        return ret;
+    }
+    static Alias forClassField(core::NameRef name) {
+        Alias ret;
+        ret.kind = AliasKind::ClassField;
+        ret.classField = name;
+        return ret;
+    }
+    static Alias forInstanceField(core::NameRef name) {
+        Alias ret;
+        ret.kind = AliasKind::InstanceField;
+        ret.instanceField = name;
+        return ret;
+    }
+    static Alias forGlobalField(core::SymbolRef sym) {
+        Alias ret;
+        ret.kind = AliasKind::GlobalField;
+        ret.globalField = sym;
+        return ret;
+    }
+};
+
 class LLVMIREmitterHelpers {
 public:
     static std::vector<llvm::Function *> getRubyBlocks2FunctionsMapping(CompilerState &cs, cfg::CFG &cfg,
                                                                         llvm::Function *func);
+
+    static BasicBlockMap getSorbetBlocks2LLVMBlockMapping(
+        CompilerState &cs, cfg::CFG &cfg, std::unique_ptr<ast::MethodDef> &md,
+        std::vector<llvm::Function *> rubyBlocks2Functions,
+        UnorderedMap<core::LocalVariable, int> &&escapedVariableIndices, int maxSendArgCount,
+        const UnorderedMap<core::LocalVariable, std::optional<int>> &variablesPrivateToBlocks,
+        UnorderedMap<core::LocalVariable, Alias> &aliases);
 };
 } // namespace sorbet::compiler
 #endif
