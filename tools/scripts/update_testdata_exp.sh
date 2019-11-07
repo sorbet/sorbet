30f86197f7eca77192ee7b4761c875dfafc81f8f
Paul Tarjan <pt@stripe.com>
2019-11-07 14:33:06 -0800

    indent to 4 spaces

--- tools/scripts/update_testdata_exp.sh
+++ tools/scripts/update_testdata_exp.sh
@@ -7,8 +7,8 @@ cd ../..
 # we are now at the repo root.
 
 if ! command -v parallel &> /dev/null; then
-  echo "This script requires GNU parallel to be installed"
-  exit 1
+    echo "This script requires GNU parallel to be installed"
+    exit 1
 fi
 
 COMMAND_FILE="$(mktemp)"
@@ -17,14 +17,14 @@ trap 'rm -f "$COMMAND_FILE"' EXIT
 bazel build //main:sorbet
 
 if [ $# -eq 0 ]; then
-  paths=(test/testdata)
+    paths=(test/testdata)
 else
-  paths=("$@")
+    paths=("$@")
 fi
 
 rb_src=()
 while IFS='' read -r line; do
-  rb_src+=("$line")
+    rb_src+=("$line")
 done < <(find "${paths[@]}" -name '*.rb*' | sort)
 
 basename=
@@ -32,35 +32,35 @@ srcs=()
 exp_extensions="llo ll stderr"
 
 for this_src in "${rb_src[@]}" DUMMY; do
-  this_base="${this_src%__*}"
-  if [ "$this_base" = "$basename" ]; then
-    srcs=("${srcs[@]}" "$this_src")
-    continue
-  fi
+    this_base="${this_src%__*}"
+    if [ "$this_base" = "$basename" ]; then
+        srcs=("${srcs[@]}" "$this_src")
+        continue
+    fi
 
-  if [ -n "$basename" ]; then
-    llvmir=$(mktemp -d)
-    # shellcheck disable=SC1010
-    echo f\(\) \{ \
-      bazel-bin/main/sorbet \
-        --silence-dev-message \
-        --no-error-count \
-        --llvm-ir-folder \
-        "$llvmir" \
-        "${srcs[@]}" \
-        2\> "$llvmir/update_testdata_exp.stderr"\; \
-      for ext in "$exp_extensions"\; do \
-        exp=${basename%.rb}.\$ext.exp\; \
-        if [ -f \$exp ]\; then \
-          cat "$llvmir/*.\$ext" \> \$exp\; \
-        fi\; \
-      done\; \
-      >> "$COMMAND_FILE" \
-    \}\; f >> "$COMMAND_FILE"
-  fi
+    if [ -n "$basename" ]; then
+        llvmir=$(mktemp -d)
+        # shellcheck disable=SC1010
+        echo f\(\) \{ \
+            bazel-bin/main/sorbet \
+            --silence-dev-message \
+            --no-error-count \
+            --llvm-ir-folder \
+            "$llvmir" \
+            "${srcs[@]}" \
+            2\> "$llvmir/update_testdata_exp.stderr"\; \
+            for ext in "$exp_extensions"\; do \
+                exp=${basename%.rb}.\$ext.exp\; \
+                if [ -f \$exp ]\; then \
+                    cat "$llvmir/*.\$ext" \> \$exp\; \
+                    fi\; \
+                done\; \
+                >> "$COMMAND_FILE" \
+                \}\; f >> "$COMMAND_FILE"
+    fi
 
-  basename="$this_base"
-  srcs=("$this_src")
+    basename="$this_base"
+    srcs=("$this_src")
 done
 
 parallel --joblog - < "$COMMAND_FILE"
