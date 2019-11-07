0b4dca73a76d48f90f0ee9df5172dacbbbf0f926
G. D. Ritter <gdritter@infinitenegativeutility.com>
2019-11-07 13:20:08 -0800

    Update ast/treemap/treemap.h
    Co-Authored-By: Trevor Elliott <awesomelyawesome@gmail.com>

--- ast/treemap/treemap.h
+++ ast/treemap/treemap.h
@@ -772,7 +772,7 @@ private:
                 optArg->default_ = mapIt(move(optArg->default_), ctx.withOwner(v->symbol));
             }
         }
-        // because this is a ShallowMap, we do not map over the body of the class unless it is synthesized by the
+        // because this is a ShallowMap, we do not map over the body of the method unless it is synthesized by the
         // rewriter pass, because we can be sure that typical Ruby code does not e.g. include constant definitions in
         // the body of a method
         if (v->isRewriterSynthesized()) {
