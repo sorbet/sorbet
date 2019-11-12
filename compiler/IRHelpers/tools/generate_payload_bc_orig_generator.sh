95ab7da015025245197cb06f09a73314bfaee3df
Paul Tarjan <pt@stripe.com>
2019-11-12 15:03:36 -0800

    fix mac

--- compiler/IRHelpers/tools/generate_payload_bc_orig_generator.sh
+++ compiler/IRHelpers/tools/generate_payload_bc_orig_generator.sh
@@ -9,12 +9,12 @@ $ar -x "$afile" payload.o
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
   readelf -x .llvmbc payload.o > bitcode.dump
-  cat bitcode.dump | tail -n +3 | sed 's/\(.\{13\}\)\(.\{35\}\).*/\2/' > bitcode-xxd.dump
+  tail -n +3 < bitcode.dump | sed 's/\(.\{13\}\)\(.\{35\}\).*/\2/' > bitcode-xxd.dump
   xxd -r -p < bitcode-xxd.dump > "$output"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   otool  -s __LLVM __bitcode payload.o > bitcode.dump
-  cat bitcode.dump | tail -n +3 > bitcode-xxd.dump
-  hex=$(head -n 1 bitcode.dump| cut -f 1 -d' ')
+  tail -n +3 < bitcode.dump > bitcode-xxd.dump
+  hex=$(head -n 1 bitcode-xxd.dump | cut -f 1)
   offset=$(( 16#$hex ))
-  xxd -r --seek $offset bitcode-xxd.dump "$output"
+  xxd -r --seek -$offset bitcode-xxd.dump "$output"
 fi
