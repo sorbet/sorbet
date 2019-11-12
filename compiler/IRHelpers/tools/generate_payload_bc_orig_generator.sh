3c3bbc5bc1c35b6ec19885b31260fca29356ff36
Paul Tarjan <pt@stripe.com>
2019-11-12 23:00:09 +0000

    get linux to xxd

--- compiler/IRHelpers/tools/generate_payload_bc_orig_generator.sh
+++ compiler/IRHelpers/tools/generate_payload_bc_orig_generator.sh
@@ -8,11 +8,13 @@ afile=$3
 $ar -x "$afile" payload.o
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-  echo "hi"
+  readelf -x .llvmbc payload.o > bitcode.dump
+  cat bitcode.dump | tail -n +3 | sed 's/\(.\{13\}\)\(.\{35\}\).*/\2/' > bitcode-xxd.dump
+  xxd -r -p < bitcode-xxd.dump > "$output"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-  otool  -s __LLVM __bitcode payload.o | tail -n +3 > bitcode.dump
+  otool  -s __LLVM __bitcode payload.o > bitcode.dump
+  cat bitcode.dump | tail -n +3 > bitcode-xxd.dump
+  hex=$(head -n 1 bitcode.dump| cut -f 1 -d' ')
+  offset=$(( 16#$hex ))
+  xxd -r --seek $offset bitcode-xxd.dump "$output"
 fi
-
-hex=$(head -n 1 bitcode.dump| cut -f 1)
-decimal=$(( 16#$hex ))
-xxd -r -seek -"$decimal" bitcode.dump "$output"
