0ca5918e9b9f68af4b33224c6c162ce05e52d773
Dmitry Petrashko <dmitry@stripe.com>
2019-11-14 09:31:10 -0800

    Add minimized test

---
+++ test/testdata/llvm/block_arg_expand.rb
@@ -0,0 +1,6 @@
+# typed: true
+
+[[1, 2]].each do |el1, el2|
+  # if a block/proc is given a single array as an arg and it expects more than a single arg, array is expanded to arguments
+  T.let(el1, Integer) + T.let(el2, Integer)
+end
