897c2fc5dca6d477ea43a33ee13e1bdc812a6666
Jake Zimmerman <zimmerman.jake@gmail.com>
2019-11-14 17:57:03 -0800

    Relax bar of "useful guess" for SUGGEST_SIG query (#2178)
    * Relax bar of "useful guess" for SUGGEST_SIG query
    
    For the purpose of autocorrects / code actions, we only insert a sig
    when we guessed a "useful" sig, namely at least one non-T.untyped
    somewhere (this was partially motivated by the effort to let multiple
    runs of sig suggestion incrementally improve the sigs each time).
    
    But for the purpose of autocomplete, even so much as having names for
    all the arguments already typed out is a benefit, regardless of whether
    they're all untyped.
    
    * Add a test

---
+++ test/testdata/lsp/completion/sig_all_untyped.rb
@@ -0,0 +1,14 @@
+# typed: true
+extend T::Sig
+
+# For this method, we did not "guess something useful" (all the params +
+# return are untyped) so we would not usually emit an autocorrect suggesting
+# this sig. But when suggesting a sig for completion specifically, we suggest
+# the sig unconditionally, so that it at least gives users something to start
+# filling in the holes.
+
+sig # error: no block
+#  ^ apply-completion: [A] item: 0
+def all_untyped(x)
+  T.unsafe(nil)
+end
