7dbecd16070e1c4028b7cbc7589f0e80d6cb0330
Trevor Elliott <awesomelyawesome@gmail.com>
2019-11-18 11:29:24 -0800

    Fix the Enumerator::Lazy test case

--- test/testdata/rbi/lazy_enumerator.rb
+++ test/testdata/rbi/lazy_enumerator.rb
@@ -11,8 +11,7 @@ T.assert_type!([1].lazy.find_all, Enumerator::Lazy[Integer])
 
 ## Generics are not sufficiently robust to type this function yet
 T.assert_type!([1].lazy.flat_map, Enumerator::Lazy[Integer])
-T.reveal_type [1].lazy.flat_map{|i| i + [1]}
-T.assert_type!([1].lazy.flat_map{|i| i + [1]}, Enumerator::Lazy[Integer])
+T.assert_type!([1].lazy.flat_map{|i| [i + 1]}, Enumerator::Lazy[Integer])
 
 T.assert_type!([1].lazy.grep(1..10), Enumerator::Lazy[Integer])
 T.assert_type!([1].lazy.grep(1..10, &:to_s), Enumerator::Lazy[String])
