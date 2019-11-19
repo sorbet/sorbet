adb3bbd8e5c72d7ad3dc3b8cf831d3eefbca1354
Getty Ritter <gdritter@infinitenegativeutility.com>
2019-11-18 16:45:29 -0800

    Clarify comment

--- rewriter/cleanup.cc
+++ rewriter/cleanup.cc
@@ -9,7 +9,8 @@ using namespace std;
 namespace sorbet::rewriter {
 
 // This pass gets rid of some unnecessary nodes that are likely to have gotten created in the course of the rewriter
-// pass, specifically by removing EmptyTree nodes whenever they are created
+// pass, specifically by removing EmptyTree nodes in places where they can be safely removed (i.e. as part of longer
+// sequences of expressions where they are not a return value)
 struct CleanupWalk {
     unique_ptr<ast::Expression> postTransformInsSeq(core::Context ctx, unique_ptr<ast::InsSeq> insSeq) {
         ast::InsSeq::STATS_store newStore;
