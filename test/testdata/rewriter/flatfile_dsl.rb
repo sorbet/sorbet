29e0e9c8bedd793fb375c4e657a8482c3cf77e90
Getty Ritter <gdritter@infinitenegativeutility.com>
2019-11-20 16:15:43 -0800

    Update tests accordingly

--- test/testdata/rewriter/flatfile_dsl.rb
+++ test/testdata/rewriter/flatfile_dsl.rb
@@ -1,13 +1,16 @@
 # typed: true
 class Record
-  def self.ff_from(*_); end
-  def self.ff_pattern(*_); end
-  def self.ff_field(*_); end
+  def self.flatfile; end
+  def self.from(*_); end
+  def self.pattern(*_); end
+  def self.field(*_); end
 end
 class Flatfile < Record
-  ff_from   1..2, :foo
-  ff_pattern(/A-Za-z/, :bar)
-  ff_field :baz
+  flatfile do
+    from   1..2, :foo
+    pattern(/A-Za-z/, :bar)
+    field :baz
+  end
 end
 
 t = Flatfile.new
