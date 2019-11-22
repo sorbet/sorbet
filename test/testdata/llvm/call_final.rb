da75f9b4cfad7ca2ce37a52defcf94892fc94d8a
Dmitry Petrashko <dmitry@stripe.com>
2019-11-21 23:39:25 -0800

    Add test

---
+++ test/testdata/llvm/call_final.rb
@@ -0,0 +1,13 @@
+# typed: true
+# compiled: true
+class A; 
+  T::Sig::WithoutRuntime.sig(:final) {params(n: Integer).returns(Integer)}
+  def self.foo(n)
+    n
+  end
+end;
+class B
+def caller(a)
+  A.foo(a)
+end
+end
