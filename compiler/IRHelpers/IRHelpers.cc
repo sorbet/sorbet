e9f4794b3852de3330e6e5061525a5285f292d1e
Dmitry Petrashko <dmitry@stripe.com>
2019-11-22 14:41:46 -0800

    Fix crash

--- compiler/IRHelpers/IRHelpers.cc
+++ compiler/IRHelpers/IRHelpers.cc
@@ -30,7 +30,7 @@ std::unique_ptr<llvm::Module> IRHelpers::readDefaultModule(const char *name, llv
     for (const auto &[funName, attr] : additionalFunctionAttributes) {
         ret.get()->getFunction(funName)->addFnAttr(attr);
     }
-    ret.get()->getFunction("sorbet_only_exists_to_keep_functions_alive")->eraseFromParent();
+    ret.get()->getFunction("__sorbet_only_exists_to_keep_functions_alive__")->eraseFromParent();
     return move(ret.get());
 }
 
