c54a4428835aa45a10a0b0faabba757f4a78a0d3
Dmitry Petrashko <dmitry@stripe.com>
2019-11-22 14:39:17 -0800

    Don't bother optimizing this function

--- compiler/IRHelpers/payload.c
+++ compiler/IRHelpers/payload.c
@@ -867,7 +867,7 @@ VALUE sorbet_rb_int_neq(VALUE recv, int argc, const VALUE *const restrict argv) 
 VALUE sorbet_i_getRubyClass(const char *const className, long classNameLen) __attribute__((const));
 VALUE sorbet_i_getRubyConstant(const char *const className, long classNameLen) __attribute__((const));
 
-VALUE sorbet_only_exists_to_keep_functions_alive() {
+VALUE __sorbet_only_exists_to_keep_functions_alive__() __attribute__((optnone)) {
     // this function will be nuked but it exists to keep forward definitions alive for clang
     return sorbet_i_getRubyClass(0, 2) + sorbet_i_getRubyConstant(0, 2);
 }
