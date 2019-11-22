e6a52d20b057d3a346000739db1c254e4e6e7180
Paul Tarjan <pt@stripe.com>
2019-11-22 09:45:03 -0800

    handle absolute paths

--- tools/scripts/lldb.sh
+++ tools/scripts/lldb.sh
@@ -3,4 +3,12 @@ set -e
 
 bazel build @ruby_2_6_3//:ruby --config dbg
 
-RUBYLIB=./bazel-bin/external/ruby_2_6_3/ruby.runfiles/ruby_2_6_3/lib/ruby/2.6.0/x86_64-darwin18:./bazel-bin/external/ruby_2_6_3/ruby.runfiles/ruby_2_6_3/lib/ruby/2.6.0 llvmir=$1 lldb -- ./bazel-bin/external/ruby_2_6_3/ruby.runfiles/ruby_2_6_3/bin/ruby -W0 -r ./run/tools/preamble.rb -r ./run/tools/patch_require.rb -e "require './$2'"
+llvmir=$1
+rb=$2
+shift
+shift
+if [ "${rb:0:1}" != "/" ]; then
+    rb="./$rb"
+fi
+
+RUBYLIB=./bazel-bin/external/ruby_2_6_3/ruby.runfiles/ruby_2_6_3/lib/ruby/2.6.0/x86_64-darwin18:./bazel-bin/external/ruby_2_6_3/ruby.runfiles/ruby_2_6_3/lib/ruby/2.6.0 llvmir=$llvmir lldb -- ./bazel-bin/external/ruby_2_6_3/ruby.runfiles/ruby_2_6_3/bin/ruby -W0 -r ./run/tools/preamble.rb -r ./run/tools/patch_require.rb -e "require '$rb'" "$@"
