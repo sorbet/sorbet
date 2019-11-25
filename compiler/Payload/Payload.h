866dadd2dec4ef8ec88182a6cf48257ca1015630
Paul Tarjan <pt@stripe.com>
2019-11-25 13:27:56 -0800

    extract out CompilerState

--- compiler/Payload/Payload.h
+++ compiler/Payload/Payload.h
@@ -1,40 +1,7 @@
-#include "common/ConstExprStr.h"
 #include "common/Exception.h"
-#include "compiler/Core/ForwardDeclarations.h"
-#include <memory>
-#include <string_view>
+#include "compiler/Core/CompilerState.h"
 
 namespace sorbet::compiler {
-class CompilerState {
-public:
-    CompilerState(const core::GlobalState &gs, llvm::LLVMContext &lctx, llvm::Module *);
-
-    const core::GlobalState &gs;
-    llvm::LLVMContext &lctx;
-    llvm::BasicBlock *functionEntryInitializers;
-    llvm::Module *module;
-
-    // useful apis for getting common types
-
-    llvm::StructType *getValueType();
-    llvm::FunctionType *getRubyFFIType();
-
-    /* run optimizations that are super cheap which are expected to be run on each function immediately as it is
-     * generated */
-    void runCheapOptimizations(llvm::Function *);
-    // conversion to Sorbet state
-    operator const sorbet::core::GlobalState &() const {
-        return gs;
-    }
-
-    operator llvm::LLVMContext &() const {
-        return lctx;
-    }
-
-    // tracing
-    void trace(std::string_view) const;
-    void failCompilation(const core::Loc &loc, ConstExprStr msg) const;
-};
 class PayloadLoader {
 public:
     static std::unique_ptr<llvm::Module> readDefaultModule(const char *name, llvm::LLVMContext &);
