6ebb9d6c9b602fa2e3b52f3e618d6c9d386daf57
Getty Ritter <gdritter@infinitenegativeutility.com>
2019-11-26 13:46:18 -0800

    Include initializer rewrite in rewrites

--- rewriter/rewriter.cc
+++ rewriter/rewriter.cc
@@ -25,6 +25,7 @@
 #include "rewriter/attr_reader.h"
 #include "rewriter/cleanup.h"
 #include "rewriter/flatten.h"
+#include "rewriter/initializer.h"
 #include "rewriter/module_function.h"
 
 using namespace std;
@@ -128,6 +129,8 @@ public:
                         return;
                     }
                 },
+
+                [&](ast::MethodDef *mdef) { Initializer::run(ctx, mdef, prevStat); },
 
                 [&](ast::Expression *e) {});
 
