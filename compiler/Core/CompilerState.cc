047fe19476b58f50ae347cda0b6b18baa0f7b1d9
Paul Tarjan <pt@stripe.com>
2019-11-26 23:20:24 -0800

    factor out CompilerState.cc

---
+++ compiler/Core/CompilerState.cc
@@ -0,0 +1,65 @@
+#include "llvm/Bitcode/BitcodeReader.h"
+#include "llvm/IR/DerivedTypes.h" // FunctionType, StructType
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Value.h"
+#include "llvm/Support/MemoryBuffer.h"
+#include "llvm/Transforms/IPO.h"
+#include "llvm/Transforms/IPO/PassManagerBuilder.h"
+#include <string_view>
+// ^^^ violate poisons
+#include "compiler/Core/CompilerState.h"
+#include "compiler/Errors/Errors.h"
+#include "compiler/Payload/AbortCompilation.h"
+#include "core/core.h"
+
+namespace sorbet::compiler {
+
+CompilerState::CompilerState(const core::GlobalState &gs, llvm::LLVMContext &lctx, llvm::Module *module)
+    : gs(gs), lctx(lctx), functionEntryInitializers(nullptr), module(module) {}
+
+llvm::StructType *CompilerState::getValueType() {
+    auto intType = llvm::Type::getInt64Ty(lctx);
+    return llvm::StructType::create(lctx, intType, "RV");
+};
+
+void CompilerState::failCompilation(const core::Loc &loc, ConstExprStr msg) const {
+    if (auto e = gs.beginError(loc, core::errors::Compiler::Unanalyzable)) {
+        e.setHeader(msg);
+    }
+
+    throw AbortCompilation(msg.str);
+}
+
+void CompilerState::trace(string_view msg) const {
+    gs.trace(msg);
+}
+
+llvm::FunctionType *CompilerState::getRubyFFIType() {
+    llvm::Type *args[] = {
+        llvm::Type::getInt32Ty(lctx),    // arg count
+        llvm::Type::getInt64PtrTy(lctx), // argArray
+        llvm::Type::getInt64Ty(lctx)     // self
+    };
+    return llvm::FunctionType::get(llvm::Type::getInt64Ty(lctx), args, false /*not varargs*/);
+}
+
+void CompilerState::runCheapOptimizations(llvm::Function *func) {
+    llvm::legacy::FunctionPassManager pm(module);
+    llvm::PassManagerBuilder pmbuilder;
+    int optLevel = 2;
+    int sizeLevel = 0;
+    pmbuilder.OptLevel = optLevel;
+    pmbuilder.SizeLevel = sizeLevel;
+    pmbuilder.Inliner = llvm::createFunctionInliningPass(optLevel, sizeLevel, false);
+    pmbuilder.DisableUnrollLoops = false;
+    pmbuilder.LoopVectorize = true;
+    pmbuilder.SLPVectorize = true;
+    pmbuilder.VerifyInput = debug_mode;
+    pmbuilder.populateFunctionPassManager(pm);
+    pm.run(*func);
+}
+
+} // namespace sorbet::compiler
--- compiler/Payload/Payload.cc
+++ compiler/Payload/Payload.cc
@@ -1,6 +1,5 @@
 #include "Payload.h"
 #include "llvm/Bitcode/BitcodeReader.h"
-#include "llvm/IR/DerivedTypes.h" // FunctionType, StructType
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/LegacyPassManager.h"
@@ -11,11 +10,12 @@
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
 #include <string_view>
 // ^^^ violate poisons
-#include "compiler/Core/CompilerState.h"
 #include "compiler/Errors/Errors.h"
 #include "compiler/Payload/AbortCompilation.h"
 #include "core/core.h"
+
 using namespace std;
+
 namespace sorbet::compiler {
 string_view getDefaultModuleBitcode();
 
@@ -33,51 +33,6 @@ std::unique_ptr<llvm::Module> PayloadLoader::readDefaultModule(const char *name,
         ret.get()->getFunction(funName)->addFnAttr(attr);
     }
     return move(ret.get());
-}
-
-CompilerState::CompilerState(const core::GlobalState &gs, llvm::LLVMContext &lctx, llvm::Module *module)
-    : gs(gs), lctx(lctx), functionEntryInitializers(nullptr), module(module) {}
-
-llvm::StructType *CompilerState::getValueType() {
-    auto intType = llvm::Type::getInt64Ty(lctx);
-    return llvm::StructType::create(lctx, intType, "RV");
-};
-
-void CompilerState::failCompilation(const core::Loc &loc, ConstExprStr msg) const {
-    if (auto e = gs.beginError(loc, core::errors::Compiler::Unanalyzable)) {
-        e.setHeader(msg);
-    }
-
-    throw AbortCompilation(msg.str);
-}
-
-void CompilerState::trace(string_view msg) const {
-    gs.trace(msg);
-}
-
-llvm::FunctionType *CompilerState::getRubyFFIType() {
-    llvm::Type *args[] = {
-        llvm::Type::getInt32Ty(lctx),    // arg count
-        llvm::Type::getInt64PtrTy(lctx), // argArray
-        llvm::Type::getInt64Ty(lctx)     // self
-    };
-    return llvm::FunctionType::get(llvm::Type::getInt64Ty(lctx), args, false /*not varargs*/);
-}
-
-void CompilerState::runCheapOptimizations(llvm::Function *func) {
-    llvm::legacy::FunctionPassManager pm(module);
-    llvm::PassManagerBuilder pmbuilder;
-    int optLevel = 2;
-    int sizeLevel = 0;
-    pmbuilder.OptLevel = optLevel;
-    pmbuilder.SizeLevel = sizeLevel;
-    pmbuilder.Inliner = llvm::createFunctionInliningPass(optLevel, sizeLevel, false);
-    pmbuilder.DisableUnrollLoops = false;
-    pmbuilder.LoopVectorize = true;
-    pmbuilder.SLPVectorize = true;
-    pmbuilder.VerifyInput = debug_mode;
-    pmbuilder.populateFunctionPassManager(pm);
-    pm.run(*func);
 }
 
 } // namespace sorbet::compiler
