b7271b16d869f5a7a327a9c9118a5415b1ebf412
Paul Tarjan <pt@stripe.com>
2019-11-26 23:14:09 -0800

    fix namespace

--- compiler/Payload/AbortCompilation.h
+++ compiler/Payload/AbortCompilation.h
@@ -1,5 +1,7 @@
+namespace sorbet::compiler {
 class AbortCompilation : public sorbet::SorbetException {
 public:
     AbortCompilation(const std::string &message) : SorbetException(message){};
     AbortCompilation(const char *message) : SorbetException(message){};
 };
+} // namespace sorbet::compiler
