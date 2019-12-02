4497a96ee4cfb6a36edcd92a32b868e5de677c99
Trevor Elliott <trevor@stripe.com>
2019-12-02 13:35:58 -0800

    Add a test for app_tarai with a signature

---
+++ test/testdata/ruby_benchmark/app_tarai_sig.rb
@@ -0,0 +1,13 @@
+# typed: true
+
+T::Sig::WithoutRuntime.sig(:final) {params(x: Integer, y: Integer, z: Integer).returns(Integer)}
+def tarai( x, y, z )
+  if x <= y
+  then y
+  else tarai(tarai(x-1, y, z),
+             tarai(y-1, z, x),
+             tarai(z-1, x, y))
+  end
+end
+
+tarai(12, 6, 0)
