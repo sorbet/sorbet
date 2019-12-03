756d926e8ec71e1bba0995b31b586771c7dfa8ea
Jake Zimmerman <zimmerman.jake@gmail.com>
2019-12-03 15:44:16 -0800

    Add some requested tests with positional args

--- test/testdata/lsp/completion/snippet_repeated.rb
+++ test/testdata/lsp/completion/snippet_repeated.rb
@@ -9,6 +9,14 @@ class A
   sig {params(x: T.untyped).void}
   def method_using_rest_arg(*x)
   end
+
+  sig {params(x: Integer, y: T.untyped).void}
+  def positional_then_kwarg(x, *y)
+  end
+
+  sig {params(x: Integer, y: T.untyped).void}
+  def positional_then_rest_arg(x, *y)
+  end
 end
 
 A.new.method_using_kwar # error: does not exist
@@ -16,3 +24,9 @@ A.new.method_using_kwar # error: does not exist
 
 A.new.method_using_rest_ar # error: does not exist
 #                         ^ apply-completion: [B] item: 0
+
+A.new.positional_then_kwar # error: does not exist
+#                         ^ apply-completion: [C] item: 0
+
+A.new.positional_then_rest_ar # error: does not exist
+#                            ^ apply-completion: [D] item: 0
