f70f7df08b6667fa79597400a1c39ae4937f7869
Farid Zakaria <farid.m.zakaria@gmail.com>
2019-12-05 14:50:59 -0800

    Java static release 2 (#2294)
    * Revert "Revert "Introduce release file for java platform (#2254)" (#2292)"
    
    This reverts commit 58c9889ce312a9e496a813228ae291afbc095e20.
    
    * Added downloading of artifacts
    
    * Removed my little woopsyt

---
+++ .buildkite/build-static-release-java.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# exit on the first error
+# this makes sure we don't create a java gem missing
+# either linux or mac
+set -o errexit
+
+echo "--- Dowloading artifacts"
+rm -rf release
+rm -rf _out_
+buildkite-agent artifact download "_out_/**/*" .
+
+# Based on the output of build-static-release.sh
+# _out_/gems/ should have the Linux & Mac sorbet-static gem
+mkdir -p gems/sorbet-static/libexec
+
+git_commit_count=$(git rev-list --count HEAD)
+prefix="0.4"
+release_version="$prefix.${git_commit_count}"
+
+for platform in universal-darwin x86_64-linux
+do
+  gem unpack _out_/gems/sorbet-static-"${release_version}"-${platform}*.gem
+
+  case $platform in
+    x86_64-linux)
+        mv sorbet-static-"${release_version}"-${platform}/libexec/sorbet  gems/sorbet-static/libexec/linux.sorbet
+    ;;
+
+    universal-darwin)
+        mv sorbet-static-"${release_version}"-${platform}*/libexec/sorbet gems/sorbet-static/libexec/mac.sorbet
+    ;;
+  esac
+
+done
+
+pushd gems/sorbet-static
+
+sed -i.bak "s/Gem::Platform::CURRENT/'java'/" sorbet-static.gemspec
+sed -i.bak "s/0\\.0\\.0/${release_version}/" sorbet-static.gemspec
+sed -i.bak "s/'libexec\/sorbet'/'libexec\/mac.sorbet', 'libexec\/linux.sorbet'/" sorbet-static.gemspec
+
+gem build sorbet-static.gemspec
+
+popd
+
+mv gems/sorbet-static/sorbet-static-"${release_version}"-java.gem _out_/gems
