49ee7c688237599b94ddb91133745a96019819ee
Paul Tarjan <github@paulisageek.com>
2019-12-04 17:54:42 -0800

    try sudo

--- test/cli/bad-perm/bad-perm.sh
+++ test/cli/bad-perm/bad-perm.sh
@@ -4,4 +4,9 @@ dir=$(mktemp -d)
 echo "parse_fail +" > "$dir/file.rb"
 mkdir "$dir/no-read"
 chmod 000 "$dir/no-read"
-main/sorbet --silence-dev-message "$dir" 2>&1 | sed "s,$dir,dir,"
+cmd="main/sorbet --silence-dev-message $dir"
+if [ $UID == 0 ]; then
+    su -s /bin/bash nobody -c "$cmd" 2>&1 | sed "s,$dir,dir,"
+else
+    $cmd 2>&1 | sed "s,$dir,dir,"
+fi
