65ce3f9efcbc47af7b4fb6476e52388ce7f4f2b8
Jake Zimmerman <zimmerman.jake@gmail.com>
2019-12-10 14:35:51 -0800

    Improve test

--- test/testdata/infer/attached_class_printing.rb
+++ test/testdata/infer/attached_class_printing.rb
@@ -1,8 +1,18 @@
 # typed: true
 
-class Child
-  extend T::Sig
+class Module
+  include T::Sig
+end
 
+class Parent
+  sig {returns(T.experimental_attached_class)}
+  def self.foo
+    # Goal of test: no < ... > in the user-visible type.
+    T.reveal_type(new) # error: Revealed type: `T.attached_class (of Parent)`
+  end
+end
+
+class Child < Parent
   sig {returns(T.experimental_attached_class)}
   def self.foo
     # Goal of test: no < ... > in the user-visible type.
