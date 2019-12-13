2a86935187ab7a087cdd43ce2186ab4f7082e1c9
Paul Tarjan <github@paulisageek.com>
2019-12-13 09:10:04 -0800

    add new errors

--- test/testdata/resolver/fuzz_ancester.rb
+++ test/testdata/resolver/fuzz_ancester.rb
@@ -4,13 +4,18 @@ end
 class A < b::C # error: Superclasses must only contain constant literals
 end
 class A
-  include b # error: `include` must only contain constant literals
+  include b
+#         ^ error: `include` must only contain constant literals
+# ^^^^^^^^^ error: Expected `Module` but found `NilClass` for argument `arg0`
 end
 class A
-  extend b # error: `extend` must only contain constant literals
+  extend b
+#        ^ error: `extend` must only contain constant literals
+# ^^^^^^^^ error: Expected `Module` but found `NilClass` for argument `arg0`
 end
 module B; end
 class A
   include B, c
 #            ^ error: `include` must only contain constant literals
+# ^^^^^^^^^^^^ error: Expected `Module` but found `NilClass` for argument `arg0`
 end
--- test/testdata/resolver/fuzz_include_type_alias.rb
+++ test/testdata/resolver/fuzz_include_type_alias.rb
@@ -3,5 +3,7 @@
 A = T.type_alias {Integer}
 
 class B
-  include A # error: Superclasses and mixins may not be type aliases
+  include A
+#         ^ error: Superclasses and mixins may not be type aliases
+# ^^^^^^^^^ error: Expected `Module` but found `<Type: Integer>` for argument `arg0`
 end
