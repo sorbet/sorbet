f08b1c35bfa1b8fa37d9dc51d78b95426fce12e6
Getty Ritter <gdritter@infinitenegativeutility.com>
2019-12-18 10:14:53 -0800

    Add tests for all typed variants of Hash.new

--- test/testdata/rbi/hash.rb
+++ test/testdata/rbi/hash.rb
@@ -29,3 +29,25 @@ T.assert_type!(
 )
 
 {a: 1}.merge({b: 2}, {c: 3})
+
+
+h1 = T::Hash[Integer, String].new
+h1[2] = "foo"
+h1[nil] = "foo" # error: Expected `Integer` but found `NilClass` for argument `arg0`
+h1[3] = nil # error: Expected `String` but found `NilClass` for argument `arg1`
+
+h2 = T::Hash[Integer, String].new("yo")
+h2[2] = "foo"
+h2[nil] = "foo" # error: Expected `Integer` but found `NilClass` for argument `arg0`
+h2[3] = nil # error: Expected `String` but found `NilClass` for argument `arg1`
+
+h3 = T::Hash[Integer, String].new do |h, k|
+  T.assert_type!(h, T::Hash[Integer, String])
+  T.assert_type!(k, Integer)
+  h[k] = "foo"
+  h[nil] = "foo" # error: Expected `Integer` but found `NilClass` for argument `arg0`
+  h[k] = nil # error: Expected `String` but found `NilClass` for argument `arg1`
+end
+h3[2] = "foo"
+h3[nil] = "foo" # error: Expected `Integer` but found `NilClass` for argument `arg0`
+h3[3] = nil # error: Expected `String` but found `NilClass` for argument `arg1`
