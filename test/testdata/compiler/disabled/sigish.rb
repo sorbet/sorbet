0c8fa570a35c4824bdf757ffbc89a330b9a15d15
Paul Tarjan <pt@stripe.com>
2019-12-20 11:24:14 -0700

    even smaller

--- test/testdata/compiler/disabled/sigish.rb
+++ test/testdata/compiler/disabled/sigish.rb
@@ -3,12 +3,7 @@
 module Sigish
   T::Sig::WithoutRuntime.sig {params(blk: T.proc.void).void}
   def self.sigish(&blk)
-    yield
   end
 
-  def self.params
-    91
-  end
-
-  puts sigish {params}
+  sigish {}
 end
