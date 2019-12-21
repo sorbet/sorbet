68ae58846d303d0589d014396e4cd85199c1e069
Paul Tarjan <pt@stripe.com>
2019-12-20 21:51:47 -0700

    a bit more testing

--- test/testdata/compiler/disabled/parameters.rb
+++ test/testdata/compiler/disabled/parameters.rb
@@ -1,6 +1,14 @@
 # typed: true
 
-def foo(a, b=1, c:)
+def foo(a, b=1, c:, d:2, **e)
+end
+
+def bar(*args)
+end
+
+def baz(*)
 end
 
 puts method(:foo).parameters
+puts method(:bar).parameters
+puts method(:baz).parameters
