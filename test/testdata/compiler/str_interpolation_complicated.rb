4e0862ff7b08d1740fd4d1957f6a7f8e672f5a5c
Trevor Elliott <awesomelyawesome@gmail.com>
2020-01-03 17:27:21 -0800

    Add another string interpolation test

---
+++ test/testdata/compiler/str_interpolation_complicated.rb
@@ -0,0 +1,24 @@
+# typed: true
+
+def foo_1
+  s = Class.new do
+    T.unsafe(self).define_method(:to_s) do
+      puts "foo_1 to_s"
+      "1"
+    end
+  end
+  s.new
+end
+
+def foo_2
+  s = Class.new do
+    T.unsafe(self).define_method(:to_s) do
+      puts "foo_2 to_s"
+      1
+    end
+  end
+  s.new
+end
+
+puts "#{foo_1}"
+puts "#{foo_2}".gsub(/0x[0-9a-f]+/, '')
