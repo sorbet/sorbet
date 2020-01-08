6462cdfb619898f213000b8e8fa4cd07d009b9d1
Jake Zimmerman <zimmerman.jake@gmail.com>
2020-01-08 15:48:40 -0800

    Add failing test

---
+++ test/testdata/lsp/completion/constants_root.rb
@@ -0,0 +1,13 @@
+# typed: true
+
+# Prevents regression against an off-by-one error we used to have that would
+# stop before suggestion similiar constants that existed on <root>
+
+class A
+  extend T::Sig
+  sig {returns(Int)} # error: Unable to resolve
+  #               ^ completion: (nothing)
+  def foo
+    0
+  end
+end
