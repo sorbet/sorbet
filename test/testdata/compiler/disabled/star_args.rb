c9524deebc37a8743c981594432b50b9679103c8
Dmitry Petrashko <dmitry@stripe.com>
2020-01-09 22:06:37 +0000

    Add test that verifies more of starr args interactions

--- test/testdata/compiler/disabled/star_args.rb
+++
@@ -1,7 +0,0 @@
-# typed: true
-
-def foo(*args)
-  puts args.inspect
-end
-foo(1)
-foo(1,2,3)
---
+++ test/testdata/compiler/star_args.rb
@@ -0,0 +1,28 @@
+# typed: true
+
+def has_only_star_args(*args)
+  puts args.inspect
+end
+has_only_star_args(1)
+has_only_star_args(1,2,3)
+has_only_star_args(1,2,3, k:2)
+
+def has_star_arg_and_positional(p, *args)
+  puts p, args.inspect
+end
+
+has_star_arg_and_positional(1)
+has_star_arg_and_positional(1,2,3)
+has_star_arg_and_positional(1,2,3, k:2)
+
+def has_star_arg_and_positional_and_default(p, d=2, *args)
+  puts p, d, args.inspect
+end
+
+has_star_arg_and_positional_and_default(1)
+has_star_arg_and_positional_and_default(1,2)
+has_star_arg_and_positional_and_default(1,k:2)
+has_star_arg_and_positional_and_default(1,2,3)
+has_star_arg_and_positional_and_default(1,2,3, k:2)
+
+
