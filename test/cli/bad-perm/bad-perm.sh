42011bbec964d27ed444d0639c76fa8bdb5144a6
Dmitry Petrashko <dmitry@stripe.com>
2020-01-10 14:22:48 -0800

    Fix test that runs su when it's not root (#2455)

--- test/cli/bad-perm/bad-perm.sh
+++ test/cli/bad-perm/bad-perm.sh
@@ -5,7 +5,7 @@ echo "parse_fail +" > "$dir/file.rb"
 mkdir "$dir/no-read"
 chmod 000 "$dir/no-read"
 cmd="main/sorbet --silence-dev-message $dir"
-if [ $UID == 0 ]; then
+if [ "$(id -u)" = 0 ]; then
     su -s /bin/bash nobody -c "$cmd" 2>&1 | sed "s,$dir,dir,"
 else
     $cmd 2>&1 | sed "s,$dir,dir,"
