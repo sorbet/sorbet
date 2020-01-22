46d463d8aebef5c2d9b4cdc1774035d90e4616fd
Harry Doan <hdoan@chanzuckerberg.com>
2020-01-22 13:50:51 -0800

    Use .send to access private attr_reader in ruby 2.3, 2.4 (#2509)
    * use .send to access private attr_reader in ruby 2.3, 2.4
    
    * Update gems/sorbet-runtime/lib/types/props/decorator.rb
    
    Co-Authored-By: Dmitry Petrashko <dark@d-d.me>
    
    Co-authored-by: Dmitry Petrashko <dark@d-d.me>

--- gems/sorbet-runtime/lib/types/props/decorator.rb
+++ gems/sorbet-runtime/lib/types/props/decorator.rb
@@ -470,7 +470,7 @@ class T::Props::Decorator
         end
       else
         # Fast path (~30x faster as of Ruby 2.6)
-        @class.attr_reader(name)
+        @class.send(:attr_reader, name) # send is used because `attr_reader` is private in 2.4
       end
     end
   end
