4f8ee9e44189b8e40954ce03c0ac401e986855da
John Vilk <jvilk@stripe.com>
2020-01-22 14:58:15 -0800

    Make editCount a u4.

--- main/lsp/json_types.h
+++ main/lsp/json_types.h
@@ -21,7 +21,7 @@ public:
     // This specific update contains edits with the given epoch
     u4 epoch = 0;
     // The total number of edits that this update represents. Used for stats.
-    u2 editCount = 0;
+    u4 editCount = 0;
 
     std::vector<std::shared_ptr<core::File>> updatedFiles;
     std::vector<core::FileHash> updatedFileHashes;
