6fc7c789fa05036e9b9f0159accd8928dfa873fb
Dmitry Petrashko <dmitry@stripe.com>
2020-01-23 03:00:15 +0000

    Simpler and more representable test

---
+++ test/testdata/compiler/self_lambda_constant.rb
@@ -0,0 +1,11 @@
+# typed: true
+# compiled: true
+
+puts "self_top " + self.to_s
+module Bug
+  puts "self_before " + self.to_s
+  XYZ = -> {puts "inside "+ self.to_s}
+  puts "after_before " +  self.to_s
+end
+
+Bug::XYZ.call
