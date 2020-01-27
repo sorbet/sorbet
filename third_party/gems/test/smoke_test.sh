38080fd4aed7cb1e7c78f5d17120f66dafd505b0
Trevor Elliott <awesomelyawesome@gmail.com>
2020-01-27 10:56:31 -0800

    Update the gem smoke tests

--- third_party/gems/test/smoke_test.sh
+++ third_party/gems/test/smoke_test.sh
@@ -3,34 +3,21 @@
 # This tests that the bundler wrapper produced by the `@gems` package is able to
 # install a gem, and provide an environment with that gem in scope.
 
+# --- begin runfiles.bash initialization v2 ---
+# Copy-pasted from the Bazel Bash runfiles library v2.
+set -uo pipefail; f=bazel_tools/tools/bash/runfiles/runfiles.bash
+# shellcheck disable=SC1090
+source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "${RUNFILES_MANIFEST_FILE:-/dev/null}" | cut -f2- -d' ')" 2>/dev/null || \
+  source "$0.runfiles/$f" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "$0.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+  { echo>&2 "ERROR: cannot find $f"; exit 1; }; f=; set -e
+# --- end runfiles.bash initialization v2 ---
+
 # This is the name of a ruby bazel package, specified in
 # //third_party/externals.bzl
 ruby_package=$1
-
-# --- begin runfiles.bash initialization ---
-# Copy-pasted from Bazel's Bash runfiles library https://github.com/bazelbuild/bazel/blob/defd737761be2b154908646121de47c30434ed51/tools/bash/runfiles/runfiles.bash
-set -euo pipefail
-if [[ ! -d "${RUNFILES_DIR:-/dev/null}" && ! -f "${RUNFILES_MANIFEST_FILE:-/dev/null}" ]]; then
-  if [[ -f "$0.runfiles_manifest" ]]; then
-    export RUNFILES_MANIFEST_FILE="$0.runfiles_manifest"
-  elif [[ -f "$0.runfiles/MANIFEST" ]]; then
-    export RUNFILES_MANIFEST_FILE="$0.runfiles/MANIFEST"
-  elif [[ -f "$0.runfiles/bazel_tools/tools/bash/runfiles/runfiles.bash" ]]; then
-    export RUNFILES_DIR="$0.runfiles"
-  fi
-fi
-if [[ -f "${RUNFILES_DIR:-/dev/null}/bazel_tools/tools/bash/runfiles/runfiles.bash" ]]; then
-  # shellcheck disable=SC1090
-  source "${RUNFILES_DIR}/bazel_tools/tools/bash/runfiles/runfiles.bash"
-elif [[ -f "${RUNFILES_MANIFEST_FILE:-/dev/null}" ]]; then
-  # shellcheck disable=SC1090
-  source "$(grep -m1 "^bazel_tools/tools/bash/runfiles/runfiles.bash " \
-            "$RUNFILES_MANIFEST_FILE" | cut -d ' ' -f 2-)"
-else
-  echo >&2 "ERROR: cannot find @bazel_tools//tools/bash/runfiles:runfiles.bash"
-  exit 1
-fi
-# --- end runfiles.bash initialization ---
 
 # put bundle in the path
 PATH="$(dirname "$(rlocation "{{workspace}}/bundler/bundle")"):$PATH"
@@ -41,9 +28,15 @@ PATH="$(dirname "$RUBY_BIN"):$PATH"
 
 export PATH
 
+sandbox="$(mktemp -d)"
+pushd "$sandbox" > /dev/null
+
 # bundle requires $HOME
-HOME=$PWD
+HOME="$sandbox"
 export HOME
+
+BUNDLE_PATH="$sandbox/bundler"
+export BUNDLE_PATH
 
 # ensure that bundle works
 bundle --help
@@ -58,7 +51,11 @@ VENDOR_CACHE=$(dirname "$(rlocation "{{workspace}}/test/vendor/cache/cantor-1.2.
 ln -s "$VENDOR_CACHE" vendor/cache
 
 # setup 'cantor'
-bundle install --deployment --local
+bundle install --local --no-prune
 
 # test that the environment is correct
 bundle exec ruby -e 'require "cantor"; puts "success"'
+
+popd > /dev/null
+
+rm -rf "$sandbox"
