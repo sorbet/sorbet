2ad182289f317d8777117ce863efdea55c399ade
Jake Zimmerman <zimmerman.jake@gmail.com>
2020-01-28 13:02:20 -0800

    Add some failing tests

---
+++ test/testdata/infer/flatten_private.rb
@@ -0,0 +1,21 @@
+# typed: true
+
+class A
+  extend T::Sig
+
+  # Horrible incompatabible override but I think it's fine.
+  # The purpose here is to prove (without needing to scruitinze the exp output)
+  # that we're leaving behind the symbol in the tree.
+  sig {params(arg0: Integer).void}
+  def self.private(arg0); end
+  sig {params(arg0: Integer).void}
+  def self.private_class_method(arg0); end
+
+  private_class_method def self.outer_static
+    private_class_method def self.inner_static; end # error: Expected `Integer` but found `Symbol(:"inner_static")` for argument `arg0`
+  end
+
+  private def outer_instance
+    self.class.private def inner_instance; end # error: Expected `Integer` but found `Symbol(:"inner_instance")` for argument `arg0`
+  end
+end
