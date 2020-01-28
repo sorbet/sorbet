0eeb98cc2795e1c7c880d041e5c3d80368c907a6
Trevor Elliott <awesomelyawesome@gmail.com>
2020-01-28 15:51:59 -0800

    Fix `Date#-` (#2567)
    As DateTime inherits from Date, this method needs to return
    `T.self_type` instead.

--- test/testdata/rbi/date.rb
+++ test/testdata/rbi/date.rb
@@ -4,3 +4,19 @@ require "date"
 
 T.assert_type!(Date.today, Date)
 T.assert_type!(Date.today + 1, Date)
+
+class Test
+  extend T::Sig
+
+  sig {params(x: Date).void}
+  def test_date(x)
+    T.reveal_type(x -  x) # error: Revealed type: `Rational`
+    T.reveal_type(x - 10) # error: Revealed type: `Date`
+  end
+
+  sig {params(x: DateTime).void}
+  def test_datetime(x)
+    T.reveal_type(x - x) # error: Revealed type: `Rational`
+    T.reveal_type(x - 10) # error: Revealed type: `DateTime`
+  end
+end
