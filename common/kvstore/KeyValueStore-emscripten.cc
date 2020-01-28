17242eaa9a8d52b33f6ab7076d35e9312fbfa2f9
Dmitry Petrashko <dmitry@stripe.com>
2020-01-28 18:50:48 +0000

    Format

--- common/kvstore/KeyValueStore-emscripten.cc
+++ common/kvstore/KeyValueStore-emscripten.cc
@@ -4,8 +4,7 @@
 
 using namespace std;
 namespace sorbet {
-struct KeyValueStore::DBState {
-};
+struct KeyValueStore::DBState {};
 
 [[noreturn]] static void throw_mdb_error(string_view what, int err) {
     fmt::print(stderr, "mdb error: {}: {}\n", what, "");
@@ -18,34 +17,34 @@ KeyValueStore::KeyValueStore(string version, string path, string flavor)
 }
 KeyValueStore::~KeyValueStore() noexcept(false) {
     throw_mdb_error("creating databases isn't supported on emscripten"sv, 0);
-   }
+}
 
 void KeyValueStore::write(string_view key, const vector<u1> &value) {
     throw_mdb_error("creating databases isn't supported on emscripten"sv, 0);
-   }
+}
 
 u1 *KeyValueStore::read(string_view key) {
     throw_mdb_error("creating databases isn't supported on emscripten"sv, 0);
-    }
+}
 
 void KeyValueStore::clear() {
     throw_mdb_error("creating databases isn't supported on emscripten"sv, 0);
-    }
+}
 
 string_view KeyValueStore::readString(string_view key) {
     throw_mdb_error("creating databases isn't supported on emscripten"sv, 0);
-    }
+}
 
 void KeyValueStore::writeString(string_view key, string_view value) {
     throw_mdb_error("creating databases isn't supported on emscripten"sv, 0);
-    }
+}
 
 void KeyValueStore::refreshMainTransaction() {
     throw_mdb_error("creating databases isn't supported on emscripten"sv, 0);
-    }
+}
 
 bool KeyValueStore::commit(unique_ptr<KeyValueStore> k) {
     throw_mdb_error("creating databases isn't supported on emscripten"sv, 0);
-   }
+}
 
 } // namespace sorbet
