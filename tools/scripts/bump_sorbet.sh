84a0f98c94ca42e15a8b2743af407d93e3ef47e6
Trevor Elliott <awesomelyawesome@gmail.com>
2020-02-04 15:15:09 -0800

    Assume that the repo argument is the base, not the .git dir

--- tools/scripts/bump_sorbet.sh
+++ tools/scripts/bump_sorbet.sh
@@ -3,11 +3,11 @@
 set -euo pipefail
 
 branch="${1:-master}"
-sorbet_repo="${2:-HOME/stripe/sorbet/.git}"
+sorbet_repo="${2:-HOME/stripe/sorbet}"
 
-git --git-dir "$sorbet_repo" fetch --quiet origin "$branch"
-new_sorbet_version="$(git --git-dir="$sorbet_repo" rev-parse "origin/$branch")"
-new_shallow_since="$(git --git-dir="$sorbet_repo" log -n 1 --pretty=format:"%cd" --date=raw "origin/$branch")"
+git --git-dir "$sorbet_repo/.git" fetch --quiet origin "$branch"
+new_sorbet_version="$(git --git-dir="$sorbet_repo/.git" rev-parse "origin/$branch")"
+new_shallow_since="$(git --git-dir="$sorbet_repo/.git" log -n 1 --pretty=format:"%cd" --date=raw "origin/$branch")"
 
 sorbet_version_bzl="third_party/sorbet_version.bzl"
 sed -i.bak -e "s/SORBET_VERSION = \"[^\"]*\"/SORBET_VERSION = \"$new_sorbet_version\"/" "$sorbet_version_bzl"
