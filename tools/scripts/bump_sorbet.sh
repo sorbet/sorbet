36fca3d7c6fb23fc61c5152bf84414f4859fdac8
Trevor Elliott <awesomelyawesome@gmail.com>
2020-02-04 14:18:04 -0800

    Update the bump script to take a branch and repo

--- tools/scripts/bump_sorbet.sh
+++ tools/scripts/bump_sorbet.sh
@@ -2,11 +2,12 @@
 
 set -euo pipefail
 
-sorbet_repo="$HOME/stripe/sorbet/.git"
+branch="${1:-master}"
+sorbet_repo="${2:-HOME/stripe/sorbet/.git}"
 
-git --git-dir "$sorbet_repo" fetch --quiet origin master
-new_sorbet_version="$(git --git-dir="$sorbet_repo" rev-parse origin/master)"
-new_shallow_since="$(git --git-dir="$sorbet_repo" log -n 1 --pretty=format:"%cd" --date=raw origin/master)"
+git --git-dir "$sorbet_repo" fetch --quiet origin "$branch"
+new_sorbet_version="$(git --git-dir="$sorbet_repo" rev-parse "origin/$branch")"
+new_shallow_since="$(git --git-dir="$sorbet_repo" log -n 1 --pretty=format:"%cd" --date=raw "origin/$branch")"
 
 sorbet_version_bzl="third_party/sorbet_version.bzl"
 sed -i.bak -e "s/SORBET_VERSION = \"[^\"]*\"/SORBET_VERSION = \"$new_sorbet_version\"/" "$sorbet_version_bzl"
