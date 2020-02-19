635f85f1fc4b7c4fe6730d8665daab738a150ca8
Jake Zimmerman <zimmerman.jake@gmail.com>
2020-02-18 17:46:05 -0800

    Improve private_class_method test

--- test/testdata/compiler/private_class_method.rb
+++ test/testdata/compiler/private_class_method.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 # typed: true
 # compiled: true
+
 module A
-  private_class_method def self.pcm(); end;
-  def self.call()
-          pcm;
+  private_class_method def self.pcm
+    puts 218
+  end
+
+  def self.main
+    pcm
   end
 end
 
-A.call()
+A.main
