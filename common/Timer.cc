46b2e448ea4504d985f4e3306d112c21d0180551
John Vilk <jvilk@stripe.com>
2020-02-20 16:57:20 -0800

    Timer histograms

--- common/Timer.cc
+++ common/Timer.cc
@@ -1,30 +1,46 @@
 #include "common/Timer.h"
+#include "common/sort.h"
 using namespace std;
 namespace sorbet {
+namespace {
+vector<int> ensureSorted(initializer_list<int> input) {
+    vector<int> output(input);
+    output.push_back(INT_MAX);
+    fast_sort(output);
+    return output;
+}
+} // namespace
 
 Timer::Timer(spdlog::logger &log, ConstExprStr name, FlowId prev, initializer_list<pair<ConstExprStr, string>> args,
-             chrono::time_point<chrono::steady_clock> start)
-    : log(log), name(name), prev(prev), self{0}, args(args), start(start) {}
+             chrono::time_point<chrono::steady_clock> start, initializer_list<int> histogramBuckets)
+    : log(log), name(name), prev(prev), self{0}, args(args), start(start),
+      histogramBuckets(ensureSorted(histogramBuckets)) {}
 
-Timer::Timer(spdlog::logger &log, ConstExprStr name, FlowId prev, initializer_list<pair<ConstExprStr, string>> args)
-    : Timer(log, name, prev, args, chrono::steady_clock::now()){};
+Timer::Timer(spdlog::logger &log, ConstExprStr name, FlowId prev, initializer_list<pair<ConstExprStr, string>> args,
+             initializer_list<int> histogramBuckets)
+    : Timer(log, name, prev, args, chrono::steady_clock::now(), histogramBuckets){};
 
 Timer::Timer(spdlog::logger &log, ConstExprStr name, initializer_list<pair<ConstExprStr, string>> args)
-    : Timer(log, name, FlowId{0}, args){};
+    : Timer(log, name, FlowId{0}, args, {}){};
+
+Timer::Timer(spdlog::logger &log, ConstExprStr name, initializer_list<int> histogramBuckets)
+    : Timer(log, name, FlowId{0}, {}, histogramBuckets) {}
 
 Timer::Timer(const shared_ptr<spdlog::logger> &log, ConstExprStr name, FlowId prev,
              initializer_list<pair<ConstExprStr, string>> args)
-    : Timer(*log, name, prev, args){};
+    : Timer(*log, name, prev, args, {}){};
 
 Timer::Timer(const shared_ptr<spdlog::logger> &log, ConstExprStr name,
              initializer_list<pair<ConstExprStr, string>> args)
     : Timer(*log, name, args){};
 
-Timer::Timer(const shared_ptr<spdlog::logger> &log, ConstExprStr name) : Timer(*log, name, {}){};
-Timer::Timer(const shared_ptr<spdlog::logger> &log, ConstExprStr name, FlowId prev) : Timer(*log, name, prev, {}){};
+Timer::Timer(const shared_ptr<spdlog::logger> &log, ConstExprStr name)
+    : Timer(*log, name, initializer_list<pair<ConstExprStr, string>>{}){};
+Timer::Timer(const shared_ptr<spdlog::logger> &log, ConstExprStr name, FlowId prev) : Timer(*log, name, prev, {}, {}){};
 
-Timer::Timer(spdlog::logger &log, ConstExprStr name) : Timer(log, name, {}){};
-Timer::Timer(spdlog::logger &log, ConstExprStr name, FlowId prev) : Timer(log, name, prev, {}){};
+Timer::Timer(spdlog::logger &log, ConstExprStr name)
+    : Timer(log, name, initializer_list<pair<ConstExprStr, string>>{}){};
+Timer::Timer(spdlog::logger &log, ConstExprStr name, FlowId prev) : Timer(log, name, prev, {}, {}){};
 
 int getGlobalTimingId() {
     static atomic<int> counter = 1;
@@ -43,10 +59,11 @@ void Timer::cancel() {
 }
 
 Timer Timer::clone(ConstExprStr name) {
-    Timer forked(log, name, prev, {}, start);
+    Timer forked(log, name, prev, {}, start, {});
     forked.args = args;
     forked.tags = tags;
     forked.canceled = canceled;
+    forked.histogramBuckets = histogramBuckets;
     return forked;
 }
 
@@ -69,8 +86,19 @@ Timer::~Timer() {
     if (!canceled && dur > std::chrono::milliseconds(1)) {
         // the trick ^^^ is to skip double comparison in the common case and use the most efficient representation.
         auto dur = std::chrono::duration<double, std::milli>(clock - start);
-        log.debug("{}: {}ms", this->name.str, dur.count());
+        const auto msCount = dur.count();
+        log.debug("{}: {}ms", this->name.str, msCount);
         sorbet::timingAdd(this->name, start, clock, move(args), move(tags), self, prev);
+
+        if (!histogramBuckets.empty()) {
+            // Find the bucket for this value. The last bucket is INT_MAX, so it's guaranteed to pick one.
+            for (const auto bucket : histogramBuckets) {
+                if (msCount < bucket) {
+                    prodHistogramInc(name, bucket);
+                    break;
+                }
+            }
+        }
     }
 }
 
