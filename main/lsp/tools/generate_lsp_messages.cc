f58c6619a8229283e00492a000c32bad9bb38556
John Vilk <jvilk@stripe.com>
2020-03-09 11:10:33 -0700

    Clean up `generate_lsp_messages.cc`c

--- main/lsp/tools/generate_lsp_messages.cc
+++ main/lsp/tools/generate_lsp_messages.cc
@@ -21,48 +21,56 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    fmt::memory_buffer headerBuffer;
-    fmt::memory_buffer classFileBuffer;
-    fmt::memory_buffer enumHeaderBuffer;
-    fmt::memory_buffer enumClassFileBuffer;
-
-    fmt::format_to(enumClassFileBuffer, "#include \"main/lsp/json_types.h\"\n");
-    fmt::format_to(enumClassFileBuffer, "#include \"main/lsp/lsp_messages_gen_helpers.h\"\n");
-    fmt::format_to(enumClassFileBuffer, "namespace sorbet::realmain::lsp {{\n");
-
-    fmt::format_to(classFileBuffer, "#include \"main/lsp/json_types.h\"\n");
-    fmt::format_to(classFileBuffer, "#include \"main/lsp/lsp_messages_gen_helpers.h\"\n");
-    fmt::format_to(classFileBuffer, "namespace sorbet::realmain::lsp {{\n");
-
     vector<std::shared_ptr<JSONClassType>> enumTypes;
     vector<std::shared_ptr<JSONObjectType>> classTypes;
     makeLSPTypes(enumTypes, classTypes);
 
-    // Emits enums before class definitions themselves.
-    for (auto &enumType : enumTypes) {
-        enumType->emit(enumHeaderBuffer, enumClassFileBuffer);
-    }
-    for (auto &classType : classTypes) {
-        classType->emit(headerBuffer, classFileBuffer);
-    }
-    fmt::format_to(classFileBuffer, "}}\n");
-    fmt::format_to(enumClassFileBuffer, "}}\n");
+    // Emit enums.
+    {
+        fmt::memory_buffer enumHeaderBuffer;
+        fmt::memory_buffer enumClassFileBuffer;
 
-    // Output buffers to files.
-    if (!writeFile(argv[1], headerBuffer)) {
-        return 1;
+        fmt::format_to(enumClassFileBuffer, "#include \"main/lsp/json_types.h\"\n");
+        fmt::format_to(enumClassFileBuffer, "#include \"main/lsp/lsp_messages_gen_helpers.h\"\n");
+        fmt::format_to(enumClassFileBuffer, "namespace sorbet::realmain::lsp {{\n");
+
+        // Emits enums before class definitions themselves.
+        for (auto &enumType : enumTypes) {
+            enumType->emit(enumHeaderBuffer, enumClassFileBuffer);
+        }
+        fmt::format_to(enumClassFileBuffer, "}}\n");
+
+        if (!writeFile(argv[3], enumHeaderBuffer)) {
+            return 1;
+        }
+
+        if (!writeFile(argv[4], enumClassFileBuffer)) {
+            return 1;
+        }
     }
 
-    if (!writeFile(argv[2], classFileBuffer)) {
-        return 1;
-    }
+    // Emit classes.
+    {
+        fmt::memory_buffer headerBuffer;
+        fmt::memory_buffer classFileBuffer;
 
-    if (!writeFile(argv[3], enumHeaderBuffer)) {
-        return 1;
-    }
+        fmt::format_to(classFileBuffer, "#include \"main/lsp/json_types.h\"\n");
+        fmt::format_to(classFileBuffer, "#include \"main/lsp/lsp_messages_gen_helpers.h\"\n");
+        fmt::format_to(classFileBuffer, "namespace sorbet::realmain::lsp {{\n");
 
-    if (!writeFile(argv[4], enumClassFileBuffer)) {
-        return 1;
+        for (auto &classType : classTypes) {
+            classType->emit(headerBuffer, classFileBuffer);
+        }
+        fmt::format_to(classFileBuffer, "}}\n");
+
+        // Output buffers to files.
+        if (!writeFile(argv[1], headerBuffer)) {
+            return 1;
+        }
+
+        if (!writeFile(argv[2], classFileBuffer)) {
+            return 1;
+        }
     }
 
     return 0;
