a806326b07d25732b3f16f510c485316216f9e9d
Jake Zimmerman <zimmerman.jake@gmail.com>
2020-03-19 12:43:48 -0500

    Delete out of date comment (#2789)
    The code that the comment was referencing was deleted in commit:
    
    ddfcab29a0b741d4417bee37d9740f419fa402eb

--- main/main.cc
+++ main/main.cc
@@ -3,7 +3,6 @@
 #include "main/realmain.h"
 int main(int argc, char *argv[]) {
     try {
-        // Initialize the symbolizer to get a human-readable stack trace
         return sorbet::realmain::realmain(argc, argv);
     } catch (sorbet::realmain::options::EarlyReturnWithCode &c) {
         return c.returnCode;
