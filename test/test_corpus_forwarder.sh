6290198ec288f61dd0f4af8bf1b0dd0730674c29
Trevor Elliott <awesomelyawesome@gmail.com>
2020-03-31 12:38:04 -0700

    Allow tests to be run from outside of sorbet

--- test/test_corpus_forwarder.sh
+++ test/test_corpus_forwarder.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-set -eux
 
-test/test_corpus_runner "$@"
+# --- begin runfiles.bash initialization v2 ---
+# Copy-pasted from the Bazel Bash runfiles library v2.
+set -uo pipefail; f=bazel_tools/tools/bash/runfiles/runfiles.bash
+# shellcheck disable=SC1090
+source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "${RUNFILES_MANIFEST_FILE:-/dev/null}" | cut -f2- -d' ')" 2>/dev/null || \
+  source "$0.runfiles/$f" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "$0.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+  { echo>&2 "ERROR: cannot find $f"; exit 1; }; f=; set -e
+# --- end runfiles.bash initialization v2 ---
+
+"$(rlocation com_stripe_ruby_typer/test/test_corpus_runner)" "$@"
