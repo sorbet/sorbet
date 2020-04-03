aa33c6a6e6abaf87f62fad0f8952ff013f9b24e5
Trevor Elliott <awesomelyawesome@gmail.com>
2020-04-03 10:09:28 -0700

    Rewrite uses of sig to T::Sig::WithoutRuntime.sig (#2836)
    * Add SigRewriter
    
    * Manually update errors from tests
    
    * Update exp files

--- test/testdata/core/fuzz_bad_subtyping.rb
+++ test/testdata/core/fuzz_bad_subtyping.rb
@@ -5,8 +5,6 @@ module MyEnumerable
 extend T::Generic
 A = type_member
   sig {params(a: MyEnumerable[])} # error: Malformed `sig`: No return type specified. Specify one with .returns()
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ error: Method `sig` does not exist on `T.class_of(MyEnumerable)`
-     # ^^^^^^^^^^^^^^^^^^^^^^^^^ error: Method `params` does not exist on `T.class_of(MyEnumerable)`
                # ^^^^^^^^^^^^^^ error-with-dupes: Wrong number of type parameters for `MyEnumerable`.
   def-(a) end
  class MySet
--- test/testdata/rewriter/default_args_malformed_sig.rb
+++ test/testdata/rewriter/default_args_malformed_sig.rb
@@ -3,6 +3,6 @@
 # This test used to crash the DefaultArgs pass because it tried to dereference
 # the block pointer to get it's body, but the block itself was nullptr.
 
-sig # error: does not exist
+sig # error: `sig` requires a block parameter
 def foo(x: nil)
 end
