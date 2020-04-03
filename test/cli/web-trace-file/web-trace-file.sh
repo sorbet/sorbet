072f3be45246acca96e8b30593a3f1a0050294d3
Jake Zimmerman <zimmerman.jake@gmail.com>
2020-04-03 14:21:32 -0700

    Add failing test

---
+++ test/cli/web-trace-file/web-trace-file.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -euo pipefail
+
+trace_json="$(mktemp)"
+# shellcheck disable=SC2064
+trap "rm -f '$trace_json'" EXIT
+
+main/sorbet --silence-dev-message --web-trace-file="$trace_json" -e 0 2>&1
+
+if [ -s "$trace_json" ]; then
+  echo 'Web trace file exists and has size greater than zero.'
+else
+  echo 'Web trace file does not exist or has size zero!'
+fi
