f847e6c9450b3a07083aa5b1c0cb60e6ea1a0ba4
Jake Zimmerman <zimmerman.jake@gmail.com>
2020-04-13 21:15:17 -0700

    Reformat

--- test/testdata/resolver/sealed_redeclare__1.rb
+++ test/testdata/resolver/sealed_redeclare__1.rb
@@ -7,7 +7,9 @@ end
 
 class A
   extend T::Helpers
-  # This is an error in the runtime, but we haven't put the time to get an error here statically because it's tricky to get to work in incremental resolve.
+  # This is an error in the runtime, but we haven't put the time to get an
+  # error here statically because it's tricky to get to work in incremental
+  # resolve.
   sealed!
 end
 
--- test/testdata/resolver/sealed_redeclare__2.rb
+++ test/testdata/resolver/sealed_redeclare__2.rb
@@ -2,6 +2,8 @@
 
 class B
   extend T::Helpers
-  # This is an error in the runtime, but we haven't put the time to get an error here statically because it's tricky to get to work in incremental resolve.
+  # This is an error in the runtime, but we haven't put the time to get an
+  # error here statically because it's tricky to get to work in incremental
+  # resolve.
   sealed!
 end
