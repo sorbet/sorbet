9273fa4fa46b7b5e75ea5b7e23f0af107d5bf3d6
Jake Zimmerman <zimmerman.jake@gmail.com>
2020-04-17 12:02:00 -0500

    Remove legacy handling for non-standard prop type syntax (#2894)
    * Remove legacy handling for non-standard prop type syntax
    
    It used to be possible to specify the `type` of a prop in multiple ways:
    
    - as a positional argument
    - in the rules hash as `type:`
    - in the rules hash implicitly as `array:`
    
    Only the first form above is currently in use.
    
    It looks like there also used to be some handling for `T.coerce`, but
    pay-server doesn't have a single `prop` definition which references
    `T.coerce`.
    
    * no-op: Reorganize the tests before changing Prop.cc
    
    * Re-record not_prop test

---
+++ test/testdata/rewriter/not_prop.rb
@@ -0,0 +1,10 @@
+# typed: true
+
+class ThingsWhichUsedToBePropSyntax
+  prop :type, type: String # error: `prop` does not exist
+  prop :object # error: `prop` does not exist
+  prop :array_of, array: String # error: `prop` does not exist
+  prop :array_of_explicit, Array, array: String
+  prop :no_class_arg, type: Array, immutable: true, array: String # error: `prop` does not exist
+  prop :proc_type, type: T.proc.params(x: Integer).void # error: `prop` does not exist
+end
--- test/testdata/rewriter/prop_proc_regression.rb
+++ test/testdata/rewriter/prop_proc_regression.rb
@@ -2,7 +2,6 @@
 
 class Foo < T::Struct
     prop :proc, T.proc.params(x: Integer).void
-    prop :proc_type, type: T.proc.params(x: Integer).void
 end
 
-T.reveal_type(Foo.new(proc: lambda {|x| nil }, proc_type: lambda {|x| nil})) # error: Revealed type: `Foo`
+T.reveal_type(Foo.new(proc: lambda {|x| nil })) # error: Revealed type: `Foo`
