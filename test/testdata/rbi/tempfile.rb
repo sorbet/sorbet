90d1f797e67bf5b6bf4b61b120d5d4664379353b
avaynshtok <avaynshtok@gmail.com>
2020-04-20 12:44:09 -0700

    fix Tempfile rbi (#2897)
    * fix Tempfile rbi
    
    For Tempfile initialize, open and create:
    * make mode a keyword parameter
    * make options a rest parameter
    * remove overloading to work around 5003 error per sorbet/sorbet#2248
    
    * loosen return type of Tempfile create and open

---
+++ test/testdata/rbi/tempfile.rb
@@ -0,0 +1,45 @@
+# typed: true
+
+require 'tempfile'
+
+Tempfile.new('hello')
+Tempfile.new(['hello', '.jpg'])
+Tempfile.new('hello', '/tmp')
+Tempfile.new('hello', '/tmp', encoding: 'ascii-8bit')
+Tempfile.new('hello', '/tmp', mode: 0)
+Tempfile.new('hello', '/tmp', mode: 0, encoding: 'ascii-8bit')
+
+Tempfile.create('hello')
+Tempfile.create(['hello', '.jpg'])
+Tempfile.create('hello', '/tmp')
+Tempfile.create('hello', '/tmp', encoding: 'ascii-8bit')
+Tempfile.create('hello', '/tmp', mode: 0)
+Tempfile.create('hello', '/tmp', mode: 0, encoding: 'ascii-8bit')
+
+Tempfile.create('hello') do; end
+Tempfile.create(['hello', '.jpg']) do; end
+Tempfile.create('hello', '/tmp') do; end
+Tempfile.create('hello', '/tmp', encoding: 'ascii-8bit') do; end
+Tempfile.create('hello', '/tmp', mode: 0) do; end
+Tempfile.create('hello', '/tmp', mode: 0, encoding: 'ascii-8bit') do; end
+
+Tempfile.open('hello')
+Tempfile.open(['hello', '.jpg'])
+Tempfile.open('hello', '/tmp')
+Tempfile.open('hello', '/tmp', encoding: 'ascii-8bit')
+Tempfile.open('hello', '/tmp', mode: 0)
+Tempfile.open('hello', '/tmp', mode: 0, encoding: 'ascii-8bit')
+
+Tempfile.open('hello') do; end
+Tempfile.open(['hello', '.jpg']) do; end
+Tempfile.open('hello', '/tmp') do; end
+Tempfile.open('hello', '/tmp', encoding: 'ascii-8bit') do; end
+Tempfile.open('hello', '/tmp', mode: 0) do; end
+Tempfile.open('hello', '/tmp', mode: 0, encoding: 'ascii-8bit') do; end
+
+# can return any value from block
+i = Tempfile.create('hello') { 1 }
+i + 1
+
+i = Tempfile.open('hello') { 1 }
+i + 1
