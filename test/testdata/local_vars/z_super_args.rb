761b1466587dccfe349f73fdb4ca8276dd5ecd84
Jake Zimmerman <zimmerman.jake@gmail.com>
2020-04-22 14:12:52 -0500

    Add z_super_args.rb index-tree test

---
+++ test/testdata/local_vars/z_super_args.rb
@@ -0,0 +1,63 @@
+# typed: true
+
+class Parent
+  def takes_positional(arg0)
+    p arg0
+  end
+
+  def takes_keyword(arg0:)
+    p arg0
+  end
+
+  def takes_two_keyword(arg0:, arg1:)
+    p arg0
+    p arg1
+  end
+
+  def takes_positional_then_keyword(arg0, arg1:)
+    p arg0
+    p arg1
+  end
+
+  def takes_rest(*args)
+    p args
+  end
+
+  def takes_keyword_rest(**args)
+    p args
+  end
+
+  def takes_block(&blk)
+    p blk
+  end
+end
+
+class Child < Parent
+  def takes_positional(arg0)
+    super
+  end
+
+  def takes_keyword(arg0:)
+    super
+  end
+
+  def takes_two_keyword(arg0:, arg1:)
+    super
+  end
+
+  def takes_positional_then_keyword(arg0, arg1:)
+    super
+  end
+
+  def takes_rest(*args)
+    super
+  end
+
+  def takes_keyword_rest(**args)
+    super
+  end
+
+  def takes_block(&blk)
+    super
+  end
+end
