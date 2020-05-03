d3fe7b310ea54ad20a89296ca640343059411087
Bo Stendal Sorensen <bo@stendal-sorensen.net>
2020-05-02 21:24:50 -0700

    Improve parsing of squiggly heredoc with string interpolation

---
+++ test/whitequark/test_bug_heredoc_squiggly_0.rb
@@ -0,0 +1,8 @@
+# typed: true
+
+str = <<~DESC
+        This\n\is a regular line
+        So\nis this
+        \n#{LOG_HEADER}.
+        the previous line however contains string interpolation
+      DESC
