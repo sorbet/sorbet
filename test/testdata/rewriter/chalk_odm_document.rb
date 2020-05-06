98178155643962adbf941105f1acf690b0dc04a1
Jake Zimmerman <zimmerman.jake@gmail.com>
2020-05-06 17:25:09 -0500

    Add a test for Chalk::ODM::Document
    This will change in a future PR but just wanted to have it here to show
    that the current PR doesn't affect this code.

---
+++ test/testdata/rewriter/chalk_odm_document.rb
@@ -0,0 +1,16 @@
+# typed: strict
+
+class MyParentDocument < Chalk::ODM::Document
+  prop :my_parent_method, String
+end
+
+class MyChildDocument < MyParentDocument
+  prop :my_child_method, Integer
+end
+
+# Uses T::Props::WeakConstructor, so these should be ok
+MyParentDocument.new
+MyChildDocument.new
+
+p MyParentDocument.new(my_parent_method: 'hello').my_parent_method
+p MyChildDocument.new(my_child_method: 42).my_child_method
