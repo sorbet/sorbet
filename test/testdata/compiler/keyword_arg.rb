46355d3c9539c512a44ed6d2ae97ed20b8bd8e00
Jake Zimmerman <zimmerman.jake@gmail.com>
2020-05-11 17:13:19 -0500

    Make keyword_arg test actually test something

--- test/testdata/compiler/keyword_arg.rb
+++ test/testdata/compiler/keyword_arg.rb
@@ -5,5 +5,5 @@ def my_name(name:, prefix: "Mr")
   prefix + " " + name
 end
 
-# puts my_name(name: "Paul", prefix: "Master")
-# puts my_name(name: "Paul")
+puts my_name(name: "Paul", prefix: "Master")
+puts my_name(name: "Paul")
