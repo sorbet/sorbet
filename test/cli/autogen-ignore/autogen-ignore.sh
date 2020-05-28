1cfc19475c95f991ad85fa53b0c4f38345b1ec42
Jake Zimmerman <zimmerman.jake@gmail.com>
2020-05-28 02:11:37 -0500

    Fix master (autogen-ignore stdout/stderr race) (#3099)

--- test/cli/autogen-ignore/autogen-ignore.sh
+++ test/cli/autogen-ignore/autogen-ignore.sh
@@ -2,4 +2,4 @@
 
 set -euo pipefail
 
-main/sorbet --silence-dev-message --stop-after=namer -p autogen test/cli/autogen-ignore/autogen-ignore.rb 2>&1
+main/sorbet --silence-dev-message --no-error-count --stop-after=namer -p autogen test/cli/autogen-ignore/autogen-ignore.rb 2>&1
