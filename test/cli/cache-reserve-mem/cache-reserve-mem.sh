3d16a5ab2d2c1497a53e9323b1027148b36941b0
John Vilk <jvilk@stripe.com>
2020-05-28 20:14:29 -0700

    Fix cache-reserve-mem test.

--- test/cli/cache-reserve-mem/cache-reserve-mem.sh
+++ test/cli/cache-reserve-mem/cache-reserve-mem.sh
@@ -5,6 +5,6 @@ cleanup() {
 }
 trap cleanup EXIT
 set -e
-main/sorbet --silence-dev-message -vvv --preallocate-symbol-size 4000 --preallocate-name-size 4000 --cache-dir "$dir" test/cli/cache-reserve-mem/input.rb
-main/sorbet --silence-dev-message -vvv --preallocate-symbol-size 4000 --preallocate-name-size 4000 --cache-dir "$dir" test/cli/cache-reserve-mem/input.rb
-echo "--preallocate-symbol-size --preallocate-name-size OK"
+main/sorbet --silence-dev-message -vvv --reserve-symbol-table-capacity 4000 --reserve-name-table-capacity 4000 --cache-dir "$dir" test/cli/cache-reserve-mem/input.rb
+main/sorbet --silence-dev-message -vvv --reserve-symbol-table-capacity 4000 --reserve-name-table-capacity 4000 --cache-dir "$dir" test/cli/cache-reserve-mem/input.rb
+echo "--reserve-symbol-table-capacity --reserve-name-table-capacity OK"
