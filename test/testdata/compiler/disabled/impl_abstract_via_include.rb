04246165e92f4156f2d7f65a2bcdb33c60ec13bb
Jake Zimmerman <zimmerman.jake@gmail.com>
2020-06-05 19:22:59 -0500

    Add yet another disabled repro for lib/flag/ failure

---
+++ test/testdata/compiler/disabled/impl_abstract_via_include.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+# typed: true
+# compiled: true
+
+module IFoo
+  extend T::Sig
+  extend T::Helpers
+  interface!
+
+  sig {abstract.returns(Integer)}
+  def foo; end
+end
+
+module FooImpl
+  extend T::Sig
+  sig {returns(Float)}
+  def foo; 0.1617; end
+end
+
+class Foo
+  include FooImpl
+  include IFoo
+end
+
+p Foo.new.foo
