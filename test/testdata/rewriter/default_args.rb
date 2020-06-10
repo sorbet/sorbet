f282b7f3dd467c719455b2c316d938f7554232c6
Trevor Elliott <awesomelyawesome@gmail.com>
2020-06-10 04:28:50 +0000

    Update the default_args test
    The added case with `attr_reader` would have exposed the bug that this
    branch corrects.

--- test/testdata/rewriter/default_args.rb
+++ test/testdata/rewriter/default_args.rb
@@ -1,6 +1,22 @@
 # typed: true
 
-extend T::Sig
-sig {params(a: String, b: Integer, c: Integer).void}
-def foo(a, b=1, c=2)
+class A
+  extend T::Sig
+
+  sig {params(a: String, b: Integer, c: Integer).void}
+  def foo(a, b=1, c=2)
+  end
+end
+
+class B
+  extend T::Sig
+
+  sig {returns(String)}
+  attr_reader :foo
+
+  sig {params(x: String).void}
+  def initialize(x="")
+    @foo = "test"
+    @x = x
+  end
 end
