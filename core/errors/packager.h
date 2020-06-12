eff42e682d2472451a3e4f439872be2503ccbdb1
John Vilk <jvilk@stripe.com>
2020-06-11 17:31:41 -0700

    Extra tests and errors.

--- core/errors/packager.h
+++ core/errors/packager.h
@@ -5,11 +5,12 @@
 namespace sorbet::core::errors::Packager {
 constexpr ErrorClass MustBeTypedStrict{8001, StrictLevel::False};
 constexpr ErrorClass InvalidPackageDefinition{8002, StrictLevel::False};
-constexpr ErrorClass DuplicatePackageName{8003, StrictLevel::False};
+constexpr ErrorClass RedefinitionOfPackage{8003, StrictLevel::False};
 constexpr ErrorClass PackageNotFound{8004, StrictLevel::False};
 constexpr ErrorClass UnpackagedFile{8005, StrictLevel::False};
 constexpr ErrorClass InvalidImportOrExport{8006, StrictLevel::False};
 constexpr ErrorClass MultiplePackagesInOneFile{8007, StrictLevel::False};
 constexpr ErrorClass MultipleExportMethodsCalls{8008, StrictLevel::False};
+constexpr ErrorClass NoSelfImport{8009, StrictLevel::False};
 } // namespace sorbet::core::errors::Packager
 #endif
---
+++ test/testdata/packager/duplicate_package_name/a/__package.rb
@@ -0,0 +1,6 @@
+# typed: strict
+# enable-packager: true
+
+class A < PackageSpec
+
+end
