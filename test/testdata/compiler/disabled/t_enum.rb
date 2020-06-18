a6ea453c2921e964f547bd0e2dc3ed97d8e9af49
Jake Zimmerman <zimmerman.jake@gmail.com>
2020-06-18 14:20:17 -0700

    Add some disabled tests

--- test/testdata/compiler/disabled/t_enum.rb
+++ test/testdata/compiler/disabled/t_enum.rb
@@ -2,10 +2,14 @@
 # typed: strict
 # compiled: true
 
+# TODO(jez) There are many tests in sorbet (test/testdata/rewriter/t_enum*.rb)
+# They could all probably be compiler tests.
 class MyEnum < T::Enum
   enums do
     X = new
+    Y = new
   end
 end
 
-puts 'required successfully'
+p MyEnum::X
+p MyEnum::Y
