64f04cc05179b315f0cbaeafa067cba57b54880f
Raphael Sousa Santos <raphael@sonologi.co>
2020-06-22 05:16:02 +0200

    Fix AttrReader rewriter for sigs with .checked. Fixes #3104
    Makes the rewriter behavior be triggered properly not only for
    sig {returns(...)}, but also for sig's using checked and on_failure.
    
    This is done by generalizing castSig in rewriter rewriter/Utils.cc and
    making the necessary adjustments to the calling code.

---
+++ test/testdata/rewriter/attr_checked.rb
@@ -0,0 +1,41 @@
+# typed: strict
+class TestAttr
+  extend T::Sig
+
+  sig {returns(String).checked(:always)}
+  attr_accessor :str1
+
+  sig {returns(String).checked(:always)}
+  attr_reader :str2
+
+  sig {params(str3:String).returns(String).checked(:always)}
+  attr_writer :str3
+
+  sig {returns(String).on_failure(:raise)}
+  attr_accessor :str4
+
+  sig {returns(String).checked(:always).on_failure(:raise)}
+  attr_accessor :str5
+
+  sig {void.checked(:always)}
+  attr_reader :str6
+
+  sig {params(str7:String).void.checked(:always).on_failure(:raise)}
+  attr_writer :str7
+
+  sig {void.on_failure(:raise)}
+  attr_accessor :str8 # error: This function does not have a `sig`
+
+  sig {void}
+  def initialize
+    @str1 = T.let('', String)
+    @str2 = T.let('', String)
+    @str3 = T.let('', String)
+    @str4 = T.let('', String)
+    @str5 = T.let('', String)
+    @str6 = T.let('', String)
+    @str7 = T.let('', String)
+    @str8 = T.let('', String)
+  end
+
+end
