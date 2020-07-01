8a001be15f71d7847c8d7d63a6fed5e9472ca2dc
Dave Ungerer <dave@simplepay.co.za>
2020-07-01 22:28:53 +0800

    Added tests

--- test/testdata/rbi/enumerable.rb
+++ test/testdata/rbi/enumerable.rb
@@ -19,3 +19,15 @@
 [1, 3, 20].sort_by {|n| [n.to_s, [1, 2]]}
 
 T.assert_type!([1].lazy, Enumerator::Lazy[Integer])
+
+# There are 3 different ways to call all?, any? and none?
+a = [1, 3, 20]
+a.all?
+a.all?(1)
+a.all? { |i| i }
+a.any?
+a.any?(1)
+a.any? { |i| i }
+a.none?
+a.none?(1)
+a.none? { |i| i }
