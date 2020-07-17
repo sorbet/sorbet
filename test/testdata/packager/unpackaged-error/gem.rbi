a7508844c37995d1cb80bc2b7a9a7735765b5e26
John Vilk <43145439+jvilk-stripe@users.noreply.github.com>
2020-07-17 15:59:04 -0700

    Make unpackaged-error test deterministic by porting it over to a testdata test. (#3298)

--- test/testdata/packager/unpackaged-error/gem.rbi
+++ test/testdata/packager/unpackaged-error/gem.rbi
@@ -1,3 +1,4 @@
+# error: File `test/testdata/packager/unpackaged-error/gem.rbi` does not belong to a package
 # typed: true
 
 # rbi files should not be exempt from packaging rules -- this will also error with an unpackaged error.
--- test/testdata/packager/unpackaged-error/unpackaged-error.rb
+++ test/testdata/packager/unpackaged-error/unpackaged-error.rb
@@ -1,4 +1,6 @@
+# error: File `test/testdata/packager/unpackaged-error/unpackaged-error.rb` does not belong to a package
 # typed: true
+# enable-packager: true
 
 # Doesn't matter what the contents are. This file is unpackaged and will have an error.
 # This is not in test/testdata since we that test runner can't make an assertion on the first line of a file, which is
