565bf28e29106a5d3df390b3cd365f821b3c304a
Giovanni Kock Bonetti <giovanni.bonetti@gmail.com>
2020-07-20 14:57:46 -0300

    Added test for Range#bsearch

--- test/testdata/rbi/range.rb
+++ test/testdata/rbi/range.rb
@@ -13,5 +13,6 @@ Range.new(0.5, 2.5).step(2+1i) # error: Expected `T.any(Integer, Float, Rational
 (:a...:z)
 
 # bsearch
+T.reveal_type((0.0..10.0).bsearch {|x| x >= 3}) # error: Revealed type: `T.nilable(Float)`
 T.reveal_type((1..10).bsearch {|x| x == 1}) # error: Revealed type: `T.nilable(Integer)`
 T.reveal_type((1..10).bsearch {|x| x }) # error: Revealed type: `T.nilable(Integer)`
