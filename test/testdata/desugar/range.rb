bd7b67f5d825b795f7b47923ca1cf97685767b54
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2020-08-12 18:43:06 -0400

    Fix expected types for literal ranges of literals
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

--- test/testdata/desugar/range.rb
+++ test/testdata/desugar/range.rb
@@ -8,15 +8,15 @@ def foo
     f = ('a'..'z').last
 
     T.reveal_type(a) # error: Revealed type: `T::Range[Integer]`
-    T.reveal_type(b) # error: Revealed type: `T::Range[Integer]`
+    T.reveal_type(b) # error: Revealed type: `T::Range[T.untyped]`
     T.reveal_type(c) # error: Revealed type: `T::Range[Integer]`
-    T.reveal_type(d) # error: Revealed type: `T::Range[Integer]`
+    T.reveal_type(d) # error: Revealed type: `T::Range[T.untyped]`
     T.reveal_type(e) # error: Revealed type: `Integer`
     T.reveal_type(f) # error: Revealed type: `String`
 
     # testing for endless ranges
     g = (1..)
     h = Range.new(1, nil)
-    T.reveal_type(h) # error: Revealed type: `T::Range[Integer(1)]`
     T.reveal_type(g) # error: Revealed type: `T::Range[Integer]`
+    T.reveal_type(h) # error: Revealed type: `T::Range[T.untyped]`
 end
