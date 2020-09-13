cfbb42ff7214d65a9f1bb593d48ab792c52d1726
David Judd <djudd@stripe.com>
2020-09-12 21:43:08 -0700

    rubocop_todo: Style/InverseMethods

--- gems/sorbet-runtime/lib/types/configuration.rb
+++ gems/sorbet-runtime/lib/types/configuration.rb
@@ -338,7 +338,7 @@ module T::Configuration
     if values.nil?
       @scalar_types = values
     else
-      bad_values = values.select {|v| v.class != String}
+      bad_values = values.reject {|v| v.class == String}
       unless bad_values.empty?
         raise ArgumentError.new("Provided values must all be class name strings.")
       end
