18aa0e5edea421392d124714be2d8d1ceb5e6b1b
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2020-09-24 15:47:07 -0400

    Cover more options in `test/cli/phases`
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

--- test/cli/phases/phases.sh
+++ test/cli/phases/phases.sh
@@ -5,7 +5,7 @@ rm -rf out fileout
 mkdir -p out fileout
 
 # Make sure these options don't change output
-for p in parse-tree parse-tree-json desugar-tree desugar-tree-raw rewrite-tree rewrite-tree-raw index-tree index-tree-raw name-tree name-tree-raw resolve-tree resolve-tree-raw flatten-tree flatten-tree-raw ast ast-raw cfg cfg-raw symbol-table symbol-table-raw; do
+for p in parse-tree parse-tree-json parse-tree-whitequark desugar-tree desugar-tree-raw rewrite-tree rewrite-tree-raw index-tree index-tree-raw name-tree name-tree-raw resolve-tree resolve-tree-raw flatten-tree flatten-tree-raw ast ast-raw cfg cfg-raw symbol-table symbol-table-raw; do
     echo "--- $p start ---"
     main/sorbet --silence-dev-message --censor-for-snapshot-tests -p "$p" -e '1' | tee "out/$p"
     main/sorbet --silence-dev-message --censor-for-snapshot-tests -p "$p:fileout/$p" -e '1' > /dev/null
@@ -17,12 +17,21 @@ echo "--- checking diff ---"
 diff -r out fileout | sort
 
 echo "--- checking crashes ---"
-# Makes sure all these options don't crash us
-for p in symbol-table-json symbol-table-full symbol-table-full-raw symbol-table-full-json; do
+
+# Test --print symbol-table-* options don't crash
+for p in symbol-table symbol-table-json symbol-table-proto symbol-table-raw symbol-table-messagepack; do
+    main/sorbet --silence-dev-message -p "$p" -e '1' > /dev/null
+    main/sorbet --silence-dev-message -p "$p" --print-full -e '1' > /dev/null
+done
+
+# Test --print file-table-* options don't crash
+for p in file-table-json file-table-proto file-table-messagepack; do
     main/sorbet --silence-dev-message -p "$p" -e '1' > /dev/null
 done
 
+# Test --stop-after
 for p in init parser desugarer dsl namer resolver cfg inferencer; do
     main/sorbet --silence-dev-message --stop-after $p -e '1'
 done
+
 echo "--- checking crashes end ---"
