4c46f47120505de8f42b3ec6d3cd15aa792eea28
Stan Lo <stan001212@gmail.com>
2020-09-25 20:16:49 +0800

    Update Etc.uname's type

--- test/testdata/rbi/etc.rb
+++ test/testdata/rbi/etc.rb
@@ -4,3 +4,5 @@ T.reveal_type(Etc.getpwuid) # error: Revealed type: `T.nilable(Etc::Passwd)`
 T.reveal_type(Etc.getpwuid(10)) # error: Revealed type: `T.nilable(Etc::Passwd)`
 
 Etc.getpwuid("foo") # error: Expected `Integer` but found `String("foo")`
+
+T.reveal_type(Etc.uname) # error: Revealed type: `T::Hash[Symbol, String]`
