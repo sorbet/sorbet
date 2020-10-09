26824700f0a1713e491c6b6f0799385ed6ec518b
Trevor Elliott <awesomelyawesome@gmail.com>
2020-10-09 21:13:46 +0000

    Add autocorrect tests for keyword args

---
+++ test/cli/autocorrect-kwargs/autocorrect-kwargs.rb
@@ -0,0 +1,23 @@
+# typed: true
+
+extend T::Sig
+
+# Wrap curly braces around the keyword args in returns to make them into a shape
+sig {returns(foo: Integer, bar: String)}
+def test1
+  {foo: 10, bar: "hi"}
+end
+
+# Wrap curly braces around the keyword args to [] to make the elemnt of the
+# array into a shape
+sig {returns(T::Array[foo: Integer])}
+def test2
+  []
+end
+
+foo = T::Array[foo: Integer, bar: String].new
+
+# Remove the curly braces from the params list
+sig {params({a: Integer, b: String}).void}
+def test3(a, b)
+end
