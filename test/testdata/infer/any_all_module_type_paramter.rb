6139472711bc49b35fb5c77b58127882e023c123
Jake Zimmerman <zimmerman.jake@gmail.com>
2020-10-16 15:10:45 -0700

    Add minimized test case for bug that we accidentally fixed
    The changes in #3505 accidentally fixed Sorbet's behavior on this file.
    The old behavior did not report an error. I'm not sure why, but I think
    it has something to do with the changes to `calls.cc` (changing
    untyped -> noSymbol).
    
    In any case, it's a nice edge case with union types and intersection
    tyoes and generics so I figured it might come in handy in preventing
    future regressions.

---
+++ test/testdata/infer/any_all_module_type_paramter.rb
@@ -0,0 +1,18 @@
+# typed: true
+extend T::Sig
+
+module M; end
+class A; end
+
+sig do
+  type_parameters(:T)
+    .params(xs: T::Array[T.any(A, T.all(T.type_parameter(:T), M))])
+    .returns(T.nilable(T.any(A, T.all(T.type_parameter(:T), M))))
+end
+def self.foo(xs)
+  xs.find do |x|
+    if x.is_a?(A) # error: `is_a?` does not exist on `M` component
+      puts x
+    end
+  end
+end
