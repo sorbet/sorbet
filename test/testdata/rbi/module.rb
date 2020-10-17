a2da72276157c03c3f968934f2f0ffb418ef55dd
ohbarye <over.rye@gmail.com>
2020-10-17 16:58:12 +0900

    Add Module#const_source_location definition
    Feature: https://bugs.ruby-lang.org/issues/10771
    Document: https://www.rubydoc.info/stdlib/core/Module:const_source_location

--- test/testdata/rbi/module.rb
+++ test/testdata/rbi/module.rb
@@ -2,3 +2,8 @@
 
 String.module_eval { puts self }
 Enumerable.class_eval { |mod| puts mod }
+
+T.assert_type!(Module.const_source_location('IO'), T.nilable([String, Integer]))
+T.assert_type!(Module.const_source_location('Zlib'), T.nilable([String, Integer]))
+T.assert_type!(Module.const_source_location('Zlib', true), T.nilable([String, Integer]))
+T.assert_type!(Module.const_source_location(:Zlib, false), T.nilable([String, Integer]))
