f0dd1602e74bec313032afd8fc1cb2a1ff9bc787
John Vilk <jvilk@stripe.com>
2020-10-19 05:07:37 +0000

    UIntSet::forEach - check 8 bits at a time to try to speed up forEach loop

--- common/UIntSetForEach.h
+++ common/UIntSetForEach.h
@@ -9,12 +9,18 @@ template <typename F> void UIntSet::forEach(F each) const {
     for (auto entry : _members) {
         u4 startIdForNextU4 = id + 32;
         while (entry != 0) {
-            // Shift until entry is 0.
-            if ((entry & 0x1) == 1) {
-                each(id);
+            u4 startIdForNextU1 = id + 8;
+            // Process 1 byte at a time so we can check 8 places at once.
+            u4 byte = entry & 0xFF;
+            while (byte != 0) {
+                if ((byte & 0x1) == 1) {
+                    each(id);
+                }
+                byte >>= 1;
+                id++;
             }
-            entry >>= 1;
-            id++;
+            entry >>= 8;
+            id = startIdForNextU1;
         }
         id = startIdForNextU4;
     }
