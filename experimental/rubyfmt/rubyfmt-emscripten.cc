f90539f1e2e70f62e7e37f547053f768a3e4c1f0
John Vilk <jvilk@stripe.com>
2020-10-22 02:57:36 +0000

    Fix emscripten build

--- experimental/rubyfmt/rubyfmt-emscripten.cc
+++ experimental/rubyfmt/rubyfmt-emscripten.cc
@@ -6,7 +6,7 @@ namespace experimental::rubyfmt {
 
 RubyfmtResult format(string_view str) {
     // no-op in emscripten to avoid pulling in rubyfmt dependency.
-    return {Rubyfmt_FormatError::RUBYFMT_FORMAT_ERROR_OK, string(str)};
+    return {RubyfmtFormatError::RUBYFMT_FORMAT_ERROR_OK, string(str)};
 }
 
 } // namespace experimental::rubyfmt
