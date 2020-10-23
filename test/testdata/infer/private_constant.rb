69fa1264d595f5e6ee4663c4b43c126c96b9a511
Bo Sorensen <bo@stendal-sorensen.net>
2020-10-23 04:45:40 +0000

    Support private_constant

--- test/testdata/infer/private_constant.rb
+++ test/testdata/infer/private_constant.rb
@@ -14,9 +14,42 @@ module Foo
   class AnotherPrivateClass; end
   private_constant 'AnotherPrivateClass'
 
-  module PrivateModule; end
+  module PrivateModule
+    def self.ok_private_usage
+      A_PUBLIC_CONST
+      A_PRIVATE_CONST
+      PrivateClass
+      PrivateModule
+    end
+
+    class ClassInsidePrivateModule
+      def self.also_ok_private_usage; end
+    end
+  end
   private_constant :PrivateModule
 
   module AnotherPrivateModule; end
   private_constant 'AnotherPrivateModule'
+
+  def self.ok_private_usage
+    A_PUBLIC_CONST
+    A_PRIVATE_CONST
+    PrivateClass
+    PrivateModule.ok_private_usage
+    PrivateModule::ClassInsidePrivateModule.also_ok_private_usage
+  end
+
+  def self.not_ok_private_usage
+    ::Foo::A_PUBLIC_CONST
+    ::Foo::A_PRIVATE_CONST # error: private constant Foo::A_PRIVATE_CONST referenced
+    ::Foo::PrivateClass # error: private constant Foo::PrivateClass referenced
+    ::Foo::PrivateModule # error: private constant Foo::PrivateModule referenced
+  end
 end
+
+
+Foo::A_PUBLIC_CONST
+Foo::A_PRIVATE_CONST # error: private constant Foo::A_PRIVATE_CONST referenced
+Foo::PrivateClass # error: private constant Foo::PrivateClass referenced
+Foo::PrivateModule # error: private constant Foo::PrivateModule referenced
+Foo::PrivateModule::ClassInsidePrivateModule.also_ok_private_usage # error: private constant Foo::PrivateModule referenced
