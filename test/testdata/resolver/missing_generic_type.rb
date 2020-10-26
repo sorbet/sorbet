4ccb076f9422b17973746b63f1bf339b1474a6f9
Nathan Froyd <froydnj@gmail.com>
2020-10-26 09:35:09 -0400

    fix LSP test with magic

--- test/testdata/resolver/missing_generic_type.rb
+++ test/testdata/resolver/missing_generic_type.rb
@@ -1,4 +1,5 @@
 # typed: __STDLIB_INTERNAL
+# disable-fast-path: true
 
 class A # error: Type `Elem` declared by parent `Enumerable` must be re-declared in `A`
 # error: Missing definition for abstract method `Enumerable#each`
