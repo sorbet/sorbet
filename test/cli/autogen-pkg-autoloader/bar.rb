e6bd97432f44e90001d51a710c4a449739d2eb17
Getty Ritter <gdritter@infinitenegativeutility.com>
2020-10-27 18:39:08 -0700

    Add a packaged autoloader test

---
+++ test/cli/autogen-pkg-autoloader/bar.rb
@@ -0,0 +1,16 @@
+# typed: true
+
+module Yabba
+  module Dabba
+    class Quuz < AWS::String
+      p 'x'
+    end
+
+    class Jazz
+      class JazBaz
+        VALUE = 1
+        p 'x'
+      end
+    end
+  end
+end
