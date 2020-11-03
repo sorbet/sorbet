53eb510cfffdd1764ee70edc08c28d859b372bdb
Adam Procter <aprocter@stripe.com>
2020-11-03 14:26:22 -0800

    Update --error-kwarg-hash-without-splat CLI test

--- test/cli/error-kwarg-hash-without-splat/goodsplat.rb
+++ test/cli/error-kwarg-hash-without-splat/goodsplat.rb
@@ -7,9 +7,4 @@ def takes_kwargs(x, y:)
 end
 
 arghash = {y: 42}
-
-# NOTE: This currently fails with "Passing a hash where the specific keys are
-# unknown to a method taking keyword arguments". This is a bug in Sorbet. Once
-# that bug is fixed, goodsplat.rb should exhibit no errors, so the expected
-# output of this test will need to be updated and this comment removed.
 takes_kwargs(99, **arghash)
