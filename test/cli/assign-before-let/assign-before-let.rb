2ecf1e97d6e5575b60cf98c7bfc31debd7ef2449
Adam Procter <aprocter@stripe.com>
2020-11-09 14:03:51 -0800

    Add test case

---
+++ test/cli/assign-before-let/assign-before-let.rb
@@ -0,0 +1,4 @@
+# typed: true
+
+x = "hello"                   # Error on this line should include a link to GitHub issue
+x = T.let("goodbye", String)
---
+++ test/cli/assign-before-let/assign-before-let.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+main/sorbet --silence-dev-message test/cli/assign-before-let/assign-before-let.rb 2>&1
