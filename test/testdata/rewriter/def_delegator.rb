203c6815b3e862bee4750eefc710eb405158f095
Jeff Carbonella <jeff.carbonella@gusto.com>
2020-11-11 13:55:38 -0700

    Fix error expectations

--- test/testdata/rewriter/def_delegator.rb
+++ test/testdata/rewriter/def_delegator.rb
@@ -30,6 +30,8 @@ class GoodUsages
 end
 
 class WorksWithoutExtendingTSig
+  extend Forwardable
+
   def_delegator :thing, :foo
   def_delegator :thing, :bar, :aliased_bar
 end
@@ -39,14 +41,25 @@ class IgnoredUsages
 
   local = 0
   not_def_delegator :thing, :foo # error: Method `not_def_delegator` does not exist
-  def_delegator # error: Not enough arguments provided for method Forwardable#def_delegator. Expected: 2..3, got: 0
-  def_delegator :thing # error: Not enough arguments provided for method Forwardable#def_delegator. Expected: 2..3, got: 0
-  def_delegator :thing, :foo, :bar, :baz # error: Too many arguments provided for method Forwardable#def_delegator. Expected: 2..3, got: 4
-  def_delegator :thing, :foo, :bar, kwarg: :thing # error: Too many arguments provided for method Forwardable#def_delegator. Expected: 2..3, got: 4
-  def_delegator :thing, kwarg: :thing # error: Expected Symbol but found {kwarg: Symbol(:"thing")} for argument method
-  def_delegator :foo, kwarg1: :thing, kwarg2: local # error: Expected Symbol but found {kwarg1: Symbol(:"thing"), kwarg2: Integer(0)} for argument method
-  def_delegator :foo, local => :thing # error: Expected Symbol but found {} for argument method
-  def_delegator 234, :foo # error: Expected T.any(Symbol, String) but found Integer(234) for argument accessor
-  def_delegator :thing => :thing # error: Method `def_delegator` does not exist
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ error: Not enough arguments provided for method Forwardable#def_delegator. Expected: 2..3, got: 1
+  def_delegator # error: Not enough arguments provided for method `Forwardable#def_delegator`
+  def_delegator :thing # error: Not enough arguments provided for method `Forwardable#def_delegator`
+  def_delegator :thing, :foo, :bar, :baz # error: Too many arguments provided for method `Forwardable#def_delegator`
+  def_delegator :thing, :foo, :bar, kwarg: :thing # error: Too many arguments provided for method `Forwardable#def_delegator`
+  def_delegator :thing, kwarg: :thing # error: Expected `Symbol` but found `{kwarg: Symbol(:thing)}` for argument `method`
+  def_delegator :foo, kwarg1: :thing, kwarg2: local # error: Expected `Symbol` but found `{kwarg1: Symbol(:thing), kwarg2: Integer(0)}` for argument `method`
+  def_delegator :foo, local => :thing # error: Expected `Symbol` but found `{}` for argument `method`
+  def_delegator 234, :foo # error: Expected `T.any(Symbol, String)` but found `Integer(234)` for argument `accessor`
+  def_delegator :thing => :foo # error: Not enough arguments provided for method `Forwardable#def_delegator
+              # ^^^^^^^^^^^^^^ error: Expected `T.any(Symbol, String)` but found `{thing: Symbol(:foo)}` for argument `accessor`
+end
+
+class EnumerableUsage
+  extend T::Generic
+  extend Forwardable
+
+  include Enumerable
+
+  Elem = type_member
+
+  def_delegator :thing, :each
 end
