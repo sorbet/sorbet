d0fe5358b72eaddbc0fca82bba4c2ea27e32120f
Jake Zimmerman <zimmerman.jake@gmail.com>
2020-11-16 13:55:01 -0800

    Add test for method generics, and better error (#3658)
    * Add test for method generics, and better error
    
    * Don't use an overload, add some comments
    
    * Fix spacing
    
    * Add a cli test, and show the type var, not the symbol
    
    * Factor helper function

--- core/Error.cc
+++ core/Error.cc
@@ -53,9 +53,22 @@ string restoreColors(string_view formatted, rang::fg color) {
 string ErrorLine::toString(const GlobalState &gs, bool color) const {
     stringstream buf;
     string indent = "  ";
-    buf << indent << FILE_POS_STYLE << loc.filePosToString(gs) << RESET_STYLE << ":";
+    buf << indent;
+    switch (this->displayLoc) {
+        case LocDisplay::Shown:
+            buf << FILE_POS_STYLE << loc.filePosToString(gs) << RESET_STYLE << ":";
+            break;
+        case LocDisplay::Hidden:
+            break;
+    }
     if (!formattedMessage.empty()) {
-        buf << " ";
+        switch (this->displayLoc) {
+            case LocDisplay::Shown:
+                buf << " ";
+                break;
+            case LocDisplay::Hidden:
+                break;
+        }
         if (color) {
             buf << DETAIL_COLOR << restoreColors(formattedMessage, DETAIL_COLOR) << RESET_COLOR;
         } else {
