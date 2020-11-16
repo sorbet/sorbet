36a4ced9e0d982ecc5797bef2f11250d95fc9f89
Jake Zimmerman <zimmerman.jake@gmail.com>
2020-11-16 11:45:16 -0800

    Add test for private methods with T.any/T.all (#3663)
    This was already implemented correctly, but as far as I could tell it
    was not exercised by our test suite, so I figured I'd add a test.

---
+++ test/testdata/infer/private_methods_any_all.rb
@@ -0,0 +1,47 @@
+# typed: strict
+extend T::Sig
+
+class Left
+  extend T::Sig
+  sig {void}
+  private def foo; end
+end
+
+class Right
+  extend T::Sig
+  sig {void}
+  def foo; end
+end
+
+sig {params(left_or_right: T.any(Left, Right)).void}
+def test_left_or_right(left_or_right)
+  left_or_right.foo # error: Non-private call to private method `Left#foo`
+end
+
+sig {params(right_or_left: T.any(Right, Left)).void}
+def test_right_or_left(right_or_left)
+  right_or_left.foo # error: Non-private call to private method `Left#foo`
+end
+
+module PrivateBar
+  extend T::Sig
+  sig {void}
+  private def bar; end
+end
+
+module PublicQux
+  extend T::Sig
+  sig {void}
+  def qux; end
+end
+
+sig {params(bar_and_qux: T.all(PrivateBar, PublicQux)).void}
+def test_bar_and_qux(bar_and_qux)
+  bar_and_qux.bar # error: Non-private call to private method `PrivateBar#bar`
+end
+
+sig {params(qux_and_bar: T.all(PublicQux, PrivateBar)).void}
+def test_qux_and_bar(qux_and_bar)
+  qux_and_bar.bar # error: Non-private call to private method `PrivateBar#bar`
+end
+
