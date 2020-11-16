3480f625052ee6888672cc19e4aa70b95f2900e2
Jake Zimmerman <zimmerman.jake@gmail.com>
2020-11-16 10:22:45 -0800

    wip: add another test

---
+++ test/testdata/namer/zsuper_mangle_rename.rb
@@ -0,0 +1,19 @@
+# typed: true
+
+# Either both these cases should be mangle rename errors, or neither.
+# TODO(jez) symbol-table snapshot test for this
+
+class NoZSuperPrivatePublic
+  def foo; end
+
+  private :foo
+  public :foo
+end
+
+class Parent
+  def foo; end
+end
+class Child < Parent
+  private :foo
+  public :foo
+end
