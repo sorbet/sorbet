0cd052b5e6da4b7d22804736dd5ddf946c8c1f40
Jake Zimmerman <zimmerman.jake@gmail.com>
2020-11-16 00:28:20 -0800

    wip add test

---
+++ test/testdata/infer/private_method_zsuper_order.rb
@@ -0,0 +1,22 @@
+# typed: strict
+
+# This test would technically fail if run in Ruby directly, but this is more or
+# less representative of a situation where constants are autoloaded and these
+# class defs are in separate files.
+
+class Child < Parent
+  private_class_method :foo
+
+  T.reveal_type(foo) # error: Revealed type `Integer`
+end
+
+class Parent
+  extend T::Sig
+
+  sig {returns(Integer)}
+  def self.foo
+    0
+  end
+end
+
+Child.foo # error: Non-private call to private method `Child.foo`
