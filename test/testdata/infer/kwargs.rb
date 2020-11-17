03b60a985002d7916d087d375056d163708b1482
Trevor Elliott <awesomelyawesome@gmail.com>
2020-11-17 00:37:00 +0000

    Add the test from issue #3647

--- test/testdata/infer/kwargs.rb
+++ test/testdata/infer/kwargs.rb
@@ -14,3 +14,17 @@ end
 # Each repeated argument is checked as element type
 A.foo(x: 1, y: 2, z: '') # error: Expected `Integer` but found `String("")` for argument `kwargs`
 A.foo(x: 1, y: 2, z: 3)
+
+
+class B
+  extend T::Sig
+
+  sig {params(y: T::Hash[String,String], z: String).void}
+  def self.foo(y={}, z:)
+  end
+end
+
+# There is some tricky logic in dispatchCallSymbol to handle the case where a final positional hash argument is used for
+# keyword args. For the following case, the hash should be used for the defaulted positional arg, /not/ the implicit
+# keyword args hash.
+B.foo({ "a" => "foo" }) # error: Missing required keyword argument `z`
