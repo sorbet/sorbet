7e32f983c1cfbc888aa866be7561d1c42df8a691
Bo Sorensen <bo@stendal-sorensen.net>
2020-11-18 00:36:24 +0000

    Improve tests

--- test/testdata/infer/private_constant.rb
+++ test/testdata/infer/private_constant.rb
@@ -1,12 +1,20 @@
 # typed: true
 
 module Foo
+  extend T::Sig
+
   A_PUBLIC_CONST = 'public'
   A_PRIVATE_CONST = 'private'
   private_constant :A_PRIVATE_CONST
 
   ANOTHER_PRIVATE_CONST = true
   private_constant 'ANOTHER_PRIVATE_CONST'
+
+  PrivateIntTypeAlias = T.type_alias { Integer }
+  private_constant :PrivateIntTypeAlias
+
+  PrivateInt = Integer
+  private_constant :PrivateInt
 
   class PrivateClass; end
   private_constant :PrivateClass
@@ -45,6 +53,15 @@ module Foo
     ::Foo::PrivateClass # error: private constant Foo::PrivateClass referenced
     ::Foo::PrivateModule # error: private constant Foo::PrivateModule referenced
   end
+
+  sig { params(x: PrivateIntTypeAlias, y: PrivateInt).void }
+  def self.using_private_ints(x, y); end
+
+  sig { params(x: PrivateClass).void }
+  def self.using_private_class(x); end
+
+  sig { params(x: T.class_of(PrivateModule)).void }
+  def self.using_private_module(x); end
 end
 
 
@@ -53,3 +70,31 @@ Foo::A_PRIVATE_CONST # error: private constant Foo::A_PRIVATE_CONST referenced
 Foo::PrivateClass # error: private constant Foo::PrivateClass referenced
 Foo::PrivateModule # error: private constant Foo::PrivateModule referenced
 Foo::PrivateModule::ClassInsidePrivateModule.also_ok_private_usage # error: private constant Foo::PrivateModule referenced
+
+Foo.using_private_ints(1, 2)
+Foo.using_private_class(1) # error: Expected `Foo::PrivateClass` but found `Integer(1)` for argument `x`
+Foo.using_private_module(1) # error: Expected `T.class_of(Foo::PrivateModule)` but found `Integer(1)` for argument `x`
+
+extend T::Sig
+
+class PrivateTypeMember
+  extend T::Generic
+  extend T::Sig
+
+  Elem = type_member
+  private_constant :Elem
+
+  sig {params(x: Elem).void}
+  def foo(x); end
+
+  class B
+    extend T::Sig
+    sig {params(x: Elem).void}
+    #              ^^^^ error: `type_member` type `PrivateTypeMember::Elem` used outside of the class definition
+    def bar(x); end
+  end
+end
+
+sig {params(x: PrivateTypeMember::Elem).void}
+#              ^^^^^^^^^^^^^^^^^^^^^^^ error: `type_member` type `PrivateTypeMember::Elem` used outside of the class definition
+def foo(x); end
