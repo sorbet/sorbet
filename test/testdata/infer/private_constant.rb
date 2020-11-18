0574ed12120b596e34d2752ec9226e1817a9bb3e
Bo Sorensen <bo@stendal-sorensen.net>
2020-11-18 19:59:13 +0000

    Fix error message in test

--- test/testdata/infer/private_constant.rb
+++ test/testdata/infer/private_constant.rb
@@ -49,9 +49,9 @@ module Foo
 
   def self.not_ok_private_usage
     ::Foo::A_PUBLIC_CONST
-    ::Foo::A_PRIVATE_CONST # error: private constant Foo::A_PRIVATE_CONST referenced
-    ::Foo::PrivateClass # error: private constant Foo::PrivateClass referenced
-    ::Foo::PrivateModule # error: private constant Foo::PrivateModule referenced
+    ::Foo::A_PRIVATE_CONST # error: Non-private reference to private constant `Foo::A_PRIVATE_CONST` referenced
+    ::Foo::PrivateClass # error: Non-private reference to private constant `Foo::PrivateClass` referenced
+    ::Foo::PrivateModule # error: Non-private reference to private constant `Foo::PrivateModule` referenced
   end
 
   sig { params(x: PrivateIntTypeAlias, y: PrivateInt).void }
@@ -66,10 +66,10 @@ end
 
 
 Foo::A_PUBLIC_CONST
-Foo::A_PRIVATE_CONST # error: private constant Foo::A_PRIVATE_CONST referenced
-Foo::PrivateClass # error: private constant Foo::PrivateClass referenced
-Foo::PrivateModule # error: private constant Foo::PrivateModule referenced
-Foo::PrivateModule::ClassInsidePrivateModule.also_ok_private_usage # error: private constant Foo::PrivateModule referenced
+Foo::A_PRIVATE_CONST # error: Non-private reference to private constant `Foo::A_PRIVATE_CONST` referenced
+Foo::PrivateClass # error: Non-private reference to private constant `Foo::PrivateClass` referenced
+Foo::PrivateModule # error: Non-private reference to private constant `Foo::PrivateModule` referenced
+Foo::PrivateModule::ClassInsidePrivateModule.also_ok_private_usage # error: Non-private reference to private constant `Foo::PrivateModule` referenced
 
 Foo.using_private_ints(1, 2)
 Foo.using_private_class(1) # error: Expected `Foo::PrivateClass` but found `Integer(1)` for argument `x`
