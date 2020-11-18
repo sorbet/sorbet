f160686ac207fbf1f119d896302147bf638ee83a
Soam Vasani <soam@stripe.com>
2020-11-18 12:53:43 -0800

    test case that renames from a send

---
+++ test/testdata/lsp/rename/method_fromsend.rb
@@ -0,0 +1,30 @@
+# typed: true
+# frozen_string_literal: true
+
+module M
+  class Foo
+    def bar(a=1)
+    end
+
+    def caller
+      bar(1)
+    end
+
+    def x
+      42
+    end
+  end
+end
+
+f = M::Foo.new
+f.bar(f.x)
+f.bar
+# ^ apply-rename: [A] newName: baz
+f  .  bar  (   )
+
+M::Foo.new.bar 3
+
+class Unrelated
+  # this should be left unchanged
+  def bar; end
+end
