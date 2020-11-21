cd0aeabbe7d2367cf3616b1e86d990a9d742fdc0
Jake Zimmerman <zimmerman.jake@gmail.com>
2020-11-20 18:33:43 -0800

    Add failing test

---
+++ test/testdata/infer/private_alias_method.rb
@@ -0,0 +1,10 @@
+# typed: true
+
+class A < T::Struct
+  const :foo, Integer
+  alias_method :foo_DEPRECATED_BUT_PUBLIC, :foo
+  private :foo
+end
+
+A.new(foo: 0).foo_DEPRECATED_BUT_PUBLIC # error: Non-private call to private method `foo_DEPRECATED_BUT_PUBLIC`
+A.new(foo: 0).foo # error: Non-private call to private method `A#foo`
