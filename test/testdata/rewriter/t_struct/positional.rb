30e8f3a11ac6c2bfd4b7a7cdb99ed9bbea20db83
Jake Zimmerman <zimmerman.jake@gmail.com>
2020-11-20 22:34:48 -0800

    Add a failing test

---
+++ test/testdata/rewriter/t_struct/positional.rb
@@ -0,0 +1,10 @@
+# typed: true
+
+class A < T::Struct
+
+  # If you're seeing this in an error message, it means you printed loc instead
+  # of declLoc somewhere.
+
+end
+
+A.new(0) # error: Too many arguments
