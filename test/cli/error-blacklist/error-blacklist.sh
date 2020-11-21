6ac1c5b8cd40e7b1f4b455acf0131e52581da45e
Issy Long <me@issyl0.co.uk>
2020-11-21 19:31:43 +0000

    test: Test `--suppress-error-code` and `--isolate-error-code` too

--- test/cli/error-blacklist/error-blacklist.sh
+++ test/cli/error-blacklist/error-blacklist.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 main/sorbet --silence-dev-message test/cli/error-blacklist/error-blacklist.rb 2>&1
 main/sorbet --silence-dev-message test/cli/error-blacklist/error-blacklist.rb --error-black-list=7002 2>&1
+main/sorbet --silence-dev-message test/cli/error-blacklist/error-blacklist.rb --suppress-error-code=7002 2>&1
--- test/cli/error-whitelist/error-whitelist.sh
+++ test/cli/error-whitelist/error-whitelist.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 main/sorbet --silence-dev-message test/cli/error-whitelist/error-whitelist.rb --error-white-list=7017 2>&1
+main/sorbet --silence-dev-message test/cli/error-whitelist/error-whitelist.rb --isolate-error-code=7017 2>&1
