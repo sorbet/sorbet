c5e3f8fdb90b5b830920d07c98c894f3d863877a
Jake Zimmerman <zimmerman.jake@gmail.com>
2020-11-20 16:36:13 -0800

    wow

--- test/testdata/namer/private_multiple.rb
+++ test/testdata/namer/private_multiple.rb
@@ -14,10 +14,10 @@ class A
   private_constant :X, :Y, :Z
 end
 
-A.new.foo # Non-private call to private method `A#foo`
-A.new.bar # Non-private call to private method `A#bar`
-A.new.qux # Non-private call to private method `A#qux`
+A.new.foo # error: Non-private call to private method `A#foo`
+A.new.bar # error: Non-private call to private method `A#bar`
+A.new.qux # error: Non-private call to private method `A#qux`
 
-p A::X # Non-private reference to private constant `A::X`
-p A::Y # Non-private reference to private constant `A::Y`
-p A::Z # Non-private reference to private constant `A::Z`
+p A::X # error: Non-private reference to private constant `A::X`
+p A::Y # error: Non-private reference to private constant `A::Y`
+p A::Z # error: Non-private reference to private constant `A::Z`
