68ec9a5b71533e208515edd153507241338c71be
Soam Vasani <soam@stripe.com>
2020-11-23 14:53:34 -0800

    Test updates

--- test/testdata/lsp/rename/method_attr.rb
+++ test/testdata/lsp/rename/method_attr.rb
@@ -2,9 +2,14 @@
 # frozen_string_literal: true
 
 class Foo
-  attr_reader :bar
+  attr_reader :m1
+#              ^ apply-rename: [A] newName: n1 invalid: true
+  attr_writer :m2
+#              ^ apply-rename: [B] newName: n2 invalid: true
+  attr_accessor :m3
+#                ^ apply-rename: [C] newName: n3 invalid: true
 end
 
 f = Foo.new
-f.bar
-# ^ apply-rename: [A] newName: baz
+f.m1
+# ^ apply-rename: [D] newName: n1 invalid: true
