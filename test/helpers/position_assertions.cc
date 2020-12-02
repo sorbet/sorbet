3a389c2b49d69b859a76726fa61ae220d8099ac7
Patrick Vilhena <62039355+vilhena-stripe@users.noreply.github.com>
2020-12-02 14:47:32 -0800

    Rename methods in mixins (#3747)
    * Add test cases and print rbedited file path when failing
    
    * Implement renaming methods in mixins
    
    * Update main/lsp/requests/rename.cc
    
    Co-authored-by: John Vilk <43145439+jvilk-stripe@users.noreply.github.com>
    
    Co-authored-by: John Vilk <43145439+jvilk-stripe@users.noreply.github.com>

--- test/helpers/position_assertions.cc
+++ test/helpers/position_assertions.cc
@@ -1366,8 +1366,8 @@ void ApplyRenameAssertion::check(const UnorderedMap<std::string, std::shared_ptr
         {
             CHECK_EQ_DIFF(
                 expectedEditedFileContents, actualEditedFileContents,
-                fmt::format("The expected (rbedited) file contents for {} did not match the actual post-edit contents",
-                            filePath));
+                fmt::format("The expected (rbedited) file contents in {} did not match the actual post-edit contents.",
+                            expectedEditedFilePath));
         }
     }
 }
