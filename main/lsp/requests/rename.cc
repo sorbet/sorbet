e2fb636800766585cc8ff4cd5de49fedc0e3eac6
soam-stripe <72465582+soam-stripe@users.noreply.github.com>
2020-12-02 19:43:00 -0800

    Method renames: test for untyped files, operator overload renames (#3750)
    Block some invalid method renames -- in untyped files, and operator overload methods.

--- main/lsp/requests/rename.cc
+++ main/lsp/requests/rename.cc
@@ -137,7 +137,17 @@ public:
     MethodRenamer(const core::GlobalState &gs, const LSPConfiguration &config, const string oldName,
                   const string newName)
         : Renamer(gs, config, oldName, newName) {
-        if (oldName == "initialize") {
+        const vector<string> invalidNames = {"initialize", "call"};
+        for (auto name : invalidNames) {
+            if (oldName == name) {
+                invalid = true;
+                error = fmt::format("The `{}` method cannot be renamed.", oldName);
+                return;
+            }
+        }
+        // block any method not starting with /[a-zA-Z0-9_]+/. This blocks operator overloads.
+        if (!isalnum(oldName[0]) && oldName[0] != '_') {
+            error = fmt::format("The `{}` method cannot be renamed.", oldName);
             invalid = true;
         }
     }
@@ -156,7 +166,13 @@ public:
         if (auto sendResp = response->isSend()) {
             auto methodNameLoc = sendResp->getMethodNameLoc(gs);
             if (!methodNameLoc) {
-                ENFORCE(0, "Method name not found in send expression")
+                // If there are locations we don't know how to rename, fail the entire rename operation
+                invalid = true;
+                if (methodNameLoc->file().exists()) {
+                    auto path = methodNameLoc->file().data(gs).path();
+                    error = fmt::format("Failed to rename `{}` method call at {}:{}", oldName, path,
+                                        methodNameLoc->position(gs).first.line);
+                }
                 return;
             }
             string::size_type methodNameOffset = methodNameLoc->beginPos() - sendResp->termLoc.beginPos();
