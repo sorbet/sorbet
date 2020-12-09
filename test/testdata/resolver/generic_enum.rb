7a1529d4f8e16c634ef6d1c9630a98f300918cd3
John Vilk <jvilk@stripe.com>
2020-12-09 06:19:32 +0000

    Generic enum test.

---
+++ test/testdata/resolver/generic_enum.rb
@@ -0,0 +1,26 @@
+# typed: true
+class MyEnum < T::Enum
+  extend T::Generic
+  Value = type_template(fixed: String) # error: All constants defined on an `T::Enum` must be unique instances of the enum
+
+  enums do
+    X = new # error: Type `Value` declared by parent `T.class_of(MyEnum)` must be re-declared
+    Y = new # error: Type `Value` declared by parent `T.class_of(MyEnum)` must be re-declared
+    Z = new # error: Type `Value` declared by parent `T.class_of(MyEnum)` must be re-declared
+  end
+end
+
+extend T::Sig
+sig {params(xy: T.any(MyEnum::X[Integer], MyEnum::Y[Integer])).void}
+#                     ^^^^^^^^^^^^^^^^^^ error: Expected a class or module
+#                     ^^^^^^^^^^^^^^^^^^ error: Method `[]` does not exist
+#                                         ^^^^^^^^^^^^^^^^^^ error: Expected a class or module
+#                                         ^^^^^^^^^^^^^^^^^^ error: Method `[]` does not exist
+def foo(xy)
+end
+
+XY = T.type_alias {T.any(MyEnum::X[Integer], MyEnum::Y[Integer])}
+#                        ^^^^^^^^^^^^^^^^^^ error: Expected a class or module
+#                        ^^^^^^^^^^^^^^^^^^ error: Method `[]` does not exist
+#                                            ^^^^^^^^^^^^^^^^^^ error: Expected a class or module
+#                                            ^^^^^^^^^^^^^^^^^^ error: Method `[]` does not exist
