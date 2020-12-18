fb4217f5ec3fb7a2bffb6265e2bfdaca53e05897
Jake Zimmerman <zimmerman.jake@gmail.com>
2020-12-18 10:15:42 -0800

    Fix error messages

--- test/testdata/namer/private_multiple.rb
+++ test/testdata/namer/private_multiple.rb
@@ -14,9 +14,9 @@ class A
   private_constant :X, :Y, :Z
 end
 
-A.new.foo # error: Non-private call to private method `A#foo`
-A.new.bar # error: Non-private call to private method `A#bar`
-A.new.qux # error: Non-private call to private method `A#qux`
+A.new.foo # error: Non-private call to private method `foo`
+A.new.bar # error: Non-private call to private method `bar`
+A.new.qux # error: Non-private call to private method `qux`
 
 p A::X # error: Non-private reference to private constant `A::X`
 p A::Y # error: Non-private reference to private constant `A::Y`
