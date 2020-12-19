1994f6e1e221f1f0edca6aace5fd1703986e1841
John Vilk <jvilk@stripe.com>
2020-12-19 13:11:50 +0000

    Simplify LocalVarSaver.

--- main/lsp/LocalVarSaver.cc
+++ main/lsp/LocalVarSaver.cc
@@ -8,30 +8,20 @@ namespace sorbet::realmain::lsp {
 ast::TreePtr LocalVarSaver::postTransformLocal(core::Context ctx, ast::TreePtr tree) {
     auto &local = ast::cast_tree_nonnull<ast::Local>(tree);
 
-    core::MethodRef owner;
+    core::MethodRef enclosingMethod;
     if (ctx.owner.data(ctx)->isMethod()) {
-        owner = ctx.owner.asMethodRef();
+        enclosingMethod = ctx.owner.asMethodRef();
     } else if (ctx.owner == core::Symbols::root()) {
-        owner = ctx.state.lookupStaticInitForFile(core::Loc(ctx.file, local.loc));
+        enclosingMethod = ctx.state.lookupStaticInitForFile(core::Loc(ctx.file, local.loc));
     } else {
-        owner = ctx.state.lookupStaticInitForClass(ctx.owner.asClassOrModuleRef());
+        enclosingMethod = ctx.state.lookupStaticInitForClass(ctx.owner.asClassOrModuleRef());
     }
 
-    bool lspQueryMatch = ctx.state.lspQuery.matchesVar(owner, local.localVariable);
+    bool lspQueryMatch = ctx.state.lspQuery.matchesVar(enclosingMethod, local.localVariable);
     if (lspQueryMatch) {
         // No need for type information; this is for a reference request.
         // Let the default constructor make tp.type an empty shared_ptr and tp.origins an empty vector
         core::TypeAndOrigins tp;
-
-        core::MethodRef enclosingMethod;
-        if (ctx.owner.isClassOrModule()) {
-            enclosingMethod = ctx.owner == core::Symbols::root()
-                                  ? ctx.state.lookupStaticInitForFile(core::Loc(ctx.file, local.loc))
-                                  : ctx.state.lookupStaticInitForClass(ctx.owner.asClassOrModuleRef());
-        } else {
-            enclosingMethod = ctx.owner.asMethodRef();
-        }
-
         core::lsp::QueryResponse::pushQueryResponse(
             ctx, core::lsp::IdentResponse(core::Loc(ctx.file, local.loc), local.localVariable, tp, enclosingMethod));
     }
