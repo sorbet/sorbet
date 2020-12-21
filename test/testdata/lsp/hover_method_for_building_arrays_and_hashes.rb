b6697d54fc8ef48f6e7a926dd8078d3232cd555e
Jake Zimmerman <zimmerman.jake@gmail.com>
2020-12-20 17:31:21 -0800

    Fix some LSP tests

--- test/testdata/lsp/hover_method_for_building_arrays_and_hashes.rb
+++ test/testdata/lsp/hover_method_for_building_arrays_and_hashes.rb
@@ -25,23 +25,23 @@ class Issue1777
   end
 
   def hash_hover
-    T.reveal_type({ # error: {foo: String("foo"), bar: Integer(123)} (shape of T::Hash[T.untyped, T.untyped])
-      #           ^ hover: {foo: String("foo"), bar: Integer(123)} (shape of T::Hash[T.untyped, T.untyped])
+    T.reveal_type({ # error: {foo: String("foo"), bar: Integer(123)}
+      #           ^ hover: {foo: String("foo"), bar: Integer(123)} (shape of T::Hash[Symbol, T.any(String, Integer)])
       # Test hovering over first item in array
       foo: "foo",
       #        ^ hover: String("foo")
       #         ^ hover: String("foo")
-      #          ^ hover: {foo: String("foo"), bar: Integer(123)} (shape of T::Hash[T.untyped, T.untyped])
+      #          ^ hover: {foo: String("foo"), bar: Integer(123)} (shape of T::Hash[Symbol, T.any(String, Integer)])
 
       # Test hovering over comment
-      # ^ hover: {foo: String("foo"), bar: Integer(123)} (shape of T::Hash[T.untyped, T.untyped])
+      # ^ hover: {foo: String("foo"), bar: Integer(123)} (shape of T::Hash[Symbol, T.any(String, Integer)])
 
       # Test hovering over last item in array
       bar: 123
       #      ^ hover: Integer(123)
       #       ^ hover: Integer(123)
-      #        ^ hover: {foo: String("foo"), bar: Integer(123)} (shape of T::Hash[T.untyped, T.untyped])
+      #        ^ hover: {foo: String("foo"), bar: Integer(123)} (shape of T::Hash[Symbol, T.any(String, Integer)])
       })
-    # ^ hover: {foo: String("foo"), bar: Integer(123)} (shape of T::Hash[T.untyped, T.untyped])
+    # ^ hover: {foo: String("foo"), bar: Integer(123)} (shape of T::Hash[Symbol, T.any(String, Integer)])
   end
 end
