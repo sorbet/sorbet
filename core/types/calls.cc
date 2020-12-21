0fbf058310fbd332690e471a456e8ee0c29b4a69
Jake Zimmerman <zimmerman.jake@gmail.com>
2020-12-20 18:12:50 -0800

    wip: Shape_squareBrackets

--- core/types/calls.cc
+++ core/types/calls.cc
@@ -2665,6 +2665,32 @@ optional<Loc> locOfValueForKey(const GlobalState &gs, const Loc origin, const Na
 
 } // namespace
 
+// TODO(jez) Add tests for this
+class Shape_squareBrackets : public IntrinsicMethod {
+public:
+    void apply(const GlobalState &gs, const DispatchArgs &args, DispatchResult &res) const override {
+        auto &shape = cast_type_nonnull<ShapeType>(args.thisType);
+
+        if (args.args.size() != 1) {
+            // Skip over cases for which arg matching should report errors
+            return;
+        }
+
+        if (!isa_type<LiteralType>(args.args.front()->type)) {
+            return;
+        }
+
+        auto argLit = cast_type_nonnull<LiteralType>(args.args.front()->type);
+        if (auto idx = indexForKey(shape, argLit)) {
+            res.returnType = shape.values[*idx];
+        } else {
+            // TODO(jez) This could be another "if you're in `typed: strict` you have to have better hashes"
+            res.returnType = Types::untypedUntracked();
+        }
+    }
+} Shape_squareBrackets;
+
+// TODO(jez) Add tests for this
 class Shape_squareBracketsEq : public IntrinsicMethod {
 public:
     void apply(const GlobalState &gs, const DispatchArgs &args, DispatchResult &res) const override {
@@ -2712,8 +2738,8 @@ public:
             // Returning here without setting res.resultType will cause dispatchCall to fall back to
             // Hash#[]=, which will have the effect of checking the arg types.
             //
-            // TODO(jez) Right now ShapeType::underlying always returns T::Hash[T.untyped, T.untyped]
-            // so it doesn't matter whether we return or not.
+            // TODO(jez) We could do something smarter here, like check that the new value is
+            // compatible with the old value.
             return;
         } else {
             // Key not found. To preserve legacy compatibility, allow any arguments here.
@@ -3342,7 +3368,6 @@ const vector<Intrinsic> intrinsicMethods{
     {Symbols::DeclBuilderForProcsSingleton(), Intrinsic::Kind::Instance, Names::params(), &DeclBuilderForProcs_params},
     {Symbols::DeclBuilderForProcsSingleton(), Intrinsic::Kind::Instance, Names::bind(), &DeclBuilderForProcs_bind},
 
-    // TODO(jez) Make or find an issue recording that we should also have Shape_squareBrackets
     {Symbols::Tuple(), Intrinsic::Kind::Instance, Names::squareBrackets(), &Tuple_squareBrackets},
     {Symbols::Tuple(), Intrinsic::Kind::Instance, Names::first(), &Tuple_first},
     {Symbols::Tuple(), Intrinsic::Kind::Instance, Names::last(), &Tuple_last},
@@ -3351,6 +3376,7 @@ const vector<Intrinsic> intrinsicMethods{
     {Symbols::Tuple(), Intrinsic::Kind::Instance, Names::toA(), &Tuple_to_a},
     {Symbols::Tuple(), Intrinsic::Kind::Instance, Names::concat(), &Tuple_concat},
 
+    {Symbols::Shape(), Intrinsic::Kind::Instance, Names::squareBrackets(), &Shape_squareBrackets},
     {Symbols::Shape(), Intrinsic::Kind::Instance, Names::squareBracketsEq(), &Shape_squareBracketsEq},
     {Symbols::Shape(), Intrinsic::Kind::Instance, Names::merge(), &Shape_merge},
     {Symbols::Shape(), Intrinsic::Kind::Instance, Names::toHash(), &Shape_to_hash},
