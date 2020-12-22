16f85d082323157e90405049837553514e805239
Soam Vasani <soam@stripe.com>
2020-12-21 17:16:07 -0800

    De-duplicate rename edits

---
+++ test/testdata/lsp/rename/method_union_2.rb
@@ -0,0 +1,14 @@
+# typed: true
+# frozen_string_literal: true
+
+class Dog
+  def sound; "Bark"; end
+end
+class Cat
+  def sound; "Meow"; end
+end
+
+animal = T.let(Cat.new, T.any(Dog, Cat))
+animal.sound
+#      ^ apply-rename: [A] newName: sound_new
+
