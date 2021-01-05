603c1df2a7c15cae2c03ab8a3f587cd30f1c6190
Alexandre Terrasa <583144+Morriar@users.noreply.github.com>
2021-01-05 19:27:23 +0100

    IO's `encoding` also accept a Encoding (#3861)
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

--- test/testdata/rbi/io.rb
+++ test/testdata/rbi/io.rb
@@ -6,3 +6,11 @@ sig {params(file_name: String).returns(T::Array[String])}
 def test_io_readlines_chomp(file_name)
   IO.readlines(file_name, chomp: true)
 end
+
+sig {params(file_name: String).void}
+def test_io_read_encoding(file_name)
+  IO.read(file_name, encoding: "ASCII_8BIT")
+  IO.read(file_name, encoding: Encoding::ASCII_8BIT)
+  IO.read(file_name, encoding: Encoding::ASCII_8BIT.to_s)
+  IO.read(file_name, encoding: 42) # error: Expected `T.any(String, Encoding)` but found `Integer(42)` for argument `encoding`
+end
