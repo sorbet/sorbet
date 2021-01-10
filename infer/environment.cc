3eb56ea8fad82393f63b025c4998b499ce1f3015
Jake Zimmerman <zimmerman.jake@gmail.com>
2021-01-09 19:24:59 -0800

    Fix alias origins
    Aliases should point at the alias itself. This makes things like
    
        A.foo
    
    point at the constant literal `A` instead of the location where `class
    A` is defined. It also makes things like
    
        x = A
        x.foo
    
    point to the `x = A` line instead of `class A`
    
    This codepath **only** applies to `isClassOrModule`, so things like
    fields, static fields, and type members are unchanged. In particular:
    
        @x.foo
    
    will point to the place where `@x` is pinned (if it exists)--it is an
    error for the field not to exist in `typed: strict`, and even if the
    error is silenced it will have been replaced with a harmless local.

--- infer/environment.cc
+++ infer/environment.cc
@@ -1047,7 +1047,7 @@ core::TypePtr Environment::processBinding(core::Context ctx, const cfg::CFG &inW
                     auto singletonClass = data->lookupSingletonClass(ctx);
                     ENFORCE(singletonClass.exists(), "Every class should have a singleton class by now.");
                     tp.type = singletonClass.data(ctx)->externalType();
-                    tp.origins.emplace_back(symbol.data(ctx)->loc());
+                    tp.origins.emplace_back(core::Loc(ctx.file, bind.loc));
                 } else if (data->isField() || (data->isStaticField() && !data->isTypeAlias()) || data->isTypeMember()) {
                     if (data->resultType != nullptr) {
                         if (data->isTypeMember()) {
