3842409fe4779d1b2d1836516a0010e9e73d7b7d
Jake Zimmerman <zimmerman.jake@gmail.com>
2021-01-17 23:05:29 -0800

    More tests

--- test/testdata/infer/shape_square_brackets.rb
+++ test/testdata/infer/shape_square_brackets.rb
@@ -5,20 +5,19 @@ extend T::Sig
 sig {params(x: {foo: Integer}).void}
 def test2(x)
   x[:foo] = 1
-  x[:foo] = '' # error:
+  x[:foo] = '' # error: Expected `Integer` but found `String("")` for key `Symbol(:foo)`
 
   # Likewise, neither of these report errors, for backwards compatibility
+  # We should work to make this stricter.
   x[:bar] = 1
   x[:bar] = ''
 end
 
 sig {params(x: {foo: Integer, 'bar' => Float}).void}
 def test3(x)
-  # could make this better, e.g.: only allow setting to `Integer`
-  x[:foo] = 0.0
-  # ... especially because the type remains the same as before:
-  T.reveal_type(x[:foo]) # error: Revealed type: `Integer`
+  x[:foo] = '' # error: Expected `Integer` but found `String("")` for key `Symbol(:foo)`
+  T.reveal_type(x[:foo]) # error: Revealed type: `T.untyped`
 
-  # This might end up being too restrictive.  This used to not error at all.
-  x[true] = nil # error: Expected `T.any(Integer, Float)` but found `NilClass` for argument `arg1`
+  # This won't error until we change the `underlying` on Shape types to be stricter
+  x[true] = nil
 end
---
+++ test/testdata/infer/super_strict_shapes.rb
@@ -0,0 +1,84 @@
+# typed: strict
+
+extend T::Sig
+
+xs = {
+  foo: T.let(nil, T.nilable(Integer)),
+  bar: nil,
+}
+
+T.reveal_type(xs) # error: Revealed type: `{foo: T.nilable(Integer), bar: NilClass} (shape of T::Hash[T.untyped, T.untyped])`
+
+xs[:foo] = 1
+xs[:bar] = 1 # error: Expected `NilClass` but found `Integer(1)` for key `Symbol(:bar)`
+xs[:qux]
+
+options = {
+  enable_foo: T.let(false, T::Boolean),
+  enable_bar: T.let(false, T::Boolean),
+  enable_qux: false,
+}
+
+T.reveal_type(options) # error: Revealed type: `{enable_foo: T::Boolean, enable_bar: T::Boolean, enable_qux: FalseClass} (shape of T::Hash[T.untyped, T.untyped])`
+
+ARGV.each do |arg|
+  case arg
+  when 'enable_foo' then options[:enable_foo] = true
+  when 'enable_bar' then options[:enable_bar] = true
+  when 'enable_qux' then options[:enable_qux] = true # error: Expected `FalseClass` but found `TrueClass` for key `Symbol(:enable_qux)`
+  end
+end
+
+T.reveal_type(options) # error: Revealed type: `{enable_foo: T::Boolean, enable_bar: T::Boolean, enable_qux: FalseClass} (shape of T::Hash[T.untyped, T.untyped])`
+
+# These merge algorithms more or less assume exact shape types.
+#
+# Otherwise, if you were assuming inexact, you'd have to widen the keys of the
+# first shape (like `:one` and `:three`) because if the second shape is
+# inexact, you don't know that it doesn't also have a `:one` or `:three` key
+# with a completely unrelated type.
+
+evens = {
+  one: 1,
+  three: 3,
+}
+odds = {
+  two: 2,
+  four: 4,
+}
+T.reveal_type(evens.merge(odds)) # error: Revealed type: `{one: Integer(1), three: Integer(3), two: Integer(2), four: Integer(4)} (shape of T::Hash[T.untyped, T.untyped])`
+
+# Looks like our Shape_merge intrinsic hardcodes one positional argument
+T.reveal_type({}.merge(evens, odds)) # error: Revealed type: `{} (shape of T::Hash[T.untyped, T.untyped])`
+
+one_three = {
+  one: 1,
+  three: 3,
+}
+three_five = {
+  three: '3',
+  five: '5',
+}
+T.reveal_type(one_three.merge(three_five)) # error: Revealed type: `{one: Integer(1), three: String("3"), five: String("5")} (shape of T::Hash[T.untyped, T.untyped])`
+
+# Fixes https://github.com/sorbet/sorbet/issues/3751
+sig {params(xs: T::Array[Integer]).void}
+def test1(xs = [])
+  T.reveal_type(xs) # error: Revealed type: `T::Array[Integer]`
+end
+
+sig {params(x: T::Array[String]).void}
+def test2(x)
+  x = []
+  T.reveal_type(x) # error: Revealed type: `[] (0-tuple)`
+
+  if T.unsafe(nil)
+    y = []
+    z = T::Array[Integer].new
+  else
+    y = T::Array[Integer].new
+    z = []
+  end
+  T.reveal_type(y) # error: Revealed type: `T::Array[Integer]`
+  T.reveal_type(z) # error: Revealed type: `T::Array[Integer]`
+end
