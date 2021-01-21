e85a602cf4419374b18bbf657a1f599287021fbd
Jake Zimmerman <zimmerman.jake@gmail.com>
2021-01-20 21:18:32 -0800

    Make the T.must cli test better

--- test/cli/suggest_t_must/suggest_t_must.sh
+++ test/cli/suggest_t_must/suggest_t_must.sh
@@ -1,2 +1,23 @@
-#!/bin/bash
-main/sorbet --silence-dev-message test/cli/suggest_t_must/suggest_t_must.rb 2>&1
+#!/usr/bin/env bash
+
+tmp="$(mktemp -d)"
+infile="test/cli/suggest_t_must/suggest_t_must.rb"
+cp "$infile" "$tmp"
+
+cwd="$(pwd)"
+cd "$tmp" || exit 1
+
+if "$cwd/main/sorbet" --silence-dev-message -a suggest_t_must.rb 2>&1; then
+  echo "Expected to fail!"
+  exit 1
+fi
+
+echo
+echo --------------------------------------------------------------------------
+echo
+
+# Also cat the file, to make that the autocorrect applied
+cat suggest_t_must.rb
+
+rm suggest_t_must.rb
+rm "$tmp"
