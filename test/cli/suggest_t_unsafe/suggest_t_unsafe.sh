1fb89b2ac3bfe35c6d5bd13356211108e300d46f
Jake Zimmerman <zimmerman.jake@gmail.com>
2021-01-22 15:57:16 -0800

    Add test for --suggest-unsafe=custom_wrapper

--- test/cli/suggest_t_unsafe/suggest_t_unsafe.sh
+++ test/cli/suggest_t_unsafe/suggest_t_unsafe.sh
@@ -1,23 +1,50 @@
 #!/usr/bin/env bash
 
-tmp="$(mktemp -d)"
-infile="test/cli/suggest_t_unsafe/suggest_t_unsafe.rb"
-cp "$infile" "$tmp"
-
 cwd="$(pwd)"
-cd "$tmp" || exit 1
+infile="$cwd/test/cli/suggest_t_unsafe/suggest_t_unsafe.rb"
 
-if "$cwd/main/sorbet" --silence-dev-message --suggest-unsafe -a suggest_t_unsafe.rb 2>&1; then
-  echo "Expected to fail!"
-  exit 1
-fi
+tmp="$(mktemp -d)"
+
+(
+  cp "$infile" "$tmp"
+
+  cd "$tmp" || exit 1
+  if "$cwd/main/sorbet" --silence-dev-message --suggest-unsafe -a suggest_t_unsafe.rb 2>&1; then
+    echo "Expected to fail!"
+    exit 1
+  fi
+
+  echo
+  echo --------------------------------------------------------------------------
+  echo
+
+  # Also cat the file, to make that the autocorrect applied
+  cat suggest_t_unsafe.rb
+
+  rm suggest_t_unsafe.rb
+)
 
 echo
 echo --------------------------------------------------------------------------
 echo
 
-# Also cat the file, to make that the autocorrect applied
-cat suggest_t_unsafe.rb
+(
+  cp "$infile" "$tmp"
 
-rm suggest_t_unsafe.rb
-rm "$tmp"
+  cd "$tmp" || exit 1
+  if "$cwd/main/sorbet" --silence-dev-message --suggest-unsafe=custom_wrapper -a suggest_t_unsafe.rb 2>&1; then
+    echo "Expected to fail!"
+    exit 1
+  fi
+
+  echo
+  echo --------------------------------------------------------------------------
+  echo
+
+  # Also cat the file, to make that the autocorrect applied
+  cat suggest_t_unsafe.rb
+
+  rm suggest_t_unsafe.rb
+)
+
+rm -r "$tmp"
