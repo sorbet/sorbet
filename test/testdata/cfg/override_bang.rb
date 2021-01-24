bb5f6a915cafbab2bfe5c5da4fd7414deb609104
Jake Zimmerman <zimmerman.jake@gmail.com>
2021-01-23 23:30:17 -0800

    no-op prework: Reindent to tidy up previous change

--- test/testdata/cfg/override_bang.rb
+++ test/testdata/cfg/override_bang.rb
@@ -10,48 +10,48 @@ class C
 end
 
 def test1
-if C.new
-  puts
-else
-  puts # error: This code is unreachable
-end
+  if C.new
+    puts
+  else
+    puts # error: This code is unreachable
+  end
 end
 
 def test2
-if !C.new
-  puts
-else
-  puts # error: This code is unreachable
-end
+  if !C.new
+    puts
+  else
+    puts # error: This code is unreachable
+  end
 end
 
 def test3
-unless C.new
-  puts # error: This code is unreachable
-else
-  puts
-end
+  unless C.new
+    puts # error: This code is unreachable
+  else
+    puts
+  end
 end
 
 def test4
-unless !C.new
-  puts # error: This code is unreachable
-else
-  puts
-end
+  unless !C.new
+    puts # error: This code is unreachable
+  else
+    puts
+  end
 end
 
 def test5
-until C.new
-  puts # TODO this puts should be unreachable
-end
-puts # error: This code is unreachable
+  until C.new
+    puts # TODO this puts should be unreachable
+  end
+  puts # error: This code is unreachable
 end
 
 def test6
-until !C.new
-  # TODO this C.new should be reachable
-  puts # error: This code is unreachable
-end
-puts
+  until !C.new
+    # TODO this C.new should be reachable
+    puts # error: This code is unreachable
+  end
+  puts
 end
