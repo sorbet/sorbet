f9e48adfef20f913e9240990522bdd1747f6ea6a
Trevor Elliott <awesomelyawesome@gmail.com>
2021-02-03 13:41:55 -0800

    Fix bugs in advance of clang-10 (#3967)
    * include <cassert>
    
    * Fix a string_view bug
    
    The string returned was a temporary, and casting to a string_view was
    causing a use-after-free

--- main/autogen/autoloader.cc
+++ main/autogen/autoloader.cc
@@ -124,8 +124,8 @@ string DefTree::show(const core::GlobalState &gs, int level) const {
 }
 
 string DefTree::fullName(const core::GlobalState &gs) const {
-    return fmt::format(
-        "{}", fmt::map_join(qname.nameParts, "::", [&](core::NameRef nr) -> string_view { return nr.show(gs); }));
+    return fmt::format("{}",
+                       fmt::map_join(qname.nameParts, "::", [&](core::NameRef nr) -> string { return nr.show(gs); }));
 }
 
 string join(string path, string file) {
