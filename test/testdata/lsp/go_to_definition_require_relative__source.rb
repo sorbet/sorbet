1e074f52272438e3068ff937f373f987ba58f49e
Jérémie Laval <jeremie@stripe.com>
2021-02-08 10:22:45 -0500

    Add case with .rb extension in require_relative call

--- test/testdata/lsp/go_to_definition_require_relative__source.rb
+++ test/testdata/lsp/go_to_definition_require_relative__source.rb
@@ -2,3 +2,5 @@
 
 require_relative 'go_to_definition_require_relative__target'
 #                      ^ file-def: go_to_definition_require_relative__target.rb 0 0
+require_relative 'go_to_definition_require_relative__target.rb'
+#                         ^ file-def: go_to_definition_require_relative__target.rb 0 0
