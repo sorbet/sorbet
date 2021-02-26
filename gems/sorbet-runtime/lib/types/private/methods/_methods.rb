10ee208ee173abe1e6867826e8940b922e32128a
Nathan Froyd <froydnj@gmail.com>
2021-02-26 05:34:44 -1000

    don't double-check protected methods when doing final methods checks
    We already (implicitly) check these because method_defined? checks both
    public and protected methods.

--- gems/sorbet-runtime/lib/types/private/methods/_methods.rb
+++ gems/sorbet-runtime/lib/types/private/methods/_methods.rb
@@ -109,8 +109,7 @@ module T::Private::Methods
         # correctness, since ancestor.method_defined?(method_name) may return true even if method_name is not defined
         # directly on ancestor but instead an ancestor of ancestor.
         if (ancestor.method_defined?(method_name) ||
-            ancestor.private_method_defined?(method_name) ||
-            ancestor.protected_method_defined?(method_name)) &&
+            ancestor.private_method_defined?(method_name)) &&
            final_method?(method_owner_and_name_to_key(ancestor, method_name))
           definition_file, definition_line = T::Private::Methods.signature_for_method(ancestor.instance_method(method_name)).method.source_location
           is_redefined = target == ancestor
