6ce3ab1e019ecc9dd726fd133a8d2fb3aa048ba8
Jake Zimmerman <zimmerman.jake@gmail.com>
2021-03-02 16:37:43 -0800

    wip

--- test/testdata/compiler/exceptions/print_trace.rb
+++ test/testdata/compiler/exceptions/print_trace.rb
@@ -14,7 +14,7 @@ rescue => e
   T.must(e.backtrace).each do |line|
     puts line
       .sub(path, '')
-      .sub('/usr/local/var/bazelcache/output-bases/test/execroot/com_stripe_sorbet_llvm/', '') # ... buildkite? idk https://buildkite.com/sorbet/sorbet-compiler/builds/3487
+      .sub(%r{^/.*bazel-out/k8-opt/bin/external/sorbet_ruby_2_7/}, 'bazel-out/k8-opt/bin/external/sorbet_ruby_2_7/')
       .sub(%r{.*test/patch_require.rb:.*}, 'test/patch_require.rb:<censored>:')
       .sub(%r{^.*tmp\..*:}, '<censored>') # OSX
       .sub(%r{^/tmp.*:}, '<censored>')    # linux
