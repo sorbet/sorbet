6573bda294afb125e8072eeab5c6d0530a931f28
Jake Zimmerman <zimmerman.jake@gmail.com>
2021-03-10 15:06:30 -0800

    Update cli test

--- test/cli/suggest_t_unsafe/suggest_t_unsafe.rb
+++ test/cli/suggest_t_unsafe/suggest_t_unsafe.rb
@@ -1,5 +1,7 @@
 # typed: true
 
+extend T::Sig
+
 def custom_wrapper(arg0); end
 
 foo = T.let(nil, T.nilable(String))
@@ -13,3 +15,12 @@ bar.even?
 "hi" + 1
 
 T::Array[T.nilable(Integer)].new.map(&:even?)
+
+sig {params(str: String).void}
+def takes_string_kw(str:); end
+
+sig {params(nilable_string: T.nilable(String)).void}
+def takes_nilable_string(nilable_string)
+  # This used to crash Sorbet, because there was no Loc for the `str` argument
+  takes_string_kw(str: nilable_string)
+end
