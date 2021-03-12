86eb7bcd1cb9af97a3b2e6f446c9fee0e4433e1f
Jake Zimmerman <zimmerman.jake@gmail.com>
2021-03-11 21:30:59 -0800

    Add sorbet/showSymbol method
    Fixes #2937
    
    This custom LSP method takes a TextDocumentPositionParams and returns a
    SymbolInformation (the symbol at that position). This is sufficient for
    editors to build on top of this to implement functionality like "copy
    fully-qualified name to clipboard" and I have a prototype of that
    working in Vim with LanguageClient-neovim.
    
    - [ ] Write tests. Be sure to test edge cases, like what happens when
      there's no result. Can probably model this like the `hover` tests.
    - [ ] Implement proper VS Code support (in Stripe's VS Code extension)

--- main/lsp/tools/make_lsp_types.cc
+++ main/lsp/tools/make_lsp_types.cc
@@ -1337,6 +1337,7 @@ void makeLSPTypes(vector<shared_ptr<JSONClassType>> &enumTypes, vector<shared_pt
                                      "sorbet/fence",
                                      "sorbet/readFile",
                                      "sorbet/showOperation",
+                                     "sorbet/showSymbol",
                                      "sorbet/typecheckRunInfo",
                                      "sorbet/watchmanFileChange",
                                      "sorbet/workspaceEdit",
@@ -1384,6 +1385,7 @@ void makeLSPTypes(vector<shared_ptr<JSONClassType>> &enumTypes, vector<shared_pt
                                                 {"workspace/symbol", WorkspaceSymbolParams},
                                                 {"sorbet/error", SorbetErrorParams},
                                                 {"sorbet/readFile", TextDocumentIdentifier},
+                                                {"sorbet/showSymbol", TextDocumentPositionParams},
                                             });
     auto RequestMessage =
         makeObject("RequestMessage",
@@ -1426,6 +1428,7 @@ void makeLSPTypes(vector<shared_ptr<JSONClassType>> &enumTypes, vector<shared_pt
             {"workspace/symbol", makeVariant({JSONNull, makeArray(SymbolInformation)})},
             {"sorbet/error", SorbetErrorParams},
             {"sorbet/readFile", TextDocumentItem},
+            {"sorbet/showSymbol", makeVariant({JSONNull, SymbolInformation})},
         });
     // N.B.: ResponseMessage.params must be optional, as it is not present when an error occurs.
     // N.B.: We add a 'requestMethod' field to response messages to make the discriminated union work.
