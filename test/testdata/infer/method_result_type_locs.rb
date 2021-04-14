77006f403d2dbcacacff6a40e1544fcb30aa360f
Jake Zimmerman <zimmerman.jake@gmail.com>
2021-04-13 22:19:30 -0700

    Add test showing current behavior

---
+++ test/testdata/infer/method_result_type_locs.rb
@@ -0,0 +1,53 @@
+# typed: true
+
+# TODO(jez) Also want to double check our autocorrect locations
+
+class Main
+  extend T::Sig
+
+  sig {returns(String)}
+  def implicit_return_via_else # error: Expected `String` but found `NilClass` for method result type
+    if T.unsafe(nil)
+      return 'yep'
+    end
+  end
+
+  sig {returns(String)}
+  def implicit_return_non_empty_cont_block
+    puts nil
+  # ^^^^^^^^ error: Expected `String` but found `NilClass` for method result type
+  end
+
+  sig {returns(String)}
+  def double_return
+    return puts nil
+  # ^^^^^^^^^^^^^^^ error: Expected `String` but found `NilClass` for method result type
+  end
+
+  sig {params(x: Module).returns(String)}
+  def initialized_twice(x)
+    if T.unsafe(nil)
+      res = x.name
+    else
+      res = x.name
+    end
+    return res
+  # ^^^^^^^^^^ error: Expected `String` but found `T.nilable(String)` for method result type
+  end
+end
+
+module M
+  extend T::Sig
+  include Kernel
+
+  sig {returns(String)}
+  def display_name
+    if self.is_a?(Module)
+      return self.name
+    # ^^^^^^^^^^^^^^^^ error: Expected `String` but found `T.nilable(String)` for method result type
+    else
+      return self.class.name
+    end
+  end
+end
+
