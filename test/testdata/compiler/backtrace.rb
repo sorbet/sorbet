f068073f0abfeef3ae6c5a588d89c2dbfe6f4cb0
Adam Procter <71457435+aprocter-stripe@users.noreply.github.com>
2021-04-26 11:00:04 -0700

    Fix backtrace path censorship to be agnostic to arch and config (#387)
    * Fix backtrace path censorship to be agnostic to arch and config
    
    * Slightly more conservative regex
    
    * Yet slightly more conservative regex (+ not *)

--- test/testdata/compiler/backtrace.rb
+++ test/testdata/compiler/backtrace.rb
@@ -11,7 +11,7 @@ def bar
   Thread.current.backtrace.map do |line|
     line
       .sub(path, '')
-      .sub(%r{^/.*bazel-out/k8-opt/bin/external/sorbet_ruby_2_7/}, 'bazel-out/k8-opt/bin/external/sorbet_ruby_2_7/')
+      .sub(%r{^/.*bazel-out/([^/]+)/bin/external/sorbet_ruby_2_7/}, 'bazel-out/\1/bin/external/sorbet_ruby_2_7/')
       .sub(%r{.*test/patch_require.rb:.*:}, 'test/patch_require.rb:<censored>:')
       .sub(%r{^.*tmp\..*:}, '<censored>') # OSX
       .sub(%r{^/tmp.*:}, '<censored>')    # linux
