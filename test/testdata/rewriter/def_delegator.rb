5eff8f37c3b569ef8c1d86cb992b4428ed2d00ed
Richard Xia <richardxia@richardxia.com>
2021-05-13 17:36:53 -0700

    Add support for def_delegators (plural) (#4205)
    * Update DefDelegator rewriter to handle def_delegators.
    
    Forwardable#def_delegators is similar to Forwardable#def_delegator
    except it takes a variable number of positional arguments, one for each
    method to delegate to.
    
    This reorganizes the DefDelegator rewriter to handle both #def_delegator
    and #def_delegators, emitting stub methods and signatures for each
    delegate method.
    
    * Add test cases for #def_delegators.

--- test/testdata/rewriter/def_delegator.rb
+++ test/testdata/rewriter/def_delegator.rb
@@ -6,6 +6,8 @@ class GoodUsages
 
   def_delegator :thing, :foo
   def_delegator :thing, :bar, :aliased_bar
+  def_delegators :thing, :baz, :qux
+  def_delegators :thing # Legal, but does nothing
 
   sig {void}
   def usages
@@ -25,6 +27,22 @@ class GoodUsages
     aliased_bar(hey: true)
     aliased_bar(hey: true) {}
 
+    baz {}
+    baz(1, 2)
+    baz(1, 2) {}
+    baz(1, 2, hey: true)
+    baz(1, 2, hey: true) {}
+    baz(hey: true)
+    baz(hey: true) {}
+
+    qux {}
+    qux(1, 2)
+    qux(1, 2) {}
+    qux(1, 2, hey: true)
+    qux(1, 2, hey: true) {}
+    qux(hey: true)
+    qux(hey: true) {}
+
     bar # error: Method `bar` does not exist
   end
 end
@@ -34,11 +52,15 @@ class WorksWithoutExtendingTSig
 
   def_delegator :thing, :foo
   def_delegator :thing, :bar, :aliased_bar
+  def_delegators :thing, :baz, :qux
+  def_delegators :thing
 end
 
 class ErrorsWhenMissingForwardable
   def_delegator :thing, :foo # error: Method `def_delegator` does not exist
   def_delegator :thing, :bar, :aliased_bar # error: Method `def_delegator` does not exist
+  def_delegators :thing, :baz, :qux # error: Method `def_delegators` does not exist
+  def_delegators :thing # error: Method `def_delegators` does not exist
 end
 
 class IgnoredUsages
@@ -62,6 +84,14 @@ class IgnoredUsages
   #             ^^^ error: Expected `T.any(Symbol, String)` but found `Integer(234)` for argument `accessor`
   def_delegator :thing => :foo # error: Not enough arguments provided for method `Forwardable#def_delegator
               # ^^^^^^^^^^^^^^ error: Expected `T.any(Symbol, String)` but found `{thing: Symbol(:foo)}` for argument `accessor`
+
+
+  def_delegators # error: Not enough arguments provided for method `Forwardable#def_delegators`
+  def_delegators :thing, :foo, :bar, kwarg: :thing # error: Expected `Symbol` but found `{kwarg: Symbol(:thing)}` for argument `methods`
+  def_delegators :foo, kwarg1: :thing, kwarg2: local # error: Expected `Symbol` but found `{kwarg1: Symbol(:thing), kwarg2: Integer(0)}` for argument `methods`
+  def_delegators :foo, local => :thing # error: Expected `Symbol` but found `{Integer(0) => Symbol(:thing)}` for argument `methods`
+  def_delegators 234, :foo # error: Expected `T.any(Symbol, String)` but found `Integer(234)` for argument `accessor`
+  def_delegators :thing => :foo # error: Expected `T.any(Symbol, String)` but found `{thing: Symbol(:foo)}` for argument `accessor`
 end
 
 class EnumerableUsage
