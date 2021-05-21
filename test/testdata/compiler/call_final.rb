ba35f9f5a6193c8cc7ab57e93333e3c2ca7dd78a
Nathan Froyd <froydnj@gmail.com>
2021-05-21 19:11:15 -0400

    convert call_final.rb to use filecheck instead of exp files (#449)

--- test/testdata/compiler/call_final.rb
+++ test/testdata/compiler/call_final.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 # typed: true
 # compiled: true
-# compiled: true
+# run_filecheck: INITIAL
+# run_filecheck: OPT
+
 class A; 
   T::Sig::WithoutRuntime.sig(:final) {params(n: Integer).returns(Integer)}
   def self.foo(n)
@@ -13,3 +15,11 @@ def caller(a)
   A.foo(a)
 end
 end
+
+# INITIAL-LABEL: define i64 @"func_B#caller"
+# INITIAL: call i64 @sorbet_callFuncDirect(%struct.FunctionInlineCache* @{{.*@func_A.foo}}
+# INITIAL{LITERAL}: }
+
+# OPT-LABEL: define i64 @"func_B#caller"
+# OPT-NOT: sorbet_callFuncWithCache
+# OPT{LITERAL}: }
