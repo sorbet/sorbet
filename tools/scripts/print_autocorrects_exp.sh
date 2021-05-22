b8eb64b0a2d70f9efdaea43570f8205f67ae6a64
Jake Zimmerman <zimmerman.jake@gmail.com>
2021-05-21 22:14:51 -0700

    Incorporate autocorrects.exp into update_testdata_exp.sh

---
+++ tools/scripts/print_autocorrects_exp.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -euo pipefail
+
+if [ "$#" -eq 0 ]; then
+  >&2 echo "usage: $0 src.rb [...] > src.rb.autocorrects.rb"
+  exit 1
+fi
+
+srcs=("${@}")
+
+tmpdir="$(mktemp -d)"
+trap 'rm -rf "$tmpdir"' EXIT
+
+srcs_basenames=()
+for src in "${srcs[@]}"; do
+  src_basename="$(basename "$src")"
+  if [ -f "$tmpdir/$src_basename" ]; then
+    >&2 echo "error: duplicate file basenames: $src_basename"
+    exit 1
+  fi
+
+  srcs_basenames+=("$src_basename")
+  cp "$src" "$tmpdir"
+done
+
+cwd="$PWD"
+pushd "$tmpdir" &> /dev/null
+
+"$cwd"/bazel-bin/main/sorbet \
+  --silence-dev-message \
+  --suppress-non-critical \
+  --censor-for-snapshot-tests \
+  --autocorrect \
+  --max-threads 0 \
+  "${srcs_basenames[@]}" \
+  &> /dev/null || true
+
+i=-1
+for src in "${srcs[@]}"; do
+  i=$((i + 1))
+  src_basename="$tmpdir/${srcs_basenames[$i]}"
+  echo "# -- $src --"
+  cat "$src_basename"
+  echo '# ------------------------------'
+done
+
+popd &> /dev/null
