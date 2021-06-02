d03138f7a6001a5b1338ac6ff2973560dd15c86b
Jake Zimmerman <zimmerman.jake@gmail.com>
2021-06-02 13:01:54 -0700

    Add another test

--- test/testdata/infer/boolean_t_boolean.rb
+++ test/testdata/infer/boolean_t_boolean.rb
@@ -13,6 +13,15 @@ class FalseClass
   include Boolean
 end
 
+class BadBang
+  extend T::Sig
+
+  sig {returns(String)}
+  def !()
+    'bad bang override'
+  end
+end
+
 sig {params(arg0: T.untyped).returns(Boolean)}
 def returns_boolean(arg0)
   false
@@ -31,4 +40,9 @@ end
 sig {returns(T::Boolean)}
 def can_have_spaces
   returns_boolean T.unsafe(nil) # error: Expected `T::Boolean` but found `Boolean`
+end
+
+sig {params(x: BadBang).returns(T::Boolean)}
+def should_not_suggest_bang_bang(x)
+  x # error: Expected `T::Boolean` but found `Boolean`
 end
