78b94d511932293fc6729866eb32c3ad06cbb700
Jake Zimmerman <zimmerman.jake@gmail.com>
2021-06-03 11:01:26 -0700

    Add Boolean -> T::Boolean autocorrect (#4229)
    * Add Boolean -> T::Boolean autocorrect
    
    * Add a test
    
    * Add another test
    
    * Fix typo

--- core/TypeDrivenAutocorrect.cc
+++ core/TypeDrivenAutocorrect.cc
@@ -18,6 +18,15 @@ void TypeDrivenAutocorrect::maybeAutocorrect(const GlobalState &gs, ErrorBuilder
         if (!withoutNil.isBottom() &&
             Types::isSubTypeUnderConstraint(gs, constr, withoutNil, expectedType, UntypedMode::AlwaysCompatible)) {
             e.replaceWith("Wrap in `T.must`", loc, "T.must({})", loc.source(gs).value());
+        } else if (Types::isSubTypeUnderConstraint(gs, constr, expectedType, Types::Boolean(),
+                                                   UntypedMode::AlwaysCompatible)) {
+            if (core::isa_type<ClassType>(actualType)) {
+                auto classSymbol = core::cast_type_nonnull<ClassType>(actualType).symbol;
+                if (classSymbol.exists() && classSymbol.data(gs)->owner == core::Symbols::root() &&
+                    classSymbol.data(gs)->name == core::Names::Constants::Boolean()) {
+                    e.replaceWith("Prepend `!!`", loc, "!!({})", loc.source(gs).value());
+                }
+            }
         }
     }
 }
