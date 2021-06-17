04e65d60d699da47f12e04533c2e7b928d772661
Nathan Froyd <froydnj@gmail.com>
2021-06-17 04:45:28 -0900

    fix test to be more precise

--- test/testdata/desugar/oror_t_let.rb
+++ test/testdata/desugar/oror_t_let.rb
@@ -3,9 +3,10 @@ extend T::Sig
 
 def returns_untyped; end
 
-sig {params(customer: T.nilable(Integer)).returns(T.untyped)}
+sig {params(customer: T.nilable(Integer)).returns(Integer)}
 def call(customer)
   customer ||= T.let(returns_untyped, Integer)
+  T.reveal_type(customer) # error: Revealed type: `Integer`
   customer
 end
 
