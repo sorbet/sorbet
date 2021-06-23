95244d14c5e3ea5d0bd55fe76acf455981668c12
Nathan Froyd <froydnj@gmail.com>
2021-06-23 06:12:02 -0900

    add some tests for T::Utils.signature_with_method

--- gems/sorbet-runtime/test/types/utils.rb
+++ gems/sorbet-runtime/test/types/utils.rb
@@ -13,5 +13,29 @@ module Opus::Types::Test
         assert(unwrapped.subtype_of?(T.any(String, Float)))
       end
     end
+
+    describe 'T::Utils.signature_for_method' do
+      it 'returns nil on methods without sigs' do
+        c = Class.new do
+          def no_sig; end
+        end
+        assert_nil(T::Utils.signature_for_method(c.instance_method(:no_sig)))
+      end
+
+      it 'returns things on methods with sigs' do
+        c = Class.new do
+          extend T::Sig
+          sig {returns(Integer)}
+          def sigfun
+            85
+          end
+        end
+        sfm = T::Utils.signature_for_method(c.instance_method(:sigfun))
+        refute_nil(sfm)
+        assert_equal(:sigfun, sfm.method.name)
+        assert_equal(:sigfun, sfm.method_name)
+        assert_equal('Integer', sfm.return_type.name)
+      end
+    end
   end
 end
