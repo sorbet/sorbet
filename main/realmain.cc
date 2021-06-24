d7c7cf8290a295aa76c85593a10e70c1b9907774
John Vilk <jvilk@stripe.com>
2021-06-24 18:38:56 +0000

    Add a timer to printing steps.

--- main/realmain.cc
+++ main/realmain.cc
@@ -286,12 +286,15 @@ void runAutogen(const core::GlobalState &gs, options::Options &opts, const autog
         }
     }
 
-    for (auto &elem : merged) {
-        if (opts.print.Autogen.enabled) {
-            opts.print.Autogen.print(elem.strval);
-        }
-        if (opts.print.AutogenMsgPack.enabled) {
-            opts.print.AutogenMsgPack.print(elem.msgpack);
+    {
+        Timer timeit(logger, "autogenAutoloaderPrint");
+        for (auto &elem : merged) {
+            if (opts.print.Autogen.enabled) {
+                opts.print.Autogen.print(elem.strval);
+            }
+            if (opts.print.AutogenMsgPack.enabled) {
+                opts.print.AutogenMsgPack.print(elem.msgpack);
+            }
         }
     }
     if (opts.print.AutogenAutoloader.enabled) {
