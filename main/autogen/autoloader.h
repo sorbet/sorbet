e48620d351831b82f1b890ad64c1cb84ecaea815
John Vilk <43145439+jvilk-stripe@users.noreply.github.com>
2021-06-28 14:26:03 -0700

    Autogen: Parallelize writing autoloader file outputs. (#4308)
    Significantly speeds up autogen on Stripe's codebase.

--- main/autogen/autoloader.h
+++ main/autogen/autoloader.h
@@ -5,6 +5,10 @@
 #include "main/options/options.h"
 #include <string_view>
 
+namespace sorbet {
+class WorkerPool;
+}
+
 namespace sorbet::autogen {
 
 // Contains same information as `realmain::options::AutoloaderConfig` except with `core::NameRef`s
@@ -123,15 +127,11 @@ private:
 
 class AutoloadWriter {
 public:
-    static void writeAutoloads(const core::GlobalState &gs, const AutoloaderConfig &, const std::string &path,
-                               const DefTree &root);
+    static void writeAutoloads(const core::GlobalState &gs, WorkerPool &workers, const AutoloaderConfig &,
+                               const std::string &path, const DefTree &root);
 
     static void writePackageAutoloads(const core::GlobalState &gs, const AutoloaderConfig &, const std::string &path,
                                       const std::vector<Package> &packages);
-
-private:
-    static void write(const core::GlobalState &gs, const AutoloaderConfig &, const std::string &path,
-                      UnorderedSet<std::string> &toDelete, const DefTree &node);
 };
 
 } // namespace sorbet::autogen
