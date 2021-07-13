bd0004d0819515177209446e6c85c7106e5e096a
Trevor Elliott <awesomelyawesome@gmail.com>
2021-07-13 15:08:08 -0700

    Add tests for superclass validation

---
+++ test/testdata/resolver/module_superclass.rb
@@ -0,0 +1,20 @@
+# typed: true
+
+# It's okay to subclass BasicObject
+class SubclassBasicObject < BasicObject; end
+
+# It's okay to subclass Object
+class SubclassObject < Object; end
+
+# It's okay to subclass Module (the class)
+class SubclassModule2 < Module; end
+
+# It's okay to subclass Class
+class SubclassClass < Class; end
+
+module M; end
+
+# It's not okay to subclass a module
+  class SubclassModule1 < M; end
+# ^^^^^^^^^^^^^^^^^^^^^^^^^ error: The super class `M` of `SubclassModule1` does not
+
