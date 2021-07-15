7b1aaf1c436b1904c39fab91601e2385f7904339
Trevor Elliott <awesomelyawesome@gmail.com>
2021-07-15 14:11:41 -0700

    Add more test cases

--- test/testdata/infer/sealed_single_element.rb
+++ test/testdata/infer/sealed_single_element.rb
@@ -1,4 +1,4 @@
-# typed: true
+# typed: strict
 
 extend T::Sig
 
@@ -12,7 +12,6 @@ module IFoo
 end
 
 x = T.let(IFoo::A.new, IFoo)
-# Sorbet has enough information to know this is always `TrueClass`
 T.reveal_type(IFoo::A === x) # error: Revealed type: `TrueClass`
 
 sig {params(foo: IFoo).returns(Integer)}
@@ -24,3 +23,47 @@ def foo!(foo)
 
   res
 end
+
+
+class Parent
+  extend T::Helpers
+
+  sealed!
+end
+
+class Child < Parent; end
+
+sig {params(x: Parent).returns(Integer)}
+def test1(x)
+  # This case is not exhaustive (Parent.new is a possible value)
+  res = case x
+  when Child then 0
+  end
+
+  res # error: Expected `Integer` but found `T.nilable(Integer)`
+end
+
+bad = T.let(Child.new, Parent)
+T.reveal_type(Child === bad) # error: Revealed type: `T::Boolean`
+
+class AbsParent
+  extend T::Helpers
+
+  abstract!
+  sealed!
+end
+
+class ChildOfAbs < AbsParent; end
+
+sig {params(x: AbsParent).returns(Integer)}
+def test2(x)
+  # This case is exhaustive
+  res = case x
+  when ChildOfAbs then 0
+  end
+
+  res
+end
+
+abs = T.let(ChildOfAbs.new, AbsParent)
+T.reveal_type(ChildOfAbs === abs) # error: Revealed type: `TrueClass`
