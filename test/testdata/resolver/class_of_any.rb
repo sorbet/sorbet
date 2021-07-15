5a853a895a932c2f3dc172bbd42a7a16b5fc06b6
Neil Parikh <neil@stripe.com>
2021-07-15 19:13:58 -0400

    update tests

--- test/testdata/resolver/class_of_any.rb
+++ test/testdata/resolver/class_of_any.rb
@@ -7,7 +7,7 @@ class C; end
 class D < A; end
 
 
-sig { returns(T.class_of(T.any(A, T.any(B, C)))) } # error: `T.class_of` needs a class or module as its argument. Apply `T.class_of` to each element of the `T.any` instead
+sig { returns(T.class_of(T.any(A, T.any(B, C)))) } # error: `T.class_of` must wrap each individual class type, not the outer `T.any`
 def foo; end
 
 
