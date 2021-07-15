c80b7c176301564e49ebbe419848608a112dca91
Julia McClellan <jmcclellan@stripe.com>
2021-07-15 16:00:08 -0400

    combine tests for private method not suggested

--- test/cli/autocorrect-private-not-suggested/autocorrect-private-not-suggested.sh
+++ test/cli/autocorrect-private-not-suggested/autocorrect-private-not-suggested.sh
@@ -1,7 +1,21 @@
+tmp="$(mktemp -d)"
+cp test/cli/autocorrect-private-not-suggested/*.rb "$tmp"
+
+cwd="$(pwd)"
+cd "$tmp" || exit 1
+
+if "$cwd/main/sorbet" --silence-dev-message -a autocorrect-private-not-suggested.rb 2>&1; then
+  echo "Expected to fail!"
+  exit 1
+fi
+
+echo
+echo --------------------------------------------------------------------------
+echo
+
 # verifies that the file is unchanged when suggestions are applied
 # i.e. it does not replace the unknown method with a private one
-tmp=$(mktemp -d)
-cp test/cli/autocorrect-private-not-suggested/*.rb "$tmp"
-main/sorbet --silence-dev-message -a "$tmp"/*.rb 2>/dev/null
-cat "$tmp"/*.rb
-rm -rf "$tmp"
+cat autocorrect-private-not-suggested.rb
+
+rm autocorrect-private-not-suggested.rb
+rm "$tmp"
--- test/testdata/core/autocorrect-private-not-suggested.rb
+++
@@ -1,8 +0,0 @@
-# typed: true
-
-class A
-  private def my_private_method; end
-end
-
-# this flags the method as not existing but does not suggest the private method
-A.new.my_private_metho # error: Method `my_private_metho` does not exist on `A`
