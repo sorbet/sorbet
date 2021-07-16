ee3f6b04330aa3307f722a3a132c3db82102a7e7
Adam Procter <aprocter@stripe.com>
2021-07-16 14:09:27 -0700

    Update test expectations

--- test/testdata/infer/metatype_new.rb
+++ test/testdata/infer/metatype_new.rb
@@ -28,17 +28,20 @@ MyArrayString.new                  # Type alias is unwound by the type we get to
 
 # -- bad --
 
-T.any(Integer, String).new         # error: Cannot call `new` on type `T.any(Integer, String)`
-T.all(Kernel, Comparable).new      # error: Cannot call `new` on type `T.all(Kernel, Comparable)`
-T.nilable(String).new              # error: Cannot call `new` on type `T.nilable(String)`
+T.any(Integer, String).new         # error: Call to method `new` on `T.any(Integer, String)` mistakes a type for a value
+T.all(Kernel, Comparable).new      # error: Call to method `new` on `T.all(Kernel, Comparable)` mistakes a type for a value
+T.nilable(String).new              # error: Call to method `new` on `T.nilable(String)` mistakes a type for a value
 
-T.untyped.new                      # error: Cannot call `new` on type `T.untyped`
-T.self_type.new                    # error: Cannot call `new` on type `T.untyped`
+T.untyped.new                      # error: Call to method `new` on `T.untyped` mistakes a type for a value
+T.self_type.new                    # error: Call to method `new` on `T.untyped` mistakes a type for a value
 
-T.class_of(String).new             # error: Cannot call `new` on type `T.class_of(String)`
-T.class_of(MyGeneric).new          # error: Cannot call `new` on type `T.class_of(MyGeneric)`
-T.class_of(MyGeneric[Integer]).new # error: Cannot call `new` on type `T.class_of(MyGeneric)`
+T.class_of(String).new             # error: Call to method `new` on `T.class_of(String)` mistakes a type for a value
+T.class_of(MyGeneric).new          # error: Call to method `new` on `T.class_of(MyGeneric)` mistakes a type for a value
 
-T.proc.void.new                    # error: Cannot call `new` on declaration builder type
+# Note: the following shouldn't actually parse, but does due to a bug (https://github.com/sorbet/sorbet/issues/4377).
+# If you fix that bug, please feel free to delete the following line.
+T.class_of(MyGeneric[Integer]).new # error: Call to method `new` on `T.class_of(MyGeneric)` mistakes a type for a value
 
-T.noreturn.new                     # error: Cannot call `new` on type `T.noreturn`
+T.proc.void.new                    # error: Call to method `new` on `T.class_of(T.proc)` mistakes a type for a value
+
+T.noreturn.new                     # error: Call to method `new` on `T.noreturn` mistakes a type for a value
