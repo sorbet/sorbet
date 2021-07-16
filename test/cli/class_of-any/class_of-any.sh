3729a6d1ab333fb572138ce8103931781452c5e1
Neil Parikh <neil@stripe.com>
2021-07-16 16:29:35 -0400

    make sure the autocorrect applies in CLI test

--- test/cli/class_of-any/class_of-any.sh
+++ test/cli/class_of-any/class_of-any.sh
@@ -1,2 +1,23 @@
-#!/bin/sh
-main/sorbet --silence-dev-message test/cli/class_of-any/class_of-any.rb 2>&1
+#!/usr/bin/env bash
+
+tmp="$(mktemp -d)"
+infile="test/cli/class_of-any/class_of-any.rb"
+cp "$infile" "$tmp"
+
+cwd="$(pwd)"
+cd "$tmp" || exit 1
+
+if "$cwd/main/sorbet" --silence-dev-message -a class_of-any.rb 2>&1; then
+  echo "Expected to fail!"
+  exit 1
+fi
+
+echo
+echo --------------------------------------------------------------------------
+echo
+
+# Also cat the file, to make that the autocorrect applied
+cat class_of-any.rb
+
+rm class_of-any.rb
+rm "$tmp"
