a8f4066cd2492f1668efa72853a74c836abe679f
Adam Procter <71457435+aprocter-stripe@users.noreply.github.com>
2021-07-20 15:58:50 -0700

    "Pre-pop args for ifuncs just like iseqs" redux (#526)
    * Pre-pop args for ifuncs just like iseqs (old broken approach)
    
    * Add cli test for regression
    
    * Fix the patch
    
    * Update patch to use a boolean for the sp adjustment
    
    * More detailed comment on test case

--- test/testdata/compiler/disabled/return-from-block/lambda_return_compiled_compiled__1.rb
+++ test/testdata/compiler/disabled/return-from-block/lambda_return_compiled_compiled__1.rb
@@ -4,6 +4,10 @@
 
 require_relative './lambda_return_compiled_compiled__2'
 
+# This test is the one that motivates the changes to vm_yield_with_cfunc (see third_party/ruby/is-lambda-ifunc.patch)
+# which pop arguments to ifuncs _before_ calling them. If the VM is not patched, the EC_JUMP_TAG for the "return"
+# statement inside the compiled lambda will jump over the POPN(argc) that is supposed to happen after the ifunc
+# returns.
 puts F0.call(100)
 puts M::F1.call(200)
 puts C::F2.call(400)
