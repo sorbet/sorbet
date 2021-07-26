87c65603abd1e47f442cda429f2524bb8506b647
Nathan Froyd <froydnj@gmail.com>
2021-07-26 10:28:48 -0400

    provide more precise location for (most) kwargs (#4400)

--- test/testdata/rewriter/t_struct/default.rb
+++ test/testdata/rewriter/t_struct/default.rb
@@ -6,7 +6,7 @@ end
 
 T.reveal_type(Default.new.foo) # error: Revealed type: `Integer`
 Default.new(foo: "no")
-#           ^^^^^^^^^ error: Expected `Integer` but found `String("no")` for argument `foo`
+#                ^^^^ error: Expected `Integer` but found `String("no")` for argument `foo`
   Default.new(foo: 3, bar: 4)
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^ error: Unrecognized keyword argument `bar` passed for method `Default#initialize`
 T.reveal_type(Default.new(foo: 3).foo) # error: Revealed type: `Integer`
