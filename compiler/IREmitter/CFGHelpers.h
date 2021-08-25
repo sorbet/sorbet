4521393c81c86ece63b5e6a740f7cc723d289c31
Trevor Elliott <awesomelyawesome@gmail.com>
2021-08-25 11:10:52 -0700

    Only consider exits to a target region

--- compiler/IREmitter/CFGHelpers.h
+++ compiler/IREmitter/CFGHelpers.h
@@ -8,8 +8,13 @@ namespace sorbet::compiler {
 
 class CFGHelpers {
 public:
-    static cfg::BasicBlock *findRubyBlockEntry(cfg::CFG &cfg, int rubyBlockId);
-    static std::vector<cfg::BasicBlock *> findRubyBlockExits(cfg::CFG &cfg, int rubyBlockId);
+    // A heuristic for finding the first block in a region: find the basic-block with the lowest id for the given ruby
+    // block id.
+    static cfg::BasicBlock *findRegionEntry(cfg::CFG &cfg, int regionId);
+
+    // Find the blocks in the target region that are jumped to from the source region. This explicitly ignores the dead
+    // block as a valid jump target.
+    static std::vector<cfg::BasicBlock *> findRegionExits(cfg::CFG &cfg, int targetRegionId, int sourceRegionId);
 };
 
 } // namespace sorbet::compiler
