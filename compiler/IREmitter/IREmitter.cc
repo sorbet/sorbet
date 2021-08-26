2de1ae9693325ca88d62cbe94ead95e38d3d4a31
Nathan Froyd <froydnj@gmail.com>
2021-08-26 13:45:14 -0400

    enable return-from-block over exception frames (#4538)

--- compiler/IREmitter/IREmitter.cc
+++ compiler/IREmitter/IREmitter.cc
@@ -566,28 +566,6 @@ void emitUserBody(CompilerState &base, cfg::CFG &cfg, const IREmitterContext &ir
                 [&](cfg::Return *i) {
                     isTerminated = true;
 
-                    // If we see a 'return' statement that crosses both block and exception frames, emit an error
-                    // message.
-                    bool sawBlockType = false;
-                    bool sawExceptionType = false;
-
-                    for (int blockId = bb->rubyBlockId; blockId != 0; blockId = irctx.rubyBlockParent[blockId]) {
-                        auto funcType = irctx.rubyBlockType[blockId];
-                        if (funcType == FunctionType::Block) {
-                            sawBlockType = true;
-                        } else if (funcType == FunctionType::Rescue || funcType == FunctionType::Ensure ||
-                                   funcType == FunctionType::ExceptionBegin) {
-                            sawExceptionType = true;
-                        }
-                    }
-
-                    if (sawBlockType && sawExceptionType) {
-                        failCompilation(
-                            cs, core::Loc(cs.file, bind.loc),
-                            "return statements crossing both block and exception frames are not yet implemented");
-                        return;
-                    }
-
                     auto *var = Payload::varGet(cs, i->what.variable, builder, irctx, bb->rubyBlockId);
 
                     if (irctx.rubyBlockType[bb->rubyBlockId] == FunctionType::Block) {
