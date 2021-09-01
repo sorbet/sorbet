4b75ee4ac03f244bad5c6489e85b8dae640f4bf8
David Judd <david.a.judd@gmail.com>
2021-09-01 10:49:24 -0700

    comment

--- gems/sorbet-runtime/lib/types/private/types/simple_pair_union.rb
+++ gems/sorbet-runtime/lib/types/private/types/simple_pair_union.rb
@@ -24,6 +24,10 @@ class T::Private::Types::SimplePairUnion < T::Types::Union
 
   # @override Union
   def types
+    # We reconstruct the simple types rather than just storing them because
+    # (1) this is normally not a hot path and (2) we want to keep the instance
+    # variable count <= 3 so that we can fit in a 40 byte heap entry along
+    # with object headers.
     @types ||= [
       T::Types::Simple::Private::Pool.type_for_module(@raw_a),
       T::Types::Simple::Private::Pool.type_for_module(@raw_b),
