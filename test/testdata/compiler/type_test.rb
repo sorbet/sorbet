c93ddb78f28323cc79e3ca94d3f00acee4e8e597
Trevor Elliott <awesomelyawesome@gmail.com>
2021-08-31 17:12:45 -0700

    Fix filecheck tests

--- test/testdata/compiler/type_test.rb
+++ test/testdata/compiler/type_test.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 # typed: true
 # compiled: true
-# run_filecheck: LOWERED
 # run_filecheck: OPT
 
 class Tests
@@ -15,12 +14,9 @@ class Tests
   def self.bar2(x)
   end
 
-# LOWERED-LABEL: define i64 @"func_Hello#foo"
-# LOWERED: tail call i64 (i64, ...) @sorbet_i_allTypeTested(i64 %rawArg_b, i64 %rawArg_b)
-# LOWERED{LITERAL}: }
-
-# OPT-LABEL: define i64 @"func_Hello#foo"
-# OPT: tail call %struct.rb_control_frame_struct* @sorbet_pushCfuncFrame(i1 zeroext true
+# OPT-LABEL: define i64 @func_Tests.test1
+# TODO(trevor) we're not adding typeTested assertions on the LHS of a T.let
+# OPT: tail call %struct.rb_control_frame_struct* @sorbet_pushCfuncFrame(i1 noundef zeroext true
 # OPT{LITERAL}: }
   def self.test1(a, b)
     y = T.let(a, Integer)
@@ -31,12 +27,8 @@ class Tests
     end
   end
 
-# LOWERED-LABEL: define i64 @"func_Hello#foo2"
-# LOWERED: tail call i64 (i64, ...) @sorbet_i_allTypeTested
-# LOWERED{LITERAL}: }
-
-# OPT-LABEL: define i64 @"func_Hello#foo2"
-# OPT: tail call %struct.rb_control_frame_struct* @sorbet_pushCfuncFrame(i1 zeroext false
+# OPT-LABEL: define i64 @func_Tests.test2
+# OPT: tail call %struct.rb_control_frame_struct* @sorbet_pushCfuncFrame(i1 noundef zeroext false
 # OPT{LITERAL}: }
   def self.test2(a, b, c)
     y = T.let(b, Integer)
@@ -44,12 +36,8 @@ class Tests
     bar2 d
   end
 
-# LOWERED-LABEL: define i64 @"func_Hello#baz"
-# LOWERED: tail call i64 (i64, ...) @sorbet_i_allTypeTested(i64 %rawArg_a, i64 %rawArg_b)
-# LOWERED{LITERAL}: }
-
-# OPT-LABEL: define i64 @"func_Hello#baz"
-# OPT: tail call %struct.rb_control_frame_struct* @sorbet_pushCfuncFrame(i1 zeroext false
+# OPT-LABEL: define i64 @func_Tests.test3
+# OPT: tail call %struct.rb_control_frame_struct* @sorbet_pushCfuncFrame(i1 noundef zeroext false
 # OPT{LITERAL}: }
   def self.test3(a, b, c)
     if c
