0c65f6b08c54b52d3b9f85e6dc5ea5fe7b8cbb1a
Jake Zimmerman <zimmerman.jake@gmail.com>
2021-09-01 16:49:17 -0700

    Add failing test case

--- test/testdata/rewriter/shard_by_merchant_prop.rb
+++ test/testdata/rewriter/shard_by_merchant_prop.rb
@@ -1,7 +1,16 @@
 # typed: true
 
+module Opus::Autogen::Tokens
+  # This isn't exactly right but it's close
+  AccountModelMerchantToken = T.type_alias {String}
+end
+
 module ShardByMerchant
   def merchant_prop(name: :merchant, override: false, without_accessors: false); end
+end
+
+module ShardByMerchantBase
+  def merchant_token_prop(name: :merchant, override: false, without_accessors: false); end
 end
 
 class MerchantPropModel
@@ -11,6 +20,15 @@ class MerchantPropModel
   merchant_prop
 end
 
+class MerchantTokenPropModel
+  include T::Props
+  extend ShardByMerchantBase
+
+  merchant_token_prop
+end
+
 T.reveal_type(MerchantPropModel.new.merchant) # error: Revealed type: `String`
 MerchantPropModel.new.merchant = "hi" # error: Method `merchant=` does not exist
 
+T.reveal_type(MerchantTokenPropModel.new.merchant) # error: Revealed type: `T.untyped`
+MerchantTokenPropModel.new.merchant = nil # error: Method `merchant=` does not exist
