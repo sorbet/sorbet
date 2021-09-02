76258f74415a5131cd44d4933bdf63422120539b
Nathan Froyd <froydnj@gmail.com>
2021-09-02 05:33:45 -0900

    fix sorbet_magic_mergeHashHelper prototype

--- compiler/IREmitter/Payload/patches/hash.c
+++ compiler/IREmitter/Payload/patches/hash.c
@@ -1,6 +1,6 @@
 
 // A faster version of merge!, under the assumption that we don't pass blocks, and only pass a single arg.
-VALUE sorbet_magic_mergeHashHelper(self, hash) {
+VALUE sorbet_magic_mergeHashHelper(VALUE self, VALUE hash) {
     rb_hash_foreach(hash, rb_hash_update_i, self);
     return self;
 }
