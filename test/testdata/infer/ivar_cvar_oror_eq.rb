a732f367e2f2e859e51df27c99b2acf31e0031b0
Nathan Froyd <froydnj@gmail.com>
2021-09-10 11:56:49 -0400

    strengthen the tests in ivar_cvar_oror_eq.rb to cover more cases (#4593)

--- test/testdata/infer/ivar_cvar_oror_eq.rb
+++ test/testdata/infer/ivar_cvar_oror_eq.rb
@@ -1,23 +1,56 @@
 # typed: true
 
-# Test that Sorbet correctly infers the types of @@var in f and @ivar in g.
+# Test that Sorbet correctly infers the types of class variables and instance
+# variables when they are used as the LHS of ||=.
 
 class C
   extend T::Sig
 
-  @@var = T.let(nil, T.nilable(Integer))
+  @@nilable_var = T.let(nil, T.nilable(Integer))
+  @@falsy_var = T.let(false, T.any(FalseClass, Integer))
+  @@nevernil = T.let(81, Integer)
 
+  sig {void}
   def initialize
     @ivar = T.let(nil, T.nilable(String))
+    @ivar
   end
 
   sig {params(x: Integer).returns(Integer)}
-  def self.f(x)
-    @@var ||= x
+  def self.f1(x)
+    @@nilable_var ||= x
+  end
+
+  sig {params(x: Integer).returns(Integer)}
+  def self.f2(x)
+    @@nevernil ||= x # error: This code is unreachable
+  end
+
+  sig {params(x: Integer).returns(Integer)}
+  def self.f3(x)
+    @@nilable_var ||= x
+    @@nilable_var
+  end
+
+  sig {params(x: Integer).returns(Integer)}
+  def self.f4(x)
+    @@falsy_var ||= x
+    @@falsy_var
+  end
+
+  sig {params(x: Integer).returns(Integer)}
+  def self.f5(x)
+    @@falsy_var ||= x
   end
 
   sig {params(x: String).returns(String)}
-  def g(x)
+  def g1(x)
     @ivar ||= x
+  end
+
+  sig {params(x: String).returns(String)}
+  def g2(x)
+    @ivar ||= x
+    @ivar
   end
 end
