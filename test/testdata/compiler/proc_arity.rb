7a19c86cb8b71eaf6a48200ffbadba713122f862
Trevor Elliott <awesomelyawesome@gmail.com>
2021-09-27 15:13:31 -0700

    Enable passing test, and add extra cases

--- test/testdata/compiler/proc_arity.rb
+++ test/testdata/compiler/proc_arity.rb
@@ -24,6 +24,15 @@ test_case("2 defaulted", lambda {|a=0,b=nil|})
 test_case("1 required, 1 defaulted", lambda {|a, b=nil|})
 test_case("1 required, 2 defaulted", lambda {|a, b=nil, c=nil|})
 test_case("2 required, 2 defaulted", lambda {|a, b, c=nil, d=nil|})
+test_case("2 required, 2 defaulted, break", lambda {|a, b, c=nil, d=nil| break})
+
+# keywords
+test_case("1 required", lambda {|a:|})
+test_case("2 required", lambda {|a:, b:|})
+test_case("1 optional", lambda {|a: 10|})
+test_case("2 optional", lambda {|a: 10, b: 20|})
+test_case("1 required, 1 optional", lambda {|a:, b: 20|})
+test_case("2 required, 1 optional", lambda {|a:, b:, c: 30|})
 
 # repeated args
 test_case("repeated", lambda {|*a|})
@@ -32,3 +41,4 @@ test_case("2 required, repeated", lambda {|a, b, *c|})
 test_case("1 required, 1 defaulted, repeated", lambda {|a, b=10, *c|})
 test_case("2 required, 1 defaulted, repeated", lambda {|a, b, c=10, *d|})
 test_case("2 required, 2 defaulted, repeated", lambda {|a, b, c=10, d=nil, *e|})
+test_case("2 required, 2 defaulted, repeated, break", lambda {|a, b, c=10, d=nil, *e| break})
