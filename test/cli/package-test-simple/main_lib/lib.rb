ec22a0d8868896677b619d71dd3486fe65185ddb
ngroman <ngroman@gmail.com>
2021-09-29 12:27:10 -0700

    cli test fixup

--- test/cli/package-test-simple/main_lib/lib.rb
+++ test/cli/package-test-simple/main_lib/lib.rb
@@ -5,5 +5,6 @@ class Project::MainLib::Lib
   Project::Util::MyUtil.new
 
   # ERROR: Normal code is not allowed to access names from `test_import`
+  # TODO(ngroman) Move this error enforcement into Sorbet
   Project::TestOnly::SomeHelper.new
 end
