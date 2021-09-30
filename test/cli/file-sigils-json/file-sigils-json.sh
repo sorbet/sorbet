8bb1f3ce34631761868a959f5fb3f297d5b9b5f1
Jake Zimmerman <zimmerman.jake@gmail.com>
2021-09-30 15:25:36 -0700

    --compiled-files-json -> --file-sigils-json (#4646)
    * --compiled-files-json -> --file-sigils-json
    
    We might want to put more stuff in this eventually; I figure we may as
    well rename it earlier rather than later.
    
    * Rename test
    
    * Rename some more tests
    
    * sto //test/cli:update_help

--- test/cli/file-sigils-json/file-sigils-json.sh
+++ test/cli/file-sigils-json/file-sigils-json.sh
@@ -6,4 +6,4 @@ sorbet="$PWD/main/sorbet"
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-"$sorbet" --silence-dev-message --print compiled-files-json ./*.rb 2>&1
+"$sorbet" --silence-dev-message --print file-sigils-json ./*.rb 2>&1
