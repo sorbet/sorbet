56d43980498766312bcc8d10e8e9ae14196498c1
Jake Zimmerman <zimmerman.jake@gmail.com>
2021-10-01 14:11:10 -0700

    Test more kinds of sorbet invocations

--- test/cli/file-sigils-json/file-sigils-json.sh
+++ test/cli/file-sigils-json/file-sigils-json.sh
@@ -6,4 +6,9 @@ sorbet="$PWD/main/sorbet"
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
+echo "----- ./*.rb -----------------------------------------------------------"
 "$sorbet" --silence-dev-message --print file-sigils-json ./*.rb 2>&1
+echo "----- --dir=. ----------------------------------------------------------"
+"$sorbet" --silence-dev-message --print file-sigils-json --dir=. 2>&1
+echo "----- *.rb -------------------------------------------------------------"
+"$sorbet" --silence-dev-message --print file-sigils-json -- *.rb 2>&1
