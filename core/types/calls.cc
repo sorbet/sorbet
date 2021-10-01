8ebc4a01eec59885e91506a0301e0ddf8c0ad7e1
Jake Zimmerman <zimmerman.jake@gmail.com>
2021-10-01 12:09:12 -0700

    Fix implementation of suggest constant type

--- core/types/calls.cc
+++ core/types/calls.cc
@@ -2376,15 +2376,17 @@ public:
         ENFORCE(args.args.size() == 1);
         auto ty = core::Types::widen(gs, args.args.front()->type);
         auto loc = args.argLoc(0);
+        auto argLocExists = true;
         if (!loc.exists()) {
             // This might be because our argument was an EmptyTree (`begin; end`). Fall back to
             // using the loc of the whole send.
+            argLocExists = false;
             loc = args.callLoc();
         }
         if (auto e = gs.beginError(loc, core::errors::Infer::UntypedConstantSuggestion)) {
             e.setHeader("Constants must have type annotations with `{}` when specifying `{}`", "T.let",
                         "# typed: strict");
-            if (!ty.isUntyped() && loc.exists() && loc != args.callLoc()) {
+            if (!ty.isUntyped() && loc.exists() && argLocExists) {
                 // (skip the autocorrect if we had to fall back to using callLoc, because using that
                 // will suggest something syntactically invalid like `T.let(U = begin; end, NilClass))`
                 e.replaceWith(fmt::format("Initialize as `{}`", ty.show(gs)), loc, "T.let({}, {})",
