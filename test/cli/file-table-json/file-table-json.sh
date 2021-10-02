b23ddeb466577e00824a64a7e63adf7b7d669915
Jake Zimmerman <zimmerman.jake@gmail.com>
2021-10-01 17:21:39 -0700

    stog //test/cli:update_file-table-json

--- test/cli/file-table-json/file-table-json.sh
+++ test/cli/file-table-json/file-table-json.sh
@@ -2,4 +2,11 @@
 
 set -euo pipefail
 
+echo "----- -p file-table-json ------------------------------------------------"
+main/sorbet --silence-dev-message -p file-table-json test/cli/file-table-json/file-table-json.rb
+echo "----- --no-stdlib -p file-table-json -----------------------------------"
 main/sorbet --silence-dev-message --no-stdlib -p file-table-json test/cli/file-table-json/file-table-json.rb
+echo "----- -p file-table-full-json ------------------------------------------"
+main/sorbet --silence-dev-message -p file-table-full-json test/cli/file-table-json/file-table-json.rb
+echo "----- --no-stdlib -p file-table-full-json ------------------------------"
+main/sorbet --silence-dev-message --no-stdlib -p file-table-full-json test/cli/file-table-json/file-table-json.rb
