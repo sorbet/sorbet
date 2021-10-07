16e5e5ec795052896c2779f2f9404817cbe3a2a8
Trevor Elliott <awesomelyawesome@gmail.com>
2021-10-06 17:29:30 -0700

    Handle kwrest with now kwargs when setting up sorbet parameter info (#4693)
    * Make sure to allocate space for kwrest args
    
    * Enable passing test
    
    * More tests for .parameters

--- test/testdata/compiler/parameters.rb
+++ test/testdata/compiler/parameters.rb
@@ -2,12 +2,44 @@
 # typed: true
 # compiled: true
 
-def foo(a, b=1, c:, d:2)
-end
+def test1; end
+p method(:test1).parameters
 
-puts method(:foo).parameters
+def test2(a); end
+p method(:test2).parameters
 
-def noparam
-end
+def test3(a, b=10); end
+p method(:test3).parameters
 
-puts method(:noparam).parameters
+def test4(a:); end
+p method(:test4).parameters
+
+def test5(a:, b: 10); end
+p method(:test4).parameters
+
+def test6(*rest); end
+p method(:test6).parameters
+
+def test7(**kwrest); end
+p method(:test7).parameters
+
+def test8(a, b:); end
+p method(:test8).parameters
+
+def test9(a, b=10, c:); end
+p method(:test9).parameters
+
+def test10(a, b:, c: 10); end
+p method(:test10).parameters
+
+def test11(a, b=10, c:, d: 10); end
+p method(:test11).parameters
+
+def test12(*rest, a:, b: 10); end
+p method(:test12).parameters
+
+def test13(a, b=10, *rest, c:, d: 10); end
+p method(:test13).parameters
+
+def test14(a, b=10, *rest, c:, d: 10, **kwrest); end
+p method(:test14).parameters
