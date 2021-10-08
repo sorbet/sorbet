4694d2657dc25b654e499321a497c1f4048315fb
Nathan Froyd <froydnj@gmail.com>
2021-10-08 17:55:49 -0400

    add a test for passing multiple defaulted kwargs (#4704)
    * add a test for passing multiple defaulted kwargs
    
    * make the test slightly more general

--- test/testdata/compiler/keyword_arg.rb
+++ test/testdata/compiler/keyword_arg.rb
@@ -14,3 +14,21 @@ puts my_name(name: "Paul", prefix: "Master")
 puts my_name(name: "Paul")
 
 puts f(1, {x: 5}, name: "Nathan")
+
+def builder(name,
+            default: :default_default,
+            validate: :validate_default,
+            overridable: :overridable_default,
+            implied: :implied_default,
+            skip_get: :skip_get_default,
+            skip_set: :skip_set_default,
+            inherit: :inherit_default)
+  p name
+  p default, validate, overridable, implied, skip_get, skip_set, inherit
+end
+
+builder(:first_last, default: :default_passed, inherit: :inherit_passed)
+builder(:middles, validate: :validate_passed, implied: :implied_passed)
+builder(:consecutive, skip_get: :skip_get_passed, skip_set: :skip_set_passed)
+builder(:three_args, overridable: :overridable_passed, skip_get: :skip_get_passed, inherit: :inherit_passed)
+builder(tricky: :keyword, args: :passed, as: :hash)
