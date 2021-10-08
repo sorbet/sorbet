060aba1102c25aa6b7b310df372a655bcb9bea3a
Nathan Froyd <froydnj@gmail.com>
2021-10-08 13:59:29 -0400

    add test mixing optional and keyword args (#4702)

--- test/testdata/compiler/keyword_arg.rb
+++ test/testdata/compiler/keyword_arg.rb
@@ -5,5 +5,12 @@ def my_name(name:, prefix: "Mr")
   prefix + " " + name
 end
 
+def f(a, b=3, name:, prefix: "Mr")
+  p a, b
+  prefix + " " + name
+end
+
 puts my_name(name: "Paul", prefix: "Master")
 puts my_name(name: "Paul")
+
+puts f(1, {x: 5}, name: "Nathan")
