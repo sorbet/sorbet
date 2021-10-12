a33dd2e6d23b02fed96d95ecc7cbd61666b18779
Jake Zimmerman <zimmerman.jake@gmail.com>
2021-10-11 19:52:18 -0700

    Move getSubclasses to LSP-specific helper (#4703)
    * Move getSubclasses to LSP-specific helper
    
    `getSubclasses` is a very slow operation, and does not belong outside of
    LSP, because it is not a primitive that should ever be used in Sorbet's
    core type checking pipeline.
    
    History: this started out as a helper function in an anonymous namespace
    in rename.cc. `workspace/rename` is fine with having the rename request
    take a little bit, because it more than makes up for how much time the
    user would have had to spend renaming things manually.
    
    The helper was moved under Symbol to be shared by
    `textDocumement/implementation`. That was overly eager--it should have
    been factored to an LSP-specific location instead.
    
    I've also added a comment to capture these considerations for the
    future.
    
    * forgot one

--- main/lsp/requests/implementation.cc
+++ main/lsp/requests/implementation.cc
@@ -39,7 +39,9 @@ const MethodImplementationResults findMethodImplementations(const core::GlobalSt
     }
 
     auto owningClassSymbolRef = owner.asClassOrModuleRef();
-    auto childClasses = owningClassSymbolRef.getSubclasses(gs, false);
+    auto includeOwner = false;
+    // Scans whole symbol table. This is slow, and we might need to make this faster eventually.
+    auto childClasses = getSubclassesSlow(gs, owningClassSymbolRef, includeOwner);
     auto methodName = method.data(gs)->name;
     for (const auto &childClass : childClasses) {
         auto methodImplementation = childClass.data(gs)->findMember(gs, methodName);
@@ -123,8 +125,9 @@ unique_ptr<ResponseMessage> ImplementationTask::runRequest(LSPTypecheckerDelegat
             return response;
         }
 
+        auto includeClassSymbol = false;
         auto classOrModuleRef = classSymbol.asClassOrModuleRef();
-        auto childClasses = classOrModuleRef.getSubclasses(gs, false);
+        auto childClasses = getSubclassesSlow(gs, classOrModuleRef, includeClassSymbol);
         for (const auto &childClass : childClasses) {
             for (auto loc : childClass.data(gs)->locs()) {
                 addLocIfExists(gs, result, loc);
