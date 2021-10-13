8b1d8fbb9f1bc192771f129592ab90cb3efe8499
ngroman <ngroman@gmail.com>
2021-10-13 16:17:04 -0700

    Suppress errors::Resolver::RecursiveTypeAlias in autogen

--- main/realmain.cc
+++ main/realmain.cc
@@ -572,6 +572,7 @@ int realmain(int argc, char *argv[]) {
             gs->suppressErrorClass(core::errors::Namer::InvalidClassOwner.code);
             gs->suppressErrorClass(core::errors::Namer::ModuleKindRedefinition.code);
             gs->suppressErrorClass(core::errors::Resolver::StubConstant.code);
+            gs->suppressErrorClass(core::errors::Resolver::RecursiveTypeAlias.code);
 
             indexed = pipeline::package(*gs, move(indexed), opts, *workers);
             indexed = move(pipeline::name(*gs, move(indexed), opts, *workers).result());
