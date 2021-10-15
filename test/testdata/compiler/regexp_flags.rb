fd86f108d46b04148bb9c656aa13f1970b5b8547
Nathan Froyd <froydnj@gmail.com>
2021-10-15 04:37:14 -0900

    fix the filecheck expectations for the changed test, doh

--- test/testdata/compiler/regexp_flags.rb
+++ test/testdata/compiler/regexp_flags.rb
@@ -36,12 +36,12 @@ def ix_flag(x)
 end
 
 # INITIAL-LABEL: define i64 @"func_Object#7ix_flag"(
-# INITIAL: load i64, i64* @rubyRegexpFrozen_pattern_3
+# INITIAL: load i64, i64* @"rubyRegexpFrozen_pattern # Comment_3"
 # INITIAL-LITERAL: }
 
 # OPT-LABEL: define i64 @"func_Object#7ix_flag"(
 # OPT-NOT: ic_new
-# OPT: load i64, i64* @rubyRegexpFrozen_pattern_3
+# OPT: load i64, i64* @"rubyRegexpFrozen_pattern # Comment_3"
 # OPT-NOT: ic_new
 # OPT-LITERAL: }
 
@@ -50,12 +50,12 @@ def ixm_flag(x)
 end
 
 # INITIAL-LABEL: define i64 @"func_Object#8ixm_flag"(
-# INITIAL: load i64, i64* @rubyRegexpFrozen_pattern_7
+# INITIAL: load i64, i64* @"rubyRegexpFrozen_pattern.more # Comment_7"
 # INITIAL-LITERAL: }
 
 # OPT-LABEL: define i64 @"func_Object#8ixm_flag"(
 # OPT-NOT: ic_new
-# OPT: load i64, i64* @rubyRegexpFrozen_pattern_7
+# OPT: load i64, i64* @"rubyRegexpFrozen_pattern.more # Comment_7"
 # OPT-NOT: ic_new
 # OPT-LITERAL: }
 
@@ -64,12 +64,12 @@ def x_flag(x)
 end
 
 # INITIAL-LABEL: define i64 @"func_Object#6x_flag"(
-# INITIAL: load i64, i64* @rubyRegexpFrozen_pattern_2
+# INITIAL: load i64, i64* @"rubyRegexpFrozen_pattern # Comment_2"
 # INITIAL-LITERAL: }
 
 # OPT-LABEL: define i64 @"func_Object#6x_flag"(
 # OPT-NOT: ic_new
-# OPT: load i64, i64* @rubyRegexpFrozen_pattern_2
+# OPT: load i64, i64* @"rubyRegexpFrozen_pattern # Comment_2"
 # OPT-NOT: ic_new
 # OPT-LITERAL: }
 
@@ -78,12 +78,12 @@ def xm_flag(x)
 end
 
 # INITIAL-LABEL: define i64 @"func_Object#7xm_flag"(
-# INITIAL: load i64, i64* @rubyRegexpFrozen_pattern_6
+# INITIAL: load i64, i64* @"rubyRegexpFrozen_pattern.still more # Comment_6"
 # INITIAL-LITERAL: }
 
 # OPT-LABEL: define i64 @"func_Object#7xm_flag"(
 # OPT-NOT: ic_new
-# OPT: load i64, i64* @rubyRegexpFrozen_pattern_6
+# OPT: load i64, i64* @"rubyRegexpFrozen_pattern.still more # Comment__6
 # OPT-NOT: ic_new
 # OPT-LITERAL: }
 
@@ -92,12 +92,12 @@ def m_flag(x)
 end
 
 # INITIAL-LABEL: define i64 @"func_Object#6m_flag"(
-# INITIAL: load i64, i64* @rubyRegexpFrozen_pattern_4
+# INITIAL: load i64, i64* @rubyRegexpFrozen_pattern.mflag_4
 # INITIAL-LITERAL: }
 
 # OPT-LABEL: define i64 @"func_Object#6m_flag"(
 # OPT-NOT: ic_new
-# OPT: load i64, i64* @rubyRegexpFrozen_pattern_4
+# OPT: load i64, i64* @rubyRegexpFrozen_pattern.mflag_4
 # OPT-NOT: ic_new
 # OPT-LITERAL: }
 
