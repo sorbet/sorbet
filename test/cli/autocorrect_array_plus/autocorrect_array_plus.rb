934d30516162fa554f5d17f01e276056e9b79cc8
Jake Zimmerman <zimmerman.jake@gmail.com>
2021-10-18 15:06:06 -0700

    Add failing test

---
+++ test/cli/autocorrect_array_plus/autocorrect_array_plus.rb
@@ -0,0 +1,17 @@
+# typed: true
+
+ints = T::Array[Integer].new
+strings = T::Array[String].new
+
+ints + strings
+
+ints + (strings)
+ints + ((strings))
+ints.+(strings)
+
+ints.+(
+  strings
+)
+
+ints +
+  strings
