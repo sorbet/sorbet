d4ebaad62cde43baf464bbb1300df727d98ac7a4
Nathan Froyd <froydnj@gmail.com>
2021-10-19 05:10:49 -0900

    use inspect to print values for type errors rather than to_s

--- test/testdata/compiler/symbol_type_error.rb
+++ test/testdata/compiler/symbol_type_error.rb
@@ -6,5 +6,7 @@ begin
   foo = T.let(T.unsafe(:foo), Integer)
 rescue TypeError => e
   # Ensure that the message about symbols matches the runtime
-  puts e.message
+  # The runtime adds a `Caller:` line; this test just cares about whether we
+  # print the value in the message identically to the runtime.
+  puts e.message.split("\n")[0]
 end
