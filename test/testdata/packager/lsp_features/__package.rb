c8b266ea6e976aa02b4f6a70fa023ad19b28dead
Jake Zimmerman <zimmerman.jake@gmail.com>
2021-10-20 15:55:32 -0700

    Add some show-symbol queries to the lsp_features packager test

--- test/testdata/packager/lsp_features/__package.rb
+++ test/testdata/packager/lsp_features/__package.rb
@@ -4,6 +4,7 @@
 
 class Simpsons < PackageSpec
     # ^^^^^^^^ symbol-search: "Simpsons", name = "Simpsons", container = "(nothing)"
+    # ^ show-symbol: Simpsons
     # go-to-def on a reference to Bart within the package goes here, but go-to-def on Bart in `import Bart` goes to the
     # package.
     import Bart
@@ -11,15 +12,20 @@ class Simpsons < PackageSpec
     #      ^^^^ def: bart 1 not-def-of-self
     #      ^^^^ usage: bartpkg
     #      ^^^^ hover: Bart package description
+    #      ^    show-symbol: Bart
 
     test_import Krabappel
     #           ^^^^^^^^^ usage: krabappel-pkg
     #           ^^^^^^^^^ hover: Bart's teacher
+    #           ^         show-symbol: Krabappel
 
     export Simpsons::Family
     #                ^^^^^^ usage: family
+    #                ^      show-symbol: Simpsons::Family
 
     export_for_test Simpsons::Private
     #                         ^^^^^^^ usage: s-private
     #               ^^^^^^^^^^^^^^^^^ symbol-search: "Simpsons", name = "Simpsons", container = "<PackageTests>::Simpsons_Package"
+    #                         ^       show-symbol: Simpsons::Private
+    #               ^                 show-symbol: Simpsons
 end
