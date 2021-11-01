436deb556e33382a70a49050155146f2f990b769
Nathan Froyd <froydnj@gmail.com>
2021-11-01 04:35:45 -0900

    add some tests for instance variable fastpaths

---
+++ test/testdata/compiler/instance_variable_fastpaths.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+# typed: true
+# compiled: true
+# run_filecheck: INITIAL
+
+class A
+  def initialize(x)
+    @x = x
+  end
+
+  def fast_get
+    instance_variable_get(:@x)
+  end
+
+# INITIAL-LABEL: define internal i64 @"func_A#8fast_get"
+# INITIAL: call i64 @sorbet_vm_instance_variable_get
+# INITIAL{LITERAL}: }
+
+# Can't really test too many or too few args because those won't get past Sorbet.
+
+  def non_constant_arg_get(sym)
+    instance_variable_get(sym)
+  end
+
+# INITIAL-LABEL: define internal i64 @"func_A#20non_constant_arg_get"
+# INITIAL-NOT: call i64 @sorbet_vm_instance_variable_get
+# INITIAL{LITERAL}: }
+
+  def fast_set(x)
+    instance_variable_set(:@x, x)
+  end
+
+# INITIAL-LABEL: define internal i64 @"func_A#8fast_set"
+# INITIAL: call i64 @sorbet_vm_instance_variable_set
+# INITIAL{LITERAL}: }
+
+  def non_constant_arg_set(sym, value)
+    instance_variable_set(sym, value)
+  end
+
+# INITIAL-LABEL: define internal i64 @"func_A#20non_constant_arg_set"
+# INITIAL-NOT: call i64 @sorbet_vm_instance_variable_set
+# INITIAL{LITERAL}: }
+
+end
+
+a = A.new(897)
+p a.fast_get
+p a.non_constant_arg_get(:@x)
+p a.non_constant_arg_get(:@y)
+
+a.fast_set(76)
+p a.fast_get
+p a.non_constant_arg_get(:@x)
+p a.non_constant_arg_get(:@y)
+
+a.non_constant_arg_set(:@x, 492)
+a.non_constant_arg_set(:@y, 371)
+p a.fast_get
+p a.non_constant_arg_get(:@x)
+p a.non_constant_arg_get(:@y)
