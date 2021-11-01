d279aa3a597ba96a0daaf89611564c6f6a3ad1ab
Nathan Froyd <froydnj@gmail.com>
2021-11-01 04:44:46 -0900

    add some overrides for behavior checks

--- test/testdata/compiler/instance_variable_fastpaths.rb
+++ test/testdata/compiler/instance_variable_fastpaths.rb
@@ -59,3 +59,49 @@ a.non_constant_arg_set(:@y, 371)
 p a.fast_get
 p a.non_constant_arg_get(:@x)
 p a.non_constant_arg_get(:@y)
+
+class FewArgs
+  def instance_variable_get
+    "nothing"
+  end
+
+# INITIAL-LABEL: define internal i64 @"func_FewArgs#21instance_variable_get"
+# INITIAL-NOT: call i64 @sorbet_vm_instance_variable_get
+# INITIAL{LITERAL}: }
+
+  def instance_variable_set
+    "nada"
+  end
+
+# INITIAL-LABEL: define internal i64 @"func_FewArgs#21instance_variable_set"
+# INITIAL-NOT: call i64 @sorbet_vm_instance_variable_set
+# INITIAL{LITERAL}: }
+
+end
+
+f = FewArgs.new
+p f.instance_variable_get
+p f.instance_variable_set
+
+class ManyArgs
+  def instance_variable_get(x, y)
+    x + y
+  end
+
+# INITIAL-LABEL: define internal i64 @"func_ManyArgs#21instance_variable_get"
+# INITIAL-NOT: call i64 @sorbet_vm_instance_variable_get
+# INITIAL{LITERAL}: }
+
+  def instance_variable_set(x, y, z)
+    x + y + z
+  end
+
+# INITIAL-LABEL: define internal i64 @"func_ManyArgs#21instance_variable_set"
+# INITIAL-NOT: call i64 @sorbet_vm_instance_variable_set
+# INITIAL{LITERAL}: }
+
+end
+
+m = ManyArgs.new
+p m.instance_variable_get(3, 9)
+p m.instance_variable_set(1, 8, 5)
