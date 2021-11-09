285dae01138f7149b858c2b1fd453541429e04db
Nathan Froyd <froydnj@gmail.com>
2021-11-09 17:03:31 -0500

    always keep the original dsl_{required,optional} send (#4843)

--- rewriter/DSLBuilder.cc
+++ rewriter/DSLBuilder.cc
@@ -82,10 +82,9 @@ vector<ast::ExpressionPtr> DSLBuilder::run(core::MutableContext ctx, ast::Send *
     ast::MethodDef::Flags flags;
     flags.discardDef = true;
 
-    if (!skipSetter && !skipGetter) {
-        stats.emplace_back(ast::MK::Send(loc, ast::MK::Unsafe(loc, move(send->recv)), sendFun, send->numPosArgs,
-                                         std::move(send->args), send->flags, move(send->block)));
-    }
+    // We need to keep around the original send for the compiler.
+    stats.emplace_back(ast::MK::Send(loc, ast::MK::Unsafe(loc, move(send->recv)), sendFun, send->numPosArgs,
+                                     std::move(send->args), send->flags, move(send->block)));
 
     // def self.<prop>
     if (!skipSetter) {
