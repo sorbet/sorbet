d5e3677aa4bb855ce0a939a2717bc1a9e33a88b4
Adam Procter <aprocter@stripe.com>
2021-11-17 23:49:51 +0000

    Add intrinsic for Array#first

--- compiler/ruby-static-exports/array.c
+++ compiler/ruby-static-exports/array.c
@@ -1,3 +1,7 @@
+VALUE sorbet_rb_ary_first(int argc, const VALUE *args, VALUE obj) {
+    return rb_ary_first(argc, args, obj);
+}
+
 VALUE sorbet_rb_ary_join_m(int argc, const VALUE *args, VALUE obj) {
     return rb_ary_join_m(argc, args, obj);
 }
