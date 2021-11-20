1e39f50e26bae0c1e339c7c603d5966f74b71784
Trevor Elliott <awesomelyawesome@gmail.com>
2021-11-19 18:40:03 -0800

    Add a comment to the test

--- test/testdata/compiler/regression/hash_any_p_no_block.rb
+++ test/testdata/compiler/regression/hash_any_p_no_block.rb
@@ -2,6 +2,11 @@
 # compiled: true
 # typed: true
 
+# This checks that the Hash#any? intrinsic isn't inheriting the block from the
+# context of the call_any_p method that it's called from. The failure mode here
+# is that Hash#any? picks up that there was a block passed in, and uses it as
+# its own.
+
 module M
   def self.call_any_p(&blk)
     {x: 10}.any?
