42570748813bc9a6ed3609ab24b26df8d58130bf
Adam Procter <aprocter@stripe.com>
2021-11-30 22:59:17 +0000

    Add intrinsic for Hash#delete

--- compiler/IREmitter/SymbolBasedIntrinsics.cc
+++ compiler/IREmitter/SymbolBasedIntrinsics.cc
@@ -965,6 +965,7 @@ static const vector<CallCMethod> knownCMethodsInstance{
      CMethod{"sorbet_rb_hash_update_withBlock", core::Symbols::Hash()}},
     {core::Symbols::Hash(), "select", CMethod{"sorbet_rb_hash_select", core::Symbols::Enumerator()},
      CMethod{"sorbet_rb_hash_select_withBlock", core::Symbols::Hash()}},
+    {core::Symbols::Hash(), "delete", CMethod{"sorbet_rb_hash_delete_m"}, CMethod{"sorbet_rb_hash_delete_m_withBlock"}},
     {core::Symbols::TrueClass(), "|", CMethod{"sorbet_int_bool_true"}},
     {core::Symbols::FalseClass(), "|", CMethod{"sorbet_int_bool_and"}},
     {core::Symbols::TrueClass(), "&", CMethod{"sorbet_int_bool_and"}},
