7c1a21365ce502dc054191a2c7154b1b0a5305aa
Jake Zimmerman <zimmerman.jake@gmail.com>
2021-12-07 14:05:33 -0800

    Add some more tests to be safe

--- test/testdata/infer/tuple_bare_types.rb
+++ test/testdata/infer/tuple_bare_types.rb
@@ -46,3 +46,25 @@ sig {returns([ChildAorChildB, FalseClass])}
 def example7
   [ChildA.new, false]
 end
+
+sig {returns([ChildA, FalseClass])}
+def example8
+  [ChildA, false] # error: Expected `[ChildA, FalseClass]` but found `[T.class_of(ChildA), FalseClass]` for method result type
+end
+
+sig {returns([ChildA, FalseClass])}
+def example9
+  [ChildA, false] # error: Expected `[ChildA, FalseClass]` but found `[T.class_of(ChildA), FalseClass]` for method result type
+end
+
+x1 = T.let(T.unsafe(nil), [true]) # error: Unsupported literal in type syntax
+T.reveal_type(x1) # error: Revealed type: `[TrueClass] (1-tuple)`
+
+x2 = T.let(T.unsafe(nil), [TrueClass])
+T.reveal_type(x2) # error: Revealed type: `[TrueClass] (1-tuple)`
+
+x3 = T.let(T.unsafe(nil), [ChildA])
+T.reveal_type(x3) # error: Revealed type: `[ChildA] (1-tuple)`
+
+x4 = T.let(T.unsafe(nil), [ChildAorChildB])
+T.reveal_type(x4) # error: Revealed type: `[T.any(ChildA, ChildB)] (1-tuple)`
