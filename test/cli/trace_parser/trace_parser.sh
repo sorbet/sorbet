efc3104db5fe71c2abc937370b7f62c95f44d521
Jake Zimmerman <zimmerman.jake@gmail.com>
2021-12-07 16:13:43 -0800

    Fix test

--- test/cli/trace_parser/trace_parser.sh
+++ test/cli/trace_parser/trace_parser.sh
@@ -2,4 +2,4 @@
 
 set -euo pipefail
 
-main/sorbet --silence-dev-message --trace-parser test/cli/trace_parser.rb 2>&1
+main/sorbet --silence-dev-message --trace-parser test/cli/trace_parser/trace_parser.rb 2>&1
