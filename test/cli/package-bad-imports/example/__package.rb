a828c7432b2c6aa5948a5173f864937caed50523
Getty Ritter <gdritter@stripe.com>
2021-12-10 16:01:14 -0800

    add test case for imports

---
+++ test/cli/package-bad-imports/example/__package.rb
@@ -0,0 +1,12 @@
+# typed: strict
+
+class Some::Example < PackageSpec
+  # this shouldn't find anything similar
+  import NonexistentPackage
+  # this should NOT try to suggest `DoNot::SuggestThis`
+  import DoNot::SUggestThis
+  # this should correctly suggest the package `Suggestion`
+  import SUggestion
+  # a name with three parts, for testing reasons
+  import A::B::C
+end
