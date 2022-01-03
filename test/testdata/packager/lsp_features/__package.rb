e9c401a992f61629e871d33526b2cc149ccca854
Aditya Anchuri <aadi@stripe.com>
2022-01-03 15:01:04 -0800

    Fix LSP packager test

--- test/testdata/packager/lsp_features/__package.rb
+++ test/testdata/packager/lsp_features/__package.rb
@@ -4,6 +4,8 @@
 
 class Simpsons < PackageSpec
     # ^^^^^^^^ symbol-search: "Simpsons", name = "Simpsons", container = "(nothing)"
+    # ^^^^^^^^ symbol-search: "Simpsons", name = "Simpsons", container = "Simpsons"
+    # ^^^^^^^^ symbol-search: "Simpsons", name = "Simpsons", container = "<PackageTests>::Simpsons_Package_Private"
     # ^ show-symbol: Simpsons
     # go-to-def on a reference to Bart within the package goes here, but go-to-def on Bart in `import Bart` goes to the
     # package.
@@ -18,13 +20,10 @@ class Simpsons < PackageSpec
     #           ^         show-symbol: Krabappel
 
     export Simpsons::Family
-    #      ^^^^^^^^ symbol-search: "Simpsons", name = "Simpsons", container = "Simpsons"
     #                ^^^^^^ usage: family
     #                ^      show-symbol: Simpsons::Family
 
     export_for_test Simpsons::Private
     #                         ^^^^^^^ usage: s-private
-    #               ^^^^^^^^^^^^^^^^^ symbol-search: "Simpsons", name = "Simpsons", container = "<PackageTests>::Simpsons_Package_Private"
     #                         ^       show-symbol: Simpsons::Private
-    #               ^                 show-symbol: Simpsons
 end
--- test/testdata/packager/lsp_features/bart/__package.rb
+++ test/testdata/packager/lsp_features/bart/__package.rb
@@ -5,18 +5,15 @@
 class Bart < PackageSpec
     # ^^^^ def: bartpkg
     # ^^^^ symbol-search: "Bart", name = "Bart", container = "(nothing)"
+    # ^^^^ symbol-search: "Bart", name = "Bart", container = "Bart"
+    # ^^^^ symbol-search: "Bart", name = "Bart", container = "<PackageTests>::Bart_Package_Private"
     # ^ show-symbol: Bart
     export Bart::Character
-    #      ^^^^ symbol-search: "Bart", name = "Bart", container = "Bart"
-    #      ^^^^ def: bart 1 not-def-of-self
     #      ^^^^ usage: bartmod
     #            ^^^^^^^^^ usage: character
     #            ^^^^^^^^^ hover: Character class description
-    #      ^^^^^^^^^^^^^^^ symbol-search: "Bart", name = "Bart", container = "<PackageTests>::Bart_Package_Private"
     #            ^ show-symbol: Bart::Character
-    #      ^ show-symbol: Bart
     export Bart::CatchPhrase
-    #      ^^^^ def: bart 2 not-def-of-self
     #      ^^^^ usage: bartmod
     #            ^^^^^^^^^^^ usage: catchphrase
     #            ^ show-symbol: Bart::CatchPhrase
