b19022165d4540e002969306f97100dfc1823cfb
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-01-04 15:57:43 -0800

    A few more tests

--- test/cli/ambiguous-definitions-packages/ambiguous-definitions-packages.sh
+++ test/cli/ambiguous-definitions-packages/ambiguous-definitions-packages.sh
@@ -4,6 +4,6 @@ set -e
 
 cd test/cli/ambiguous-definitions-packages || exit 1
 
-../../../main/sorbet --silence-dev-message --report-ambiguous-definitions --stripe-packages . 2>&1
+../../../main/sorbet --silence-dev-message --stripe-packages . 2>&1
 
 
--- test/cli/ambiguous-definitions/ambiguous-definitions.sh
+++ test/cli/ambiguous-definitions/ambiguous-definitions.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-main/sorbet --silence-dev-message --report-ambiguous-definitions test/cli/ambiguous-definitions/foo.rb 2>&1
+main/sorbet --silence-dev-message test/cli/ambiguous-definitions/foo.rb 2>&1
 
