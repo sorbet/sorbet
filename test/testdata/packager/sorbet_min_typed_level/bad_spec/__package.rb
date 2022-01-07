6cc76a306d210d3ab5f70063eecf31b83af3cc10
Getty Ritter <gdritter@stripe.com>
2022-01-07 12:18:16 -0800

    a bit more bad examples

--- test/testdata/packager/sorbet_min_typed_level/bad_spec/__package.rb
+++ test/testdata/packager/sorbet_min_typed_level/bad_spec/__package.rb
@@ -7,4 +7,10 @@ class BadSorbetSpec < PackageSpec
     min_typed_level: false, # error: All package type levels must be specified as string literals
     tests_min_typed_level: "other" # error: Invalid typed level `"other"`
   )
+  sorbet min_typed_level: "false" # error: Missing required keyword argument `tests_min_typed_level`
+  sorbet tests_min_typed_level: "true" # error: Missing required keyword argument `min_typed_level`
+  sorbet other_typed_level: "true"
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ error: Missing required keyword argument `min_typed_level` for method
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ error: Missing required keyword argument `tests_min_typed_level` for method
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ error: Unrecognized keyword argument `other_typed_level` passed for method
 end
