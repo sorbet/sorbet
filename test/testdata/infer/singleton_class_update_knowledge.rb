b107441bac846af709b499fdfd6217fe44b0afc6
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-01-07 20:20:05 -0800

    Add bad tests

---
+++ test/testdata/infer/singleton_class_update_knowledge.rb
@@ -0,0 +1,69 @@
+# typed: true
+extend T::Sig
+
+class A; end
+class B; end
+
+sig {params(x: T.any(T.class_of(A), T.class_of(B))).void}
+def test1(x)
+  if x == A
+    T.reveal_type(x) # error: `T.class_of(A)`
+  else
+    T.reveal_type(x) # error: `T.any(T.class_of(A), T.class_of(B))`
+  end
+end
+
+sig {params(x: T.any(T.class_of(A), T.class_of(B))).void}
+def test2(x)
+  if A == x
+    T.reveal_type(x) # error: `T.class_of(A)`
+  else
+    T.reveal_type(x) # error: `T.any(T.class_of(A), T.class_of(B))`
+  end
+end
+
+sig {params(x: T.any(T::Array[Integer], T::Set[String])).void}
+def test3(x)
+  if x == []
+    T.reveal_type(x) # error: `[] (0-tuple)`
+  else
+    T.reveal_type(x) # error: `T.any(T::Array[Integer], T::Set[String])`
+  end
+end
+
+sig {params(x: T.any(T::Array[Integer], T::Set[String])).void}
+def test4(x)
+  if [] == x
+    T.reveal_type(x) # error: `[] (0-tuple)`
+  else
+    T.reveal_type(x) # error: `T.any(T::Array[Integer], T::Set[String])`
+  end
+end
+
+class C
+  extend T::Generic
+  Elem = type_template
+end
+
+class D
+  extend T::Generic
+  Elem = type_template
+end
+
+sig {params(x: T.any(T.class_of(C), T.class_of(D))).void}
+def test5(x)
+  if x == C
+    T.reveal_type(x) # error: `T.class_of(C)[T.untyped]`
+  else
+    T.reveal_type(x) # error: `T.any(T.class_of(C)[T.untyped], T.class_of(D)[T.untyped])`
+  end
+end
+
+sig {params(x: T.any(T.class_of(C), T.class_of(D))).void}
+def test6(x)
+  if C == x
+    T.reveal_type(x) # error: `T.class_of(C)[T.untyped]`
+  else
+    T.reveal_type(x) # error: `T.any(T.class_of(C)[T.untyped], T.class_of(D)[T.untyped])`
+  end
+end
---
+++ test/testdata/infer/update_knowledge_untyped.rb
@@ -0,0 +1,26 @@
+# typed: true
+extend T::Sig
+
+# Type tests are not allowed to have T.untyped in them.
+# the "Not enough arguments" error below introduces `T.untyped`
+# This caused an ENFORCE to fail halfway through implementing support for `x ==
+# C`, so I figured it'd be a good test to have.
+
+class C
+  extend T::Generic
+  Elem = type_template
+end
+
+class D
+  extend T::Generic
+  Elem = type_template
+end
+
+sig {params(x: T.any(T.class_of(C), T.class_of())).void} # error: Not enough arguments provided for method `T.class_of`
+def test5(x)
+  if x == C
+    T.reveal_type(x) # error: `T.class_of(C)[T.untyped]`
+  else
+    T.reveal_type(x) # error: `T.untyped`
+  end
+end
