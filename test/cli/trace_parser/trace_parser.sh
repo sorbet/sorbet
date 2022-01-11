351ed7d9a0d3fe664cf18ce2d032149cc2c9a5ed
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-01-11 10:17:17 -0800

    Update test/cli/trace_parser/trace_parser.sh
    Co-authored-by: Nathan Froyd <froydnj@gmail.com>

--- test/cli/trace_parser/trace_parser.sh
+++ test/cli/trace_parser/trace_parser.sh
@@ -5,6 +5,8 @@ set -euo pipefail
 tmp="$(mktemp)"
 trap 'rm -f "$tmp"' EXIT
 
+# We are just testing that the --trace-parser option works.
+# The full output is too dependent on the productions in the parser.
 main/sorbet --silence-dev-message --trace-parser test/cli/trace_parser/trace_parser.rb > "$tmp" 2>&1
 
 head -n 4 "$tmp"
