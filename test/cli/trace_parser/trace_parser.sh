37bf9a4c98b324cd2a225d8b87001bc838e02de9
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-01-11 10:36:23 -0800

    Make the cli-trace test more robust (#5074)
    * Make the cli-trace test more robust
    
    There were a lot of autogenerated IDs in the state names, that mean we
    would have to run this test every time the ypp file changes which is
    annoying and not useful. The test just cares that the option works.
    
    * Update test/cli/trace_parser/trace_parser.sh
    
    Co-authored-by: Nathan Froyd <froydnj@gmail.com>
    
    Co-authored-by: Nathan Froyd <froydnj@gmail.com>

--- test/cli/trace_parser/trace_parser.sh
+++ test/cli/trace_parser/trace_parser.sh
@@ -2,4 +2,11 @@
 
 set -euo pipefail
 
-main/sorbet --silence-dev-message --trace-parser test/cli/trace_parser/trace_parser.rb 2>&1
+tmp="$(mktemp)"
+trap 'rm -f "$tmp"' EXIT
+
+# We are just testing that the --trace-parser option works.
+# The full output is too dependent on the productions in the parser.
+main/sorbet --silence-dev-message --trace-parser test/cli/trace_parser/trace_parser.rb > "$tmp" 2>&1
+
+head -n 4 "$tmp"
