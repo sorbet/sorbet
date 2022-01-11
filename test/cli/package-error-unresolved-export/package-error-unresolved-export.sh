22e99dad183326cdceeb7f16b12213feb7af2d7b
Nathaniel Roman <ngroman@gmail.com>
2022-01-11 13:50:24 -0800

    Optional --stripe-packages-hint-message for packager errors (#5082)
    * Optional --stripe-packages-hint-message
    
    * CLI tests for hint
    
    * missed setPackagerOptions
    
    * whoops forgot to update help.out
    
    * Update test/cli/help/help.out
    
    Co-authored-by: Adam Procter <71457435+aprocter-stripe@users.noreply.github.com>
    
    * Update options.cc
    
    Co-authored-by: Adam Procter <71457435+aprocter-stripe@users.noreply.github.com>

--- test/cli/package-error-unresolved-export/package-error-unresolved-export.sh
+++ test/cli/package-error-unresolved-export/package-error-unresolved-export.sh
@@ -1,3 +1,5 @@
 cd test/cli/package-error-unresolved-export || exit 1
 
-../../../main/sorbet --silence-dev-message --stripe-packages --max-threads=0 . 2>&1
+../../../main/sorbet --silence-dev-message --stripe-packages \
+  --stripe-packages-hint-message="Try running generate-packages.sh" \
+  --max-threads=0 . 2>&1
--- test/pipeline_test_runner.cc
+++ test/pipeline_test_runner.cc
@@ -309,7 +309,8 @@ TEST_CASE("PerPhaseTest") { // NOLINT
         {
             core::UnfreezeNameTable packageNS(*gs);
             core::packages::UnfreezePackages unfreezeToEnterPackagerOptionsPackageDB = gs->unfreezePackages();
-            gs->setPackagerOptions(secondaryTestPackageNamespaces, extraPackageFilesDirectoryPrefixes);
+            gs->setPackagerOptions(secondaryTestPackageNamespaces, extraPackageFilesDirectoryPrefixes,
+                                   "PACKAGE_ERROR_HINT");
         }
 
         // Packager runs over all trees.
