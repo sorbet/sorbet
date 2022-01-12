9ac610bd45118db979cdd69e87cd900173c2cebf
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-01-12 13:42:48 -0800

    Add more tests

--- test/testdata/lsp/completion/missing_assign_rhs.rb
+++ test/testdata/lsp/completion/missing_assign_rhs.rb
@@ -12,4 +12,15 @@ class A
     y =
     #  ^ completion: x, y, foo, test_missing_rhs, ...
   end # error: unexpected token "end"
+
+  def test_variable_end_same_line(x)
+    puts 'before'
+    y =end # error: unexpected token "end"
+    #  ^ completion: x, y, foo, test_missing_rhs, ...
+
+  def test_variable_end_same_line_after(x)
+    # This one is admittedly janky. There's a note about it in the parser.
+    puts 'before'
+    y =end # error: unexpected token "end"
+    #     ^ completion: x, y, foo, test_missing_rhs, ...
 end
