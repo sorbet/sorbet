5f16aa894b9c6a2d7736a7dcd59951f9339f895f
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-01-13 12:24:46 -0800

    wip

--- main/lsp/ErrorReporter.cc
+++ main/lsp/ErrorReporter.cc
@@ -130,7 +130,7 @@ void ErrorReporter::pushDiagnostics(uint32_t epoch, core::FileRef file, const ve
             tags.push_back(DiagnosticTag::Unnecessary);
             diagnostic->tags = move(tags);
         }
-        diagnostic->severity = DiagnosticSeverity::Error;
+        diagnostic->severity = DiagnosticSeverity::Information;
 
         if (!error->autocorrects.empty()) {
             diagnostic->message += " (fix available)";
