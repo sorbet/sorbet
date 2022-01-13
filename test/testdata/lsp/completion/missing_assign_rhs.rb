4b1dc6597ebe6387c5482784665a1889e3ab3ac8
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-01-12 19:07:05 -0800

    Add another test

--- test/testdata/lsp/completion/missing_assign_rhs.rb
+++ test/testdata/lsp/completion/missing_assign_rhs.rb
@@ -2,25 +2,32 @@
 
 class A
   def foo; end
+  attr_writer :foo_writer
+
   def test_missing_rhs(x)
     y =
-    #  ^ completion: x, y, foo, test_missing_rhs, ...
+    #  ^ completion: x, y, foo, ...
   end # error: unexpected token "end"
 
   def test_variable_after_method(x)
     puts 'before'
     y =
-    #  ^ completion: x, y, foo, test_missing_rhs, ...
+    #  ^ completion: x, y, foo, ...
   end # error: unexpected token "end"
 
   def test_variable_end_same_line(x)
     puts 'before'
     y =end # error: unexpected token "end"
-    #  ^ completion: x, y, foo, test_missing_rhs, ...
+    #  ^ completion: x, y, foo, ...
 
   def test_variable_end_same_line_after(x)
     # This one is admittedly janky. There's a note about it in the parser.
     puts 'before'
     y =end # error: unexpected token "end"
-    #     ^ completion: x, y, foo, test_missing_rhs, ...
+    #     ^ completion: x, y, foo, ...
+
+  def test_attr_writer(x)
+    self.foo_writer =
+    #                ^ completion: x, foo, foo_writer=, ...
+  end # error: unexpected token "end"
 end
