d8930fbf266df5d415aacfbfbb2d707b16cae94c
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-01-13 21:20:35 -0800

    Add failing test

---
+++ test/testdata/lsp/completion/missing_kwarg_value.rb
@@ -0,0 +1,8 @@
+# typed: true
+def takes_keyword_args(x:)
+end
+
+def calls_with_keyword_args(x, y)
+  takes_keyword_args(x: ) # error: unexpected token ")"
+  #                     ^ completion: (nothing)
+end
