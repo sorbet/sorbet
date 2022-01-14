a9121e26896668de90e467c0a20d5599d54d919d
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-01-14 11:54:53 -0800

    Add error assertions to test

--- test/testdata/parser/method_def_trailing_comma.rb
+++ test/testdata/parser/method_def_trailing_comma.rb
@@ -1,10 +1,10 @@
 # typed: false
 
-def comma_after_pos_args(x, y,)
+def comma_after_pos_args(x, y,) # error: unexpected token ")"
 end
 
-def comma_after_kwargs(x:, y:,)
+def comma_after_kwargs(x:, y:,) # error: unexpected token ")"
 end
 
-def comma_after_all_kwargs(a, b, x:, y:,)
+def comma_after_all_kwargs(a, b, x:, y:,) # error: unexpected token ")"
 end
