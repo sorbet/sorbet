601da249ba77a4e4bb98359553edcc6de4b881a1
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-01-14 01:29:37 -0800

    Add add one more case, fix tests

--- test/testdata/lsp/completion/bad_arguments.rb
+++ test/testdata/lsp/completion/bad_arguments.rb
@@ -19,32 +19,39 @@ def various_bad_commas_in_send(a, b, x, y)
   #         ^ completion: a, b, x, y, ...
   #          ^ completion: a, b, x, y, ...
 
-
   foo(a, , y: y) # error: unexpected token ","
   #      ^ completion: a, b, x, y, ...
 
   # This one was kind of an accident of the implementation for the next one. I
   # don't think it's too bad if the behavior here needs to change in the future.
   foo(a, x: y:)
+  #      ^^ error: unexpected token tLABEL
+  #           ^ error: unexpected token ")"
+
+  # Either another positional arg, or the start of a keyword
+  foo(a, x y: y)
+  #       ^ completion: x, ...
+  #      ^ error: Method `x` does not exist
+  #        ^^ error: unexpected token tLABEL
 
   # Inserting new keyword arg into list
-  foo(a, x: y: y)
-  #        ^ completion: todo
-  #         ^ completion: todo
+  foo(a, x: y: y) # error: unexpected token tLABEL
+  #        ^ completion: a, b, x, y, ...
+  #         ^ completion: a, b, x, y, ...
 
-  foo(a, x: x, , y: y)
-  #            ^ completion: todo
+  foo(a, x: x, , y: y) # error: unexpected token ","
+  #            ^ completion: a, b, x, y, ...
 
-  foo(a, x: , y: y)
-  #         ^ completion: todo
+  foo(a, x: , y: y) # error: unexpected token ","
+  #         ^ completion: a, b, x, y, ...
 
 
   foo(x: x y: y)
-  #       ^ completion: todo
+  #       ^ completion: x, ...
+  #      ^ error: Method `x` does not exist
+  #        ^^ error: unexpected token tLABEL
   foo(a: a, x: x y: y)
-  #             ^ completion: todo
-
-  # TODO(jez) these are kind of tricky, which is unfortunate becuase they're
-  # also important for completion
-  # foo(a, x y: y)
+  #             ^ completion: x, ...
+  #            ^ error: Method `x` does not exist
+  #              ^^ error: unexpected token tLABEL
 end
