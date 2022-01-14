ff04c51d92822f0971ef252d6bfce380dfbf3c6a
Nathan Froyd <froydnj@gmail.com>
2022-01-14 11:32:23 -1000

    move things that belong with `builder` to `builder.hh`

--- parser/parser/include/ruby_parser/driver.hh
+++ parser/parser/include/ruby_parser/driver.hh
@@ -3,16 +3,12 @@
 
 #include <memory>
 
+#include "builder.hh"
 #include "diagnostic.hh"
 #include "lexer.hh"
 #include "node.hh"
 
 namespace ruby_parser {
-
-struct builder;
-
-using ForeignPtr = const void *;
-using SelfPtr = const void *;
 
 struct node_list {
     node_list() = default;
