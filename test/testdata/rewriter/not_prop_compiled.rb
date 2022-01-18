57b6d28062029e5e54d5f0754aadc5eb37a3c881
Nathan Froyd <froydnj@gmail.com>
2022-01-18 09:56:39 -1000

    new files for compiled props

---
+++ test/testdata/rewriter/not_prop_compiled.rb
@@ -0,0 +1,13 @@
+# typed: true
+
+class ThingsWhichUsedToBePropSyntax
+  prop :type, type: String # error: `prop` does not exist
+  prop :object # error: `prop` does not exist
+  prop :array_of, array: String # error: `prop` does not exist
+  prop :array_of_explicit, Array, array: String
+# ^^^^ error: `prop` does not exist
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ error: `decorator` does not exist
+  prop :no_class_arg, type: Array, immutable: true, array: String # error: `prop` does not exist
+  prop :proc_type, type: T.proc.params(x: Integer).void # error: `prop` does not exist
+  prop :enum_prop, enum: ["hello", "goodbye"] # error: `prop` does not exist
+end
---
+++ test/testdata/rewriter/prop_missing_compiled.rb
@@ -0,0 +1,10 @@
+# typed: true
+
+class ForgotTStructOrTProps
+  prop :foo, Integer
+# ^^^^               error: `prop` does not exist
+# ^^^^^^^^^^^^^^^^^^ error: `decorator` does not exist
+  const :bar, Integer
+# ^^^^^               error: `const` does not exist
+# ^^^^^^^^^^^^^^^^^^^ error: `decorator` does not exist
+end
