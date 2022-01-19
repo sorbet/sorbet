12d7d2116cb5dfb9f9b5167b1b656ff39a756b32
Nathan Froyd <froydnj@gmail.com>
2022-01-19 02:55:05 -1000

    `decorator` is no longer accessed for not-compiled props

--- test/testdata/rewriter/not_prop.rb
+++ test/testdata/rewriter/not_prop.rb
@@ -6,7 +6,6 @@ class ThingsWhichUsedToBePropSyntax
   prop :array_of, array: String # error: `prop` does not exist
   prop :array_of_explicit, Array, array: String
 # ^^^^ error: `prop` does not exist
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ error: `decorator` does not exist
   prop :no_class_arg, type: Array, immutable: true, array: String # error: `prop` does not exist
   prop :proc_type, type: T.proc.params(x: Integer).void # error: `prop` does not exist
   prop :enum_prop, enum: ["hello", "goodbye"] # error: `prop` does not exist
--- test/testdata/rewriter/prop_missing.rb
+++ test/testdata/rewriter/prop_missing.rb
@@ -3,8 +3,6 @@
 class ForgotTStructOrTProps
   prop :foo, Integer
 # ^^^^               error: `prop` does not exist
-# ^^^^^^^^^^^^^^^^^^ error: `decorator` does not exist
   const :bar, Integer
 # ^^^^^               error: `const` does not exist
-# ^^^^^^^^^^^^^^^^^^^ error: `decorator` does not exist
 end
