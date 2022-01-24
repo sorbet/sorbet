004041805b0a3aced3c28d2f8ab7354c54148863
Trevor Elliott <awesomelyawesome@gmail.com>
2022-01-24 14:59:17 -0800

    Add a positional error test

---
+++ test/testdata/resolver/nilable_untyped.rb
@@ -0,0 +1,39 @@
+# typed: false
+#
+# The type_member errors don't get re-raised in the fast-path
+# disable-fast-path: true
+
+
+Bad = T.type_alias {T.nilable(T.untyped)}
+                  # ^^^^^^^^^^^^^^^^^^^^ error: `T.nilable(T.untyped)` is the same as `T.untyped`
+
+# This is a tough to handle case, as the argument to `T.nilable` in `Tricky` is
+# still `T.untyped`, but it's hard to distinguish that from a case where
+# `T.untyped` was returned because the argument type failed to parse in
+# type_syntax.cc. The compromise here is to only raise the error when we
+# encounter `T.nilable(T.untyped)` explicitly foo.
+Untyped = T.type_alias {T.untyped}
+Tricky = T.type_alias {T.nilable(Untyped)}
+
+class A
+  extend T::Sig
+  extend T::Generic
+
+  X = type_member(upper: T.nilable(T.untyped))
+                       # ^^^^^^^^^^^^^^^^^^^^ error: `T.nilable(T.untyped)` is the same as `T.untyped`
+
+  sig {params(x: T.nilable(T.untyped)).void}
+               # ^^^^^^^^^^^^^^^^^^^^ error: `T.nilable(T.untyped)` is the same as `T.untyped`
+  def test1(x)
+  end
+
+end
+
+class B < T::Struct
+
+  prop :foo, T.nilable(T.untyped)
+           # ^^^^^^^^^^^^^^^^^^^^ error: `T.nilable(T.untyped)` is the same as `T.untyped`
+
+  const :bar, T.nilable(T.untyped), default: 10
+            # ^^^^^^^^^^^^^^^^^^^^ error: `T.nilable(T.untyped)` is the same as `T.untyped`
+end
