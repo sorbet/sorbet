a22990b1540a1c82e41fbfadcd333121bf3194d5
Trevor Elliott <awesomelyawesome@gmail.com>
2022-01-24 15:56:42 -0800

    More resilient formatting for error assertions

--- test/testdata/resolver/nilable_untyped.rb
+++ test/testdata/resolver/nilable_untyped.rb
@@ -5,7 +5,7 @@
 
 
 Bad = T.type_alias {T.nilable(T.untyped)}
-                  # ^^^^^^^^^^^^^^^^^^^^ error: `T.nilable(T.untyped)` is the same as `T.untyped`
+#                   ^^^^^^^^^^^^^^^^^^^^ error: `T.nilable(T.untyped)` is the same as `T.untyped`
 
 # This is a tough to handle case, as the argument to `T.nilable` in `Tricky` is
 # still `T.untyped`, but it's hard to distinguish that from a case where
@@ -20,10 +20,10 @@ class A
   extend T::Generic
 
   X = type_member(upper: T.nilable(T.untyped))
-                       # ^^^^^^^^^^^^^^^^^^^^ error: `T.nilable(T.untyped)` is the same as `T.untyped`
+  #                      ^^^^^^^^^^^^^^^^^^^^ error: `T.nilable(T.untyped)` is the same as `T.untyped`
 
   sig {params(x: T.nilable(T.untyped)).void}
-               # ^^^^^^^^^^^^^^^^^^^^ error: `T.nilable(T.untyped)` is the same as `T.untyped`
+  #              ^^^^^^^^^^^^^^^^^^^^ error: `T.nilable(T.untyped)` is the same as `T.untyped`
   def test1(x)
   end
 
@@ -32,8 +32,8 @@ end
 class B < T::Struct
 
   prop :foo, T.nilable(T.untyped)
-           # ^^^^^^^^^^^^^^^^^^^^ error: `T.nilable(T.untyped)` is the same as `T.untyped`
+  #          ^^^^^^^^^^^^^^^^^^^^ error: `T.nilable(T.untyped)` is the same as `T.untyped`
 
   const :bar, T.nilable(T.untyped), default: 10
-            # ^^^^^^^^^^^^^^^^^^^^ error: `T.nilable(T.untyped)` is the same as `T.untyped`
+  #           ^^^^^^^^^^^^^^^^^^^^ error: `T.nilable(T.untyped)` is the same as `T.untyped`
 end
