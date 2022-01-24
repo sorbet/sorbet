8b7b71cc1b00b9e321f635190de4aef29ecb745f
Vinicius Stock <vinicius.stock@shopify.com>
2022-01-24 16:07:18 -0500

    Remove unused variables

--- common/os/mac.cc
+++ common/os/mac.cc
@@ -41,7 +41,6 @@ bool amIBeingDebugged()
     int junk __attribute__((unused));
     int mib[4];
     struct kinfo_proc info;
-    size_t size;
 
     // Initialize the flags so that, if sysctl fails for some bizarre
     // reason, we get a predictable result.
@@ -58,9 +57,8 @@ bool amIBeingDebugged()
 
     // Call sysctl.
 
-    size = sizeof(info);
-    junk = sysctl(mib, sizeof(mib) / sizeof(*mib), &info, &size, nullptr, 0);
-    assert(junk == 0);
+    size_t size = sizeof(info);
+    assert(sysctl(mib, sizeof(mib) / sizeof(*mib), &info, &size, nullptr, 0) == 0);
 
     // We're being debugged if the P_TRACED flag is set.
 
