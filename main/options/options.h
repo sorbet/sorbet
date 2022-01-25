eab090cf36154d1def4b6a1a6b64fc33c8592ef9
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-01-25 11:22:29 -0800

    Use `:` and Space as trigger characters
    I had also tried doing `::` and `= `, but those both failed to do what I
    wanted them to, so I'm thinking that it might only be the last character
    that matters.

--- main/options/options.h
+++ main/options/options.h
@@ -229,6 +229,7 @@ struct Options {
     bool lspDocumentSymbolEnabled = false;
     bool lspDocumentFormatRubyfmtEnabled = false;
     bool lspSignatureHelpEnabled = false;
+    bool lspNewTriggerCharactersEnabled = false;
 
     // Experimental feature `requires_ancestor`
     bool requiresAncestorEnabled = false;
