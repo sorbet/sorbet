700bd99cd1b3314afd87539537afacee85ce3c69
Trevor Elliott <awesomelyawesome@gmail.com>
2022-01-24 16:02:08 -0800

    Change the CLI test to output the autocorrected file

--- test/cli/autocorrect-nilable-untyped/autocorrect-nilable-untyped.sh
+++ test/cli/autocorrect-nilable-untyped/autocorrect-nilable-untyped.sh
@@ -1,7 +1,22 @@
-tmp="$(mktemp)"
-pre="test/cli/autocorrect-nilable-untyped/pre.rb"
-post="test/cli/autocorrect-nilable-untyped/post.rb"
-cp "$pre" "$tmp"
-main/sorbet --silence-dev-message -a "$tmp"
-diff "$post" "$tmp"
-rm "$tmp"
+#!/usr/bin/env bash
+
+cwd="$(pwd)"
+infile="$cwd/test/cli/autocorrect-nilable-untyped/autocorrect-nilable-untyped.rb"
+
+tmp="$(mktemp -d)"
+
+cp "$infile" "$tmp"
+
+cd "$tmp" || exit 1
+if "$cwd/main/sorbet" --silence-dev-message -a autocorrect-nilable-untyped.rb 2>&1; then
+  echo "Expected to fail!"
+  exit 1
+fi
+
+echo
+echo --------------------------------------------------------------------------
+echo
+
+cat autocorrect-nilable-untyped.rb
+
+rm autocorrect-nilable-untyped.rb
