3c1d2c4e6de9253610e698b4ba3a55546c78a4b0
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-01-26 17:57:26 -0800

    autocorrect-bare-stdlib-generics

--- test/cli/autocorrect-bare-stdlib-generics/autocorrect-bare-stdlib-generics.sh
+++ test/cli/autocorrect-bare-stdlib-generics/autocorrect-bare-stdlib-generics.sh
@@ -1,6 +1,23 @@
-tmp="$(mktemp)"
-file="test/cli/autocorrect-bare-stdlib-generics/autocorrect-bare-stdlib-generics.rb"
-cp "$file" "$tmp"
-main/sorbet --silence-dev-message -a "$tmp"
-diff "$file" "$tmp"
-rm "$tmp"
+#!/usr/bin/env bash
+
+cwd="$(pwd)"
+infile="$cwd/test/cli/autocorrect-bare-stdlib-generics/autocorrect-bare-stdlib-generics.rb"
+
+tmp="$(mktemp -d)"
+
+cp "$infile" "$tmp"
+
+cd "$tmp" || exit 1
+if "$cwd/main/sorbet" --silence-dev-message -a autocorrect-bare-stdlib-generics.rb 2>&1; then
+  echo "Expected to fail!"
+  exit 1
+fi
+
+echo
+echo --------------------------------------------------------------------------
+echo
+
+# Also cat the file, to make that the autocorrect applied
+cat autocorrect-bare-stdlib-generics.rb
+
+rm autocorrect-bare-stdlib-generics.rb
