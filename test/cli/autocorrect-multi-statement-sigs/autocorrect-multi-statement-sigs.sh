c832cf2161fefc5e88ef457ad0badf72fce0f3dc
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-01-26 17:55:35 -0800

    autocorrect-multi-statement-sigs

--- test/cli/autocorrect-multi-statement-sigs/autocorrect-multi-statement-sigs.sh
+++ test/cli/autocorrect-multi-statement-sigs/autocorrect-multi-statement-sigs.sh
@@ -1,7 +1,23 @@
-tmp="$(mktemp)"
-pre="test/cli/autocorrect-multi-statement-sigs/pre.rb"
-post="test/cli/autocorrect-multi-statement-sigs/post.rb"
-cp "$pre" "$tmp"
-main/sorbet --silence-dev-message -a "$tmp"
-diff "$post" "$tmp"
-rm "$tmp"
+#!/usr/bin/env bash
+
+cwd="$(pwd)"
+infile="$cwd/test/cli/autocorrect-multi-statement-sigs/autocorrect-multi-statement-sigs.rb"
+
+tmp="$(mktemp -d)"
+
+cp "$infile" "$tmp"
+
+cd "$tmp" || exit 1
+if "$cwd/main/sorbet" --silence-dev-message -a autocorrect-multi-statement-sigs.rb 2>&1; then
+  echo "Expected to fail!"
+  exit 1
+fi
+
+echo
+echo --------------------------------------------------------------------------
+echo
+
+# Also cat the file, to make that the autocorrect applied
+cat autocorrect-multi-statement-sigs.rb
+
+rm autocorrect-multi-statement-sigs.rb
