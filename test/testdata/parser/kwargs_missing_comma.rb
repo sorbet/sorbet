aec6e2006fc1b23042453e35be8de765ef28ffb1
Adam Procter <amp269@gmail.com>
2022-01-31 12:19:38 -0800

    Better error message and an autocorrect

--- test/testdata/parser/kwargs_missing_comma.rb
+++ test/testdata/parser/kwargs_missing_comma.rb
@@ -2,26 +2,26 @@
 
 class A
   def example1(slug:, token:, merchant:request:)
-                                     # error: missing token ","
+                                     # error: missing "," between keyword args
   end
   
   def example2(merchant: request:)
-                      # ^ error: missing token ","
+                      # ^ error: missing "," between keyword args
   end
 
   def example3(merchant:request:)
-                      # error: missing token ","
+                      # error: missing "," between keyword args
     puts
   end
 
   def example4(merchant:request:, etc:)
-                      # error: missing token ","
+                      # error: missing "," between keyword args
     puts
   end
 
   def example5(merchant:request: etc:)
-                      # error: missing token ","
-                              # ^ error: missing token ","
+                      # error: missing "," between keyword args
+                              # ^ error: missing "," between keyword args
     puts
   end
 end
