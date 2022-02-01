53609eab6a30d71799dafab55aca3136d441d4b3
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-01-31 17:17:27 -0800

    Add some negative tests that we wish worked

--- test/cli/autocorrect_and_and/autocorrect_and_and.rb
+++ test/cli/autocorrect_and_and/autocorrect_and_and.rb
@@ -22,5 +22,20 @@ class A
         foo.even?
       puts a.foo
     end
+
+    # Wildly, this is valid method call syntax.
+    # Our current implementation will not trigger (not even the error).
+    if a . foo &&
+        a.foo.even?
+      puts a.foo
+    end
+
+    # Our autocorrect is very simplicit right now.
+    # It will not fire when the `.` and method name are on separate lines.
+    if a.foo && a.foo.
+        even?
+      puts a.foo
+    end
+
   end
 end
