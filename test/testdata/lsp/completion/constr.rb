42006f64f5538cd0fa2cb9fd9e0e0f0caa98b1ae
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-02-04 14:12:25 -0800

    Fix failing test

--- test/testdata/lsp/completion/constr.rb
+++ test/testdata/lsp/completion/constr.rb
@@ -4,7 +4,12 @@ extend T::Sig
 
 xs = T.let([], T::Array[Integer])
 
+# This completion result is a bit non-sensical at the moment (it doesn't makes
+# sense to return any results there) but at least it doesn't crash Sorbet like
+# it used to. If you made a change that improves this test, feel free to delete
+# this comment.
+
 xs.each.map do |x|
   x
 end
-#  ^ completion: (nothing)
+#  ^ completion: each, ...
