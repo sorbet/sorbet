5309939fce6b9e58b00ca938e0e6863fe18dfa9d
Trevor Elliott <awesomelyawesome@gmail.com>
2022-02-04 16:32:53 -0800

    Add a cli test for the autocorrect

---
+++ test/cli/autocorrect-t-combinator-kwargs/autocorrect-t-combinator-kwargs.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+cwd="$(pwd)"
+infile="$cwd/test/cli/autocorrect-t-combinator-kwargs/autocorrect-t-combinator-kwargs.rb"
+
+tmp="$(mktemp -d)"
+
+cp "$infile" "$tmp"
+
+cd "$tmp" || exit 1
+if "$cwd/main/sorbet" --silence-dev-message -a autocorrect-t-combinator-kwargs.rb 2>&1; then
+  echo "Expected to fail!"
+  exit 1
+fi
+
+echo
+echo --------------------------------------------------------------------------
+echo
+
+# Also cat the file, to make that the autocorrect applied
+cat autocorrect-t-combinator-kwargs.rb
+
+rm autocorrect-t-combinator-kwargs.rb
