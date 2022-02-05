1367d23876bef1f204d46c168403c8ba58ecbf87
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-02-04 18:20:09 -0800

    Existing test improves

--- test/testdata/parser/error_recovery/if_no_end.rb
+++ test/testdata/parser/error_recovery/if_no_end.rb
@@ -1,19 +1,12 @@
 # typed: false
 
-# We still don't recover from this error as much as we'd like to.
-#
-# Some options for handling this better:
-# - tweak the parser somehow?
-# - make sure that cancellable slow path is really good
-# - drive people towards using completion snippets for `if` / `else` / `end`
-
 class A
   def foo
     if true
-  end
+  end # error: Closing "end" token was not indented as far as "if" token
 end
 
 class B
   def bar
   end
-end # error: unexpected token
+end
