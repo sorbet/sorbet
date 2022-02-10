b1e797fbc375ea10daece9a774df445a407e2488
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-02-09 16:04:22 -0800

    Add test showing master behavior

---
+++ test/testdata/parser/error_recovery/missing_operator.rb
@@ -0,0 +1,229 @@
+# typed: false
+
+module Opus::Log
+  def self.info; end
+end
+
+class A
+  def g
+    t = T.let(true, T::Boolean)
+    f = T.let(false, T::Boolean)
+    puts(0 .. 0)
+    puts(0 .. )
+    if 0 ..
+    end
+    if 0 ..
+      puts 'hello'
+    end
+    puts(1 ... 1)
+    puts(1 ... )
+    if 1...
+    end
+    if 1...
+      puts 'hello'
+    end
+    puts(2 + 2)
+    puts(2 + )
+    if 2 +
+    end
+    if 2 +
+      puts 'hello'
+    end
+    puts(3 - 3)
+    puts(3 - )
+    if 3 -
+    end
+    if 3 -
+      puts 'hello'
+    end
+    puts(4 * 4)
+    puts(4 * )
+    if 4 *
+    end
+    if 4 *
+      puts 'hello'
+    end
+    puts(5 / 5)
+    puts(5 / )
+    if 5 /
+    end
+    if 5 /
+      puts 'hello'
+    end
+    puts(6 % 6)
+    puts(6 % )
+    if 6 %
+    end
+    if 6 %
+      puts 'hello'
+    end
+    puts(7 ** 7)
+    puts(7 ** )
+    if 7 **
+    end
+    if 7 **
+      puts 'hello'
+    end
+    puts(-8 ** 8)
+    puts(-8 ** )
+    if -8 **
+    end
+    if -8 **
+      puts 'hello'
+    end
+    puts(+9 ** 9)
+    puts(+9 ** )
+    if +9 **
+    end
+    if +9 **
+      puts 'hello'
+    end
+    puts(-10)
+    puts(-)
+    if -
+    end
+    if -
+      puts 'hello'
+    end
+    puts(+10)
+    puts(+)
+    if +
+    end
+    if +
+      puts 'hello'
+    end
+    puts(10 | 10)
+    puts(10 | )
+    if 10 |
+    end
+    if 10 |
+      puts 'hello'
+    end
+    puts(11 ^ 11)
+    puts(11 ^ )
+    if 11 ^
+    end
+    if 11 ^
+      puts 'hello'
+    end
+    puts(12 & 12)
+    puts(12 & )
+    if 12 &
+    end
+    if 12 &
+      puts 'hello'
+    end
+    puts(13 <=> 13)
+    puts(13 <=> )
+    if 13 <=>
+    end
+    if 13 <=>
+      puts 'hello'
+    end
+    puts(14 == 14)
+    puts(14 == )
+    if 14 ==
+    end
+    if 14 ==
+      puts 'hello'
+    end
+    puts(15 === 15)
+    puts(15 === )
+    if 15 ===
+    end
+    if 15 ===
+      puts 'hello'
+    end
+    puts(16 != 16)
+    puts(16 != )
+    if 16 !=
+    end
+    if 16 !=
+      puts 'hello'
+    end
+    puts(/17/ =~ "17")
+    puts(/17/ =~ )
+    if /17/ =~
+    end
+    if /17/ =~
+      puts 'hello'
+    end
+    puts(/18/ !~ "eighteen")
+    puts(/18/ !~ )
+    if /18/ !~
+    end
+    if /18/ !~
+      puts 'hello'
+    end
+    puts(!t)
+    puts(!)
+    if !
+    end
+    if !
+      puts 'hello'
+    end
+    puts(~19)
+    puts(~)
+    if ~
+    end
+    if ~
+      puts 'hello'
+    end
+    puts(20 << 20)
+    puts(20 << )
+    if 20 <<
+    end
+    if 20 <<
+      puts 'hello'
+    end
+    puts(21 >> 21)
+    puts(21 >> )
+    if 21 >>
+    end
+    if 21 >>
+      puts 'hello'
+    end
+    puts(t && t)
+    puts(t && )
+    if t &&
+    end
+    if t &&
+      puts 'hello'
+    end
+    puts(f || f)
+    puts(f || )
+    if f ||
+    end
+    if f ||
+      puts 'hello'
+    end
+    puts(24 > 24)
+    puts(24 > )
+    if 24 >
+    end
+    if 24 >
+      puts 'hello'
+    end
+    puts(25 < 25)
+    puts(25 < )
+    if 25 <
+    end
+    if 25 <
+      puts 'hello'
+    end
+    puts(26 >= 26)
+    puts(26 >= )
+    if 26 >=
+    end
+    if 26 >=
+      puts 'hello'
+    end
+    puts(27 <= 27)
+    puts(27 <= )
+    if 27 <=
+    end
+    if 27 <=
+      puts 'hello'
+    end
+  end
+end
