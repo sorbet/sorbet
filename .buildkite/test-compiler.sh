a07ba94cc011ee89a2e3d3e4ec46641077222002
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-02-16 21:57:36 -0800

    Delete "uploading test results" to save time in CI

--- .buildkite/test-compiler.sh
+++ .buildkite/test-compiler.sh
@@ -43,48 +43,7 @@ mkdir -p _out_
   --spawn_strategy=local \
   --test_output=errors || err=$?
 
-echo "--- uploading test results"
-
-rm -rf _tmp_
-mkdir -p _tmp_/log/junit/
-
-./bazel query '(tests(//test:compiler) + tests(//test/cli/compiler)) except attr("tags", "manual", //...)' | \
-  while read -r line; do
-    path="${line/://}"
-    path="${path#//}"
-    cp "bazel-testlogs/$path/test.xml" _tmp_/log/junit/"${path//\//_}-${BUILDKITE_JOB_ID}.xml"
-done
-
-annotation_dir="$(mktemp -d "junit-annotate-plugin-annotation-tmp.XXXXXXXXXX")"
-annotation_path="${annotation_dir}/annotation.md"
-
-function cleanup {
-  rm -rf "${annotation_dir}"
-}
-
-trap cleanup EXIT
-
-rbenv install --skip-existing
-.buildkite/tools/annotate.rb _tmp_/log/junit > "$annotation_path"
-cat "$annotation_path"
-
-if grep -q "<details>" "$annotation_path"; then
-  echo "--- :buildkite: Creating annotation"
-
-  if [[ "linux" == "$platform" ]]; then
-      wget -O - https://github.com/buildkite/terminal-to-html/releases/download/v3.2.0/terminal-to-html-3.2.0-linux-amd64.gz | gunzip -c > ./terminal-to-html
-  elif [[ "mac" == "$platform" ]]; then
-      wget -O - https://github.com/buildkite/terminal-to-html/releases/download/v3.2.0/terminal-to-html-3.2.0-darwin-amd64.gz | gunzip -c > ./terminal-to-html
-  fi
-  chmod u+x ./terminal-to-html
-
-  # https://buildkite.com/docs/agent/v3/cli-annotate
-  annotation_path_wrapped="${annotation_dir}/annotation_wrapped.md"
-  (echo '<pre class="term"><code>'; ./terminal-to-html < "$annotation_path"; echo '</code></pre>') > "$annotation_path_wrapped"
-
-  # shellcheck disable=SC2002
-  cat "$annotation_path" | buildkite-agent annotate --context junit-${platform} --style error --append
-fi
+# --- post process test results here if you want ---
 
 if [ "$err" -ne 0 ]; then
     exit "$err"
--- .buildkite/test-static-sanitized.sh
+++ .buildkite/test-static-sanitized.sh
@@ -41,38 +41,8 @@ mkdir -p _out_
   //gems/sorbet/test/hidden-method-finder \
   //... $CONFIG_OPTS --test_summary=terse || err=$?
 
-echo "--- uploading test results"
 
-rm -rf _tmp_
-mkdir -p _tmp_/log/junit/
-
-# TODO: does this query omit the snapshot tests?
-./bazel query 'tests(//...) except (attr("tags", "manual", //...) + attr("tags", "compiler", //...))' | while read -r line; do
-    path="${line/://}"
-    path="${path#//}"
-    cp "bazel-testlogs/$path/test.xml" _tmp_/log/junit/"${path//\//_}-${BUILDKITE_JOB_ID}.xml"
-done
-
-annotation_dir="$(mktemp -d "junit-annotate-plugin-annotation-tmp.XXXXXXXXXX")"
-annotation_path="${annotation_dir}/annotation.md"
-
-function cleanup {
-  rm -rf "${annotation_dir}"
-}
-
-trap cleanup EXIT
-
-
-rbenv install --skip-existing
-.buildkite/tools/annotate.rb _tmp_/log/junit > "$annotation_path"
-
-cat "$annotation_path"
-
-if grep -q "<details>" "$annotation_path"; then
-  echo "--- :buildkite: Creating annotation"
-  # shellcheck disable=SC2002
-  cat "$annotation_path" | buildkite-agent annotate --context junit-${platform} --style error --append
-fi
+# --- post process test results here if you want ---
 
 if [ "$err" -ne 0 ]; then
     exit "$err"
