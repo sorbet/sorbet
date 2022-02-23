6f9c241836070e81c01f8a30c37b7c8e31b18cbc
Trevor Elliott <awesomelyawesome@gmail.com>
2022-02-23 09:27:39 -0800

    Mark type aliases as todo() items to delay resolution after stubbing (#5338)

--- test/cli/rbi-gen-single/family/family.rb
+++ test/cli/rbi-gen-single/family/family.rb
@@ -5,6 +5,13 @@ module Family
   class Simpsons
     extend T::Sig
 
+    MaybeBart = T.type_alias{T.nilable(Bart::Character)}
+
+    sig {returns(MaybeBart)}
+    def no_bart
+      nil
+    end
+
     sig {returns(Bart::Character)}
     def bart
       Bart::Character.new
