3125fe7195311c3ed04efa26d0dbcfda0f1767ae
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-02-23 13:29:06 -0800

    Useless cast: strict -> true

--- core/errors/infer.h
+++ core/errors/infer.h
@@ -19,7 +19,7 @@ constexpr ErrorClass GlobalReassignmentTypeMismatch{7012, StrictLevel::True};
 constexpr ErrorClass FieldReassignmentTypeMismatch{7013, StrictLevel::True};
 // constexpr ErrorClass GenericMethodConstraintUnsolved{7013, StrictLevel::True};
 constexpr ErrorClass RevealType{7014, StrictLevel::True};
-constexpr ErrorClass InvalidCast{7015, StrictLevel::Strict};
+constexpr ErrorClass InvalidCast{7015, StrictLevel::True};
 constexpr ErrorClass ExpectedLiteralType{7016, StrictLevel::True};
 constexpr ErrorClass UntypedMethod{7017, StrictLevel::Strict};
 constexpr ErrorClass UntypedValue{7018, StrictLevel::Strong};
