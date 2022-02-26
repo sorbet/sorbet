534967dbc68b871a010853b5d2c3ec1cad60ffaa
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-02-25 21:17:09 -0800

    Remove redundant Loc from matchArgType

--- test/testdata/resolver/fuzz_include_type_alias.rb
+++ test/testdata/resolver/fuzz_include_type_alias.rb
@@ -4,6 +4,6 @@ A = T.type_alias {Integer}
 
 class B
   include A
-#         ^ error: Superclasses and mixins may not be type aliases
-# ^^^^^^^^^ error: Expected `Module` but found `<Type: Integer>` for argument `arg0`
+  #       ^ error: Superclasses and mixins may not be type aliases
+  #       ^ error: Expected `Module` but found `<Type: Integer>` for argument `arg0`
 end
