sorbet/sorbet

commit c3365c5fbc4f652b30cc45428792f4646783c3a8

Fix block arg types for calls on multiple components (#5361)
Fixes #3653

infer/environment.cc
@@ -1188,8 +1188,24 @@ core::TypePtr Environment::processBinding(core::Context ctx, const cfg::CFG &inW
                 ENFORCE(insn.link);
                 ENFORCE(insn.link->result);
                 ENFORCE(insn.link->result->main.blockPreType);
+
                 auto &procType = insn.link->result->main.blockPreType;
                 auto params = procType.getCallArguments(ctx, core::Names::call());
+                auto it = insn.link->result->secondary.get();
+                while (it != nullptr) {
+                    auto &secondaryProcType = it->main.blockPreType;
+                    auto secondaryParams = secondaryProcType.getCallArguments(ctx, core::Names::call());
+                    switch (insn.link->result->secondaryKind) {
+                        case core::DispatchResult::Combinator::OR:
+                            params = core::Types::any(ctx, params, secondaryParams);
+                            break;
+                        case core::DispatchResult::Combinator::AND:
+                            params = core::Types::all(ctx, params, secondaryParams);
+                            break;
+                    }
+
+                    it = it->secondary.get();
+                }
 
                 // A multi-arg proc, if provided a single arg which is an array,
                 // will implicitly splat it out.

core/TypePtr.cc
@@ -215,6 +215,11 @@ core::SymbolRef TypePtr::untypedBlame() const {
     return Symbols::noSymbol();
 }
 
+// Converts a type like this:
+//   T.proc.params(arg0: Integer, arg1: Integer).void
+// into this:
+//   [Integer, Integer]
+// for use with LoadYieldParams
 TypePtr TypePtr::getCallArguments(const GlobalState &gs, NameRef name) const {
     switch (tag()) {
         case Tag::MetaType:

core/TypePtr.h
@@ -293,6 +293,11 @@ public:
 
     core::SymbolRef untypedBlame() const;
 
+    // Converts a type like this:
+    //   T.proc.params(arg0: Integer, arg1: Integer).void
+    // into this:
+    //   [Integer, Integer]
+    // for use with LoadYieldParams
     TypePtr getCallArguments(const GlobalState &gs, NameRef name) const;
 
     TypePtr _approximate(const GlobalState &gs, const TypeConstraint &tc) const;

test/testdata/infer/yield_load_arg_all.rb
@@ -1,0 +1,42 @@
+# typed: true
+
+module IFoo; end
+module IBar; end
+
+class Foo; include IFoo; end
+class Bar; include IBar; end
+
+module A
+  extend T::Sig
+
+  sig {params(blk: T.proc.params(arg0: IFoo, arg1: IFoo).void).void}
+  def foo(&blk)
+    yield Foo.new, Foo.new
+  end
+end
+
+module B
+  extend T::Sig
+
+  sig {params(blk: T.proc.params(arg0: IBar, arg1: IBar).void).void}
+  def foo(&blk)
+    yield Bar.new, Bar.new
+  end
+end
+
+extend T::Sig
+sig {params(ab: T.all(A, B)).void}
+def example_1(ab)
+  ab.foo do |x, y|
+    T.reveal_type(x) # error: `T.all(IFoo, IBar)`
+    T.reveal_type(y) # error: `T.all(IFoo, IBar)`
+  end
+end
+
+sig {params(ba: T.all(B, A)).void}
+def example_2(ba)
+  ba.foo do |x, y|
+    T.reveal_type(x) # error: `T.all(IBar, IFoo)`
+    T.reveal_type(y) # error: `T.all(IBar, IFoo)`
+  end
+end

test/testdata/infer/yield_load_arg_any.rb
@@ -1,0 +1,35 @@
+# typed: true
+class A
+  extend T::Sig
+
+  sig {params(blk: T.proc.params(arg0: Integer, arg1: Integer).void).void}
+  def foo(&blk)
+    yield 0, 0
+  end
+end
+
+class B
+  extend T::Sig
+
+  sig {params(blk: T.proc.params(arg0: String, arg1: String).void).void}
+  def foo(&blk)
+    yield '', ''
+  end
+end
+
+extend T::Sig
+sig {params(ab: T.any(A, B)).void}
+def example_1(ab)
+  ab.foo do |x, y|
+    T.reveal_type(x) # error: `T.any(Integer, String)`
+    T.reveal_type(y) # error: `T.any(Integer, String)`
+  end
+end
+
+sig {params(ba: T.any(B, A)).void}
+def example_2(ba)
+  ba.foo do |x, y|
+    T.reveal_type(x) # error: `T.any(String, Integer)`
+    T.reveal_type(y) # error: `T.any(String, Integer)`
+  end
+end

commit a9de83b384b7bff877f1399b3f236934b8d36943

Fix crash in LoadYieldParams
This was caused by #5361

infer/environment.cc
@@ -1194,14 +1194,24 @@ core::TypePtr Environment::processBinding(core::Context ctx, const cfg::CFG &inW
                 auto it = insn.link->result->secondary.get();
                 while (it != nullptr) {
                     auto &secondaryProcType = it->main.blockPreType;
-                    auto secondaryParams = secondaryProcType.getCallArguments(ctx, core::Names::call());
-                    switch (insn.link->result->secondaryKind) {
-                        case core::DispatchResult::Combinator::OR:
-                            params = core::Types::any(ctx, params, secondaryParams);
-                            break;
-                        case core::DispatchResult::Combinator::AND:
-                            params = core::Types::all(ctx, params, secondaryParams);
-                            break;
+                    if (secondaryProcType != nullptr) {
+                        auto secondaryParams = secondaryProcType.getCallArguments(ctx, core::Names::call());
+                        switch (insn.link->result->secondaryKind) {
+                            case core::DispatchResult::Combinator::OR:
+                                params = core::Types::any(ctx, params, secondaryParams);
+                                break;
+                            case core::DispatchResult::Combinator::AND:
+                                params = core::Types::all(ctx, params, secondaryParams);
+                                break;
+                        }
+                    } else {
+                        // One of the components doesn't have a blockPreType. An error was already
+                        // reported by calls.cc (like "Method `.map` doesn't exist on `NilClass`")
+                        // Here, we just want to not crash.
+                        //
+                        // We could do something like set `params` to `T.untyped`, but it's probably
+                        // nicer to let it be whatever it would have been. This way they'll get
+                        // completion results as if the previous type error was fixed. So do nothing.
                     }
 
                     it = it->secondary.get();

test/testdata/infer/load_yield_params_error.rb
@@ -1,0 +1,11 @@
+# typed: strict
+
+extend T::Sig
+
+sig {params(xs: T.nilable(T::Array[String])).returns(T.untyped)}
+def call(xs)
+  xs.map do |x|
+  #  ^^^ error: Method `map` does not exist on `NilClass` component of `T.nilable(T::Array[String])`
+    T.reveal_type(x) # error: `String`
+  end
+end