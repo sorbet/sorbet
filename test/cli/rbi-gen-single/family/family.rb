7a57f122d0aff7164a1e598b9ef91bd0b85bcc74
Trevor Elliott <awesomelyawesome@gmail.com>
2022-03-02 14:23:42 -0800

    Update test outputs for rbi-gen-single now that stubbing is better

--- test/cli/rbi-gen-single/family/family.rb
+++ test/cli/rbi-gen-single/family/family.rb
@@ -5,16 +5,19 @@ module Family
   class Simpsons
     extend T::Sig
 
+    # These two aliases will have the same RHS in the rbi file that's generated.
     MaybeBart = T.type_alias{T.nilable(Bart::Character)}
+    MaybeBartFull = T.type_alias{T.nilable(Family::Bart::Character)}
 
     sig {returns(MaybeBart)}
     def no_bart
       nil
     end
 
-    # This exposes another problem, Bart::Character is used completely
-    # unqualified at the top level in the rbi.
-    LocalBart = Bart::Character
+    # These two constants will have the same RHS in the rbi file that's
+    # generated.
+    RelativeBart = Bart::Character
+    FullyQualifiedBart = Family::Bart::Character
 
     sig {returns(Bart::Character)}
     def bart
