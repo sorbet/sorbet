919dadb8fa59fbc3744b9772941daf42947f93f4
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-03-03 15:14:25 -0800

    Update tests

--- test/testdata/infer/lub_and_glb_corner_cases.rb
+++ test/testdata/infer/lub_and_glb_corner_cases.rb
@@ -80,7 +80,7 @@ module Main
   T4 = T.type_alias{T.any(T.all(Boolean, T.nilable(FalseClass)), T.all(Boolean, NilClass), T.all(Boolean, NilClass), T.all(Boolean, NilClass), T.all(Boolean, T.nilable(FalseClass)), FalseClass, T.all(Boolean, NilClass), TrueClass)}
 
   def equivalent_and_types_lub
-    T.reveal_type(T4) # error: Revealed type: `<Type: T.any(T.all(Main::Boolean, T.nilable(FalseClass)), FalseClass, T.all(Main::Boolean, NilClass), TrueClass)>`
+    T.reveal_type(T4) # error: Revealed type: `Runtime object representing type: T.any(T.all(Main::Boolean, T.nilable(FalseClass)), FalseClass, T.all(Main::Boolean, NilClass), TrueClass)`
   end
 
 end
