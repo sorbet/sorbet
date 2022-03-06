9c8a79bdaad64789bfd8da02f7ea1701d12e7b8d
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-03-05 18:51:28 -0800

    Add a generics test
    Closes #3791
    
    It looks like at one point this was broken and subsequently fixed, but I
    liked the test case in the issue, so I figured I'd add it.

---
+++ test/testdata/infer/generics/returns_all.rb
@@ -0,0 +1,33 @@
+# typed: true
+
+class A
+  extend T::Sig
+
+  sig {
+    type_parameters(:U)
+    .params(x: T.type_parameter(:U))
+    .returns(T.type_parameter(:U))
+  }
+  def generic_nested(x)
+    x
+  end
+
+  sig {
+    type_parameters(:U)
+    .params(x: T.all(T.type_parameter(:U), T.any(String, Integer)))
+    .returns(T.all(T.type_parameter(:U), T.any(String, Integer)))
+  }
+  def generic(x)
+    generic_nested(x)
+  end
+
+  def test
+    v1 = generic(1017)
+    T.reveal_type(v1) # Revealed type: `Integer(1017)`
+    T.let(v1, Integer)
+
+    v2 = generic("hi")
+    T.reveal_type(v2) # Revealed type: `String("hi")`
+    T.let(v2, String)
+  end
+end
