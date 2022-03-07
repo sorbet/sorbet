1ed7d68fecc29ea5a7a60e16b46f956a87fff12f
Trevor Elliott <awesomelyawesome@gmail.com>
2022-03-07 12:45:19 -0800

    Fix echo clobbering `globalError`

--- .buildkite/linters.sh
+++ .buildkite/linters.sh
@@ -11,50 +11,50 @@ globalErr=0
 
 echo "~~~ Checking build files"
 if ! ./tools/scripts/format_build_files.sh -t &> buildifier; then
-    echo "^^^ +++"
     globalErr=$?
+    echo "^^^ +++"
     buildkite-agent annotate --context tools/scripts/format_build_files.sh --style error --append < buildifier
 fi
 
 echo "~~~ Checking c++ formatting"
 if ! ./tools/scripts/format_cxx.sh -t &> format_cxx; then
-    echo "^^^ +++"
     globalErr=$?
+    echo "^^^ +++"
     buildkite-agent annotate --context tools/scripts/format_cxx.sh --style error --append < format_cxx
 fi
 
 echo "~~~ Checking that the compilation db builds"
 if ! ./tools/scripts/build_compilation_db.sh &> compdb; then
-    echo "^^^ +++"
     globalErr=$?
+    echo "^^^ +++"
     buildkite-agent annotate --context tools/scripts/build_compilation_db.sh --style error --append < compdb
 fi
 
 echo "~~~ Checking compilation db targets"
 if ! ./tools/scripts/generate_compdb_targets.sh -t &> compdb-targets; then
-    echo "^^^ +++"
     globalErr=$?
+    echo "^^^ +++"
     buildkite-agent annotate --context tools/scripts/generate_compdb_targets.sh --style error --append < compdb-targets
 fi
 
 echo "~~~ Linting uses of \`using namespace std\`"
 if ! ./tools/scripts/check_using_namespace_std.sh &> std_check; then
-    echo "^^^ +++"
     globalErr=$?
+    echo "^^^ +++"
     buildkite-agent annotate --context tools/scripts/check_using_namespace_std.sh --style error --append < std_check
 fi
 
 echo "~~~ Running shellcheck"
 if ! ./tools/scripts/lint_sh.sh -t &> lint_sh; then
-    echo "^^^ +++"
     globalErr=$?
+    echo "^^^ +++"
     buildkite-agent annotate --context tools/scripts/lint_sh.sh --style error --append < lint_sh
 fi
 
 echo "~~~ Checking markdown formatting"
 if ! ./tools/scripts/format_website.sh -t &> format_website; then
-    echo "^^^ +++"
     globalErr=$?
+    echo "^^^ +++"
     buildkite-agent annotate --context tools/scripts/format_website.sh --style error --append < format_website
 fi
 
@@ -62,8 +62,8 @@ echo "~~~ Checking the vscode extension"
 pushd vscode_extension
 yarn install
 if ! yarn lint --output-file=yarn_lint; then
-    echo "^^^ +++"
     globalErr=$?
+    echo "^^^ +++"
     buildkite-agent annotate --context 'yarn lint' --style error --append <<EOF
 There were eslint errors in vscode_extension/. Fix with:
 
