647f4f22cbf600ba8c1f936161d3d83b4b434b3e
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-03-14 17:32:00 -0700

    Add nameBestEffortConst function

--- namer/namer.h
+++ namer/namer.h
@@ -11,6 +11,8 @@ namespace sorbet::namer {
 
 class Namer final {
 public:
+    static ast::ParsedFilesOrCancelled
+    symbolizeTreesBestEffort(const core::GlobalState &gs, std::vector<ast::ParsedFile> trees, WorkerPool &workers);
     static ast::ParsedFilesOrCancelled run(core::GlobalState &gs, std::vector<ast::ParsedFile> trees,
                                            WorkerPool &workers);
 
