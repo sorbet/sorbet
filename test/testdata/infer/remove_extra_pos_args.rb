9256bda55a8d0dfe93cc06ec6d2d4367f4f115da
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-03-30 17:50:05 -0700

    Add another test

--- test/testdata/infer/remove_extra_pos_args.rb
+++ test/testdata/infer/remove_extra_pos_args.rb
@@ -5,6 +5,9 @@ sig {params(x: Integer, blk: T.nilable(T.proc.void)).void}
 def takes_one_arg(x, &blk)
 end
 
+def takes_one_arg_with_kwargs(x, **args)
+end
+
 takes_one_arg(0)
 takes_one_arg(0, 0)
 #                ^ error: Too many arguments
@@ -22,3 +25,8 @@ takes_one_arg(0, 0, 0) do
   #              ^^^^ error: Too many arguments
   nil
 end
+
+takes_one_arg_with_kwargs(1, 2, a: 1)
+#                            ^ error: Too many positional
+takes_one_arg_with_kwargs(1, 2, 3, a: 1, b: 2)
+#                            ^^^^ error: Too many positional
