2ecb49c05dfc9909805f82df41a4fbec49a9411a
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-03-31 11:42:30 -0700

    Drop procs.rbi file back to true (#5554)

--- test/testdata/rbi/proc.rb
+++ test/testdata/rbi/proc.rb
@@ -1,6 +1,22 @@
-# typed: true
+# typed: strict
+extend T::Sig
 
 -> (x) { x }[123]
 
 my_proc = Proc.new {}
 T.reveal_type(my_proc) # error: type: `Proc`
+
+sig {params(f: T.proc.params(x: Integer).void).void}
+def example(f)
+  f.call(0) do
+    puts 'hello'
+  end
+end
+
+f = ->(x, &blk) do
+  puts x
+  blk.call
+end
+
+example(f)
+#       ^ error: Expected `T.proc.params(arg0: Integer).void` but found `T.proc.params(arg0: T.untyped, arg1: T.untyped).returns(T.untyped)` for argument `f`
