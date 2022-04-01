5f1ad5ca3517d27830d0ab9d9cb63094a8a3cdd7
ilyailya <bkmz2@yandex.ru>
2022-04-01 19:49:22 +0100

    Add tests which execute non-lazy part

---
+++ test/testdata/lsp/code_actions/move_method/no_resolve.rb
@@ -0,0 +1,44 @@
+# typed: strict
+# use-code-action-resolve: false
+# selective-apply-code-action: refactor.extract
+#
+# The test asserts the refactoring works correctly without `codeAction/resolve` request
+
+module Foo
+  extend T::Sig
+
+  sig {returns(String)}
+  def self.greeting
+         # ^^^^^^^^ apply-code-action: [A] Move method to a new module
+    'Hello'
+  end
+
+  sig do
+    params(x: String)
+    .returns(String)
+  end
+  def name(x)
+    "#{Foo.greeting} #{x}"
+  end
+end
+
+module A
+  class B
+    extend T::Sig
+
+    sig {void}
+    def bar
+      m = Foo
+      Foo.greeting
+
+      m.greeting
+      print((Foo if true).greeting)
+    end
+
+    sig {params(m: T.class_of(Foo)).void}
+    def example3(m)
+      m.greeting
+    end
+  end
+end
+Foo.greeting
