868d7d58fd69e14f2ee4a937850543501d149e58
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-04-04 14:23:56 -0700

    Add some more generics tests (#5577)
    * Add some more generics tests
    
    * Add some colons

--- test/testdata/infer/bug_generics_pr3642.rb
+++ test/testdata/infer/bug_generics_pr3642.rb
@@ -6,11 +6,26 @@ sig do
   type_parameters(:U)
     .params(
       f: T.proc.params(x: T.type_parameter(:U)).void,
-      x: T.type_parameter(:U)
+      x: T.type_parameter(:U),
     )
-    .void
+    .returns(T.type_parameter(:U))
 end
-def foo(f, x)
+def f_x(f, x)
+  f.call(x)
+  x
+end
+
+sig do
+  type_parameters(:U)
+    .params(
+      x: T.type_parameter(:U),
+      f: T.proc.params(x: T.type_parameter(:U)).void,
+    )
+    .returns(T.type_parameter(:U))
+end
+def x_f(x, f)
+  f.call(x)
+  x
 end
 
 f = T.let(
@@ -18,4 +33,41 @@ f = T.let(
   T.proc.params(x: T.any(String, Integer)).void
 )
 
-foo(f, 0)
+g = T.let(
+  proc {|x| nil},
+  T.proc.params(x: T.untyped).void
+)
+
+str_or_int = T.let(0, T.any(String, Integer))
+
+res = f_x(f, 0)
+T.reveal_type(res) # error: Revealed type: `T.any(String, Integer)`
+res = x_f(0, f)
+T.reveal_type(res) # error: Revealed type: `T.any(String, Integer)`
+
+res = f_x(T.unsafe(f), 0)
+T.reveal_type(res) # error: Revealed type: `T.untyped`
+res = x_f(0, T.unsafe(f))
+T.reveal_type(res) # error: Revealed type: `T.untyped`
+
+res = f_x(f, T.unsafe(0))
+T.reveal_type(res) # error: Revealed type: `T.any(String, Integer)`
+res = x_f(T.unsafe(0), f)
+T.reveal_type(res) # error: Revealed type: `T.any(String, Integer)`
+
+# ---------------
+
+res = f_x(g, str_or_int)
+T.reveal_type(res) # error: Revealed type: `T.untyped`
+res = x_f(str_or_int, g)
+T.reveal_type(res) # error: Revealed type: `T.untyped`
+
+res = f_x(T.unsafe(g), str_or_int)
+T.reveal_type(res) # error: Revealed type: `T.untyped`
+res = x_f(str_or_int, T.unsafe(g))
+T.reveal_type(res) # error: Revealed type: `T.untyped`
+
+res = f_x(g, T.unsafe(str_or_int))
+T.reveal_type(res) # error: Revealed type: `T.untyped`
+res = x_f(T.unsafe(str_or_int), g)
+T.reveal_type(res) # error: Revealed type: `T.untyped`
