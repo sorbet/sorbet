b3c3d2517f1580a20eaf191d0a28e47d1f0eff90
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-04-05 20:00:57 -0700

    wip: This causes a crash

---
+++ test/cli/packager_suggest_nested_crash/consumer_auth/__package.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+# typed: strict
+# enable-packager: true
+
+class Project::ConsumerAuth < PackageSpec
+  import Project::ConsumerAuth::Data
+end
---
+++ test/cli/packager_suggest_nested_crash/consumer_auth/identity_type.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+# typed: strict
+
+class Project::ConsumerAuth
+  class IdentifierType
+  end
+end
