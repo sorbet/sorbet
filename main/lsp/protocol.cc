36e95bda714c0ee55855f45648a8f0cfff22eef0
Trevor Elliott <awesomelyawesome@gmail.com>
2022-04-06 14:21:46 -0700

    Revert "Don't pause the indexer during initialization (#5488)"
    This reverts commit 3e7c4a2faf7ec2de1a9ceed672320e708ae31e06.

--- main/lsp/protocol.cc
+++ main/lsp/protocol.cc
@@ -221,7 +221,7 @@ optional<unique_ptr<core::GlobalState>> LSPLoop::runLSP(shared_ptr<LSPInput> inp
             {
                 absl::MutexLock lck(taskQueue->getMutex());
                 Timer timeit(logger, "idle");
-                taskQueue->waitReady(indexer);
+                taskQueue->getMutex()->Await(absl::Condition(taskQueue.get(), &TaskQueue::ready));
                 ENFORCE(!taskQueue->isPaused());
                 if (taskQueue->isTerminated()) {
                     if (taskQueue->getErrorCode() != 0) {
@@ -234,122 +234,106 @@ optional<unique_ptr<core::GlobalState>> LSPLoop::runLSP(shared_ptr<LSPInput> inp
                     }
                 }
 
-                // Find the first task that the indexer is able to handle
-                bool frontOfQueue = true;
-                auto it = absl::c_find_if(taskQueue->tasks(), [&frontOfQueue, &indexer = this->indexer](auto &task) {
-                    bool canHandle = indexer.canHandleTask(frontOfQueue, *task);
-                    frontOfQueue = false;
-                    return canHandle;
-                });
-
-                // taskQueue->waitReady ensures that there is a task to run if the queue isn't terminated.
-                ENFORCE(it != taskQueue->tasks().end());
-
                 // Before giving up the lock, check if the typechecker is running a slow path and if the task at the
                 // head of the queue can either operate on stale data (i.e., the GlobalState just prior to the change
                 // that required the slow path to run) or preempt the currently running slow path. (Note that we don't
                 // bother with either one in cases where we only need the indexer. TODO(aprocter): is that restriction
                 // actually appropriate for stale-data tasks?)
-                auto &frontTask = *it;
+                auto &frontTask = taskQueue->tasks().front();
 
-                // TODO: currently we only support stale global state and preemption for tasks that sit at the front of
-                // the queue.
-                if (it == taskQueue->tasks().begin()) {
-                    // Note that running on stale data is an experimental feature, so we hide it behind the
-                    // --enable-experimental-lsp-stale-state flag.
-                    if (opts.lspStaleStateEnabled && frontTask->finalPhase() == LSPTask::Phase::RUN &&
-                        epochManager->getStatus().slowPathRunning && frontTask->canUseStaleData()) {
-                        logger->debug("Trying to run on stale data");
+                // Note that running on stale data is an experimental feature, so we hide it behind the
+                // --enable-experimental-lsp-stale-state flag.
+                if (opts.lspStaleStateEnabled && frontTask->finalPhase() == LSPTask::Phase::RUN &&
+                    epochManager->getStatus().slowPathRunning && frontTask->canUseStaleData()) {
+                    logger->debug("Trying to run on stale data");
 
-                        frontTask = typecheckerCoord.syncRunOnStaleState(move(frontTask));
+                    frontTask = typecheckerCoord.syncRunOnStaleState(move(frontTask));
 
-                        // If the coordinator has consumed the task, we know it was able to run it on stale state. Pop
-                        // it and move on to the next task.
-                        if (frontTask == nullptr) {
-                            logger->debug("Succeeded in running on stale data");
-                            taskQueue->tasks().erase(it);
-                        }
-                        // If the coordinator has not consumed the task, that means it was not able to run it on stale
-                        // state, because no cancellationUndoState was present. There are (we think! see below) two
-                        // possibilities here:
-                        //
-                        //   1. by the time we acquired the cancellationUndoState lock, the typechecker had already
-                        //      finished the slow path and nulled out the cancellationUndoState; or
-                        //   2. (not sure if this case is actually possible!) we acquired the lock between the time
-                        //      slowPathRunning became true and the time that the typechecker actually initialized the
-                        //      cancellationUndoState.
-                        //
-                        // In case 1, we should be able to process the task as normal shortly, since slowPathRunning has
-                        // become (or is about to become) false.
-                        //
-                        // In case 2, we should be able to process the task on stale state once the typechecker
-                        // initializes cancellationUndoState.
-                        //
-                        // To handle both of these cases, we insert a short sleep before heading around for another turn
-                        // of the loop.
-                        //
-                        // TODO(aprocter): Investigate whether case 2 is actually possible, and consider if there's a
-                        // better way to handle all of this than sleep-and-retry.
-                        else {
-                            logger->debug("Failed to grab the stale state, will try again in 100ms");
-                            Timer::timedSleep(100ms, *logger, "stale_state.sleep");
-                        }
-
-                        continue;
+                    // If the coordinator has consumed the task, we know it was able to run it on stale state. Pop it
+                    // and move on to the next task.
+                    if (frontTask == nullptr) {
+                        logger->debug("Succeeded in running on stale data");
+                        taskQueue->tasks().pop_front();
+                    }
+                    // If the coordinator has not consumed the task, that means it was not able to run it on stale
+                    // state, because no cancellationUndoState was present. There are (we think! see below) two
+                    // possibilities here:
+                    //
+                    //   1. by the time we acquired the cancellationUndoState lock, the typechecker had already
+                    //      finished the slow path and nulled out the cancellationUndoState; or
+                    //   2. (not sure if this case is actually possible!) we acquired the lock between the time
+                    //      slowPathRunning became true and the time that the typechecker actually initialized the
+                    //      cancellationUndoState.
+                    //
+                    // In case 1, we should be able to process the task as normal shortly, since slowPathRunning has
+                    // become (or is about to become) false.
+                    //
+                    // In case 2, we should be able to process the task on stale state once the typechecker initializes
+                    // cancellationUndoState.
+                    //
+                    // To handle both of these cases, we insert a short sleep before heading around for another turn of
+                    // the loop.
+                    //
+                    // TODO(aprocter): Investigate whether case 2 is actually possible, and consider if there's a
+                    // better way to handle all of this than sleep-and-retry.
+                    else {
+                        logger->debug("Failed to grab the stale state, will try again in 100ms");
+                        Timer::timedSleep(100ms, *logger, "stale_state.sleep");
                     }
 
-                    // If the task can preempt, we may be able to schedule a preemption. Don't bother scheduling tasks
-                    // to preempt that only need the indexer. N.B.: We check `canPreempt` last as it is mildly expensive
-                    // for edits (it hashes the files)
-                    if (frontTask->finalPhase() == LSPTask::Phase::RUN && epochManager->getStatus().slowPathRunning &&
-                        frontTask->canPreempt(indexer)) {
-                        absl::Notification finished;
-                        string methodStr = convertLSPMethodToString(frontTask->method);
-                        auto preemptTask = make_unique<LSPQueuePreemptionTask>(*config, finished, *taskQueue, indexer);
-                        auto scheduleToken = typecheckerCoord.trySchedulePreemption(move(preemptTask));
-
-                        if (scheduleToken != nullptr) {
-                            logger->debug("[Processing] Preempting slow path for task {}", methodStr);
-                            // Preemption scheduling success!
-                            // In this if statement **only**, `taskQueueMutex` protects all accesses to LSPIndexer. This
-                            // is needed to linearize the indexing of edits, which may happen in the typechecking thread
-                            // if a fast path edit preempts, with the `canPreempt` checks of edits in this thread.
-                            auto headOfQueueCanPreempt = [&indexer = this->indexer,
-                                                          &tasks = taskQueue->tasks()]() -> bool {
-                                // Await always holds taskQueueMutex when calling this function, but absl doesn't know
-                                // that.
-                                return tasks.empty() || !tasks.front()->canPreempt(indexer);
-                            };
-                            // Wait until the head of the queue turns into a non-preemptible task to resume processing
-                            // the queue.
-                            taskQueue->getMutex()->Await(absl::Condition(&headOfQueueCanPreempt));
-
-                            // The queue is now empty or has a task that cannot preempt. There are two possibilities
-                            // here: 1) The scheduled work is now irrelevant because the task that was scheduled is now
-                            // gone (e.g., the request was canceled or, in the case of an edit, merged w/ a slow path
-                            // edit) 2) The scheduled work has already started (and may have finished). Pessimistically
-                            // assume 1) and try to cancel the scheduled preemption.
-                            if (!typecheckerCoord.tryCancelPreemption(scheduleToken)) {
-                                // Cancelation failed: 2) must be the case. Unlock the queue and wait until task
-                                // finishes to avoid races.
-                                taskQueue->getMutex()->Unlock();
-                                finished.WaitForNotification();
-                                taskQueue->getMutex()->Lock();
-                                logger->debug("[Processing] Preemption for task {} complete", methodStr);
-                            } else {
-                                logger->debug("[Processing] Canceled scheduled preemption for task {}", methodStr);
-                            }
-
-                            // At this point, we are guaranteed that the scheduled task has run or has been canceled.
-                            continue;
-                        }
-                        // If preemption scheduling failed, then the slow path probably finished just now. Continue as
-                        // normal.
-                    }
+                    continue;
                 }
 
-                task = move(frontTask);
-                taskQueue->tasks().erase(it);
+                // If the task can preempt, we may be able to schedule a preemption. Don't bother scheduling tasks to
+                // preempt that only need the indexer.
+                // N.B.: We check `canPreempt` last as it is mildly expensive for edits (it hashes the files)
+                if (frontTask->finalPhase() == LSPTask::Phase::RUN && epochManager->getStatus().slowPathRunning &&
+                    frontTask->canPreempt(indexer)) {
+                    absl::Notification finished;
+                    string methodStr = convertLSPMethodToString(frontTask->method);
+                    auto preemptTask = make_unique<LSPQueuePreemptionTask>(*config, finished, *taskQueue, indexer);
+                    auto scheduleToken = typecheckerCoord.trySchedulePreemption(move(preemptTask));
+
+                    if (scheduleToken != nullptr) {
+                        logger->debug("[Processing] Preempting slow path for task {}", methodStr);
+                        // Preemption scheduling success!
+                        // In this if statement **only**, `taskQueueMutex` protects all accesses to LSPIndexer. This is
+                        // needed to linearize the indexing of edits, which may happen in the typechecking thread if a
+                        // fast path edit preempts, with the `canPreempt` checks of edits in this thread.
+                        auto headOfQueueCanPreempt = [&indexer = this->indexer,
+                                                      &tasks = this->taskQueue->tasks()]() -> bool {
+                            // Await always holds taskQueueMutex when calling this function, but absl doesn't know that.
+                            return tasks.empty() || !tasks.front()->canPreempt(indexer);
+                        };
+                        // Wait until the head of the queue turns into a non-preemptible task to resume processing the
+                        // queue.
+                        taskQueue->getMutex()->Await(absl::Condition(&headOfQueueCanPreempt));
+
+                        // The queue is now empty or has a task that cannot preempt. There are two possibilities here:
+                        // 1) The scheduled work is now irrelevant because the task that was scheduled is now gone
+                        // (e.g., the request was canceled or, in the case of an edit, merged w/ a slow path edit)
+                        // 2) The scheduled work has already started (and may have finished).
+                        // Pessimistically assume 1) and try to cancel the scheduled preemption.
+                        if (!typecheckerCoord.tryCancelPreemption(scheduleToken)) {
+                            // Cancelation failed: 2) must be the case. Unlock the queue and wait until task finishes to
+                            // avoid races.
+                            taskQueue->getMutex()->Unlock();
+                            finished.WaitForNotification();
+                            taskQueue->getMutex()->Lock();
+                            logger->debug("[Processing] Preemption for task {} complete", methodStr);
+                        } else {
+                            logger->debug("[Processing] Canceled scheduled preemption for task {}", methodStr);
+                        }
+
+                        // At this point, we are guaranteed that the scheduled task has run or has been canceled.
+                        continue;
+                    }
+                    // If preemption scheduling failed, then the slow path probably finished just now. Continue as
+                    // normal.
+                }
+
+                task = move(taskQueue->tasks().front());
+                taskQueue->tasks().pop_front();
 
                 // Test only: Collect counters from other threads into this thread for reporting.
                 if (task->method == LSPMethod::GETCOUNTERS && !taskQueue->getCounters().hasNullCounters()) {
