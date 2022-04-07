da637be99fd42812eea0649a4ee729c05bae8361
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-04-07 16:56:07 -0700

    Add a test that fails before these changes

---
+++ test/testdata/packager/lsp_stale_state/bar/__package.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+# typed: strict
+# enable-packager: true
+
+class Project::Bar < PackageSpec
+  import Project::Foo
+
+  export Project::Bar::Bar
+end
---
+++ test/testdata/packager/lsp_stale_state/bar/bar.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+# typed: strict
+
+class Project::Bar::Bar
+  extend T::Sig
+  sig {params(value: Integer).void}
+  def initialize(value)
+    @value = T.let(value, Integer)
+  end
+
+  puts(Project::Bar::Bar)
+  #                  ^ hover: T.class_of(Project::Bar::Bar)
+  #                  _________________________________________
+  puts(Project::Foo::Foo)
+  #                  ^ hover: T.class_of(Project::Foo::Foo)
+  #                  _________________________________________
+
+  # ___
+end
