357e2e2d6c63f8c804033e9c25e028a53a2051b1
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-04-11 16:10:13 -0700

    Add test capturing existing behavior (#5621)
    This behavior isn't amazing (the error messages could be better, and
    it's not 100% clear that there shouldn't be an error on the final
    example's sig), but it's honestly better than nothing and I figured I'd
    add a test just for good measure.

---
+++ test/testdata/infer/generics/conflicting_bounds_all.rb
@@ -0,0 +1,48 @@
+# typed: true
+
+# Vaguely inspired by this post that dmitry linked me once:
+# <https://lptk.github.io/programming/2019/09/13/type-projection.html>
+
+module BadLower
+  extend T::Generic
+
+  A = type_member(lower: String)
+end
+
+module BadUpper
+  extend T::Generic
+
+  A = type_member(upper: Integer)
+end
+
+extend T::Sig
+
+sig {
+  params(x: T.all(BadLower, BadUpper)).void
+  #               ^^^^^^^^ error: Generic class without type arguments `BadLower`
+  #                         ^^^^^^^^ error: Generic class without type arguments `BadUpper`
+}
+def example1(x)
+  T.reveal_type(x) # error: `T.all(BadLower[T.untyped], BadUpper[T.untyped])`
+end
+
+sig {
+  type_parameters(:U)
+  .params(x: T.all(BadLower[T.type_parameter(:U)], BadUpper[T.type_parameter(:U)])).void
+  #                         ^^^^^^^^^^^^^^^^^^^^ error: `T.type_parameter(:<todo typeargument>)` is not a supertype of lower bound of type member `::BadLower::A`
+  #                                                         ^^^^^^^^^^^^^^^^^^^^ error: `T.type_parameter(:<todo typeargument>)` is not a subtype of upper bound of type member `::BadUpper::A`
+}
+def example2(x)
+  T.reveal_type(x) # error: `T.all(BadLower[T.untyped], BadUpper[T.untyped])`
+end
+
+sig {
+  type_parameters(:U)
+  # here, x has bad bounds and no error, but maybe that's fine because
+  # String <: T.untyped <: Integer is just natural due to untyped
+  # (i.e., maybe we should only do bad bounds checking for non-untyped)
+  .params(x: T.all(BadLower[T.untyped], BadUpper[T.untyped])).void
+}
+def example3(x)
+  T.reveal_type(x) # error: `T.all(BadLower[T.untyped], BadUpper[T.untyped])`
+end
