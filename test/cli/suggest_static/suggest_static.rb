e97dcb2b23d09f4c3bbab1928690e61f3a1a5dd1
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-04-11 19:12:23 -0700

    Update exp files

--- test/cli/suggest_static/suggest_static.rb
+++ test/cli/suggest_static/suggest_static.rb
@@ -9,3 +9,13 @@ class A
 end
 
 A.new.foo
+
+class Left
+  def self.on_both; end
+end
+class Right
+  def self.on_both; end
+end
+
+left_or_right = T.let(Left.new, T.any(Left, Right))
+left_or_right.on_both
