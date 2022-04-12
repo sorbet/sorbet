b955a80d15531fb760949244080cd062a257f625
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-04-11 20:08:22 -0700

    wip

--- test/cli/ignore-slash/test.sh
+++ test/cli/ignore-slash/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Tests both relative and absolute ignore patterns.
-main/sorbet --silence-dev-message --ignore foo/ --dir test/cli/ignore-slash 2>&1
+main/sorbet --silence-dev-message --ignore ignore_me/ --dir test/cli/ignore-slash 2>&1
---
+++ test/cli/ignore/subfolder2/ignore_me.rb
@@ -0,0 +1,4 @@
+# typed: true
+
+# This file should be ignored because `ignore_me.rb` is ignored.
+1 + "subfolder2/ignore_me.rb"
