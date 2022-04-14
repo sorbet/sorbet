ddb358f2b508ae389db848baf03adf47d92d839d
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-04-14 16:06:14 -0700

    fix test

--- test/testdata/namer/type_member_autocorrect_multiline.rb
+++ test/testdata/namer/type_member_autocorrect_multiline.rb
@@ -1,35 +1,35 @@
-# typed: true
+# typed: false
 extend T::Sig
 
-class Example
+module Example
   extend T::Sig
   extend T::Generic
 
   # Also need to delete the double space in other changes: `type_member  {{...}}`
 
   A1 = type_member(
-    fixed: Integer
+    fixed: Integer # error: syntax for bounds has changed
   )
   A2 = type_member(
-    lower: Integer,
+    lower: Integer, # error: syntax for bounds has changed
     upper: T.any(Integer, String)
   )
 
   B1 = type_member(
     :out,
-    fixed: Integer
+    fixed: Integer # error: syntax for bounds has changed
   )
   B2 = type_member(
     :out,
-    lower: Integer,
+    lower: Integer, # error: syntax for bounds has changed
     upper: T.any(Integer, String)
   )
 
-  C1 = type_member(fixed: T.any(
+  C1 = type_member(fixed: T.any( # error: syntax for bounds has changed
     Integer,
     String
   ))
-  C2 = type_member(:out, fixed: T.any(
+  C2 = type_member(:out, fixed: T.any( # error: syntax for bounds has changed
     Integer,
     String
   ))
