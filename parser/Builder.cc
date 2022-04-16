43ae6c512465ce4ebb7f968ba691f7e88c88602f
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-04-16 16:22:05 -0700

    Fix off-by-one error in loc for resolver error

--- parser/Builder.cc
+++ parser/Builder.cc
@@ -1339,8 +1339,15 @@ public:
     }
 
     unique_ptr<Node> pair_keyword(const token *key, unique_ptr<Node> value) {
-        auto keyLoc =
-            core::LocOffsets{clamp((uint32_t)key->start()), clamp((uint32_t)key->end() - 1)}; // drop the trailing :
+        auto start = clamp((uint32_t)key->start());
+        auto end = clamp((uint32_t)key->end());
+        if (key->type() == ruby_parser::token_type::tLABEL) {
+            // drop the trailing :
+            end = clamp(end - 1);
+        } else {
+            ENFORCE(key->type() == ruby_parser::token_type::tIDENTIFIER);
+        }
+        auto keyLoc = core::LocOffsets{start, end};
 
         return make_unique<Pair>(tokLoc(key).join(maybe_loc(value)),
                                  make_unique<Symbol>(keyLoc, gs_.enterNameUTF8(key->view())), std::move(value));
