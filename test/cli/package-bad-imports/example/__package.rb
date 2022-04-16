1647d6fad8aaa478ea9a56b09ce3ed8a9f7aa044
ngroman <ngroman@gmail.com>
2022-04-16 16:35:49 -0700

    import fix

--- test/cli/package-bad-imports/example/__package.rb
+++ test/cli/package-bad-imports/example/__package.rb
@@ -3,7 +3,7 @@
 class Some::Example < PackageSpec
   # this shouldn't find anything similar
   import NonexistentPackage
-  # this should NOT try to suggest `DoNot::SuggestThis`
+  # this should NOT try to suggest `DoNot::SuggestThis` can suggest `Suggestion` though
   import DoNot::SUggestThis
   # this should correctly suggest the package `Suggestion`
   import SUggestion
