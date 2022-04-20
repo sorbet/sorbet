5ae83f9349d43561a2250b5fc79500a6dc293605
Nathan Froyd <froydnj@gmail.com>
2022-04-20 08:30:54 -0900

    update test and ensure completion applies cleanly

--- test/testdata/lsp/completion/untyped_receiver.rb
+++ test/testdata/lsp/completion/untyped_receiver.rb
@@ -4,5 +4,6 @@ extend T::Sig
 sig {params(x: T.untyped).void}
 def foo(x)
   x.
-  # ^ completion: (nothing)
+  # ^ completion: (call site is T.untyped)
+  # ^ apply-completion: [A] item: 0
 end # error: unexpected token
