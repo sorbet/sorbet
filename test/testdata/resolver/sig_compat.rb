c27fdb7f937fdc6d3ccd5f97b7eb318b7a195995
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-04-25 10:33:35 -0700

    Fix sig_compat test

--- test/testdata/resolver/sig_compat.rb
+++ test/testdata/resolver/sig_compat.rb
@@ -1,11 +1,11 @@
 # typed: false
-# Not typed; Demonstrating lax `sig`-parsing in untyped code
+
 
 class A
   sig do
     params(
       b: T.deprecated_enum([1,2]),
-      c: T.any(*[Integer, String])
+      c: T.any(*[Integer, String]) # error: splats cannot be used in types
     ).returns(T.untyped)
   end
   def f(b, c)
