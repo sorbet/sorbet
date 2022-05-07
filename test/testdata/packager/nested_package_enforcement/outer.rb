7e1bda61b608bb097e45e3064b5e216b37224c89
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-05-07 15:37:35 -0700

    Fix some testdata tests

--- test/testdata/packager/nested_package_enforcement/outer.rb
+++ test/testdata/packager/nested_package_enforcement/outer.rb
@@ -7,10 +7,10 @@ module Outer
   MY_CONST = 1
 
   module Inner
-#        ^^^^^ error: Class or method definition must match enclosing package namespace `Outer`
+#        ^^^^^ error: File belongs to package `Outer` but defines a constant that does not match this namespace
     module Foo; end
   end
 
   module Inner::Bar; end
-#        ^^^^^^^^^^ error: Class or method definition must match enclosing package namespace `Outer`
+#        ^^^^^^^^^^ error: File belongs to package `Outer` but defines a constant that does not match this namespace
 end
