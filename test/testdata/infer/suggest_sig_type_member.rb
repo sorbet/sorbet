4bfede721df22f33b3b592781903ccb3f1151e81
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-05-12 10:12:57 -0700

    Add a test for type_template to be sure

--- test/testdata/infer/suggest_sig_type_member.rb
+++ test/testdata/infer/suggest_sig_type_member.rb
@@ -5,12 +5,18 @@ class Parent
   extend T::Generic
 
   X = type_member
+  Y = type_template
 
   sig {params(x: X).void}
   def foo(x); end
+
+  sig {params(y: Y).void}
+  def self.foo(y); end
 end
 
 class Child < Parent
   X = type_member
+  Y = type_template
   def foo(x); end # error: does not have a `sig`
+  def self.foo(y); end # error: does not have a `sig`
 end
