1c915f4ed5416a492e115c19048df00c33d1cc20
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-05-16 11:50:23 -0700

    Crash in refactor.extract code action

---
+++ test/testdata/lsp/code_actions/move_method/dsl_required.rb
@@ -0,0 +1,9 @@
+# typed: strict
+# selective-apply-code-action: refactor.extract
+#
+# No code actions should be available for the props
+
+class TestDSLBuilder
+  dsl_required :example1, String
+  #                         ^ apply-code-action: [A] Move method to a new module
+end
