97641394efa379ee1f33bcf7c39d8785da0b4524
Nathan Froyd <froydnj@gmail.com>
2022-05-17 11:14:46 -0900

    update test error

--- test/testdata/namer/fuzz_type_member.rb
+++ test/testdata/namer/fuzz_type_member.rb
@@ -1,4 +1,4 @@
 # typed: true
 
-A=3 # error: Expected `Runtime object representing type: T.untyped` but found `Integer(3)` for field
+A=3 # error: Expected `Runtime object representing type: Integer` but found `Integer(3)` for field
 A=type_member # error: `type_member` cannot be used at the top-level
