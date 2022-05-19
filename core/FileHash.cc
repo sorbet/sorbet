2c05930f91a3626c302fa19c420fbdf3d6be37bf
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-05-19 13:19:10 -0700

    Add and use hashFullNameRef in FullNameHash
    This is the only difference between the two classes.
    
    We might want to consider attempting to do something like this to avoid
    so much code duplication.
    
        enum class Kind {
          HashShortName,
          HashFullName,
        };
    
        template <Kind kind>
        class NameHash {
            ...most of NameHash stays the same...
        };
    
        // you can use typedef/using so people don't have to type
        // NameHash<HashShortName> everywhere
    
        // ...somewhere in FileHash.cc
        template<>
        NameHash<HashShortName>::NameHash(const GlobalState &gs, NameRef nm)
          : _hashValue(incZero(_hash(nm.shortName(gs)))) {}
        template<>
        NameHash<HashFullName>::NameHash(const GlobalState &gs, NameRef nm)
          : _hashValue(incZero(hashFullNameRef(gs, nm))) {}

--- core/FileHash.cc
+++ core/FileHash.cc
@@ -6,9 +6,40 @@
 
 using namespace std;
 namespace sorbet::core {
+namespace {
+
+// We use 0 to signal whether the hash is defined (see isDefined in the header)
 uint32_t incZero(uint32_t a) {
     return a == 0 ? 1 : a;
 };
+
+uint32_t hashFullNameRef(const GlobalState &gs, NameRef nm) {
+    uint32_t result;
+    auto kind = nm.kind();
+
+    switch (kind) {
+        case NameKind::UTF8: {
+            result = _hash(nm.dataUtf8(gs)->utf8);
+            break;
+        }
+        case NameKind::CONSTANT: {
+            result = hashFullNameRef(gs, nm.dataCnst(gs)->original);
+            break;
+        }
+        case NameKind::UNIQUE: {
+            auto data = nm.dataUnique(gs);
+            auto hashedUniqueNameKind = static_cast<underlying_type<UniqueNameKind>::type>(data->uniqueNameKind);
+            result = mix(mix(hashFullNameRef(gs, data->original), data->num), hashedUniqueNameKind);
+            break;
+        }
+    }
+
+    auto hashedKind = static_cast<underlying_type<NameKind>::type>(kind);
+    return mix(result, hashedKind);
+}
+
+} // namespace
+
 ShortNameHash::ShortNameHash(const GlobalState &gs, NameRef nm) : _hashValue(incZero(_hash(nm.shortName(gs)))){};
 
 void ShortNameHash::sortAndDedupe(std::vector<core::ShortNameHash> &hashes) {
@@ -16,7 +47,7 @@ void ShortNameHash::sortAndDedupe(std::vector<core::ShortNameHash> &hashes) {
     hashes.resize(std::distance(hashes.begin(), std::unique(hashes.begin(), hashes.end())));
 }
 
-FullNameHash::FullNameHash(const GlobalState &gs, NameRef nm) : _hashValue(incZero(_hash(nm.shortName(gs)))){};
+FullNameHash::FullNameHash(const GlobalState &gs, NameRef nm) : _hashValue(incZero(hashFullNameRef(gs, nm))) {}
 
 void FullNameHash::sortAndDedupe(std::vector<core::FullNameHash> &hashes) {
     fast_sort(hashes);
