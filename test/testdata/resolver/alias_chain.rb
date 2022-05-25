6e57bccab903e3726bb222a4af7a963826902bff
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-05-24 17:56:08 -0700

    Add another test

---
+++ test/testdata/resolver/alias_chain.rb
@@ -0,0 +1,10 @@
+module Constants
+  MAX_NUM_ADDITIONAL_DEVBOXES = 200
+end
+module Routes::Helpers
+  MAX_NUM_ADDITIONAL_DEVBOXES = Constants::MAX_NUM_ADDITIONAL_DEVBOXES
+end
+module Routes::WillCollapseOut
+  Helpers = Routes::Helpers
+end
+Routes::WillCollapseOut::Helpers::MAX_NUM_ADDITIONAL_DEVBOXES
