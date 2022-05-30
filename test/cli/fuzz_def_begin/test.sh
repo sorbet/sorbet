b59d1454b1686d118ff6caa3df0c3300dfd102ce
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-05-29 19:22:08 -0700

    whoops, flip condition

--- test/cli/fuzz_def_begin/test.sh
+++ test/cli/fuzz_def_begin/test.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-if ! main/sorbet --silence-dev-message 2>&1 test/cli/fuzz_def_begin/test.rb; then
+if main/sorbet --silence-dev-message 2>&1 test/cli/fuzz_def_begin/test.rb; then
   echo "Expected to fail"
   exit 1
 fi
