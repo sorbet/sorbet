292ecae2256c4aabb1cfb79a28a8844bcc273c6f
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-06-01 08:44:48 -0700

    Add FoundDefinitionRef::toString for debugging (#5853)

--- core/FoundDefinitions.cc
+++ core/FoundDefinitions.cc
@@ -64,4 +64,73 @@ core::SymbolRef FoundDefinitionRef::symbol() const {
     return core::SymbolRef::fromRaw(_storage.id);
 }
 
+string FoundDefinitionRef::kindToString(Kind kind) {
+    switch (kind) {
+        case FoundDefinitionRef::Kind::Class:
+            return "FoundClass";
+        case FoundDefinitionRef::Kind::Method:
+            return "FoundMethod";
+        case FoundDefinitionRef::Kind::StaticField:
+            return "FoundStaticField";
+        case FoundDefinitionRef::Kind::TypeMember:
+            return "FoundTypeMember";
+        case FoundDefinitionRef::Kind::ClassRef:
+            return "FoundClassRef";
+        case FoundDefinitionRef::Kind::Empty:
+            return "FoundEmpty";
+        case FoundDefinitionRef::Kind::Symbol:
+            return "FoundSymbol";
+    }
+}
+
+string FoundDefinitionRef::toString(const core::GlobalState &gs, const FoundDefinitions &foundDefs) const {
+    string result;
+    switch (this->kind()) {
+        case FoundDefinitionRef::Kind::Class:
+            result = this->klass(foundDefs).toString(gs, foundDefs, this->idx());
+            break;
+        case FoundDefinitionRef::Kind::Method:
+            result = this->method(foundDefs).toString(gs, foundDefs, this->idx());
+            break;
+        case FoundDefinitionRef::Kind::StaticField:
+            result = this->staticField(foundDefs).toString(gs, foundDefs, this->idx());
+            break;
+        case FoundDefinitionRef::Kind::TypeMember:
+            result = this->typeMember(foundDefs).toString(gs, foundDefs, this->idx());
+            break;
+        case FoundDefinitionRef::Kind::ClassRef:
+            result = this->klassRef(foundDefs).toString(gs, foundDefs, this->idx());
+            break;
+        case FoundDefinitionRef::Kind::Empty:
+            result = "{}";
+            break;
+        case FoundDefinitionRef::Kind::Symbol:
+            result = fmt::format("{{ symbol = {} }}", this->symbol().show(gs));
+            break;
+    }
+    return fmt::format("{} {}", kindToString(this->kind()), result);
+}
+
+string FoundClassRef::toString(const core::GlobalState &gs, const FoundDefinitions &foundDefs, uint32_t id) const {
+    return fmt::format("{{ id = {}, name = {}, owner = {} }}", id, name.toString(gs), owner.idx());
+}
+
+string FoundClass::toString(const core::GlobalState &gs, const FoundDefinitions &foundDefs, uint32_t id) const {
+    return fmt::format("{{ id = {}, owner = {}, klass = {}, classKind = {} }}", id, owner.idx(), klass.idx(),
+                       classKind == Kind::Module ? "module" : "class");
+}
+
+string FoundStaticField::toString(const core::GlobalState &gs, const FoundDefinitions &foundDefs, uint32_t id) const {
+    return fmt::format("{{ id = {}, owner = {}, klass = {}, name = {} }}", id, owner.idx(), klass.idx(),
+                       name.toString(gs));
+}
+
+string FoundTypeMember::toString(const core::GlobalState &gs, const FoundDefinitions &foundDefs, uint32_t id) const {
+    return fmt::format("{{ id = {}, owner = {}, name = {} }}", id, owner.idx(), name.toString(gs));
+}
+
+std::string FoundMethod::toString(const core::GlobalState &gs, const FoundDefinitions &foundDefs, uint32_t id) const {
+    return fmt::format("{{ id = {}, owner = {}, name = {} }}", id, owner.idx(), name.toString(gs));
+}
+
 } // namespace sorbet::core
