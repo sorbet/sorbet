564e263f51a2f3fef38540630c4a7a374fe5e425
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-06-02 14:59:04 -0700

    Two more pre-work test changes (#5871)
    * pre-work: Another test that passes on master
    
    I should have added these changes in #5863.
    
    * pre-work: Add a test that still takes the slow path
    
    I thought I added this in #5863 but it looks like I forgot it.

---
+++ test/testdata/lsp/fast_path/class_add_static_field.rb
@@ -0,0 +1,12 @@
+# typed: true
+
+class A extend T::Sig
+  sig {params(x: Integer).returns(String)}
+  def bar(x)
+    x.to_s
+  end
+end
+
+A.new.bar # error: Not enough arguments provided for method `A#bar`. Expected: `1`, got: `0`
+p(A::FOO.nope)
+# ^^^^^^ error: Unable to resolve constant `FOO`
--- test/testdata/lsp/sig_deletion.rb
+++ test/testdata/lsp/sig_deletion.rb
@@ -5,3 +5,5 @@ class Foo
   sig { returns(String) }
   def foo; 1; end # error: Expected `String` but found `Integer(1)` for method result type
 end
+
+T.reveal_type(Foo.new.foo) # error: `String`
