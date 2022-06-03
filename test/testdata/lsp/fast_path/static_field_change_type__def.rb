sourcegraph/scip-ruby

add a more interesting LSP test

test/testdata/lsp/fast_path/static_field_change_type__def.rb
@@ -1,0 +1,3 @@
+# typed: strict
+
+A = T.let({}, T::Hash[Symbol, String])

test/testdata/lsp/fast_path/static_field_change_type__use.rb
@@ -1,0 +1,7 @@
+# typed: strict
+extend T::Sig
+
+sig {returns(String)}
+def foo
+  A.fetch(:key, "default") + "suffix"
+end

sorbet/sorbet

test/testdata/lsp/fast_path/static_field_change_type__def.rb
@@ -1,0 +1,3 @@
+# typed: strict
+
+STATIC_FIELD = T.let({}, T::Hash[Symbol, String])

test/testdata/lsp/fast_path/static_field_change_type__use.rb
@@ -1,0 +1,7 @@
+# typed: strict
+extend T::Sig
+
+sig {returns(String)}
+def foo
+  STATIC_FIELD.fetch(:key, "default") + "suffix"
+end