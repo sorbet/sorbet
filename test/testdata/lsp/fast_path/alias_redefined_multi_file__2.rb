6e265fdf20c46c09274aeb7c987885f3d1ef9ea7
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-06-03 21:16:33 -0700

    Add failing alias method redefinition test

---
+++ test/testdata/lsp/fast_path/alias_redefined_multi_file__2.rb
@@ -0,0 +1,11 @@
+# typed: true
+# spacer for exclude from file update
+
+class A
+  alias_method :from_method, :to_method
+end
+
+A.new.to_method
+A.new.to_method_new # error: Method `to_method_new` does not exist on `A`
+A.new.from_method
+A.new.from_method_new # error: Method `from_method_new` does not exist on `A`
