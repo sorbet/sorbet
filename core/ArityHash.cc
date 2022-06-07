ad19b8af84e375607af790d23fec88209ca294fc
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-06-07 10:24:08 -0700

    Track, but don't use, alias methods in namer (#5890)
    This will let us have a better sense of which methods we'll need to
    delete on the fast path methods change (#5808).

--- core/ArityHash.cc
+++ core/ArityHash.cc
@@ -7,9 +7,17 @@ namespace sorbet::core {
 ArityHash::ArityHash(uint32_t _hashValue) {
     if (_hashValue == UNDEFINED) {
         this->_hashValue = UNDEFINED_COLLISION_AVOID;
+    } else if (_hashValue == ALIAS_METHOD) {
+        this->_hashValue = ALIAS_METHOD_COLLISION_AVOID;
     } else {
         this->_hashValue = _hashValue;
     }
+}
+
+ArityHash ArityHash::aliasMethodHash() {
+    ArityHash result;
+    result._hashValue = ALIAS_METHOD;
+    return result;
 }
 
 } // namespace sorbet::core
