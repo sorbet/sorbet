34def4310fb08d188752dd01ece433da11af9e76
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-06-21 09:36:51 -0700

    Report histogram of how many files per fast path (#5929)
    The fast path could run on a single file or on tens of thousands of
    files because of how our naive, name-based approach works.
    
    We are going to be taking the fast path for more and more edits, which
    will drive up the likelihood that we encounter worst-case edits that
    typecheck many files on the fast path.
    
    I want this metric so that we can get a sense of the magnitude.

--- main/lsp/LSPTypechecker.cc
+++ main/lsp/LSPTypechecker.cc
@@ -334,6 +334,7 @@ vector<core::FileRef> LSPTypechecker::runFastPath(LSPFileUpdates &updates, Worke
     subset.resize(std::distance(subset.begin(), std::unique(subset.begin(), subset.end())));
 
     config->logger->debug("Running fast path");
+    prodHistogramInc("lsp.fast_path_files_retypechecked", subset.size());
     ENFORCE(gs->errorQueue->isEmpty());
     vector<ast::ParsedFile> updatedIndexed;
     for (auto &f : subset) {
