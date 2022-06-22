a01b8ea78e22e272e3e4dc2cdc575f97cd82cbe1
Nathan Froyd <froydnj@gmail.com>
2022-06-22 03:29:37 -0900

    format C++

--- ast/treemap/treemap.h
+++ ast/treemap/treemap.h
@@ -462,120 +462,120 @@ private:
             }
         }
 
-            // TODO: reorder by frequency
-            if constexpr (Funcs::template HAS_MEMBER_preTransformExpression<FUNC>()) {
+        // TODO: reorder by frequency
+        if constexpr (Funcs::template HAS_MEMBER_preTransformExpression<FUNC>()) {
+            if constexpr (Kind == TreeMapKind::Map) {
+                what = Funcs::template CALL_MEMBER_preTransformExpression<FUNC>::call(func, ctx, Funcs::pass(what));
+            } else if constexpr (Kind == TreeMapKind::Walk) {
+                Funcs::template CALL_MEMBER_preTransformExpression<FUNC>::call(func, ctx, Funcs::pass(what));
+            }
+        }
+
+        switch (what.tag()) {
+            case Tag::EmptyTree:
                 if constexpr (Kind == TreeMapKind::Map) {
-                    what = Funcs::template CALL_MEMBER_preTransformExpression<FUNC>::call(func, ctx, Funcs::pass(what));
+                    return what;
                 } else if constexpr (Kind == TreeMapKind::Walk) {
-                    Funcs::template CALL_MEMBER_preTransformExpression<FUNC>::call(func, ctx, Funcs::pass(what));
+                    return;
                 }
-            }
 
-            switch (what.tag()) {
-                case Tag::EmptyTree:
-                    if constexpr (Kind == TreeMapKind::Map) {
-                        return what;
-                    } else if constexpr (Kind == TreeMapKind::Walk) {
-                        return;
-                    }
+            case Tag::Send:
+                return mapSend(Funcs::pass(what), ctx);
 
-                case Tag::Send:
-                    return mapSend(Funcs::pass(what), ctx);
+            case Tag::ClassDef:
+                return mapClassDef(Funcs::pass(what), ctx);
 
-                case Tag::ClassDef:
-                    return mapClassDef(Funcs::pass(what), ctx);
+            case Tag::MethodDef:
+                return mapMethodDef(Funcs::pass(what), ctx);
 
-                case Tag::MethodDef:
-                    return mapMethodDef(Funcs::pass(what), ctx);
+            case Tag::If:
+                return mapIf(Funcs::pass(what), ctx);
 
-                case Tag::If:
-                    return mapIf(Funcs::pass(what), ctx);
+            case Tag::While:
+                return mapWhile(Funcs::pass(what), ctx);
 
-                case Tag::While:
-                    return mapWhile(Funcs::pass(what), ctx);
+            case Tag::Break:
+                return mapBreak(Funcs::pass(what), ctx);
 
-                case Tag::Break:
-                    return mapBreak(Funcs::pass(what), ctx);
+            case Tag::Retry:
+                return mapRetry(Funcs::pass(what), ctx);
 
-                case Tag::Retry:
-                    return mapRetry(Funcs::pass(what), ctx);
+            case Tag::Next:
+                return mapNext(Funcs::pass(what), ctx);
 
-                case Tag::Next:
-                    return mapNext(Funcs::pass(what), ctx);
+            case Tag::Return:
+                return mapReturn(Funcs::pass(what), ctx);
 
-                case Tag::Return:
-                    return mapReturn(Funcs::pass(what), ctx);
+            case Tag::RescueCase:
+                Exception::raise("should never happen. Forgot to add new tree kind? {}", what.nodeName());
+                break;
 
-                case Tag::RescueCase:
-                    Exception::raise("should never happen. Forgot to add new tree kind? {}", what.nodeName());
-                    break;
+            case Tag::Rescue:
+                return mapRescue(Funcs::pass(what), ctx);
 
-                case Tag::Rescue:
-                    return mapRescue(Funcs::pass(what), ctx);
+            case Tag::Local:
+                return mapLocal(Funcs::pass(what), ctx);
 
-                case Tag::Local:
-                    return mapLocal(Funcs::pass(what), ctx);
+            case Tag::UnresolvedIdent:
+                return mapUnresolvedIdent(Funcs::pass(what), ctx);
 
-                case Tag::UnresolvedIdent:
-                    return mapUnresolvedIdent(Funcs::pass(what), ctx);
+            case Tag::RestArg:
+                Exception::raise("should never happen. Forgot to add new tree kind? {}", what.nodeName());
+                break;
 
-                case Tag::RestArg:
-                    Exception::raise("should never happen. Forgot to add new tree kind? {}", what.nodeName());
-                    break;
+            case Tag::KeywordArg:
+                Exception::raise("should never happen. Forgot to add new tree kind? {}", what.nodeName());
+                break;
 
-                case Tag::KeywordArg:
-                    Exception::raise("should never happen. Forgot to add new tree kind? {}", what.nodeName());
-                    break;
+            case Tag::OptionalArg:
+                Exception::raise("should never happen. Forgot to add new tree kind? {}", what.nodeName());
+                break;
 
-                case Tag::OptionalArg:
-                    Exception::raise("should never happen. Forgot to add new tree kind? {}", what.nodeName());
-                    break;
+            case Tag::BlockArg:
+                Exception::raise("should never happen. Forgot to add new tree kind? {}", what.nodeName());
+                break;
 
-                case Tag::BlockArg:
-                    Exception::raise("should never happen. Forgot to add new tree kind? {}", what.nodeName());
-                    break;
+            case Tag::ShadowArg:
+                Exception::raise("should never happen. Forgot to add new tree kind? {}", what.nodeName());
+                break;
 
-                case Tag::ShadowArg:
-                    Exception::raise("should never happen. Forgot to add new tree kind? {}", what.nodeName());
-                    break;
+            case Tag::Assign:
+                return mapAssign(Funcs::pass(what), ctx);
 
-                case Tag::Assign:
-                    return mapAssign(Funcs::pass(what), ctx);
+            case Tag::Cast:
+                return mapCast(Funcs::pass(what), ctx);
 
-                case Tag::Cast:
-                    return mapCast(Funcs::pass(what), ctx);
+            case Tag::Hash:
+                return mapHash(Funcs::pass(what), ctx);
 
-                case Tag::Hash:
-                    return mapHash(Funcs::pass(what), ctx);
+            case Tag::Array:
+                return mapArray(Funcs::pass(what), ctx);
 
-                case Tag::Array:
-                    return mapArray(Funcs::pass(what), ctx);
+            case Tag::Literal:
+                return mapLiteral(Funcs::pass(what), ctx);
 
-                case Tag::Literal:
-                    return mapLiteral(Funcs::pass(what), ctx);
+            case Tag::UnresolvedConstantLit:
+                return mapUnresolvedConstantLit(Funcs::pass(what), ctx);
 
-                case Tag::UnresolvedConstantLit:
-                    return mapUnresolvedConstantLit(Funcs::pass(what), ctx);
+            case Tag::ConstantLit:
+                return mapConstantLit(Funcs::pass(what), ctx);
 
-                case Tag::ConstantLit:
-                    return mapConstantLit(Funcs::pass(what), ctx);
+            case Tag::ZSuperArgs:
+                if constexpr (Kind == TreeMapKind::Map) {
+                    return what;
+                } else if constexpr (Kind == TreeMapKind::Walk) {
+                    return;
+                }
 
-                case Tag::ZSuperArgs:
-                    if constexpr (Kind == TreeMapKind::Map) {
-                        return what;
-                    } else if constexpr (Kind == TreeMapKind::Walk) {
-                        return;
-                    }
+            case Tag::Block:
+                return mapBlock(Funcs::pass(what), ctx);
 
-                case Tag::Block:
-                    return mapBlock(Funcs::pass(what), ctx);
+            case Tag::InsSeq:
+                return mapInsSeq(Funcs::pass(what), ctx);
 
-                case Tag::InsSeq:
-                    return mapInsSeq(Funcs::pass(what), ctx);
-
-                case Tag::RuntimeMethodDefinition:
-                    return mapRuntimeMethodDefinition(Funcs::pass(what), ctx);
-            }
+            case Tag::RuntimeMethodDefinition:
+                return mapRuntimeMethodDefinition(Funcs::pass(what), ctx);
+        }
     }
 
 #undef CALL_PRE
