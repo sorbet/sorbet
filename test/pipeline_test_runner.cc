1b76f2863d7b29966b3e34d2a1764e780c2f10c0
Nathan Froyd <froydnj@gmail.com>
2022-07-01 10:28:35 -0900

    unlock the name table in one more place in the test runner

--- test/pipeline_test_runner.cc
+++ test/pipeline_test_runner.cc
@@ -850,6 +850,7 @@ TEST_CASE("PerPhaseTest") { // NOLINT
     {
         // namer
         for (auto &tree : trees) {
+            core::UnfreezeNameTable nameTableAccess(*gs);
             core::UnfreezeSymbolTable symbolTableAccess(*gs);
             vector<ast::ParsedFile> vTmp;
             vTmp.emplace_back(move(tree));
