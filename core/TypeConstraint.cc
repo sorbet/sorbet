03d01ba80d587206ba0e779edc480412cb825171
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-07-19 15:03:12 -0700

    clang-format

--- core/TypeConstraint.cc
+++ core/TypeConstraint.cc
@@ -266,7 +266,7 @@ string TypeConstraint::toString(const core::GlobalState &gs) const {
     fmt::memory_buffer buf;
     fmt::format_to(std::back_inserter(buf), "bounds: [{}]\n",
                    fmt::map_join(
-                       collated.begin(), collated.end(), ", ", [&gs](auto entry) -> auto{
+                       collated.begin(), collated.end(), ", ", [&gs](auto entry) -> auto {
                            const auto &[sym, bounds] = entry;
                            const auto &[lowerBound, upperBound] = bounds;
                            auto lower = lowerBound != nullptr ? lowerBound.show(gs) : "_";
@@ -275,7 +275,7 @@ string TypeConstraint::toString(const core::GlobalState &gs) const {
                        }));
     fmt::format_to(std::back_inserter(buf), "solution: [{}]\n",
                    fmt::map_join(
-                       this->solution.begin(), this->solution.end(), ", ", [&gs](auto pair) -> auto{
+                       this->solution.begin(), this->solution.end(), ", ", [&gs](auto pair) -> auto {
                            return fmt::format("{}: {}", pair.first.show(gs), pair.second.show(gs));
                        }));
     return to_string(buf);
