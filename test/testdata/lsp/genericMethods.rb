862a14e384573cdf3a7c4e897d71f09219044eb6
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-07-22 23:27:26 -0700

    Fix some other tests

--- test/testdata/lsp/genericMethods.rb
+++ test/testdata/lsp/genericMethods.rb
@@ -20,19 +20,19 @@ end
 def main
   foo = Foo.new
   v1 = foo.id(1)
-# ^ hover: Integer(1)
-         # ^ hover: sig {params(a: Integer(1)).returns(Integer(1))}
+# ^ hover: Integer
+         # ^ hover: sig {params(a: Integer).returns(Integer)}
          # ^ usage: id
   v2 = foo.id("1")
-# ^ hover: String("1")
-         # ^ hover: sig {params(a: String("1")).returns(String("1"))}
+# ^ hover: String
+         # ^ hover: sig {params(a: String).returns(String)}
          # ^ usage: id
   v3 = Foo.id(1)
-# ^ hover: Integer(1)
-         # ^ hover: sig {params(a: Integer(1)).returns(Integer(1))}
+# ^ hover: Integer
+         # ^ hover: sig {params(a: Integer).returns(Integer)}
          # ^ usage: staticid
   v4 = Foo.id("1")
-# ^ hover: String("1")
-         # ^ hover: sig {params(a: String("1")).returns(String("1"))}
+# ^ hover: String
+         # ^ hover: sig {params(a: String).returns(String)}
          # ^ usage: staticid
 end
