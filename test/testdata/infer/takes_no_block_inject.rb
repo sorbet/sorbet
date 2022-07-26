c57496142b7171b3d9433417c68ba14d2a740b99
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-07-25 19:40:57 -0700

    Update a test

--- test/testdata/infer/takes_no_block_inject.rb
+++ test/testdata/infer/takes_no_block_inject.rb
@@ -2,3 +2,14 @@
 
 foo = T::Array[Integer].new
 foo.inject(nil) {|sum, x| sum ? sum + x : x}
+#                               ^^^^^^^ error: This code is unreachable
+foo.inject(T.unsafe(nil)) do |sum, x|
+  T.reveal_type(sum) # error: `T.untyped`
+  T.reveal_type(x) # error: `Integer`
+  sum ? sum + x : x
+end
+foo.inject(T.let(nil, T.nilable(Integer))) do |sum, x|
+  T.reveal_type(sum) # error: `T.nilable(Integer)`
+  T.reveal_type(x) # error: `Integer`
+  sum ? sum + x : x
+end
