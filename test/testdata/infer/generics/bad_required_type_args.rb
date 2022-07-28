f4ae3e9c0f653f18d448dc7fc0ba097c442d7228
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-07-28 12:56:20 -0700

    Add a negative test

---
+++ test/testdata/infer/generics/bad_required_type_args.rb
@@ -0,0 +1,39 @@
+# typed: true
+extend T::Sig
+
+# All of the examples in this file showcase current limitations in how we've
+# chosen to implement the requirement that a generic class be provided type
+# arguments on instantiation.
+#
+# The check is currently syntactic, which means it fails in ways that you might
+# expect Sorbet to be able to detect using full inference.
+#
+# This is a tradeoff, because implementing something was better than nothing.
+
+class Box
+  extend T::Sig
+  extend T::Generic
+  Elem = type_member
+
+  sig {returns(T.attached_class)}
+  def self.make
+    res = self.new
+    T.reveal_type(res) # error: `T.attached_class (of Box)`
+    res = new
+    T.reveal_type(res) # error: `T.attached_class (of Box)`
+    res
+  end
+end
+
+sig {params(box_class: T.class_of(Box)).void}
+def make_box(box_class)
+  box = box_class.new
+  T.reveal_type(box) # error: `Box[T.untyped]`
+end
+
+sig {void}
+def example
+  box_class = Box
+  box = box_class.new
+  T.reveal_type(box) # error: `Box[T.untyped]`
+end
