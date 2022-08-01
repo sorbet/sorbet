a5cbb0c0f88fbdbbcb92078624ce3a25910a4313
Nathan Froyd <froydnj@gmail.com>
2022-08-01 05:23:57 -0900

    use a non-initializing allocator for (de)serialization buffers

--- core/serialize/pickler.h
+++ core/serialize/pickler.h
@@ -17,10 +17,30 @@ public:
     Pickler() = default;
 };
 
+// Allocator adaptor that interposes construct() calls to
+// convert value initialization into default initialization.
+template <typename T, typename A = std::allocator<T>> class default_init_allocator : public A {
+    typedef std::allocator_traits<A> a_t;
+
+public:
+    template <typename U> struct rebind {
+        using other = default_init_allocator<U, typename a_t::template rebind_alloc<U>>;
+    };
+
+    using A::A;
+
+    template <typename U> void construct(U *ptr) noexcept(std::is_nothrow_default_constructible<U>::value) {
+        ::new (static_cast<void *>(ptr)) U;
+    }
+    template <typename U, typename... Args> void construct(U *ptr, Args &&...args) {
+        a_t::construct(static_cast<A &>(*this), ptr, std::forward<Args>(args)...);
+    }
+};
+
 class UnPickler {
     int pos;
     uint8_t zeroCounter = 0;
-    std::vector<uint8_t> data;
+    std::vector<uint8_t, default_init_allocator<uint8_t>> data;
 
 public:
     uint32_t getU4();
