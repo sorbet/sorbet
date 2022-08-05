009933e6aac613e9600d7cfe4f375e4db2d4706e
Nathan Froyd <froydnj@gmail.com>
2022-08-05 11:53:00 -0400

    fix problems with un-overlapped ranges for document symbols (#6170)

--- test/testdata/lsp/dropped_document_symbols__0.rb
+++ test/testdata/lsp/dropped_document_symbols__0.rb
@@ -1,6 +1,13 @@
-# typed: true
+# typed: strict
 
+class PublicOuterface
+end
+
+# Move the declaration down so locs change for `Bottom` between this file
+# and the other files in this testcase.
 module Top::Upper::Lower::Bottom
   class Private::Basement::Primus
+  end
+  class PublicInterface
   end
 end
