90e0c75db6a8d6d03921ac6e347bd7c1792a7d3b
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-08-06 18:16:52 -0700

    Add more tests

--- test/testdata/infer/puts_after_return.rb
+++ test/testdata/infer/puts_after_return.rb
@@ -1,6 +1,32 @@
 # typed: true
 
-def example
+def basic_example
   return
   puts(123)
+end
+
+def only_once_per_method
+  [1].map do |x|
+    if T.unsafe(nil)
+      next []
+      puts(x)
+    else
+      next [x]
+      puts(123)
+    end
+  end
+
+  return 123
+  puts('hello')
+end
+
+def next_in_block
+  [1].map do |x|
+    if T.unsafe(nil)
+      next []
+    else
+      next [x]
+      puts(123)
+    end
+  end
 end
