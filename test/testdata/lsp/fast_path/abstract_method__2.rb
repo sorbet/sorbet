ec4848539cdcd746019cae8f60e216810f324ad3
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-08-09 14:49:57 -0700

    Add fast path test with abstract and override

---
+++ test/testdata/lsp/fast_path/abstract_method__2.rb
@@ -0,0 +1,11 @@
+# typed: true
+# spacer for exclude from file update
+
+class Foo
+  extend T::Sig
+  include IFoo
+
+  sig {override.void}
+  def foo; end
+# ^^^^^^^ error: Method `Foo#foo` is marked `override` but does not override anything
+end
