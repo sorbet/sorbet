1c972181341af22cc1791560677c90d29b6f69c7
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-08-09 16:20:12 -0700

    Add failng call-with-splat test

---
+++ test/testdata/lsp/fast_path/call_with_splat__2.rb
@@ -0,0 +1,21 @@
+# typed: strict
+# spacer for exclude from file update
+
+res = A.example(&:foo)
+T.reveal_type(res) # error: `String`
+res = A.example(&:fooo) # error: Method `fooo` does not exist on `A`
+T.reveal_type(res) # error: `T.untyped`
+
+int_str = [1, '']
+str_int = ['', 1]
+int_str_false = [1, '', false]
+  A.takes_int_and_string(*int_str)
+# spacer
+# spacer
+  A.takes_int_and_string(*str_int)
+# ^ error: Expected `Integer` but found `String("")` for argument `x`
+# ^ error: Expected `String` but found `Integer(1)` for argument `y`
+  A.takes_int_and_string(*int_str_false)
+# ^ error: Too many arguments provided
+# spacer
+# spacer
