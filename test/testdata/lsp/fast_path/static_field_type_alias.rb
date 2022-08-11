26b0d8e9e842250a4869b8302bb723f177dcf0a6
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-08-10 19:08:09 -0700

    Improve this test

--- test/testdata/lsp/fast_path/static_field_type_alias.rb
+++ test/testdata/lsp/fast_path/static_field_type_alias.rb
@@ -1,5 +1,12 @@
 # typed: true
 
 class AliasContainer
+  extend T::Sig
+
   ContainedThing = T.type_alias {T.any(Float, Symbol)}
+
+  sig {params(x: ContainedThing).void}
+  def example(x)
+    T.reveal_type(x) # error: `T.any(Float, Symbol)`
+  end
 end
