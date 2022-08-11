50a6fe056e66c8171c146569eac394a5bc0e56a7
Nathan Froyd <froydnj@gmail.com>
2022-08-11 09:38:28 -0900

    prefix `--ruby3-keyword-args` with `experimental`

--- test/cli/ruby3-keyword-args/test.sh
+++ test/cli/ruby3-keyword-args/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-main/sorbet --silence-dev-message --ruby3-keyword-args test/cli/ruby3-keyword-args/goodsplat.rb 2>&1
+main/sorbet --silence-dev-message --experimental-ruby3-keyword-args test/cli/ruby3-keyword-args/goodsplat.rb 2>&1
 main/sorbet --silence-dev-message test/cli/ruby3-keyword-args/goodsplat.rb 2>&1
-main/sorbet --silence-dev-message --ruby3-keyword-args test/cli/ruby3-keyword-args/badsplat.rb 2>&1
+main/sorbet --silence-dev-message --experimental-ruby3-keyword-args test/cli/ruby3-keyword-args/badsplat.rb 2>&1
 main/sorbet --silence-dev-message test/cli/ruby3-keyword-args/badsplat.rb 2>&1
