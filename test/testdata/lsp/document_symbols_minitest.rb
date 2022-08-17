c0766196986356c7d7b12010bfdb2ea53c9a7345
Nathan Froyd <froydnj@gmail.com>
2022-08-17 07:52:58 -0900

    add a document symbols test dealing with rewritten minitest bits

---
+++ test/testdata/lsp/document_symbols_minitest.rb
@@ -0,0 +1,20 @@
+# typed: true
+
+class Test
+  # placeholder for initialize
+  before do
+    nil
+  end
+
+  describe 'with this block' do
+    before do
+      nil
+    end
+
+    it 'runs one test' do
+    end
+
+    it 'runs another test' do
+    end
+  end
+end
