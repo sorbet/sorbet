33155252b2b210f9a04a028db64e92c8522ea197
Nathan Froyd <froydnj@gmail.com>
2022-08-18 14:36:35 -0400

    use separate stats for fields and static fields (#6234)

--- main/lsp/LSPIndexer.cc
+++ main/lsp/LSPIndexer.cc
@@ -138,10 +138,15 @@ bool LSPIndexer::canTakeFastPathInternal(
                 newHash.localSymbolTableHashes.typeMemberHash != oldHash.localSymbolTableHashes.typeMemberHash;
             const bool fieldsDiffer =
                 newHash.localSymbolTableHashes.fieldHash != oldHash.localSymbolTableHashes.fieldHash;
+            const bool staticFieldsDiffer =
+                newHash.localSymbolTableHashes.staticFieldHash != oldHash.localSymbolTableHashes.staticFieldHash;
+            const bool classAliasesDiffer =
+                newHash.localSymbolTableHashes.classAliasHash != oldHash.localSymbolTableHashes.classAliasHash;
             const bool methodsDiffer =
                 newHash.localSymbolTableHashes.methodHash != oldHash.localSymbolTableHashes.methodHash;
             const uint32_t differCount = int(classesDiffer) + int(typeArgumentsDiffer) + int(typeMembersDiffer) +
-                                         int(fieldsDiffer) + int(methodsDiffer);
+                                         int(fieldsDiffer) + int(staticFieldsDiffer) + int(classAliasesDiffer) +
+                                         int(methodsDiffer);
             if (classesDiffer) {
                 prodCategoryCounterInc("lsp.slow_path_changed_def", "classmodule");
             }
@@ -152,7 +157,13 @@ bool LSPIndexer::canTakeFastPathInternal(
                 prodCategoryCounterInc("lsp.slow_path_changed_def", "typemember");
             }
             if (fieldsDiffer) {
-                prodCategoryCounterInc("lsp.slow_path_changed_def", "field");
+                prodCategoryCounterInc("lsp.slow_path_changed_def", "icvar");
+            }
+            if (staticFieldsDiffer) {
+                prodCategoryCounterInc("lsp.slow_path_changed_def", "staticfield");
+            }
+            if (classAliasesDiffer) {
+                prodCategoryCounterInc("lsp.slow_path_changed_def", "classalias");
             }
             if (methodsDiffer) {
                 prodCategoryCounterInc("lsp.slow_path_changed_def", "method");
@@ -165,7 +176,11 @@ bool LSPIndexer::canTakeFastPathInternal(
                 } else if (typeMembersDiffer) {
                     prodCategoryCounterInc("lsp.slow_path_changed_def", "onlytypemembers");
                 } else if (fieldsDiffer) {
-                    prodCategoryCounterInc("lsp.slow_path_changed_def", "onlyfields");
+                    prodCategoryCounterInc("lsp.slow_path_changed_def", "onlyicvars");
+                } else if (staticFieldsDiffer) {
+                    prodCategoryCounterInc("lsp.slow_path_changed_def", "onlystaticfields");
+                } else if (classAliasesDiffer) {
+                    prodCategoryCounterInc("lsp.slow_path_changed_def", "onlyclassaliases");
                 } else {
                     ENFORCE(methodsDiffer);
                     prodCategoryCounterInc("lsp.slow_path_changed_def", "onlymethods");
