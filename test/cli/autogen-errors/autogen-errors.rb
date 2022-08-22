c42cf2baac3a087d75b389bbf4d79f26e2f240b6
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-08-22 14:33:25 -0700

    Add failing test

--- test/cli/autogen-errors/autogen-errors.rb
+++ test/cli/autogen-errors/autogen-errors.rb
@@ -4,3 +4,17 @@ class Foo < Bar
 end
 class Bar < Foo
 end
+
+class Opus::Autogen::Proto::ProtobufGeneratedClass1
+end
+
+module Opus::Autogen::Proto
+  ProtobufGeneratedClass1 = T.unsafe(Class).new
+end
+
+module Opus::Autogen::Proto
+  ProtobufGeneratedClass2 = T.unsafe(Class).new
+end
+
+class Opus::Autogen::Proto::ProtobufGeneratedClass2
+end
