bdc771ab36986bf61821af22f4f16f02ef49ab90
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-08-23 16:00:04 -0700

    Show error output from autogen

--- test/cli/autogen-ignore/test.sh
+++ test/cli/autogen-ignore/test.sh
@@ -2,4 +2,13 @@
 
 set -euo pipefail
 
-main/sorbet --silence-dev-message --no-error-count --stop-after=namer -p autogen test/cli/autogen-ignore/autogen-ignore.rb 2>&1
+# We're using temp files because there was some stdout/stderr syncing problems
+
+stderr_log="$(mktemp)"
+trap 'rm -rf "$stderr_log"' EXIT
+
+main/sorbet --silence-dev-message --stop-after=namer -p autogen test/cli/autogen-ignore/autogen-ignore.rb 2> "$stderr_log"
+
+echo --------------------------------------------------------------------------
+
+cat "$stderr_log"
