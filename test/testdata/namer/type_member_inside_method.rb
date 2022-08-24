f0c395a774e4b045e74bca744ac66c2fd38f1da3
Nathan Froyd <froydnj@gmail.com>
2022-08-24 14:05:43 -0400

    add tests for disallowed static fields within methods (#6293)

---
+++ test/testdata/namer/type_member_inside_method.rb
@@ -0,0 +1,13 @@
+# typed: true
+module A
+  extend T::Sig
+
+  class C1; end
+
+  sig {params(x: C1).returns(NilClass)}
+  def f(x)
+    C2 = type_member # error: dynamic constant assignment
+    #    ^^^^^^^^^^^ error: Method `type_member` does not exist on `A`
+    nil
+  end
+end
