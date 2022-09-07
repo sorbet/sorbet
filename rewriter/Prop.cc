0c658ec895af9b1cb621a5a9d5eb18d6a73a355e
bhang-stripe <91229201+bhang-stripe@users.noreply.github.com>
2022-09-07 14:01:19 -0700

    Rename AccountModelMerchantToken to AccountModelMerchant::Token (#6351)

--- rewriter/Prop.cc
+++ rewriter/Prop.cc
@@ -197,10 +197,13 @@ optional<PropInfo> parseProp(core::MutableContext ctx, const ast::Send *send) {
                     send->loc,
                     ast::MK::UnresolvedConstant(
                         send->loc,
-                        ast::MK::UnresolvedConstant(send->loc, ast::MK::EmptyTree(), core::Names::Constants::Opus()),
-                        core::Names::Constants::Autogen()),
-                    core::Names::Constants::Tokens()),
-                core::Names::Constants::AccountModelMerchantToken());
+                        ast::MK::UnresolvedConstant(send->loc,
+                                                    ast::MK::UnresolvedConstant(send->loc, ast::MK::EmptyTree(),
+                                                                                core::Names::Constants::Opus()),
+                                                    core::Names::Constants::Autogen()),
+                        core::Names::Constants::Tokens()),
+                    core::Names::Constants::AccountModelMerchant()),
+                core::Names::Constants::Token());
             break;
 
         default:
