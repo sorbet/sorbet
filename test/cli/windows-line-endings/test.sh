10c1706b94140853f42b7ad7ada7a5aaa530dac9
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-09-08 13:04:15 -0700

    Remove realpath from a test (#6354)
    * Remove realpath from a test
    
    We don't really need `realpath` in this test
    
    * dereference

--- test/cli/windows-line-endings/test.sh
+++ test/cli/windows-line-endings/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-file --brief "$(realpath test/cli/windows-line-endings/windows-line-endings.rb)"
+file --brief --dereference test/cli/windows-line-endings/windows-line-endings.rb
 
 main/sorbet --silence-dev-message test/cli/windows-line-endings/windows-line-endings.rb 2>&1
