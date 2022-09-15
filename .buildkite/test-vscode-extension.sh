70730f298c51b53f08b990747627f9e94a329428
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-09-15 14:39:43 -0700

    Retry when yarn install fails

--- .buildkite/test-vscode-extension.sh
+++ .buildkite/test-vscode-extension.sh
@@ -10,7 +10,33 @@ apt-get install -y libxshmfence-dev libnss3-dev libatk1.0-0 libatk-bridge2.0-0 \
 
 pushd vscode_extension
 
-yarn
+# https://stackoverflow.com/a/8351489
+with_backoff() {
+  local attempts=5
+  local timeout=1 # doubles each failure
+
+  local attempt=0
+  while true; do
+    attempt=$(( attempt + 1 ))
+    echo "Attempt $attempt"
+    if "$@"; then
+      return 0
+    fi
+
+    if (( attempt >= attempts )); then
+      echo "'$1' failed $attempts times. Quitting." 1>&2
+      exit 1
+    fi
+
+    echo "'$1' failed. Retrying in ${timeout}s..." 1>&2
+    sleep $timeout
+    timeout=$(( timeout * 2 ))
+  done
+}
+
+# Sometimes requests to https://registry.yarnpkg.com hit transient errors.
+with_backoff yarn
+
 xvfb-run -a yarn test
 
 popd
