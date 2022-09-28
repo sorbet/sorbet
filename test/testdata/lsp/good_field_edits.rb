5ab71b0b797f5788fb7538722cc69dcb6e2b2dd7
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-09-28 16:34:00 -0700

    pre-work: Change field test

--- test/testdata/lsp/good_field_edits.rb
+++ test/testdata/lsp/good_field_edits.rb
@@ -1,8 +1,21 @@
-# typed: true
+# typed: strict
+
 class C
+  extend T::Sig
+
   @@foo = T.let(3, Integer)
 
+  sig {void}
   def bar
-    @@quz
+    @@quz # error: Use of undeclared variable `@@quz`
   end
+end
+
+class D < C
+  T.reveal_type( # error: `Integer`
+    @@foo
+  )
+  T.reveal_type( # error: `T.untyped`
+    @@food # error: Use of undeclared variable `@@food`
+  )
 end
