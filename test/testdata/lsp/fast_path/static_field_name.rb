131c5949973e3ec925ba5ed6c1ce658d4e650e87
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-09-28 18:02:45 -0700

    stale -> fast path

--- test/testdata/lsp/fast_path/static_field_name.rb
+++ test/testdata/lsp/fast_path/static_field_name.rb
@@ -2,26 +2,20 @@
 
 class X
   #   ^ hover: T.class_of(X)
-  # ___________________________________________
   extend T::Sig
   sig {params(x: Integer).void}
   def foo(
     # ^ hover: sig {params(x: Integer).void}
     # ^ hover: def foo(x); end
-    # _________________________________________
     x
   # ^ hover: Integer
-  # _________________________________________
   )
     puts(x)
     #    ^ hover: Integer
-    #    _________________________________________
   end
 end
 
 AA = 123
 #^ hover: Integer
-#_________________________________________
 puts AA
 #    ^ hover: Integer
-#    _________________________________________
