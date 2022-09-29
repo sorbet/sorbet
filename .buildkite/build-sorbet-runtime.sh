1f96a1030cbe5db51bb636894151d02d11ce93d5
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-09-28 18:25:55 -0700

    Skip rubocop on Ruby 3.1
    We can do a Rubocop upgrade some other time.
    
    It looks like 1.14.0 is the first version with Ruby 3.1 support:
    
    - <https://github.com/rubocop/rubocop/commit/93a325fbf9dbf8f49f4666b7a5f330e0ef755f68>
    - <https://github.com/rubocop/rubocop/releases/tag/v1.14.0>

--- .buildkite/build-sorbet-runtime.sh
+++ .buildkite/build-sorbet-runtime.sh
@@ -23,9 +23,12 @@ for runtime_version in "${runtime_versions[@]}"; do
 
   failed=
 
-  echo "+++ rubocop ($runtime_version)"
-  if ! rbenv exec bundle exec rake rubocop; then
-    failed=1
+  if [ "$runtime_version" = "2.7.2" ]; then
+    # Our Rubocop version doesn't understand Ruby 3.1 as a valid Ruby version
+    echo "+++ rubocop ($runtime_version)"
+    if ! rbenv exec bundle exec rake rubocop; then
+      failed=1
+    fi
   fi
 
   echo "+++ tests ($runtime_version)"
