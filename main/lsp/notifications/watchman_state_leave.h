5603173822b6b89cf137e56a9a6c898ba7afc5e3
Nathan Froyd <froydnj@gmail.com>
2022-09-30 11:38:22 -0400

    add actual `LSPTask`s for `state-enter`/`state-leave`

---
+++ main/lsp/notifications/watchman_state_leave.h
@@ -0,0 +1,21 @@
+#ifndef RUBY_TYPER_LSP_NOTIFICATIONS_WATCHMAN_STATE_LEAVE_H
+#define RUBY_TYPER_LSP_NOTIFICATIONS_WATCHMAN_STATE_LEAVE_H
+
+#include "main/lsp/LSPTask.h"
+
+namespace sorbet::realmain::lsp {
+class WatchmanStateLeaveTask final : public LSPTask {
+public:
+    const std::unique_ptr<WatchmanStateLeave> params;
+
+    WatchmanStateLeaveTask(const LSPConfiguration &config, std::unique_ptr<WatchmanStateLeave> params);
+
+    LSPTask::Phase finalPhase() const override;
+
+    void preprocess(LSPPreprocessor &preprocessor) override;
+
+    void run(LSPTypecheckerInterface &tc) override;
+};
+} // namespace sorbet::realmain::lsp
+
+#endif
