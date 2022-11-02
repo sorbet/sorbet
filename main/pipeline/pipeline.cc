924d52828c29d4caa6ffad9ef1c56b648b7c3689
Nathan Froyd <froydnj@gmail.com>
2022-11-02 09:00:10 -0400

    fix `autogenWriteCacheFile`

--- main/pipeline/pipeline.cc
+++ main/pipeline/pipeline.cc
@@ -1219,7 +1219,7 @@ bool cacheTreesAndFiles(const core::GlobalState &gs, WorkerPool &workers, vector
     return written;
 }
 
-vector<ast::ParsedFile> autogenWriteCacheFile(const core::GlobalState &gs, string_view cachePath,
+vector<ast::ParsedFile> autogenWriteCacheFile(const core::GlobalState &gs, const string &cachePath,
                                               vector<ast::ParsedFile> what, WorkerPool &workers) {
 #ifndef SORBET_REALMAIN_MIN
     Timer timeit(gs.tracer(), "autogenWriteCacheFile");
