517fc26dded82958f89407027589605e90478070
Aditya Anchuri <aadi@stripe.com>
2022-11-18 14:55:55 -0800

    Fix deepCopy logic

--- core/GlobalState.cc
+++ core/GlobalState.cc
@@ -1986,6 +1986,7 @@ unique_ptr<GlobalState> GlobalState::deepCopy(bool keepId) const {
     result->autocorrect = this->autocorrect;
     result->ensureCleanStrings = this->ensureCleanStrings;
     result->runningUnderAutogen = this->runningUnderAutogen;
+    result->autogenPrintingDepDB = this->autogenPrintingDepDB;
     result->censorForSnapshotTests = this->censorForSnapshotTests;
     result->sleepInSlowPathSeconds = this->sleepInSlowPathSeconds;
     result->requiresAncestorEnabled = this->requiresAncestorEnabled;
@@ -2082,6 +2083,7 @@ unique_ptr<GlobalState> GlobalState::copyForIndex() const {
     result->autocorrect = this->autocorrect;
     result->ensureCleanStrings = this->ensureCleanStrings;
     result->runningUnderAutogen = this->runningUnderAutogen;
+    result->autogenPrintingDepDB = this->autogenPrintingDepDB;
     result->censorForSnapshotTests = this->censorForSnapshotTests;
     result->lspExperimentalFastPathEnabled = this->lspExperimentalFastPathEnabled;
     result->sleepInSlowPathSeconds = this->sleepInSlowPathSeconds;
