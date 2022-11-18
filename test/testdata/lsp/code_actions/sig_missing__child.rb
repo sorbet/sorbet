5ca2be1000bb1f8ae48011e62b6e4cc43fd72910
Jake Zimmerman <zimmerman.jake@gmail.com>
2022-11-18 12:52:19 -0800

    Fix rbedited tests

--- test/testdata/lsp/code_actions/sig_missing__child.rb
+++ test/testdata/lsp/code_actions/sig_missing__child.rb
@@ -6,6 +6,6 @@ require_relative './sig_missing__parent.rb'
 class FooChild < Foo
   def self.bar
 # ^^^^^^^^^^^^ error: The method `bar` does not have a `sig`
-# ^^^^^^^^^^^^ apply-code-action: [A] Add `sig {override.void}`
+# ^^^^^^^^^^^^ apply-code-action: [A] Add `sig { override.void }`
   end
 end
