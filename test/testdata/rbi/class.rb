3bb563e01ff7d81892ca2387f402b0fb327a6a71
Emily Samp <emily.giurleo@shopify.com>
2022-12-05 16:53:16 -0600

    Specify return type of Class.new using RBI sigs (#6589)
    * Cast return type of Class.new using RBI sigs
    
    The goal of this commit is to cast the return of Class.new so that it
    takes into account the type of the argument. This is our third attempt,
    this time using RBI sigs and generics to specify that the return type
    should be the same as the argument.
    
    Co-authored-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>
    
    * Run `tools/scripts/update_testdata_exp.sh` to fix snapshop failure
    
    * Revert unnecessary test changes generated by script
    
    Co-authored-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

--- test/testdata/rbi/class.rb
+++ test/testdata/rbi/class.rb
@@ -1,23 +1,36 @@
-
 # typed: true
+
 class Parent
   def self.foo; end
 end
 
-Class.new
-Class.new(Parent)
+c1 = Class.new
+T.reveal_type(c1) # error: Revealed type: `Class`
+T.reveal_type(c1.new) # error: Revealed type: `Object`
 
-Class.new {|cls| cls.superclass}
-Class.new(Parent) {|cls| cls.superclass}
+c2 = Class.new(Parent)
+T.reveal_type(c2) # error: Revealed type: `T.class_of(Parent)`
+T.reveal_type(c2.new) # error: Revealed type: `Parent`
+
+c3 = Class.new { |cls| cls.superclass }
+T.reveal_type(c3) # error: Revealed type: `Class`
+T.reveal_type(c3.new) # error: Revealed type: `Object`
+
+c4 = Class.new(Parent) { |cls| cls.superclass }
+T.reveal_type(c4) # error: Revealed type: `T.class_of(Parent)`
+T.reveal_type(c4.new) # error: Revealed type: `Parent`
 
 # Our ClassNew Rewriter pass can only re-write Class.new where the lefthand
 # side is assigned to a constant
-Class.new(Parent).foo # error: Method `foo` does not exist on `Class`
+Class.new(Parent).foo
 c = Class.new(Parent)
-c.foo # error: Method `foo` does not exist on `Class`
+c.foo
+c.new.foo # error: Method `foo` does not exist on `Parent`
 
 C = Class.new(Parent) do |cls|
   cls.foo
   foo
 end
 C.foo
+
+Class.new('Foo') # error: Expected `T.all(Class, T.type_parameter(:Parent))` but found `String("Foo")` for argument `super_class`
