170539da474704458009e2797a1b2e711f9b0eba
Jake Zimmerman <zimmerman.jake@gmail.com>
2023-01-04 14:39:51 -0600

    Add enclosingMethodDefLoc to IdentResponse
    It's a lot easier to just collect this from the IdentResponse than it is
    to try to recreate it from the information contained in a VAR lsp query.

--- infer/environment.cc
+++ infer/environment.cc
@@ -1163,9 +1163,9 @@ Environment::processBinding(core::Context ctx, const cfg::CFG &inWhat, cfg::Bind
                 tp.origins = typeAndOrigin.origins;
 
                 if (lspQueryMatch && !bind.value.isSynthetic()) {
-                    core::lsp::QueryResponse::pushQueryResponse(ctx, core::lsp::IdentResponse(ctx.locAt(bind.loc),
-                                                                                              i.what.data(inWhat), tp,
-                                                                                              ctx.owner.asMethodRef()));
+                    core::lsp::QueryResponse::pushQueryResponse(
+                        ctx, core::lsp::IdentResponse(ctx.locAt(bind.loc), i.what.data(inWhat), tp,
+                                                      ctx.owner.asMethodRef(), ctx.locAt(inWhat.loc)));
                 }
 
                 ENFORCE(ctx.file.data(ctx).hasParseErrors() || !tp.origins.empty(),
@@ -1392,7 +1392,7 @@ Environment::processBinding(core::Context ctx, const cfg::CFG &inWhat, cfg::Bind
                 if (lspQueryMatch) {
                     core::lsp::QueryResponse::pushQueryResponse(
                         ctx, core::lsp::IdentResponse(ctx.locAt(bind.loc), bind.bind.variable.data(inWhat), tp,
-                                                      ctx.owner.asMethodRef()));
+                                                      ctx.owner.asMethodRef(), ctx.locAt(inWhat.loc)));
                 }
             },
             [&](cfg::Return &i) {
--- main/lsp/LocalVarSaver.h
+++ main/lsp/LocalVarSaver.h
@@ -8,9 +8,12 @@
 namespace sorbet::realmain::lsp {
 
 class LocalVarSaver {
+    std::vector<core::Loc> enclosingMethodDefLoc;
+
 public:
     void postTransformBlock(core::Context ctx, ast::ExpressionPtr &local);
     void postTransformLocal(core::Context ctx, ast::ExpressionPtr &local);
+    void preTransformMethodDef(core::Context ctx, ast::ExpressionPtr &methodDef);
     void postTransformMethodDef(core::Context ctx, ast::ExpressionPtr &methodDef);
 };
 }; // namespace sorbet::realmain::lsp
