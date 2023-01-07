0b97994b617211198013e86e9b7eb8c2feb5838a
Aditya Anchuri <aadi@stripe.com>
2023-01-06 17:45:05 -0800

    PR feedback

--- test/testdata/packager/export_for_test/foo/foo.test.rb
+++ test/testdata/packager/export_for_test/foo/foo.test.rb
@@ -11,6 +11,10 @@ class Test::Opus::Foo::FooTest
   Test::Opus::Util::TestUtil
   Opus::Util::Nesting::Public.public_method
 
+  # util/__package.rb exposed via export_for_test, cannot access from here:
+  Opus::Util::Nesting.nesting_method
+# ^^^^^^^^^^^^^^^^^^^ error: `Opus::Util::Nesting` resolves but is not exported from `Opus::Util`
+
   # via test_import Opus::TestImported
   Opus::TestImported::TIClass
   Test::Opus::TestImported::TITestClass
--- test/testdata/packager/export_for_test/util/util.test.rb
+++ test/testdata/packager/export_for_test/util/util.test.rb
@@ -3,5 +3,7 @@
 module Test::Opus::Util
   class TestUtil; end
 
+  # Available via `export_for_test Opus::Util::UtilClass::Nesting`
+  Opus::Util::Nesting.nesting_method
   Opus::Util::Nesting::Public.public_method
 end
