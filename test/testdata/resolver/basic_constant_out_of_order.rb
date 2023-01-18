aa86dea9645bb5b76aeb41df1fe53a8c4b9457f2
Aditya Anchuri <aadi@stripe.com>
2023-01-18 15:19:06 -0800

    WIP - catch out-of-order constant references

---
+++ test/testdata/resolver/basic_constant_out_of_order.rb
@@ -0,0 +1,26 @@
+# typed: true
+
+module Foo
+  A = X 
+    # ^ error: `Foo::X` referenced before it is declared
+
+  def self.foo(arg:)
+    X # this is ok
+  end
+
+  def self.bar(&blk)
+    X # this is ok
+  end
+
+  foo arg: ->{ X } # this is ok
+
+  bar do
+    X # this is ok
+  end
+
+  class X; end
+
+  B = X # this is ok
+
+  class X; end
+end
