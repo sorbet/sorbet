ac39b11ebb781854e91b62e0a1db596508200d1f
Jake Zimmerman <zimmerman.jake@gmail.com>
2023-01-24 14:00:23 -0600

    Forcibly fetch expired yarn key

--- .buildkite/test-vscode-extension.sh
+++ .buildkite/test-vscode-extension.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 
 # TODO: We probably want to move this into https://github.com/sorbet/sorbet-build-image eventually
 
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 apt-get update
 apt-get install -y libxshmfence-dev libnss3-dev libatk1.0-0 libatk-bridge2.0-0 \
  libdrm2 xvfb libgdk-pixbuf2.0-0 libgtk-3-0 libgbm1	libasound2
