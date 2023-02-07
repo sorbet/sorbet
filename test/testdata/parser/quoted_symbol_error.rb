b28ea02149596270386015525bb5450a5322a41b
Maruth Goyal <maruth@stripe.com>
2023-02-07 13:44:17 -0800

    added double quote test

--- test/testdata/parser/quoted_symbol_error.rb
+++ test/testdata/parser/quoted_symbol_error.rb
@@ -2,7 +2,9 @@
 
 class A < T::Struct
   prop :'foo', Integer
+  prop :"bar", Integer
 end
 
-a = A.new(foo: 0)
+a = A.new(foo: 0, bar: 1)
 a.foo = '' # error: Assigning a value to `foo` that does not match expected type `Integer`
+a.bar = '' # error: Assigning a value to `bar` that does not match expected type `Integer`
