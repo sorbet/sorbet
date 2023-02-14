888da1818253395e14e346838e400f36779e7027
ilyailya <bkmz2@yandex.ru>
2023-02-13 16:42:55 -0800

    Remove rename command invocation from the "move method" refactoring

--- main/lsp/requests/code_action.cc
+++ main/lsp/requests/code_action.cc
@@ -167,15 +167,6 @@ unique_ptr<ResponseMessage> CodeActionTask::runRequest(LSPTypecheckerDelegate &t
 
             bool canResolveLazily = config.getClientConfig().clientCodeActionResolveEditSupport &&
                                     config.getClientConfig().clientCodeActionDataSupport;
-            auto newModuleLoc = getNewModuleLocation(gs, *def, typechecker);
-            auto renameCommand = make_unique<Command>("Rename Symbol", "sorbet.rename");
-            auto arg = make_unique<TextDocumentPositionParams>(
-                make_unique<TextDocumentIdentifier>(params->textDocument->uri), move(newModuleLoc));
-            auto args = vector<unique_ptr<TextDocumentPositionParams>>();
-            args.emplace_back(move(arg));
-
-            renameCommand->arguments = move(args);
-            action->command = move(renameCommand);
             if (canResolveLazily) {
                 action->data = move(params);
             } else {
