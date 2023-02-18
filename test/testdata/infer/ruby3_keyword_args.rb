ce70710668aaadd9aee1ba676bdb6d856f12c383
ilyailya <bkmz2@yandex.ru>
2023-02-17 16:51:16 -0800

    Do not convert keyword args to hash if a block is present

--- test/testdata/infer/ruby3_keyword_args.rb
+++ test/testdata/infer/ruby3_keyword_args.rb
@@ -11,3 +11,19 @@ arghash = {y: 42}
 takes_kwargs(99, arghash) # error: Keyword argument hash without `**` is deprecated
 
 takes_kwargs(99, **arghash)
+
+sig do
+  params(
+    name: String,
+    tags: T::Hash[T.untyped, T.untyped],
+    x: Integer,
+    blk: T.proc.returns(String)
+  )
+  .returns(String)
+end
+def foo(name, tags: {}, x: 42, &blk)
+  yield
+end
+
+blk = Proc.new
+foo("", tags: {}, x: 1, &blk)
