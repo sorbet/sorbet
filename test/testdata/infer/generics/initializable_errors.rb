a60e465b3bbc083b479bf59e907141315f75a0d2
Jake Zimmerman <zimmerman.jake@gmail.com>
2023-02-24 18:21:31 -0800

    wip

--- test/testdata/infer/generics/initializable_errors.rb
+++ test/testdata/infer/generics/initializable_errors.rb
@@ -4,6 +4,8 @@ module A
   extend T::Generic
 
   initializable!('') # error: Invalid param, must be a :symbol
+  #              ^^ error: Expected `Symbol` but found `String("")` for argument `variance`
+  #              ^^ error: Expected `Symbol` but found `String("")` for argument `variance`
 end
 
 module B
@@ -16,12 +18,5 @@ module C
   extend T::Generic
 
   initializable! { {nope: Integer} }
-  #                 ^^^^ error: Unknown key `nope` provided in block to `type_member`
-end
-
-module D
-  extend T::Generic
-
-  initializable!(:out, :extra)
   #                 ^^^^ error: Unknown key `nope` provided in block to `type_member`
 end
