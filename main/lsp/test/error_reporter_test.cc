d8b984a04923fbda2a7ed7d7635f8597db690836
Maruth Goyal <maruth@stripe.com>
2023-03-01 15:01:52 -0800

    fix test

--- main/lsp/test/error_reporter_test.cc
+++ main/lsp/test/error_reporter_test.cc
@@ -54,9 +54,9 @@ unique_ptr<core::GlobalState> makeGS() {
 // Generate `count` errors for `fref` in `errors`.
 void generateErrors(core::FileRef fref, vector<unique_ptr<core::Error>> &errors, uint16_t count) {
     for (uint16_t i = 0; i < count; i++) {
-        errors.emplace_back(
-            make_unique<core::Error>(core::Loc(fref, 0, 0), core::ErrorClass{1, core::StrictLevel::True}, "MyError",
-                                     vector<core::ErrorSection>(), vector<core::AutocorrectSuggestion>(), false));
+        errors.emplace_back(make_unique<core::Error>(
+            core::Loc(fref, 0, 0), core::ErrorClass{1, core::StrictLevel::True}, "MyError",
+            vector<core::ErrorSection>(), vector<core::AutocorrectSuggestion>(), false, core::ErrorSeverity::Error));
     }
 }
 uint32_t errorsReported(LSPOutputToVector &outputVector) {
