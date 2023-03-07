adee3ad33057f0fb51d87f9770b93f5fc1ea2d76
Jake Zimmerman <zimmerman.jake@gmail.com>
2023-03-06 17:12:04 -0800

    Add failing test

---
+++ test/testdata/infer/no_valid_instantiation_result.rb
@@ -0,0 +1,23 @@
+# typed: true
+extend T::Sig
+
+sig do
+  type_parameters(:U)
+    .params(
+      x: T.type_parameter(:U),
+      f: T.proc.params(x: T.type_parameter(:U)).void,
+    )
+    .returns(T.type_parameter(:U))
+end
+def apply_f(x, f)
+  x
+end
+
+sig {params(str: String).void}
+def main(str)
+  f = T.let(->(y) { y }, T.proc.params(x: Integer).void)
+  res = apply_f(str, f)
+  #     ^^^^^^^ error: Could not find valid instantiation of type parameters for `Object#apply_f`
+  #     ^^^^^^^^^^^^^^^ error: Expression does not have a fully-defined type (Did you reference another class's type members?)
+  T.reveal_type(res) # error: `T.untyped`
+end
