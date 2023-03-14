b6c497605b8f438914e279d5114c1266df222a5b
Douglas Eichelberger <dduugg@users.noreply.github.com>
2023-03-14 07:29:37 -0700

    Improve Pathname sigs (#6846)

--- test/testdata/rbi/pathname.rb
+++ test/testdata/rbi/pathname.rb
@@ -9,3 +9,10 @@ end
 T.reveal_type(md_file) # error: `T.nilable(Pathname)`
 
 T.reveal_type(Pathname('/')) # error: `Pathname`
+T.reveal_type(Pathname(Pathname('/'))) # error: `Pathname`
+
+T.reveal_type(Pathname.glob(['*.rb', Pathname('/')])) # error: `T::Array[Pathname]`
+T.reveal_type(Pathname.glob(['*.rb', Pathname('/')]) { puts _1 }) # error: `NilClass`
+
+T.reveal_type(Pathname('/usr/bin').glob(['*.d', Pathname('/')])) # error: `T::Array[Pathname]`
+T.reveal_type(Pathname('/usr/bin').glob(['*.d', Pathname('/')]) { puts _1 }) # error: `NilClass`
