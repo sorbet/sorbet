5c40623c51174ae999500ef44d2f3290748a66a2
unknown <aadi@stripe.com>
2023-03-30 00:09:46 +0000

    Add test for feature

---
+++ test/cli/autogen-pkg-autoloader-non-annotated/bar2.rb
@@ -0,0 +1,9 @@
+# typed: true
+
+module RootPackage::Yabba::Dabba
+  class Bar2
+    p 'x'
+  end
+
+  class NoBehavior; end
+end
---
+++ test/cli/autogen-pkg-autoloader-non-annotated/scripts/baz.rb
@@ -0,0 +1,8 @@
+# typed: true
+
+# This file should be excluded
+require 'script_gem'
+
+module RootPackage::Foo::Baz
+  p 'x'
+end
