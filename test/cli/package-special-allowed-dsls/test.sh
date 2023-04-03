8556f537606a37c0d38a457ee4caf413fb20c3a7
Jake Zimmerman <zimmerman.jake@gmail.com>
2023-04-03 11:57:46 -0700

    Use `--max-threads=0` in a newly-created CLI test (#6896)

--- test/cli/package-special-allowed-dsls/test.sh
+++ test/cli/package-special-allowed-dsls/test.sh
@@ -1,4 +1,4 @@
 cd test/cli/package-special-allowed-dsls || exit 0
 
-../../../main/sorbet --silence-dev-message --stripe-packages . 2>&1
+../../../main/sorbet --silence-dev-message --stripe-packages --max-threads=0 . 2>&1
 
