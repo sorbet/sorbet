0d4651b751918b08601f33e5d9bf5b323b4927b3
Jake Zimmerman <zimmerman.jake@gmail.com>
2023-04-06 15:20:55 -0700

    Delete existing `"This code is untyped"` error

--- infer/inference.cc
+++ infer/inference.cc
@@ -298,14 +298,6 @@ unique_ptr<cfg::CFG> Inference::run(core::Context ctx, unique_ptr<cfg::CFG> cfg)
                         typedSendCount++;
                     } else if (bind.bind.type.hasUntyped()) {
                         DEBUG_ONLY(histogramInc("untyped.sources", bind.bind.type.untypedBlame().rawId()););
-                        auto what = core::errors::Infer::errorClassForUntyped(ctx.state, ctx.file);
-                        if (auto e = ctx.beginError(bind.loc, what)) {
-                            e.setHeader("This code is untyped");
-                            if (what == core::errors::Infer::UntypedValue) {
-                                e.addErrorNote("Support for `{}` is minimal. Consider using `{}` instead.",
-                                               "typed: strong", "typed: strict");
-                            }
-                        }
                     }
                 }
                 ENFORCE(bind.bind.type);
