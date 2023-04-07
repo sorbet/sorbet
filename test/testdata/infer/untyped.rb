df54432346fc1aebf74d95ef05406c410168096e
Jake Zimmerman <zimmerman.jake@gmail.com>
2023-04-06 17:43:46 -0700

    Add a test

---
+++ test/testdata/infer/untyped.rb
@@ -0,0 +1,43 @@
+# typed: strong
+extend T::Sig
+
+sig {returns(T.untyped)}
+def returns_untyped
+end
+
+sig {params(x: Integer).void}
+def takes_integer(x)
+end
+
+if T.unsafe(nil)
+  #^^^^^^^^^^^^^ error: Conditional branch on `T.untyped`
+  x = returns_untyped
+else
+  x = 0
+end
+
+  x.foo
+# ^ error: Call to method `foo` on `T.untyped`
+
+takes_integer(
+  T.unsafe(0)
+# ^^^^^^^^^^^ error: Argument passed to parameter `x` is `T.untyped`
+)
+
+sig {params(blk: T.proc.void).void}
+def example(&blk)
+end
+f = ->(){}
+example(&f)
+
+class Parent
+  def foo # error: does not have a `sig`
+  end
+end
+
+class Child < Parent
+  def foo # error: does not have a `sig`
+    super
+    nil
+  end
+end
