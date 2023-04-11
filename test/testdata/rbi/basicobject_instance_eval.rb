cd2f6f8f0220df1d2b2fb2914a4cc41b65cb1a38
Jake Zimmerman <zimmerman.jake@gmail.com>
2023-04-11 14:17:45 -0700

    Revert "Add the self parameter for `instance_eval` on `BasicObject` (#6914)" (#6923)
    This reverts commit f5b5a186051a77a20dcb1d991b72d1ab96ad681a.

--- test/testdata/rbi/basicobject_instance_eval.rb
+++ test/testdata/rbi/basicobject_instance_eval.rb
@@ -1,5 +1,17 @@
 # typed: true
+class A
+  extend T::Sig
+  sig do
+    type_parameters(:U)
+    .params(
+        blk: T.proc.bind(T.untyped).returns(T.type_parameter(:U)),
+    )
+    .returns(T.type_parameter(:U))
+  end
+  def instance_eval(&blk)
+    blk.call
+  end
+end
 
-T.reveal_type(4.instance_eval {4}) # error: Revealed type: `Integer`
-4.instance_eval {T.reveal_type(self)} # error: Revealed type: `T.untyped`
-4.instance_eval {|x| T.reveal_type(x)} # error: Revealed type: `T.untyped`
+T.reveal_type(A.new.instance_eval {4}) # error: Revealed type: `Integer`
+A.new.instance_eval {T.reveal_type(self)} # error: Revealed type: `T.untyped`
