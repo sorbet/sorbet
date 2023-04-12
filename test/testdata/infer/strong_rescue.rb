f61f3844707df36d2f5734002bf0465435acef23
Nathan Froyd <froydnj@gmail.com>
2023-04-12 08:55:10 -0400

    delete errors

--- test/testdata/infer/strong_rescue.rb
+++ test/testdata/infer/strong_rescue.rb
@@ -1,17 +1,10 @@
 # typed: strong
 extend T::Sig
 
-# Sorbet's implementation of exceptions currently heavily relies on T.untyped
-# Fixing that involves more work than I have time for, so let's at least just
-# not spam an entire method with untyped squiggles.
-
 sig {void}
 def example1
   begin
   rescue TypeError => e
-# ^^^^^^ error: Conditional branch on `T.untyped`
-# ^^^^^^ error: Conditional branch on `T.untyped`
-    #    ^^^^^^^^^ error: Argument passed to parameter `other` is `T.untyped`
     T.reveal_type(e) # error: `TypeError`
   else
   ensure
@@ -22,9 +15,6 @@ sig {void}
 def example2
   begin
   rescue; puts("")
-# ^^^^^^ error: Conditional branch on `T.untyped`
-# ^^^^^^ error: Conditional branch on `T.untyped`
-# ^^^^^^ error: Argument passed to parameter `other` is `T.untyped`
   ensure
   end
 end
@@ -33,9 +23,6 @@ sig {void}
 def example3
   begin
   rescue => e; T.reveal_type(e)
-# ^^^^^^ error: Conditional branch on `T.untyped`
-# ^^^^^^ error: Conditional branch on `T.untyped`
-    #       ^ error: Argument passed to parameter `other` is `T.untyped`
     #          ^^^^^^^^^^^^^^^^ error: `StandardError`
   ensure
   end
@@ -46,9 +33,6 @@ def example4
   begin
     puts("here we are")
   rescue; puts("")
-# ^^^^^^ error: Conditional branch on `T.untyped`
-# ^^^^^^ error: Conditional branch on `T.untyped`
-# ^^^^^^ error: Argument passed to parameter `other` is `T.untyped`
   ensure
   end
 end
@@ -57,9 +41,6 @@ sig {void}
 def example5
   begin
   rescue Exception => e
-# ^^^^^^ error: Conditional branch on `T.untyped`
-# ^^^^^^ error: Conditional branch on `T.untyped`
-    #    ^^^^^^^^^ error: Argument passed to parameter `other` is `T.untyped`
     T.reveal_type(e) # error: `Exception`
   else
   ensure
