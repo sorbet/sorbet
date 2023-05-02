fa06d278f3498f940e820283b31743a0f275f615
Akshay Joshi <me@akshayjoshi.com>
2023-05-02 12:21:42 -0400

    is_a?

--- gems/sorbet-runtime/lib/types/props/private/serde_transform.rb
+++ gems/sorbet-runtime/lib/types/props/private/serde_transform.rb
@@ -99,7 +99,7 @@ module T::Props
             else
               "#{varname}.nil? ? nil : #{inner}"
             end
-          elsif type.types.all? {|x| x < T::Types::Simple && x.raw_type < T::Props::Serializable}
+          elsif type.types.all? {|x| x.is_a?(T::Types::Simple) && x.raw_type < T::Props::Serializable}
             case mode
             when Serialize
               "#{varname}.serialize(strict).merge!(__class__: #{varname}.class.name)"
