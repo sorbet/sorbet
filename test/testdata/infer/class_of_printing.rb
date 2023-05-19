53bbdad3b5fba7a8392d2de459fcd63db3d9ef92
Jake Zimmerman <zimmerman.jake@gmail.com>
2023-05-19 13:23:29 -0700

    Show changes

--- test/testdata/infer/class_of_printing.rb
+++ test/testdata/infer/class_of_printing.rb
@@ -7,12 +7,12 @@ class Parent; end
 
 sig {params(x: T.all(T.class_of(Parent), T::Class[Left])).void}
 def example1(x)
-  T.reveal_type(x) # error: `T.class_of(Parent)`
+  T.reveal_type(x) # error: `T.class_of(Parent)[T.all(Parent, Left)]`
 end
 
 sig {params(x: T.all(T.class_of(Parent), T::Class[Right])).void}
 def example2(x)
-  T.reveal_type(x) # error: `T.class_of(Parent)`
+  T.reveal_type(x) # error: `T.class_of(Parent)[T.all(Parent, Right)]`
   example1(x)
-  #        ^ error: `T.class_of(Parent)` but found `T.class_of(Parent)` for argument `x`
+  #        ^ error: `T.class_of(Parent)[T.all(Parent, Left)]` but found `T.class_of(Parent)[T.all(Parent, Right)]` for argument `x`
 end
