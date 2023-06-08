6f3513a36c872bec01348ef9daf475d365a1c00b
Jake Zimmerman <zimmerman.jake@gmail.com>
2023-06-08 15:47:16 -0700

    Demonstrate `--did-you-mean=false`

--- test/cli/did-you-mean-false/test.sh
+++ test/cli/did-you-mean-false/test.sh
@@ -8,7 +8,7 @@ tmp="$(mktemp -d)"
 cp "$infile" "$tmp"
 
 cd "$tmp" || exit 1
-if "$cwd/main/sorbet" --silence-dev-message -a test.rb 2>&1; then
+if "$cwd/main/sorbet" --silence-dev-message -a --did-you-mean=false test.rb 2>&1; then
   echo "Expected to fail!"
   exit 1
 fi
