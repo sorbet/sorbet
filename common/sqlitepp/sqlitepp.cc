a5d23ee5cce160a421d6556b050f283628c1c26a
Maruth Goyal <maruth@stripe.com>
2023-06-25 10:13:03 -0700

    sqlitepp

---
+++ common/sqlitepp/sqlitepp.cc
@@ -0,0 +1,26 @@
+#include "sqlitepp.h"
+#include "absl/strings/str_format.h"
+#include "absl/strings/str_join.h"
+
+namespace sorbet::sqlitepp {
+void SqliteDb::create(const SqliteTable table) {
+    auto query = absl::StrCat("CREATE TABLE ", table.tableName, table.schema.toString(), ";");
+    simpleExec(query);
+}
+
+void SqliteDb::insert(const SqliteTable table, const std::vector<std::variant<int, float, std::string>> &values) {
+    auto query = absl::StrCat("INSERT INTO ", table.tableName, " ", table.schema.toStringOnlyNames(), " VALUES (",
+                              absl::StrJoin(values, ","), ")", ";");
+    simpleExec(query);
+}
+void SqliteDb::simpleExec(const std::string &query) {
+    char *errMsg;
+    auto rc = sqlite3_exec(db, query.c_str(), NULL, NULL, &errMsg);
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "SQL error: %s\n", errMsg);
+        sqlite3_free(errMsg);
+    } else {
+        fprintf(stdout, "Table created successfully\n");
+    }
+}
+} // namespace sorbet::sqlitepp
---
+++ common/sqlitepp/sqlitepp.h
@@ -0,0 +1,116 @@
+#pragma once
+#include "absl/strings/str_join.h"
+#include "common/common.h"
+#include "sqlite3.h"
+#include <memory>
+
+namespace sorbet::sqlitepp {
+
+enum SqliteColumnType {
+    Null,
+    Integer,
+    Real,
+    Text,
+    Blob,
+};
+
+enum SqliteColumnConstraint {
+    PRIMARY_KEY,
+    UNIQUE,
+    NOT_NULL,
+};
+
+struct SqliteColumn {
+    const std::string name;
+    const SqliteColumnType dataType;
+    const std::vector<SqliteColumnConstraint> constraints;
+
+public:
+    SqliteColumn(const std::string name, const SqliteColumnType dataType)
+        : name(name), dataType(dataType), constraints() {}
+
+    const std::string getName() const {
+        return name;
+    }
+
+    const std::string toString() const {
+        return absl::StrCat(name, " ", dataTypeToString(dataType), " ", constraintSetToString());
+    }
+
+    const std::string constraintSetToString() const {
+        std::string s = "";
+        for (auto &constraint : constraints) {
+            absl::StrAppend(&s, constraintToString(constraint), " ");
+        }
+
+        return s;
+    }
+
+    const std::string constraintToString(SqliteColumnConstraint constraint) const {
+        switch (constraint) {
+            case UNIQUE:
+                return "UNIQUE";
+            case PRIMARY_KEY:
+                return "PRIMARY KEY";
+            case NOT_NULL:
+                return "NOT NULL";
+        }
+    }
+
+    const std::string dataTypeToString(SqliteColumnType type) const {
+        switch (type) {
+            case Null:
+                return "NULL";
+            case Integer:
+                return "INTEGER";
+            case Real:
+                return "REAL";
+            case Text:
+                return "TEXT";
+            case Blob:
+                return "BLOB";
+        }
+    }
+};
+
+struct SqliteSchema {
+    const std::vector<SqliteColumn> columns;
+
+public:
+    const std::string toString() const {
+        std::string s = "(";
+        std::vector<std::string> cols;
+        for (auto &col : columns) {
+            cols.push_back(col.toString());
+        }
+        return absl::StrCat("(", absl::StrJoin(cols, ","), ")");
+    }
+
+    const std::string toStringOnlyNames() const {
+        std::string s = "(";
+        std::vector<std::string> cols;
+        for (auto &col : columns) {
+            cols.push_back(col.getName());
+        }
+        return absl::StrCat("(", absl::StrJoin(cols, ","), ")");
+    }
+};
+
+struct SqliteTable {
+    const std::string tableName;
+    const SqliteSchema schema;
+};
+
+class SqliteDb {
+    sqlite3 *db;
+    void simpleExec(const std::string &query);
+
+public:
+    SqliteDb(const std::string dbName) {
+        sqlite3_open(dbName.c_str(), &db);
+    }
+
+    void create(const SqliteTable table);
+    void insert(const SqliteTable table, const std::vector<std::variant<int, float, std::string>> &values);
+};
+} // namespace sorbet::sqlitepp
