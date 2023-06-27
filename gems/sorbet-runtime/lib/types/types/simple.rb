e02caa04ca2b1053c582d6070d8820ac3f44f2d9
Aiden Storey <git@storey.dev>
2023-06-26 20:30:19 -0400

    Add fallback for anon class with #name

--- gems/sorbet-runtime/lib/types/types/simple.rb
+++ gems/sorbet-runtime/lib/types/types/simple.rb
@@ -19,7 +19,7 @@ module T::Types
       #
       # `name` isn't normally a hot path for types, but it is used in initializing a T::Types::Union,
       # and so in `T.nilable`, and so in runtime constructions like `x = T.let(nil, T.nilable(Integer))`.
-      @name ||= NAME_METHOD.bind(@raw_type).call.freeze
+      @name ||= (NAME_METHOD.bind(@raw_type).call || @raw_type.name).freeze
     end
 
     # overrides Base
