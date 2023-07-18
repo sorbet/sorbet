97b041e7dad871d7116fb309d7c01e36abc62285
Daniel Molina <damolina@stripe.com>
2023-07-18 11:26:07 -0700

    Update missing configuration logs
    Update missing configuration log message to include a hint on adding `sorbet/config`.

--- vscode_extension/src/sorbetStatusProvider.ts
+++ vscode_extension/src/sorbetStatusProvider.ts
@@ -171,12 +171,14 @@ export class SorbetStatusProvider implements Disposable {
    */
   public async startSorbet(): Promise<void> {
     if (this.isStarting) {
-      this.context.log.trace("Ignoring start request, already starting.");
+      this.context.log.trace("Ignored start request, already starting.");
       return;
     }
 
     if (!this.context.configuration.activeLspConfig) {
-      this.context.log.info("Ignoring start request, no active configuration.");
+      this.context.log.info(
+        "Ignored start request, no active configuration. If you want to use Sorbet in this project, you need to add a sorbet/config file.",
+      );
       return;
     }
 
