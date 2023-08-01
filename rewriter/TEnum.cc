4d1fd0ab13995487ce26e4a1f15544084209f3c6
Neil Parikh <parikh.neil@me.com>
2023-08-01 10:50:32 -0700

    Fix crash in TEnum rewriter pass (#7193)
    * Only call underlying on proxy type
    
    * Use dropLiteral
    
    Co-authored-by: Jake Zimmerman <zimmerman.jake@gmail.com>
    
    ---------
    
    Co-authored-by: Jake Zimmerman <zimmerman.jake@gmail.com>

--- rewriter/TEnum.cc
+++ rewriter/TEnum.cc
@@ -134,9 +134,9 @@ std::optional<ProcessStatResult> processStat(core::MutableContext ctx, ast::Clas
         serializeType = core::Types::String();
     } else if (selfNew->numPosArgs() == 1) {
         if (auto *selfNewArg = ast::cast_tree<ast::Literal>(selfNew->getPosArg(0))) {
-            // If the enum has exactly one variant that as a literal passed (ex. "a"),
+            // If the enum has exactly one variant that has a literal passed (ex. "a"),
             // then its type will be String("a"), but we want a ClassType as the return type.
-            serializeType = selfNewArg->value.underlying(ctx);
+            serializeType = core::Types::dropLiteral(ctx, selfNewArg->value);
         }
     }
 
