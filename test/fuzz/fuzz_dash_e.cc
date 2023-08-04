70ab49de01353b6bf2ab61ec2994ad802c843a74
Jake Zimmerman <zimmerman.jake@gmail.com>
2023-08-03 17:19:27 -0700

    fixup: Previous PR forgot to make this edit
    See changes in ddf9172f2

--- test/fuzz/fuzz_dash_e.cc
+++ test/fuzz/fuzz_dash_e.cc
@@ -69,7 +69,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
         file.data(*gs).strictLevel = core::StrictLevel::True;
     }
 
-    indexed = realmain::pipeline::index(*gs, inputFiles, *opts, *workers, kvstore);
+    indexed = realmain::pipeline::index(*gs, absl::Span<core::FileRef>(inputFiles), *opts, *workers, kvstore);
     auto foundHashes = nullptr;
     indexed = move(realmain::pipeline::resolve(gs, move(indexed), *opts, *workers, foundHashes).result());
     realmain::pipeline::typecheck(*gs, move(indexed), *opts, *workers);
