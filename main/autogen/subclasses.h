b12af013d44b99f064c27d6c3dd0f7037151edd7
Youssef Soliman <youssefdevelops@gmail.com>
2023-08-10 10:59:02 -0700

    Add paths option to autogen subclasses generation (#7191)
    * Add option for paths in subclasses output
    
    * Add functionality to show paths alongside subclasses output
    
    * Add tests
    
    * Move format strings outside of loop
    
    * Add SymbolRefs to autogen types
    
    * Add string -> symbol and symbol -> name helpers
    
    * Switch to using SymbolRef and Definitions instead of strings
    
    * Indicate `file` is unused
    
    * Remove unused hash function
    
    * Switch to equality by symbol
    
    * Store 8 byte struct instead of 36 bytes
    
    * Only rely on SymbolRefs
    
    * Make global state constant
    
    * Use `getPrintablePath` instead of manually stripping path
    
    * Use `sym.show`
    
    * Remove symbolName helper
    
    * Do exists check before map lookup
    
    * Remove need for SymbolRef existence check
    
    * Remove parent existence check in merge
    
    * Explicitly don't include path if showPath is false
    
    * Remove neglected comment
    
    * Switch Entries to ClassOrModuleRef
    
    * Inline format string with path
    
    * Remove "Add option for paths in subclasses output"
    
    This reverts commit 1a6e4709d6fb4950049e9bdb504615cfd24bf973.
    
    * Remove dependency on showPaths option
    
    * Update subclasses-ignore test

--- main/autogen/subclasses.h
+++ main/autogen/subclasses.h
@@ -7,8 +7,7 @@ namespace sorbet::autogen {
 
 class Subclasses final {
 public:
-    using Entry = std::pair<std::string, Definition::Type>;
-    using Entries = UnorderedSet<Entry>;
+    using Entries = UnorderedSet<core::ClassOrModuleRef>;
     struct SubclassInfo {
         ClassKind classKind = ClassKind::Module;
         Entries entries;
@@ -16,20 +15,23 @@ public:
         SubclassInfo() = default;
         SubclassInfo(ClassKind classKind, Entries entries) : classKind(classKind), entries(std::move(entries)){};
     };
-    using Map = UnorderedMap<std::string, SubclassInfo>;
+    using Map = UnorderedMap<core::SymbolRef, SubclassInfo>;
 
-    static std::optional<Subclasses::Map> listAllSubclasses(core::Context ctx, ParsedFile &pf,
+    static std::optional<Subclasses::Map> listAllSubclasses(core::Context ctx, const ParsedFile &pf,
                                                             const std::vector<std::string> &absoluteIgnorePatterns,
-                                                            const std::vector<std::string> &relativeIgnorePatterns);
-    static std::vector<std::string> genDescendantsMap(Subclasses::Map &childMap, std::vector<std::string> &parentNames);
+                                                            const std::vector<std::string> &relativeIgnorePattern);
+    static std::vector<std::string> genDescendantsMap(const core::GlobalState &gs, Subclasses::Map &childMap,
+                                                      std::vector<core::SymbolRef> &parentRefs);
+    static const core::SymbolRef getConstantRef(const core::GlobalState &gs, std::string rawName);
 
 private:
-    static void patchChildMap(Subclasses::Map &childMap);
+    static void patchChildMap(const core::GlobalState &gs, Subclasses::Map &childMap);
     static bool isFileIgnored(const std::string &path, const std::vector<std::string> &absoluteIgnorePatterns,
                               const std::vector<std::string> &relativeIgnorePatterns);
-    static std::optional<SubclassInfo> descendantsOf(const Subclasses::Map &childMap, const std::string &parents);
-    static std::vector<std::string> serializeSubclassMap(const Subclasses::Map &descendantsMap,
-                                                         const std::vector<std::string> &parentNames);
+    static std::optional<SubclassInfo> descendantsOf(const Subclasses::Map &childMap, const core::SymbolRef &parentRef);
+    static std::vector<std::string> serializeSubclassMap(const core::GlobalState &gs,
+                                                         const Subclasses::Map &descendantsMap,
+                                                         const std::vector<core::SymbolRef> &parentNames);
 };
 
 } // namespace sorbet::autogen
