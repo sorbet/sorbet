cd6e895f23de54cc00379d1c54ea75b191ddfffc
Jake Zimmerman <zimmerman.jake@gmail.com>
2023-08-28 14:04:11 -0700

    Add some tests

---
+++ test/testdata/rewriter/struct_super_strict.rb
@@ -0,0 +1,19 @@
+# typed: strict
+# highlight-untyped-values: true
+class Module; include T::Sig; end
+
+WithoutAnyOverride = Struct.new(:foo) do
+  # We did not always generate `T.untyped` signatures for Struct members, which
+  # prevented using `Struct.new` in `# typed: strict` files unless you went and
+  # defined an override for every method.
+end
+
+ExplicitOverride = Struct.new(:foo) do
+  sig {returns(String)}
+  def foo; super; end
+  #        ^^^^^ untyped: Value returned from method is `T.untyped`
+
+  sig {params(foo: String).returns(String)}
+  def foo=(foo); super; end
+  #              ^^^^^ untyped: Value returned from method is `T.untyped`
+end
