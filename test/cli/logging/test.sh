0ed2024fec6ba92b6f0e313b0ca89770f3e021aa
Jake Zimmerman <zimmerman.jake@gmail.com>
2023-09-12 16:26:55 -0700

    Revert "🔊 Make combinedLogger use the same time format as debug loggers (#7301)" (#7307)
    This reverts commit fb4754a802629845674e429ea5859636e4655608.

--- test/cli/logging/test.sh
+++ test/cli/logging/test.sh
@@ -2,7 +2,7 @@ LOG_FILE=$(mktemp)
 main/sorbet --silence-dev-message -e '1' -q --debug-log-file="$LOG_FILE"
 echo LOG BEGINS
 # only keep message parts, drop timings and the entire counter section
-sed -E "s/\\[[0-9]+\\-[0-9]+-[0-9]+T[0-9]+:[0-9]+:[0-9]+.[0-9]+\\]/TIMESTAMP/"< "$LOG_FILE" |  # replace timestamps
+sed -E "s/\\[[0-9]+\\-[0-9]+-[0-9]+ [0-9]+:[0-9]+:[0-9]+.[0-9]+\\]/TIMESTAMP/"< "$LOG_FILE" |  # replace timestamps
   grep 'TIMESTAMP'                                                                       |  # Only give first lines
   grep -Eve ': [0-9]+\.?[0-9]*(e(-|\+))?[0-9]*ms$'                                                       |  # remove timings
   grep -v debug-log-file                                                                     # remove header line that contains generated log name
