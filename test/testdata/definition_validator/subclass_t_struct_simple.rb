e3706452d5e79f6296b8a2f3e3f4721ec7b76215
Jake Zimmerman <zimmerman.jake@gmail.com>
2023-09-15 21:07:50 -0700

    Add another test

--- test/testdata/definition_validator/subclass_t_struct_simple.rb
+++ test/testdata/definition_validator/subclass_t_struct_simple.rb
@@ -7,3 +7,6 @@ end
 class Child < Parent # error: Subclassing `Parent` is not allowed
 end
 
+# autocorrect not implemented
+Child2 = Class.new(Parent) do # error: Subclassing `Parent` is not allowed
+end
