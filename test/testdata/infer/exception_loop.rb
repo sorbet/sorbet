e67622d320b40522853020e5d839f541184fe1fe
Jake Zimmerman <zimmerman.jake@gmail.com>
2023-09-26 20:04:09 -0700

    Improve rescue variable pinning error

--- test/testdata/infer/exception_loop.rb
+++ test/testdata/infer/exception_loop.rb
@@ -11,17 +11,38 @@ def raises_b
   raise B.new
 end
 
-begin
-  raises_a
-rescue A => e
+def example1
+  begin
+    raises_a
+  rescue A => e
+  end
+
+  2.times do
+    begin
+      puts(e.class)
+      T.reveal_type(e) # error: `T.nilable(A)`
+      raises_b
+    rescue B => e
+      #         ^ error: Changing the type of a variable in a loop is not permitted
+    end
+  end
 end
 
-2.times do
+def example2
+  # works to pin this outside the loop
+  e = T.let(nil, T.nilable(T.any(A, B)))
+
   begin
-    puts(e.class)
-    T.reveal_type(e) # error: `T.nilable(A)`
-    raises_b
-  rescue B => e
-    #         ^ error: Changing the type of a variable in a loop is not permitted
+    raises_a
+  rescue A => e
+  end
+
+  2.times do
+    begin
+      puts(e.class)
+      T.reveal_type(e) # error: `T.nilable(A)`
+      raises_b
+    rescue B => e
+    end
   end
 end
