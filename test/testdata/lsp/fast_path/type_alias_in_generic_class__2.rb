31742a7419a77989f023507303679fc05eac1313
Jake Zimmerman <zimmerman.jake@gmail.com>
2023-09-30 16:58:15 -0700

    Fix some tests

--- test/testdata/lsp/fast_path/type_alias_in_generic_class__2.rb
+++ test/testdata/lsp/fast_path/type_alias_in_generic_class__2.rb
@@ -2,6 +2,12 @@
 # spacer for exclude-from-file-update
 
 class Box
+  extend T::Sig
   extend T::Generic
   Elem = type_member
+
+  sig { params(x: MyInteger).void }
+  def example(x)
+    T.reveal_type(x) # error: `Integer`
+  end
 end
