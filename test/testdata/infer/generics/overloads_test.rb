1cdf4028d8e039246bbec878e5493361ff4f1cc0
Jake Zimmerman <zimmerman.jake@gmail.com>
2023-10-05 15:54:36 -0700

    Show behavior before

--- test/testdata/infer/generics/overloads_test.rb
+++ test/testdata/infer/generics/overloads_test.rb
@@ -20,3 +20,34 @@ T.reveal_type(res) # error: `T.nilable(Integer)`
 
 res = example('')
 T.reveal_type(res) # error: `T.nilable(String)`
+
+sig do
+  params(
+    type: T::Class[Integer],
+    args: T.untyped,
+    blk: T.nilable(T.proc.params(arg0: Integer).void)
+  )
+  .void
+end
+sig do
+   params(
+      args: T.untyped,
+      blk: T.untyped
+    )
+    .void
+end
+def self.example2(type=T.unsafe(nil), *args, &blk)
+end
+
+example2(Integer, "--jobs=<n>") do |x|
+  T.reveal_type(x) # error: `Integer`
+end
+example2(Integer, "-j<n>", "--jobs=<n>") do |x|
+  # We pick the wrong overload, because our arity is weird in guessOverload
+  T.reveal_type(x) # error: `T.untyped`
+end
+example2("foo", /bar/) do |x|
+  #      ^^^^^ error: Expected `T::Class[Integer]` but found `String("foo")` for argument `type`
+  T.reveal_type(x) # => `Integer`
+end
+
