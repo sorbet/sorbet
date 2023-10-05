fa5f809c20a52f09b0c2151d2d251d0ffa2cfdcf
Ilia <bkmz2@yandex.ru>
2023-10-05 16:42:04 -0700

    Revert "Add --stress_incremental_resolver flag to the pipeline_test_runner"
    This reverts commit 60d57030f7d9808a67a7f46c0ad525881bc12bc6.

--- test/pipeline_test_runner.cc
+++ test/pipeline_test_runner.cc
@@ -60,7 +60,6 @@ namespace sorbet::test {
 using namespace std;
 
 string singleTest;
-bool shouldStressResolver;
 
 constexpr string_view whitelistedTypedNoneTest = "missing_typed_sigil.rb"sv;
 constexpr string_view packageFileName = "__package.rb"sv;
@@ -858,9 +857,7 @@ TEST_CASE("PerPhaseTest") { // NOLINT
 int main(int argc, char *argv[]) {
     cxxopts::Options options("test_corpus", "Test corpus for Sorbet typechecker");
     options.allow_unrecognised_options().add_options()("single_test", "run over single test.",
-                                                       cxxopts::value<std::string>()->default_value(""), "testpath")(
-        "stress_incremental_resolver", "Force incremental updates to discover resolver & namer bugs",
-        cxxopts::value<bool>()->implicit_value("true"));
+                                                       cxxopts::value<std::string>()->default_value(""), "testpath");
     auto res = options.parse(argc, argv);
 
     if (res.count("single_test") != 1) {
@@ -869,7 +866,7 @@ int main(int argc, char *argv[]) {
     }
 
     sorbet::test::singleTest = res["single_test"].as<std::string>();
-    sorbet::test::shouldStressResolver = res["stress_incremental_resolver"].as<bool>();
+
     doctest::Context context(argc, argv);
     return context.run();
 }
