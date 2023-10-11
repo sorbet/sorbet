1ecebbbf30ebef9382dbb13536713e5605aadd85
Ilia <bkmz2@yandex.ru>
2023-10-10 17:19:33 -0700

    Refactor a cycle trhough the symbol table

--- main/pipeline/definition_checker/DefinitionLinesDenylistEnforcer.cc
+++ main/pipeline/definition_checker/DefinitionLinesDenylistEnforcer.cc
@@ -2,49 +2,33 @@
 #include "ast/treemap/treemap.h"
 
 namespace sorbet::pipeline::definition_checker {
-namespace {
-void checkLoc(core::GlobalState &gs, const InlinedVector<core::Loc, 2> &locs, core::FileRef fref,
-              int prohibitedLinesStart, int prohibitedLinesEnd) {
-    for (auto loc : locs) {
-        if (loc.file() != fref) {
-            continue;
-        }
-        auto [detailStart, detailEnd] = loc.position(gs);
-        ENFORCE(!(detailStart.line >= prohibitedLinesStart && detailEnd.line <= prohibitedLinesEnd));
-    }
-}
-} // namespace
 
 void checkNoDefinitionsInsideProhibitedLines(core::GlobalState &gs, core::FileRef fref, int prohibitedLinesStart,
                                              int prohibitedLinesEnd) {
-    for (int i = 0; i < gs.classAndModulesUsed(); i++) {
-        auto classOrModule = core::ClassOrModuleRef(gs, i);
-        auto locs = classOrModule.data(gs)->locs();
-        checkLoc(gs, locs, fref, prohibitedLinesStart, prohibitedLinesEnd);
-    }
+    std::vector<std::pair<core::SymbolRef::Kind, uint32_t>> symbolTypes = {
+        {core::SymbolRef::Kind::ClassOrModule, gs.classAndModulesUsed()},
+        {core::SymbolRef::Kind::Method, gs.methodsUsed()},
+        {core::SymbolRef::Kind::FieldOrStaticField, gs.fieldsUsed()},
+        {core::SymbolRef::Kind::TypeArgument, gs.typeArgumentsUsed()},
+        {core::SymbolRef::Kind::TypeMember, gs.typeMembersUsed()},
+    };
 
-    for (int i = 0; i < gs.methodsUsed(); i++) {
-        auto method = core::MethodRef(gs, i);
-        auto locs = method.data(gs)->locs();
-        checkLoc(gs, locs, fref, prohibitedLinesStart, prohibitedLinesEnd);
-    }
+    for (auto [kind, used] : symbolTypes) {
+        for (uint32_t idx = 1; idx < used; idx++) {
+            auto sym = core::SymbolRef(gs, kind, idx);
+            if (!sym.exists()) {
+                continue;
+            }
 
-    for (int i = 0; i < gs.fieldsUsed(); i++) {
-        auto field = core::FieldRef(gs, i);
-        auto locs = field.data(gs)->locs();
-        checkLoc(gs, locs, fref, prohibitedLinesStart, prohibitedLinesEnd);
-    }
-
-    for (int i = 0; i < gs.typeArgumentsUsed(); i++) {
-        auto typeArgument = core::TypeArgumentRef(gs, i);
-        auto locs = typeArgument.data(gs)->locs();
-        checkLoc(gs, locs, fref, prohibitedLinesStart, prohibitedLinesEnd);
-    }
-
-    for (int i = 0; i < gs.typeMembersUsed(); i++) {
-        auto typeMember = core::TypeMemberRef(gs, i);
-        auto locs = typeMember.data(gs)->locs();
-        checkLoc(gs, locs, fref, prohibitedLinesStart, prohibitedLinesEnd);
+            auto locs = sym.locs(gs);
+            for (auto loc : locs) {
+                if (loc.file() != fref) {
+                    continue;
+                }
+                auto [detailStart, detailEnd] = loc.position(gs);
+                ENFORCE(!(detailStart.line >= prohibitedLinesStart && detailEnd.line <= prohibitedLinesEnd));
+            }
+        }
     }
 }
 } // namespace sorbet::pipeline::definition_checker
