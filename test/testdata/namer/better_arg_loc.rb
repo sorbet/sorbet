a5271577a08c9795700eef412d18d4ba2480b9e5
Jake Zimmerman <zimmerman.jake@gmail.com>
2023-10-18 16:30:06 -0700

    Add an error assertion

--- test/testdata/namer/better_arg_loc.rb
+++ test/testdata/namer/better_arg_loc.rb
@@ -3,7 +3,7 @@
 class ::Integer
   # This used to accidentally overwrite the loc of Integer#times's block
   # argument to have an empty Loc.
-  def times
+  def times # error: Refusing to typecheck `Integer#times` against an overloaded signature
   end
 end
 
