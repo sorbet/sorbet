0128105f051fe6ccd2d58ecc007b42d0933e4638
Jake Zimmerman <zimmerman.jake@gmail.com>
2023-10-20 05:37:05 -0700

    Show untyped blame in the editor if available (#7385)
    * Show untyped blame in the editor if available
    
    * wip
    
    * Also do this half
    
    * Actually, this is dead
    
    * Always do this
    
    * Fix tests

--- core/TypeErrorDiagnostics.cc
+++ core/TypeErrorDiagnostics.cc
@@ -122,15 +122,21 @@ void TypeErrorDiagnostics::insertTypeArguments(const GlobalState &gs, ErrorBuild
 void TypeErrorDiagnostics::explainUntyped(const GlobalState &gs, ErrorBuilder &e, ErrorClass what,
                                           const TypeAndOrigins &untyped, Loc originForUninitialized) {
     e.addErrorSection(untyped.explainGot(gs, originForUninitialized));
-    if (what == core::errors::Infer::UntypedValue) {
-        e.addErrorNote("Support for `{}` is minimal. Consider using `{}` instead.", "typed: strong", "typed: strict");
-    }
-}
 
-void TypeErrorDiagnostics::explainUntyped(const GlobalState &gs, ErrorBuilder &e, ErrorClass what, TypePtr untyped,
-                                          Loc origin, Loc originForUninitialized) {
-    auto untypedTpo = TypeAndOrigins{untyped, origin};
-    e.addErrorSection(untypedTpo.explainGot(gs, originForUninitialized));
+    if constexpr (sorbet::track_untyped_blame_mode || sorbet::debug_mode) {
+        auto blameSymbol = untyped.type.untypedBlame();
+        if (blameSymbol.exists()) {
+            auto loc = blameSymbol.loc(gs);
+            if (loc.exists()) {
+                e.addErrorLine(loc, "Blames to `{}`, defined here", blameSymbol.show(gs));
+            } else {
+                e.addErrorNote("Blames to `{}`", blameSymbol.show(gs));
+            }
+        } else {
+            e.addErrorNote("Blames to `{}`", "<none>");
+        }
+    }
+
     if (what == core::errors::Infer::UntypedValue) {
         e.addErrorNote("Support for `{}` is minimal. Consider using `{}` instead.", "typed: strong", "typed: strict");
     }
