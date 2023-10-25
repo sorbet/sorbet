6bf0f854fff97cd0d0bc42866f3e696f51daca95
Nathan Froyd <froydnj@gmail.com>
2023-10-25 16:47:21 -0400

    update autogen versions in tests and bump min depdb to v5 (#7421)

--- test/cli/print_to_file_v5/test.sh
+++ test/cli/print_to_file_v5/test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -eu
 
-main/sorbet --silence-dev-message --stop-after namer --autogen-version=2 \
+main/sorbet --silence-dev-message --stop-after namer --autogen-version=5 \
   -p autogen:autogen.txt -p autogen-msgpack:autogen.msgpack \
-  test/cli/print_to_file_v2/*.rb
+  test/cli/print_to_file_v5/*.rb
 
 echo "--- autogen.txt start ---"
 cat autogen.txt
