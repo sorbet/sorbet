32634c69bd004d1f2444d17daa5772545fc9a37d
Nathan Froyd <froydnj@gmail.com>
2023-10-25 15:47:50 -0400

    modify `autogen_print_rbi`

--- test/cli/autogen_print_rbi/test.sh
+++ test/cli/autogen_print_rbi/test.sh
@@ -1,24 +1,12 @@
 #!/bin/bash
 set -eu
 
-echo "Version 3:"
-main/sorbet --silence-dev-message --stop-after namer --autogen-version=3 \
-  -p autogen:autogen3.txt -p autogen-msgpack:autogen3.msgpack \
+echo "Version 5:"
+main/sorbet --silence-dev-message --stop-after namer --autogen-version=5 \
+  -p autogen:autogen5.txt -p autogen-msgpack:autogen5.msgpack --autogen-behavior-allowed-in-rbi-files-paths="test/cli/autogen_print_rbi/behavior_allowed.rbi" \
   test/cli/autogen_print_rbi/*.{rb,rbi}
 
-cat autogen3.txt
+cat autogen5.txt
 
 echo
-echo
-echo "Version 4:"
-main/sorbet --silence-dev-message --stop-after namer --autogen-version=4 \
-  -p autogen:autogen4.txt -p autogen-msgpack:autogen4.msgpack --autogen-behavior-allowed-in-rbi-files-paths="test/cli/autogen_print_rbi/behavior_allowed.rbi" \
-  test/cli/autogen_print_rbi/*.{rb,rbi}
-
-cat autogen4.txt
-
-echo
-shasum autogen3.msgpack autogen4.msgpack
-
-echo
-diff autogen3.txt autogen4.txt
+shasum autogen5.msgpack
