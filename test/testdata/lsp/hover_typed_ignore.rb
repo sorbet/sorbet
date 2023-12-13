965f6fa32aafea71aa6d6fe64259a338a7401645
Jake Zimmerman <zimmerman.jake@gmail.com>
2023-12-13 14:11:04 -0800

    Allow hover in `typed: false` files if possible (#7535)
    This matches the behavior of go to definition, which has worked that way
    since 99f0ffe9a559f75072dabe3c138ca64ebe8165f5.

--- test/testdata/lsp/hover_typed_ignore.rb
+++ test/testdata/lsp/hover_typed_ignore.rb
@@ -2,14 +2,14 @@
 
 class Foo
     # ^ hover: This file is `# typed: ignore`.
-    # ^ hover: Hover, Go To Definition, and other features are disabled in this file.
+    # ^ hover: No Sorbet IDE features will work in this file.
 
   def foo
     # ^ hover: This file is `# typed: ignore`.
-    # ^ hover: Hover, Go To Definition, and other features are disabled in this file.
+    # ^ hover: No Sorbet IDE features will work in this file.
     x = 10
   # ^ hover: This file is `# typed: ignore`.
-  # ^ hover: Hover, Go To Definition, and other features are disabled in this file.
+    # ^ hover: No Sorbet IDE features will work in this file.
     x
   end
 end
