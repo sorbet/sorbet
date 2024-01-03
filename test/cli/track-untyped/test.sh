18d4cfcf63107c68c726be7c259cab04551c66b5
Jake Zimmerman <zimmerman.jake@gmail.com>
2024-01-03 15:47:42 -0600

    Update the test

--- test/cli/track-untyped/test.sh
+++ test/cli/track-untyped/test.sh
@@ -2,4 +2,19 @@
 
 set -euo pipefail
 
-main/sorbet --censor-for-snapshot-tests --silence-dev-message --track-untyped --print=file-table-json --max-threads=0 test/cli/track-untyped 2>&1
+args=(
+  --censor-for-snapshot-tests
+  --silence-dev-message
+  --print=file-table-json
+  --max-threads=0
+  test/cli/track-untyped
+)
+
+echo --- implicit 'everywhere' ---
+main/sorbet "${args[@]}" --track-untyped 2>&1 || true
+
+echo --- explicit 'nowhere' ---
+main/sorbet "${args[@]}" --track-untyped=nowhere 2>&1 || true
+
+echo --- explicit 'everywhere' ---
+main/sorbet "${args[@]}" --track-untyped=everywhere 2>&1 || true
