fdc0ccb57cd7e91e6931eea03f34b508abf4c845
Neil Parikh <parikh.neil@me.com>
2024-01-03 17:17:52 -0700

    fix action naming in method.rb

--- test/testdata/lsp/code_actions/extract_variable/method.rb
+++ test/testdata/lsp/code_actions/extract_variable/method.rb
@@ -3,24 +3,24 @@
 # enable-experimental-lsp-extract-to-variable: true
 
 def b; 1 + 123; end
-#          ^^^ apply-code-action: [B] Extract Variable
+#          ^^^ apply-code-action: [A] Extract Variable
 
 def c; 1 + 1; 1 + 123; end
-#                 ^^^ apply-code-action: [C] Extract Variable
+#                 ^^^ apply-code-action: [B] Extract Variable
 
 def d
   1 + 123
-#     ^^^ apply-code-action: [D] Extract Variable
+#     ^^^ apply-code-action: [C] Extract Variable
 end
 
 def e
   1 + 1
   1 + 123
-#     ^^^ apply-code-action: [E] Extract Variable
+#     ^^^ apply-code-action: [D] Extract Variable
 end
 
 def f
   1 + 1
   1 + 2; 1 + 123
-#            ^^^ apply-code-action: [F] Extract Variable
+#            ^^^ apply-code-action: [E] Extract Variable
 end
