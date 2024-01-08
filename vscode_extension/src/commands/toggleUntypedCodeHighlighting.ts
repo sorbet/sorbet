454f05a8babb0d87dbd62c7c81c98fb965ae1a24
Jake Zimmerman <zimmerman.jake@gmail.com>
2024-01-08 11:59:57 -0800

    Convert back to boolean

--- vscode_extension/src/commands/toggleUntypedCodeHighlighting.ts
+++ vscode_extension/src/commands/toggleUntypedCodeHighlighting.ts
@@ -9,8 +9,25 @@ function toggle(log: Log, trackWhere: TrackUntyped): TrackUntyped {
     case "everywhere":
       return "nowhere";
     default:
-      log.warning(`Got unexpected state: ${trackWhere}`);
+      const exhaustiveCheck: never = trackWhere;
+      log.warning(`Got unexpected state: ${exhaustiveCheck}`);
       return "nowhere";
+  }
+}
+
+function backwardsCompatibleValue(
+  log: Log,
+  trackWhere: TrackUntyped,
+): boolean | TrackUntyped {
+  switch (trackWhere) {
+    case "nowhere":
+      return false;
+    case "everywhere":
+      return true;
+    default:
+      const exhaustiveCheck: never = trackWhere;
+      log.warning(`Got unexpected state: ${exhaustiveCheck}`);
+      return false;
   }
 }
 
@@ -22,23 +39,20 @@ function toggle(log: Log, trackWhere: TrackUntyped): TrackUntyped {
 export async function toggleUntypedCodeHighlighting(
   context: SorbetExtensionContext,
 ): Promise<TrackUntyped> {
-  const targetState = toggle(
-    context.log,
-    context.configuration.highlightUntyped,
-  );
-  await context.configuration.setHighlightUntyped(targetState);
-  context.log.info(`ToggleUntyped: Untyped code highlighting: ${targetState}`);
+  const toggled = toggle(context.log, context.configuration.highlightUntyped);
+  await context.configuration.setHighlightUntyped(toggled);
+  context.log.info(`ToggleUntyped: Untyped code highlighting: ${toggled}`);
 
   const { activeLanguageClient: client } = context.statusProvider;
   if (client) {
     client.sendNotification("workspace/didChangeConfiguration", {
       settings: {
-        highlightUntyped: targetState,
+        highlightUntyped: backwardsCompatibleValue(context.log, toggled),
       },
     });
   } else {
     context.log.debug("ToggleUntyped: No active Sorbet LSP to notify.");
   }
 
-  return targetState;
+  return toggled;
 }
