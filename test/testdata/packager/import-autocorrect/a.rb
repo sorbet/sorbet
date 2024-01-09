7dfafb41705ae16b26cc859004871df1480cde4c
Ilia <bkmz2@yandex.ru>
2024-01-08 17:48:49 -0800

    Add an LSP test, which asserts there is no duplicated import autocorrects
    The test calls a code action `Apply all Sorbet fixes for file` for a package autocorrects. The bug doesn't reproduce in CLI mode, because we allow only one edit per location in `core/AutocorrectSuggestion.cc`

---
+++ test/testdata/packager/import-autocorrect/a.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+# typed: strict
+# selective-apply-code-action: source.fixAll.sorbet
+
+class Package::A
+  extend T::Sig
+
+  sig {void}
+  def self.get_exported_item
+    Dep::ExportedItem.new
+  # ^^^               apply-code-action: [1] Apply all Sorbet fixes for file
+  # ^^^^^^^^^^^^^^^^^ error: `Dep::ExportedItem` resolves but its package is not imported (fix available)
+    Dep::ExportedItem.new
+  # ^^^               apply-code-action: [1] Apply all Sorbet fixes for file
+  # ^^^^^^^^^^^^^^^^^ error: `Dep::ExportedItem` resolves but its package is not imported (fix available)
+
+  end
+end
