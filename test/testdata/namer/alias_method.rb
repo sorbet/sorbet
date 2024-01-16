7f1be9d2110866fa020e969cd390bfe30a571c81
Patrick Linnane <patrick@linnane.io>
2024-01-16 12:20:58 -0800

    alias_method.rb: fix typos
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- test/testdata/namer/alias_method.rb
+++ test/testdata/namer/alias_method.rb
@@ -10,8 +10,8 @@ module Alias
   alias_method :f
   #            ^^ error: Not enough arguments provided for method `Module#alias_method`. Expected: `2`, got: `1`
   alias_method :f, 8 # error: Expected `Symbol` but found `Integer(8)` for argument `old_name`
-  alias_method :bad_alias, :nonexistant_method
-  #                        ^^^^^^^^^^^^^^^^^^^ error: Can't make method alias from `bad_alias` to non existing method `nonexistant_method`
+  alias_method :bad_alias, :nonexistent_method
+  #                        ^^^^^^^^^^^^^^^^^^^ error: Can't make method alias from `bad_alias` to non existing method `nonexistent_method`
 
   def alias_user
     f_alias_1
