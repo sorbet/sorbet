86bb7e5690fb42a2fae1a9da6e4a3afe5e408f16
Ilia <bkmz2@yandex.ru>
2024-01-16 11:22:13 -0800

    cd into test directory

--- test/cli/package-import-export-autocorrect/test.sh
+++ test/cli/package-import-export-autocorrect/test.sh
@@ -1,9 +1,11 @@
 tmp="$(mktemp -d)"
 cp -R test/cli/package-import-export-autocorrect/* "$tmp"
 
-main/sorbet --silence-dev-message --stripe-packages --max-threads=0 --autocorrect "$tmp" 2>&1
+cwd="$(pwd)"
+cd "$tmp" || exit 1
+
+"$cwd/main/sorbet" --silence-dev-message --stripe-packages --max-threads=0 --autocorrect . 2>&1
 
 echo "===================="
 
-cat "$tmp/b/__package.rb"
-rm -rf "$tmp"
+cat b/__package.rb
