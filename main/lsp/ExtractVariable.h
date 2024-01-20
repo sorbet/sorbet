456d705ba1335ca5a971f77247a7bc06a7eb2059
Neil Parikh <parikh.neil@me.com>
2024-01-19 16:57:24 -0800

    pre-work: store some of the intermediate values from extract variable (single)
    - enclosing class/method
    - the matching expressionPtr
    - skipped locs

--- main/lsp/ExtractVariable.h
+++ main/lsp/ExtractVariable.h
@@ -11,11 +11,15 @@ class VariableExtractor {
     const LSPTypecheckerDelegate &typechecker;
     const LSPConfiguration &config;
     const core::Loc selectionLoc;
+    ast::ExpressionPtr matchingNode;
+    ast::ExpressionPtr enclosingClassOrMethod;
+    std::vector<core::LocOffsets> skippedLocs;
 
 public:
     VariableExtractor(const LSPTypecheckerDelegate &typechecker, const LSPConfiguration &config,
                       const core::Loc selectionLoc)
-        : typechecker(typechecker), config(config), selectionLoc(selectionLoc) {}
+        : typechecker(typechecker), config(config), selectionLoc(selectionLoc), matchingNode(),
+          enclosingClassOrMethod(nullptr) {}
     std::vector<std::unique_ptr<TextDocumentEdit>> getExtractSingleOccurrenceEdits();
 };
 
