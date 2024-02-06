8ee972cd1400efe4ed306f1c705a8e2aa130b965
Jake Zimmerman <zimmerman.jake@gmail.com>
2024-02-05 16:39:20 -0800

    Create the folder if cache-dir doesn't exist (#7637)
    * Create the folder if cache-dir doesn't exist
    
    * wip
    
    * This test is flaky, just delete it

--- test/cli/cache-dir-dne/test.sh
+++ test/cli/cache-dir-dne/test.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
+set -euo pipefail
+
+trap 'rm -rf nope2 nope3' EXIT
+
 main/sorbet --silence-dev-message --cache-dir=nope2 -e '0' 2>&1
+
+touch nope3
+if main/sorbet --silence-dev-message --cache-dir=nope3/subdir -e 0 2>&1; then
+  2>&1 echo 'Expected to fail!'
+  exit 1
+fi
