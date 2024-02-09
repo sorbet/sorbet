3e5431ef81ec66af2a5949eb39ae4b00c55e7130
Jake Zimmerman <zimmerman.jake@gmail.com>
2024-02-08 23:03:41 -0800

    Add a test for repeated option

--- test/cli/suppress-payload-superclass-redefinition-for/test.sh
+++ test/cli/suppress-payload-superclass-redefinition-for/test.sh
@@ -15,9 +15,16 @@ echo "-- complex.rbi --"
   --suppress-payload-superclass-redefinition-for=IRB::RelineInputMethod irb.rbi
 
 echo "-- expect constant resolution error --"
-if ! 2>&1 "$cwd/main/sorbet" --silence-dev-message -e '' \
+if 2>&1 "$cwd/main/sorbet" --silence-dev-message -e '' \
   --suppress-payload-superclass-redefinition-for=DoesNotExist; then
   echo "Expected to fail!"
   exit 1
 fi
+
+
+echo "-- double.rb --"
+2>&1 "$cwd/main/sorbet" --silence-dev-message \
+  --suppress-payload-superclass-redefinition-for=Complex \
+  --suppress-payload-superclass-redefinition-for=IRB::RelineInputMethod \
+  double.rb
 
