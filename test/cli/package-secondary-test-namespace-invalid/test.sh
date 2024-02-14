e84a664abbe976355b771ca7612facbbfc0dade7
Jake Zimmerman <zimmerman.jake@gmail.com>
2024-02-13 18:03:37 -0800

    Delete useless test

--- test/cli/package-secondary-test-namespace-invalid/test.sh
+++
@@ -1,5 +0,0 @@
-cd test/cli/package-secondary-test-namespace-invalid || exit 1
-
-../../../main/sorbet --silence-dev-message --stripe-packages --secondary-test-package-namespaces=Critic:: --stripe-mode . 2>&1
-../../../main/sorbet --silence-dev-message --stripe-packages --secondary-test-package-namespaces=critic --stripe-mode . 2>&1
-../../../main/sorbet --silence-dev-message --stripe-packages --secondary-test-package-namespaces=^ --stripe-mode . 2>&1
