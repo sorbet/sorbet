144de58fff25f4c00035b4e560c178f71c8e2838
Jake Zimmerman <zimmerman.jake@gmail.com>
2024-02-15 16:43:53 -0800

    Acceptable test behavior changes
    This change makes it so that a global constant (`class ::A; end`)
    defined inside a package isn't treated as owned by that package (nor any
    package).
    
    It also makes it so that a package defined in the wrong namespace is
    defined in the package whose namespace it would actually belong to (e.g.
    `A::X = 1` belongs to `A` even if this definition happens in package
    `A::B`).
    
    And finally, this changes the location of some symbols in error
    messages, but not in meaningful ways.

--- test/testdata/packager/package-prefix-enforcement/nested/nested.test.rb
+++ test/testdata/packager/package-prefix-enforcement/nested/nested.test.rb
@@ -3,6 +3,7 @@
 module Test::Root
   BAD = 1
 # ^^^ error: Tests in the `Root::Nested` package must define tests in the `Test::Root::Nested` namespace
+# ^^^ error: `Test::Root::BAD` resolves but is not exported from `Root`
   #
   module Nested
     class Ok; end
