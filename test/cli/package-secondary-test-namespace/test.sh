fb09bed285d9bb073a4ab129e51d7fcbc8d8c548
Jake Zimmerman <zimmerman.jake@gmail.com>
2024-02-16 11:21:40 -0800

    Remove `--secondary-test-package-namespaces` option (#7708)
    * Remove `--secondary-test-package-namespaces` option
    
    Follow up to #7696
    
    * Delete test/cli/package-prefix-enforcement/test/critic/__package.rb
    
    * Delete test/cli/package-prefix-enforcement/test/critic/real.test.rb

--- test/cli/package-secondary-test-namespace/test.sh
+++ test/cli/package-secondary-test-namespace/test.sh
@@ -1,4 +1,4 @@
 cd test/cli/package-secondary-test-namespace || exit 0
 
-../../../main/sorbet --silence-dev-message --stripe-packages --secondary-test-package-namespaces=Critic,Minitest --stripe-mode . 2>&1
+../../../main/sorbet --silence-dev-message --stripe-packages --stripe-mode . 2>&1
 
--- test/cli/package-test-simple/test.sh
+++ test/cli/package-test-simple/test.sh
@@ -1,3 +1,3 @@
 cd test/cli/package-test-simple || exit 1
 
-../../../main/sorbet --silence-dev-message --stripe-packages --max-threads=0 --secondary-test-package-namespaces=Critic --stripe-mode --stripe-packages-hint-message="RUN SCRIPT HINT" . 2>&1
+../../../main/sorbet --silence-dev-message --stripe-packages --max-threads=0 --stripe-mode --stripe-packages-hint-message="RUN SCRIPT HINT" . 2>&1
