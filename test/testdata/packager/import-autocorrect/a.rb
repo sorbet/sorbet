856ac0fe5c991f33dcaa431f9430ede527d8b2c5
Ilia <bkmz2@yandex.ru>
2024-02-22 19:06:54 -0800

    Update LSP test

--- test/testdata/packager/import-autocorrect/a.rb
+++ test/testdata/packager/import-autocorrect/a.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 # typed: strict
-# selective-apply-code-action: source.fixAll.sorbet
 
 class Package::A
   extend T::Sig
@@ -8,11 +7,9 @@ class Package::A
   sig {void}
   def self.get_exported_item
     Dep::ExportedItem.new
-  # ^^^               apply-code-action: [1] Apply all Sorbet fixes for file
-  # ^^^^^^^^^^^^^^^^^ error: `Dep::ExportedItem` resolves but its package is not imported (fix available)
+  # ^^^^^^^^^^^^^^^^^ error: `Dep::ExportedItem` resolves but its package is not imported
     Dep::ExportedItem.new
-  # ^^^               apply-code-action: [1] Apply all Sorbet fixes for file
-  # ^^^^^^^^^^^^^^^^^ error: `Dep::ExportedItem` resolves but its package is not imported (fix available)
+  # ^^^^^^^^^^^^^^^^^ error: `Dep::ExportedItem` resolves but its package is not imported
 
   end
 end
