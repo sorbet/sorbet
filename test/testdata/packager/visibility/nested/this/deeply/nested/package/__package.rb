5f68bf1a391acd202bea53bbf841d1226d966e3f
Getty Ritter <tristero@infinitenegativeutility.com>
2024-03-08 00:21:58 -0800

    add a deeper nesting too

---
+++ test/testdata/packager/visibility/nested/this/deeply/nested/package/__package.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+# typed: strict
+# enable-packager: true
+# allow-relaxed-packager-checks-for: SkipCheck::For
+
+class Nested::This::Deeply::Nested::Package < PackageSpec
+  import Foo
+end
