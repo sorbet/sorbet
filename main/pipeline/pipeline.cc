26591f256ad453c901626adf08b74cd1ef043078
Jake Zimmerman <zimmerman.jake@gmail.com>
2024-03-12 15:01:15 -0700

    Cache files before pipeline::package (#7759)
    The call to `pipeline::package` will do some rewrites that only apply if
    the `--stripe-packages` option was passed.
    
    What this would mean is that the later call to
    `maybeCacheGlobalStateAndFiles` would accidentally add ASTs produced by
    phases later than indexer to the cache, which could then sometimes get
    read back out in contexts where that flag hadn't been passed.

--- main/pipeline/pipeline.cc
+++ main/pipeline/pipeline.cc
@@ -1230,7 +1230,7 @@ void printFileTable(unique_ptr<core::GlobalState> &gs, const options::Options &o
 #endif
 }
 
-bool cacheTreesAndFiles(const core::GlobalState &gs, WorkerPool &workers, const vector<ast::ParsedFile> &parsedFiles,
+bool cacheTreesAndFiles(const core::GlobalState &gs, WorkerPool &workers, absl::Span<const ast::ParsedFile> parsedFiles,
                         const unique_ptr<OwnedKeyValueStore> &kvstore) {
     if (kvstore == nullptr) {
         return false;
