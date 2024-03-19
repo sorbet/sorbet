80c6785783dbc514c9c268a21c59ba82c557fddb
Jake Zimmerman <zimmerman.jake@gmail.com>
2024-03-19 14:49:23 -0700

    Forcibly segfault for uncaught exception
    This means that uncaught exceptions will look like segfaults. Stripe's
    crash reporting mechanisms already look for segfaults, so this saves
    some work having to integrate a different way to report uncaught
    top-level exceptions.

--- main/main.cc
+++ main/main.cc
@@ -1,6 +1,7 @@
 #include "common/common.h"
 #include "main/options/options.h"
 #include "main/realmain.h"
+#include <signal.h>
 int main(int argc, char *argv[]) {
     try {
         return sorbet::realmain::realmain(argc, argv);
@@ -8,6 +9,9 @@ int main(int argc, char *argv[]) {
         return c.returnCode;
     } catch (sorbet::SorbetException &e) {
         fprintf(stderr, "caught %s: %s\n", typeid(e).name(), e.what());
-        return 1;
+
+        // Forcibly exit with a segfault signal for uncaught exceptions, which makes it easier to
+        // use `catchsegv.sh` to report these errors.
+        kill(0, SIGSEGV);
     }
 };
