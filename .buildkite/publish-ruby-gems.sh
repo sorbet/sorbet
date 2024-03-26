37edcada39aa90e1b565671657ac9fbd9c9079d2
Jake Zimmerman <zimmerman.jake@gmail.com>
2024-03-26 10:47:32 -0700

    Attempt to debug rubygem publish failures (#7782)

--- .buildkite/publish-ruby-gems.sh
+++ .buildkite/publish-ruby-gems.sh
@@ -26,6 +26,19 @@ source .buildkite/tools/with_backoff.sh
 
 rbenv install --skip-existing
 
+echo "--- Patching rubygems/package.rb"
+# There's a bug in rubygems/package.rb where if there's a GZip reader error, in
+# the process of reporting a good error saying what failed, there's a
+# NoMethodError.
+# https://github.com/rubygems/rubygems/pull/7539
+package_rb="$HOME/.rbenv/versions/3.1.2/lib/ruby/3.1.0/rubygems/package.rb"
+if [ -f "$package_rb" ]; then
+  echo "Using sed to patch rubygems/package.rb"
+  sed -i 's/@path = source.path/@path = source.is_a?(String) ? source : source.path/' "$package_rb"
+else
+  echo "Could not find rubygems/package.rb, skipping"
+fi
+
 publish_sorbet_static_gem() {
   gem_archive=$1
   platform=$2
@@ -69,7 +82,16 @@ publish_gem() {
   fi
 
   # This is last so the exit code is used as the status code for with_backoff
+  # TODO(jez) All this extra garbage is attempting to debug the root cause of https://github.com/rubygems/rubygems/pull/7539
+  set +e
   gem push --verbose "$gem_archive"
+  exit_code="$?"
+  set -e
+  if [ "$exit_code" -ne 0 ]; then
+    mkdir -p _out_/corrupt
+    cp "$gem_archive" _out_/corrupt
+  fi
+  return "$exit_code"
 }
 
 with_backoff publish_gem "sorbet-runtime"
