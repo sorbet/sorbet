81d9842bd5fb08cf9f6ea6b1c0856130f754ee76
Jake Zimmerman <zimmerman.jake@gmail.com>
2024-04-02 15:19:19 -0700

    Fix one more test

--- test/testdata/lsp/fast_path/method_change_kw_arg_name.rb
+++ test/testdata/lsp/fast_path/method_change_kw_arg_name.rb
@@ -10,5 +10,6 @@ end
 A.new.bar
 #        ^ error: Missing required keyword argument `x` for method `A#bar`
 A.new.bar(x: 0)
-A.new.bar(y: 0) # error: Unrecognized keyword argument `y` passed for method `A#bar`
+A.new.bar(y: 0)
+#         ^^^^ error: Unrecognized keyword argument `y` passed for method `A#bar`
 #         ^^^^ error: Missing required keyword argument `x` for method `A#bar`
