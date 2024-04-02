66c07e8e10603d2fdb16698fe673dbc5f575313c
Jake Zimmerman <zimmerman.jake@gmail.com>
2024-04-02 15:09:57 -0700

    Add some tests

--- test/cli/detailed-errors/type_members.rb
+++ test/cli/detailed-errors/type_members.rb
@@ -20,3 +20,18 @@ end
 
 T.let(A[String, Lower, Upper].new, A[Middle, Middle, Middle])
 
+class Box1
+  extend T::Generic
+  Elem = type_member
+end
+
+class Box2
+  extend T::Generic
+  Elem = type_member
+end
+
+sig { params(box1: Box1[Integer]).void }
+def takes_box1(box1)
+  T.let(box1, Box2[Integer])
+  T.let(box1, Integer)
+end
