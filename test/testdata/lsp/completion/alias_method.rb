6a21de9e0c8fc3370c2c69a68416ed1e9cd9a304
Jake Zimmerman <zimmerman.jake@gmail.com>
2024-04-12 11:00:51 -0700

    Add requested test

--- test/testdata/lsp/completion/alias_method.rb
+++ test/testdata/lsp/completion/alias_method.rb
@@ -9,3 +9,7 @@ end
 A.new.from_thi # error: does not exist
 #             ^ completion: from_this
 #             ^ apply-completion: [A] item: 0
+
+A.new.to_thi # error: does not exist
+#           ^ completion: to_this
+#           ^ apply-completion: [B] item: 0
