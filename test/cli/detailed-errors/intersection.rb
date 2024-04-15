2decaadfe49204e15bb37d308ba2e553127489cf
Neil Parikh <parikh.neil@me.com>
2024-04-15 15:41:54 -0700

    more test cases

--- test/cli/detailed-errors/intersection.rb
+++ test/cli/detailed-errors/intersection.rb
@@ -8,6 +8,12 @@ end
 module M2
 end
 
+module M3
+end
+
+module M4
+end
+
 class C
 end
 
@@ -19,10 +25,28 @@ class C2
   include M2
 end
 
-sig { params(x: T.all(M1, M2)).void }
-def takes_intersection(x)
+class C124
+  include M1
+  include M2
+  include M4
 end
 
-takes_intersection(C.new)
-takes_intersection(C1.new)
-takes_intersection(C2.new)
+class C123
+  include M1
+  include M2
+  include M3
+end
+
+class C134
+  include M1
+  include M3
+  include M4
+end
+
+T.let(C.new, T.all(M1, M2))
+T.let(C1.new, T.all(M1, M2))
+T.let(C2.new, T.all(M1, M2))
+T.let(C1.new, T.all(M1, M2, M3, M4))
+T.let(C123.new, T.all(M1, M2, M3, M4))
+T.let(C124.new, T.all(M1, M2, M3, M4))
+T.let(C134.new, T.all(M1, M2, M3, M4))
