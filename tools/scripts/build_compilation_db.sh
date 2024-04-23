26b17c4de1c4fb5a8d09b461e3fb6d32009220a5
Jake Zimmerman <zimmerman.jake@gmail.com>
2024-04-23 09:59:04 -0500

    build_compilation_db.sh

--- tools/scripts/build_compilation_db.sh
+++ tools/scripts/build_compilation_db.sh
@@ -4,12 +4,6 @@ set -e
 
 cd "$(dirname "$0")/../.."
 
-if [ "$(uname -s)-$(uname -m)" = "Darwin-arm64" ]; then
-  >&2 echo "Note: building the full compile_commands.json file is not yet supported on Apple Silicon machines."
-  >&2 echo "For rudimentary support, comment out all //compiler: targets in tools/BUILD and re-run this script."
-  >&2 echo
-fi
-
 ./bazel build //tools:compdb --config=dbg
 
 if command -v jq &> /dev/null; then
