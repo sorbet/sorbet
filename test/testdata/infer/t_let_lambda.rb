f61a32e484c678ae359639738f61d9ea2451cd39
Jake Zimmerman <zimmerman.jake@gmail.com>
2024-04-26 13:26:54 -0500

    Add a test

--- test/testdata/infer/t_let_lambda.rb
+++ test/testdata/infer/t_let_lambda.rb
@@ -8,6 +8,15 @@ f = T.let(
 )
 T.reveal_type(f)
 
+f = T.let(
+  -> (x) do
+    T.reveal_type(x)
+    x.to_s # error: Expected `Integer` but found `String` for block result type
+  end,
+  T.proc.params(x: Integer).returns(Integer)
+)
+T.reveal_type(f)
+
 # Should we error if this is not a proc type?
 # (What would have happened if you put `blk: Integer` in a sig somewhere?)
 f = T.let(
