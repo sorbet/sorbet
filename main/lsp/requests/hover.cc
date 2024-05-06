sorbet/sorbet

commit 9c90faa74c5702af44b4bc02b3e6a2c604307b86

Rework hover/def/ref w.r.t. constant aliases (#7800)
* Make Go to Definition on constant alias go to the alias definition

... not to the definition of the thing it aliases to.

* Do the same for references and document highlight

This is required to get our tests to pass because our test harness
requires some symmetry between definition and usages. But I think the
change to references is the user preference as well.

* Stop using ConstantResponse::symbol in hover

We'll show the documentation for both the alias and the thing it aliases
to, there might be useful documentation on one or both of those.

* Remove ConstantResponse::symbol

Turns out that my comment here is wrong, you probably do want this, and
if you don't, you can get it by manually dealiasing.

* Fix some hover messages

Want to avoid `<StubModule>` showing up in the hover text.

* Drop the CheckSize by 8 bytes

main/lsp/requests/hover.cc
@@ -83,7 +83,12 @@ unique_ptr<ResponseMessage> HoverTask::runRequest(LSPTypecheckerDelegate &typech
             }
         }
     } else if (auto c = resp->isConstant()) {
-        for (auto loc : c->symbol.locs(gs)) {
+        for (auto loc : c->symbolBeforeDealias.locs(gs)) {
+            if (loc.exists()) {
+                documentationLocations.emplace_back(loc);
+            }
+        }
+        for (auto loc : c->symbolBeforeDealias.dealias(gs).locs(gs)) {
             if (loc.exists()) {
                 documentationLocations.emplace_back(loc);
             }
@@ -121,7 +126,7 @@ unique_ptr<ResponseMessage> HoverTask::runRequest(LSPTypecheckerDelegate &typech
         typeString = core::source_generator::prettyTypeForMethod(gs, defResp->symbol, nullptr, defResp->retType.type,
                                                                  nullptr, options);
     } else if (auto constResp = resp->isConstant()) {
-        typeString = prettyTypeForConstant(gs, constResp->symbol);
+        typeString = prettyTypeForConstant(gs, constResp->symbolBeforeDealias);
     } else {
         core::TypePtr retType = resp->getRetType();
         // Some untyped arguments have null types.

core/lsp/QueryResponse.h
@@ -67,18 +67,10 @@ CheckSize(LiteralResponse, 48, 8);
 class ConstantResponse final {
 public:
     using Scopes = InlinedVector<core::SymbolRef, 1>;
-    ConstantResponse(core::SymbolRef symbol, core::SymbolRef symbolBeforeDealias, core::Loc termLoc, Scopes scopes,
-                     core::NameRef name, core::TypeAndOrigins retType, core::MethodRef enclosingMethod)
-        : symbol(symbol), symbolBeforeDealias(symbolBeforeDealias), termLoc(termLoc), scopes(scopes), name(name),
+    ConstantResponse(core::SymbolRef symbolBeforeDealias, core::Loc termLoc, Scopes scopes, core::NameRef name,
+                     core::TypeAndOrigins retType, core::MethodRef enclosingMethod)
+        : symbolBeforeDealias(symbolBeforeDealias), termLoc(termLoc), scopes(scopes), name(name),
           enclosingMethod(enclosingMethod), retType(std::move(retType)) {}
-    const core::SymbolRef symbol;
-    // You probably don't want this. Almost all of Sorbet's type system operates on dealiased
-    // symbols transparently (e.g., for a constant like `X = Integer`, Sorbet reports that `''` is
-    // not an `Integer`, not that `''` is not an `X`).
-    //
-    // But for some interactive features, it's important to respond using names the user typed.
-    // If you're thinking about using this, probably just use `symbol` instead, and if you still
-    // think you need this, double check with a Sorbet contributor.
     const core::SymbolRef symbolBeforeDealias;
     const core::Loc termLoc;
     const Scopes scopes;
@@ -86,7 +78,7 @@ public:
     const core::MethodRef enclosingMethod;
     const core::TypeAndOrigins retType;
 };
-CheckSize(ConstantResponse, 88, 8);
+CheckSize(ConstantResponse, 80, 8);
 
 class FieldResponse final {
 public:

main/lsp/DefLocSaver.cc
@@ -126,8 +126,8 @@ void matchesQuery(core::Context ctx, ast::ConstantLit *lit, const core::lsp::Que
             }
 
             auto enclosingMethod = enclosingMethodFromContext(ctx);
-            auto resp = core::lsp::ConstantResponse(symbol, symbolBeforeDealias, ctx.locAt(lit->loc), scopes,
-                                                    unresolved.cnst, tp, enclosingMethod);
+            auto resp = core::lsp::ConstantResponse(symbolBeforeDealias, ctx.locAt(lit->loc), scopes, unresolved.cnst,
+                                                    tp, enclosingMethod);
             core::lsp::QueryResponse::pushQueryResponse(ctx, resp);
         }
         lit = ast::cast_tree<ast::ConstantLit>(unresolved.scope);

main/lsp/lsp_helpers.cc
@@ -64,12 +64,15 @@ unique_ptr<MarkupContent> formatRubyMarkup(MarkupKind markupKind, string_view ru
 }
 
 string prettyTypeForConstant(const core::GlobalState &gs, core::SymbolRef constant) {
-    // Request that the constant already be dealiased, rather than dealias here to avoid defensively dealiasing.
-    // We should understand where dealias calls go.
-    ENFORCE(constant == constant.dealias(gs));
-
     if (constant == core::Symbols::StubModule()) {
-        return "This constant is not defined";
+        return "(unable to resolve constant)";
+    }
+
+    if (constant.isClassAlias(gs)) {
+        auto dealiased = constant.dealias(gs);
+        auto dealiasedShow =
+            dealiased == core::Symbols::StubModule() ? "(unable to resolve constant)" : dealiased.show(gs);
+        return fmt::format("{} = {}", constant.name(gs).show(gs), dealiasedShow);
     }
 
     core::TypePtr result;

main/lsp/requests/definition.cc
@@ -71,7 +71,7 @@ unique_ptr<ResponseMessage> DefinitionTask::runRequest(LSPTypecheckerDelegate &t
 
         // Only support go-to-definition on constants and fields in untyped files.
         if (auto c = resp->isConstant()) {
-            auto sym = c->symbol;
+            auto sym = c->symbolBeforeDealias;
             vector<pair<core::Loc, unique_ptr<Location>>> locMapping;
             for (auto loc : sym.locs(gs)) {
                 locMapping.emplace_back(loc, config.loc2Location(gs, loc));

main/lsp/requests/document_highlight.cc
@@ -65,8 +65,8 @@ unique_ptr<ResponseMessage> DocumentHighlightTask::runRequest(LSPTypecheckerDele
 
         // If file is untyped, only supports find reference requests from constants and class definitions.
         if (auto constResp = resp->isConstant()) {
-            response->result =
-                getHighlights(typechecker, getReferencesToSymbolInFile(typechecker, fref, constResp->symbol));
+            response->result = getHighlights(
+                typechecker, getReferencesToSymbolInFile(typechecker, fref, constResp->symbolBeforeDealias));
         } else if (auto fieldResp = resp->isField()) {
             // This could be a `prop` or `attr_*`, which have multiple associated symbols.
             response->result = getHighlights(

main/lsp/requests/implementation.cc
@@ -100,7 +100,7 @@ unique_ptr<ResponseMessage> ImplementationTask::runRequest(LSPTypecheckerDelegat
         }
     } else if (auto constant = queryResponse->isConstant()) {
         // User called "Go to Implementation" from the abstract class reference
-        auto classSymbol = constant->symbol.asClassOrModuleRef();
+        auto classSymbol = constant->symbolBeforeDealias.dealias(gs).asClassOrModuleRef();
 
         if (!classSymbol.data(gs)->flags.isAbstract) {
             response->error = makeInvalidRequestError(classSymbol, gs);

main/lsp/requests/references.cc
@@ -120,7 +120,7 @@ unique_ptr<ResponseMessage> ReferencesTask::runRequest(LSPTypecheckerDelegate &t
                 //  Returns all global usages of Foo::A
 
                 auto packageName = gs.packageDB().getPackageNameForFile(fref);
-                auto symsToCheck = getSymsToCheckWithinPackage(gs, constResp->symbol, packageName);
+                auto symsToCheck = getSymsToCheckWithinPackage(gs, constResp->symbolBeforeDealias, packageName);
 
                 if (!symsToCheck.empty()) {
                     std::vector<std::unique_ptr<Location>> locations;
@@ -137,13 +137,13 @@ unique_ptr<ResponseMessage> ReferencesTask::runRequest(LSPTypecheckerDelegate &t
                 } else {
                     // Fall back to normal case when we are not querying for an external symbol, e.g. class Foo <
                     // PackageSpec declarations, or export statements.
-                    response->result =
-                        extractLocations(typechecker.state(), getReferencesToSymbol(typechecker, constResp->symbol));
+                    response->result = extractLocations(
+                        typechecker.state(), getReferencesToSymbol(typechecker, constResp->symbolBeforeDealias));
                 }
             } else {
                 // Normal handling for non-package files
-                response->result =
-                    extractLocations(typechecker.state(), getReferencesToSymbol(typechecker, constResp->symbol));
+                response->result = extractLocations(typechecker.state(),
+                                                    getReferencesToSymbol(typechecker, constResp->symbolBeforeDealias));
             }
         } else if (auto fieldResp = resp->isField()) {
             // This could be a `prop` or `attr_*`, which have multiple associated symbols.

commit 02a2340a8f9fa10f15dfb58a2d6b5d13f206085a

Don't dup documentationLocations when not aliased
The changes in 9c90faa74 accidentally made all documentation for
constants that were not constant aliases duplicated.

We should definitely include both the pre- and post-dealiased
documentation locations if the symbols are different, but if they're the
same, then it's just going to mean showing two copies of the
documentation.

main/lsp/requests/hover.cc
@@ -88,9 +88,12 @@ unique_ptr<ResponseMessage> HoverTask::runRequest(LSPTypecheckerDelegate &typech
                 documentationLocations.emplace_back(loc);
             }
         }
-        for (auto loc : c->symbolBeforeDealias.dealias(gs).locs(gs)) {
-            if (loc.exists()) {
-                documentationLocations.emplace_back(loc);
+        auto dealiased = c->symbolBeforeDealias.dealias(gs);
+        if (dealiased != c->symbolBeforeDealias) {
+            for (auto loc : dealiased.locs(gs)) {
+                if (loc.exists()) {
+                    documentationLocations.emplace_back(loc);
+                }
             }
         }
     } else if (auto d = resp->isMethodDef()) {