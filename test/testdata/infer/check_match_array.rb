52c21582dd6637b6d152a8f8f1b8617a167a826c
Jake Zimmerman <zimmerman.jake@gmail.com>
2024-06-24 16:28:11 -0700

    Show after, add more tests

--- test/testdata/infer/check_match_array.rb
+++ test/testdata/infer/check_match_array.rb
@@ -8,11 +8,21 @@ TYPES = T.let(
 def foo(x)
   case x
   when *[Integer, String]
-    T.reveal_type(x) # error: `T.untyped`
+    T.reveal_type(x) # error: `T.any(Integer, String)`
   end
 
   case x
   when *TYPES
-    T.reveal_type(x) # error: `T.untyped`
+    T.reveal_type(x) # error: `T.any(Integer, String)`
+  end
+
+  case x
+  when *[]
+    T.reveal_type(x) # error: This code is unreachable
+  end
+
+  case x
+  when *[Integer]
+    T.reveal_type(x) # error: `Integer`
   end
 end
