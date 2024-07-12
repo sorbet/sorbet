073eb7c2e0318cc34fce9fd9bdd174cab89b7db5
Jake Zimmerman <zimmerman.jake@gmail.com>
2024-07-12 16:13:19 -0700

    Make the test better

--- test/cli/warm-cache/test.sh
+++ test/cli/warm-cache/test.sh
@@ -12,36 +12,22 @@ echo "class A; end" > "$dir/a.rb"
 echo "class B; end" > "$dir/b.rb"
 echo "class C; end" > "$dir/c.rb"
 
-echo "====first run (cold cache)===="
-# Explicitly trying to put stdout into a file and stderr to stdout.
-# Not trying to send stdout and stderr to the same place
-# shellcheck disable=SC2069
-main/sorbet --silence-dev-message --cache-dir "$dir/cache" "$dir"/{a,b,c}.rb 2>&1 #"$dir/stderr.txt"
-# grep 'No errors! Great job.' "$dir/stderr.txt"
-# grep 'types.input' "$dir/stderr.txt"
+run_sorbet() {
+  if ! main/sorbet --silence-dev-message --counters --cache-dir "$dir/cache" "$dir"/{a,b,c}.rb 2> "$dir/stderr.txt"; then
+    cat "$dir/stderr.txt"
+    exit 1
+  fi
+  grep 'types.input.files\(.kvstore.miss\|.kvstore.hit\)\? :' "$dir/stderr.txt"
+}
 
+echo "====first run (cold cache)===="
+run_sorbet
 echo "====second run (warm cache)===="
-# Explicitly trying to put stdout into a file and stderr to stdout.
-# Not trying to send stdout and stderr to the same place
-# shellcheck disable=SC2069
-main/sorbet --silence-dev-message --cache-dir "$dir/cache" "$dir"/{a,b,c}.rb 2>&1 #"$dir/stderr.txt"
-# grep 'No errors! Great job.' "$dir/stderr.txt"
-# grep 'types.input' "$dir/stderr.txt"
+run_sorbet
 
 echo '# comment' >> "$dir/a.rb"
 
 echo "====first after change (one cache miss)===="
-# Explicitly trying to put stdout into a file and stderr to stdout.
-# Not trying to send stdout and stderr to the same place
-# shellcheck disable=SC2069
-main/sorbet --silence-dev-message --cache-dir "$dir/cache" "$dir"/{a,b,c}.rb 2>&1 #"$dir/stderr.txt"
-# grep 'No errors! Great job.' "$dir/stderr.txt"
-# grep 'types.input' "$dir/stderr.txt"
-
+run_sorbet
 echo "====second after change (warm cache)===="
-# Explicitly trying to put stdout into a file and stderr to stdout.
-# Not trying to send stdout and stderr to the same place
-# shellcheck disable=SC2069
-main/sorbet --silence-dev-message --cache-dir "$dir/cache" "$dir"/{a,b,c}.rb 2>&1 #"$dir/stderr.txt"
-# grep 'No errors! Great job.' "$dir/stderr.txt"
-# grep 'types.input' "$dir/stderr.txt"
+run_sorbet
