6f4c654381c20e9b5f16e53d121cebfb051c50d3
Jake Zimmerman <zimmerman.jake@gmail.com>
2024-07-19 16:47:09 -0700

    wip: Add a test without error assertions

---
+++ test/testdata/infer/generics/t_proc.rb
@@ -0,0 +1,28 @@
+# typed: strong
+extend T::Sig
+
+sig do
+  type_parameters(:Return)
+    .params(blk: T::Proc[T.type_parameter(:Return)])
+    .returns(T.type_parameter(:Return))
+end
+def example(&blk)
+  res = yield
+  T.reveal_type(res)
+  res
+end
+
+res = example do |x:|
+  0
+end
+T.reveal_type(res)
+
+res = T::Proc[Integer].new do |x:|
+  0
+end
+
+T.reveal_type(res)
+
+f = T.let(->() { 0 }, T.proc.returns(Integer))
+T.let(f, T::Proc[Integer])
+T.let(f, T::Proc[String])
