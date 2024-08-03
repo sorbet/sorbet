c45de4fc395b620d0b55d389ac05b3a85136eff6
Jake Zimmerman <zimmerman.jake@gmail.com>
2024-08-03 14:47:06 -0700

    These are not modules

--- test/testdata/infer/all_order.rb
+++ test/testdata/infer/all_order.rb
@@ -2,12 +2,12 @@
 
 extend T::Sig
 
-class MParent
+class Parent
   extend T::Generic
   Elem = type_member(:out)
 end
 
-class MChild < MParent
+class Child < Parent
   extend T::Generic
   Elem = type_member(:out)
 end
@@ -17,20 +17,20 @@ module Other2; end
 
 sig {
   params(
-    x1: T.all(Other2, MParent[Other1], MChild[T.anything]),
-    x2: T.all(MParent[Other1], Other2, MChild[T.anything]),
-    x3: T.all(MParent[Other1], MChild[T.anything], Other2),
+    x1: T.all(Other2, Parent[Other1], Child[T.anything]),
+    x2: T.all(Parent[Other1], Other2, Child[T.anything]),
+    x3: T.all(Parent[Other1], Child[T.anything], Other2),
   ).void
 }
 def example(x1, x2, x3)
-  T.reveal_type(x1) # error: `T.all(MChild[Other1], Other2)`
+  T.reveal_type(x1) # error: `T.all(Child[Other1], Other2)`
   takes_mchild_other1(x1)
-  T.reveal_type(x2) # error: `T.all(MChild[Other1], Other2)`
+  T.reveal_type(x2) # error: `T.all(Child[Other1], Other2)`
   takes_mchild_other1(x2)
-  T.reveal_type(x3) # error: `T.all(MChild[Other1], Other2)`
+  T.reveal_type(x3) # error: `T.all(Child[Other1], Other2)`
   takes_mchild_other1(x3)
 end
 
-sig { params(mchild_other1: MChild[Other1]).void }
+sig { params(mchild_other1: Child[Other1]).void }
 def takes_mchild_other1(mchild_other1)
 end
