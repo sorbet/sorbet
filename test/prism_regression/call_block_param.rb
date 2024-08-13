d851d77c3914d4ad28225802315f8710c4cfa496
Alexander Momchilov <alexander.momchilov@shopify.com>
2024-08-13 12:56:44 -0400

    Implement translation for simple explicit blocks

---
+++ test/prism_regression/call_block_param.rb
@@ -0,0 +1,16 @@
+# typed: true
+
+ foo {} # empty inline block
+#^^^ error: Method `foo` does not exist on `T.class_of(<root>)`
+
+ foo do # empty do-end block
+#^^^ error: Method `foo` does not exist on `T.class_of(<root>)`
+ end
+
+ foo { "inline block" }
+#^^^ error: Method `foo` does not exist on `T.class_of(<root>)`
+
+ foo do
+#^^^ error: Method `foo` does not exist on `T.class_of(<root>)`
+   "do-end block"
+ end
