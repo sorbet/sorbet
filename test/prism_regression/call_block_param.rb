bee6043df21139beb04147c166fc6627b49f3e54
Alexander Momchilov <alexander.momchilov@shopify.com>
2024-08-15 13:13:19 -0400

    Implement transation for blocks with args

--- test/prism_regression/call_block_param.rb
+++ test/prism_regression/call_block_param.rb
@@ -14,3 +14,11 @@
 #^^^ error: Method `foo` does not exist on `T.class_of(<root>)`
    "do-end block"
  end
+
+ foo { |positional, kwarg:, &block| "inline block with params" }
+#^^^ error: Method `foo` does not exist on `T.class_of(<root>)`
+
+ foo do |positional, kwarg:, &block|
+#^^^ error: Method `foo` does not exist on `T.class_of(<root>)`
+   "inline block with params"
+ end
