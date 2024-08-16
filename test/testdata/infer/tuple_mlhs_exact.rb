733f756afc07d66fc076566d390dde12cbedadeb
Jake Zimmerman <zimmerman.jake@gmail.com>
2024-08-16 10:05:39 -0700

    Show failing `|(x, y), _|` behavior

--- test/testdata/infer/tuple_mlhs_exact.rb
+++ test/testdata/infer/tuple_mlhs_exact.rb
@@ -17,3 +17,8 @@ x, y, z = returns_2_tuple # error: Attempted to unpack 2-tuple into 3 targets
 
 x, y = returns_3_tuple # error: Attempted to unpack 3-tuple into 2 targets
 x, y, = returns_3_tuple # error: Attempted to unpack 3-tuple into 2 targets
+
+xs = T::Array[[Integer, String, Symbol]].new
+xs.each do |(x, y)|
+  #         ^^^^^^ error: Attempted to unpack 3-tuple into 2 targets
+end
