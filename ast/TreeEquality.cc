fcd3f1d1b13f3f41c1f53a5c1326b96a6d756919
Neil Parikh <parikh.neil@me.com>
2024-08-21 15:03:41 -0700

    check if original is nullptr

--- ast/TreeEquality.cc
+++ ast/TreeEquality.cc
@@ -275,7 +275,16 @@ bool structurallyEqual(const void *avoid, const Tag tag, const void *tree, const
         case Tag::ConstantLit: {
             auto *a = reinterpret_cast<const ConstantLit *>(tree);
             auto *b = reinterpret_cast<const ConstantLit *>(other);
-            return a->symbol == b->symbol && structurallyEqual(avoid, a->original, b->original);
+            if (a->symbol != b->symbol) {
+                return false;
+            }
+            if (a->original && b->original) {
+                return structurallyEqual(avoid, a->original, b->original);
+            } else if (!a->original && !b->original) {
+                return true;
+            } else {
+                return false;
+            }
         }
 
         case Tag::ZSuperArgs: {
