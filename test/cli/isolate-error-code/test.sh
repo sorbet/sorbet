bd80c77af77cf446e5cb806ccb19fc0c3c1556ea
Jake Zimmerman <zimmerman.jake@gmail.com>
2024-09-16 11:53:25 -0700

    Organize all --help options into meaningful sections (#8151)
    * Organize all --help options into meaningful sections
    
    Makes it substantially easier to read the options.
    
    * Changes to CLI test that use short help
    
    * Fully remove error-{white,black}-list
    
    I accidentally dropped this in the refactor. I figure that rather than
    adding it back, now's the time to drop it entirely.
    
    * We don't have a double-dot in the metric names now
    
    * Full help output changes

--- test/cli/isolate-error-code/test.sh
+++ test/cli/isolate-error-code/test.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
 main/sorbet --silence-dev-message test/cli/isolate-error-code/isolate-error-code.rb --isolate-error-code=7017 2>&1
-
-# TODO(jvilk): Remove when we remove --error-white-list
-main/sorbet --silence-dev-message test/cli/isolate-error-code/isolate-error-code.rb --error-white-list=7017 2>&1
