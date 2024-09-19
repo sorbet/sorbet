905b168fb6b6a9bcb1336a32b47997faee16ff50
Jake Zimmerman <zimmerman.jake@gmail.com>
2024-09-18 17:02:55 -0700

    Remove one more reference

--- test/cli/package-multiple-files-behavior/test.sh
+++ test/cli/package-multiple-files-behavior/test.sh
@@ -1,2 +1,2 @@
-# Ensures that Sorbet doesn't complain about the package having behavior defined in multiple files in Stripe mode.
+# Ensures that Sorbet doesn't complain about the package having behavior defined in multiple files in "uniquely-defined-behavior" mode
 main/sorbet --silence-dev-message --uniquely-defined-behavior --stripe-packages --dir test/cli/package-multiple-files-behavior 2>&1
