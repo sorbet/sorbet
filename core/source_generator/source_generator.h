7631f378c8fa16048f6005c779dab568de7e0976
Jake Zimmerman <zimmerman.jake@gmail.com>
2024-09-30 18:13:51 -0700

    no-op: Formatting

--- core/source_generator/source_generator.h
+++ core/source_generator/source_generator.h
@@ -10,8 +10,7 @@ core::TypePtr getResultType(const core::GlobalState &gs, const core::TypePtr &ty
                             core::TypePtr receiver);
 
 std::string prettyTypeForMethod(const core::GlobalState &gs, core::MethodRef method, const core::TypePtr &receiver,
-                                const core::TypePtr &retType,
-                                const ShowOptions options);
+                                const core::TypePtr &retType, const ShowOptions options);
 
 } // namespace sorbet::core::source_generator
 #endif // SORBET_CORE_SOURCE_GENERATOR_H
