8a6d0a5b17b98e0aa51cd2f070ba2085ce739b6c
Jake Zimmerman <zimmerman.jake@gmail.com>
2024-10-01 10:33:18 -0700

    Update the tests that change

--- test/testdata/lsp/hover_ampersand_operations.rb
+++ test/testdata/lsp/hover_ampersand_operations.rb
@@ -12,10 +12,15 @@ def main
   dogs = T.let([], T::Array[Dog])
   # Check hover of breeds / map / : / breed
   breeds = dogs.map(&:breed)
-# ^ hover: T::Array[String]
-              # ^ hover:     blk: T.proc.params(arg0: Dog).returns(String)
-                   # ^ hover: sig { returns(String) }
-                    # ^ hover: sig { returns(String) }
+  # ^ hover: T::Array[String]
+  #             ^ hover-line: 2 sig do
+  #             ^ hover-line: 3   params(
+  #             ^ hover-line: 4     blk: T.proc.params(arg0: Dog).returns(T.type_parameter(:U))
+  #             ^ hover-line: 5   )
+  #             ^ hover-line: 6   .returns(T::Array[T.type_parameter(:U)])
+  #             ^ hover-line: 7 end
+  #                  ^ hover: sig { returns(String) }
+  #                   ^ hover: sig { returns(String) }
 
   # Safenav
   dog = Dog.new
