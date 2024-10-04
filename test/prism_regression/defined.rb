67b737f82d0af445175d1c9a0203ca900aec5ea6
Stan Lo <stan.lo@shopify.com>
2024-10-04 21:48:10 +0100

    Add more test cases for defined node

--- test/prism_regression/defined.rb
+++ test/prism_regression/defined.rb
@@ -1,2 +1,12 @@
 # typed: false
-defined?(Foo)
+local_var = 1
+
+defined?(Constant::Path)
+defined?(::FullyQualified::Constant::Path)
+defined?(method_or_local_var)
+defined?(local_var)
+defined?(@ivar)
+defined?(@@cvar)
+defined?($gvar)
+defined?(yield)
+defined?(super)
