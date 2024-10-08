11cadc74f2c361aa049ad6e8f9ef748e264e54ca
Neil Parikh <parikh.neil@me.com>
2024-10-08 10:57:55 -0700

    CLI test for the flag

---
+++ test/cli/packager-layer/test.sh
@@ -0,0 +1,14 @@
+cd test/cli/packager-layer || exit 1
+
+# No --packager-layer flag with --stripe-packages
+../../../main/sorbet --silence-dev-message --stripe-packages . 2>&1
+
+
+# No --packager-layer flag with no --stripe-packages
+../../../main/sorbet --silence-dev-message . 2>&1
+
+# --packager-layer flag with --stripe-packages
+../../../main/sorbet --silence-dev-message --stripe-packages --packager-layer=a,b . 2>&1
+
+# --packager-layer flag with no --stripe-packages
+../../../main/sorbet --silence-dev-message --packager-layer=a,b . 2>&1
