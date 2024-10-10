783ab99afe97afaf59c4937df2e69298b3a3bee4
Alexander Momchilov <alexander.momchilov@shopify.com>
2024-10-10 17:54:06 -0400

    `move` istead of `std::move`

--- parser/prism/Translator.cc
+++ parser/prism/Translator.cc
@@ -283,10 +283,10 @@ unique_ptr<parser::Node> Translator::translate(pm_node_t *node) {
             if (flags & PM_CALL_NODE_FLAGS_SAFE_NAVIGATION) {
                 // Handle conditional send, e.g. `self&.target1, self&.target2 = 1, 2`
                 // It's not valid Ruby, but the parser needs to support it for the diagnostics to work
-                return make_unique<parser::CSend>(location, std::move(receiver), gs.enterNameUTF8(name), messageLoc,
+                return make_unique<parser::CSend>(location, move(receiver), gs.enterNameUTF8(name), messageLoc,
                                                   NodeVec{});
             } else { // Regular send, e.g. `self.target1, self.target2 = 1, 2`
-                return make_unique<parser::Send>(location, std::move(receiver), gs.enterNameUTF8(name), messageLoc,
+                return make_unique<parser::Send>(location, move(receiver), gs.enterNameUTF8(name), messageLoc,
                                                  NodeVec{});
             }
         }
@@ -416,7 +416,7 @@ unique_ptr<parser::Node> Translator::translate(pm_node_t *node) {
 
             auto arg = translate(definedNode->value);
 
-            return make_unique<parser::Defined>(location.join(arg->loc), std::move(arg));
+            return make_unique<parser::Defined>(location.join(arg->loc), move(arg));
         }
         case PM_ELSE_NODE: { // An `else` clauses, which can pertain to an `if`, `begin`, `case`, etc.
             auto elseNode = reinterpret_cast<pm_else_node *>(node);
@@ -522,8 +522,8 @@ unique_ptr<parser::Node> Translator::translate(pm_node_t *node) {
             auto receiver = translate(indexedTargetNode->receiver);
             auto arguments = translateArguments(indexedTargetNode->arguments);
 
-            return make_unique<parser::Send>(location, std::move(receiver), core::Names::squareBracketsEq(),
-                                             lBracketLoc, std::move(arguments));
+            return make_unique<parser::Send>(location, move(receiver), core::Names::squareBracketsEq(), lBracketLoc,
+                                             move(arguments));
         }
         case PM_INSTANCE_VARIABLE_AND_WRITE_NODE: { // And-assignment to an instance variable, e.g. `@iv &&= false`
             return translateOpAssignment<pm_instance_variable_and_write_node, parser::AndAsgn, parser::IVarLhs>(node);
@@ -1103,7 +1103,7 @@ unique_ptr<parser::Node> Translator::patternTranslate(pm_node_t *node) {
             auto left = translate(alternationPatternNode->left);
             auto right = translate(alternationPatternNode->right);
 
-            return make_unique<parser::MatchAlt>(location, std::move(left), std::move(right));
+            return make_unique<parser::MatchAlt>(location, move(left), move(right));
         }
         case PM_ARRAY_PATTERN_NODE: { // An array pattern such as the `[head, *tail]` in the `a in [head, *tail]`
             auto arrayPatternNode = reinterpret_cast<pm_array_pattern_node *>(node);
