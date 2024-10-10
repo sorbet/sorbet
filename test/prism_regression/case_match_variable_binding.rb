77089dbfd8375331b6bb25496c1595443443ac92
Alexander Momchilov <alexander.momchilov@shopify.com>
2024-10-10 16:47:57 -0400

    Test Array patterns inside other patterns

--- test/prism_regression/case_match_variable_binding.rb
+++ test/prism_regression/case_match_variable_binding.rb
@@ -6,6 +6,11 @@ in [x]      # Variable binding nested in an Array pattern
 in { k: x } # Variable binding nested in a Hash pattern
   "A Hash-like whose key `:k` has value #{x}"
 
+in [[value], *tail] # Array pattern inside an Array pattern
+  "An array-like thing that starts with a one-element Array containing #{value}, and ends with #{tail}"
+in { k: [value] }   # Array pattern inside a Hash pattern
+  "A hash-like whose key `:k` has a one-element Array value containing #{value}"
+
 in x
   "Some other value: #{x}"
 end
