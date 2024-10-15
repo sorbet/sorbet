07b6bf0a4d5313d8e29b13eede6bd373f603883f
Jake Zimmerman <zimmerman.jake@gmail.com>
2024-10-15 14:13:55 -0700

    Needs to be inside a loop

--- test/testdata/infer/begin_rescue_uninitialized.rb
+++ test/testdata/infer/begin_rescue_uninitialized.rb
@@ -4,21 +4,21 @@ extend T::Sig
 sig { returns(String) }
 def returns_string = ""
 
-sig { returns(T.nilable(String)) }
-def returns_nilable_string = nil
-
-begin
-  x = returns_string
-rescue ArgumentError
+1.times do
   begin
     x = returns_string
   rescue ArgumentError
-    raise
+    begin
+      x = returns_string
+    rescue ArgumentError
+      raise
+    end
   end
+
+  T.reveal_type(x) # error: `String`
+
+  x&.chars || []
+  #^^ error: can never be nil
+  #           ^^ error: This code is unreachable
 end
 
-T.reveal_type(x) # error: `String`
-
-x&.chars || []
-#^^ error: can never be nil
-#           ^^ error: This code is unreachable
