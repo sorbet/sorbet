4832a0960437f76a7bd457ab656e0d8895feace3
Stan Lo <stan.lo@shopify.com>
2024-10-15 17:36:19 +0100

    Add comments to rescues' test fixture

--- test/prism_regression/rescue.rb
+++ test/prism_regression/rescue.rb
@@ -1,11 +1,13 @@
 # typed: false
 
+# Testing a simple rescue clause
 begin
   bar
 rescue
   "rescued"
 end
 
+# Testing a rescue clause with multiple body statements
 begin
   foo
   bar
@@ -13,18 +15,21 @@ rescue
   "rescued"
 end
 
+# Testing a rescue clause with a specific exception
 begin
   foo
 rescue RuntimeError
   "rescued Foo"
 end
 
+# Testing a rescue clause with multiple exceptions and a variable assignment
 begin
   foo
 rescue RuntimeError, NotImplementedError => e
   "rescued Foo #{e}"
 end
 
+# Testing a rescue clause with an else clause
 begin
   foo
 rescue RuntimeError
@@ -33,6 +38,7 @@ else
   "rescued else"
 end
 
+# Testing multiple rescue clauses with different exceptions
 begin
   foo
 rescue RuntimeError => e
@@ -43,6 +49,7 @@ rescue => e
   "rescued #{e}"
 end
 
+# Testing multiple rescue clauses with different exceptions and a final else clause
 begin
   foo
 rescue RuntimeError => e
@@ -55,6 +62,7 @@ else
   "rescued else"
 end
 
+# Testing rescue modifiers
 problematic_code rescue puts "rescued"
 problematic_code rescue nil
 problematic_code rescue raise rescue puts "rescued again"
