4dcd05eccb2abcc3e856bbc78934aa9c0ad47259
Trevor Elliott <awesomelyawesome@gmail.com>
2024-10-25 17:45:36 -0700

    Fix a crash with duplicate keyword arguments (#8259)
    * Mangle duplicate keyword args in the parser instead of the namer
    
    * Stop mangling duplicate keyword args in the namer
    
    * Update the parser's kwargs test to exercise duplicated kwargs
    
    * Update parser/Builder.cc
    
    Co-authored-by: Jake Zimmerman <zimmerman.jake@gmail.com>
    
    * Rename `checkDuplicateArg` to `hasDuplicateArg`
    
    * Add the test case from #7969
    
    ---------
    
    Co-authored-by: Jake Zimmerman <zimmerman.jake@gmail.com>

---
+++ test/testdata/namer/duplicate_field_error.rb
@@ -0,0 +1,17 @@
+# typed: strict
+
+# Discovered in https://github.com/sorbet/sorbet/issues/7969
+
+class A
+  extend T::Sig
+  sig { params(x: Integer, x: String).void }
+                         # ^ error: Hash key `x` is duplicated
+                         # ^ error: Unknown argument name `x`
+  def initialize(x:, x: nil)
+                   # ^^ error: Malformed `sig`
+                   # ^^^^^^ error: duplicate argument name x
+    @x = T.let(x, T.nilable(String))
+    @x = T.let(x, T.nilable(String))
+       # ^^^^^^^^^^^^^^^^^^^^^^^^^^^ error: Argument does not have asserted type
+  end
+end
