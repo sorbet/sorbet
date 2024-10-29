0c917188e244a085a56b5e249616247692580cb6
Trevor Elliott <awesomelyawesome@gmail.com>
2024-10-29 14:12:16 -0700

    Remove the hasParseError guards, and handle the error in the parser

--- infer/environment.cc
+++ infer/environment.cc
@@ -1205,8 +1205,7 @@ Environment::processBinding(core::Context ctx, const cfg::CFG &inWhat, cfg::Bind
                                                       ctx.owner.asMethodRef(), ctx.locAt(inWhat.loc)));
                 }
 
-                ENFORCE(ctx.file.data(ctx).hasIndexErrors() || !tp.origins.empty(),
-                        "Inferencer did not assign location");
+                ENFORCE(!tp.origins.empty(), "Inferencer did not assign location");
             },
             [&](cfg::Alias &a) {
                 core::SymbolRef symbol = a.what.dealias(ctx);
@@ -1703,7 +1702,7 @@ Environment::processBinding(core::Context ctx, const cfg::CFG &inWhat, cfg::Bind
             }
             tp.type = core::Types::untypedUntracked();
         }
-        ENFORCE(ctx.file.data(ctx).hasIndexErrors() || !tp.origins.empty(), "Inferencer did not assign location");
+        ENFORCE(!tp.origins.empty(), "Inferencer did not assign location");
 
         if (!noLoopChecking && loopCount != bindMinLoops) {
             auto pin = pinnedTypes.find(bind.bind.variable);
