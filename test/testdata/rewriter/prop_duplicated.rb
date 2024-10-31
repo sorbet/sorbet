e816f4e88e37bb319123bd1153224c4295fc4500
Trevor Elliott <awesomelyawesome@gmail.com>
2024-10-30 17:29:39 -0700

    Update the prop_duplicated test

--- test/testdata/rewriter/prop_duplicated.rb
+++ test/testdata/rewriter/prop_duplicated.rb
@@ -3,14 +3,29 @@
 class A < T::Struct
 
   const :name, String
+#        ^^^^         error: Argument does not have asserted type `Float`
   prop :name, Integer
-# ^^^^^^^^^^^^^^^^^^^ error: prop is defined multiple times
+# ^^^^^^^^^^^^^^^^^^^ error: Expected `Integer`
+# ^^^^^^^^^^^^^^^^^^^ error: Malformed `sig`
+# ^^^^^^^^^^^^^^^^^^^ error: Unknown argument
+# ^^^^^^^^^^^^^^^^^^^ error: duplicate argument name
+#       ^^^^          error: Argument does not have asserted type `Float`
+#       ^^^^          error: Expected `Float`
 
   prop :age, Integer
+# ^^^^^^^^^^^^^^^^^^ error: Expected `Integer` but found
+#       ^^^          error: Argument does not have asserted type `Float`
+#       ^^^          error: Expected `Float`
   const :name, Float
-# ^^^^^^^^^^^^^^^^^^ error: const is defined multiple times
+# ^^^^^^^^^^^^^^^^^^ error: Malformed `sig`
+# ^^^^^^^^^^^^^^^^^^ error: Unknown argument
+# ^^^^^^^^^^^^^^^^^^ error: duplicate argument name
+#        ^^^^        error: Argument does not have asserted type `Float`
 
   const :age, Float
-# ^^^^^^^^^^^^^^^^^ error: const is defined multiple times
+# ^^^^^^^^^^^^^^^^^ error: Malformed `sig`
+# ^^^^^^^^^^^^^^^^^ error: Unknown argument
+# ^^^^^^^^^^^^^^^^^ error: duplicate argument name
+#        ^^^        error: Argument does not have asserted type `Float`
 
 end
