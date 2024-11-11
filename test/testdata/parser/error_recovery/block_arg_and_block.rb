c8746eeeec58134a773c490708eabcce2c8fd0e7
Trevor Elliott <awesomelyawesome@gmail.com>
2024-11-11 10:08:34 -0800

    Update error assertions

--- test/testdata/parser/error_recovery/block_arg_and_block.rb
+++ test/testdata/parser/error_recovery/block_arg_and_block.rb
@@ -2,7 +2,7 @@
 
 [1,2,3].each do |x|
   foo(&bar) do puts(x) end
-#     ^^^^         error: both block argument and literal block are passed
-#      ^^^         error: Method `bar` does not exist
-# ^^^^^^^^^^^^^^^^ error: Method `foo` does not exist
+#     ^^^^                 error: both block argument and literal block are passed
+#      ^^^                 error: Method `bar` does not exist
+# ^^^^^^^^^^^^^^^^^^^^^^^^ error: Method `foo` does not exist
 end
