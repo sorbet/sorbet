ff89b36aa5938d28fceedfba47273180fdf61a1a
Ilia <bkmz2@yandex.ru>
2024-11-20 16:56:08 -0800

    Use new errors and update tests

--- core/types/calls.cc
+++ core/types/calls.cc
@@ -1748,7 +1748,8 @@ DispatchResult MetaType::dispatchCall(const GlobalState &gs, const DispatchArgs 
                 return badMetaTypeCall(gs, args, errLoc, this->wrapped);
             }
 
-            auto returnType = Types::applyTypeArguments(gs, args.locs, args.numPosArgs, args.args, applied->klass);
+            auto returnType = Types::applyTypeArguments(gs, args.locs, args.numPosArgs, args.args, applied->klass,
+                                                        /* inResolver */ false);
             return DispatchResult(returnType, args.selfType, Symbols::T_Generic_squareBrackets());
         }
         case Names::bind().rawId():
@@ -2238,7 +2239,8 @@ public:
             return;
         }
 
-        res.returnType = Types::applyTypeArguments(gs, args.locs, args.numPosArgs, args.args, attachedClass);
+        res.returnType =
+            Types::applyTypeArguments(gs, args.locs, args.numPosArgs, args.args, attachedClass, /* inResolver */ false);
     }
 } T_Generic_squareBrackets;
 
--- core/types/types.cc
+++ core/types/types.cc
@@ -1003,7 +1003,8 @@ TypePtr Types::unwrapType(const GlobalState &gs, Loc loc, const TypePtr &tp) {
 // again by infer).
 
 TypePtr Types::applyTypeArguments(const GlobalState &gs, const CallLocs &locs, uint16_t numPosArgs,
-                                  const InlinedVector<const TypeAndOrigins *, 2> &args, ClassOrModuleRef genericClass) {
+                                  const InlinedVector<const TypeAndOrigins *, 2> &args, ClassOrModuleRef genericClass,
+                                  bool inResolver) {
     genericClass = genericClass.maybeUnwrapBuiltinGenericForwarder();
 
     int arity;
@@ -1020,7 +1021,9 @@ TypePtr Types::applyTypeArguments(const GlobalState &gs, const CallLocs &locs, u
         auto end = locs.args.back().endPos();
         core::Loc kwargsLoc{locs.file, begin, end};
 
-        if (auto e = gs.beginError(kwargsLoc, errors::Infer::GenericArgumentKeywordArgs)) {
+        auto errorClass =
+            inResolver ? errors::Resolver::GenericArgumentKeywordArgs : errors::Infer::GenericArgumentKeywordArgs;
+        if (auto e = gs.beginError(kwargsLoc, errorClass)) {
             e.setHeader("Keyword arguments given to `{}`", genericClass.show(gs));
             // offer an autocorrect to turn the keyword args into a hash if there is no double-splat
             if (numKwArgs % 2 == 0 && kwargsLoc.exists()) {
@@ -1044,7 +1047,9 @@ TypePtr Types::applyTypeArguments(const GlobalState &gs, const CallLocs &locs, u
         auto squareBracketsLoc = core::Loc(locs.file, locs.fun.endPos(), locs.call.endPos());
         auto errLoc =
             !locs.args.empty() ? core::Loc(locs.file, locs.args.front().join(locs.args.back())) : squareBracketsLoc;
-        if (auto e = gs.beginError(errLoc, errors::Infer::GenericArgumentCountMismatch)) {
+        auto errorClass =
+            inResolver ? errors::Resolver::GenericArgumentCountMismatch : errors::Infer::GenericArgumentCountMismatch;
+        if (auto e = gs.beginError(errLoc, errorClass)) {
             if (arity == 0) {
                 if (genericClass.data(gs)->typeMembers().empty()) {
                     e.setHeader("`{}` is not a generic class, but was given type parameters", genericClass.show(gs));
