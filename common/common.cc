1ee826ee0f24efde864a86b92a885000cccce33a
Ray Zane <raymondzane@gmail.com>
2024-11-21 19:58:41 -0500

    Format

--- common/common.cc
+++ common/common.cc
@@ -312,128 +312,129 @@ void appendFilesInDir(string_view basePath, const string &path, const sorbet::Un
     ++pendingJobs;
     jobq->push(path, 1);
 
-    workers.multiplexJob("options.findFiles", [numWorkers, jobq, resultq, &pendingJobs, &basePath, &extensions,
-                                               &recursive, &absoluteIgnorePatterns, &relativeIgnorePatterns,
-                                               &visited, &visitedMutex]() {
-        Job job;
-        vector<string> output;
+    workers.multiplexJob("options.findFiles",
+                         [numWorkers, jobq, resultq, &pendingJobs, &basePath, &extensions, &recursive,
+                          &absoluteIgnorePatterns, &relativeIgnorePatterns, &visited, &visitedMutex]() {
+                             Job job;
+                             vector<string> output;
 
-        try {
-            while (true) {
-                auto result = jobq->try_pop(job);
-                if (!result.gotItem()) {
-                    continue;
-                }
-                if (std::holds_alternative<QuitToken>(job)) {
-                    break;
-                }
+                             try {
+                                 while (true) {
+                                     auto result = jobq->try_pop(job);
+                                     if (!result.gotItem()) {
+                                         continue;
+                                     }
+                                     if (std::holds_alternative<QuitToken>(job)) {
+                                         break;
+                                     }
 
-                auto *strvariant = std::get_if<string>(&job);
-                ENFORCE(strvariant != nullptr);
-                auto &path = *strvariant;
+                                     auto *strvariant = std::get_if<string>(&job);
+                                     ENFORCE(strvariant != nullptr);
+                                     auto &path = *strvariant;
 
-                DIR *dir;
-                struct dirent *entry;
+                                     DIR *dir;
+                                     struct dirent *entry;
 
-                if ((dir = opendir(path.c_str())) == nullptr) {
-                    switch (errno) {
-                        case ENOTDIR: {
-                            throw sorbet::FileNotDirException();
-                        }
-                        default:
-                            // Mirrors other FileOps functions: Assume other errors are from FileNotFound.
-                            auto msg = fmt::format("Couldn't open directory `{}`", path);
-                            throw sorbet::FileNotFoundException(msg);
-                    }
-                }
+                                     if ((dir = opendir(path.c_str())) == nullptr) {
+                                         switch (errno) {
+                                             case ENOTDIR: {
+                                                 throw sorbet::FileNotDirException();
+                                             }
+                                             default:
+                                                 // Mirrors other FileOps functions: Assume other errors are from
+                                                 // FileNotFound.
+                                                 auto msg = fmt::format("Couldn't open directory `{}`", path);
+                                                 throw sorbet::FileNotFoundException(msg);
+                                         }
+                                     }
 
-                while ((entry = readdir(dir)) != nullptr) {
-                    const auto namelen = strlen(entry->d_name);
-                    string_view nameview{entry->d_name, namelen};
+                                     while ((entry = readdir(dir)) != nullptr) {
+                                         const auto namelen = strlen(entry->d_name);
+                                         string_view nameview{entry->d_name, namelen};
 
-                    auto fullPath = fmt::format("{}/{}", path, nameview);
-                    bool isDir = entry->d_type == DT_DIR;
-                    ino_t inode = entry->d_ino;
+                                         auto fullPath = fmt::format("{}/{}", path, nameview);
+                                         bool isDir = entry->d_type == DT_DIR;
+                                         ino_t inode = entry->d_ino;
 
-                    if (entry->d_type == DT_LNK) {
-                        struct stat statbuf;
-                        if (stat(fullPath.c_str(), &statbuf) == 0 && S_ISDIR(statbuf.st_mode)) {
-                            isDir = true;
-                            inode = statbuf.st_ino;
-                        }
-                    }
+                                         if (entry->d_type == DT_LNK) {
+                                             struct stat statbuf;
+                                             if (stat(fullPath.c_str(), &statbuf) == 0 && S_ISDIR(statbuf.st_mode)) {
+                                                 isDir = true;
+                                                 inode = statbuf.st_ino;
+                                             }
+                                         }
 
-                    if (isDir) {
-                        if (!recursive) {
-                            continue;
-                        }
-                        if (nameview == "."sv || nameview == ".."sv) {
-                            continue;
-                        }
-                        {
-                            absl::MutexLock lock(&visitedMutex);
-                            if (!visited.insert(inode).second) {
-                                continue;
-                            }
-                        }
-                    } else {
-                        auto dotLocation = nameview.rfind('.');
-                        if (dotLocation == string_view::npos) {
-                            continue;
-                        }
+                                         if (isDir) {
+                                             if (!recursive) {
+                                                 continue;
+                                             }
+                                             if (nameview == "."sv || nameview == ".."sv) {
+                                                 continue;
+                                             }
+                                             {
+                                                 absl::MutexLock lock(&visitedMutex);
+                                                 if (!visited.insert(inode).second) {
+                                                     continue;
+                                                 }
+                                             }
+                                         } else {
+                                             auto dotLocation = nameview.rfind('.');
+                                             if (dotLocation == string_view::npos) {
+                                                 continue;
+                                             }
 
-                        string_view ext = nameview.substr(dotLocation);
-                        if (!extensions.contains(ext)) {
-                            continue;
-                        }
-                    }
+                                             string_view ext = nameview.substr(dotLocation);
+                                             if (!extensions.contains(ext)) {
+                                                 continue;
+                                             }
+                                         }
 
-                    if (sorbet::FileOps::isFileIgnored(basePath, fullPath, absoluteIgnorePatterns,
-                                                       relativeIgnorePatterns)) {
-                        continue;
-                    }
+                                         if (sorbet::FileOps::isFileIgnored(basePath, fullPath, absoluteIgnorePatterns,
+                                                                            relativeIgnorePatterns)) {
+                                             continue;
+                                         }
 
-                    if (isDir) {
-                        ++pendingJobs;
-                        jobq->push(move(fullPath), 1);
-                    } else {
-                        output.push_back(move(fullPath));
-                    }
-                }
+                                         if (isDir) {
+                                             ++pendingJobs;
+                                             jobq->push(move(fullPath), 1);
+                                         } else {
+                                             output.push_back(move(fullPath));
+                                         }
+                                     }
 
-                closedir(dir);
+                                     closedir(dir);
 
-                // Now that we've finished with this directory, we can decrement.
-                auto remaining = --pendingJobs;
-                // If this thread is finished with the last job in the queue, then
-                // we can start signaling other threads that they need to quit.
-                if (remaining == 0) {
-                    // Maintain the invariant, even though we're all done.
-                    pendingJobs += numWorkers;
-                    for (auto i = 0; i < numWorkers; ++i) {
-                        jobq->push(QuitToken{}, 1);
-                    }
-                    break;
-                }
-            }
-        } catch (sorbet::FileNotFoundException &e) {
-            resultq->push(e, 1);
-            pendingJobs += numWorkers;
-            for (auto i = 0; i < numWorkers; ++i) {
-                jobq->push(QuitToken{}, 1);
-            }
-            return;
-        } catch (sorbet::FileNotDirException &e) {
-            resultq->push(e, 1);
-            pendingJobs += numWorkers;
-            for (auto i = 0; i < numWorkers; ++i) {
-                jobq->push(QuitToken{}, 1);
-            }
-            return;
-        }
+                                     // Now that we've finished with this directory, we can decrement.
+                                     auto remaining = --pendingJobs;
+                                     // If this thread is finished with the last job in the queue, then
+                                     // we can start signaling other threads that they need to quit.
+                                     if (remaining == 0) {
+                                         // Maintain the invariant, even though we're all done.
+                                         pendingJobs += numWorkers;
+                                         for (auto i = 0; i < numWorkers; ++i) {
+                                             jobq->push(QuitToken{}, 1);
+                                         }
+                                         break;
+                                     }
+                                 }
+                             } catch (sorbet::FileNotFoundException &e) {
+                                 resultq->push(e, 1);
+                                 pendingJobs += numWorkers;
+                                 for (auto i = 0; i < numWorkers; ++i) {
+                                     jobq->push(QuitToken{}, 1);
+                                 }
+                                 return;
+                             } catch (sorbet::FileNotDirException &e) {
+                                 resultq->push(e, 1);
+                                 pendingJobs += numWorkers;
+                                 for (auto i = 0; i < numWorkers; ++i) {
+                                     jobq->push(QuitToken{}, 1);
+                                 }
+                                 return;
+                             }
 
-        resultq->push(move(output), 1);
-    });
+                             resultq->push(move(output), 1);
+                         });
 
     {
         JobOutput threadResult;
