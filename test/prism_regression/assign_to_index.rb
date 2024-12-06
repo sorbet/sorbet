058e65336da5c178717315762481a19f8053d883
Emily Samp <emily.samp@shopify.com>
2024-12-06 09:48:01 -0600

    Standardize handling of block arguments in Prism -> Sorbet translation
    Handle forwarding super node with block argument
    
    Correctly handle calls to `super` with block
    
    Change argument name to `blockArgumentNode`
    
    Refactor `translateArguments` to remove unused parameter
    
    And make block parameter optional
    
    Refactor index operator writes to use refactored arguments logic
    
    Refactor `PM_CALL_NODE` case to use refactored arguments logic
    
    Refactor `PM_INDEX_TARGET_NODE` case to use refactored arguments logic
    
    Refactor `PM_SUPER_NODE` case to use refactored arguments logic
    
    Refactor `translateArguments` to handle block argument as well
    
    Add a `super` test case to `def_block_param` regression test
    
    Pass block arguments to super calls
    
    Add case to `def_singleton` Prism regression test
    
    Testing the translation of a call to super that passes a block argument.

--- test/prism_regression/assign_to_index.rb
+++ test/prism_regression/assign_to_index.rb
@@ -3,7 +3,7 @@
 # Regular assignment
 regular[0] = 1
 
-# # Compound assignment operators
+# Compound assignment operators
 bitwise_and[0] &= 2
 bitwise_xor[0] ^= 4
 shift_right[0] >>= 5
@@ -15,7 +15,7 @@ divide_assign[0] /= 10
 multiply_assign[0] *= 11
 exponentiate_assign[0] **= 12
 
-# # Special cases
+# Special cases
 lazy_and_assign[0] &&= 13
 lazy_or_assign[0] ||= 14
 
@@ -23,3 +23,23 @@ lazy_or_assign[0] ||= 14
 target[0], target[1] = 15, 16
 target[2, 3], target[4, 5] = 17, 18, 19, 20
 target[] = 21 # Yes, this is valid. You can have `def []=(only_one_param)`.
+
+# Assign to index with a block
+# Note: this is technically not valid Ruby, but Prism allows it.
+
+bitwise_and[&blk] &= 2
+bitwise_xor[&blk] ^= 4
+shift_right[&blk] >>= 5
+shift_left[&blk] <<= 6
+subtract_assign[&blk] -= 7
+module_assign[&blk] %= 8
+bitwise_or[&blk] |= 9
+divide_assign[&blk] /= 10
+multiply_assign[&blk] *= 11
+exponentiate_assign[&blk] **= 12
+
+lazy_and_assign[&blk] &&= 13
+lazy_or_assign[&blk] ||= 14
+
+
+target[&blk], target[1] = 4
--- test/prism_regression/def_singleton.rb
+++ test/prism_regression/def_singleton.rb
@@ -2,3 +2,7 @@
 
 def self.foo; end
 def x.foo; end # This is invalid
+
+def self.foo(&blk)
+  super(&blk)
+end
