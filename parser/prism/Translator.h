ec187efff5f703a1d27dfb136e0be646e39d78be
Emily Samp <emily.samp@shopify.com>
2024-12-17 11:00:25 -0600

    Refactor `skipDynamicConstantWorkaround` logic
    Rather than specifying whether to *skip* the logic, we should
    say whether to replace the constant node with a dynamic const
    assignment.
    
    Then, remove unnecessary type check and move this logic to
    the top of the method so we short-circuit any unnecessary
    code in that case.
    
    Co-authored-by: Stan Lo <stan.lo@shopify.com>

--- parser/prism/Translator.h
+++ parser/prism/Translator.h
@@ -87,7 +87,7 @@ private:
     std::unique_ptr<SorbetAssignmentNode> translateOpAssignment(pm_node_t *node);
 
     template <typename PrismLhsNode, typename SorbetLHSNode>
-    std::unique_ptr<parser::Node> translateConst(PrismLhsNode *node, bool skipDynamicConstantWorkaround = false);
+    std::unique_ptr<parser::Node> translateConst(PrismLhsNode *node, bool replaceWithDynamicConstAssign = false);
 
     // Pattern-matching
     // ... variations of the main translation functions for pattern-matching related nodes.
