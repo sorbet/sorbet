e91c4ad40d112cf6fb6c8fe178d7d3f7878623e2
Trevor Elliott <awesomelyawesome@gmail.com>
2025-01-09 09:42:26 -0800

    Revert "Remove the nullptr reference in the cfg::Send constructor"
    This reverts commit 862c34d824743c35a0692e886d0991a139487e96.

--- cfg/Instructions.h
+++ cfg/Instructions.h
@@ -120,15 +120,8 @@ public:
     std::shared_ptr<core::SendAndBlockLink> link;
 
     Send(LocalRef recv, core::LocOffsets receiverLoc, core::NameRef fun, core::LocOffsets funLoc, uint16_t numPosArgs,
-         const InlinedVector<LocalRef, 2> &args, InlinedVector<core::LocOffsets, 2> &&argLocs,
-         bool isPrivateOk = false);
-
-    Send(LocalRef recv, core::LocOffsets receiverLoc, core::NameRef fun, core::LocOffsets funLoc, uint16_t numPosArgs,
-         const InlinedVector<LocalRef, 2> &args, InlinedVector<core::LocOffsets, 2> &&argLocs, bool isPrivateOk,
-         const std::shared_ptr<core::SendAndBlockLink> &link)
-        : Send(recv, receiverLoc, fun, funLoc, numPosArgs, args, std::move(argLocs), isPrivateOk) {
-        this->link = link;
-    }
+         const InlinedVector<LocalRef, 2> &args, InlinedVector<core::LocOffsets, 2> &&argLocs, bool isPrivateOk = false,
+         const std::shared_ptr<core::SendAndBlockLink> &link = nullptr);
 
     core::LocOffsets locWithoutBlock(core::LocOffsets bindLoc);
 
