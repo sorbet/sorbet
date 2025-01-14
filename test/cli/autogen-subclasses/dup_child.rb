9d6fbb844ee2273723973b969f7bcd586c62291e
Nathan Froyd <froydnj@gmail.com>
2025-01-14 13:25:11 -0500

    update test with multiple locs for `DupChild`

---
+++ test/cli/autogen-subclasses/dup_child.rb
@@ -0,0 +1,12 @@
+module Opus
+  # Should only appear once in output
+  class DupChild
+    class Spec
+      interesting_dsl_class DupChild
+
+      module NamespacedMixinThing
+      end
+    end
+  end
+end
+
