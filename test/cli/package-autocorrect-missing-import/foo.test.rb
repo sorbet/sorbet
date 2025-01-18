e95bf7084e794b386b716fec0618c9d2ee9eb10a
Neil Parikh <parikh.neil@me.com>
2025-01-17 19:18:44 -0500

    tests

--- test/cli/package-autocorrect-missing-import/foo.test.rb
+++ test/cli/package-autocorrect-missing-import/foo.test.rb
@@ -4,4 +4,8 @@ module Test::Foo::MyPackage
   Test::Foo::Bar::OtherPackage::TestUtil
 
   Foo::Bar::OtherPackage::ImportMeTestOnly
+
+  Test::Foo::Bar::AppPackage::TestUtil
+
+  Foo::Bar::AppPackage::ImportMeTestOnly
 end
