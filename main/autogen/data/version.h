63e1c3dc8cc99622a4cf5e305c8b7922e2f95ffa
Nathan Froyd <froydnj@gmail.com>
2025-01-27 11:56:55 -0500

    release depdb v7 (#8490)

--- main/autogen/data/version.h
+++ main/autogen/data/version.h
@@ -6,13 +6,14 @@ namespace sorbet::autogen {
 class AutogenVersion {
 public:
     constexpr static int MIN_VERSION = 6;
-    constexpr static int MAX_VERSION = 6;
+    constexpr static int MAX_VERSION = 7;
 
     // Version history:
     // 3 - Add type alias information in dependency db output
     // 4 - Include .rbi reference information in dependency db
     // 5 - Pack information more tightly in various places
     // 6 - deduplicated nesting vectors, better `resolved` on refs, "lite" db, better symbol placement
+    // 7 - no array headers for defs/refs, more information in "lite" db
     constexpr static int VERSION_INCLUDE_RBI = 4;
 };
 
