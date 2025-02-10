91e154231bf544d8d4b0af97353f7b7e9e24861d
michascant <89426143+MichaScant@users.noreply.github.com>
2025-02-10 16:32:01 -0500

    moved the test cases to a different folder path, used the exmaple provided by the issue originally to test for apply-code-action

---
+++ test/testdata/lsp/code_actions/boolean_type_suggestion.rb
@@ -0,0 +1,24 @@
+# typed: strict
+# selective-apply-code-action: quickfix
+
+class BooleanTypeSuggestion1
+    extend T::Sig
+
+    sig { void }
+    def initialize
+      @a = false
+    # ^^ error: The instance variable `@a` must be declared using `T.let` when specifying `# typed: strict`
+    # ^^ apply-code-action: [A] Replace with T.let(false, T::Boolean)
+    end
+end
+
+class BooleanTypeSuggestion2
+    extend T::Sig
+
+    sig { void }
+    def initialize
+      @a = true
+    # ^^ error: The instance variable `@a` must be declared using `T.let` when specifying `# typed: strict`
+    # ^^ apply-code-action: [A] Replace with T.let(true, T::Boolean)
+    end
+end
