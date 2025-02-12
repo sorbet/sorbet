2f5e651d6816d256aa9dc9cf62ea58346a76aaf1
Jake Zimmerman <zimmerman.jake@gmail.com>
2025-02-13 08:09:30 +0900

    Add failing test

---
+++ test/testdata/parser/implicit_block_rest_arg.rb
@@ -0,0 +1,15 @@
+# typed: true
+extend T::Sig
+
+sig {params(opts: T::Hash[String, Integer]).void}
+def example(opts)
+  opts.each do |k|
+    T.reveal_type(k) # error: `[String, Integer] (2-tuple)`
+  end
+  opts.each do |k,|
+    T.reveal_type(k) # error: `[String, Integer] (2-tuple)`
+  end
+  opts.each do |k, _|
+    T.reveal_type(k) # error: `String`
+  end
+end
