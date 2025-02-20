f92fe7d95969e1728ca3d125723e995c303d098d
Trevor Elliott <awesomelyawesome@gmail.com>
2025-02-20 14:51:09 -0800

    Revert "Avoid using `.at(0)` in getResolved (#8570)"
    This reverts commit 99b1f8ff6041c647cb6fa9f719eeb8c7aff11183.

--- main/lsp/LSPTypechecker.cc
+++ main/lsp/LSPTypechecker.cc
@@ -783,15 +783,9 @@ vector<ast::ParsedFile> LSPTypechecker::getResolved(absl::Span<const core::FileR
 ast::ParsedFile LSPTypechecker::getResolved(core::FileRef fref, WorkerPool &workers) const {
     std::vector<ast::ParsedFile> trees = this->getResolved(std::initializer_list<core::FileRef>{fref}, workers);
 
-    if (trees.empty()) {
-        // This case can happen if the associated file ref was to a payload file, in which case the tree will be
-        // `nullptr` in `this->indexed` and not added to the vector produced by the multi-file version of
-        // `this-fgetResolved`.
-        return ast::ParsedFile{nullptr, fref};
-    } else {
-        ENFORCE(trees.size() == 1);
-        return std::move(trees.front());
-    }
+    ENFORCE(trees.size() == 1);
+    // We explicitly use `.at(0)` so that we see a crash if the above ENFORCE is violated in a release build.
+    return std::move(trees.at(0));
 }
 
 const core::GlobalState &LSPTypechecker::state() const {
