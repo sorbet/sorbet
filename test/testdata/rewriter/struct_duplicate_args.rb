efd583c12521a9098d8ea725199be76ace56d6cb
Eric Hasegawa <erichasegawa1@gmail.com>
2025-02-20 15:58:12 -0800

    feat: detect duplicate members in struct definitions

---
+++ test/testdata/rewriter/struct_duplicate_args.rb
@@ -0,0 +1,21 @@
+# typed: true
+
+class TestStructs
+    DuplicateStruct1 = Struct.new(:foo, :foo) # error: Duplicate member 'foo' in Struct definition
+
+    DuplicateStruct2 = Struct.new(:foo, 'foo') # error: Duplicate member 'foo' in Struct definition
+  
+    DuplicateStruct3 = Struct.new("FooStructDup", :foo, :bar, :foo) # error: Duplicate member 'foo' in Struct definition
+  
+    DuplicateStruct4 = Struct.new(:foo, :bar, :foo, keyword_init: true) # error: Duplicate member 'foo' in Struct definition
+  
+    ValidStruct1 = Struct.new(:foo, :bar)
+  
+    ValidStruct2 = Struct.new("FooStruct", :foo, :bar)
+  
+    ValidStruct3 = Struct.new(:foo, "bar")
+  
+    ValidStruct4 = Struct.new(:foo, :bar, keyword_init: true)
+  
+  end
+ 
