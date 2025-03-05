dddd375e4ef960e7a80f55e278702256b32303df
michascant <89426143+MichaScant@users.noreply.github.com>
2025-03-05 15:38:21 -0500

    added handling for array and hash

--- test/testdata/infer/proc_type_messages.rb
+++ test/testdata/infer/proc_type_messages.rb
@@ -8,6 +8,14 @@ class ProcTypeErrorTest
   def foo(x)
   end
 
+  sig {params(x: T.proc.params(x: T::Array[Integer]).returns(T::Array[String])).void}
+  def array_proc(x)
+  end
+
+  sig {params(x: T.proc.params(x: T::Hash[String, Integer]).returns(T::Hash[Integer, String])).void}
+  def hash_proc(x)
+  end
+
   sig {void}
   def test_proc_type_error
     x = T.cast(nil, T.proc.params(x: String).returns(Integer))
@@ -16,6 +24,26 @@ class ProcTypeErrorTest
 #       Detailed explanation:
 #         Integer is not a subtype of String for return type of Proc1
 #         String is not a supertype of Integer for Arg0 of Proc1
+  end
+
+  sig {void}
+  def test_array_proc_error
+    x = T.cast(nil, T.proc.params(x: T::Array[String]).returns(T::Array[Integer]))
+    array_proc(x)
+#              ^ error: Expected `T.proc.params(arg0: T::Array[Integer]).returns(T::Array[String])` but found `T.proc.params(arg0: T::Array[String]).returns(T::Array[Integer])` for argument `x`
+#              Detailed explanation:
+#                T::Array[Integer] is not a subtype of T::Array[String] for return type of Proc1
+#                T::Array[String] is not a supertype of T::Array[Integer] for Arg0 of Proc1
+  end
+
+  sig {void}
+  def test_hash_proc_error
+    x = T.cast(nil, T.proc.params(x: T::Hash[Integer, String]).returns(T::Hash[String, Integer]))
+    hash_proc(x)
+#             ^ error: Expected `T.proc.params(arg0: T::Hash[String, Integer]).returns(T::Hash[Integer, String])` but found `T.proc.params(arg0: T::Hash[Integer, String]).returns(T::Hash[String, Integer])` for argument `x`
+#             Detailed explanation:
+#               T::Hash[String, Integer] is not a subtype of T::Hash[Integer, String] for return type of Proc1
+#               T::Hash[Integer, String] is not a supertype of T::Hash[String, Integer] for Arg0 of Proc1
   end
 
 end
