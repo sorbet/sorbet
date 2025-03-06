d5ad802ab05e1d0c2c398157aa81bfaee5a13952
Jake Zimmerman <zimmerman.jake@gmail.com>
2025-03-06 15:13:28 -0800

    Add `adjustToLeadingWhitespace` helper
    Makes it easier to do certain kinds of autocorrects.

--- core/Loc.cc
+++ core/Loc.cc
@@ -379,6 +379,21 @@ Loc Loc::adjustLen(const GlobalState &gs, int32_t beginAdjust, int32_t len) cons
     return Loc{this->file(), newBegin, newEnd};
 }
 
+Loc Loc::adjustToLeadingWhitespace(const GlobalState &gs) const {
+    if (!this->exists()) {
+        return Loc::none(this->file());
+    }
+
+    auto viewToStartOfFile = this->file().data(gs).source().substr(0, this->beginPos());
+    size_t newStart = viewToStartOfFile.find_last_not_of(" \t\n");
+    if (newStart == string::npos) {
+        // If this line didn't have a whitespace prefix, then it doesn't need changing.
+        return *this;
+    }
+
+    return Loc(this->file(), newStart, this->endPos());
+}
+
 pair<Loc, uint32_t> Loc::findStartOfLine(const GlobalState &gs) const {
     auto &file = this->file().data(gs);
     auto lineBreaks = file.lineBreaks();
