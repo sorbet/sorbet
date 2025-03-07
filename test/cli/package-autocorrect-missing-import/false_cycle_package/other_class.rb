6a211a45fc25fb7b5ae712b21182763425309c91
Neil Parikh <parikh.neil@me.com>
2025-03-06 16:33:05 -0800

    test for cycle + strict deps too low

---
+++ test/cli/package-autocorrect-missing-import/false_cycle_package/other_class.rb
@@ -0,0 +1,4 @@
+# typed: strict
+
+class Foo::Bar::FalseCyclePackage::OtherClass
+end
--- test/cli/package-autocorrect-missing-import/test.sh
+++ test/cli/package-autocorrect-missing-import/test.sh
@@ -54,6 +54,14 @@ echo
 
 cat use_app_cycle_package/__package.rb
 
+echo
+echo --------------------------------------------------------------------------
+echo
+
+"$cwd/main/sorbet" -a --censor-for-snapshot-tests --silence-dev-message --stripe-packages --packager-layers=lib,app --max-threads=0 false_cycle_package use_false_cycle_package 2>&1
+
+cat use_false_cycle_package/__package.rb
+
 rm -rf "$tmp"
 
 exit 1
