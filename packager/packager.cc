2770ea286f9730fedcc99c69a7238a0874c30b89
Jake Zimmerman <zimmerman.jake@gmail.com>
2025-03-11 16:14:39 -0700

    Remove another include from packager

--- packager/packager.cc
+++ packager/packager.cc
@@ -15,7 +15,6 @@
 #include "core/errors/packager.h"
 #include "core/packages/MangledName.h"
 #include "core/packages/PackageInfo.h"
-#include "rapidjson/writer.h"
 #include <algorithm>
 #include <cctype>
 #include <sstream>
