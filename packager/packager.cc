sorbet/sorbet

commit 24b6a8f436186a52120e5b92555c5934117762c5

Don't suggest an autocorrect to add an `import` if importing it would cause a cycle (#8647)
* refactor

* don't suggest imports if it would cause a cycle

* test for putting into a cycle

* test for cycle + layering violation

* test for cycle + strict deps too low

* test case for cycle + layering violation + strict deps too low

* pass in just mangledName instead of entire package info

* add note about test imports being ignored

* remove timer and add a comment

packager/packager.cc
@@ -598,6 +598,37 @@ public:
                 return core::packages::StrictDependenciesLevel::Dag;
         }
     }
+
+    bool importsTransitively(const core::GlobalState &gs, const core::packages::MangledName &otherPkg) const {
+        UnorderedSet<core::packages::MangledName> seen;
+        vector<core::packages::MangledName> toVisit;
+        toVisit.push_back(mangledName());
+
+        while (!toVisit.empty()) {
+            auto current = toVisit.back();
+            toVisit.pop_back();
+            if (seen.contains(current)) {
+                continue;
+            }
+            toVisit.push_back(current);
+
+            if (current == otherPkg) {
+                return true;
+            }
+
+            auto &info = PackageInfoImpl::from(gs.packageDB().getPackageInfo(current));
+
+            for (auto &import : info.importedPackageNames) {
+                if (import.type == core::packages::ImportType::Test ||
+                    !gs.packageDB().getPackageInfo(import.name.mangledName).exists()) {
+                    continue;
+                }
+
+                toVisit.push_back(import.name.mangledName);
+            }
+        }
+        return false;
+    }
 };
 
 // If the __package.rb file itself is a test file, then the whole package is a test-only package.

test/cli/package-autocorrect-missing-import/cycle_package/__package.rb
@@ -1,0 +1,15 @@
+# frozen_string_literal: true
+# typed: strict
+# enable-packager: true
+
+class Foo::Bar::CyclePackage < PackageSpec
+  layer 'lib'
+  strict_dependencies 'layered'
+
+  import Foo::Bar::CyclePackage::SubPackage
+  import Foo::MyPackage
+
+  export Foo::Bar::CyclePackage::OtherClass
+  export Foo::Bar::CyclePackage::ImportMeTestOnly
+  export Test::Foo::Bar::CyclePackage::TestUtil
+end

test/cli/package-autocorrect-missing-import/app_cycle_package/__package.rb
@@ -1,0 +1,15 @@
+# frozen_string_literal: true
+# typed: strict
+# enable-packager: true
+
+class Foo::Bar::AppCyclePackage < PackageSpec
+  layer 'app'
+  strict_dependencies 'layered'
+
+  import Foo::Bar::AppCyclePackage::SubPackage
+  import Foo::MyPackage
+
+  export Foo::Bar::AppCyclePackage::OtherClass
+  export Foo::Bar::AppCyclePackage::ImportMeTestOnly
+  export Test::Foo::Bar::AppCyclePackage::TestUtil
+end

test/cli/package-autocorrect-missing-import/app_cycle_package/sub_package/__package.rb
@@ -1,0 +1,8 @@
+# frozen_string_literal: true
+# typed: strict
+# enable-packager: true
+
+class Foo::Bar::AppCyclePackage::SubPackage < PackageSpec
+  layer 'app'
+  strict_dependencies 'layered'
+end

test/cli/package-autocorrect-missing-import/app_false_cycle_package/__package.rb
@@ -1,0 +1,15 @@
+# frozen_string_literal: true
+# typed: strict
+# enable-packager: true
+
+class Foo::Bar::AppFalseCyclePackage < PackageSpec
+  layer 'app'
+  strict_dependencies 'false'
+
+  import Foo::Bar::AppFalseCyclePackage::SubPackage
+  import Foo::MyPackage
+
+  export Foo::Bar::AppFalseCyclePackage::OtherClass
+  export Foo::Bar::AppFalseCyclePackage::ImportMeTestOnly
+  export Test::Foo::Bar::AppFalseCyclePackage::TestUtil
+end

test/cli/package-autocorrect-missing-import/app_false_cycle_package/sub_package/__package.rb
@@ -1,0 +1,11 @@
+# frozen_string_literal: true
+# typed: strict
+# enable-packager: true
+
+
+class Foo::Bar::AppFalseCyclePackage::SubPackage < PackageSpec
+  layer 'lib'
+  strict_dependencies 'false'
+
+  import Foo::Bar::AppFalseCyclePackage
+end

test/cli/package-autocorrect-missing-import/cycle_package/sub_package/__package.rb
@@ -1,0 +1,11 @@
+# frozen_string_literal: true
+# typed: strict
+# enable-packager: true
+
+
+class Foo::Bar::CyclePackage::SubPackage < PackageSpec
+  layer 'lib'
+  strict_dependencies 'layered'
+
+  import Foo::Bar::CyclePackage
+end

test/cli/package-autocorrect-missing-import/false_cycle_package/__package.rb
@@ -1,0 +1,15 @@
+# frozen_string_literal: true
+# typed: strict
+# enable-packager: true
+
+class Foo::Bar::FalseCyclePackage < PackageSpec
+  layer 'lib'
+  strict_dependencies 'false'
+
+  import Foo::Bar::FalseCyclePackage::SubPackage
+  import Foo::MyPackage
+
+  export Foo::Bar::FalseCyclePackage::OtherClass
+  export Foo::Bar::FalseCyclePackage::ImportMeTestOnly
+  export Test::Foo::Bar::FalseCyclePackage::TestUtil
+end

commit f416167bdef7ade4515da3f75afcb9599f43dde7

Fix infinite loop introduced in #8647 (#8675)
* make test fail

* fix test

packager/packager.cc
@@ -610,7 +610,7 @@ public:
             if (seen.contains(current)) {
                 continue;
             }
-            toVisit.push_back(current);
+            seen.insert(current);
 
             if (current == otherPkg) {
                 return true;

test/cli/package-autocorrect-missing-import/cycle_package/__package.rb
@@ -6,8 +6,8 @@ class Foo::Bar::CyclePackage < PackageSpec
   layer 'lib'
   strict_dependencies 'layered'
 
-  import Foo::Bar::CyclePackage::SubPackage
   import Foo::MyPackage
+  import Foo::Bar::CyclePackage::SubPackage
 
   export Foo::Bar::CyclePackage::OtherClass
   export Foo::Bar::CyclePackage::ImportMeTestOnly