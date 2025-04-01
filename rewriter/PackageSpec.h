271ebea9aec4ee15e8361b93df05c59a96708714
Jake Zimmerman <zimmerman.jake@gmail.com>
2025-04-01 16:44:12 -0700

    Document PackageSpec.h

--- rewriter/PackageSpec.h
+++ rewriter/PackageSpec.h
@@ -5,7 +5,23 @@
 namespace sorbet::rewriter {
 
 /**
- * TODO(jez) Document me
+ * Does some syntactic rewrites of `__package.rb` files.
+ *
+ * This rewriter only has an effect if `--stripe-packages` has been passed.
+ *
+ * Rewrites:
+ *
+ *     class Foo::Bar < PackageSpec
+ *     end
+ *
+ * to
+ *
+ *     class <PackageSpecRegistry>::Foo::Bar < ::Sorbet::Private::Static::PackageSpec
+ *     end
+ *
+ * TODO(jez) When we move packages into the symbol table, we can drop the `<PackageSpecRegistry>`
+ * bit, which is only required because there could be a class or module symbol with the `Foo::Bar`
+ * name that collides with the package definition.
  */
 class PackageSpec final {
 public:
