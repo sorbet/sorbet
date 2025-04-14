ca58b2a98731f27a0d75cfc1152f6760eaa92cc1
Jake Zimmerman <zimmerman.jake@gmail.com>
2025-04-14 14:30:23 -0700

    Add a `T.must` example (already worked, no work needed)

--- test/testdata/rewriter/cast_block.rb
+++ test/testdata/rewriter/cast_block.rb
@@ -1,4 +1,5 @@
 # typed: strict
+extend T::Sig
 
 T.let(0, Integer) { puts }
 #                ^^^^^^^^^ error: Method `T.let` does not take a block
@@ -6,3 +7,10 @@ T.let(0, Integer) { puts }
 X = T.let(0, Integer) { puts }
 #                    ^^^^^^^^^ error: Method `T.let` does not take a block
 #   ^^^^^^^^^^^^^^^^^^^^^^^^^^ error: Constants must have type annotations with `T.let`
+
+
+sig { params(x: T.nilable(Integer)).void }
+def takes_nilable(x)
+  T.must(x) { puts }
+  #        ^^^^^^^^^ error: Method `T.must` does not take a block
+end
