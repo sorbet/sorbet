4c21346093c489440bed55c9cf7bd78fe27fa17a
Neil Parikh <parikh.neil@me.com>
2025-05-06 14:10:51 -0400

    new folder for main package

--- test/cli/package-error-missing-export/test.sh
+++ test/cli/package-error-missing-export/test.sh
@@ -2,4 +2,4 @@ cd test/cli/package-error-missing-export || exit 1
 
 ../../../main/sorbet --silence-dev-message --stripe-packages \
   --stripe-packages-hint-message="Try running generate-packages.sh" \
-  --max-threads=0 . 2>&1
+  --max-threads=0 other use_other 2>&1
