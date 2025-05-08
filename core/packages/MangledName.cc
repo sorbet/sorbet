e5e3ce60fc7a284ec1eace1cbc51acc569b8cfc4
Jake Zimmerman <zimmerman.jake@gmail.com>
2025-05-07 22:27:20 -0700

    no-op: Remove useless StrCat call

--- core/packages/MangledName.cc
+++ core/packages/MangledName.cc
@@ -9,7 +9,7 @@ namespace sorbet::core::packages {
 MangledName MangledName::mangledNameFromParts(GlobalState &gs, const std::vector<std::string_view> &parts,
                                               ClassOrModuleRef owner) {
     // Foo::Bar => Foo_Bar
-    auto mangledName = absl::StrCat(absl::StrJoin(parts, "_"));
+    auto mangledName = absl::StrJoin(parts, "_");
 
     auto utf8Name = gs.enterNameUTF8(mangledName);
     auto packagerName = gs.freshNameUnique(UniqueNameKind::Packager, utf8Name, 1);
@@ -19,7 +19,7 @@ MangledName MangledName::mangledNameFromParts(GlobalState &gs, const std::vector
 MangledName MangledName::mangledNameFromParts(GlobalState &gs, const std::vector<NameRef> &parts,
                                               ClassOrModuleRef owner) {
     // Foo::Bar => Foo_Bar
-    auto mangledName = absl::StrCat(absl::StrJoin(parts, "_", NameFormatter(gs)));
+    auto mangledName = absl::StrJoin(parts, "_", NameFormatter(gs));
 
     auto utf8Name = gs.enterNameUTF8(mangledName);
     auto packagerName = gs.freshNameUnique(UniqueNameKind::Packager, utf8Name, 1);
