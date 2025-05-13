ea420e837e1ca7225aa6c073e4f59884b152d184
Jake Zimmerman <zimmerman.jake@gmail.com>
2025-05-13 10:41:05 -0700

    Stop entering all mangled names (#8851)
    * Drop NameRef from MangledName
    
    * Stop entering all mangled names
    
    * Add a comment to getUnresolvedPackageName
    
    This was a request from #8848.

--- core/packages/MangledName.cc
+++ core/packages/MangledName.cc
@@ -1,45 +1,11 @@
 #include "core/packages/MangledName.h"
-#include "absl/strings/str_join.h"
 #include "core/GlobalState.h"
 #include "core/Names.h"
 
 using namespace std;
 
 namespace sorbet::core::packages {
-MangledName MangledName::mangledNameFromParts(GlobalState &gs, const std::vector<std::string_view> &parts,
-                                              ClassOrModuleRef owner) {
-    // Foo::Bar => Foo_Bar
-    auto mangledName = absl::StrJoin(parts, "_");
-
-    auto utf8Name = gs.enterNameUTF8(mangledName);
-    auto packagerName = gs.freshNameUnique(UniqueNameKind::Packager, utf8Name, 1);
-    return MangledName(gs.enterNameConstant(packagerName), owner);
-}
-
-MangledName MangledName::mangledNameFromParts(GlobalState &gs, const std::vector<NameRef> &parts,
-                                              ClassOrModuleRef owner) {
-    // Foo::Bar => Foo_Bar
-    auto mangledName = absl::StrJoin(parts, "_", NameFormatter(gs));
-
-    auto utf8Name = gs.enterNameUTF8(mangledName);
-    auto packagerName = gs.freshNameUnique(UniqueNameKind::Packager, utf8Name, 1);
-    return MangledName(gs.enterNameConstant(packagerName), owner);
-}
-
 MangledName MangledName::lookupMangledName(const GlobalState &gs, const vector<string> &parts) {
-    // Foo::Bar => Foo_Bar
-    auto mangledName = absl::StrJoin(parts, "_");
-
-    auto utf8Name = gs.lookupNameUTF8(mangledName);
-    if (!utf8Name.exists()) {
-        return MangledName();
-    }
-
-    auto packagerName = gs.lookupNameUnique(core::UniqueNameKind::Packager, utf8Name, 1);
-    if (!packagerName.exists()) {
-        return MangledName();
-    }
-
     auto owner = core::Symbols::PackageSpecRegistry();
     for (auto part : parts) {
         auto member = owner.data(gs)->findMember(gs, gs.lookupNameConstant(part));
@@ -54,7 +20,7 @@ MangledName MangledName::lookupMangledName(const GlobalState &gs, const vector<s
         owner = core::Symbols::noClassOrModule();
     }
 
-    return MangledName(gs.lookupNameConstant(packagerName), owner);
+    return MangledName(owner);
 }
 
 } // namespace sorbet::core::packages
