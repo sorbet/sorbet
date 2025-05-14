938f34979134908e9e3a20addc57a56e9e62fadc
Jake Zimmerman <zimmerman.jake@gmail.com>
2025-05-14 15:34:52 -0700

    Change a comment

--- infer/environment.h
+++ infer/environment.h
@@ -177,7 +177,7 @@ class Environment {
     /* variable was reasigned. Forget everything about previous value */
     void clearKnowledge(core::Context ctx, cfg::LocalRef reassigned, KnowledgeFilter &knowledgeFilter);
 
-    // Handles updateKnowledge for Kernel#kind_of?, Kernel#is_a?, and Module#===
+    // Handles updateKnowledge for methods that behave like Kernel#is_a?, Module#===, etc.
     void updateKnowledgeKindOf(core::Context ctx, cfg::LocalRef local, core::Loc loc, const core::TypePtr &klassType,
                                cfg::LocalRef ref, KnowledgeFilter &knowledgeFilter, core::NameRef fun);
 
