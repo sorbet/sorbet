46c10873c3fb9dc26a55cc69f473522c8f0e2b57
Jake Zimmerman <zimmerman.jake@gmail.com>
2025-05-14 15:04:37 -0700

    wip

--- .buildkite/tools/setup-bazel.sh
+++ .buildkite/tools/setup-bazel.sh
@@ -6,8 +6,8 @@ echo "--- Pre-setup :bazel:"
 
 if [ "${CLEAN_BUILD:-}" != "" ]; then
   echo "--- cleanup"
-  chmod -R 755 /usr/local/var/bazelcache/
-  rm -rf /usr/local/var/bazelcache/*
+  # chmod -R 755 /usr/local/var/bazelcache/
+  # rm -rf /usr/local/var/bazelcache/*
 fi
 
 rm -f bazel-*
@@ -17,12 +17,12 @@ function finish {
   rm .bazelrc.local
 }
 
-mkdir -p "/usr/local/var/bazelcache/output-bases/${JOB_NAME}" /usr/local/var/bazelcache/build /usr/local/var/bazelcache/repos
+# mkdir -p "/usr/local/var/bazelcache/output-bases/${JOB_NAME}" /usr/local/var/bazelcache/build /usr/local/var/bazelcache/repos
 {
   echo 'common --curses=no --color=yes'
-  echo "startup --output_base=/usr/local/var/bazelcache/output-bases/${JOB_NAME}"
-  echo 'build  --disk_cache=/usr/local/var/bazelcache/build --repository_cache=/usr/local/var/bazelcache/repos'
-  echo 'test   --disk_cache=/usr/local/var/bazelcache/build --repository_cache=/usr/local/var/bazelcache/repos'
+  # echo "startup --output_base=/usr/local/var/bazelcache/output-bases/${JOB_NAME}"
+  # echo 'build  --disk_cache=/usr/local/var/bazelcache/build --repository_cache=/usr/local/var/bazelcache/repos'
+  # echo 'test   --disk_cache=/usr/local/var/bazelcache/build --repository_cache=/usr/local/var/bazelcache/repos'
 } > .bazelrc.local
 
 ./bazel version
