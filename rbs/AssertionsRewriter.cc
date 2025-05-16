6fe8dd946cefb229e887b4b74fcbca6068dea51e
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2025-05-16 14:29:46 -0400

    Use `isspace`
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

--- rbs/AssertionsRewriter.cc
+++ rbs/AssertionsRewriter.cc
@@ -183,7 +183,7 @@ optional<rbs::InlineComment> AssertionsRewriter::commentForNode(const unique_ptr
         auto content = commentNode.string.substr(2);        // skip the #: prefix
 
         // Skip whitespace after the #:
-        while (contentStart < commentNode.loc.endPos() && content[0] == ' ') {
+        while (contentStart < commentNode.loc.endPos() && std::isspace(content[0])) {
             contentStart++;
             content = content.substr(1);
         }
