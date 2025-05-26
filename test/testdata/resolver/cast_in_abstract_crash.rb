7b79ec3c08897a43a5030e0fcb656ffbd18cb07f
Nathan Froyd <froydnj@gmail.com>
2025-05-26 10:32:58 -0400

    strip test down a bit

--- test/testdata/resolver/cast_in_abstract_crash.rb
+++ test/testdata/resolver/cast_in_abstract_crash.rb
@@ -1,34 +1,9 @@
-class HasPrivate
-  private def my_private; end
-end
-
+# typed: true
 class A
   extend T::Sig
 
   sig {abstract.params(blk: T.proc.returns(Out)).void}
   def foo
-    T.reveal_type(@y1)
-    T.reveal_type(@y2)
-
-    @z = T.let(nil, T.nilable(T.type_parameter(:U)))
-  end
-
-  sig {returns(T.nilable(HasPrivate))}
-  def ;end; end
-
-  def bar
-    a = A.new
-
-    if a.foo && a.foo.even?
-      puts a.foo
-    end
-
-    if foo && foo.even?
-      puts a.foo
-    end
-
-    if a.get_private && a.get_private.my_private
-      puts a.foo
-    end
+    @z ||= T.let(nil, T.nilable(T.type_parameter(:U)))
   end
 end
