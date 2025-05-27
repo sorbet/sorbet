330c1b002a7055f67f3dbea9a239ba21c4e605d8
Trevor Elliott <awesomelyawesome@gmail.com>
2025-05-27 09:52:52 -0700

    Fix the crash

--- test/testdata/resolver/generic_missing_args_in_sig.rb
+++ test/testdata/resolver/generic_missing_args_in_sig.rb
@@ -4,5 +4,7 @@ class A
   extend T::Sig, T::Generic
   Elem = type_member
   sig { params(x: self.A).void }
+                # ^^^^^^ error: Malformed type declaration
+                #      ^ error: Method `A` does not exist
   def test(x); end
 end
