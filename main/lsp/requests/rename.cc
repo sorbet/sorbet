b91b6128afc8c7ac6704b7dbe76ef6759bf9c216
Trevor Elliott <awesomelyawesome@gmail.com>
2025-05-27 16:13:12 -0700

    Reject stdlib renames

--- main/lsp/requests/rename.cc
+++ main/lsp/requests/rename.cc
@@ -267,6 +267,11 @@ void enrichResponse(unique_ptr<ResponseMessage> &responseMsg, shared_ptr<Abstrac
 shared_ptr<AbstractRewriter> makeRenamer(const core::GlobalState &gs,
                                          const sorbet::realmain::lsp::LSPConfiguration &config, core::SymbolRef symbol,
                                          const string newName) {
+    auto loc = symbol.loc(gs);
+    if (!loc.exists() || loc.file().data(gs).isStdlib()) {
+        return nullptr;
+    }
+
     if (symbol.isMethod()) {
         auto originalName = symbol.name(gs).show(gs);
         return make_shared<MethodRenamer>(gs, config, originalName, newName);
