8b70f112435b6bd2b2cc5ac376695d87fd34a98c
Trevor Elliott <awesomelyawesome@gmail.com>
2025-05-29 10:32:26 -0700

    Rework the test as a protocol test

--- test/testdata/lsp/completion/constant_fuzz_crash.rb
+++
@@ -1,7 +0,0 @@
-
-# It's important that the name in the class decl be an existing module name
-class Enumerable # error: `Enumerable` was previously defined as a `module`
-  extend T::Generic
-        # ^ completion: DATA, ...
-  X = type_member
-end
