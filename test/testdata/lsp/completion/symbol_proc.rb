dbe9b9a71a0c9f6c49ce5c82c57261cf18eb2e0a
Nathan Froyd <froydnj@gmail.com>
2025-06-02 18:30:07 -0400

    add test for empty symbol proc name

--- test/testdata/lsp/completion/symbol_proc.rb
+++ test/testdata/lsp/completion/symbol_proc.rb
@@ -12,4 +12,9 @@ sig { params(avec: T::Array[A]).returns(T.untyped) }
 def sumup(avec)
   avec.sum(&:val) # error: does not exist
 #               ^ completion: value, ...
+
+  avec.sum(&:)
+#          ^ error: no anonymous block parameter
+#           ^ error: unexpected token
+#            ^ completion: avec, CSV, class, ...
 end
