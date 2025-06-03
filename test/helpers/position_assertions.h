5719465097471d9c9e1b4cc701edb129bd845f6f
Zack Freedman <zack.freedman@shopify.com>
2025-06-03 18:14:38 -0400

    Formatting and PR comments
    Use hint instead of warning for deprecated calls

--- test/helpers/position_assertions.h
+++ test/helpers/position_assertions.h
@@ -14,7 +14,7 @@ using namespace sorbet::realmain::lsp;
 extern realmain::options::Parser parser;
 
 class ErrorAssertion;
-class WarningAssertion;
+class HintAssertion;
 class UntypedAssertion;
 
 /**
@@ -104,9 +104,9 @@ public:
     bool check(const Diagnostic &diagnostic, std::string_view sourceLine, std::string_view errorPrefix);
 };
 
-class WarningAssertion final : public RangeAssertion {
+class HintAssertion final : public RangeAssertion {
 public:
-    static std::shared_ptr<WarningAssertion> make(std::string_view filename, std::unique_ptr<Range> &range,
+    static std::shared_ptr<HintAssertion> make(std::string_view filename, std::unique_ptr<Range> &range,
                                                 int assertionLine, std::string_view assertionContents,
                                                 std::string_view assertionType);
 
@@ -114,7 +114,7 @@ public:
      * Given a set of position-based assertions and Sorbet-generated diagnostics, check that the assertions pass.
      */
     static bool checkAll(const UnorderedMap<std::string, std::shared_ptr<core::File>> &files,
-                         std::vector<std::shared_ptr<WarningAssertion>> warningAssertions,
+                         std::vector<std::shared_ptr<HintAssertion>> warningAssertions,
                          std::map<std::string, std::vector<std::unique_ptr<Diagnostic>>> &filenamesAndDiagnostics,
                          std::string warningPrefix = "");
 
@@ -122,7 +122,7 @@ public:
     const bool matchesDuplicateErrors;  // Template compatibility - same as matchesDuplicateWarnings
     static constexpr DiagnosticSeverity severity = DiagnosticSeverity::Warning;
 
-    WarningAssertion(std::string_view filename, std::unique_ptr<Range> &range, int assertionLine,
+    HintAssertion(std::string_view filename, std::unique_ptr<Range> &range, int assertionLine,
                    std::string_view message, bool matchesDuplicateWarnings);
 
     std::string toString() const override;
