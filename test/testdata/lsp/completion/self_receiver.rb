a7bc8f7a76093b35108758e2258cacd2fc1de511
Trevor Elliott <awesomelyawesome@gmail.com>
2025-06-03 16:21:48 -0700

    Update tests

--- test/testdata/lsp/completion/self_receiver.rb
+++ test/testdata/lsp/completion/self_receiver.rb
@@ -10,6 +10,6 @@ end
 class A
   def foo
     self.y # error: does not exist
-    #     ^ completion: to_yaml, yield_self, Array, Array, display, initialize_copy, pretty_inspect, syscall, system
+    #     ^ completion: to_yaml, yield_self, Array, display, initialize_copy, pretty_inspect, syscall, system
   end
 end
