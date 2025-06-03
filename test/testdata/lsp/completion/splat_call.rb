304488cf24a3d31be039b9f200f9f0dbca7a0a9b
Nathan Froyd <froydnj@gmail.com>
2025-06-03 10:45:29 -0400

    make the completion tests actually pass

--- test/testdata/lsp/completion/splat_call.rb
+++ test/testdata/lsp/completion/splat_call.rb
@@ -16,13 +16,13 @@ end
 sig { params(avec: T::Array[A], blk: T.proc.params(a0: A).returns(T.untyped)).returns(T.untyped) }
 def call_with_block(avec, &blk)
   avec.su(&blk) # error: does not exist
-#        ^ completion: sum
+#        ^ completion: sum, sum, sum, sum
 end
 
 sig { params(as: A, blk: T.proc.params(a0: A).returns(T.untyped)).returns(T.untyped) }
 def sumup_splat_with_block(*as, &blk)
   as.su(&blk) # error: does not exist
-#      ^ completion: sum
+#      ^ completion: sum, sum, sum, sum
 end
 
 sig { params(avec: T::Array[A]).returns(T.untyped) }
@@ -32,11 +32,11 @@ end
 def f
   as = [A.new]
   sumup_(*as) # error: does not exist
-#       ^ completion: sumup_splat
+#       ^ completion: sumup_splat, sumup_splat_with_block
 end
 
 def g(&blk)
   as = [A.new]
   sumup_(*as, &blk) # error: does not exist
-#       ^ completion: sumup_splat
+#       ^ completion: sumup_splat, sumup_splat_with_block
 end
