164af5ef222777a8c959a7d0336d65a73b0a9fe8
Alexander Momchilov <alexander.momchilov@shopify.com>
2025-06-16 08:31:47 -0400

    Convert some sample keywords

--- ast/desugar/Desugar.cc
+++ ast/desugar/Desugar.cc
@@ -1550,6 +1550,8 @@ ExpressionPtr node2TreeImplBody(DesugarContext dctx, parser::Node *what) {
                 result = std::move(res);
             },
             [&](parser::Self *self) {
+                TRANSLATED_BY_PRISM(dctx, self);
+
                 ExpressionPtr res = MK::Self(loc);
                 result = std::move(res);
             },
@@ -1566,6 +1568,8 @@ ExpressionPtr node2TreeImplBody(DesugarContext dctx, parser::Node *what) {
                 result = std::move(res);
             },
             [&](parser::FileLiteral *fileLiteral) {
+                TRANSLATED_BY_PRISM(dctx, fileLiteral);
+
                 ExpressionPtr res = MK::String(loc, core::Names::currentFile());
                 result = std::move(res);
             },
@@ -1725,6 +1729,10 @@ ExpressionPtr node2TreeImplBody(DesugarContext dctx, parser::Node *what) {
                 result = std::move(res);
             },
             [&](parser::Nil *wl) {
+                // This can still be hit because of the call to `make_unique<parser::Nil>(loc)`
+                // in the `parser::Send` case below.
+                // TRANSLATED_BY_PRISM(dctx, wl);
+
                 ExpressionPtr res = MK::Nil(loc);
                 result = std::move(res);
             },
@@ -2223,10 +2231,14 @@ ExpressionPtr node2TreeImplBody(DesugarContext dctx, parser::Node *what) {
                 result = std::move(res);
             },
             [&](parser::True *t) {
+                TRANSLATED_BY_PRISM(dctx, t);
+
                 auto res = MK::True(loc);
                 result = std::move(res);
             },
             [&](parser::False *t) {
+                TRANSLATED_BY_PRISM(dctx, t);
+
                 auto res = MK::False(loc);
                 result = std::move(res);
             },
@@ -2446,6 +2458,8 @@ ExpressionPtr node2TreeImplBody(DesugarContext dctx, parser::Node *what) {
                 result = std::move(res);
             },
             [&](parser::EncodingLiteral *encodingLiteral) {
+                TRANSLATED_BY_PRISM(dctx, encodingLiteral);
+
                 auto recv = MK::Magic(loc);
                 result = MK::Send0(loc, std::move(recv), core::Names::getEncoding(), locZeroLen);
             },
