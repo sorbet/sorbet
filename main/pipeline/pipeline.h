4fb66cffeb69d4ef8b63bfc53c9709fbee72ac31
Trevor Elliott <awesomelyawesome@gmail.com>
2025-06-17 13:02:21 -0700

    Include package files in the condensation order

--- main/pipeline/pipeline.h
+++ main/pipeline/pipeline.h
@@ -48,10 +48,15 @@ size_t partitionPackageFiles(const core::GlobalState &gs, absl::Span<core::FileR
 void unpartitionPackageFiles(std::vector<ast::ParsedFile> &packageFiles,
                              std::vector<ast::ParsedFile> &&nonPackageFiles);
 
-// Using the condensation graph, sort the files according to the layer they would show up in the parallel traversal of
-// the condensation graph.
-std::vector<absl::Span<core::FileRef>> condensationLayers(const core::GlobalState &gs, absl::Span<core::FileRef> files,
-                                                          const options::Options &opts);
+struct CondensationLayerInfo {
+    std::vector<absl::Span<ast::ParsedFile>> packageFiles;
+    std::vector<absl::Span<core::FileRef>> sourceFiles;
+};
+
+// Using the condensation graph, sort the package and source files according to the layer they would show up in the
+// parallel traversal of the condensation graph.
+CondensationLayerInfo condensationLayers(const core::GlobalState &gs, absl::Span<ast::ParsedFile> packageFiles,
+                                         absl::Span<core::FileRef> files, const options::Options &opts);
 
 void package(core::GlobalState &gs, absl::Span<ast::ParsedFile> what, const options::Options &opts,
              WorkerPool &workers);
