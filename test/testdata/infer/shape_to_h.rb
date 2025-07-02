dd1ad46bd9ea728441bbb80f32203e9744e1c6f2
Jake Zimmerman <zimmerman.jake@gmail.com>
2025-07-01 17:22:42 -0700

    Fix test

--- test/testdata/infer/shape_to_h.rb
+++ test/testdata/infer/shape_to_h.rb
@@ -14,7 +14,7 @@ T.reveal_type(opts.to_h) # error: `T::Hash[T.any(Symbol, String), T.any(Integer,
 
 opts = {x: {y: 1}}
 T.reveal_type(opts) # error: `{x: {y: Integer(1)}} (shape of T::Hash[T.untyped, T.untyped])`
-T.reveal_type(opts.to_h) # error: `T::Hash[Symbol, T::Hash[T.untyped, T.untyped]]`
+T.reveal_type(opts.to_h) # error: `T::Hash[Symbol, {y: Integer(1)}]`
 
 opts = {x: {y: 1}.to_h}
 T.reveal_type(opts) # error: `{x: T::Hash[Symbol, Integer]} (shape of T::Hash[T.untyped, T.untyped])`
