c85dfbad68c22883894b9f4dc3d7e7e67de441c1
Trevor Elliott <awesomelyawesome@gmail.com>
2025-07-10 09:24:34 -0700

    Add imports of non-prelude packages

--- test/testdata/packager/prelude_packages_one/prelude/__package.rb
+++ test/testdata/packager/prelude_packages_one/prelude/__package.rb
@@ -5,5 +5,8 @@
 class Prelude < PackageSpec
   prelude_package
 
+  import Application # error: `Prelude` may not `import` non-prelude package `Application`
+  test_import Application # error: `Prelude` may not `test_import` non-prelude package `Application`
+
   export Prelude::A
 end
