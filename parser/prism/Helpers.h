e79124a6951e68031c345082f66e585f774c9aab
Alexander Momchilov <alexander.momchilov@shopify.com>
2025-07-15 18:04:14 -0400

    Remove `using std::is_same_v` from header

--- parser/prism/Helpers.h
+++ parser/prism/Helpers.h
@@ -178,15 +178,13 @@ DEF_TYPE_HELPER(pm_yield_node_t,                           PM_YIELD_NODE);
 #undef DEF_TYPE_HELPER
 // clang-format on
 
-using std::is_same_v;
-
 // Returns true if the given `T` is a Prism node types. All Prism node types start with a `pm_node_t base` member.
-template <typename T> constexpr bool isPrismNode = is_same_v<decltype(T::base), pm_node_t>;
+template <typename T> constexpr bool isPrismNode = std::is_same_v<decltype(T::base), pm_node_t>;
 
 // Take a pointer to a Prism node "subclass" (a thing with an embedded `pm_node_t base` as its first member),
 // and up-casts it back to a general `pm_node_t` pointer.
 template <typename PrismNode> pm_node_t *up_cast(PrismNode *node) {
-    static_assert(!is_same_v<PrismNode, pm_node_t>,
+    static_assert(!std::is_same_v<PrismNode, pm_node_t>,
                   "There's no need to call `up_cast` here, because this is already a `pm_node_t`.");
     static_assert(isPrismNode<PrismNode>, "The `up_cast` function should only be called on Prism node pointers.");
     return reinterpret_cast<pm_node_t *>(node);
@@ -195,7 +193,7 @@ template <typename PrismNode> pm_node_t *up_cast(PrismNode *node) {
 // Take a pointer to a type-erased `pm_node_t` and down-cast it to a pointer of a specific Prism node "subclass".
 // In debug builds, this helper checks the node's type before casting, to ensure it's casted correctly.
 template <typename PrismNode> PrismNode *down_cast(pm_node_t *anyNode) {
-    static_assert(is_same_v<decltype(PrismNode::base), pm_node_t>,
+    static_assert(std::is_same_v<decltype(PrismNode::base), pm_node_t>,
                   "The `down_cast` function should only be called on Prism node pointers.");
     ENFORCE(anyNode == nullptr || PM_NODE_TYPE_P(anyNode, PrismNodeTypeHelper<PrismNode>::TypeID),
             "Failed to cast a Prism AST Node. Expected {} (#{}), but got {} (#{}).",
