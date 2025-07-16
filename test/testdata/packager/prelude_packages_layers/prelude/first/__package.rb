3059072a1855097020f8d88eeb23fa89af18ba13
Trevor Elliott <awesomelyawesome@gmail.com>
2025-07-15 17:22:37 -0700

    Add tests for prelude packages with layering

---
+++ test/testdata/packager/prelude_packages_layers/prelude/first/__package.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+# typed: strict
+# enable-packager: true
+# packager-layers: utility, product
+
+class Prelude::First < PackageSpec
+  prelude_package
+
+  layer "product"
+  #     ^^^^^^^^^ error: Prelude package `Prelude::First` must be in layer `utility`
+  strict_dependencies "dag"
+
+  export Prelude::First::A
+end
