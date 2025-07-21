89c7068a1676510e336ef342a55fde0026b51231
Jake Zimmerman <zimmerman.jake@gmail.com>
2025-07-21 16:25:34 -0700

    Ban `T.self_type` in `T.proc` (except `.bind`) (#9093)
    * Add failing test
    
    * Ban `T.self_type` in `T.proc` (except `.bind`)

---
+++ test/testdata/infer/yield_self.rb
@@ -0,0 +1,22 @@
+# typed: true
+extend T::Sig
+
+class MyObject
+  extend T::Sig
+
+  sig do
+    type_parameters(:U)
+    .params(
+      blk: T.proc.params(arg0: T.self_type).returns(T.type_parameter(:U)),
+      #                        ^^^^^^^^^^^ error: Only top-level T.self_type is supported
+    )
+    .returns(T.type_parameter(:U))
+  end
+  def my_yield_self(&blk)
+    yield self
+  end
+end
+
+MyObject.new.my_yield_self do |this|
+  T.reveal_type(this) # error: `T.untyped`
+end
