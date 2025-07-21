a9fc4ebb74ebcf3be3d841d3c561bdc7f464b968
Jake Zimmerman <zimmerman.jake@gmail.com>
2025-07-21 15:32:40 -0700

    Ban `T.self_type` in `T.proc` (except `.bind`)

--- test/testdata/infer/yield_self.rb
+++ test/testdata/infer/yield_self.rb
@@ -8,14 +8,15 @@ class MyObject
     type_parameters(:U)
     .params(
       blk: T.proc.params(arg0: T.self_type).returns(T.type_parameter(:U)),
+      #                        ^^^^^^^^^^^ error: Only top-level T.self_type is supported
     )
     .returns(T.type_parameter(:U))
   end
-  def my_yield_self(&blk) # error: Expression does not have a fully-defined type
+  def my_yield_self(&blk)
     yield self
   end
 end
 
-MyObject.new.my_yield_self do |this| # error: Expression does not have a fully-defined type
+MyObject.new.my_yield_self do |this|
   T.reveal_type(this) # error: `T.untyped`
 end
