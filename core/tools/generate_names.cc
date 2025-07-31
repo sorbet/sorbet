95602e1834eae2921201c68d8128bdec296d8bc9
Alex Evanczuk <alexevanczuk@gmail.com>
2025-07-30 23:23:11 -0400

    Revert "add its"
    This reverts commit e94976287b71c065b51910ffc9d174a5889a4e17.

--- core/tools/generate_names.cc
+++ core/tools/generate_names.cc
@@ -280,7 +280,6 @@ NameDef names[] = {
     {"subject"},
     {"sharedExamples", "shared_examples"},
     {"letBang", "let!"},
-    {"its"},
 
     {"dslOptional", "dsl_optional"},
     {"dslRequired", "dsl_required"},
