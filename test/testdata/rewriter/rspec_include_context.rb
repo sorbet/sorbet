2031ca2ba3a64e6cf4ee5c9bfbb105afd082536b
Alex Evanczuk <alexevanczuk@gmail.com>
2025-08-01 12:04:19 -0400

    move into describe

--- test/testdata/rewriter/rspec_include_context.rb
+++ test/testdata/rewriter/rspec_include_context.rb
@@ -3,10 +3,6 @@
 class RSpecIncludeContextTest
   extend T::Sig
 
-  shared_examples 'user helpers' do
-    let(:shared_variable) { 'shared value' }
-  end
-
   describe 'nonexistent context' do
     it 'cannot reference undefined context' do
       include_context 'nonexistent context' # error: Unable to resolve constant `<shared_examples 'nonexistent context'>`
@@ -14,6 +10,10 @@ class RSpecIncludeContextTest
   end
 
   describe 'working include_context' do
+    shared_examples 'user helpers' do
+      let(:shared_variable) { 'shared value' }
+    end
+
     include_context 'user helpers'
     
     it 'can access variable from included context' do
