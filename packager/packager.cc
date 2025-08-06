652ea18c4d1440305ed74cccd18991e87dde48a1
Trevor Elliott <awesomelyawesome@gmail.com>
2025-08-06 14:48:41 -0700

    Always compute SCCs (#9135)

--- packager/packager.cc
+++ packager/packager.cc
@@ -2233,9 +2233,7 @@ void packageRunCore(core::GlobalState &gs, WorkerPool &workers, absl::Span<ast::
         Timer timeit(gs.tracer(), "packager.rewritePackagesAndFiles");
 
         if constexpr (buildPackageDB) {
-            if (gs.packageDB().enforceLayering()) {
-                ComputePackageSCCs::run(gs);
-            }
+            ComputePackageSCCs::run(gs);
         }
 
         {
