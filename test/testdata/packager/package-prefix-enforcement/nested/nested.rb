328fdf1614f70e39ba39155d138e59da0cb161a9
Jake Zimmerman <zimmerman.jake@gmail.com>
2025-08-12 15:54:40 -0700

    Add a usage of `Root::NOT_IN_PACKAGE`
    Right now, this doesn't report an error saying that `Root` is not
    imported, because Sorbet thinks the owner of `Root::NOT_IN_PACKAGE` is
    actually `Root::Nested` (the current package, where the usage is).
    
    This is because Sorbet uses location information for where the symbol
    was defined to figure out which package owns this symbol.
    
    This is kind of a weird case, because it's a question of how Sorbet
    recovers from reporting errors.
    
    One thing that Sorbet could choose to do when recovering from the
    "defined somewhere it wasn't supposed to be defined" is to... forcibly
    set the canonical loc of `Root::NOT_IN_PACKAGE` to some file that's
    owned by the `Root` package? Which would then make Sorbet report that
    `Root` is not imported in this package, so `NOT_IN_PACKAGE` cannot be
    used.
    
    This is a case that will change in behavior once we stop using canonical
    locs to determine package ownership, so I wanted to capture it.

--- test/testdata/packager/package-prefix-enforcement/nested/nested.rb
+++ test/testdata/packager/package-prefix-enforcement/nested/nested.rb
@@ -30,12 +30,19 @@ end
 
 module Root
   module Nested
+    extend T::Sig
+
     class SomeClass
       class Deeper; end
     end
 
     class OtherClass
       class Deep2; end
+    end
+
+    sig { void }
+    def example
+      NOT_IN_PACKAGE
     end
   end
 end
