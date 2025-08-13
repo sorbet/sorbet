e73365cc9d9fb0af13f82ed2fa869cc6c4411c30
Jake Zimmerman <zimmerman.jake@gmail.com>
2025-08-13 10:49:36 -0700

    Ban package-private methods outside packages

--- namer/namer.cc
+++ namer/namer.cc
@@ -1018,6 +1018,16 @@ private:
                     break;
                 case core::Names::packagePrivate().rawId():
                 case core::Names::packagePrivateClassMethod().rawId():
+                    if (!owner.data(ctx)->package.exists()) {
+                        if (auto e = ctx.beginError(mod.loc, core::errors::Namer::PackagePrivateOutsidePackage)) {
+                            e.setHeader("Method `{}` is not in a package and so cannot be made `{}`", method.show(ctx),
+                                        mod.name.show(ctx));
+                            e.addErrorLine(owner.data(ctx)->loc(), "This class has no package");
+                            e.addErrorLine(method.data(ctx)->loc(), "Method defined here");
+                        }
+                        break;
+                    }
+
                     method.data(ctx)->flags.isPackagePrivate = true;
                     break;
                 case core::Names::protected_().rawId():
