b1b33936b47c7b9e94300c6578a49e8104cb8c32
Jake Zimmerman <zimmerman.jake@gmail.com>
2025-08-14 14:52:25 -0700

    Fix master introduced in 207e59f5fa (#9176)
    I tested locally to make sure that this wasn't a flaky failure. It's
    probably just that something about how we model overloads landed between
    that PR's merge base and now, but I haven't confirmed that for sure.

--- test/testdata/rbi/net.rb
+++ test/testdata/rbi/net.rb
@@ -10,6 +10,6 @@ Net::HTTP.get(URI("https://sub.example.com/some/path"), { "Accept" => "applicati
                                                                                                  # error: Expected `String` but found `{String("Accept") => String("application/json")}` for argument `path_or_header`
 
 # Invalid combinations
-Net::HTTP.get("www.example.com") # error: Not enough arguments provided for method `Net::HTTP.get (overload.2)`. Expected: `2..3`, got: `1`
+Net::HTTP.get("www.example.com") # error: Not enough arguments provided for method `Net::HTTP.get (overload.3)`. Expected: `2..3`, got: `1`
 Net::HTTP.get("www.example.com", { "Accept" => "application/json" }) # error: Expected `URI::Generic` but found `String("www.example.com")` for argument `uri_or_host`
 Net::HTTP.get(URI("https://sub.example.com"), "/some/path") # error: Expected `T.nilable(T::Hash[T.any(String, Symbol), String])` but found `String("/some/path")` for argument `path_or_header
