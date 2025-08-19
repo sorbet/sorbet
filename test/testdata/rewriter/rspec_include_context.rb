49bc1db13c713e5d62ffe7e621f20f3747f6f857
Alex Evanczuk <alexevanczuk@gmail.com>
2025-08-19 01:47:14 -0400

    revert changes to file

--- test/testdata/rewriter/rspec_include_context.rb
+++ test/testdata/rewriter/rspec_include_context.rb
@@ -1,23 +1,13 @@
 # typed: true
 
-module RSpec
-  module Core
-    class ExampleGroup
-      def self.describe(name, &block); end
-      def expect(*args); end
-      def eq(*args); end
-    end
-  end
-  
-  def self.describe(name, &block)
-    Core::ExampleGroup.describe(name, &block)
-  end
-end
+class RSpecIncludeContextTest
+  extend T::Sig
 
-RSpec.describe 'include context' do
+  # NOTE: I cannot get this test to pass – but I know this is working "in production" because
+  # it works on the Rails monolith this was tested against.
   describe 'nonexistent context' do
     it 'cannot reference undefined context' do
-      include_context 'nonexistent context' # error: Unable to resolve constant `<shared_examples_module 'nonexistent context'>
+      include_context 'nonexistent context' # error: Unable to resolve constant `<shared_examples 'nonexistent context'>`
     end
   end
 
@@ -36,38 +26,54 @@ RSpec.describe 'include context' do
       puts shared_variable
     end
   end
+end
 
-  describe 'RSpec context with shared_context' do
-    shared_context 'authenticated user' do
-      let(:user) { 'authenticated_user' }
-      let(:token) { 'auth_token_123' }
-      let(:auth_helper) { "#{user} with #{token}" }
+# Test shared_context within RSpec.describe (proper RSpec context)
+# Define minimal RSpec for testing
+module RSpec
+  module Core
+    class ExampleGroup
+      def self.describe(name, &block); end
+      def expect(*args); end
+      def eq(*args); end
+    end
+  end
+  
+  def self.describe(name, &block)
+    Core::ExampleGroup.describe(name, &block)
+  end
+end
+
+RSpec.describe 'RSpec context with shared_context' do
+  shared_context 'authenticated user' do
+    let(:user) { 'authenticated_user' }
+    let(:token) { 'auth_token_123' }
+    let(:auth_helper) { "#{user} with #{token}" }
+  end
+
+  describe 'API endpoint' do
+    include_context 'authenticated user'
+    
+    it 'has access to shared context variables and methods' do
+      expect(user).to eq('authenticated_user')
+      expect(token).to eq('auth_token_123') 
+      expect(auth_helper).to eq('authenticated_user with auth_token_123')
+    end
+  end
+
+  # Test include_context with parameters (should ignore parameters)
+  describe 'e2e context' do
+    shared_context 'e2e context' do
+      let(:foo) { 'bar' }
+      let(:config) { 'default_config' }
     end
 
-    describe 'API endpoint' do
-      include_context 'authenticated user'
+    describe 'with parameters' do
+      include_context 'e2e context', { foo: 'bar' }
       
-      it 'has access to shared context variables and methods' do
-        expect(user).to eq('authenticated_user')
-        expect(token).to eq('auth_token_123') 
-        expect(auth_helper).to eq('authenticated_user with auth_token_123')
-      end
-    end
-
-    # Test include_context with parameters (should ignore parameters)
-    describe 'e2e context' do
-      shared_context 'e2e context' do
-        let(:foo) { 'bar' }
-        let(:config) { 'default_config' }
-      end
-
-      describe 'with parameters' do
-        include_context 'e2e context', { foo: 'bar' }
-        
-        it 'should include the context and ignore parameters' do
-          expect(foo).to eq('bar')
-          expect(config).to eq('default_config')
-        end
+      it 'should include the context and ignore parameters' do
+        expect(foo).to eq('bar')
+        expect(config).to eq('default_config')
       end
     end
   end
