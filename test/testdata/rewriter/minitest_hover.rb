0b3e4fa904acff3b6be3bb79e13dda6f0264c59e
Jake Zimmerman <zimmerman.jake@gmail.com>
2025-08-20 10:58:03 -0700

    Add some more minitest tests (#9197)

--- test/testdata/rewriter/minitest_hover.rb
+++ test/testdata/rewriter/minitest_hover.rb
@@ -7,7 +7,27 @@ def after(name, &blk); end
 def test_each(arg, &blk); end
 def test_each_hash(hash, &blk); end
 
+X = "hello"
+
 describe 'example' do
+  x = "hello"
+
+  it "outside test_each #{x}" do
+    # No results
+    # ^ hover: null
+  end
+
+  it "outside test_each #{y}" do
+    #                     ^ error: Method `y` does not exist
+    # No results
+    # ^ hover: null
+  end
+
+  it "outside test_each #{X}" do
+    # No results
+    # ^ hover: null
+  end
+
   test_each(['foo']) do |x|
     it "bar #{x}" do
       # No results
