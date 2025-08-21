94cf7ccaa3af6be0242e40d8fa4efa87401358fe
Jake Zimmerman <zimmerman.jake@gmail.com>
2025-08-20 21:15:56 -0700

    Show how it fails now

--- test/testdata/packager/dag/c/__package.rb
+++ test/testdata/packager/dag/c/__package.rb
@@ -7,5 +7,6 @@ class C < PackageSpec
 
   import A # error: All of `C`'s `import`s must be `dag` or higher
   import Nested
+  #      ^^^^^^ error: Expected `T.class_of(Sorbet::Private::Static::PackageSpec)` but found `T.class_of(Nested)`
   test_import B
 end
