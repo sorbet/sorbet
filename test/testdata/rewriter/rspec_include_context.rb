5f62b14304383c33e8669607f051cf0f149371b1
Alex Evanczuk <alexevanczuk@gmail.com>
2025-09-01 13:39:48 -0400

    maybe make tests pass

--- test/testdata/rewriter/rspec_include_context.rb
+++ test/testdata/rewriter/rspec_include_context.rb
@@ -3,16 +3,11 @@
 class RSpecIncludeContextTest
   extend T::Sig
 
-  # NOTE: I cannot get this test to pass – but I know this is working "in production" because
-  # it works on the Rails monolith this was tested against.
-  describe 'nonexistent context' do
-    it 'cannot reference undefined context' do
-      include_context 'nonexistent context' # error: Method `include` does not exist on `RSpecIncludeContextTest::<describe 'nonexistent context'>` (fix available)
-                                         # error: Unable to resolve constant `<shared_examples_module 'nonexistent context'>` (fix available)
-    end
+  RSpec.describe 'nonexistent context' do
+    include_context 'nonexistent context' # error-with-dupes: Unable to resolve constant `<shared_examples_module 'nonexistent context'>`
   end
 
-  describe 'working include_context' do
+  RSpec.describe 'working include_context' do
     shared_examples 'user helpers' do
       let(:shared_variable) { 'shared value' }
 
