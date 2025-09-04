05bc358069ebf5795359dc15aa99d064aecce720
Nathan Froyd <froydnj@gmail.com>
2025-09-04 14:43:38 -0400

    Squashed commit of the following:
    commit cf6b588e36eae01ce5156e7621ca5f6f96ed91a7
    Author: Nathan Froyd <froydnj@gmail.com>
    Date:   Thu Sep 4 14:26:34 2025 -0400
    
        we need to continue calling through `prop` due to overrides
    
    commit f7d1cd68c66193c53e378c823d6048f521c059fb
    Author: Nathan Froyd <froydnj@gmail.com>
    Date:   Thu Sep 4 11:26:01 2025 -0400
    
        stop supporting outdated `const` calls
    
    commit 52e5111498a18f598bd38df9a5c5fa9f0dec5f55
    Author: Nathan Froyd <froydnj@gmail.com>
    Date:   Thu Sep 4 11:24:21 2025 -0400
    
        avoid creating multiple hashes for `const` props

--- gems/sorbet-runtime/lib/types/props/_props.rb
+++ gems/sorbet-runtime/lib/types/props/_props.rb
@@ -132,17 +132,14 @@ module T::Props
     end
 
     # Shorthand helper to define a `prop` with `immutable => true`
-    sig { params(name: Symbol, cls_or_args: T.untyped, args: T.untyped).void }
-    def const(name, cls_or_args, **args)
-      if (cls_or_args.is_a?(Hash) && cls_or_args.key?(:immutable)) || args.key?(:immutable)
+    sig { params(name: Symbol, cls: T.untyped, rules: T.untyped).void }
+    def const(name, cls, **rules)
+      if rules.key?(:immutable)
         Kernel.raise ArgumentError.new("Cannot pass 'immutable' argument when using 'const' keyword to define a prop")
       end
 
-      if cls_or_args.is_a?(Hash)
-        self.prop(name, **cls_or_args.merge(immutable: true))
-      else
-        self.prop(name, cls_or_args, **args.merge(immutable: true))
-      end
+      rules[:immutable] = true
+      self.prop(name, cls, **rules)
     end
 
     def included(child)
