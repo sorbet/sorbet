9fb5553b2f17010364925469264797662fbc7931
Jake Zimmerman <zimmerman.jake@gmail.com>
2025-09-04 11:36:30 -0700

    Actually fix test

--- test/testdata/infer/let_use_valid_syntax.rb
+++ test/testdata/infer/let_use_valid_syntax.rb
@@ -13,6 +13,8 @@ class B
   extend T::Sig
   sig { void }
   def self.foo
-    @foo = T.cast(new, T.attached_class)
+    @foo = T.cast(new, T.nilable(T.attached_class))
+    #      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ error: Use `T.let` to specify the type of instance variables
+    #      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ error: `T.cast` is useless because `T.attached_class (of B)` is already a subtype of `T.attached_class (of B)`
   end
 end
