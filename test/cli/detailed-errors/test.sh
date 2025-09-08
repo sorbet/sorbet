19335421297cde73e95ff7a48bc75aa247571bde
Neil Parikh <parikh.neil@me.com>
2025-09-07 23:44:04 -0400

    tests

--- test/cli/detailed-errors/test.sh
+++ test/cli/detailed-errors/test.sh
@@ -5,3 +5,9 @@ cwd="$(pwd)"
 cd test/cli/detailed-errors
 
 "$cwd/main/sorbet" --silence-dev-message --max-threads=0 attached_class.rb hash.rb shapes.rb type_members.rb class_of.rb intersection.rb tuples.rb 2>&1
+
+echo
+echo --------------------------------------------------------------------------
+echo
+
+"$cwd/main/sorbet" --silence-dev-message --max-threads=0 union.rb 2>&1
