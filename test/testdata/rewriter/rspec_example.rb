0d1ecfe7c6c3f5edd563e48d6c713b1b4d2c33a5
Jake Zimmerman <zimmerman.jake@gmail.com>
2025-09-10 21:24:17 -0700

    Add test for rspec examples

---
+++ test/testdata/rewriter/rspec_example.rb
@@ -0,0 +1,25 @@
+# typed: true
+
+class A
+  def outer_helper; end
+
+  describe "inside describe" do
+    def my_helper; end
+
+    xit do
+      my_helper
+    end
+
+    it "example", focus: true do
+      my_helper
+    end
+
+    example do
+      my_helper
+    end
+  end
+
+  example do # error: Method `example` does not exist
+    outer_helper # error: Method `outer_helper` does not exist
+  end
+end
