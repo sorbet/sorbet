79c013d1bab0238787ea5235d47200d4d2f12709
Jake Zimmerman <zimmerman.jake@gmail.com>
2025-09-17 16:10:48 -0700

    Improve output of `prism_location_test.sh`
    Before:
    
        FAIL: //test:prism_regression/call_location_test (see /private/var/tmp/_bazel_jez/491a13d243378e23690a7f8390a5c09f/execroot/com_stripe_ruby_typer/bazel-out/darwin_arm64-dbg/testlogs/test/prism_regression/call_location_test/test.log)
        INFO: From Testing //test:prism_regression/call_location_test:
        ==================== Test output for //test:prism_regression/call_location_test:
        Test failed for prism_regression/call.rb
        Differences:
        --- /var/folders/gt/3hq5tw5d62bfpr903pnnsdh80000gn/T/tmp.viJ51BdZL1     2025-09-17 22:55:45
        +++ /var/folders/gt/3hq5tw5d62bfpr903pnnsdh80000gn/T/tmp.KmiLchijES     2025-09-17 22:55:46
        @@ -62,7 +62,7 @@
                 ]
               },
               "method" : "[]=",
        -      "methodLoc" : "/private/var/tmp/_bazel_jez/491a13d243378e23690a7f8390a5c09f/execroot/com_stripe_ruby_typer/bazel-out/darwin_arm64-dbg/bin/test/prism_regression/call_location_test.runfiles/com_stripe_ruby_typer/test/prism_regression/call.rb:7:4-7:4",
        +      "methodLoc" : "/private/var/tmp/_bazel_jez/491a13d243378e23690a7f8390a5c09f/execroot/com_stripe_ruby_typer/bazel-out/darwin_arm64-dbg/bin/test/prism_regression/call_location_test.runfiles/com_stripe_ruby_typer/test/prism_regression/call.rb:7:4-7:12",
               "args" : [
                 {
                   "type" : "Symbol",
        ================================================================================
    
    After:
    
        FAIL: //test:prism_regression/call_location_test (see /private/var/tmp/_bazel_jez/491a13d243378e23690a7f8390a5c09f/execroot/com_stripe_ruby_typer/bazel-out/darwin_arm64-dbg/testlogs/test/prism_regression/call_location_test/test.log)
        INFO: From Testing //test:prism_regression/call_location_test:
        ==================== Test output for //test:prism_regression/call_location_test:
        Running sorbet --print=parse-tree-json-with-locs produced different results for test/prism_regression/call.rb
    
        --- parser_original.ARKUe1hGwe  2025-09-17 23:10:28
        +++ parser_prism   .oIlrnRjFQz  2025-09-17 23:10:28
        @@ -62,7 +62,7 @@
                 ]
               },
               "method" : "[]=",
        -      "methodLoc" : "test/prism_regression/call.rb:7:4-7:4",
        +      "methodLoc" : "test/prism_regression/call.rb:7:4-7:12",
               "args" : [
                 {
                   "type" : "Symbol",
        external/bazel_tools/tools/test/test-setup.sh: line 353: 62796 Killed: 9               sleep 10
        ================================================================================

--- test/prism_location_test.sh
+++ test/prism_location_test.sh
@@ -12,31 +12,38 @@ fi
 
 # Locate the Sorbet binary
 SORBET_BIN="$(rlocation com_stripe_ruby_typer/main/sorbet)"
-TEST_DIR="$(rlocation com_stripe_ruby_typer/test)"
+root_dir="$(rlocation com_stripe_ruby_typer)"
 
 # The Ruby file to test is passed as the first argument
-TEST_FILE="$1"
+test_file="test/$1"
 
 # Temporary files to hold parser outputs
-sorbet_output_prism=$(mktemp)
-sorbet_output_sorbet=$(mktemp)
+sorbet_output_sorbet=$(mktemp -t "parser_original")
+sorbet_output_prism=$(mktemp  -t "parser_prism   ")
 
-set +e  # Temporarily disable exit on error
-# Run Sorbet with default parser
-"$SORBET_BIN" --print=parse-tree-json-with-locs --parser=original "$TEST_DIR/$TEST_FILE" > "$sorbet_output_sorbet" 2>/dev/null
-# Run Sorbet with Prism parser
-"$SORBET_BIN" --print=parse-tree-json-with-locs --parser=prism "$TEST_DIR/$TEST_FILE" > "$sorbet_output_prism" 2>/dev/null
-set -e  # Re-enable exit on error
+(
+  cd "$root_dir"
+
+  set +e  # Temporarily disable exit on error
+  # Run Sorbet with default parser
+  "$SORBET_BIN" --print=parse-tree-json-with-locs --parser=original "$test_file" > "$sorbet_output_sorbet" 2>/dev/null
+  # Run Sorbet with Prism parser
+  "$SORBET_BIN" --print=parse-tree-json-with-locs --parser=prism "$test_file" > "$sorbet_output_prism" 2>/dev/null
+  set -e  # Re-enable exit on error
+)
 
 # Compare outputs
 if ! diff -q "$sorbet_output_prism" "$sorbet_output_sorbet" >/dev/null 2>&1; then
-    echo "Test failed for $TEST_FILE"
-    echo "Differences:"
-    diff -u "$sorbet_output_sorbet" "$sorbet_output_prism"
+    echo "Running sorbet --print=parse-tree-json-with-locs produced different results for $test_file"
+    echo
+    (
+      cd "$(dirname "$sorbet_output_sorbet")"
+      diff -u "$(basename "${sorbet_output_sorbet}")" "$(basename "$sorbet_output_prism")"
+    )
     rm "$sorbet_output_prism" "$sorbet_output_sorbet"
     exit 1
 fi
 
 # Clean up temporary files
 rm "$sorbet_output_prism" "$sorbet_output_sorbet"
-echo "Test passed for $TEST_FILE"
+echo "Test passed for $test_file"
