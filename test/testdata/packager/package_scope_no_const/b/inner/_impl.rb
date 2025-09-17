a66b9906ed75128fb4adfb2d0a386a745ad74b16
Jake Zimmerman <zimmerman.jake@gmail.com>
2025-09-17 11:16:24 -0700

    Don't allow non-class symbols to share name with package

--- test/testdata/packager/package_scope_no_const/b/inner/_impl.rb
+++ test/testdata/packager/package_scope_no_const/b/inner/_impl.rb
@@ -3,4 +3,5 @@
 class B
   Inner = type_member
 # ^^^^^ error: File belongs to package `B::Inner` but defines a constant that does not match this namespace
+# ^^^^^ error: `B::Inner` shares a name with a package and thus must be a class or module, not a constant assignment
 end
