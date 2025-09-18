74b816bccdb6a902a5289decb6b7a4675cd4a771
Trevor Elliott <awesomelyawesome@gmail.com>
2025-09-18 14:20:23 -0700

    Add Parallel::mapReduce

--- common/concurrency/Parallel.h
+++ common/concurrency/Parallel.h
@@ -3,6 +3,7 @@
 
 #include "common/concurrency/ConcurrentQueue.h"
 #include "common/concurrency/WorkerPool.h"
+#include "spdlog/logger.h"
 
 namespace sorbet {
 
@@ -34,6 +35,53 @@ public:
                     }
                 }
             });
+        }
+    }
+
+    template <typename T, typename R, typename Map, typename Reduce>
+    static void mapReduce(spdlog::logger &logger, WorkerPool &workers, std::string_view taskName, absl::Span<T> args,
+                          Map map, Reduce reduce) {
+        if (args.size() <= 1 || workers.size() == 0) {
+            for (T &arg : args) {
+                auto result = std::invoke(map, arg);
+                std::invoke(reduce, std::move(result));
+            }
+        } else {
+            auto taskq = std::make_shared<ConcurrentBoundedQueue<size_t>>(args.size());
+            for (size_t i = 0; i < args.size(); ++i) {
+                taskq->push(i, 1);
+            }
+
+            struct ThreadResult {
+                std::vector<R> results;
+            };
+
+            auto resultq = std::make_shared<BlockingBoundedQueue<ThreadResult>>(workers.size());
+
+            workers.multiplexJob(taskName, [taskq, &args, map, resultq]() mutable {
+                size_t idx;
+                ThreadResult tr;
+                for (auto result = taskq->try_pop(idx); !result.done(); result = taskq->try_pop(idx)) {
+                    if (result.gotItem()) {
+                        T &arg = args[idx];
+                        result.results.emplace_back(std::invoke(map, arg));
+                    }
+                }
+
+                resultq.push(std::move(tr), 1);
+            });
+
+            ThreadResult tr;
+            for (auto result = resultq->wait_pop_timed(tr, WorkerPool::BLOCK_INTERVAL(), logger); !result.done();
+                 result = resultq->wait_pop_timed(tr, WorkerPool::BLOCK_INTERVAL(), logger)) {
+                if (!result.gotItem()) {
+                    continue;
+                }
+
+                for (auto &r : result.results) {
+                    std::invoke(reduce, std::move(r));
+                }
+            }
         }
     }
 };
