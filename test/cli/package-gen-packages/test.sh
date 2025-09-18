0c4eceb267e9ddf5b9cbc171deb82567672873b8
Neil Parikh <parikh.neil@me.com>
2025-09-18 17:23:00 -0400

    test for invalid CLI options

--- test/cli/package-gen-packages/test.sh
+++ test/cli/package-gen-packages/test.sh
@@ -11,6 +11,10 @@ done
 cd "$tmp" || exit 1
 
 
+"$cwd/main/sorbet" --max-threads=0 --silence-dev-message --gen-packages --packager-layers=util,app -a . 2>&1
+
+"$cwd/main/sorbet" --max-threads=0 --silence-dev-message --lsp --stripe-packages --gen-packages --packager-layers=util,app -a . 2>&1
+
 "$cwd/main/sorbet" --max-threads=0 --silence-dev-message --stripe-packages --gen-packages --packager-layers=util,app -a . 2>&1
 
 cat C/__package.rb
