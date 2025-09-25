de4a4c9e709a3e1a4d14eda41c6b84b661a1a62e
Jake Zimmerman <zimmerman.jake@gmail.com>
2025-09-24 18:08:04 -0700

    Test export_all! with multiple errors+autocorrects

--- test/testdata/packager/export_all/other/__package.rb
+++ test/testdata/packager/export_all/other/__package.rb
@@ -5,4 +5,11 @@ class Other < PackageSpec
   import Foo::Bar
   import Foo::Bar::Baz
   import Typical
+
+  export_all!
+
+  export Other::OtherClass
+# ^^^^^^^^^^^^^^^^^^^^^^^^ error: Package `Other` declares `export_all!` and therefore should not use explicit exports
+  export Other::OtherClass2
+# ^^^^^^^^^^^^^^^^^^^^^^^^^ error: Package `Other` declares `export_all!` and therefore should not use explicit exports
 end
