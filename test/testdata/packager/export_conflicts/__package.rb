6326688ae3c2e2913e7330855410474b22e89245
Jake Zimmerman <zimmerman.jake@gmail.com>
2025-09-25 18:22:32 -0700

    Add failing test

--- test/testdata/packager/export_conflicts/__package.rb
+++ test/testdata/packager/export_conflicts/__package.rb
@@ -7,4 +7,6 @@ class MyPackage < PackageSpec
   export MyPackage::A::C
 # ^^^^^^^^^^^^^^^^^^^^^^ error: Cannot export `MyPackage::A::C` because another exported name `MyPackage::A` is a prefix of it
   export MyPackage::A
+
+  export MyPackage::HasSingletonIvar # error: Cannot export `T.class_of(MyPackage::HasSingletonIvar::Nested)#@nested` because another exported name `MyPackage::HasSingletonIvar` is a prefix of it
 end
