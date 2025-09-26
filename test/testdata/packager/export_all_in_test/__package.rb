c5f82eb08f896fee6dcc11a03cb7d5e9d3fdc065
Jake Zimmerman <zimmerman.jake@gmail.com>
2025-09-25 20:32:39 -0700

    Fix another test

--- test/testdata/packager/export_all_in_test/__package.rb
+++ test/testdata/packager/export_all_in_test/__package.rb
@@ -4,8 +4,8 @@
 
 class Foo::Bar < PackageSpec
   export_all!
+# ^^^^^^^^^^^ error: Package `Foo::Bar` declares `export_all!` and therefore should not use explicit exports
 
   export Test::Foo::Bar::Thing
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^ error: Package `Foo::Bar` declares `export_all!` and therefore should not use explicit exports
 
 end
