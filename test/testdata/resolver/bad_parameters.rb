34db99b9796ec8b8c305d52b6094776923ee48dc
Jake Zimmerman <zimmerman.jake@gmail.com>
2025-09-26 19:12:19 -0700

    Show changes to the test

--- test/testdata/resolver/bad_parameters.rb
+++ test/testdata/resolver/bad_parameters.rb
@@ -3,23 +3,23 @@
 extend T::Sig
 
 sig { params(name: String).returns(T.nilable(String)) }
-#            ^^^^ error: Malformed `sig`. Type not specified for argument `name`
 def foo(name)
 end
 
 
 sig { params(other_name: String).returns(T.nilable(String)) }
-#            ^^^^^^^^^^ error: Unknown argument name `other_name`
+#            ^^^^^^^^^^ error: Unknown parameter name `other_name`
 def foo(name)
+#       ^^^^ error: Malformed `sig`. Type not specified for parameter `name`
 end
 
 def bar(name)
 end
 
 sig { params(other_name: String).returns(T.nilable(String)) }
-#            ^^^^^^^^^^ error: Unknown argument name `other_name`
+#            ^^^^^^^^^^ error: Unknown parameter name `other_name`
 def bar(name)
-#       ^^^^ error: Malformed `sig`. Type not specified for argument `name`
+#       ^^^^ error: Malformed `sig`. Type not specified for parameter `name`
 end
 
 def qux(name)
@@ -27,5 +27,5 @@ end
 
 sig { returns(T.nilable(String)) }
 def qux(name)
-#       ^^^^ error: Malformed `sig`. Type not specified for argument `name`
+#       ^^^^ error: Malformed `sig`. Type not specified for parameter `name`
 end
