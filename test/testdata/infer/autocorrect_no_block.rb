98cf0e74c37711245aba35824ea69bd3c95c95b2
Jake Zimmerman <zimmerman.jake@gmail.com>
2025-09-29 19:22:35 -0700

    Show tests after

--- test/testdata/infer/autocorrect_no_block.rb
+++ test/testdata/infer/autocorrect_no_block.rb
@@ -6,22 +6,22 @@ def takes_no_block(x: 0)
 end
 
 takes_no_block(x: 0) do end
-#                   ^^^^^^^ error: does not take a block
+#                    ^^^^^^ error: does not take a block
 takes_no_block do end
-#             ^^^^^^^ error: does not take a block
+#              ^^^^^^ error: does not take a block
 takes_no_block{}
 #             ^^ error: does not take a block
 
 sig {params(xs: T::Array[Integer]).void}
 def example1(xs)
   xs[0] {}
-  #    ^^^ error: does not take a block
+  #     ^^ error: does not take a block
 end
 
 f = ->(){}
 takes_no_block(&f)
-#             ^^^^ error: does not take a block
+#              ^^ error: does not take a block
 
 xs = []
 takes_no_block(*xs, &f)
-#             ^^^^^^^^^ error: does not take a block
+#                   ^^ error: does not take a block
