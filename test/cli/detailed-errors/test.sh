89323823f95fd5b0271b47a5e9508c1f6fe7aa9f
Neil Parikh <parikh.neil@me.com>
2025-10-03 18:36:42 -0400

    explicitly pass in the list of files in test.sh

--- test/cli/detailed-errors/test.sh
+++ test/cli/detailed-errors/test.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+cwd="$(pwd)"
 
-main/sorbet --silence-dev-message --max-threads=0 test/cli/detailed-errors 2>&1
+cd test/cli/detailed-errors
+
+"$cwd/main/sorbet" --silence-dev-message --max-threads=0 attached_class.rb hash.rb shapes.rb type_members.rb class_of.rb intersection.rb tuples.rb 2>&1
