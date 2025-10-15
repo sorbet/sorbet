6ff5ae12e786845a7f00c909473380d4cf32e0bd
Jake Zimmerman <zimmerman.jake@gmail.com>
2025-10-15 10:32:00 -0700

    Add NewSelfType
    Will replace `SelfType` eventually, but because the current `SelfType` is
    going to become essentially `LambdaParam` and `NewSelfType` will act
    more like a `SelfTypeParam` without a `SymbolRef definition`, I want to
    rename everything to make sure I've touched all call sites.
    
    Once I'm sure that the new thing is being used correctly, I'll go back
    to calling this `SelfType` (or maybe SelfTypeParamForSelfType or
    something, not sure).

--- core/types/types.cc
+++ core/types/types.cc
@@ -784,6 +784,10 @@ SelfTypeParam::SelfTypeParam(const SymbolRef definition) : definition(definition
     recordAllocatedType("selftypeparam");
 }
 
+NewSelfType::NewSelfType(const TypePtr &upperBound) : upperBound(move(upperBound)) {
+    recordAllocatedType("selftype");
+}
+
 bool LambdaParam::derivesFrom(const GlobalState &gs, ClassOrModuleRef klass) const {
     Exception::raise(
         "LambdaParam::derivesFrom not implemented, not clear what it should do. Let's see this fire first.");
@@ -793,9 +797,19 @@ bool SelfTypeParam::derivesFrom(const GlobalState &gs, ClassOrModuleRef klass) c
     return false;
 }
 
+bool NewSelfType::derivesFrom(const GlobalState &gs, ClassOrModuleRef klass) const {
+    return false;
+}
+
 void LambdaParam::_sanityCheck(const GlobalState &gs) const {}
 void SelfTypeParam::_sanityCheck(const GlobalState &gs) const {
     ENFORCE(definition.isTypeMember() || definition.isTypeParameter());
+}
+
+void NewSelfType::_sanityCheck(const GlobalState &gs) const {
+    ENFORCE(this->upperBound != nullptr);
+    ENFORCE(!this->upperBound.isUntyped());
+    ENFORCE(this->upperBound.isFullyDefined());
 }
 
 TypePtr OrType::make_shared(const TypePtr &left, const TypePtr &right) {
@@ -835,6 +849,7 @@ TypePtr TupleType::elementType(const GlobalState &gs) const {
 SelfType::SelfType() {
     recordAllocatedType("selftype");
 };
+
 AppliedType::AppliedType(ClassOrModuleRef klass, vector<TypePtr> targs) : klass(klass), targs(std::move(targs)) {
     recordAllocatedType("appliedtype");
     histogramInc("appliedtype.targs", this->targs.size());
