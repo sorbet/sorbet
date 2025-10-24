7ad1430b770d35fcb18d78e1ca9e4430bc10650d
Jake Zimmerman <zimmerman.jake@gmail.com>
2025-10-23 19:29:36 -0700

    Show the tests that change

--- test/testdata/namer/has_attached_class_subclass_module.rb
+++ test/testdata/namer/has_attached_class_subclass_module.rb
@@ -1,10 +1,11 @@
 # typed: strict
+# disable-fast-path: true
 
 class C < Module
   extend T::Generic
   has_attached_class!
 end
 
-class D < Module
+class D < Module # error: `has_attached_class!` declared by parent `Module` must be re-declared in `D`
   extend T::Generic
 end
