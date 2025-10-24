6d12aa76933084419bcffb68eee29ce5b60131bd
Jake Zimmerman <zimmerman.jake@gmail.com>
2025-10-24 13:57:06 -0700

    Fix failing test

--- test/testdata/infer/kwsplat_nil.rb
+++ test/testdata/infer/kwsplat_nil.rb
@@ -13,17 +13,13 @@ class Example
   sig { params(hash: T.nilable(T::Hash[T.untyped, T.untyped]), maybe_hash_tm: MaybeHash).void }
   def example(hash, maybe_hash_tm)
     { success: true, **hash }
-  # ^^^^^^^^^^^^^^^^^^^^^^^^^ error: Method `to_hash` does not exist on `NilClass` component
 
     takes_kwargs(**nil)
-    #            ^^^^^ error: Method `to_hash` does not exist on `NilClass`
 
     no_kwargs = nil
     takes_kwargs(**no_kwargs)
-    #            ^^^^^^^^^^^ error: Method `to_hash` does not exist on `NilClass`
 
     takes_kwargs(**hash)
-    #            ^^^^^^ error: Method `to_hash` does not exist on `NilClass` component
 
     takes_kwargs(**maybe_hash_tm) # error: Method `to_hash` does not exist on `NilClass` component
   end
