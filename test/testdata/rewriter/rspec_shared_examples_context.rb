8cd8bb18a98f6aeb5da902e0c01a653cec3529e8
Jake Zimmerman <zimmerman.jake@gmail.com>
2025-10-24 10:05:15 -0700

    Support for `context` inside `shared_examples` (#9491)
    * Add test showing behavior before
    
    * Support for `context` inside `shared_examples`
    
    And show behavior to tests that change

---
+++ test/testdata/rewriter/rspec_shared_examples_context.rb
@@ -0,0 +1,30 @@
+# typed: true
+# enable-experimental-requires-ancestor: true
+
+module RSpec
+  module Core
+    class ExampleGroup
+      def expect(arg)
+      end
+
+      def eq(arg)
+      end
+    end
+  end
+end
+
+class MyClass < RSpec::Core::ExampleGroup
+  describe("single shared example with context") do
+    shared_examples "my shared example" do
+      let(:foo_exists) { 'bar' }
+
+      context 'context' do
+        it 'works' do
+          expect(foo_exists).to eq('bar')
+          this_does_not_exist
+        # ^^^^^^^^^^^^^^^^^^^ error: `this_does_not_exist` does not exist
+        end
+      end
+    end
+  end
+end
