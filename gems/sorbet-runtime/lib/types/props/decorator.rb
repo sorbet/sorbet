258b76baf0bc6911f16b5a7ffb9c9911ce3c73bf
Nathan Froyd <froydnj@gmail.com>
2025-10-24 13:55:30 -0400

    don't freeze immutable objects

--- gems/sorbet-runtime/lib/types/props/decorator.rb
+++ gems/sorbet-runtime/lib/types/props/decorator.rb
@@ -264,9 +264,9 @@ class T::Props::Decorator
     nil
   end
 
-  SAFE_NAME = T.let(/\A[A-Za-z_][A-Za-z0-9_-]*\z/.freeze, Regexp, checked: false)
+  SAFE_NAME = T.let(/\A[A-Za-z_][A-Za-z0-9_-]*\z/, Regexp, checked: false)
   # Should be exactly the same as `SAFE_NAME`, but with a leading `@`.
-  SAFE_ACCESSOR_KEY_NAME = T.let(/\A@[A-Za-z_][A-Za-z0-9_-]*\z/.freeze, Regexp, checked: false)
+  SAFE_ACCESSOR_KEY_NAME = T.let(/\A@[A-Za-z_][A-Za-z0-9_-]*\z/, Regexp, checked: false)
 
   # Used to validate both prop names and serialized forms
   sig { params(name: T.any(Symbol, String)).void.checked(:never) }
