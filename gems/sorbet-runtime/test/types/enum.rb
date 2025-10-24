b661de736fa734965408e7c32dac434c0e01a95c
Nathan Froyd <froydnj@gmail.com>
2025-10-24 14:23:14 -0400

    tweak rubocops a little bit (#9503)

--- gems/sorbet-runtime/test/types/enum.rb
+++ gems/sorbet-runtime/test/types/enum.rb
@@ -372,7 +372,7 @@ class T::Enum::Test::EnumTest < Critic::Unit::UnitTest
   end
 
   describe 'string value conversion assertions' do
-    ENUM_CONVERSION_MSG = /Implicit conversion of Enum instances to strings is not allowed. Call #serialize instead./.freeze
+    ENUM_CONVERSION_MSG = /Implicit conversion of Enum instances to strings is not allowed. Call #serialize instead./
 
     before do
       T::Configuration.expects(:soft_assert_handler).never
