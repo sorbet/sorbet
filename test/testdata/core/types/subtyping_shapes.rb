4063af78eb020e935c21b232d9ab42202306f8c9
Neil Parikh <parikh.neil@me.com>
2025-10-29 11:03:20 -0700

    failing test

--- test/testdata/core/types/subtyping_shapes.rb
+++ test/testdata/core/types/subtyping_shapes.rb
@@ -14,3 +14,13 @@ bar(["ok"])
 bar({a: "bad"}) # error: Expected `T.any({a: Integer}, [String])` but found `{a: String("bad")}` for argument `x`
 bar({b: -1}) # error: Expected `T.any({a: Integer}, [String])` but found `{b: Integer(-1)}` for argument `x`
 bar({c: -1}) # error: Expected `T.any({a: Integer}, [String])` but found `{c: Integer(-1)}` for argument `x`
+
+elements = [
+  {
+    url: "https://go/pr_cov"
+  }
+]
+
+if T.unsafe(nil)
+  elements.unshift({ style: "primary", url: "" }) # error: Expected `{url: String("https://go/pr_cov")}` but found `{style: String("primary"), url: String("")}` for argument `arg0`
+end
