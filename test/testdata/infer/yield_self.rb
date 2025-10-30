1937648a3cb16ae33de592382a788a8ad04de4d0
Jake Zimmerman <zimmerman.jake@gmail.com>
2025-10-30 11:03:14 -0700

    Non-top-level self type is allowed now

--- test/testdata/infer/yield_self.rb
+++ test/testdata/infer/yield_self.rb
@@ -8,7 +8,6 @@ class MyObject
     type_parameters(:U)
     .params(
       blk: T.proc.params(arg0: T.self_type).returns(T.type_parameter(:U)),
-      #                        ^^^^^^^^^^^ error: Only top-level `T.self_type` is supported
     )
     .returns(T.type_parameter(:U))
   end
@@ -18,5 +17,5 @@ class MyObject
 end
 
 MyObject.new.my_yield_self do |this|
-  T.reveal_type(this) # error: `T.untyped`
+  T.reveal_type(this) # error: `MyObject`
 end
