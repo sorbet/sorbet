b60807e4bafd64770d417f8c7a8d6fe0b3623c69
Jake Zimmerman <zimmerman.jake@gmail.com>
2025-10-30 17:10:59 -0700

    Squashed commit of the following:
    commit 989bdb160e2ad72870f4cb9903fcbda7b6275f7d
    Author: Jake Zimmerman <zimmerman.jake@gmail.com>
    Date:   Thu Oct 30 17:08:09 2025 -0700
    
        This test changes
    
    commit cdae4d6aa895732cd70b8ea6b566324c113d83d9
    Author: Jake Zimmerman <zimmerman.jake@gmail.com>
    Date:   Wed Oct 29 23:00:12 2025 -0700
    
        Make `T.proc.bind(...)` use the externalType
    
    commit d1a14ef927e683bdd40dafa302612bfb3139da0e
    Author: Jake Zimmerman <zimmerman.jake@gmail.com>
    Date:   Thu Oct 30 17:04:58 2025 -0700
    
        Add some more tests

--- test/testdata/infer/attached_class_intrinsic.rb
+++ test/testdata/infer/attached_class_intrinsic.rb
@@ -31,19 +31,25 @@ class B
 
   sig {params(blk: T.proc.bind(T.attached_class).void).void}
   def self.bind_attached(&blk); end
+
+  sig {params(blk: T.proc.bind(T.self_type).void).void}
+  def self.bind_self(&blk); end
 end
 
 B.bind_example do
+  T.reveal_type(self) # error: `T.class_of(Example)`
   example = new
-  T.reveal_type(example) # error: `T.attached_class (of Example)`
+  T.reveal_type(example) # error: `Example`
   examples = T::Array[T.attached_class].new
   T.reveal_type(examples) # error: `T::Array[T.attached_class (of Example)]`
   examples << example
+  #           ^^^^^^^ error: Expected `T.attached_class (of Example)` but found `Example`
   examples << 'nope'
   #           ^^^^^^ error: Expected `T.attached_class (of Example)` but found `String("nope")`
 end
 
 B.bind_attached do
+  T.reveal_type(self) # error: `B`
   # There is a duplicate error here, because the resolver pass that looks at
   # `T.let` and the intrinsic report this error
   b = T.let(new, T.attached_class)
@@ -55,4 +61,16 @@ B.bind_attached do
   bs = T::Array[T.attached_class].new
   #             ^^^^^^^^^^^^^^^^ error: `T.attached_class` may only be used in singleton methods on classes or instance methods on `has_attached_class!` modules
   T.reveal_type(bs) # error: `T::Array[T.untyped]`
+end
+
+B.bind_self do
+  T.reveal_type(self) # error: `T.class_of(B)`
+  # The `T.let` type gets parsed in resolver (before T.proc.bind)
+  b = T.let(new, T.attached_class)
+  #              ^^^^^^^^^^^^^^^^ error: `T.attached_class` may only be used in singleton methods on classes or instance methods on `has_attached_class!` modules
+
+  puts(b)
+  # The `T.let` type here gets handled by calls.cc
+  bs = T::Array[T.attached_class].new
+  T.reveal_type(bs) # error: `T::Array[T.attached_class (of B)]`
 end
