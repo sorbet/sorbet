a67eb3f99aa203c191d551b3e0f27a036397b110
Jake Zimmerman <zimmerman.jake@gmail.com>
2025-11-11 20:57:51 -0500

    Remove an ancient TODO (#9532)
    * testing
    
    * Even more ENFORCE
    
    * Use resultTypeAsSeenFrom in resolver/
    
    * Some more calls to resultTypeAsSeenFrom
    
    * Tidy up ENFORCEs, make lub and glb symmetric
    
    Before this, `Types::glb` had no handling for the `LambdaParam` case,
    but `Types::lub` did. That made me suspect that there was a bug.
    
    * Use resultTypeAsSeenFromSelf for better perf
    
    * Make the resultTypeAsSeenFrom happen first
    
    This matches everywhere else in the codebase.
    
    I'm not sure if it should be this way or not, and I think that if we
    unified `TypeVar` with `LambdaParam` this problem would not happen
    (because it would mean that `resultTypeAsSeenFrom` and
    `{approximate,instantiate}TypeVars` all combine into one.
    
    But the idea is that as the type is being created on the way back up
    from `OrType::approximateTypeVars`, we're going to combine both halves
    using `Types::any` (because maybe approximating left and right could
    make the `OrType` collapse). That's going to mean invoking isSubType,
    and isSubType is not implemented for LambdaParam. So if we don't
    eliminate all LambdaParam first, and then handle all TypeVar, then we
    get an ENFORCE in approximateTypeVars.
    
    The test that fails without this is `test/testdata/infer/result.rb`.
    
    * prework: Remove a TODO
    
    * Exempt `badAliasMethodStub`
    
    This method is owned by `Sorbet::Private::Static`, which is almost
    certainly not what the bad alias method was called on.
    
    If you look in `test/testdata/namer/alias_method.rb`, you see that we
    have this bad method alias stub method so that if Sorbet fails to
    resolve where the alias method targets, we don't report errors at calls
    to it (rather, we treat it like an untyped method that accepts whatever
    args).
    
    But that means that realistically, we want to skip all the logic that
    `dispatchCallSymbol` would otherwise do, including any calls to
    `resultTypeAsSeenFrom`.
    
    * requires_ancestor breaks this

--- core/types/types.cc
+++ core/types/types.cc
@@ -692,19 +692,18 @@ InlinedVector<TypeMemberRef, 4> Types::alignBaseTypeArgs(const GlobalState &gs, 
  */
 TypePtr Types::resultTypeAsSeenFrom(const GlobalState &gs, const TypePtr &what, ClassOrModuleRef fromWhat,
                                     ClassOrModuleRef inWhat, const vector<TypePtr> &targs) {
+    // requires_ancestor leads to dispatches that target some other symbol entirely.
+    // There is no guarantee that we have redeclared the other method's type members.
+    // This is yet another case where requires ancestor is just broken.
+    ENFORCE(inWhat == fromWhat || inWhat.data(gs)->derivesFrom(gs, fromWhat) ||
+                fromWhat.data(gs)->derivesFrom(gs, inWhat) || gs.cacheSensitiveOptions.requiresAncestorEnabled,
+            "\n{}\nis unrelated to\n\n{}", fromWhat.toString(gs), inWhat.toString(gs));
+
     auto originalOwner = fromWhat;
 
-    // TODO: the ENFORCE below should be above this conditional, but there is
-    // currently a problem with the handling of `module_function` that causes it
-    // to fail reliably. https://github.com/sorbet/sorbet/issues/904
-    // TODO(jez) Is this comment still true?
     if (originalOwner.data(gs)->typeMembers().empty() || (what == nullptr)) {
         return what;
     }
-
-    ENFORCE(inWhat == fromWhat || inWhat.data(gs)->derivesFrom(gs, fromWhat) ||
-                fromWhat.data(gs)->derivesFrom(gs, inWhat),
-            "\n{}\nis unrelated to\n\n{}", fromWhat.toString(gs), inWhat.toString(gs));
 
     TypePtr::InstantiationContext ictx(originalOwner, inWhat, targs);
     auto result = what._instantiateLambdaParams(gs, ictx);
