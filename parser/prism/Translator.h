5a934f014478b0ba49213a676c986ee968cf31f8
Thomas Marshall <thomas.marshall@shopify.com>
2025-11-26 18:40:28 +0000

    Reset desguar unique counter to 1 in Prism translator
    This commit fixes an issue where the desugar unique counter was being
    incorrectly reset to 0 instead of 1 (to match the original desugar
    behavior).

--- parser/prism/Translator.h
+++ parser/prism/Translator.h
@@ -71,7 +71,7 @@ private:
         : parser(parent.parser), ctx(parent.ctx), parseErrors(parent.parseErrors),
           directlyDesugar(parent.directlyDesugar), preserveConcreteSyntax(parent.preserveConcreteSyntax),
           parserUniqueCounterStorage(std::numeric_limits<uint16_t>::min()),
-          desugarUniqueCounterStorage(resetDesugarUniqueCounter ? std::numeric_limits<uint32_t>::min() : 1),
+          desugarUniqueCounterStorage(resetDesugarUniqueCounter ? 1 : std::numeric_limits<uint32_t>::min()),
           parserUniqueCounter(parent.parserUniqueCounter),
           desugarUniqueCounter(resetDesugarUniqueCounter ? this->desugarUniqueCounterStorage
                                                          : parent.desugarUniqueCounter),
