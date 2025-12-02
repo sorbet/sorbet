7b96fbb1209b876374eb41d6bb837be48728d5da
Alexander Momchilov <alexander.momchilov@shopify.com>
2025-12-02 11:46:29 -0500

    Move `supportedCallType` down

--- parser/prism/Translator.cc
+++ parser/prism/Translator.cc
@@ -1786,7 +1786,6 @@ unique_ptr<parser::Node> Translator::translate(pm_node_t *node) {
             };
 
             enforceHasExpr(receiver);
-            auto supportedCallType = true;
 
             unique_ptr<parser::Node> blockBody;       // e.g. `123` in `foo { |x| 123 }`
             unique_ptr<parser::Node> blockParameters; // e.g. `|x|` in `foo { |x| 123 }`
@@ -1923,7 +1922,7 @@ unique_ptr<parser::Node> Translator::translate(pm_node_t *node) {
                         } else {
                             auto blockPassArgNode = translate(bp->expression);
 
-                            if (supportedCallType) {
+                            if (true) {
                                 enforceHasExpr(blockPassArgNode);
 
                                 blockPassArg = blockPassArgNode->takeDesugaredExpr();
@@ -1953,6 +1952,7 @@ unique_ptr<parser::Node> Translator::translate(pm_node_t *node) {
                 supportedBlock = true;
             }
 
+            auto supportedCallType = true;
             supportedCallType &= supportedBlock;
 
             if (PM_NODE_FLAG_P(callNode, PM_CALL_NODE_FLAGS_SAFE_NAVIGATION)) {
