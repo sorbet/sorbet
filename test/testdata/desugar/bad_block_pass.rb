263e3b1653cb2825b1cc56c0bc988621fd2aad9f
Jake Zimmerman <zimmerman.jake@gmail.com>
2025-12-04 15:46:41 -0800

    Forgot to add error assertions

--- test/testdata/desugar/bad_block_pass.rb
+++ test/testdata/desugar/bad_block_pass.rb
@@ -3,7 +3,16 @@
 def a(...); end
 
 a[&blk] = 1
+# ^^^^ error: Unsupported block pass node in non-final argument
 a.[]=(&blk, 1)
+#     ^^^^ error: Unsupported block pass node in non-final argument
+#         ^ error: Unexpected token ","
 a(&blk, 1)
+# ^^^^ error: Unsupported block pass node in non-final argument
 a.[]=(0, &blk, 1)
+#        ^^^^ error: Unsupported block pass node in non-final argument
+#            ^ error: Unexpected token ","
+# ^^^^ error: Unsupported block pass node in non-final argument
 a(0, &blk, 1)
+#    ^^^^ error: Unsupported block pass node in non-final argument
+#        ^ error: Unexpected token ","
