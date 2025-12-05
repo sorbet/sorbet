e76004ca301384f0b27c1e397c58d289eeff0e41
Alexander Momchilov <alexander.momchilov@shopify.com>
2025-12-05 15:24:50 -0500

    Bugfix: check for `self` on desugared tree
    the `sendNode->receiver` has already been moved out by this point, so it's always `nullptr`.

--- ast/desugar/Desugar.cc
+++ ast/desugar/Desugar.cc
@@ -707,7 +707,7 @@ bool isCallToBlockGivenP(parser::Send *sendNode, ast::ExpressionPtr &receiverExp
         return false;
     }
 
-    if (sendNode->receiver == nullptr || parser::isa_node<parser::Self>(sendNode->receiver.get())) {
+    if (receiverExpr == nullptr || receiverExpr.isSelfReference()) {
         return true;
     }
 
