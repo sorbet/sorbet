522c65ae8e4c5aeee560d88e052030509a6c5b19
Jake Zimmerman <zimmerman.jake@gmail.com>
2025-12-05 11:45:48 -0800

    Fix tests

--- test/testdata/desugar/bad_block_pass.rb
+++ test/testdata/desugar/bad_block_pass.rb
@@ -6,13 +6,13 @@ a[&blk] = 1
 # ^^^^ error: Unsupported block pass node in non-final argument
 a.[]=(&blk, 1)
 #     ^^^^ error: Unsupported block pass node in non-final argument
-#         ^ error: Unexpected token ","
+#         ^ error: unexpected token ","
 a(&blk, 1)
 # ^^^^ error: Unsupported block pass node in non-final argument
+#     ^ error: unexpected token ","
 a.[]=(0, &blk, 1)
 #        ^^^^ error: Unsupported block pass node in non-final argument
-#            ^ error: Unexpected token ","
-# ^^^^ error: Unsupported block pass node in non-final argument
+#            ^ error: unexpected token ","
 a(0, &blk, 1)
 #    ^^^^ error: Unsupported block pass node in non-final argument
-#        ^ error: Unexpected token ","
+#        ^ error: unexpected token ","
