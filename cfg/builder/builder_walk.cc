d02395da19c9d5a754259680319e3eed01476e45
Jake Zimmerman <zimmerman.jake@gmail.com>
2025-12-05 18:39:50 -0800

    Move this method to the new helper

--- cfg/builder/builder_walk.cc
+++ cfg/builder/builder_walk.cc
@@ -128,38 +128,6 @@ bool isKernelLambda(ast::Send &s) {
     return cnst != nullptr && cnst->symbol() == core::Symbols::Kernel();
 }
 
-void noInstantiateAbstract(core::Context ctx, const ast::Send &s) {
-    if (s.fun != core::Names::new_()) {
-        return;
-    }
-
-    auto id = ast::cast_tree<ast::ConstantLit>(s.recv);
-    if (id == nullptr || !id->symbol().exists() || !id->symbol().isClassOrModule()) {
-        return;
-    }
-
-    auto symbol = id->symbol().asClassOrModuleRef().data(ctx);
-    if (!symbol->flags.isAbstract) {
-        return;
-    }
-
-    auto singletonClass = symbol->lookupSingletonClass(ctx.state);
-    if (!singletonClass.exists()) {
-        return;
-    }
-
-    auto method_new = singletonClass.data(ctx)->findMethodTransitive(ctx.state, core::Names::new_());
-    // If the .new method we find is owned by Class, that means
-    // there was no user defined .new method, which warrants an error.
-    if (method_new.data(ctx)->owner == core::Symbols::Class()) {
-        if (auto e = ctx.beginError(s.loc, core::errors::Resolver::AbstractClassInstantiated)) {
-            auto symbolName = id->symbol().show(ctx);
-            e.setHeader("Attempt to instantiate abstract class `{}`", symbolName);
-            e.addErrorLine(id->symbol().loc(ctx), "`{}` defined here", symbolName);
-        }
-    }
-}
-
 InstructionPtr maybeMakeTypeParameterAlias(CFGContext &cctx, ast::Send &s) {
     const auto &ctx = cctx.ctx;
     auto method = cctx.inWhat.symbol;
@@ -356,6 +324,33 @@ BasicBlock *CFGBuilder::handleSpecialMethods(CFGContext cctx, BasicBlock *curren
             }
             break;
         }
+        case core::Names::new_().rawId(): {
+    auto id = ast::cast_tree<ast::ConstantLit>(s.recv);
+    if (id == nullptr || !id->symbol().exists() || !id->symbol().isClassOrModule()) {
+        return;
+    }
+
+    auto symbol = id->symbol().asClassOrModuleRef().data(ctx);
+    if (!symbol->flags.isAbstract) {
+        return;
+    }
+
+    auto singletonClass = symbol->lookupSingletonClass(ctx.state);
+    if (!singletonClass.exists()) {
+        return;
+    }
+
+    auto method_new = singletonClass.data(ctx)->findMethodTransitive(ctx.state, core::Names::new_());
+    // If the .new method we find is owned by Class, that means
+    // there was no user defined .new method, which warrants an error.
+    if (method_new.data(ctx)->owner == core::Symbols::Class()) {
+        if (auto e = ctx.beginError(s.loc, core::errors::Resolver::AbstractClassInstantiated)) {
+            auto symbolName = id->symbol().show(ctx);
+            e.setHeader("Attempt to instantiate abstract class `{}`", symbolName);
+            e.addErrorLine(id->symbol().loc(ctx), "`{}` defined here", symbolName);
+        }
+    }
+                                          }
     }
 }
 
