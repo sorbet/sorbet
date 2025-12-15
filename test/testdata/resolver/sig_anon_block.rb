b0cd31e52002a69cf6334acc1c798666fc976e8a
Jake Zimmerman <zimmerman.jake@gmail.com>
2025-12-15 17:04:24 -0500

    Allow `&` to specify anonymous block parameter types

--- test/testdata/resolver/sig_anon_block.rb
+++ test/testdata/resolver/sig_anon_block.rb
@@ -11,9 +11,7 @@ def takes_block(&blk)
 end
 
 sig {params("&": T.proc.returns(Integer)).void}
-#           ^^^^ error: Unknown parameter name `&`
 def foo(&)
-  #     ^ error: Malformed `sig`. Type not specified for parameter `&`
-  res = takes_block(&) # error: Expected `T.proc.returns(T.anything)` but found `NilClass` for block argument
-  T.reveal_type(res) # error: `T.untyped`
+  res = takes_block(&)
+  T.reveal_type(res) # error: `Integer`
 end
