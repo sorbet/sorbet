bcec0040ad2588854572acdeb94c31d2efeed651
Jake Zimmerman <zimmerman.jake@gmail.com>
2025-12-16 17:23:42 -0800

    Add failing test

--- test/testdata/infer/autocorrect_decl_loc.rb
+++ test/testdata/infer/autocorrect_decl_loc.rb
@@ -18,3 +18,17 @@ def multiline_bad_arg(
     #       ^^^^^^^^^^^^^^^^ error: Changing the type of a variable is not permitted in loops and blocks
   end
 end
+
+sig {params(klass: Class).void}
+def bad_arg_defined_twice(klass)
+  loop do
+    klass = klass.superclass
+    #       ^^^^^^^^^^^^^^^^ error: Changing the type of a variable is not permitted in loops and blocks
+  end
+end
+def bad_arg_defined_twice(klass)
+  loop do
+    klass = klass.superclass
+    #       ^^^^^^^^^^^^^^^^ error: Changing the type of a variable is not permitted in loops and blocks
+  end
+end
