f70aa220688611713f426715ccb231e5e73f2805
Trevor Elliott <awesomelyawesome@gmail.com>
2025-12-19 15:44:41 -0800

    Update existing tests

--- test/testdata/packager/nested_package_enforcement/inner/inner.rb
+++ test/testdata/packager/nested_package_enforcement/inner/inner.rb
@@ -14,4 +14,5 @@ module Outer
 
   include Inner::Mixin
 # ^^^^^^^^^^^^^^^^^^^^ error: This file must only define behavior in enclosing package `Outer::Inner`
+#         ^^^^^^^^^^^^ error: Package namespace `Outer` may not have mixins applied
 end
