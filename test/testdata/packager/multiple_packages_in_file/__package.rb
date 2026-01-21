09e610e3aa5e3e0d77f517c54abf7f4a54c0a0a2
Trevor Elliott <awesomelyawesome@gmail.com>
2026-01-21 13:39:41 -0800

    Update tests for superclass check

--- test/testdata/packager/multiple_packages_in_file/__package.rb
+++ test/testdata/packager/multiple_packages_in_file/__package.rb
@@ -6,4 +6,5 @@ class MyPackage < PackageSpec
 end
 
 class SecondPackage < PackageSpec; end # error: Invalid expression in package: `ClassDef` not allowed
+#                     ^^^^^^^^^^^ error: Superclasses may only be set on constants in the package that owns them
 #                     ^^^^^^^^^^^ error: Unable to resolve constant `PackageSpec`
