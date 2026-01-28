a5e80fe639357a13e171dbccb5b95d96ec542de6
Neil Parikh <parikh.neil@me.com>
2026-01-28 16:43:21 -0500

    aggregate missing visible_tos

--- core/packages/PackageInfo.h
+++ core/packages/PackageInfo.h
@@ -274,6 +274,8 @@ public:
     std::optional<core::AutocorrectSuggestion> aggregateMissingImports(const core::GlobalState &gs) const;
     std::optional<core::AutocorrectSuggestion> aggregateMissingExports(const core::GlobalState &gs,
                                                                        std::vector<core::SymbolRef> &toExport) const;
+    std::optional<core::AutocorrectSuggestion>
+    aggregateMissingVisibleTo(const core::GlobalState &gs, std::vector<core::packages::MangledName> &visibleTos) const;
 };
 CheckSize(PackageInfo, 240, 8);
 
