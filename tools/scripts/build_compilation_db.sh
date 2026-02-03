de16ece2dde670b96fafc4e132db3c321808e8ca
Jake Zimmerman <zimmerman.jake@gmail.com>
2026-02-03 15:10:19 -0800

    Add `--remote_download_outputs=all` to `--config=dbg` (#9872)
    This makes clangd work again.
    
    We recently upgraded to Bazel 7 (see 0e3ed53dfd3caac88496ebca1ce714623e65e952).
    In that release, they flipped the default of
    `--remote_download_outputs`, which broke our `clangd` integration
    depending on whether certain intermediate outputs were computed from
    scratch or downloaded from the cache.
    
    See <https://blog.bazel.build/2023/12/11/bazel-7-release.html#build-without-the-bytes-bwob>

--- tools/scripts/build_compilation_db.sh
+++ tools/scripts/build_compilation_db.sh
@@ -4,6 +4,19 @@ set -e
 
 cd "$(dirname "$0")/../.."
 
+#   NOTE on `--config=dbg` and `--remote_download_outputs=all`
+#
+# We assume that you'll be iterating locally with `--config=dbg` builds.
+# If you choose to use something else, be sure to:
+#
+# - edit this script and then (re-)generat the compile_commands.json file
+# - pass `--remote_download_outputs=all` when building
+#
+# (The `--config=dbg` configuration already passes that flag.) Without this
+# flag, intermediate generated outputs will not be placed in the `bazel-bin/`
+# folder if they have been cached, which means that clangd won't find them.
+#
+# TODO(jez) Do other compile commands generators work better here?
 ./bazel build //tools:compdb --config=dbg "$@"
 
 if command -v jq &> /dev/null; then
