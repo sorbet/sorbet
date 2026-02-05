2b829fe3ca119925e0d063357e68f9a5502222ab
Jake Zimmerman <zimmerman.jake@gmail.com>
2026-02-04 16:51:45 -0800

    Add a test

---
+++ test/testdata/packager/critic/lib/foo/bar/__package.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+# typed: strict
+# enable-packager: true
+
+class Opus::Treasury::Sweeps < PackageSpec
+  import Critic::Helpers
+end
---
+++ test/testdata/packager/critic/test/_lib/helpers/zip.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+# typed: strict
+# fast_load: true
+require_relative "../../../init_autoloader"
+
+module Critic
+  module Helpers
+    module Zip
+      T::Sig::WithoutRuntime.sig {void}
+      def assert_zip_contents
+      end
+    end
+  end
+end
