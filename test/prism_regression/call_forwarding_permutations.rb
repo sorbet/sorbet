07001f3e263d87e01f4e8e412466815be2b85177
Jesse Johnson <jesse.johnson@shopify.com>
2026-02-12 12:27:42 -0800

    better document tests of various block permutations

--- test/prism_regression/call_forwarding_permutations.rb
+++ test/prism_regression/call_forwarding_permutations.rb
@@ -2,19 +2,23 @@
 
 # This file tests combinations of block forwarding and literal blocks.
 #
-# Summary of 8 combinations:
+# Summary of 12 combinations:
 #   Valid:
-#     1. None           - def f; bar; end
-#     2. Just &         - def f(&b); bar(&b); end
-#     3. Just ...       - def f(...); bar(...); end
-#     4. Just { }       - def f; bar { }; end
-#     5. ... + { }      - def f(...); bar(...) { }; end
-#     6. & + { }        - def f(&b); bar(&b) { }; end
+#     1. None                 - def f; bar; end
+#     2. &                    - def f(&b); bar(&b); end
+#     3. & (anonymous)        - def f(&); bar(&); end
+#     4. ...                  - def f(...); bar(...); end
+#     5. { }                  - def f; bar { }; end
+#     6. ... + { }            - def f(...); bar(...) { }; end
+#     7. & + { }              - def f(&b); bar(&b) { }; end
+#     8. & (anonymous) + { }  - def f(&); bar(&) { }; end
 #   Error cases (definition - cannot be tested, see below):
-#     7. & + ...        - def f(&, ...); end
-#     8. & + ... + { }  - def f(&, ...); bar(...) { }; end
+#     9. & + ...              - def f(&b, ...); end
+#    10. & (anonymous) + ...  - def f(&, ...); end
+#    11. & + ... + { }        - def f(&b, ...); bar(...) { }; end
+#    12. & (anonymous) + ... + { } - def f(&, ...); bar(...) { }; end
 
-# Cases 7, 7a, 8, 8a are excluded from this test.
+# Cases 9-12 are excluded from this test.
 #
 # These cases have `&` and `...` together in the definition, which is a parser
 # error. The error recovery is fundamentally different between parsers:
@@ -23,6 +27,7 @@
 #
 # Since this tests parser error recovery (not desugaring), these cases cannot
 # be meaningfully compared between parsers.
+# Tests for these cases are in test/testdata/parser/error_recovery/block_forwarding_invalid_def.rb.
 
 # ==============================================================================
 # VALID CASES - These should produce identical output
@@ -38,37 +43,37 @@ def case2_block_param(&block)
   bar(&block)
 end
 
-# Case 2a: Anonymous block parameter
-def case2a_anonymous_block_param(&)
+# Case 3: Anonymous block parameter
+def case3_anonymous_block_param(&)
   bar(&)
 end
 
-# Case 3: Just ... - Forwarding only
-def case3_forwarding(...)
+# Case 4: Just ... - Forwarding only
+def case4_forwarding(...)
   bar(...)
 end
 
-# Case 4: Just { } - Block literal only
-def case4_block_literal
+# Case 5: Just { } - Block literal only
+def case5_block_literal
   bar { "block body" }
 end
 
-# CASE 5: ... + { } at call site
+# CASE 6: ... + { } at call site
 # Forwarding includes <fwd-block>, AND there's a literal block.
 # Both should be kept in the output.
-def case5_forwarding_and_literal(...)
+def case6_forwarding_and_literal(...)
   foo(...) { "literal block" }
 end
 
-# CASE 6: & + { } at call site
+# CASE 7: & + { } at call site
 # Block pass argument AND a literal block.
 # Both should be kept in the output.
-def case6_block_pass_and_literal(&block)
+def case7_block_pass_and_literal(&block)
   foo(&block) { "literal block" }
 end
 
-# Case 6a: Anonymous block param with literal block
-def case6a_anonymous_block_pass_and_literal(&)
+# Case 8: Anonymous block param with literal block
+def case8_anonymous_block_pass_and_literal(&)
   foo(&) { "literal block" }
 end
 
@@ -76,22 +81,22 @@ end
 # ERROR CASES - These cannot be tested, see above
 # ==============================================================================
 
-# Case 7: & + ... in definition (named block param)
-# def case7_block_param_and_forwarding(&block, ...)
+# Case 9: & + ... in definition (named block param)
+# def case9_block_param_and_forwarding(&block, ...)
 #   bar(...)
 # end
 
-# Case 7a: & + ... in definition (anonymous block param)
-# def case7a_anonymous_block_param_and_forwarding(&, ...)
+# Case 10: & + ... in definition (anonymous block param)
+# def case10_anonymous_block_param_and_forwarding(&, ...)
 #   bar(...)
 # end
 
-# Case 8: & + ... + { } (all three, named block param)
-# def case8_all_three(&block, ...)
+# Case 11: & + ... + { } (all three, named block param)
+# def case11_all_three(&block, ...)
 #   foo(...) { "literal block" }
 # end
 
-# Case 8a: & + ... + { } (all three, anonymous block param)
-# def case8a_all_three_anonymous(&, ...)
+# Case 12: & + ... + { } (all three, anonymous block param)
+# def case12_all_three_anonymous(&, ...)
 #   foo(...) { "literal block" }
 # end
--- test/testdata/parser/block_forwarding_permutations.rb
+++ test/testdata/parser/block_forwarding_permutations.rb
@@ -1,56 +1,57 @@
 # typed: false
 
 # This file documents combinations of block forwarding.
+# For more details, see test/prism_regression/call_forwarding_permutations.rb.
 #
-# 7 valid/error cases:
-#   1. None           - def f; bar; end
-#   2. Just & (named) - def f(&b); bar(&b); end
-#   2a. Just & (anon) - def f(&); bar(&); end
-#   3. Just ...       - def f(...); bar(...); end
-#   4. Just { }       - def f; bar { }; end
-#   5. ... + { }      - def f(...); bar(...) { }; end  (error: both block arg and literal)
-#   6. & + { } (named)- def f(&b); bar(&b) { }; end   (error: both block arg and literal)
-#   6a. & + { } (anon)- def f(&); bar(&) { }; end     (error: both block arg and literal)
+# 8 valid/error cases:
+#   1. None                 - def f; bar; end
+#   2. &                    - def f(&b); bar(&b); end
+#   3. & (anonymous)        - def f(&); bar(&); end
+#   4. ...                  - def f(...); bar(...); end
+#   5. { }                  - def f; bar { }; end
+#   6. ... + { }            - def f(...); bar(...) { }; end  (error: both block arg and literal)
+#   7. & + { }              - def f(&b); bar(&b) { }; end   (error: both block arg and literal)
+#   8. & (anonymous) + { }  - def f(&); bar(&) { }; end     (error: both block arg and literal)
 
 # Case 1: None
 def case1_none
   bar
 end
 
-# Case 2: Just & (named)
+# Case 2: Just & - Explicit block parameter
 def case2_block_param(&block)
   bar(&block)
 end
 
-# Case 2a: Just & (anonymous)
-def case2a_anonymous_block_param(&)
+# Case 3: Anonymous block parameter
+def case3_anonymous_block_param(&)
   bar(&)
 end
 
-# Case 3: Just ...
-def case3_forwarding(...)
+# Case 4: Just ... - Forwarding only
+def case4_forwarding(...)
   bar(...)
 end
 
-# Case 4: Just { }
-def case4_block_literal
+# Case 5: Just { } - Block literal only
+def case5_block_literal
   bar { "block body" }
 end
 
-# Case 5: ... + { }
-def case5_forwarding_and_literal(...)
+# Case 6: ... + { }
+def case6_forwarding_and_literal(...)
   foo(...) { "literal block" }
 #     ^^^ error: both block argument and literal block are passed
 end
 
-# Case 6: & + { } (named)
-def case6_block_pass_and_literal(&block)
+# Case 7: & + { }
+def case7_block_pass_and_literal(&block)
   foo(&block) { "literal block" }
 #     ^^^^^^ error: both block argument and literal block are passed
 end
 
-# Case 6a: & + { } (anonymous)
-def case6a_anonymous_block_pass_and_literal(&)
+# Case 8: & (anonymous) + { }
+def case8_anonymous_block_pass_and_literal(&)
   foo(&) { "literal block" }
 #     ^ error: both block argument and literal block are passed
 end
