4924aef8c4a70e52096123b75613d1e8692ffca7
Trevor Elliott <awesomelyawesome@gmail.com>
2026-02-19 10:16:26 -0800

    Fix testSCCId setting

--- test/testdata/packager/test-packages-strict-deps-layer/__package.rb
+++ test/testdata/packager/test-packages-strict-deps-layer/__package.rb
@@ -3,6 +3,8 @@
 # enable-test-packages: true
 # packager-layers: a
 
-class Root < PackageSpec
+  class Root < PackageSpec
+# ^^^^^^^^^^^^^^^^^^^^^^^^ error: This package does not declare a `layer`
+# ^^^^^^^^^^^^^^^^^^^^^^^^ error: This package does not declare a `strict_dependencies` level
   export Root::A
 end
