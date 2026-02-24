3332ca0400e8dd87a27e65fe232f90b3b965cf0a
Trevor Elliott <awesomelyawesome@gmail.com>
2026-02-24 15:25:30 -0800

    Require strict_dependencies and layer in test packages (#9921)
    * Require strict_dependencies and layer in test packages
    
    * Update tests

--- test/testdata/packager/test-packages-strict-deps-layer/test/__package.rb
+++ test/testdata/packager/test-packages-strict-deps-layer/test/__package.rb
@@ -1,6 +1,8 @@
 # typed: strict
 
-class Test::Root < PackageSpec
+  class Test::Root < PackageSpec
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ error: This package does not declare a `layer`
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ error: This package does not declare a `strict_dependencies` level
   test!
 
   import Root, uses_internals: true
