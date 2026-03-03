e68085712a8e5b1dd39eecad4ef685718bc5b909
Neil Parikh <parikh.neil@me.com>
2026-03-02 19:26:21 -0500

    Add a test case for running `--gen-packages` with `--package-directed` (#9941)
    * add gen-packages invocation with --experimental-package-directed
    
    * test case for different behaviour

--- test/cli/package-gen-packages/test.sh
+++ test/cli/package-gen-packages/test.sh
@@ -15,7 +15,29 @@ cd "$tmp" || exit 1
 
 "$cwd/main/sorbet" --max-threads=0 --silence-dev-message --lsp --sorbet-packages --gen-packages --packager-layers=util,app -a . 2>&1
 
-"$cwd/main/sorbet" --max-threads=0 --silence-dev-message --sorbet-packages --gen-packages --packager-layers=util,app  --gen-packages-update-visibility-for=B -a . 2>&1
+echo "------- Non package directed -------"
+
+"$cwd/main/sorbet" --did-you-mean=false --max-threads=0 --silence-dev-message --sorbet-packages --gen-packages --packager-layers=util,app  --gen-packages-update-visibility-for=B -a . 2>&1
+
+cat B/__package.rb
+
+cat C/__package.rb
+
+cat D/__package.rb
+
+tmp="$(mktemp -d)"
+cd "$cwd" || exit 1
+cd test/cli/package-gen-packages || exit 1
+for file in $(find . -name '*.rb' | sort); do
+    mkdir -p "$tmp/$(dirname "$file")"
+    cp "$file" "$tmp/$file"
+done
+cd "$tmp" || exit 1
+
+
+echo "------- Package directed -------"
+
+"$cwd/main/sorbet" --max-threads=0 --did-you-mean=false --silence-dev-message --sorbet-packages --experimental-package-directed --gen-packages --packager-layers=util,app  --gen-packages-update-visibility-for=B -a . 2>&1
 
 cat B/__package.rb
 
