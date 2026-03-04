ea0dfbf6f737dd9ca913fcc1f145bafe855e254f
Trevor Elliott <awesomelyawesome@gmail.com>
2026-03-03 16:13:05 -0800

    Only run sorbet once in the cli test

--- test/cli/autocorrect-package-export-declared/test.sh
+++ test/cli/autocorrect-package-export-declared/test.sh
@@ -13,10 +13,6 @@ cp -r "${sources[@]}" "$dir"
 
 "$root/main/sorbet" \
   --censor-for-snapshot-tests --silence-dev-message --max-threads=0 \
-  --sorbet-packages "$dir" 2>&1
-
-"$root/main/sorbet" \
-  --censor-for-snapshot-tests --silence-dev-message --max-threads=0 \
   --sorbet-packages -a "$dir" 2>&1
 
 "$root/main/sorbet" \
