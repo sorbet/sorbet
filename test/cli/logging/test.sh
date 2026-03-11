0635a4554c40c188b4e04e3e4dc350c592036d4a
Trevor Elliott <awesomelyawesome@gmail.com>
2026-03-11 15:19:20 -0700

    Change logging format to logfmt (#9984)
    * Only change the log format for the file sink
    
    * Update logging test

--- test/cli/logging/test.sh
+++ test/cli/logging/test.sh
@@ -1,9 +1,10 @@
 LOG_FILE=$(mktemp)
-main/sorbet --silence-dev-message -e '1' -q --debug-log-file="$LOG_FILE"
-echo LOG BEGINS
+echo "STDOUT BEGINS"
+main/sorbet --silence-dev-message -e '1' --debug-log-file="$LOG_FILE" 2>&1
+echo "DEBUG LOG BEGINS"
 # only keep message parts, drop timings and the entire counter section
-sed -E "s/\\[[0-9]+\\-[0-9]+-[0-9]+ [0-9]+:[0-9]+:[0-9]+.[0-9]+\\]/TIMESTAMP/"< "$LOG_FILE" |  # replace timestamps
-  grep 'TIMESTAMP'                                                                       |  # Only give first lines
-  grep -Eve ': [0-9]+\.?[0-9]*(e(-|\+))?[0-9]*ms$'                                                       |  # remove timings
-  grep -v debug-log-file                                                                     # remove header line that contains generated log name
+sed -E "s/[0-9]+\\-[0-9]+-[0-9]+T[0-9]+:[0-9]+:[0-9]+.[0-9]+/TIMESTAMP/"< "$LOG_FILE" |  # replace timestamps
+  grep 'TIMESTAMP'                                                                    |  # Only give first lines
+  grep -Eve ': [0-9]+\.?[0-9]*(e(-|\+))?[0-9]*ms$'                                    |  # remove timings
+  grep -v debug-log-file                                                                 # remove header line that contains generated log name
 rm "$LOG_FILE"
