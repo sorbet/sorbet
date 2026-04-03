e37869b59bb7dd44a184e514d74fc0f80a2c18d2
Jake Zimmerman <zimmerman.jake@gmail.com>
2026-04-03 11:07:13 -0700

    Not wip anymore

--- test/cli/scc-path-crash/test.sh
+++ test/cli/scc-path-crash/test.sh
@@ -1,3 +1,3 @@
-cd test/cli/wip-scc-path-crash || exit 1
+cd test/cli/scc-path-crash || exit 1
 
 ../../../main/sorbet --silence-dev-message --sorbet-packages --packager-layers --max-threads=0 . 2>&1
