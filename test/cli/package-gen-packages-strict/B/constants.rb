e6dde58b978765476e8fb85c01adee7b0b86daa4
Neil Parikh <parikh.neil@me.com>
2026-04-06 18:04:56 -0400

    changes to test files to preserve existing output

--- test/cli/package-gen-packages-strict/B/constants.rb
+++ test/cli/package-gen-packages-strict/B/constants.rb
@@ -2,4 +2,6 @@
 
 module B
   CONSTANT_FROM_B = "Hello from Package B"
+  puts A::CONSTANT_FROM_A
+  puts C::CONSTANT_FROM_C
 end
---
+++ test/cli/package-gen-packages-strict/C/constants.rb
@@ -0,0 +1,6 @@
+# typed: strict
+
+module C
+  CONSTANT_FROM_C = "Hello from Package C"
+  puts B::CONSTANT_FROM_B
+end
