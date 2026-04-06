b413c1994bffbeced80c939a3d66d7029b489623
Jake Zimmerman <zimmerman.jake@gmail.com>
2026-04-06 13:06:50 -0700

    Move `ENFORCE` to the top of the loop
    At the start of the loop `nested` is `this` and we already exited early
    from `fullUnresolvedPath` if `this->symbol()` was not `StubModule`.

--- ast/Trees.cc
+++ ast/Trees.cc
@@ -353,6 +353,8 @@ optional<pair<core::SymbolRef, vector<core::NameRef>>> ConstantLit::fullUnresolv
     vector<core::NameRef> namesFailedToResolve;
     auto *nested = this;
     while (true) {
+        ENFORCE(nested->symbol() == core::Symbols::StubModule());
+
         if (nested->resolutionScopes() == nullptr || nested->resolutionScopes()->empty()) [[unlikely]] {
             ENFORCE(false);
             bool hasScopes = nested->resolutionScopes() != nullptr;
@@ -370,7 +372,6 @@ optional<pair<core::SymbolRef, vector<core::NameRef>>> ConstantLit::fullUnresolv
 
         nested = ast::cast_tree<ast::ConstantLit>(orig.scope);
         ENFORCE(nested);
-        ENFORCE(nested->symbol() == core::Symbols::StubModule());
     }
     auto prefix = nested->resolutionScopes()->front();
     return make_pair(prefix, move(namesFailedToResolve));
