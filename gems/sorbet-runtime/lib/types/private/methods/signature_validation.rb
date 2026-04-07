e32682828585de84d88a7081f3b37e9cbcca1083
Ufuk Kayserilioglu <ufuk.kayserilioglu@shopify.com>
2026-04-07 22:21:40 +0300

    Start using `effective_return_type` in all validations
    Now that `effective_return_type` is the field that holds the return type
    that validations should work against, we should change all validation
    logic to use that.

--- gems/sorbet-runtime/lib/types/private/methods/signature_validation.rb
+++ gems/sorbet-runtime/lib/types/private/methods/signature_validation.rb
@@ -262,17 +262,17 @@ module T::Private::Methods::SignatureValidation
     end
 
     # return types must be covariant
-    super_signature_return_type = super_signature.return_type
+    super_signature_return_type = super_signature.effective_return_type
 
     if super_signature_return_type == T::Private::Types::Void::Private::INSTANCE
       # Treat `.void` as `T.anything` (see corresponding comment in definition_valitor for more)
       super_signature_return_type = T::Types::Anything::Private::INSTANCE
     end
 
-    if !signature.return_type.subtype_of?(super_signature_return_type)
+    if !signature.effective_return_type.subtype_of?(super_signature_return_type)
       raise "Incompatible return type in signature for #{mode_noun} of method `#{signature.method_name}`:\n" \
-            "* Base: `#{super_signature.return_type}` (in #{method_loc_str(super_signature.method)})\n" \
-            "* #{mode_noun.capitalize}: `#{signature.return_type}` (in #{method_loc_str(signature.method)})\n" \
+            "* Base: `#{super_signature.effective_return_type}` (in #{method_loc_str(super_signature.method)})\n" \
+            "* #{mode_noun.capitalize}: `#{signature.effective_return_type}` (in #{method_loc_str(signature.method)})\n" \
             "(The types must be covariant.)"
     end
   end
