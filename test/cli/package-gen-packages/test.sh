692bb148c0842c526e84c769154d1ff45bc14dd2
Neil Parikh <parikh.neil@me.com>
2026-04-09 16:52:29 -0400

    add headers to the gen-packages CLI test

--- test/cli/package-gen-packages/test.sh
+++ test/cli/package-gen-packages/test.sh
@@ -10,22 +10,44 @@ for file in $(find . -name '*.rb' | sort); do
 done
 cd "$tmp" || exit 1
 
+echo "###### --gen-packages not allowed without --sorbet-packages ######"
 
 "$cwd/main/sorbet" --max-threads=0 --silence-dev-message --gen-packages --packager-layers=util,app -a . 2>&1
 
+echo "###### --gen-packages not allowed with --lsp ######"
+
 "$cwd/main/sorbet" --max-threads=0 --silence-dev-message --lsp --sorbet-packages --gen-packages --packager-layers=util,app -a . 2>&1
 
-echo "------- Non package directed -------"
+echo "##################################"
+echo "###### Non package directed ######"
+echo "##################################"
+
+echo
+echo "###### Running gen-packages ######"
 
 "$cwd/main/sorbet" --did-you-mean=false --max-threads=0 --silence-dev-message --sorbet-packages --gen-packages --packager-layers=util,app  --gen-packages-update-visibility-for=B --gen-packages-update-visibility-for=E --gen-packages-allow-relaxing-test-visibility -a . 2>&1
 
+echo
+echo "###### cat B/__package.rb ######"
 cat B/__package.rb
 
+echo
+echo "###### cat C/__package.rb ######"
 cat C/__package.rb
 
+echo
+echo "###### cat D/__package.rb ######"
 cat D/__package.rb
 
+echo
+echo "###### cat E/__package.rb ######"
 cat E/__package.rb
+
+
+
+echo "##############################"
+echo "###### Package directed ######"
+echo "##############################"
 
 tmp="$(mktemp -d)"
 cd "$cwd" || exit 1
@@ -36,15 +58,23 @@ for file in $(find . -name '*.rb' | sort); do
 done
 cd "$tmp" || exit 1
 
-
-echo "------- Package directed -------"
+echo
+echo "###### Running gen-packages ######"
 
 "$cwd/main/sorbet" --max-threads=0 --did-you-mean=false --silence-dev-message --sorbet-packages --experimental-package-directed --gen-packages --packager-layers=util,app  --gen-packages-update-visibility-for=B --gen-packages-update-visibility-for=E --gen-packages-allow-relaxing-test-visibility -a . 2>&1
 
+echo
+echo "###### cat B/__package.rb ######"
 cat B/__package.rb
 
+echo
+echo "###### cat C/__package.rb ######"
 cat C/__package.rb
 
+echo
+echo "###### cat D/__package.rb ######"
 cat D/__package.rb
 
+echo
+echo "###### cat E/__package.rb ######"
 cat E/__package.rb
