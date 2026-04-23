2fc1f8749bfe7b38e7fec90711ae5e83316853c5
Getty Ritter <genburi@infinitenegativeutility.com>
2026-04-23 10:48:23 -0700

    single comment change

--- test/testdata/packager/unpackaged/unpackaged-code.rb
+++ test/testdata/packager/unpackaged/unpackaged-code.rb
@@ -5,6 +5,8 @@
 # Despite this being in a packaged context, this constant is
 # unpackaged and therefore can be freely used by packaged code.
 module UnpackagedCode
+  # Referencing packaged constants in unpackaged code is not allowed,
+  # though
   MyPackage::MyClass
 # ^^^^^^^^^^^^^^^^^^ error: `MyPackage::MyClass` is defined in a package and cannot
 end
