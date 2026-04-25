b6d6af50041b5d1e9d39864f0ff8b0ee5495ac02
Jake Zimmerman <zimmerman.jake@gmail.com>
2026-04-24 19:00:46 -0700

    Get shellcheck on all platforms

--- tools/scripts/lint_sh.sh
+++ tools/scripts/lint_sh.sh
@@ -8,25 +8,21 @@ list_sh_src() {
   git ls-files -z -c -m -o --exclude-standard -- '*.sh' '*.bash'
 }
 
-
-unameOut="$(uname -s)"
-case "${unameOut}" in
-    Linux*)     platform="linux";;
-    Darwin*)    platform="mac";;
-    *)          exit 1
-esac
+kernel_name="$(uname -s | tr '[:upper:]' '[:lower:]')"
+march="$(uname -m)"
+shellcheck_pkg="shellcheck_${kernel_name}_${march}"
 
 # Fetches shellcheck binary
 bazel_build="$(mktemp)"
 trap 'rm -f "$bazel_build"' EXIT
-if ! bazel build "@shellcheck_$platform//:shellcheck_exe" &> "$bazel_build"; then
+if ! bazel build "@$shellcheck_pkg//:shellcheck_exe" &> "$bazel_build"; then
   echo "warning: Could not fetch shellcheck with Bazel. Falling back to system"
   echo '```'
   cat "$bazel_build"
   echo '```'
   shellcheck="shellcheck"
 else
-  shellcheck="$(bazel info output_base 2> /dev/null)/external/shellcheck_$platform/shellcheck"
+  shellcheck="$(bazel info output_base 2> /dev/null)/external/$shellcheck_pkg/shellcheck"
 fi
 
 "$shellcheck" --version
