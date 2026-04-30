df04505fa30e3684bb45e202e6786db9d6a98148
Neil Parikh <parikh.neil@me.com>
2026-04-30 14:13:10 -0400

    look at counters for cache misses and hits

--- test/cli/package-mixed-cache/test.sh
+++ test/cli/package-mixed-cache/test.sh
@@ -12,15 +12,31 @@ cd "$tmp" || exit 1
 
 
 # Create the cache in monolithic mode
-"$cwd/main/sorbet" --silence-dev-message --sorbet-packages --max-threads=0 --cache-dir=cache . 2>&1
+"$cwd/main/sorbet" --silence-dev-message --sorbet-packages --max-threads=0 --cache-dir=cache --counters . > output.txt 2>&1
+
+cat output.txt | grep "No errors! Great job."
+
+cat output.txt | grep kvstore
 
 # Consume the cache in package-directed mode
-"$cwd/main/sorbet" --silence-dev-message --sorbet-packages --experimental-package-directed --max-threads=0 --cache-dir=cache . 2>&1
+"$cwd/main/sorbet" --silence-dev-message --sorbet-packages --experimental-package-directed --max-threads=0 --cache-dir=cache --counters . > output.txt 2>&1
+
+cat output.txt | grep "No errors! Great job."
+
+cat output.txt | grep kvstore
 
 rm -rf cache
 
 # Create the cache in package-directed mode
-"$cwd/main/sorbet" --silence-dev-message --sorbet-packages --experimental-package-directed --max-threads=0 --cache-dir=cache . 2>&1
+"$cwd/main/sorbet" --silence-dev-message --sorbet-packages --experimental-package-directed --max-threads=0 --cache-dir=cache --counters . > output.txt 2>&1
+
+cat output.txt | grep "No errors! Great job."
+
+cat output.txt | grep kvstore
 
 # Consume the cache in monolithic mode
-"$cwd/main/sorbet" --silence-dev-message --sorbet-packages --max-threads=0 --cache-dir=cache . 2>&1
+"$cwd/main/sorbet" --silence-dev-message --sorbet-packages --max-threads=0 --cache-dir=cache --counters . > output.txt 2>&1
+
+cat output.txt | grep "No errors! Great job."
+
+cat output.txt | grep kvstore
