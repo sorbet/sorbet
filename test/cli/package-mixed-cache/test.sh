4c6c30c53533e444dc40a83dfcfca041576b4619
Neil Parikh <parikh.neil@me.com>
2026-04-30 15:32:45 -0400

    don't output the timing

--- test/cli/package-mixed-cache/test.sh
+++ test/cli/package-mixed-cache/test.sh
@@ -16,14 +16,18 @@ cd "$tmp" || exit 1
 
 cat output.txt | grep "No errors! Great job."
 
-cat output.txt | grep kvstore
+cat output.txt | grep "kvstore.miss"
+cat output.txt | grep "kvstore.hit"
+cat output.txt | grep "kvstore.write"
 
 # Consume the cache in package-directed mode
 "$cwd/main/sorbet" --silence-dev-message --sorbet-packages --experimental-package-directed --max-threads=0 --cache-dir=cache --counters . > output.txt 2>&1
 
 cat output.txt | grep "No errors! Great job."
 
-cat output.txt | grep kvstore
+cat output.txt | grep "kvstore.miss"
+cat output.txt | grep "kvstore.hit"
+cat output.txt | grep "kvstore.write"
 
 rm -rf cache
 
@@ -32,11 +36,15 @@ rm -rf cache
 
 cat output.txt | grep "No errors! Great job."
 
-cat output.txt | grep kvstore
+cat output.txt | grep "kvstore.miss"
+cat output.txt | grep "kvstore.hit"
+cat output.txt | grep "kvstore.write"
 
 # Consume the cache in monolithic mode
 "$cwd/main/sorbet" --silence-dev-message --sorbet-packages --max-threads=0 --cache-dir=cache --counters . > output.txt 2>&1
 
 cat output.txt | grep "No errors! Great job."
 
-cat output.txt | grep kvstore
+cat output.txt | grep "kvstore.miss"
+cat output.txt | grep "kvstore.hit"
+cat output.txt | grep "kvstore.write"
