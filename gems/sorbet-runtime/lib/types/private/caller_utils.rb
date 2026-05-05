65473377d599c87e8c63f7b03f4315084c9760a6
Alexander Momchilov <alexander.momchilov@shopify.com>
2026-05-05 11:48:53 -0400

    Move `SORBET_RUNTIME_LIB_PATH`

--- gems/sorbet-runtime/lib/types/private/caller_utils.rb
+++ gems/sorbet-runtime/lib/types/private/caller_utils.rb
@@ -2,6 +2,10 @@
 # typed: false
 
 module T::Private::CallerUtils
+  # Fetch the directory name of the file that defines the `T::Private` constant and
+  # add a trailing slash to allow us to match it as a directory prefix.
+  SORBET_RUNTIME_LIB_PATH = File.dirname(T.const_source_location(:Private).first) + File::SEPARATOR
+
   if Thread.respond_to?(:each_caller_location) # RUBY_VERSION >= "3.2"
     def self.find_caller(callers_to_skip: 0)
       remaining_to_skip = callers_to_skip + 1
