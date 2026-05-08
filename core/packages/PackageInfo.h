d691d88d964faea7a620d8a4ff2f87c7d726eca0
Neil Parikh <parikh.neil@me.com>
2026-05-08 03:09:33 -0400

    delete unused method

--- core/packages/PackageInfo.h
+++ core/packages/PackageInfo.h
@@ -55,12 +55,6 @@ struct Import {
     Import(MangledName mangledName, ImportType type, core::LocOffsets loc)
         : mangledName(mangledName), type(type), loc(loc) {}
 
-    // Generate a prelude import, with the given origin.
-    // NOTE: the origin must point to something to hang any errors related to the implicit import off of. Currently this
-    // is the declLoc_ of the package that is getting the implicit import, and because of this it's important to not use
-    // these locs when computing locations for autocorrects.
-    static Import prelude(MangledName mangledName, core::LocOffsets implicitLoc);
-
     Import(Import &&other) = default;
     Import(const Import &other) = default;
     Import &operator=(Import &&other) = default;
