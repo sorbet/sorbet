96cf5765908576ef68d81825ae900b83ca953edf
Neil Parikh <parikh.neil@me.com>
2026-05-12 16:26:21 -0700

    add typed: ignore

---
+++ test/cli/packager_min_typed_level/test/test_too_low_ignore.rb
@@ -0,0 +1,5 @@
+# typed: ignore
+# Note that no error is reported here even those 'ignore' < 'true'
+
+class Test::Project::MyPackage::TestTooLow
+end
---
+++ test/cli/packager_min_typed_level/too_low_ignore.rb
@@ -0,0 +1,5 @@
+# typed: ignore
+# Note that no error is reported here even those 'ignore' < 'strict'
+
+class Project::MyPackage::TooLow
+end
