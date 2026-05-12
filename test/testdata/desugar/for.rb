42a132a092e46ec9544f8a524ed903ef1e1fc29c
Alexander Momchilov <alexander.momchilov@shopify.com>
2026-05-12 17:01:42 -0400

    Improve `for` loop desugar tests

--- test/testdata/desugar/for.rb
+++ test/testdata/desugar/for.rb
@@ -1,5 +1,7 @@
 # typed: strict
 
+extend T::Sig
+
 class A
     def self.each(&blk)
   # ^^^^^^^^^^^^^^^^^^^ error: The method `each` does not have a `sig`
@@ -61,3 +63,69 @@ class Main
     end
 end
 Main.main
+
+@ivar = T.let([:x, 0], [Symbol, Integer])
+@@cvar = T.let([:x, 0], [Symbol, Integer])
+$gvar = T.let([:x, 0], [Symbol, Integer])
+
+sig { params(arg: [Symbol, Integer]).void }
+def call_target=(arg); end
+
+ConstantTarget = T.let([:x, 0], [Symbol, Integer])
+ConstantPathTarget = T.let([:x, 0], [Symbol, Integer])
+
+module Nested
+  ConstantPathTarget = T.let([:x, 0], [Symbol, Integer])
+end
+
+a = T.let([], T::Array[[Symbol, Integer]])
+
+for @ivar in [[:a, 1], [:b, 2], [:c, 3]] # error: Expected `[Symbol, Integer]` but found `NilClass` for field
+  "loop body"
+end
+
+
+for @@cvar in [[:a, 1], [:b, 2], [:c, 3]] # error: Expected `[Symbol, Integer]` but found `NilClass` for field
+  "loop body"
+end
+
+
+for $gvar in [[:a, 1], [:b, 2], [:c, 3]] # error: Expected `[Symbol, Integer]` but found `NilClass` for field
+  "loop body"
+end
+
+
+for self.call_target in [[:a, 1], [:b, 2], [:c, 3]] # error: Assigning a value to `arg` that does not match expected type `[Symbol, Integer]`
+  # isPrivateOk should be true
+  "loop body"
+end
+
+for self::call_target in [[:a, 1], [:b, 2], [:c, 3]] # error: Assigning a value to `arg` that does not match expected type `[Symbol, Integer]`
+  # isPrivateOk should be true
+  "loop body"
+end
+
+for E.e in [] do # isPrivateOk should be false
+  "loop body"
+end
+
+for ConstantTarget in [[:a, 1], [:b, 2], [:c, 3]] # error: Expected `[Symbol, Integer]` but found `NilClass` for field
+  "loop body"
+end
+
+
+for ::ConstantPathTarget in [[:a, 1], [:b, 2], [:c, 3]] # error: Expected `[Symbol, Integer]` but found `NilClass` for field
+  "loop body"
+end
+
+for Nested::ConstantPathTarget in [[:a, 1], [:b, 2], [:c, 3]] # error: Expected `[Symbol, Integer]` but found `NilClass` for field
+  "loop body"
+end
+
+for ::Nested::ConstantPathTarget in [[:a, 1], [:b, 2], [:c, 3]] # error: Expected `[Symbol, Integer]` but found `NilClass` for field
+  "loop body"
+end
+
+for a[123] in [[:a, 1], [:b, 2], [:c, 3]] do # error: Expected `[Symbol, Integer]` but found `NilClass` for argument `arg1`
+  "loop body"
+end
