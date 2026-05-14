db43902ac2dd15ef505cff11cc780af235650ab3
Neil Parikh <parikh.neil@me.com>
2026-05-14 10:28:31 -0700

    always keep the arg to describe in the tree

--- test/testdata/rewriter/describe_with_expression.rb
+++ test/testdata/rewriter/describe_with_expression.rb
@@ -5,9 +5,11 @@ end
 
 class Test
   describe("".dne) do
+            # ^^^ error: Method `dne` does not exist on `String` component of `String("")`
   end
 
   describe A do
+         # ^ error: Unable to resolve constant `A`
   end
 
   describe B do
