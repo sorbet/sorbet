6bd58600c42b997c43a1f7f5f9e65642d122929d
Neil Parikh <parikh.neil@me.com>
2026-05-20 14:45:52 -0400

    cp -r test/cli/package-gen-packages/ test/cli/package-gen-packages-test-packages

---
+++ test/cli/package-gen-packages-test-packages/D/test/test_constants.rb
@@ -0,0 +1,5 @@
+# typed: strict
+
+module Test::D
+  puts E::CONSTANT_FROM_E
+end
---
+++ test/cli/package-gen-packages-test-packages/G/constants.rb
@@ -0,0 +1,6 @@
+# typed: strict
+
+module G
+  CONSTANT_FROM_G = "Hello from Package G"
+  ANOTHER_CONSTANT_FROM_G = "Hello from Package G"
+end
