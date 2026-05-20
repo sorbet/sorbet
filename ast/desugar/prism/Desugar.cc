42e2e076c7ef0d07f6a3b0be8c62635a4267d54e
Alexander Momchilov <amomchilov@users.noreply.github.com>
2026-05-20 14:26:34 -0400

    Implement Prism support for splats in `rescue` clauses (#10286)
    * Desugar splats in rescue clauses
    
    * Fix location of parenthesized `case` expression

--- ast/desugar/prism/Desugar.cc
+++ ast/desugar/prism/Desugar.cc
@@ -2086,7 +2086,9 @@ ast::ExpressionPtr Desugarer::desugar(pm_node_t *node) {
             bool hasPredicate = (caseNode->predicate != nullptr);
 
             if (hasPredicate) {
-                predicateLoc = predicate.loc();
+                // Use the Prism node's loc directly (not the desugared expr's loc) so that
+                // e.g. `case (1)` retains the parens-inclusive loc rather than collapsing to `1`.
+                predicateLoc = translateLoc(caseNode->predicate->location);
                 tempName = nextUniqueDesugarName(core::Names::assignTemp());
             } else {
                 tempName = core::NameRef::noName();
@@ -4880,8 +4882,24 @@ ast::Rescue::RESCUE_CASE_store Desugarer::desugarRescueCases(pm_rescue_node *fir
         // Build exception store from exceptions being rescued
         ast::RescueCase::EXCEPTION_store exceptions;
         auto exceptionsNodes = absl::MakeSpan(rescueNode->exceptions.nodes, rescueNode->exceptions.size);
-        for (auto *ex : exceptionsNodes) {
-            exceptions.emplace_back(desugar(ex));
+
+        bool hasSplat = absl::c_any_of(exceptionsNodes, [](auto *ex) { return isa_node<pm_splat_node>(ex); });
+        if (hasSplat) {
+            // When there's a splat, desugar the entire list as an array with concatenations.
+            // This produces: [A].concat(<splat>(B)).concat([C]) for `rescue A, *B, C`
+            ast::Array::ENTRY_store exceptionElements;
+            exceptionElements.reserve(exceptionsNodes.size());
+            for (auto *ex : exceptionsNodes) {
+                exceptionElements.emplace_back(desugar(ex));
+            }
+            auto exceptionsLoc =
+                translateLoc(exceptionsNodes.front()->location.start, exceptionsNodes.back()->location.end);
+            auto concatenatedExceptions = desugarArray(exceptionsLoc, exceptionsNodes, move(exceptionElements));
+            exceptions.emplace_back(move(concatenatedExceptions));
+        } else {
+            for (auto *ex : exceptionsNodes) {
+                exceptions.emplace_back(desugar(ex));
+            }
         }
 
         // Desugar the rescue body
