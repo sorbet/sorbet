4557c60fd43074a3eb8614dd066a948313ea506d
Jake Zimmerman <zimmerman.jake@gmail.com>
2026-05-28 15:04:54 -0700

    Squashed commit of the following:
    commit 47b89427623acb585db8daf89b40f8cffc11c0ff
    Author: Alexander Momchilov <alexander.momchilov@shopify.com>
    Date:   Tue May 26 11:33:49 2026 -0400
    
        Elide temp var when `case` is given a variable
    
    commit aa1e2325ffcd4681e85da8f0582115635884f59d
    Author: Alexander Momchilov <alexander.momchilov@shopify.com>
    Date:   Tue May 26 12:37:44 2026 -0400
    
        Test when `case` desugar uses temp var
    
    commit 68b740b14401dc8b7d238c9ab3be01e220635886
    Author: Alexander Momchilov <alexander.momchilov@shopify.com>
    Date:   Wed May 27 22:23:15 2026 -0400
    
        Make existing test `# typed: true`

--- test/testdata/desugar/case.rb
+++ test/testdata/desugar/case.rb
@@ -1,5 +1,7 @@
-# typed: false
+# typed: true
+
 class Test
+  attr_reader :a, :foo1, :foo2, :foo3, :test1, :test2, :test3
   def test_case
     case a
     when foo1
@@ -16,5 +18,61 @@ class Test
     when test2, test3
       :b
     end
+  end
+
+  @@cvar = T.let(123, T.nilable(Integer))
+
+  # Test when Sorbet synthesizes a temporary variable, vs. when it skips it.
+  def test_when_temporary_variables_are_used
+    # Method calls can have side-effects. Extract the result to a synthetic local variable.
+    case to_s()
+    when 1
+    when 2
+    end
+
+    # Reading from `self` is side-effect-free, so we can elide the synthetic local variable.
+    T.bind(self, T.nilable(Integer))
+    case self
+    when Integer
+    else T.absurd(self)
+    #    ^^^^^^^^^^^^^^ error: Control flow could reach `T.absurd` because the type `NilClass` wasn't handled
+    end
+
+    # Local variables are ... already local variables. We can elide the synthetic local variable.
+    lvar = T.let(123, T.nilable(Integer))
+    case lvar
+    when Integer
+    else T.absurd(lvar)
+    #    ^^^^^^^^^^^^^^ error: Control flow could reach `T.absurd` because the type `NilClass` wasn't handled
+    end
+
+    @ivar  = T.let(123, T.nilable(Integer))
+    case @ivar
+    when Integer
+    else T.absurd(@ivar)
+    #    ^^^^^^^^^^^^^^^ error: Control flow could reach `T.absurd` because the type `NilClass` wasn't handled
+    end
+
+    case @@cvar
+    when Integer
+    else T.absurd(@@cvar)
+    #    ^^^^^^^^^^^^^^^^ error: Control flow could reach `T.absurd` because the type `NilClass` wasn't handled
+    end
+
+    $gvar = T.let(123, T.nilable(Integer))
+    case $gvar
+    when Integer
+    else T.absurd($gvar)
+    #    ^^^^^^^^^^^^^^^ error: Control flow could reach `T.absurd` because the type `NilClass` wasn't handled
+    end
+
+    # Incorrect Desugar tree until https://github.com/sorbet/sorbet/pull/10020 is merged.
+    # tap do |_; block_local_var|
+    #   block_local_var = T.let(123, T.nilable(Integer))
+    #   case block_local_var
+    #   when Integer
+    #   when 2
+    #   end
+    # end
   end
 end
