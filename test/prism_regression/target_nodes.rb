a02ab38a45346a67fc78047e432250fd213ce518
Alexander Momchilov <amomchilov@users.noreply.github.com>
2026-06-01 14:41:11 -0400

    Add support for call target nodes in `for` loops (#10270)

--- test/prism_regression/target_nodes.rb
+++ test/prism_regression/target_nodes.rb
@@ -43,3 +43,35 @@ end
 begin
 rescue => a[123]
 end
+
+##### Test target nodes as a `for` loop variable
+
+for @ivar in []
+end
+
+for @@cvar in []
+end
+
+for $gvar in []
+end
+
+for self.call_target1 in []
+end
+
+for self::call_target2 in []
+end
+
+for ConstantTarget in []
+end
+
+for ::ConstantPathTarget in []
+end
+
+for Nested::ConstantPathTarget in []
+end
+
+for ::Nested::ConstantPathTarget in []
+end
+
+for a[123] in []
+end
