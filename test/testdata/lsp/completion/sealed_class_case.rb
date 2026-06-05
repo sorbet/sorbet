59c39caacc0c5922676f712b10da802ad39b62bf
Brian Shu <bshu@stripe.com>
2026-06-05 13:56:08 -0700

    add some tests

---
+++ test/testdata/lsp/completion/sealed_class_case.rb
@@ -0,0 +1,26 @@
+# typed: true
+class Module; include T::Sig; end
+
+module Parent
+  extend T::Helpers
+
+  sealed!
+end
+
+class Child1; include Parent; end
+
+class Child2; include Parent; end
+
+sig { params(parent: Parent).void }
+def example(parent)
+  parent.cas
+  #      ^^^ error: does not exist
+  #         ^ apply-completion: [A] item: 0
+    
+  x = parent.case
+  #          ^^^^ error: does not exist
+  #              ^ apply-completion: [C] item: 0
+
+  parent.
+  #      ^ apply-completion: [D] item: 121
+end # error: unexpected token "end"
