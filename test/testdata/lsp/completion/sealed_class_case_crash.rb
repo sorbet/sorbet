f92ccc8269c81b3ef5b115f2e334fd4907c7449e
Brian Shu <littlebubu.shu@gmail.com>
2026-06-05 15:49:31 -0700

    fix tests and add to exclude for prism

--- test/testdata/lsp/completion/sealed_class_case_crash.rb
+++ test/testdata/lsp/completion/sealed_class_case_crash.rb
@@ -9,10 +9,13 @@ class Child1; include Parent; end
 
 class Child2; include Parent; end
 
-def example
+def example1
   c1 = Child1.new
   c1.
   #  ^ completion: class, ...
+end # error: unexpected token "end"
+
+def example2
   c2 = Child2.new
   c2.
   #  ^ completion: class, ...
