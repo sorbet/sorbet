3be6b800499a5f12eb1825767440a7a9fc8fcff9
Brian Shu <bshu@stripe.com>
2026-06-05 12:31:26 -0700

    add into other test

--- test/cli/ambiguous-this-module-include/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-
-main/sorbet --silence-dev-message test/cli/ambiguous-this-module-include/ 2>&1
-
--- test/cli/suggest-kernel/suggest-kernel.rb
+++ test/cli/suggest-kernel/suggest-kernel.rb
@@ -13,3 +13,19 @@ sig { params(x: Foo).void }
 def example(x)
   x.is_a?(Foo)
 end
+
+module Parent
+end
+
+module Child
+  include Parent
+end
+
+module A
+  extend T::Sig
+  sig {params(parent: Parent).void}
+  def example(parent)
+    if parent.is_a?(Child)
+    end
+  end
+end
