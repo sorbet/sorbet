b84f7cbfd514613250829e8078721f922006f754
Jake Zimmerman <zimmerman.jake@gmail.com>
2026-06-05 16:55:36 -0700

    Remove isTestPath helper

--- core/Files.cc
+++ core/Files.cc
@@ -290,10 +290,6 @@ string File::censorFilePathForSnapshotTests(string_view orig) {
     }
 }
 
-bool File::isTestPath() const {
-    return flags.isTestPath;
-}
-
 bool File::isPackagedTest() const {
     return flags.isTestFile || flags.isTestPath;
 }
