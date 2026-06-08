4aa9b30aca8931620cf93b4a3c6e505393d3ac97
Sergey Semyonov <sergey.semenov@workato.com>
2026-06-08 18:20:51 +0400

    Add sigs for allbits? and anybits? in Integer (#10352)

--- test/testdata/rbi/integer.rb
+++ test/testdata/rbi/integer.rb
@@ -5,6 +5,14 @@
 255[1..]
 255[..4]
 
+# allbits? takes an Integer mask and return a Boolean
+T.reveal_type(0b1010.allbits?(0b1000)) # error: type: `T::Boolean`
+0b1010.allbits?(:nope) # error: Expected `Integer` but found `Symbol(:nope)` for argument `mask`
+
+# anybits? takes an Integer mask and return a Boolean
+T.reveal_type(0b1010.anybits?(0b1000)) # error: type: `T::Boolean`
+0b1010.anybits?(:nope) # error: Expected `Integer` but found `Symbol(:nope)` for argument `mask`
+
 # <=> returns Integer for comparable types, T.nilable(Integer) for incomparable types
 T.reveal_type(1 <=> 2) # error: type: `Integer`
 T.reveal_type(1 <=> 2.0) # error: type: `Integer`
