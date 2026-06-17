12363f12d3914f8f0a4dd1d2fad507514a68b685
Jake Zimmerman <zimmerman.jake@gmail.com>
2026-06-16 19:48:54 -0700

    Rename this test

--- test/cli/package-gen-packages-test-packages/A/__package.rb
+++
@@ -1,8 +0,0 @@
-# typed: strict
-
-class A < PackageSpec
-  strict_dependencies 'dag'
-  layer 'app'
-
-  export A::CONSTANT_FROM_A
-end
--- test/cli/package-gen-packages-test-packages/A/constants.rb
+++
@@ -1,6 +0,0 @@
-# typed: strict
-
-module A
-  CONSTANT_FROM_A = "Hello from Package A"
-  puts G::ANOTHER_CONSTANT_FROM_G
-end
