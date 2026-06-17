a0fc87bd939fdcb3f3d5c40106d08767bd2345f5
Jake Zimmerman <zimmerman.jake@gmail.com>
2026-06-16 19:48:54 -0700

    Rename this test

--- test/cli/package-gen-packages-test-packages/F/constants.rb
+++
@@ -1,5 +0,0 @@
-# typed: strict
-
-module F
-  CONSTANT_FROM_F = "Hello from Package F"
-end
--- test/cli/package-gen-packages-test-packages/G/constants.rb
+++
@@ -1,6 +0,0 @@
-# typed: strict
-
-module G
-  CONSTANT_FROM_G = "Hello from Package G"
-  ANOTHER_CONSTANT_FROM_G = "Hello from Package G"
-end
