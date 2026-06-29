df12fc57a09c120ab9b0f8c98f36d17924999d07
prnvjn <prnvjn@gmail.com>
2026-06-29 00:17:12 -0500

    Update rewriter tests for prop/attr name validation
    Adds typed: false coverage for the empty-name and non-symbol BadAttrArg
    messages, and updates quoted_symbol_error and prop_duplicated to assert
    the new behavior now that invalid prop/const names are rejected before
    they are processed.

--- test/testdata/rewriter/attr_bad_string_false.rb
+++ test/testdata/rewriter/attr_bad_string_false.rb
@@ -3,3 +3,5 @@ attr :'foo-bar' # error: Bad attribute name `foo-bar`
 attr_reader :'left-right' # error: Bad attribute name `left-right`
 attr_writer :'baz-qux' # error: Bad attribute name `baz-qux`
 attr_accessor :'a-b' # error: Bad attribute name `a-b`
+attr :"" # error: Attribute names must be non-empty
+attr 10 # error: Argument to `attr` must be a Symbol or String
