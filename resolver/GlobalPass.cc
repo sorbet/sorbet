f6507130c625be599f9a3714c11d1e79a1f4ded4
Alexander Momchilov <alexander.momchilov@shopify.com>
2026-07-08 19:31:04 -0400

    Document implied invariants

--- resolver/GlobalPass.cc
+++ resolver/GlobalPass.cc
@@ -331,6 +331,9 @@ void Resolver::finalizeAncestors(core::GlobalState &gs) {
                     ref.data(gs)->setSuperClass(core::Symbols::Object());
                 }
             } else {
+                ENFORCE(ref.data(gs)->isModule());
+                ENFORCE(!ref.data(gs)->superClass().exists() || ref.data(gs)->superClass() == core::Symbols::todo());
+
                 ref.data(gs)->setSuperClass(core::Symbols::Sorbet_Private_Static_ImplicitModuleSuperClass());
             }
         }
