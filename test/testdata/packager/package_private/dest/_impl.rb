05a2ecee85948714a0fd14da42c7c1bc6ff8835a
Getty Ritter <gdritter@infinitenegativeutility.com>
2026-07-11 14:52:08 -0700

    fix up test

--- test/testdata/packager/package_private/dest/_impl.rb
+++ test/testdata/packager/package_private/dest/_impl.rb
@@ -8,7 +8,7 @@ module Dest
   Nested::Outer::InnerPublic
   Nested::Outer::InnerPrivate # error: `Nested::Outer::InnerPrivate` resolves but is declared `package_private` in `Nested`
 
-  Nested::PrivateOuter # error: `Source::Private` resolves but is declared `package_private` in `Source`
-  Nested::PrivateOuter::InnerThing # error: `Source::Private` resolves but is declared `package_private` in `Source`
-  Nested::PrivateOuter::InnerOtherThing # error: `Source::Private` resolves but is declared `package_private` in `Source`
+  Nested::PrivateOuter # error: `Nested::PrivateOuter` resolves but is declared `package_private` in `Nested`
+  Nested::PrivateOuter::InnerThing # error: `Nested::PrivateOuter::InnerThing` resolves but is not exported from `Nested`
+  Nested::PrivateOuter::InnerOtherThing # error: `Nested::PrivateOuter::InnerOtherThing` resolves but is declared `package_private` in `Nested`
 end
