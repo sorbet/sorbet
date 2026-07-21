5d927a6eb83c3ab9125aec886aeecfa03253a6cd
Jake Zimmerman <zimmerman.jake@gmail.com>
2026-07-20 17:50:08 -0700

    Change a test that was added recently

--- test/testdata/packager/apply-all-fixes/a/foo.test.rb
+++ test/testdata/packager/apply-all-fixes/a/foo.test.rb
@@ -2,17 +2,17 @@
 # selective-apply-code-action: quickfix
 # omit-apply-all-quickfix: false
 
-class Test::A::FooTest
+class A::FooTest
   B::Foo.new
 # ^^^^^^ error: `B::Foo` resolves but its package is not imported
-# ^^^^^^ apply-code-action: [AA] Test Import `B` in package `A`
+# ^^^^^^ apply-code-action: [AA] Import `B` in package `A`
 # ^^^^^^ apply-code-action: [BB] Apply all Sorbet fixes for file
   B::Foo.new
 # ^^^^^^ error: `B::Foo` resolves but its package is not imported
-# ^^^^^^ apply-code-action: [CC] Test Import `B` in package `A`
+# ^^^^^^ apply-code-action: [CC] Import `B` in package `A`
 # ^^^^^^ apply-code-action: [DD] Apply all Sorbet fixes for file
   C::Foo.new
 # ^^^^^^ error: `C::Foo` resolves but its package is not imported
-# ^^^^^^ apply-code-action: [EE] Test Import `C` in package `A`
+# ^^^^^^ apply-code-action: [EE] Import `C` in package `A`
 # ^^^^^^ apply-code-action: [FF] Apply all Sorbet fixes for file
 end
