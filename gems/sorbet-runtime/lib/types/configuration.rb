9ad40013b1f3e6957cfa6791f53ea08bab53e5f2
Jake Zimmerman <zimmerman.jake@gmail.com>
2026-07-22 14:26:10 -0700

    sorbet-runtime: Remove `hard_assert_handler` (#10435)
    * sorbet-runtime: Remove `hard_assert_handler`
    
    There are no more uses, and we should aim to either use plain `raise` or
    to use other, targeted handlers in the future.
    
    * Also remove this while we're here

--- gems/sorbet-runtime/lib/types/configuration.rb
+++ gems/sorbet-runtime/lib/types/configuration.rb
@@ -337,41 +337,6 @@ module T::Configuration
     end
   end
 
-  @hard_assert_handler = nil
-  # Set a handler for hard assertions
-  #
-  # These generally should stop execution of the program, and optionally inform
-  # some party of the assertion.
-  #
-  # @param [Lambda, Proc, Object, nil] value Proc that handles the error
-  #   report (pass nil to reset to default behavior)
-  #
-  # Parameters passed to value.call:
-  #
-  #  @param [String] str Assertion message
-  #  @param [Hash] extra A hash containing additional parameters to be passed along to the handler.
-  #
-  # @example
-  #   T::Configuration.hard_assert_handler = lambda do |str, extra|
-  #     raise "#{str}, context: #{extra}"
-  #   end
-  def self.hard_assert_handler=(value)
-    validate_lambda_given!(value)
-    @hard_assert_handler = value
-  end
-
-  private_class_method def self.hard_assert_handler_default(str, _)
-    raise str
-  end
-
-  def self.hard_assert_handler(str, extra={})
-    if @hard_assert_handler
-      @hard_assert_handler.call(str, extra)
-    else
-      hard_assert_handler_default(str, extra)
-    end
-  end
-
   @scalar_types = nil
   # Set a list of class strings that are to be considered scalar.
   #   (pass nil to reset to default behavior)
@@ -459,20 +424,6 @@ module T::Configuration
 
   def self.redaction_handler
     @redaction_handler
-  end
-
-  @class_owner_finder = nil
-  # Set to a function which can get the 'owner' of a class. This is
-  # used in reporting deserialization errors
-  #
-  # @param [Lambda, Proc, nil] handler Proc that takes a class and
-  # produces its owner, or `nil` if it does not have one.
-  def self.class_owner_finder=(handler)
-    @class_owner_finder = handler
-  end
-
-  def self.class_owner_finder
-    @class_owner_finder
   end
 
   # Temporarily disable ruby warnings while executing the given block. This is
