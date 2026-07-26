e31ff8456a54f2dad238916f919658cef36833d5
Ramy Kaddouri <myramkadd@gmail.com>
2026-07-25 23:11:21 -0700

    add narrowing for literal case checks

--- infer/environment.cc
+++ infer/environment.cc
@@ -674,6 +674,14 @@ void Environment::updateKnowledge(core::Context ctx, cfg::LocalRef local, core::
             whoKnows.truthy().addYesTypeTest(local, typeTestsWithVar, ref, klassType);
             whoKnows.falsy().addNoTypeTest(local, typeTestsWithVar, ref, klassType);
         }
+
+        if(
+            core::isa_type<core::NamedLiteralType>(klassType) ||
+            core::isa_type<core::IntegerLiteralType>(klassType) ||
+            core::isa_type<core::FloatLiteralType>(klassType)
+          ) {
+            whoKnows.truthy().addYesTypeTest(local, typeTestsWithVar, ref, klassType);
+        }
         whoKnows.sanityCheck();
         return;
     }
