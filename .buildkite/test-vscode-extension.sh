639b0ce086b323ea23d56a3f60b2869b4164620d
Jake Zimmerman <zimmerman.jake@gmail.com>
2026-07-29 14:29:21 -0700

    Skip vscode extension jobs on branches if no changes (#10504)

--- .buildkite/test-vscode-extension.sh
+++ .buildkite/test-vscode-extension.sh
@@ -2,6 +2,13 @@
 
 set -euo pipefail
 
+if [ "${BUILDKITE_BRANCH:-}" != "master" ]; then
+  if ! git diff --name-only "origin/master...HEAD" | grep -q '^vscode_extension/'; then
+    echo "Skipping because there are no changes to vscode_extension/"
+    exit 0
+  fi
+fi
+
 # shellcheck source-path=SCRIPTDIR/..
 source .buildkite/tools/with_backoff.sh
 
