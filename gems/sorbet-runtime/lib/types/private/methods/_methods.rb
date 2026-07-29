2e9dd57aa56f5a8200f40e9758311ab6543bfb6b
Jake Zimmerman <zimmerman.jake@gmail.com>
2026-07-28 20:53:24 -0700

    no-op: Re-indent

--- gems/sorbet-runtime/lib/types/private/methods/_methods.rb
+++ gems/sorbet-runtime/lib/types/private/methods/_methods.rb
@@ -359,11 +359,11 @@ module T::Private::Methods
     T::Private::ClassUtils.replace_method(original_method, mod, method_name) do |*args, &blk|
       method_sig = T::Private::Methods.maybe_run_sig_block_for_key(key)
       if !method_sig
-      method_sig = T::Private::Methods._handle_missing_method_signature(
-        self,
-        original_method,
-        __callee__ || raise("Unknown __callee__ for method without a signature"),
-      )
+        method_sig = T::Private::Methods._handle_missing_method_signature(
+          self,
+          original_method,
+          __callee__ || raise("Unknown __callee__ for method without a signature"),
+        )
       end
 
       # Should be the same logic as CallValidation.wrap_method_if_needed but we
