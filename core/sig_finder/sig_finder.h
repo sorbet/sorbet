9b4fe53c49826520bde1215c3028920b8ba06893
Trevor Elliott <awesomelyawesome@gmail.com>
2026-08-03 21:52:52 -0400

    Move sig finder internals out of the header (#10532)

--- core/sig_finder/sig_finder.h
+++ core/sig_finder/sig_finder.h
@@ -1,6 +1,6 @@
 #ifndef SORBET_SIG_FINDER
 #define SORBET_SIG_FINDER
-#include "core/core.h"
+
 #include "resolver/type_syntax/type_syntax.h"
 
 namespace sorbet::sig_finder {
@@ -12,31 +12,6 @@ public:
         const ast::Send &origSend;
         Result(resolver::ParsedSig &&sig, const ast::Send &origSend) : sig(std::move(sig)), origSend(origSend) {}
     };
-
-private:
-    const core::Loc queryLoc;
-
-    // Track the narrowest location range that still contains the queryLoc.
-    //
-    // If we find a method that's after queryLoc but it's not in this narrowest range,
-    // it means we found a sig that's outside the scope where the queryLoc was.
-    core::Loc narrowestClassDefRange;
-
-    // Track whether the current scope has the queryLoc.
-    std::vector<bool> scopeContainsQueryLoc;
-
-    std::optional<Result> result_;
-
-public:
-    SigFinder(core::Loc queryLoc)
-        : queryLoc(queryLoc), narrowestClassDefRange(core::Loc::none()), scopeContainsQueryLoc(std::vector<bool>{}),
-          result_(std::nullopt) {}
-
-    void preTransformClassDef(core::Context ctx, const ast::ClassDef &tree);
-    void postTransformClassDef(core::Context ctx, const ast::ClassDef &tree);
-    void preTransformMethodDef(core::Context ctx, const ast::MethodDef &tree);
-    void postTransformRuntimeMethodDefinition(core::Context ctx, const ast::RuntimeMethodDefinition &tree);
-    void preTransformSend(core::Context ctx, const ast::Send &tree);
 
     static std::optional<Result> findSignature(core::Context ctx, const ast::ExpressionPtr &tree, core::Loc queryLoc);
     static std::optional<Result> findSignature(core::Context ctx, const ast::ClassDef &tree, core::Loc queryLoc);
