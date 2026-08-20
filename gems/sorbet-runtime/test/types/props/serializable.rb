b74599c5f3e420e968e3af3a49a206c2f7f47de8
Jake Zimmerman <zimmerman.jake@gmail.com>
2026-08-20 15:50:24 -0700

    Squashed commit of the following:
    commit 73247b5feb4da3fcd9817fec37287319d9785196
    Author: Getty Ritter <gdritter@infinitenegativeutility.com>
    Date:   Thu Aug 20 14:26:30 2026 -0700
    
        one more
    
    commit 25c79cbce8690fc36a95053b30711db36d31d9b4
    Author: Getty Ritter <gdritter@infinitenegativeutility.com>
    Date:   Thu Aug 20 12:03:20 2026 -0700
    
        also remove those tests
    
    commit 90df682a2e82d3bc99f84225f31e16b17cb36dd4
    Author: Getty Ritter <gdritter@infinitenegativeutility.com>
    Date:   Thu Aug 20 11:22:24 2026 -0700
    
        whoops
    
    commit c5585f24e74d80b527a3ad379376635f8c9e9954
    Author: Getty Ritter <gdritter@infinitenegativeutility.com>
    Date:   Thu Aug 20 11:12:36 2026 -0700
    
        delete with for now

--- gems/sorbet-runtime/test/types/props/serializable.rb
+++ gems/sorbet-runtime/test/types/props/serializable.rb
@@ -518,54 +518,6 @@ class Opus::Types::Test::Props::SerializableTest < Critic::Unit::UnitTest
     prop :f2, T.nilable(WithModel1)
   end
 
-  describe 'with function' do
-    it 'with simple fields' do
-      a = WithModel1.new(foo: 'foo')
-      b = a.with(bar: 10)
-
-      assert_equal('foo', a.foo)
-      assert_nil(a.bar)
-      assert_equal('foo', b.foo)
-      assert_equal(10, b.bar)
-    end
-
-    it 'with invalid fields' do
-      a = WithModel1.new(foo: 'foo')
-      e = assert_raises(ArgumentError) do
-        a.with(non_bar: 10)
-      end
-      assert_equal("Unexpected arguments: input(#{{non_bar: 10}}), unexpected(#{{'non_bar' => 10}})", e.to_s) # rubocop:disable Lint/LiteralInInterpolation
-
-      a = WithModel1.from_hash({'foo' => 'foo', 'foo1' => 'foo1'})
-      e = assert_raises(ArgumentError) do
-        a.with(non_bar: 10)
-      end
-      assert_equal("Unexpected arguments: input(#{{non_bar: 10}}), unexpected(#{{'non_bar' => 10}})", e.to_s) # rubocop:disable Lint/LiteralInInterpolation
-    end
-
-    it 'with overwrite fields' do
-      a = WithModel1.new(foo: 'foo', bar: 10)
-      b = a.with(bar: 20)
-
-      assert_equal('foo', a.foo)
-      assert_equal(10, a.bar)
-      assert_equal('foo', b.foo)
-      assert_equal(20, b.bar)
-    end
-
-    it 'with nested fields' do
-      a = WithModel2.new(f1: 'foo')
-      b = a.with(f2: {foo: 'foo', bar: 10})
-
-      assert_equal('foo', a.f1)
-      assert_nil(a.f2)
-      assert_equal('foo', b.f1)
-      refute_nil(b.f2)
-      assert_equal('foo', b.f2.foo)
-      assert_equal(10, b.f2.bar)
-    end
-  end
-
   class BooleanStruct < T::Struct
     prop :prop, T::Boolean
     prop :nilable_prop, T.nilable(T::Boolean)
@@ -1461,16 +1413,6 @@ class Opus::Types::Test::Props::SerializableTest < Critic::Unit::UnitTest
   class ConstDefault < T::Struct
     const :required_at_some_point, NilClass, default: nil
     const :still_required_prop, Integer
-  end
-
-  describe 'const NilClass' do
-    it 'round-trip serializes' do
-      h = {'still_required_prop' => 5}
-      x = ConstDefault.from_hash!(h)
-      assert_nil(x.required_at_some_point)
-      x = x.with(still_required_prop: 6)
-      assert_nil(x.required_at_some_point)
-    end
   end
 
   class MuckAboutWithPropInternals
