388b94425e49a266606cafbab2a98602e7d1605a
Trevor Elliott <awesomelyawesome@gmail.com>
2026-09-11 13:35:51 -0700

    Add a test for module forward decls

--- test/testdata/packager/scope_restriction/a_b/c/impl.rb
+++ test/testdata/packager/scope_restriction/a_b/c/impl.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 # typed: false
 
- module A::B
-#^^^^^^^^^^^ error: `A::B` belongs to package `A::B`
+# As neither of these reference constants, it should be fine to forward-declare them.
+module A
+  module B
+  end
+end
+
+module A::B # error: `A::B` belongs to package `A::B`
   module C
   end
 end
