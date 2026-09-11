01376a5ea8c799012f3301f6dc16765dfacf57f5
Jake Zimmerman <zimmerman.jake@gmail.com>
2026-09-10 22:22:25 -0700

    Add coverage for nested method modifier edge cases
    Test mismatch diagnostics and autocorrects through nested modifiers, and assert that package-private visibility is retained in either modifier order.

--- test/testdata/packager/test-packages-package-private/b/test/test.rb
+++ test/testdata/packager/test-packages-package-private/b/test/test.rb
@@ -13,12 +13,15 @@ module Root::B::Test
   class Child < Root::ModifierParent
     def test_inherited_package_private
       package_private_private
+    # ^^^^^^^^^^^^^^^^^^^^^^^ error: Method `package_private_private` on `Root::ModifierParent` is package-private and cannot be called from package `Root::B::Test`
       private_package_private
     # ^^^^^^^^^^^^^^^^^^^^^^^ error: Method `private_package_private` on `Root::ModifierParent` is package-private and cannot be called from package `Root::B::Test`
       package_private_protected
+    # ^^^^^^^^^^^^^^^^^^^^^^^^^ error: Method `package_private_protected` on `Root::ModifierParent` is package-private and cannot be called from package `Root::B::Test`
       protected_package_private
     # ^^^^^^^^^^^^^^^^^^^^^^^^^ error: Method `protected_package_private` on `Root::ModifierParent` is package-private and cannot be called from package `Root::B::Test`
       package_private_public
+    # ^^^^^^^^^^^^^^^^^^^^^^ error: Method `package_private_public` on `Root::ModifierParent` is package-private and cannot be called from package `Root::B::Test`
       public_package_private
     # ^^^^^^^^^^^^^^^^^^^^^^ error: Method `public_package_private` on `Root::ModifierParent` is package-private and cannot be called from package `Root::B::Test`
     end
